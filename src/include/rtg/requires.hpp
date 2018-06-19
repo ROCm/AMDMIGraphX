43dc4ccf7e5fb8dd8d1867a16e78d9a031d7edde
Paul <pfultz2@yahoo.com>
2018-06-19 14:35:35 -0500

    Add fixes for cppcheck

--- src/include/rtg/requires.hpp
+++ src/include/rtg/requires.hpp
@@ -6,11 +6,15 @@
 namespace rtg {
 
 template <bool... Bs>
-struct and_ : std::is_same<and_<Bs...>, and_<(Bs || true)...>>
+struct and_ : std::is_same<and_<Bs...>, and_<(Bs || true)...>> // NOLINT
 {
 };
 
+#ifdef CPPCHECK
+#define RTG_REQUIRES(...) class=void
+#else
 #define RTG_REQUIRES(...) class = typename std::enable_if<and_<__VA_ARGS__, true>{}>::type
+#endif
 
 } // namespace rtg
 

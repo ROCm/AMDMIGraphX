df46bb45014497ef256da457a2ac25618e45a006
Scott Thornton <wsttiger@gmail.com>
2018-06-20 14:01:16 -0500

    fixed cppcheck + clang formatting

--- test/cpu_ops_test.cpp
+++ test/cpu_ops_test.cpp
@@ -80,42 +80,42 @@ void add_test()
 void broadcast_test()
 {
     rtg::program p;
-    rtg::shape a_shape{rtg::shape::int32_type, {2,2}};
-    std::vector<int32_t> a_data{0,0,0,0};
+    rtg::shape a_shape{rtg::shape::int32_type, {2, 2}};
+    std::vector<int32_t> a_data{0, 0, 0, 0};
     rtg::shape b_shape{rtg::shape::int32_type, {2}};
-    std::vector<int32_t> b_data{-2,-3};
+    std::vector<int32_t> b_data{-2, -3};
     uint64_t axis = 0;
-    auto l1 = p.add_literal(rtg::literal{a_shape, a_data});
-    auto l2 = p.add_literal(rtg::literal{b_shape, b_data});
+    auto l1       = p.add_literal(rtg::literal{a_shape, a_data});
+    auto l2       = p.add_literal(rtg::literal{b_shape, b_data});
     p.add_instruction(rtg::broadcast{axis}, l1, l2);
     p.compile(rtg::cpu::cpu_target{});
     auto result = p.eval({});
     std::vector<int32_t> results_vector(4);
 
     // result.visit([&](auto output) {
-    //     EXPECT(output(0,0) == -2); 
-    //     EXPECT(output(0,1) == -2); 
-    //     EXPECT(output(1,0) == -3); 
-    //     EXPECT(output(1,1) == -3); 
+    //     EXPECT(output(0,0) == -2);
+    //     EXPECT(output(0,1) == -2);
+    //     EXPECT(output(1,0) == -3);
+    //     EXPECT(output(1,1) == -3);
     // });
 }
 void add_broadcast_test()
 {
     rtg::program p;
-    rtg::shape a_shape{rtg::shape::float_type, {2,2,3}};
-    std::vector<float> a_data{0,1,2,3,4,5,6,7,8,9,10,11};
-    rtg::shape b_shape{rtg::shape::float_type, {2,2}};
-    std::vector<float> b_data{0,-1,-2,-3};
+    rtg::shape a_shape{rtg::shape::float_type, {2, 2, 3}};
+    std::vector<float> a_data{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    rtg::shape b_shape{rtg::shape::float_type, {2, 2}};
+    std::vector<float> b_data{0, -1, -2, -3};
     uint64_t axis = 0;
-    auto l1 = p.add_literal(rtg::literal{a_shape, a_data});
-    auto l2 = p.add_literal(rtg::literal{b_shape, b_data});
-    auto l3 = p.add_instruction(rtg::broadcast{axis}, l1, l2);
+    auto l1       = p.add_literal(rtg::literal{a_shape, a_data});
+    auto l2       = p.add_literal(rtg::literal{b_shape, b_data});
+    auto l3       = p.add_instruction(rtg::broadcast{axis}, l1, l2);
     p.add_instruction(rtg::add{}, l1, l3);
     p.compile(rtg::cpu::cpu_target{});
     auto result = p.eval({});
     std::vector<float> results_vector(12);
     result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
-    std::vector<float> gold = {0,1,2,2,3,4,4,5,6,6,7,8};
+    std::vector<float> gold = {0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8};
     EXPECT(test::verify_range(results_vector, gold));
 }
 

28a0263d9087737c68e6c1630accc6513910cb9b
Paul <pfultz2@yahoo.com>
2018-06-21 19:40:26 -0500

    Formatting

--- src/generate.cpp
+++ src/generate.cpp
@@ -5,11 +5,10 @@ namespace rtg {
 rtg::argument generate_argument(rtg::shape s, std::mt19937::result_type seed)
 {
     rtg::argument result;
-    s.visit_type([&](auto as)
-    {
+    s.visit_type([&](auto as) {
         using type = typename decltype(as)::type;
-        auto v = generate_tensor_data<type>(s, seed);
-        result = {s, [v]() mutable { return reinterpret_cast<char*>(v.data()); }};
+        auto v     = generate_tensor_data<type>(s, seed);
+        result     = {s, [v]() mutable { return reinterpret_cast<char*>(v.data()); }};
     });
     return result;
 }

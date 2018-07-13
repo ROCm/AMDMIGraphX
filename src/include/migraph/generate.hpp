3bbb328a84e8af371b291bdbe53bf8fcd8995fc6
Paul <pfultz2@yahoo.com>
2018-07-12 21:58:06 -0500

    Add test for literals

--- src/include/migraph/generate.hpp
+++ src/include/migraph/generate.hpp
@@ -2,6 +2,7 @@
 #define MIGRAPH_GUARD_MIGRAPHLIB_GENERATE_HPP
 
 #include <migraph/argument.hpp>
+#include <migraph/literal.hpp>
 #include <random>
 
 namespace migraph {
@@ -16,7 +17,9 @@ std::vector<T> generate_tensor_data(migraph::shape s, std::mt19937::result_type 
     return result;
 }
 
-migraph::argument generate_argument(migraph::shape s, std::mt19937::result_type seed = 0);
+argument generate_argument(shape s, std::mt19937::result_type seed = 0);
+
+literal generate_literal(shape s, std::mt19937::result_type seed = 0);
 
 } // namespace migraph
 

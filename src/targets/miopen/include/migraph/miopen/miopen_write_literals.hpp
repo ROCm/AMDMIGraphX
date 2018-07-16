a385c8cf788de38e63f9043300b32fbba7cf48f9
Paul <pfultz2@yahoo.com>
2018-07-16 16:50:16 -0500

    Use lowering class

---
+++ src/targets/miopen/include/migraph/miopen/miopen_write_literals.hpp
@@ -0,0 +1,21 @@
+#ifndef MIGRAPH_GUARD_RTGLIB_MIOPEN_WRITE_LITERALS_HPP
+#define MIGRAPH_GUARD_RTGLIB_MIOPEN_WRITE_LITERALS_HPP
+
+#include <migraph/program.hpp>
+
+namespace migraph {
+
+namespace miopen {
+
+struct miopen_write_literals
+{
+    std::string name() const { return "miopen::write_literals"; }
+
+    void apply(program& p) const;
+};
+
+} // namespace miopen
+
+} // namespace migraph
+
+#endif

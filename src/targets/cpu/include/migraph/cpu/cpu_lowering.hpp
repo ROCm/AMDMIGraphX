a47f8e4b54fce9bdb91c907385b978f01657632e
Paul <pfultz2@yahoo.com>
2018-07-16 17:17:28 -0500

    Refactor cpu lowering

---
+++ src/targets/cpu/include/migraph/cpu/cpu_lowering.hpp
@@ -0,0 +1,19 @@
+#ifndef MIGRAPH_GUARD_RTGLIB_CPU_LOWERING_HPP
+#define MIGRAPH_GUARD_RTGLIB_CPU_LOWERING_HPP
+
+#include <migraph/program.hpp>
+
+namespace migraph {
+namespace cpu {
+
+struct cpu_lowering
+{
+    std::string name() const { return "cpu::lowering"; }
+    void apply(program& p) const;
+};
+
+} // namespace cpu
+
+} // namespace migraph
+
+#endif

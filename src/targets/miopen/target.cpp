bbac80d2c6b3b81153f101ed984abc4d380d4d6d
Paul <pfultz2@yahoo.com>
2018-07-18 18:32:43 -0500

    Formatting

--- src/targets/miopen/target.cpp
+++ src/targets/miopen/target.cpp
@@ -6,14 +6,17 @@
 namespace migraph {
 namespace miopen {
 
-std::vector<pass> target::get_passes(migraph::context&) const { return {lowering{}, write_literals{}}; }
+std::vector<pass> target::get_passes(migraph::context&) const
+{
+    return {lowering{}, write_literals{}};
+}
 
 std::string target::name() const { return "miopen"; }
 
 migraph::context target::get_context() const
 {
     return context{share(make_obj<miopen_handle>(&miopenCreate)),
-                          share(create_rocblas_handle_ptr())};
+                   share(create_rocblas_handle_ptr())};
 }
 
 } // namespace miopen

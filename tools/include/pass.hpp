4a99d2c4f4bf270b03040fc487018d5b6fba8a78
Paul <pfultz2@yahoo.com>
2018-07-20 16:50:54 -0500

    Add documentation for the interfaces

--- tools/include/pass.hpp
+++ tools/include/pass.hpp
@@ -11,12 +11,27 @@ namespace migraph {
 
 struct program;
 
+#ifdef DOXYGEN
+
+/// This applies a transformation to the instruction in a `program`
+struct pass
+{
+    /// A unique name used to identify the pass
+    std::string name() const;
+    /// Run the pass on the program
+    void apply(program & p) const;
+};
+
+#else
+
 <%
 interface('pass',
     virtual('name', returns='std::string', const=True),
     virtual('apply', returns='void', p='program &', const=True)
 )
 %>
+
+#endif
 
 } // namespace migraph
 

48f35aa01d63d4b3554ea8831422615150dc0623
Paul <pfultz2@yahoo.com>
2018-08-13 18:50:22 -0500

    Formatting

--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -102,12 +102,13 @@ struct miopen_convolution
         auto y_desc = make_tensor(output_shape);
 
         std::size_t workspace_size = 0;
-        miopenConvolutionForwardGetWorkSpaceSize(ctx.handle.get(), x_desc.get(), w_desc.get(), cd.get(), y_desc.get(), &workspace_size);
+        miopenConvolutionForwardGetWorkSpaceSize(
+            ctx.handle.get(), x_desc.get(), w_desc.get(), cd.get(), y_desc.get(), &workspace_size);
         workspace_shape = shape{shape::int8_type, {workspace_size}};
 
-        auto x = to_gpu(generate_argument(inputs[0]->get_shape()));
-        auto w = to_gpu(generate_argument(inputs[1]->get_shape()));
-        auto y = to_gpu(generate_argument(output_shape));
+        auto x         = to_gpu(generate_argument(inputs[0]->get_shape()));
+        auto w         = to_gpu(generate_argument(inputs[1]->get_shape()));
+        auto y         = to_gpu(generate_argument(output_shape));
         auto workspace = allocate_gpu(workspace_shape);
 
         int algo_count;
@@ -355,12 +356,13 @@ struct miopen_apply
         auto&& op = any_cast<convolution>(ins->op);
 
         auto conv = miopen_convolution{op, make_conv(op)};
-        auto ws = conv.compile(ctx, ins->result, ins->arguments);
+        auto ws   = conv.compile(ctx, ins->result, ins->arguments);
 
         auto workspace = insert_allocation(ins, ws);
-        auto output = insert_allocation(ins, ins->result);
+        auto output    = insert_allocation(ins, ins->result);
 
-        prog->replace_instruction(ins, conv, ins->arguments.at(0), ins->arguments.at(1), workspace, output);
+        prog->replace_instruction(
+            ins, conv, ins->arguments.at(0), ins->arguments.at(1), workspace, output);
     }
 
     void apply_pooling(instruction_ref ins)

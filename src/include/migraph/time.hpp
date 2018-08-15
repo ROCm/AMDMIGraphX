7f1d94abc6236fe8fb2f20f50a3a640815ac78fb
Paul <pfultz2@yahoo.com>
2018-08-15 18:16:06 -0500

    Add some initial perf refactoring

---
+++ src/include/migraph/time.hpp
@@ -0,0 +1,19 @@
+#ifndef MIGRAPH_GUARD_RTGLIB_TIME_HPP
+#define MIGRAPH_GUARD_RTGLIB_TIME_HPP
+
+#include <chrono>
+
+namespace migraph {
+
+template<class Duration, class F>
+auto time(F f)
+{
+    auto start = std::chrono::steady_clock::now();
+    f();
+    auto finish = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<Duration>(finish - start).count();
+}
+
+} // namespace migraph
+
+#endif

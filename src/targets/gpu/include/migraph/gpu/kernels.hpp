c86c96266b846f23d2f898601711ea7f6401420d
Paul <pfultz2@yahoo.com>
2018-08-23 18:58:05 -0500

    Add header tests for cpu and gpu targets as well

--- src/targets/gpu/include/migraph/gpu/kernels.hpp
+++ src/targets/gpu/include/migraph/gpu/kernels.hpp
@@ -1,9 +1,12 @@
 #ifndef MIGRAPH_GUARD_MIGRAPHLIB_KERNELS_HPP
 #define MIGRAPH_GUARD_MIGRAPHLIB_KERNELS_HPP
+
+#include <migraph/argument.hpp>
+
 namespace migraph {
 namespace gpu {
 
-void hip_contiguous(migraph::shape output_shape, migraph::argument arg, migraph::argument result);
+void hip_contiguous(shape output_shape, argument arg, argument result);
 
 } // namespace gpu
 

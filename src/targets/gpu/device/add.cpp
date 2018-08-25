549a4dc58b821237df74899d84924063d1bf0070
Paul <pfultz2@yahoo.com>
2018-08-24 22:45:14 -0500

    Formatting

--- src/targets/gpu/device/add.cpp
+++ src/targets/gpu/device/add.cpp
@@ -7,8 +7,7 @@ namespace device {
 
 void add(argument result, argument arg1, argument arg2)
 {
-    nary(std::move(result), std::move(arg1), std::move(arg2))(
-        [](auto x, auto y) { return x + y; });
+    nary(std::move(result), std::move(arg1), std::move(arg2))([](auto x, auto y) { return x + y; });
 }
 
 } // namespace device

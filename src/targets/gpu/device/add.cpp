b2ccccde0643f3a0629874aa1b03ff736919aa9f
Paul <pfultz2@yahoo.com>
2018-08-24 22:45:05 -0500

    Add an add operator

--- src/targets/gpu/device/add.cpp
+++ src/targets/gpu/device/add.cpp
@@ -1,14 +1,14 @@
-#include <migraph/gpu/device/add_relu.hpp>
+#include <migraph/gpu/device/add.hpp>
 #include <migraph/gpu/device/nary.hpp>
 
 namespace migraph {
 namespace gpu {
 namespace device {
 
-void add_relu(argument result, argument arg1, argument arg2)
+void add(argument result, argument arg1, argument arg2)
 {
     nary(std::move(result), std::move(arg1), std::move(arg2))(
-        [](auto x, auto y) { return max(0, x + y); });
+        [](auto x, auto y) { return x + y; });
 }
 
 } // namespace device

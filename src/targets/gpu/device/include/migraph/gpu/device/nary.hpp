93dc36d4ee91323eb929180a03e22ed64c895fb7
Paul <pfultz2@yahoo.com>
2018-08-28 02:15:31 -0500

    Latest

--- src/targets/gpu/device/include/migraph/gpu/device/nary.hpp
+++ src/targets/gpu/device/include/migraph/gpu/device/nary.hpp
@@ -16,7 +16,23 @@ using vec4 = T __attribute__((ext_vector_type(4)));
 template <class T>
 vec4<T>* as_vec4(T* x)
 {
+    std::uintptr_t a = reinterpret_cast<std::uintptr_t>(x);
+    if(a % 32 != 0)
+        throw std::runtime_error("Memory not aligned for vector operations");
     return reinterpret_cast<vec4<T>*>(x);
+    // return (vec4<T>*)(x);
+}
+
+template <class T>
+vec4<T> vec4_load(T* x, size_t i)
+{
+    vec4<T> result;
+    auto n = i * 4;
+    result[0] = x[n + 0];
+    result[1] = x[n + 1];
+    result[2] = x[n + 2];
+    result[3] = x[n + 3];
+    return result;
 }
 
 template <class... Ts>
@@ -64,6 +80,33 @@ inline auto binary_broadcast(argument result, argument arg1, argument arg2)
 
         visit_all(result, arg1, arg2)([&](auto output, auto input1, auto input2) {
             using type = std::remove_cv_t<typename decltype(output)::value_type>;
+#if 1
+            auto* xp   = input1.data();
+            auto* yp   = input2.data();
+            auto* outp = output.data();
+
+            const std::size_t nlocal       = 1024;
+            const std::size_t nglobal      = 256 * nlocal;
+            const std::size_t n            = output.size();
+
+            launch(nglobal, nlocal)([=](auto idx) __device__ {
+                __shared__ type buffer[2048];
+                // Load bias into LDS
+                for(size_t i = idx.local; i < bdim_len; i += nlocal)
+                {
+                    buffer[i] = yp[i];
+                }
+                __syncthreads();
+                // Process the data
+                for(size_t i = idx.global; i < n; i += nglobal)
+                {
+                    auto bidx      = i % bdim_len;
+                    auto b         = buffer[bidx];
+                    type x   = xp[i];
+                    outp[i] = f(x, b);
+                }
+            });
+#else
             auto* xp   = as_vec4(input1.data());
             auto* yp   = as_vec4(input2.data());
             auto* outp = as_vec4(output.data());
@@ -72,18 +115,31 @@ inline auto binary_broadcast(argument result, argument arg1, argument arg2)
             const std::size_t nlocal       = 1024;
             const std::size_t nglobal      = 256 * nlocal;
             const std::size_t n            = output.size() / vec_size;
+            const std::size_t rem          = output.size() % vec_size;
             const std::size_t bdim_vec_len = bdim_len / vec_size;
+            const std::size_t bdim_vec_rem = bdim_len % vec_size;
 
             launch(nglobal, nlocal)([=](auto idx) __device__ {
                 __shared__ vec4<type> buffer[2048];
-                for(size_t i = idx.local; i < bdim_len / vec_size; i += nlocal)
+                // Load bias into LDS
+                for(size_t i = idx.local; i < bdim_vec_len; i += nlocal)
                 {
                     buffer[i] = yp[i];
                 }
+                // Load remaining bias data
+                for(size_t i = idx.local; i < bdim_vec_rem; i += nlocal)
+                {
+                    buffer[bdim_vec_len][i] = yp[bdim_vec_len][i];
+                }
+                for(size_t i = idx.local; i < (vec_size-bdim_vec_rem); i += nlocal)
+                {
+                    buffer[bdim_vec_len][i] = yp[0][i];
+                }
                 __syncthreads();
+                // Process the data
                 for(size_t i = idx.global; i < n; i += nglobal)
                 {
-                    auto bidx      = i % bdim_vec_len;
+                    auto bidx      = bdim_vec_len == 0 ? 0 : i % bdim_vec_len;
                     auto b         = buffer[bidx];
                     vec4<type> x   = xp[i];
                     vec4<type> out = outp[i];
@@ -93,7 +149,12 @@ inline auto binary_broadcast(argument result, argument arg1, argument arg2)
                     }
                     outp[i] = out;
                 }
+                for(size_t i = idx.global; i < rem; i += nglobal)
+                {
+                    outp[n][i] = f(xp[n][i], buffer[bdim_vec_len][i]);
+                }
             });
+#endif
         });
     };
 }
@@ -157,20 +218,26 @@ auto nary(argument result, Arguments... args)
 inline auto nary(argument result, argument arg1, argument arg2)
 {
     return [=](auto f) {
-        // TODO: Check for one broadcast stride
         // TODO: Check result and arg1 shape is the same
-        // TODO: CHeck that broadcast shape doesnt have more than 2048 elements
         if(arg1.get_shape().standard() and arg2.get_shape().broadcasted() and
            std::count_if(arg2.get_shape().strides().begin(),
                          arg2.get_shape().strides().end(),
                          [](auto x) { return x != 0; }) == 1)
         {
-            binary_broadcast(result, arg1, arg2)(f);
+            auto not_zero = [](auto x) { return x != 0; };
+            const auto& strides = arg2.get_shape().strides();
+            auto stride_it = std::find_if(strides.begin(),
+                         strides.end(), not_zero);
+            auto stride_idx = std::distance(strides.begin(), stride_it);
+            auto stride_len = arg2.get_shape().lens()[stride_idx];
+            // TODO: Dont require disibility by 4
+            bool divisible_by_4 = (stride_len % 4 == 0) and (arg1.get_shape().elements() % 4 == 0);
+            if(divisible_by_4 and stride_len <= 2048 and std::none_of(std::next(stride_it), strides.end(), not_zero)) {
+                binary_broadcast(result, arg1, arg2)(f);
+                return;
+            }
         }
-        else
-        {
-            nary_impl(result, arg1, arg2)(f);
-        }
+        nary_impl(result, arg1, arg2)(f);
     };
 }
 

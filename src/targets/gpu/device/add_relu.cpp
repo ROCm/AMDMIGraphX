26a7875041ce2c095bd93fa8c04887845403763d
Paul <pfultz2@yahoo.com>
2018-08-28 17:04:17 -0500

    Formatting

--- src/targets/gpu/device/add_relu.cpp
+++ src/targets/gpu/device/add_relu.cpp
@@ -7,8 +7,7 @@ namespace device {
 
 void add_relu(const argument& result, const argument& arg1, const argument& arg2)
 {
-    nary(result, arg1, arg2)(
-        [](auto x, auto y) { return std::max<decltype(x + y)>(0, x + y); });
+    nary(result, arg1, arg2)([](auto x, auto y) { return std::max<decltype(x + y)>(0, x + y); });
 }
 
 } // namespace device

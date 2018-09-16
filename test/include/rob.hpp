6ecfa3dcd3c82f0e78cd5854715bd6f59a66f279
Paul <pfultz2@yahoo.com>
2018-09-16 01:59:19 -0500

    Formating

--- test/include/rob.hpp
+++ test/include/rob.hpp
@@ -10,33 +10,36 @@
 template <class Tag>
 struct stowed
 {
-     static typename Tag::type value;
-}; 
-template <class Tag> 
+    static typename Tag::type value;
+};
+template <class Tag>
 typename Tag::type stowed<Tag>::value;
 
 template <class Tag, typename Tag::type x>
 struct stow_private
 {
-     stow_private() { stowed<Tag>::value = x; }
-     static stow_private instance;
+    stow_private() { stowed<Tag>::value = x; }
+    static stow_private instance;
 };
-template <class Tag, typename Tag::type x> 
-stow_private<Tag,x> stow_private<Tag,x>::instance;
+template <class Tag, typename Tag::type x>
+stow_private<Tag, x> stow_private<Tag, x>::instance;
 
-template<class C, class T>
-struct mem_data_ptr { typedef T(C::*type); };
+template <class C, class T>
+struct mem_data_ptr
+{
+    typedef T(C::*type);
+};
 
-#define MIGRAPH_ROB(name, Type, C, mem) \
-struct name ## _tag \
-: mem_data_ptr<C, Type> \
-{}; \
-template struct stow_private<name ## _tag,&C::mem>; \
-template<class T> \
-auto& name(T&& x) \
-{ \
-    return x.*stowed<name ## _tag>::value; \
-}
+#define MIGRAPH_ROB(name, Type, C, mem)                \
+    struct name##_tag : mem_data_ptr<C, Type>          \
+    {                                                  \
+    };                                                 \
+    template struct stow_private<name##_tag, &C::mem>; \
+    template <class T>                                 \
+    auto& name(T&& x)                                  \
+    {                                                  \
+        return x.*stowed<name##_tag>::value;           \
+    }
 
 #ifdef __clang__
 #pragma clang diagnostic pop

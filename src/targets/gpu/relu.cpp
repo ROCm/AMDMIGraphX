ad0ab35784cb456548d589a77449df3b89a999cf
wsttiger <wsttiger@gmail.com>
2018-10-01 12:00:58 -0500

    Formatting

--- src/targets/gpu/relu.cpp
+++ src/targets/gpu/relu.cpp
@@ -13,8 +13,9 @@ shape miopen_relu::compute_shape(const std::vector<shape>& inputs) const
     return inputs.at(1);
 }
 
-argument
-miopen_relu::compute(context& ctx, const shape& output_shape, const std::vector<argument>& args) const
+argument miopen_relu::compute(context& ctx,
+                              const shape& output_shape,
+                              const std::vector<argument>& args) const
 {
     float alpha = 1, beta = 0;
     auto x_desc = make_tensor(args[0].get_shape());

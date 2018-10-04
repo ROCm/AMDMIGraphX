cfdff4f668d7aaa6798053ae2d00e87b14fd9b0a
Paul <pfultz2@yahoo.com>
2018-10-03 19:29:03 -0500

    Formatting

--- src/opt/memory_coloring_impl.cpp
+++ src/opt/memory_coloring_impl.cpp
@@ -184,17 +184,17 @@ void memory_coloring_impl::build()
 
 void memory_coloring_impl::register_operand_alias()
 {
-    operand_alias["hip::allocate"] = -1;
+    operand_alias["hip::allocate"]     = -1;
     operand_alias["hip::load_literal"] = -1;
-    operand_alias["@outline"]      = -1;
-    operand_alias["check_context"] = -1;
-    operand_alias["@literal"]      = -1;
-    operand_alias["@param"]        = -1;
-    operand_alias["transpose"]     = 0;
-    operand_alias["flatten"]       = 0;
-    operand_alias["broadcast"]     = 1;
-    operand_alias["reshape"]       = 0;
-    operand_alias["pass"]          = 0;
+    operand_alias["@outline"]          = -1;
+    operand_alias["check_context"]     = -1;
+    operand_alias["@literal"]          = -1;
+    operand_alias["@param"]            = -1;
+    operand_alias["transpose"]         = 0;
+    operand_alias["flatten"]           = 0;
+    operand_alias["broadcast"]         = 1;
+    operand_alias["reshape"]           = 0;
+    operand_alias["pass"]              = 0;
 }
 
 void memory_coloring_impl::rewrite()

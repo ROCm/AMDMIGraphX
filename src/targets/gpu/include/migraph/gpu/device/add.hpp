270194c4690f6adeefe874692f139bf2949450ce
Paul <pfultz2@yahoo.com>
2018-10-12 18:22:37 -0500

    Add triadd

--- src/targets/gpu/include/migraph/gpu/device/add.hpp
+++ src/targets/gpu/include/migraph/gpu/device/add.hpp
@@ -10,6 +10,8 @@ namespace device {
 
 void add(const argument& result, const argument& arg1, const argument& arg2);
 
+void add(const argument& result, const argument& arg1, const argument& arg2, const argument& arg3);
+
 } // namespace device
 } // namespace gpu
 } // namespace migraph

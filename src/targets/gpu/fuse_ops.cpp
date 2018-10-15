3dad1044046d2eb6fe6a584a1ea0f6bb9fe9d125
Paul <pfultz2@yahoo.com>
2018-10-15 11:45:06 -0500

    Formatting

--- src/targets/gpu/fuse_ops.cpp
+++ src/targets/gpu/fuse_ops.cpp
@@ -185,8 +185,10 @@ struct match_add_relu
 {
     auto matcher() const
     {
-        return match::name("gpu::relu")(match::used_once(), match::arg(0)(
-            match::any_of(match::name("gpu::add"), match::name("hip::triadd")).bind("add")));
+        return match::name("gpu::relu")(
+            match::used_once(),
+            match::arg(0)(
+                match::any_of(match::name("gpu::add"), match::name("hip::triadd")).bind("add")));
     }
 
     void apply(program& p, match::matcher_result r) const
@@ -356,7 +358,10 @@ struct match_conv_bias
 struct match_conv_bias_relu
 {
     context* ctx = nullptr;
-    auto matcher() const { return match::name("gpu::relu")(match::used_once(), match::arg(0)(conv_bias())); }
+    auto matcher() const
+    {
+        return match::name("gpu::relu")(match::used_once(), match::arg(0)(conv_bias()));
+    }
 
     void apply(program& p, match::matcher_result r) const
     {

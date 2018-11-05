f036db5bb7b7178d35675ef6077ea1a7bd1e0c3a
Paul <pfultz2@yahoo.com>
2018-11-05 11:16:49 -0600

    Formatting

--- test/output_alias.cpp
+++ test/output_alias.cpp
@@ -3,11 +3,10 @@
 #include <test.hpp>
 #include <basic_ops.hpp>
 
-
 void simple_alias()
 {
     migraph::program p;
-    auto l = p.add_literal(1);
+    auto l  = p.add_literal(1);
     auto p1 = p.add_instruction(pass_op{}, l);
     EXPECT(bool{migraph::instruction::get_output_alias(l) == l});
     EXPECT(bool{migraph::instruction::get_output_alias(p1) == l});
@@ -16,7 +15,7 @@ void simple_alias()
 void cascade_alias()
 {
     migraph::program p;
-    auto l = p.add_literal(1);
+    auto l  = p.add_literal(1);
     auto p1 = p.add_instruction(pass_op{}, l);
     auto p2 = p.add_instruction(pass_op{}, p1);
     auto p3 = p.add_instruction(pass_op{}, p2);
@@ -29,13 +28,14 @@ void cascade_alias()
 void no_alias()
 {
     migraph::program p;
-    auto x = p.add_literal(1);
-    auto y = p.add_literal(2);
+    auto x   = p.add_literal(1);
+    auto y   = p.add_literal(2);
     auto sum = p.add_instruction(sum_op{}, x, y);
     EXPECT(bool{migraph::instruction::get_output_alias(sum) == sum});
 }
 
-int main() {
+int main()
+{
     simple_alias();
     cascade_alias();
     no_alias();

47e22d89d4bcd1d74005a30f36642921b9694034
Paul <pfultz2@yahoo.com>
2018-11-07 12:18:02 -0600

    Fix clang tidy warnings

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -194,10 +194,11 @@ inline void add_test_case(std::string name, std::function<void()> f)
 
 struct auto_register
 {
-    auto_register(std::string name, std::function<void()> f) { add_test_case(name, f); }
+    template<class F>
+    auto_register(const char * name, F f) noexcept { add_test_case(name, f); }
 };
 
-inline void run_test_case(std::string name, std::function<void()> f)
+inline void run_test_case(const std::string& name, const std::function<void()>& f)
 {
     std::cout << "[   RUN    ] " << name << std::endl;
     f();

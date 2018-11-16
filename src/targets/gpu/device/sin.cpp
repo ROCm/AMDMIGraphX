bd4ced2149b8aa300210915a02edcebd7499c6d2
Shucai Xiao <shucai.xiao@amd.com>
2018-11-16 13:25:47 -0600

    Add the sin operator on GPU.

--- src/targets/gpu/device/sin.cpp
+++ src/targets/gpu/device/sin.cpp
@@ -1,5 +1,6 @@
 #include <migraphx/gpu/device/sin.hpp>
 #include <migraphx/gpu/device/nary.hpp>
+#include <migraphx/gpu/device/types.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPH_INLINE_NS {
@@ -8,7 +9,7 @@ namespace device {
 
 void sin(hipStream_t stream, const argument& result, const argument& arg)
 {
-    nary(stream, result, arg)([](auto x) { return sinf(x); });
+    nary(stream, result, arg)([](auto x) { return ::sin(x); });
 }
 
 } // namespace device

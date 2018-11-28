51aada7ed08e5c67bb3b4a9d4216a943318db78e
Khalique <kahmed10@illinois.edu>
2018-11-28 15:12:41 -0600

    formatting

--- src/targets/gpu/device/max.cpp
+++ src/targets/gpu/device/max.cpp
@@ -9,7 +9,8 @@ namespace device {
 
 void max(hipStream_t stream, const argument& result, const argument& arg1, const argument& arg2)
 {
-    nary(stream, result, arg1, arg2)([](auto x, auto y) { return std::max(to_hip_type(x), to_hip_type(y)); });
+    nary(stream, result, arg1, arg2)(
+        [](auto x, auto y) { return std::max(to_hip_type(x), to_hip_type(y)); });
 }
 
 } // namespace device

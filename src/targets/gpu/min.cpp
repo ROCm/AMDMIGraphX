502e60c1840e9f40071f999d88f3e5b8c381a14a
Khalique <kahmed10@illinois.edu>
2018-11-28 15:12:34 -0600

    added gpu version of max and min

---
+++ src/targets/gpu/min.cpp
@@ -0,0 +1,26 @@
+#include <migraphx/gpu/min.hpp>
+#include <migraphx/operators.hpp>
+#include <migraphx/manage_ptr.hpp>
+#include <migraphx/gpu/miopen.hpp>
+#include <utility>
+
+namespace migraphx {
+inline namespace MIGRAPH_INLINE_NS {
+namespace gpu {
+
+shape hip_min::compute_shape(const std::vector<shape>& inputs) const
+{
+    // check_shapes{inputs, *this}.has(3).standard();
+    check_shapes{inputs, *this}.has(3);
+    return inputs.at(0);
+}
+
+argument hip_min::compute(context& ctx, const shape&, const std::vector<argument>& args) const
+{
+    device::min(ctx.get_stream().get(), args[2], args[0], args[1]);
+    return args[2];
+}
+
+} // namespace gpu
+} // namespace MIGRAPH_INLINE_NS
+} // namespace migraphx

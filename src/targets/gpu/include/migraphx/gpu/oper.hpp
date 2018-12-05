24b9a339ef2da165186b6edb9e9da5d9f967ea6c
Shucai Xiao <Shucai.Xiao@amd.com>
2018-12-05 17:46:24 -0600

    clang format.

--- src/targets/gpu/include/migraphx/gpu/oper.hpp
+++ src/targets/gpu/include/migraphx/gpu/oper.hpp
@@ -26,18 +26,19 @@ namespace gpu {
 template <class Derived>
 struct oper
 {
-    std::string name() const {
+    std::string name() const
+    {
         const std::string& name = get_type_name<Derived>();
         // search the namespace gpu (::gpu::)
         auto pos_ns = name.find("::gpu::");
-        if (pos_ns != std::string::npos) 
+        if(pos_ns != std::string::npos)
         {
             auto pos_name = name.find("hip_", pos_ns + std::string("::gpu::").length());
-            if (pos_name != std::string::npos) 
+            if(pos_name != std::string::npos)
             {
                 return std::string("gpu::") + name.substr(pos_name + 4);
             }
-            else 
+            else
             {
                 return name.substr(pos_ns + 2);
             }

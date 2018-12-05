c63319bff42a73d1414d17ad99f3e704f75c1e73
Shucai Xiao <Shucai.Xiao@amd.com>
2018-12-05 17:45:45 -0600

    combine unary and binary gpu operators to simplify code.

--- src/targets/gpu/include/migraphx/gpu/oper.hpp
+++ src/targets/gpu/include/migraphx/gpu/oper.hpp
@@ -17,6 +17,7 @@
 #include <migraphx/gpu/context.hpp>
 #include <migraphx/config.hpp>
 #include <utility>
+#include <iostream>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -25,7 +26,25 @@ namespace gpu {
 template <class Derived>
 struct oper
 {
-    std::string name() const { return get_type_name<Derived>(); }
+    std::string name() const {
+        const std::string& name = get_type_name<Derived>();
+        // search the namespace gpu (::gpu::)
+        auto pos_ns = name.find("::gpu::");
+        if (pos_ns != std::string::npos) 
+        {
+            auto pos_name = name.find("hip_", pos_ns + std::string("::gpu::").length());
+            if (pos_name != std::string::npos) 
+            {
+                return std::string("gpu::") + name.substr(pos_name + 4);
+            }
+            else 
+            {
+                return name.substr(pos_ns + 2);
+            }
+        }
+
+        return "unknown";
+    }
 };
 
 template <class Derived, void (*F)(hipStream_t, const argument&, const argument&)>

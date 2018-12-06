5df4d2a8783caa755c473065a51b785ddf31fdb6
Khalique <kahmed10@illinois.edu>
2018-12-06 15:24:13 -0600

    formatting

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -441,14 +441,14 @@ void add_scalar_test()
 {
     migraphx::program p;
     auto l0 = p.add_parameter("0", migraphx::shape{migraphx::shape::float_type, {2, 3, 4, 5}});
-    auto l1 = p.add_literal(migraphx::literal{migraphx::shape{migraphx::shape::float_type, {1}}, {1}});
-    auto m0 = p.add_instruction(migraphx::op::multibroadcast{{0, 0, 0, 5}}, l0); 
+    auto l1 =
+        p.add_literal(migraphx::literal{migraphx::shape{migraphx::shape::float_type, {1}}, {1}});
+    auto m0 = p.add_instruction(migraphx::op::multibroadcast{{0, 0, 0, 5}}, l0);
     auto m1 = p.add_instruction(migraphx::op::multibroadcast{{0, 0, 0, 5}}, l1);
     p.add_instruction(migraphx::op::add{}, m0, m1);
     auto prog = migraphx::parse_onnx("add_scalar_test.onnx");
 
     EXPECT(p == prog);
-
 }
 
 int main()

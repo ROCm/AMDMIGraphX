40cf2b063774b7dd31076a17c930c72349c171b2
Paul <pfultz2@yahoo.com>
2019-01-07 17:42:40 -0600

    Fix clang tidy

--- src/targets/cpu/lowering.cpp
+++ src/targets/cpu/lowering.cpp
@@ -299,7 +299,7 @@ struct cpu_concat
     shape compute_shape(const std::vector<shape>& inputs) const { return op.compute_shape(inputs); }
     argument compute(context&, const shape& output_shape, std::vector<argument> args) const
     {
-        return op.compute(output_shape, args);
+        return op.compute(output_shape, std::move(args));
     }
 };
 

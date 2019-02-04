1b6387fab0eadef94e0b129e97bbeeaeeda430d1
Paul <pfultz2@yahoo.com>
2019-02-04 12:10:46 -0600

    Add more complete test

--- test/py/test.py
+++ test/py/test.py
@@ -1,3 +1,11 @@
 import migraphx
 
 p = migraphx.parse_onnx("conv_relu_maxpool.onnx")
+p.compile(migraphx.get_target("cpu"))
+
+params = {}
+for key, value in p.get_parameter_shapes().items():
+    params[key] = migraphx.generate_argument(value)
+
+r = p.run(params)
+print(r)

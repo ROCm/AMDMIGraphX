4b00457d31048d1f2d109b38b5e693d1aeaafc64
Paul <pfultz2@yahoo.com>
2019-02-04 12:39:23 -0600

    Make program and shape printable

--- test/py/test.py
+++ test/py/test.py
@@ -1,10 +1,13 @@
 import migraphx
 
 p = migraphx.parse_onnx("conv_relu_maxpool.onnx")
+print(p)
+print("Compiling ...")
 p.compile(migraphx.get_target("cpu"))
-
+print(p)
 params = {}
 for key, value in p.get_parameter_shapes().items():
+    print("Parameter {} -> {}".format(key, value))
     params[key] = migraphx.generate_argument(value)
 
 r = p.run(params)

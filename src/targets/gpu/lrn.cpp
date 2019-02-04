e7cf8a26a011dbb89191a60c66c5e489174a2187
Khalique <kahmed10@illinois.edu>
2019-02-04 17:46:36 -0600

    fix style warning

--- src/targets/gpu/lrn.cpp
+++ src/targets/gpu/lrn.cpp
@@ -18,7 +18,8 @@ argument miopen_lrn::compute(context& ctx,
                              const shape& output_shape,
                              const std::vector<argument>& args) const
 {
-    float alpha = 1, beta = 0;
+    float alpha = 1;
+    float beta  = 0;
     auto x_desc = make_tensor(args[0].get_shape());
     auto y_desc = make_tensor(output_shape);
     miopenLRNForward(ctx.get_stream().get_miopen(),

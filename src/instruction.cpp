ef6036471626872d6dd4f173bc844301683f5031
Paul <pfultz2@yahoo.com>
2019-02-09 00:28:38 -0600

    Check equality operator for instruction class

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -97,7 +97,8 @@ const std::vector<instruction_ref>& instruction::outputs() const { return output
 
 bool operator==(const instruction& x, const instruction& y)
 {
-    if(not(x.result == y.result and x.op == y.op and x.arguments == y.arguments))
+    static_assert(sizeof(instruction) == (sizeof(x.op) + sizeof(x.result) + sizeof(x.output) + sizeof(x.arguments) + sizeof(x.lit)), "Update equality operator");
+    if(std::tie(x.result, x.op, x.arguments) != std::tie(y.result, y.op, y.arguments))
         return false;
     if(x.name() == "@literal")
         return x.lit == y.lit;

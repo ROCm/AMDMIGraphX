fc54f5d55070715e0f0d635b0fe67855dea7f622
Paul <pfultz2@yahoo.com>
2019-02-09 22:37:52 -0600

    Add equality operators

--- test/py/cpu.py
+++ test/py/cpu.py
@@ -2,9 +2,12 @@ import migraphx
 
 p = migraphx.parse_onnx("conv_relu_maxpool.onnx")
 print(p)
+s1 = p.get_shape()
 print("Compiling ...")
 p.compile(migraphx.get_target("cpu"))
 print(p)
+s2 = p.get_shape()
+assert s1 == s2
 params = {}
 for key, value in p.get_parameter_shapes().items():
     print("Parameter {} -> {}".format(key, value))

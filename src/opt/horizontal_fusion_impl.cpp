0025a8eaeb5c5edbf317fe94aff88fcf3ec0a1b1
mei-ye <meiandmimi@yahoo.com>
2019-02-19 06:46:00 +0000

    build tree of hash values

--- src/opt/horizontal_fusion_impl.cpp
+++ src/opt/horizontal_fusion_impl.cpp
@@ -27,23 +27,51 @@ unsigned short horizontal_fusion_impl::get_opcode(instruction_ref ins)
 
 // Encode "conv_bias_relu":
 //           
-// |----- 16 bits -----|----- 16 bits -----|----- 8 bits -----|----- 8 bits-----|----- 16 bits -----|
-// |     opcode        | 1st operand value |   filter size    |  kernel size    |     0x0000        |
-unsigned long long EncodeConvBiasRelu(instruction_ref ins, unsigned short opcode)
+// |----- 16 bits -----|----- 16 bits -------|----- 8 bits -----|----- 8 bits-----|----- 16 bits -----|
+// |     opcode        | 1st operand hash id |   filter size    |  kernel size    |     0x0000        |
+encode_info EncodeConvBiasRelu(instruction_ref ins, unsigned short opcode, Ins2Val instr2_value)
 {
-    unsigned long long encode = (opcode << 48);
-    
-    return 0;
+    unsigned long long encode = (static_cast<unsigned long long>(opcode) << 48);
+    instruction_ref op1 = ins->inputs().front();
+    assert(instr2_value.find(op1) != instr2_value.end());
+    hash_value_ptr op1_val = instr2_value[op1];
+    encode |= (static_cast<unsigned long long>(op1_val->get_id()) << 32);
+    instruction_ref op2 = ins->inputs().at(1);
+    auto lens = op2->get_shape().lens();
+    auto filter = lens[lens.size() - 2];
+    auto kernel = lens[lens.size() - 1];
+    encode |= (filter << 24);
+    encode |= (kernel << 16);
+    encode_info info = encode_info{encode};
+    info.add_input(op1_val);
+    return info;
 }
-           
-               
+
+// Hash the instruction.           
 hash_value_ptr horizontal_fusion_impl::hash(instruction_ref ins)
 {
     if (op_registry.find(ins->name()) == op_registry.end())
         return nullptr;
-    Encoder encode = op_registry.at(ins->name());
-    unsigned long long val = encode(ins, get_opcode(ins));
-    return nullptr;
+
+    Encoder encode_func = op_registry.at(ins->name());
+    encode_info encode_val = encode_func(ins, get_opcode(ins), instr2_value);
+    unsigned long long encoding = encode_val.get_encoding();
+    hash_value_ptr hash_val = nullptr;
+
+    if (encode2_value.find(encoding) != encode2_value.end()) {
+        hash_val = encode2_value[encoding];
+        hash_val->add_instr(ins);
+        instr2_value[ins] = hash_val;
+    } else {
+        hash_val = &(create_value(ins));
+        encode2_value[encoding] = hash_val;
+    }
+    for (auto&& input : encode_val.get_inputs())
+    {
+        hash_val->add_input(input);
+        input->add_output(hash_val);
+    }
+    return hash_val;
 }
 void horizontal_fusion_impl::process(instruction_ref ins)
 {
@@ -55,7 +83,7 @@ void horizontal_fusion_impl::process(instruction_ref ins)
         // Hash this instruction.
         if (hash(ins) != nullptr)
         {
-            for (auto output : ins->outputs())
+            for (auto&& output : ins->outputs())
             {
                 instr2_hash[output] = true;
             } 
@@ -65,7 +93,7 @@ void horizontal_fusion_impl::process(instruction_ref ins)
         std::unordered_map<std::string, int> op2_cnt;
         bool hash_child = false;
         // Do hash if at least two outputs have same operations.
-        for (auto output : ins->outputs())
+        for (auto&& output : ins->outputs())
         {
             const std::string& str = output->name();
             if (op2_cnt.find(str) == op2_cnt.end())
@@ -82,7 +110,7 @@ void horizontal_fusion_impl::process(instruction_ref ins)
             hash_value& value = create_value(ins);
             add_root(&value);
             // Flag children to be hashed.
-            for (auto output : ins->outputs())
+            for (auto&& output : ins->outputs())
             {
                 if (op2_cnt[output->name()] > 1)
                     instr2_hash[output] = true;
--- src/opt/horizontal_fusion_impl.hpp
+++ src/opt/horizontal_fusion_impl.hpp
@@ -6,23 +6,52 @@
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
-using Encoder = std::function<unsigned long long(instruction_ref, unsigned short)>;
-           
+// Nodes representing hashed instructions.
 struct hash_value
 {
     hash_value(unsigned i) : id (i)
     {
     };
-    std::vector<instruction_ref> instrs;
-    std::vector<struct hash_value*> outputs;
-    unsigned id;
+
     void add_instr(instruction_ref ins)
     {
         instrs.push_back(ins);
     }
+
+    void add_input(struct hash_value* i) { inputs.push_back(i); }
+    void add_output(struct hash_value* o) { outputs.push_back(o); }
+
+    unsigned get_id() { return id; }
+    const std::vector<struct hash_value*>& get_inputs() const { return inputs; }
+    const std::vector<struct hash_value*>& get_outputs() const { return outputs; }
+    const std::vector<instruction_ref>& get_instrs() const { return instrs; }
+    
+private:
+    unsigned id;
+    std::vector<struct hash_value*> inputs;
+    std::vector<struct hash_value*> outputs;
+    std::vector<instruction_ref> instrs;
 };
 
-using hash_value_ptr = hash_value*;           
+using hash_value_ptr = hash_value*;
+
+//  Instruction encoding information, used to hash instructions.
+struct encode_info
+{
+    encode_info(unsigned long long e) : encoding (e)
+    {
+    };
+    void add_input(hash_value_ptr p) { inputs.push_back(p); }
+    unsigned long long get_encoding() const { return encoding; }
+    const std::vector<hash_value_ptr>& get_inputs() const { return inputs; }
+    private:
+    unsigned long long encoding;
+    std::vector<hash_value_ptr> inputs;
+};
+
+
+using Ins2Val = std::unordered_map<instruction_ref, hash_value_ptr>;
+using Encoder = std::function<encode_info(instruction_ref, unsigned short, Ins2Val&)>;
 
 struct horizontal_fusion_impl
 {
@@ -31,42 +60,56 @@ struct horizontal_fusion_impl
     {
         instr2_hash.clear();
         instr2_value.clear();
+        encode2_value.clear();
         register_all();
     }
     void run();
     void process(instruction_ref ins);
     hash_value_ptr hash(instruction_ref ins);
-    hash_value& create_value(instruction_ref ins)
-    {
-        unsigned id = static_cast<unsigned>(values.size());
-        values.push_back(hash_value{id});
-        hash_value& val = values[id];
-        val.add_instr(ins);
-        instr2_value[ins] = &val;
-        return val;
-    }
     void add_root(hash_value_ptr ptr)
     {
         root_values.push_back(ptr);
     }
-    void register_all();
-    void register_op(std::string, unsigned short, Encoder);
-    unsigned short get_opcode(instruction_ref ins);
 
+    unsigned short get_opcode(instruction_ref ins);
+    hash_value& get_value(unsigned id) { return values[id]; }
+
+    hash_value& create_value(instruction_ref ins)
+    {
+        unsigned id = static_cast<unsigned>(values.size());
+        values.push_back(hash_value{id});
+        hash_value& val = get_value(id);
+        val.add_instr(ins);
+        instr2_value[ins] = &val;
+        return val;
+    }
+
+    void register_op(std::string, unsigned short, Encoder);
+    void register_all();
+    
 #ifdef MIGRAPHX_DEBUG_OPT
     void dump_program();
 #endif
    private:
     program* p_program;
+    // Flag an instruction to hash.
     std::unordered_map<instruction_ref, bool> instr2_hash;
-    std::unordered_map<instruction_ref, hash_value_ptr> instr2_value;
+    // Map an instruction to a hash value pointer.
+    Ins2Val instr2_value;
+    // Map an encoding to a hash value pointer.
+    std::unordered_map<unsigned long long, hash_value_ptr> encode2_value;
+    // Map an operation name to its encoding.
     std::unordered_map<std::string, unsigned short> opcode_table;
+    // Map an operation name to its encoder fucntion.
     std::unordered_map<std::string, Encoder> op_registry;
+    // Universe of hash values.
     std::vector<hash_value> values;
+    // A collection of root nodes in the hash_value tree.
     std::vector<hash_value_ptr> root_values;
 };
 
-unsigned long long EncodeConvBiasRelu(instruction_ref in, unsigned short opcode);
+// Encoding functions.
+encode_info EncodeConvBiasRelu(instruction_ref in, unsigned short opcode, Ins2Val instr2_value);
            
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

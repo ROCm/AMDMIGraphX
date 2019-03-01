34d57fbce4ed518e074a2bd379964a8f83b44144
Paul <pfultz2@yahoo.com>
2019-03-01 16:42:04 -0600

    Add initial scheduler with topo sort

---
+++ src/targets/gpu/include/migraphx/gpu/schedule_model.hpp
@@ -0,0 +1,29 @@
+#ifndef MIGRAPHX_GUARD_RTGLIB_GPU_SCHEDULE_MODEL_HPP
+#define MIGRAPHX_GUARD_RTGLIB_GPU_SCHEDULE_MODEL_HPP
+
+#include <migraphx/config.hpp>
+#include <migraphx/instruction_ref.hpp>
+#include <vector>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+
+struct program;
+struct operation;
+
+namespace gpu {
+
+struct schedule_model
+{
+    std::size_t n = 4;
+    std::size_t concurrency() const;
+    void schedule_instruction(program& p, instruction_ref ins, std::size_t n) const;
+    void wait(program& p, instruction_ref ins, std::size_t wait_on, const std::vector<std::size_t>& wait_for) const;
+    std::size_t weight(const operation& op) const;
+};
+
+} // namespace gpu
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx
+
+#endif
---
+++ src/targets/gpu/schedule_model.cpp
@@ -0,0 +1,24 @@
+#include <migraphx/gpu/schedule_model.hpp>
+#include <migraphx/program.hpp>
+#include <migraphx/operation.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace gpu {
+
+std::size_t schedule_model::concurrency() const
+{
+    return n;
+}
+void schedule_model::schedule_instruction(program& p, instruction_ref ins, std::size_t n) const
+{}
+void schedule_model::wait(program& p, instruction_ref ins, std::size_t wait_on, const std::vector<std::size_t>& wait_for) const
+{}
+std::size_t schedule_model::weight(const operation& op) const
+{
+    return 1;
+}
+
+} // namespace gpu
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx

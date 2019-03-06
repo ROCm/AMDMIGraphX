bbd4e0c358e649e5675fc8c47fa207d2822acf56
Shucai Xiao <Shucai.Xiao@amd.com>
2019-03-06 11:17:53 -0600

    clang format

--- src/targets/cpu/gemm.cpp
+++ src/targets/cpu/gemm.cpp
@@ -55,7 +55,7 @@ void migemm_impl(tensor_view<T> cmat,
     visit_mat(amat, [&](const auto& a) {
         visit_mat(bmat, [&](const auto& b) {
             auto c = make_mat(cmat);
-            c = beta * c;
+            c      = beta * c;
 
             if(alpha != 0.0)
             {

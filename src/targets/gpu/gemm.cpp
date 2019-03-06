0b639e942fd242e86fab654637b2bfc539604058
Shucai Xiao <Shucai.Xiao@amd.com>
2019-03-06 15:46:45 -0600

    optimize the calling of rocmblas function.

--- src/targets/gpu/gemm.cpp
+++ src/targets/gpu/gemm.cpp
@@ -117,26 +117,47 @@ argument miopen_gemm::compute(context& ctx,
         auto alpha_r    = to_rocblas_type(as(alpha));
         auto beta_r     = to_rocblas_type(as(beta));
         auto to_pointer = [&](auto&& arg) { return to_rocblas_type(as.from(arg.data())); };
-        generic_rocblas_batched_gemm(as,
-                                     ctx.get_stream().get_rocblas(),
-                                     transb ? rocblas_operation_transpose : rocblas_operation_none,
-                                     transa ? rocblas_operation_transpose : rocblas_operation_none,
-                                     n,
-                                     m,
-                                     k,
-                                     &alpha_r,
-                                     to_pointer(args[1]),
-                                     ldb,
-                                     k * n,
-                                     to_pointer(args[0]),
-                                     lda,
-                                     m * k,
-                                     &beta_r,
-                                     to_pointer(args[2]),
-                                     ldc,
-                                     m * n,
-                                     batch_num);
-
+        // call the strided implementation only if there are multiple matrices
+        if (batch_num > 1)
+        {
+            generic_rocblas_batched_gemm(as,
+                                        ctx.get_stream().get_rocblas(),
+                                        transb ? rocblas_operation_transpose : rocblas_operation_none,
+                                        transa ? rocblas_operation_transpose : rocblas_operation_none,
+                                        n,
+                                        m,
+                                        k,
+                                        &alpha_r,
+                                        to_pointer(args[1]),
+                                        ldb,
+                                        k * n,
+                                        to_pointer(args[0]),
+                                        lda,
+                                        m * k,
+                                        &beta_r,
+                                        to_pointer(args[2]),
+                                        ldc,
+                                        m * n,
+                                        batch_num);
+        }
+        else
+        {
+            generic_rocblas_gemm(as,
+                                 ctx.get_stream().get_rocblas(),
+                                 transb ? rocblas_operation_transpose : rocblas_operation_none,
+                                 transa ? rocblas_operation_transpose : rocblas_operation_none,
+                                 n,
+                                 m,
+                                 k,
+                                 &alpha_r,
+                                 to_pointer(args[1]),
+                                 ldb,
+                                 to_pointer(args[0]),
+                                 lda,
+                                 &beta_r,
+                                 to_pointer(args[2]),
+                                 ldc);
+        }
     });
 
     return args[2];

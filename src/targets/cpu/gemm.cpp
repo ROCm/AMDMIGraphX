7a613711012d577c7ac5745a731919b32822c2dc
Shucai Xiao <Shucai.Xiao@amd.com>
2019-03-11 16:34:18 -0500

    Added the CPU implementation of the dot operator

--- src/targets/cpu/gemm.cpp
+++ src/targets/cpu/gemm.cpp
@@ -73,18 +73,32 @@ void migemm_impl(tensor_view<T> cmat,
                  float beta,
                  std::false_type)
 {
-    std::size_t n_dims = cmat.get_shape().lens().size();
+    auto a_lens = amat.get_shape().lens();
+    auto b_lens = bmat.get_shape().lens();
+    auto c_lens = cmat.get_shape().lens();
+
+    std::size_t n_dims = c_lens.size();
     std::size_t dim_0  = n_dims - 2;
     std::size_t dim_1  = n_dims - 1;
-    auto k             = amat.get_shape().lens()[dim_1];
+    auto k             = a_lens[dim_1];
 
-    assert(amat.get_shape().lens()[dim_1] == bmat.get_shape().lens()[dim_0]);
-    assert(cmat.get_shape().lens()[dim_0] == amat.get_shape().lens()[dim_0]);
-    assert(cmat.get_shape().lens()[dim_1] == bmat.get_shape().lens()[dim_1]);
+    assert(a_lens[dim_1] == b_lens[dim_0]);
+    assert(c_lens[dim_0] == a_lens[dim_0]);
+    assert(c_lens[dim_1] == b_lens[dim_1]);
+
+    std::size_t a_len_diff = c_lens.size() - a_lens.size();
+    std::size_t b_len_diff = c_lens.size() - b_lens.size();
+    std::vector<std::size_t> a_idx(a_lens.size());
+    std::vector<std::size_t> b_idx(b_lens.size());
 
     shape_for_each(cmat.get_shape(), [&](const auto& c_idx) {
-        auto a_idx = c_idx;
-        auto b_idx = c_idx;
+        std::transform(c_lens.begin() + a_len_diff, c_lens.end(), a_lens.begin(), a_idx.begin(), [&](auto i, auto j) {
+            return (j == 1) ? 0 : i;
+        });
+        std::transform(c_lens.begin() + b_len_diff, c_lens.end(), b_lens.begin(), b_idx.begin(), [&](auto i, auto j) {
+            return (j == 1) ? 0 : i;
+        });
+
         double s   = 0.0;
         dfor(k)([&](auto kk) {
             a_idx[dim_1] = b_idx[dim_0] = kk;
@@ -98,11 +112,10 @@ template <class T>
 void migemm_impl(
     tensor_view<T> cmat, tensor_view<T> amat, tensor_view<T> bmat, float alpha, float beta)
 {
-    auto lens = amat.get_shape().lens();
-    bool batch_mul =
-        std::accumulate(lens.begin(), lens.end(), std::size_t{1}, std::multiplies<std::size_t>()) ==
-        (*lens.rbegin()) * (*(lens.rbegin() + 1));
-    if(batch_mul)
+    auto lens = cmat.get_shape().lens();
+    std::size_t num_matrices =
+        std::accumulate(lens.rbegin() + 2, lens.rend(), std::size_t{1}, std::multiplies<std::size_t>());
+    if(num_matrices == 1)
     {
         migemm_impl(cmat, amat, bmat, alpha, beta, is_fast_gemm_type<T>{});
     }

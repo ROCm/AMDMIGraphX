71d4f4c5a590d5351c47a84233a68e19b859c943
Khalique <kahmed10@illinois.edu>
2019-03-13 11:49:21 -0500

    added files for eliminating identity

---
+++ src/eliminate_identity.cpp
@@ -0,0 +1,39 @@
+#include <migraphx/eliminate_identity.hpp>
+#include <migraphx/program.hpp>
+#include <migraphx/instruction.hpp>
+#include <migraphx/operators.hpp>
+#include <migraphx/iterator_for.hpp>
+#include <migraphx/ranges.hpp>
+#include <migraphx/stringutils.hpp>
+#include <utility>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+
+
+void eliminate_identity::apply(program& p) const
+{
+    for(auto ins : iterator_for(p))
+    {
+        if(ins->get_operator().name() == "identity")
+        {
+            if(ins != p.end())
+            {
+                instruction_ref identity_input{ins->inputs().at(0)};
+                auto next_ins = std::next(ins);
+                std::vector<instruction_ref> next_ins_inputs{next_ins->inputs()};
+                for (auto& input : next_ins_inputs)
+                {
+                    if(input == ins)
+                    {
+                        input = identity_input;
+                    }
+                }
+                p.replace_instruction(next_ins, next_ins->get_operator(), next_ins_inputs);
+            }
+        }
+    }
+}
+
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx

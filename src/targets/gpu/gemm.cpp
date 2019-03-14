f9c38c090c3d75bcf3ad6ae71a83d5aea5125995
Shucai Xiao <Shucai.Xiao@amd.com>
2019-03-14 18:42:47 -0500

    support the case of multiple matrices as inputs with C, but C should be the same shape as A * B

--- src/targets/gpu/gemm.cpp
+++ src/targets/gpu/gemm.cpp
@@ -369,23 +369,25 @@ argument miopen_gemm::compute(context& ctx,
     bool is_3inputs = (args.size() == 4);
     if(is_3inputs)
     {
-        fill_result(output_shape, args[3], args[2]);
-
+        fill_result(output_shape, args[3], args[2]);        
         output_shape.visit_type([&](auto as) {
+            auto n_dim =    output_shape.lens().size();
+            auto dim_1 =    n_dim - 1;
+            auto dim_0 =    n_dim - 2;
             auto alpha_r    = to_rocblas_type(as(op.alpha));
             auto beta_r     = to_rocblas_type(as(op.beta));
             bool transa     = args[0].get_shape().transposed();
             bool transb     = args[1].get_shape().transposed();
-            rocblas_int lda = args[0].get_shape().strides()[transa ? 1 : 0];
-            rocblas_int ldb = args[1].get_shape().strides()[transb ? 1 : 0];
-            rocblas_int ldc = args[3].get_shape().strides()[0];
+            rocblas_int lda = args[0].get_shape().strides()[transa ? dim_1 : dim_0];
+            rocblas_int ldb = args[1].get_shape().strides()[transb ? dim_1 : dim_0];
+            rocblas_int ldc = args[3].get_shape().strides()[dim_0];
             auto out_lens   = output_shape.lens();
-            rocblas_int m   = out_lens[0];
-            rocblas_int n   = out_lens[1];
-            rocblas_int k   = args[0].get_shape().lens()[1];
+            rocblas_int m   = out_lens[dim_0];
+            rocblas_int n   = out_lens[dim_1];
+            rocblas_int k   = args[0].get_shape().lens()[dim_1];
+            auto num_matrices = std::accumulate(out_lens.rbegin() + 2, out_lens.rend(), std::size_t{1}, std::multiplies<std::size_t>());
             auto to_pointer = [&](auto&& arg) { return to_rocblas_type(as.from(arg.data())); };
-
-            generic_rocblas_gemm(as,
+            generic_rocblas_batched_gemm(as,
                                  ctx.get_stream().get_rocblas(),
                                  transb ? rocblas_operation_transpose : rocblas_operation_none,
                                  transa ? rocblas_operation_transpose : rocblas_operation_none,
@@ -395,11 +397,15 @@ argument miopen_gemm::compute(context& ctx,
                                  &alpha_r,
                                  to_pointer(args[1]),
                                  ldb,
+                                 k * n,
                                  to_pointer(args[0]),
                                  lda,
+                                 m * k,
                                  &beta_r,
                                  to_pointer(args[3]),
-                                 ldc);
+                                 ldc,
+                                 m * n,
+                                 num_matrices);
 
         });
 

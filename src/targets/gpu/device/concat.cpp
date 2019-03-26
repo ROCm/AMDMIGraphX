ead0d9be4df01fd04f17cf0ccd150537c829f562
Paul <pfultz2@yahoo.com>
2019-03-25 19:56:25 -0500

    Formatting

--- src/targets/gpu/device/concat.cpp
+++ src/targets/gpu/device/concat.cpp
@@ -45,21 +45,20 @@ argument concat(hipStream_t stream,
                 std::vector<std::size_t> offsets)
 {
     auto ninputs = args.size() - 1;
-    for(std::size_t j = 0;j < ninputs;j++)
+    for(std::size_t j = 0; j < ninputs; j++)
     {
-        auto&& arg = args[j];
+        auto&& arg            = args[j];
         std::size_t nelements = arg.get_shape().elements();
-        auto offset = offsets[j];
+        auto offset           = offsets[j];
         hip_visit_all(args.back(), arg)([&](auto output, auto input) {
             gs_launch(stream, nelements)([=](auto i) {
-                auto idx = output.get_shape().index(input.get_shape().multi(i));
+                auto idx                    = output.get_shape().index(input.get_shape().multi(i));
                 output.data()[idx + offset] = input.data()[i];
             });
         });
     }
     return args.back();
 }
-
 
 // argument concat(hipStream_t stream,
 //                 const migraphx::shape& output_shape,

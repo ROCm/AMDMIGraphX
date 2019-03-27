c94a9a5838dbde8547e1e1cf6114012ee0910837
Khalique <kahmed10@illinois.edu>
2019-03-27 17:04:31 -0500

    fixed tests or pad_rewrite

--- src/include/migraphx/pad_rewrite.hpp
+++ src/include/migraphx/pad_rewrite.hpp
@@ -13,14 +13,16 @@ inline namespace MIGRAPHX_INLINE_NS {
 struct program;
 
 /**
- * Rewrite pads to use attribute from other instructions instead.
+ * Remove identity instructions. Currently when used as the last pass, it will
+ * preserve the semantics of previous program state, therefore dead code elimination
+ * should not be used afterwards.
  */
 struct pad_rewrite
 {
     std::string name() const { return "pad_rewrite"; }
     void apply(program& p) const;
     template <class T>
-    void update_op(T, instruction_ref ins, instruction_ref output, program& p) const;
+    void update_op(T, const instruction_ref& input, const instruction_ref& ins, program& p) const;
 };
 
 } // namespace MIGRAPHX_INLINE_NS
--- src/pad_rewrite.cpp
+++ src/pad_rewrite.cpp
@@ -12,39 +12,38 @@ void pad_rewrite::apply(program& p) const
 {
     for(auto ins : iterator_for(p))
     {
-        if(ins->name() != "pad")
+        const std::string& op_name = ins->name();
+        if(op_name != "convolution" and op_name != "im2col" and op_name != "pooling")
             continue;
-        for(auto output : ins->outputs())
-        {
-            auto op_name = output->name();
-            if(op_name == "convolution")
-                update_op(op::convolution{}, ins, output, p);
-            else if(op_name == "im2col")
-                update_op(op::im2col{}, ins, output, p);
-            else if(op_name == "pooling")
-                update_op(op::pooling{}, ins, output, p);
-        }
+        auto input = ins->inputs().front();
+        if(input->name() != "pad")
+            continue;
+        if(op_name == "convolution")
+            update_op(op::convolution{}, input, ins, p);
+        else if(op_name == "im2col")
+            update_op(op::im2col{}, input, ins, p);
+        else if(op_name == "pooling")
+            update_op(op::pooling{}, input, ins, p);
     }
 }
 
 template <class T>
-void pad_rewrite::update_op(T, instruction_ref ins, instruction_ref output, program& p) const
+void pad_rewrite::update_op(T, const instruction_ref& input, const instruction_ref& ins, program& p) const
 {
-    auto pad_op = any_cast<op::pad>(ins->get_operator());
+    auto pad_op = any_cast<op::pad>(input->get_operator());
     if(!pad_op.symmetric())
         return;
 
     std::vector<int64_t> pads = pad_op.pads;
-    assert(pads.size() == 8); // ensure input being padded has 4 dims (*2 for font and back padding)
     std::array<size_t, 2> new_pads{static_cast<size_t>(pads[2]), static_cast<size_t>(pads[3])};
 
-    T op       = any_cast<T>(output->get_operator());
+    T op       = any_cast<T>(ins->get_operator());
     op.padding = new_pads;
 
-    std::vector<instruction_ref> new_inputs{output->inputs()};
-    new_inputs.front() = ins->inputs().front();
+    std::vector<instruction_ref> new_inputs{ins->inputs()};
+    new_inputs.front() = input->inputs().front();
 
-    p.replace_instruction(output, op, new_inputs);
+    p.replace_instruction(ins, op, new_inputs);
 }
 
 } // namespace MIGRAPHX_INLINE_NS

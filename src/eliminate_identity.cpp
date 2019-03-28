cdf25b5095c83c9b685740f71072331d0228c67a
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-03-28 16:49:03 -0500

    revert header from prev branch

--- src/eliminate_identity.cpp
+++ src/eliminate_identity.cpp
@@ -3,6 +3,7 @@
 #include <migraphx/instruction.hpp>
 #include <migraphx/operators.hpp>
 #include <migraphx/iterator_for.hpp>
+#include <migraphx/ranges.hpp>
 #include <migraphx/stringutils.hpp>
 #include <utility>
 

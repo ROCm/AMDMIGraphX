e977a8cb9d63f9bff0bca3541c62cb82e37c1728
Shucai Xiao <Shucai.Xiao@amd.com>
2019-03-29 16:14:52 -0500

    put each operator in a separate head file.

---
+++ src/include/migraphx/op/mul.hpp
@@ -0,0 +1,29 @@
+#ifndef MIGRAPHX_GUARD_OPERATORS_MUL_HPP
+#define MIGRAPHX_GUARD_OPERATORS_MUL_HPP
+
+#include <array>
+#include <migraphx/op/binary.hpp>
+#include <migraphx/operation.hpp>
+#include <migraphx/check_shapes.hpp>
+#include <migraphx/stringutils.hpp>
+#include <migraphx/streamutils.hpp>
+#include <migraphx/literal.hpp>
+#include <migraphx/shape_for_each.hpp>
+#include <migraphx/config.hpp>
+#include <cmath>
+#include <utility>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace op {
+
+struct mul : binary
+{
+    std::string name() const { return "mul"; }
+};
+
+} // namespace op
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx
+
+#endif

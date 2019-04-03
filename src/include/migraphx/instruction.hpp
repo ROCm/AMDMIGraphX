efa8d9f4c71a549d9f8d27ebc26ee4ddd1588f7b
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-03 17:46:11 -0500

    clang format.

--- src/include/migraphx/instruction.hpp
+++ src/include/migraphx/instruction.hpp
@@ -68,8 +68,10 @@ struct instruction
 
     static void backreference(instruction_ref ref);
 
-    static void replace_argument(instruction_ref ins, instruction_ref old, instruction_ref new_ins, 
-            bool recompute_shape = true);
+    static void replace_argument(instruction_ref ins,
+                                 instruction_ref old,
+                                 instruction_ref new_ins,
+                                 bool recompute_shape = true);
 
     static void
     replace(instruction_ref ins, operation o, const shape& r, std::vector<instruction_ref> args);

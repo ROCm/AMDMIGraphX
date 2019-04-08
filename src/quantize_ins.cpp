c6396935084f4bd325d03b3b6d96c547fc80ae0d
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-08 14:55:16 -0500

    clang format

--- src/quantize_ins.cpp
+++ src/quantize_ins.cpp
@@ -10,15 +10,13 @@ inline namespace MIGRAPHX_INLINE_NS {
 
 void quantize_ins(program& prog, const std::vector<std::string>& ins_names)
 {
-    for (auto ins : iterator_for(prog))
+    for(auto ins : iterator_for(prog))
     {
         auto name_it = std::find(ins_name.begin(), ins_name.end(), ins->name());
-        if (name_it == ins_name.end())
+        if(name_it == ins_name.end())
         {
             continue;
         }
-
-        
     }
 }
 

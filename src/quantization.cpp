4e673ca35bf985010319f14b60d070e5c381b053
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-10 12:55:50 -0500

    change to simplify the type conversion for literals

--- src/quantization.cpp
+++ src/quantization.cpp
@@ -24,11 +24,18 @@ instruction_ref convert_fp32_fp16(program& prog, instruction_ref& ins)
     instruction_ref ins_fp16{};
     if(ins->name() == "@literal")
     {
-        std::vector<float> values;
-        auto l_fp32 = ins->get_literal();
         shape s     = ins->get_shape();
-        l_fp32.visit([&](auto val) { values.assign(val.begin(), val.end()); });
-        ins_fp16 = prog.add_literal(literal({shape::half_type, s.lens()}, values));
+        auto l = ins->get_literal();
+        if (s.type() == shape::float_type)
+        {
+            auto tv = l.get<const float>();
+            ins_fp16 = prog.add_literal(literal({shape::half_type, s.lens()}, tv.begin(), tv.end()));
+        }
+        else
+        {
+            auto tv = l.get<const double>();
+            ins_fp16 = prog.add_literal(literal({shape::half_type, s.lens()}, tv.begin(), tv.end()));
+        }
     }
     else if(ins->name() == "@param")
     {

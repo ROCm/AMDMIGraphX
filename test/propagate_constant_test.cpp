8607853b871b6a1f8629aca9609f3559c86fed31
Paul <pfultz2@yahoo.com>
2019-04-15 11:10:24 -0500

    Add more tests

--- test/propagate_constant_test.cpp
+++ test/propagate_constant_test.cpp
@@ -1,6 +1,7 @@
 #include <migraphx/propagate_constant.hpp>
 #include <migraphx/dead_code_elimination.hpp>
 #include <migraphx/op/add.hpp>
+#include <migraphx/op/scalar.hpp>
 #include <migraphx/op/mul.hpp>
 #include <basic_ops.hpp>
 #include <test.hpp>
@@ -15,7 +16,7 @@ struct const_prop_target
     migraphx::context get_context() const { return {}; }
 };
 
-TEST_CASE(const_add1)
+TEST_CASE(const_add)
 {
     migraphx::program p1;
     auto one = p1.add_literal(1);
@@ -30,7 +31,7 @@ TEST_CASE(const_add1)
     EXPECT(p1 == p2);
 }
 
-TEST_CASE(const_add2)
+TEST_CASE(const_add_parameter)
 {
     migraphx::program p1;
     auto one = p1.add_parameter("one", {migraphx::shape::int32_type, {1}});
@@ -45,7 +46,7 @@ TEST_CASE(const_add2)
     EXPECT(p1 != p2);
 }
 
-TEST_CASE(const_add3)
+TEST_CASE(const_multiadd)
 {
     migraphx::program p1;
     auto one  = p1.add_literal(1);
@@ -61,7 +62,7 @@ TEST_CASE(const_add3)
     EXPECT(p1 == p2);
 }
 
-TEST_CASE(const_add4)
+TEST_CASE(const_add_mul)
 {
     migraphx::program p1;
     auto one  = p1.add_literal(1);
@@ -75,6 +76,38 @@ TEST_CASE(const_add4)
     migraphx::program p2;
     auto total = p2.add_literal(7);
     p2.add_instruction(pass_op{}, total);
+    EXPECT(p1 == p2);
+}
+
+TEST_CASE(const_add_scalar)
+{
+    migraphx::program p1;
+    auto one = p1.add_instruction(migraphx::op::scalar{{migraphx::shape::int32_type, {2, 2}}}, p1.add_literal(1));
+    auto two = p1.add_instruction(migraphx::op::scalar{{migraphx::shape::int32_type, {2, 2}}}, p1.add_literal(2));
+    auto sum = p1.add_instruction(migraphx::op::add{}, one, two);
+    p1.add_instruction(pass_op{}, sum);
+    p1.compile(const_prop_target{});
+
+    migraphx::program p2;
+    auto total = p2.add_literal(migraphx::literal{{migraphx::shape::int32_type, {2, 2}}, {3, 3, 3, 3}});
+    p2.add_instruction(pass_op{}, total);
+    EXPECT(p1 == p2);
+}
+
+TEST_CASE(const_scalar)
+{
+    migraphx::program p1;
+    {
+        auto one = p1.add_instruction(migraphx::op::scalar{{migraphx::shape::int32_type, {2, 2}}}, p1.add_literal(1));
+        p1.add_instruction(pass_op{}, one);
+    }
+    p1.compile(const_prop_target{});
+
+    migraphx::program p2;
+    {
+        auto one = p2.add_instruction(migraphx::op::scalar{{migraphx::shape::int32_type, {2, 2}}}, p2.add_literal(1));
+        p2.add_instruction(pass_op{}, one);
+    }
     EXPECT(p1 == p2);
 }
 

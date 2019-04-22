893c68dfb1d1e8cc1f21af36c3f6b8dad3c96310
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-22 15:32:54 -0500

    clang format

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -335,7 +335,7 @@ struct test_trans_tanh : verify_program<test_trans_tanh>
         auto x  = p.add_parameter("x", migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}});
         auto tx = p.add_instruction(migraphx::op::transpose{{0, 1, 3, 2}}, x);
         auto tanhx = p.add_instruction(migraphx::op::tanh{}, tx);
-        auto r = p.add_instruction(migraphx::op::add{}, tanhx, tanhx);
+        auto r     = p.add_instruction(migraphx::op::add{}, tanhx, tanhx);
         p.add_instruction(migraphx::op::contiguous{}, r);
 
         return p;
@@ -697,9 +697,9 @@ struct test_trans_abs : verify_program<test_trans_abs>
         auto x  = p.add_parameter("x", migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}});
         auto tx = p.add_instruction(migraphx::op::transpose{{0, 1, 3, 2}}, x);
         auto absx = p.add_instruction(migraphx::op::abs{}, tx);
-        auto r = p.add_instruction(migraphx::op::add{}, absx, absx);
+        auto r    = p.add_instruction(migraphx::op::add{}, absx, absx);
         p.add_instruction(migraphx::op::contiguous{}, r);
-        
+
         return p;
     }
 };

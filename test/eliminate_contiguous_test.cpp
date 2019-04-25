08161428791219d8887bb374f3f58865b3a8d369
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-25 11:39:56 -0500

    add more test cases for better code coverage.

--- test/eliminate_contiguous_test.cpp
+++ test/eliminate_contiguous_test.cpp
@@ -1,5 +1,7 @@
 #include <migraphx/eliminate_contiguous.hpp>
 #include <migraphx/dead_code_elimination.hpp>
+#include <migraphx/op/identity.hpp>
+#include <migraphx/op/dot.hpp>
 #include <migraphx/op/transpose.hpp>
 #include <migraphx/op/contiguous.hpp>
 #include <basic_ops.hpp>
@@ -34,6 +36,19 @@ TEST_CASE(non_standard_op)
     auto t = p.add_instruction(migraphx::op::transpose{{1, 0}}, l);
     auto c = p.add_instruction(migraphx::op::contiguous{}, t);
     p.add_instruction(pass_op{}, c);
+    auto count = std::distance(p.begin(), p.end());
+    p.compile(eliminate_contiguous_target{});
+    EXPECT(std::distance(p.begin(), p.end()) == (count - 1));
+}
+
+TEST_CASE(transpose_gemm)
+{
+    migraphx::program p;
+    auto l = p.add_literal(get_2x2());
+    auto t = p.add_instruction(migraphx::op::transpose{{1, 0}}, l);
+    auto c = p.add_instruction(migraphx::op::contiguous{}, t);
+    auto ic = p.add_instruction(migraphx::op::identity{}, c);
+    p.add_instruction(migraphx::op::dot{}, ic, l);
     auto count = std::distance(p.begin(), p.end());
     p.compile(eliminate_contiguous_target{});
     EXPECT(std::distance(p.begin(), p.end()) == (count - 1));

6e131270ee3993f38c35c7622895f7646157aeaf
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-05-02 12:58:29 -0500

    formatting

--- src/targets/gpu/include/migraphx/gpu/device/clip.hpp
+++ src/targets/gpu/include/migraphx/gpu/device/clip.hpp
@@ -10,11 +10,7 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
-void clip(hipStream_t stream,
-          const argument& result,
-          const argument& arg1,
-          float max,
-          float min);
+void clip(hipStream_t stream, const argument& result, const argument& arg1, float max, float min);
 
 } // namespace device
 } // namespace gpu

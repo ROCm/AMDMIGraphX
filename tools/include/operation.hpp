1a1b31ed626fc2cfdca5a4b9cbc8f6633f7f86d1
Paul <pfultz2@yahoo.com>
2019-05-03 22:49:22 -0500

    Fix dangling reference

--- tools/include/operation.hpp
+++ tools/include/operation.hpp
@@ -69,7 +69,7 @@ auto operator<<(std::ostream& os, const T& x) -> decltype(os << x.name())
 {
     os << x.name();
     char delim = '[';
-    reflect_each(x, [&](auto& y, auto name) {
+    reflect_each(x, [&](auto&& y, auto name) {
         os << delim;
         os << name << "=";
         stream_write_value(os, y);

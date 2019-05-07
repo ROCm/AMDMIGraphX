69e6c84c98aa80d5d52a232764f11f8a16472dac
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-07 11:39:43 -0500

    clang format'

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -1263,10 +1263,10 @@ struct quant_dot_3args_2 : verify_program<quant_dot_3args_2>
         migraphx::shape m2_shape{migraphx::shape::int8_type, {8, 7}};
         migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
 
-        auto l1 = p.add_parameter("a", m1_shape);
+        auto l1  = p.add_parameter("a", m1_shape);
         auto tl1 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l1);
-        auto l2 = p.add_parameter("b", m2_shape);
-        auto l3 = p.add_parameter("c", m3_shape);
+        auto l2  = p.add_parameter("b", m2_shape);
+        auto l3  = p.add_parameter("c", m3_shape);
         p.add_instruction(migraphx::op::quant_dot{1, 3}, tl1, l2, l3);
         return p;
     }
@@ -1281,10 +1281,10 @@ struct quant_dot_3args_3 : verify_program<quant_dot_3args_3>
         migraphx::shape m2_shape{migraphx::shape::int8_type, {7, 8}};
         migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
 
-        auto l1 = p.add_parameter("a", m1_shape);
-        auto l2 = p.add_parameter("b", m2_shape);
+        auto l1  = p.add_parameter("a", m1_shape);
+        auto l2  = p.add_parameter("b", m2_shape);
         auto tl2 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l2);
-        auto l3 = p.add_parameter("c", m3_shape);
+        auto l3  = p.add_parameter("c", m3_shape);
         p.add_instruction(migraphx::op::quant_dot{2, 3}, l1, tl2, l3);
         return p;
     }
@@ -1299,11 +1299,11 @@ struct quant_dot_3args_4 : verify_program<quant_dot_3args_4>
         migraphx::shape m2_shape{migraphx::shape::int8_type, {7, 8}};
         migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
 
-        auto l1 = p.add_parameter("a", m1_shape);
+        auto l1  = p.add_parameter("a", m1_shape);
         auto tl1 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l1);
-        auto l2 = p.add_parameter("b", m2_shape);
+        auto l2  = p.add_parameter("b", m2_shape);
         auto tl2 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l2);
-        auto l3 = p.add_parameter("c", m3_shape);
+        auto l3  = p.add_parameter("c", m3_shape);
         p.add_instruction(migraphx::op::quant_dot{3, 2}, tl1, tl2, l3);
         return p;
     }

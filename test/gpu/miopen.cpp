06005bb0717a7a6383b4cc8c2fad0b9c77c4f64d
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-07 11:39:32 -0500

    add gpu test for quant_dot

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -1237,6 +1237,78 @@ struct gemm_multi_3args_alpha0 : verify_program<gemm_multi_3args_alpha0>
     }
 };
 
+struct quant_dot_3args_1 : verify_program<quant_dot_3args_1>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        migraphx::shape m1_shape{migraphx::shape::int8_type, {2, 8}};
+        migraphx::shape m2_shape{migraphx::shape::int8_type, {8, 7}};
+        migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
+
+        auto l1 = p.add_parameter("a", m1_shape);
+        auto l2 = p.add_parameter("b", m2_shape);
+        auto l3 = p.add_parameter("c", m3_shape);
+        p.add_instruction(migraphx::op::quant_dot{}, l1, l2, l3);
+        return p;
+    }
+};
+
+struct quant_dot_3args_2 : verify_program<quant_dot_3args_2>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        migraphx::shape m1_shape{migraphx::shape::int8_type, {8, 2}};
+        migraphx::shape m2_shape{migraphx::shape::int8_type, {8, 7}};
+        migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
+
+        auto l1 = p.add_parameter("a", m1_shape);
+        auto tl1 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l1);
+        auto l2 = p.add_parameter("b", m2_shape);
+        auto l3 = p.add_parameter("c", m3_shape);
+        p.add_instruction(migraphx::op::quant_dot{1, 3}, tl1, l2, l3);
+        return p;
+    }
+};
+
+struct quant_dot_3args_3 : verify_program<quant_dot_3args_3>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        migraphx::shape m1_shape{migraphx::shape::int8_type, {2, 8}};
+        migraphx::shape m2_shape{migraphx::shape::int8_type, {7, 8}};
+        migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
+
+        auto l1 = p.add_parameter("a", m1_shape);
+        auto l2 = p.add_parameter("b", m2_shape);
+        auto tl2 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l2);
+        auto l3 = p.add_parameter("c", m3_shape);
+        p.add_instruction(migraphx::op::quant_dot{2, 3}, l1, tl2, l3);
+        return p;
+    }
+};
+
+struct quant_dot_3args_4 : verify_program<quant_dot_3args_4>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        migraphx::shape m1_shape{migraphx::shape::int8_type, {8, 2}};
+        migraphx::shape m2_shape{migraphx::shape::int8_type, {7, 8}};
+        migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
+
+        auto l1 = p.add_parameter("a", m1_shape);
+        auto tl1 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l1);
+        auto l2 = p.add_parameter("b", m2_shape);
+        auto tl2 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l2);
+        auto l3 = p.add_parameter("c", m3_shape);
+        p.add_instruction(migraphx::op::quant_dot{3, 2}, tl1, tl2, l3);
+        return p;
+    }
+};
+
 struct test_contiguous : verify_program<test_contiguous>
 {
     migraphx::program create_program() const

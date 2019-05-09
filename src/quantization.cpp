7f6f4acb6e5db12a65ffc1fdb52124c8d38670a1
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-09 10:49:50 -0500

    clang format

--- src/quantization.cpp
+++ src/quantization.cpp
@@ -101,10 +101,7 @@ void quantize(program& prog, const std::vector<std::string>& ins_names)
     }
 }
 
-void quantize(program& prog)
-{
-    quantize(prog, {"all"});
-}
+void quantize(program& prog) { quantize(prog, {"all"}); }
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

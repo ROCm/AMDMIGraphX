618a0acdac95a94fc271c22d52ccebac9d8ff440
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-05-13 18:29:19 -0500

    formatting

--- test/tf/tf_test.cpp
+++ test/tf/tf_test.cpp
@@ -171,14 +171,14 @@ TEST_CASE(matmul_test)
 TEST_CASE(mean_test)
 {
     migraphx::program p;
-    migraphx::literal l{migraphx::shape{migraphx::shape::int32_type, {2}}, {2,3}};
+    migraphx::literal l{migraphx::shape{migraphx::shape::int32_type, {2}}, {2, 3}};
     auto l0 = p.add_parameter("0", migraphx::shape{migraphx::shape::float_type, {1, 3, 16, 16}});
     p.add_literal(l);
     p.add_literal(l);
     migraphx::op::pooling op;
-    op.lengths = {16,16};
-    auto l3 = p.add_instruction(op, l0);
-    p.add_instruction(migraphx::op::squeeze{{2,3}}, l3);
+    op.lengths = {16, 16};
+    auto l3    = p.add_instruction(op, l0);
+    p.add_instruction(migraphx::op::squeeze{{2, 3}}, l3);
     p.add_instruction(op, l0);
     auto prog = migraphx::parse_tf("mean_test.pb", false);
 
@@ -188,14 +188,14 @@ TEST_CASE(mean_test)
 TEST_CASE(mean_test_nhwc)
 {
     migraphx::program p;
-    migraphx::literal l{migraphx::shape{migraphx::shape::int32_type, {2}}, {1,2}};
+    migraphx::literal l{migraphx::shape{migraphx::shape::int32_type, {2}}, {1, 2}};
     auto l0 = p.add_parameter("0", migraphx::shape{migraphx::shape::float_type, {1, 3, 16, 16}});
     p.add_literal(l);
     p.add_literal(l);
     migraphx::op::pooling op;
-    op.lengths = {16,16};
-    auto l3 = p.add_instruction(op, l0);
-    p.add_instruction(migraphx::op::squeeze{{2,3}}, l3);
+    op.lengths = {16, 16};
+    auto l3    = p.add_instruction(op, l0);
+    p.add_instruction(migraphx::op::squeeze{{2, 3}}, l3);
     p.add_instruction(op, l0);
     auto prog = migraphx::parse_tf("mean_test_nhwc.pb", true);
 

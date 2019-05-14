852a517a80f467811e5af5326692fe35a72c74d4
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-14 18:02:58 -0500

    clang format

--- src/targets/gpu/convert.cpp
+++ src/targets/gpu/convert.cpp
@@ -13,9 +13,7 @@ shape hip_convert::compute_shape(std::vector<shape> inputs) const
     return op.compute_shape(inputs);
 }
 
-argument hip_convert::compute(context& ctx,
-                             const shape&,
-                             const std::vector<argument>& args) const
+argument hip_convert::compute(context& ctx, const shape&, const std::vector<argument>& args) const
 {
     device::convert(ctx.get_stream().get(), args[1], args[0], op.scale, op.shift);
     return args[1];

7e1308ce00c51fccdb416d80a803ac51ab2d60a3
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-20 17:37:26 -0500

    fix build error.

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -1262,11 +1262,12 @@ struct quant_dot_3args_2 : verify_program<quant_dot_3args_2>
         migraphx::shape m1_shape{migraphx::shape::int8_type, {8, 2}};
         migraphx::shape m2_shape{migraphx::shape::int8_type, {8, 7}};
         migraphx::shape m3_shape{migraphx::shape::int32_type, {2, 7}};
+        std::vector<int> m3_data(2 * 7, 1);
 
         auto l1  = p.add_parameter("a", m1_shape);
         auto tl1 = p.add_instruction(migraphx::op::transpose{{1, 0}}, l1);
         auto l2  = p.add_parameter("b", m2_shape);
-        auto l3  = p.add_parameter("c", m3_shape);
+        auto l3  = p.add_literal(m3_shape, m3_data);
         p.add_instruction(migraphx::op::quant_dot{1, 3}, tl1, l2, l3);
         return p;
     }

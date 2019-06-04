88dac3b85e4d553dae23e65a124029c6e1c76edb
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-06-04 17:57:01 -0500

    move header file

--- src/include/migraphx/pad_calc.hpp
+++ src/include/migraphx/pad_calc.hpp
@@ -5,14 +5,12 @@
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
-namespace op {
 
 inline size_t calculate_padding(size_t weight_dim, size_t dilation)
 {
     return (dilation * (weight_dim - 1)) / 2;
 }
 
-} // namespace op
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx
 

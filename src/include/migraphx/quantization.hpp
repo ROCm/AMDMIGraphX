2fcc8c098a42dda69f9dfcd374b8101f1e48a131
Shucai Xiao <Shucai.Xiao@amd.com>
2019-06-05 11:54:57 -0500

    code cleanup

--- src/include/migraphx/quantization.hpp
+++ src/include/migraphx/quantization.hpp
@@ -19,11 +19,8 @@ void quantize(program& prog);
 // to int8
 void capture_arguments(program& prog,
                        const std::vector<std::string>& ins_names,
-                       std::size_t& num_quant_params,
-                       std::function<void(std::size_t, std::vector<argument> args)> func);
-void quantize_int8(program& prog,
-                   const std::vector<std::string>& ins_names,
-                   std::vector<std::pair<float, float>>& int8_quant_params);
+                       std::function<void(std::size_t, std::vector<argument>)> func);
+void capture_arguments(program& prog, const std::vector<std::string>& ins_names);
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

64fa434c5b0173efd8746ddcb4c52bd43a68cb49
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-06-05 16:47:55 -0500

    reinsert miopen_softmax

--- src/targets/gpu/include/migraphx/gpu/softmax.hpp
+++ src/targets/gpu/include/migraphx/gpu/softmax.hpp
@@ -22,6 +22,28 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
+struct context;
+
+struct miopen_softmax
+{
+    op::softmax op;
+
+    template <class Self, class F>
+    static auto reflect(Self& self, F f)
+    {
+        return migraphx::reflect(self.op, f);
+    }
+
+    std::string name() const { return "gpu::softmax"; }
+    shape compute_shape(const std::vector<shape>& inputs) const;
+    argument
+    compute(context& ctx, const shape& output_shape, const std::vector<argument>& args) const;
+    std::ptrdiff_t output_alias(const std::vector<shape>& shapes) const
+    {
+        return shapes.size() - 1;
+    }
+};
+
 struct hip_softmax
 {
     op::softmax op;

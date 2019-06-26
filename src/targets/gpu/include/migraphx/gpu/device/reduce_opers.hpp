8deed1f74276fdcac2e575ea07812c44bca43057
Shucai Xiao <Shucai.Xiao@amd.com>
2019-06-26 10:44:38 -0500

    clang format

--- src/targets/gpu/include/migraphx/gpu/device/reduce_opers.hpp
+++ src/targets/gpu/include/migraphx/gpu/device/reduce_opers.hpp
@@ -30,11 +30,11 @@ struct sum_op
 
 template <class T, class Op>
 inline __device__ void block_reduce(T* data_ptr,
-                                  Op op,
-                                  std::size_t block_size,
-                                  std::size_t thr_idx,
-                                  std::size_t item_num,
-                                  std::size_t max_index)
+                                    Op op,
+                                    std::size_t block_size,
+                                    std::size_t thr_idx,
+                                    std::size_t item_num,
+                                    std::size_t max_index)
 {
     while(true)
     {
@@ -42,7 +42,7 @@ inline __device__ void block_reduce(T* data_ptr,
         auto size   = item_num / 2;
         for(std::size_t i = thr_idx; i < size; i += block_size)
         {
-            //data_ptr[i] = ::max(to_hip_type(data_ptr[i]), to_hip_type(data_ptr[i + stride]));
+            // data_ptr[i] = ::max(to_hip_type(data_ptr[i]), to_hip_type(data_ptr[i + stride]));
             data_ptr[i] = op(data_ptr[i], data_ptr[i + stride]);
         }
         __syncthreads();
@@ -61,7 +61,6 @@ inline __device__ void block_reduce(T* data_ptr,
 
     __syncthreads();
 }
-
 
 template <class T>
 inline __device__ void reduce_max(T* data_ptr,

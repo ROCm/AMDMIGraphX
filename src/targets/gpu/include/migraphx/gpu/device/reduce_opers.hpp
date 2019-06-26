fba752b6c2627dbd37392c98b29794078742077c
Shucai Xiao <Shucai.Xiao@amd.com>
2019-06-26 11:15:40 -0500

    further code cleanup

--- src/targets/gpu/include/migraphx/gpu/device/reduce_opers.hpp
+++ src/targets/gpu/include/migraphx/gpu/device/reduce_opers.hpp
@@ -63,68 +63,6 @@ inline __device__ void block_reduce(T* data_ptr,
 }
 
 template <class T>
-inline __device__ void reduce_max(T* data_ptr,
-                                  std::size_t block_size,
-                                  std::size_t thr_idx,
-                                  std::size_t item_num,
-                                  std::size_t max_index)
-{
-    while(true)
-    {
-        auto stride = (item_num + 1) / 2;
-        auto size   = item_num / 2;
-        for(std::size_t i = thr_idx; i < size; i += block_size)
-        {
-            data_ptr[i] = ::max(to_hip_type(data_ptr[i]), to_hip_type(data_ptr[i + stride]));
-        }
-        __syncthreads();
-        item_num = stride;
-
-        if(item_num == 1)
-            break;
-    }
-
-    if(thr_idx == 0)
-    {
-        data_ptr[max_index] =
-            (data_ptr[0] < data_ptr[max_index]) ? data_ptr[max_index] : data_ptr[0];
-    }
-
-    __syncthreads();
-}
-
-template <class T>
-inline __device__ void reduce_min(T* data_ptr,
-                                  std::size_t block_size,
-                                  std::size_t thr_idx,
-                                  std::size_t item_num,
-                                  std::size_t min_index)
-{
-    while(true)
-    {
-        auto stride = (item_num + 1) / 2;
-        auto size   = item_num / 2;
-        for(std::size_t i = thr_idx; i < size; i += block_size)
-        {
-            data_ptr[i] = ::min(to_hip_type(data_ptr[i]), to_hip_type(data_ptr[i + stride]));
-        }
-        __syncthreads();
-        item_num = stride;
-
-        if(item_num == 1)
-            break;
-    }
-
-    if(thr_idx == 0)
-    {
-        data_ptr[min_index] =
-            (data_ptr[0] > data_ptr[min_index]) ? data_ptr[min_index] : data_ptr[0];
-    }
-
-    __syncthreads();
-}
-
-template <class T>
 inline __device__ void reduce_argmax(T* data_ptr,
                                      int64_t* index_ptr,
                                      std::size_t block_size,
@@ -197,36 +135,6 @@ inline __device__ void reduce_argmin(T* data_ptr,
             data_ptr[min_index]  = data_ptr[0];
             index_ptr[min_index] = index_ptr[0];
         }
-    }
-
-    __syncthreads();
-}
-
-template <class T>
-inline __device__ void reduce_sum(T* data_ptr,
-                                  std::size_t block_size,
-                                  std::size_t thr_idx,
-                                  std::size_t item_num,
-                                  std::size_t sum_index)
-{
-    while(true)
-    {
-        auto stride = (item_num + 1) / 2;
-        auto size   = item_num / 2;
-        for(std::size_t i = thr_idx; i < size; i += block_size)
-        {
-            data_ptr[i] += data_ptr[i + stride];
-        }
-        __syncthreads();
-        item_num = stride;
-
-        if(item_num == 1)
-            break;
-    }
-
-    if(thr_idx == 0)
-    {
-        data_ptr[sum_index] += data_ptr[0];
     }
 
     __syncthreads();

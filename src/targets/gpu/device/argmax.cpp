b08781f49bb7ce3c594b0a46223c0eec6d8db658
Shucai Xiao <Shucai.Xiao@amd.com>
2019-06-27 10:08:56 -0500

    add test example to argmax and argmin operators.

--- src/targets/gpu/device/argmax.cpp
+++ src/targets/gpu/device/argmax.cpp
@@ -5,7 +5,6 @@
 #include <migraphx/gpu/device/tensor.hpp>
 #include <migraphx/gpu/device/launch.hpp>
 #include <migraphx/gpu/device/types.hpp>
-#include <migraphx/gpu/device/reduce_opers.hpp>
 #include <migraphx/gpu/hip.hpp>
 
 namespace migraphx {
@@ -13,13 +12,53 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
+template <class T>
+inline __device__ void reduce_argmax(T* data_ptr,
+                                     int64_t* index_ptr,
+                                     std::size_t block_size,
+                                     std::size_t thr_idx,
+                                     std::size_t item_num,
+                                     std::size_t max_index)
+{
+    while(true)
+    {
+        auto stride = (item_num + 1) / 2;
+        auto size   = item_num / 2;
+        for(std::size_t i = thr_idx; i < size; i += block_size)
+        {
+            if(data_ptr[i] < data_ptr[i + stride])
+            {
+                data_ptr[i]  = data_ptr[i + stride];
+                index_ptr[i] = index_ptr[i + stride];
+            }
+        }
+        __syncthreads();
+        item_num = stride;
+
+        if(item_num == 1)
+            break;
+    }
+
+    if(thr_idx == 0)
+    {
+        if(data_ptr[max_index] < data_ptr[0])
+        {
+            data_ptr[max_index]  = data_ptr[0];
+            index_ptr[max_index] = index_ptr[0];
+        }
+    }
+
+    __syncthreads();
+}
+
 void argmax(hipStream_t stream, const argument& result, const argument& arg, int axis)
 {
     auto lens             = arg.get_shape().lens();
     auto batch_lens       = lens;
     size_t batch_item_num = lens[axis];
     batch_lens[axis]      = 1;
-    migraphx::shape batch_shape{shape::float_type, batch_lens};
+    migraphx::shape batch_shape{shape::int64_type, batch_lens};
+    auto arg_shape = arg.get_shape();
 
     hip_visit_all(result, arg, batch_shape)([&](auto output, auto input, auto batch) {
         // use one block for items in one batch.
@@ -33,7 +72,7 @@ void argmax(hipStream_t stream, const argument& result, const argument& arg, int
         launch(stream, batch_shape.elements() * block_size, block_size)([=](auto idx) __device__ {
             size_t thr_idx = idx.local;
             size_t blk_idx = idx.group;
-            using type     = device_type<std::remove_cv_t<typename decltype(output)::value_type>>;
+            using type     = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
 
             auto batch_idx = batch.multi(blk_idx);
             auto data_idx  = batch_idx;

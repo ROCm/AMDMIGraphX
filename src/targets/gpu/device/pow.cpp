f76ed7cdb3f6900ca71d8b4af45501af11630a19
Shucai Xiao <Shucai.Xiao@amd.com>
2019-07-02 17:43:04 -0500

    add the operator pow

---
+++ src/targets/gpu/device/pow.cpp
@@ -0,0 +1,17 @@
+#include <migraphx/gpu/device/pow.hpp>
+#include <migraphx/gpu/device/nary.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace gpu {
+namespace device {
+
+void pow(hipStream_t stream, const argument& result, const argument& arg1, const argument& arg2)
+{
+    nary(stream, result, arg1, arg2)([](auto x, auto y) { return ::pow(to_hip_type(x), to_hip_type(y)); });
+}
+
+} // namespace device
+} // namespace gpu
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx

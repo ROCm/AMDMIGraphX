cb9bfaf40423592b8ca4fe78b9cefcb9aa6af775
Paul <pfultz2@yahoo.com>
2019-08-01 15:09:05 -0500

    Add mul_add_relu fusion

--- src/targets/gpu/device/add_relu.cpp
+++ src/targets/gpu/device/add_relu.cpp
@@ -6,6 +6,16 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
+void mul_add_relu(hipStream_t stream,
+              const argument& result,
+              const argument& arg1,
+              const argument& arg2,
+              const argument& arg3)
+{
+    nary(stream, result, arg1, arg2, arg3)(
+        [](auto x, auto a, auto b) { return std::max<decltype(a*x + b)>(0, a*x + b); });
+}
+
 void add_relu(hipStream_t stream,
               const argument& result,
               const argument& arg1,

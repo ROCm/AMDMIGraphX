5645b165d027c3cad8af9971699d927e7276c04d
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-01 17:03:16 -0500

    clang format

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -3796,8 +3796,10 @@ struct test_convert : verify_program<test_convert>
         migraphx::shape sb{migraphx::shape::float_type, {24, 6}};
         auto pa = p.add_parameter("a", sa);
         auto pb = p.add_parameter("b", sb);
-        auto ia = p.add_instruction(migraphx::op::convert{migraphx::shape::int8_type, 16.0f, 1.0f}, pa);
-        auto ib = p.add_instruction(migraphx::op::convert{migraphx::shape::int8_type, 16.0f, 2.0f}, pb);
+        auto ia =
+            p.add_instruction(migraphx::op::convert{migraphx::shape::int8_type, 16.0f, 1.0f}, pa);
+        auto ib =
+            p.add_instruction(migraphx::op::convert{migraphx::shape::int8_type, 16.0f, 2.0f}, pb);
         p.add_instruction(migraphx::op::quant_dot{}, ia, ib);
 
         return p;

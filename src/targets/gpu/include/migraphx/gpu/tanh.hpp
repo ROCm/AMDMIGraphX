f65ef97cc7d6a5f1f11e99f47ca933b1bf92fb96
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-01 18:40:33 -0500

    rewrite relu and tanh to using hip kernels

--- src/targets/gpu/include/migraphx/gpu/tanh.hpp
+++ src/targets/gpu/include/migraphx/gpu/tanh.hpp
@@ -1,33 +1,15 @@
 #ifndef MIGRAPHX_GUARD_RTGLIB_TANH_HPP
 #define MIGRAPHX_GUARD_RTGLIB_TANH_HPP
 
-#include <migraphx/shape.hpp>
-#include <migraphx/gpu/miopen.hpp>
+#include <migraphx/gpu/oper.hpp>
+#include <migraphx/gpu/device/tanh.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
-struct context;
-
-struct miopen_tanh
+struct hip_tanh : unary_device<hip_tanh, device::tanh>
 {
-    shared<activation_descriptor> ad;
-
-    template <class Self, class F>
-    static auto reflect(Self& self, F f)
-    {
-        return gpu::reflect(self.ad.get(), f);
-    }
-
-    std::string name() const { return "gpu::tanh"; }
-    shape compute_shape(const std::vector<shape>& inputs) const;
-    argument
-    compute(context& ctx, const shape& output_shape, const std::vector<argument>& args) const;
-    std::ptrdiff_t output_alias(const std::vector<shape>& shapes) const
-    {
-        return shapes.size() - 1;
-    }
 };
 
 } // namespace gpu

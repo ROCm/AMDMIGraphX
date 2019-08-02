efb704c5d37c7c49450244b8a38550f7849c4f41
Paul <pfultz2@yahoo.com>
2019-08-02 18:21:34 -0500

    Formatting

--- src/targets/gpu/device/mul_add.cpp
+++ src/targets/gpu/device/mul_add.cpp
@@ -7,13 +7,12 @@ namespace gpu {
 namespace device {
 
 void mul_add(hipStream_t stream,
-              const argument& result,
-              const argument& arg1,
-              const argument& arg2,
-              const argument& arg3)
+             const argument& result,
+             const argument& arg1,
+             const argument& arg2,
+             const argument& arg3)
 {
-    nary(stream, result, arg1, arg2, arg3)(
-        [](auto x, auto a, auto b) { return a*x + b; });
+    nary(stream, result, arg1, arg2, arg3)([](auto x, auto a, auto b) { return a * x + b; });
 }
 
 } // namespace device

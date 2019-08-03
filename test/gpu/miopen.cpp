dc2e2cf3a890956b2c938b5ce29d23043713b059
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-03 00:12:48 -0500

    clang format

--- test/gpu/miopen.cpp
+++ test/gpu/miopen.cpp
@@ -3625,7 +3625,7 @@ struct test_round : verify_program<test_round>
     {
         migraphx::program p;
         migraphx::shape s{migraphx::shape::float_type, {2, 3, 4, 6}};
-        auto param     = p.add_parameter("x", s);
+        auto param = p.add_parameter("x", s);
         p.add_instruction(migraphx::op::round{}, param);
         return p;
     }

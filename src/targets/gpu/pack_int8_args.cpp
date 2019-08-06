b679e200c7e44c8911232c448cb0313e3c037bd7
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-06 10:15:34 -0500

    clang format

--- src/targets/gpu/pack_int8_args.cpp
+++ src/targets/gpu/pack_int8_args.cpp
@@ -19,23 +19,24 @@ void pack_int8_args::apply(program& p) const
             bool transa = inputs[0]->get_shape().transposed();
             bool transb = inputs[1]->get_shape().transposed();
 
-            if (!transb)
+            if(!transb)
             {
                 auto packed_b = p.insert_instruction(ins, hip_allocate{inputs[1]->get_shape()});
-                auto output_b = p.insert_instruction(ins, hip_int8_gemm_pack_a{}, {inputs[1], packed_b});
+                auto output_b =
+                    p.insert_instruction(ins, hip_int8_gemm_pack_a{}, {inputs[1], packed_b});
                 instruction::replace_argument(ins, inputs[1], output_b);
             }
 
-            if (transa)
+            if(transa)
             {
                 auto packed_a = p.insert_instruction(ins, hip_allocate{inputs[0]->get_shape()});
-                auto output_a = p.insert_instruction(ins, hip_int8_gemm_pack_b{}, {inputs[0], packed_a});
+                auto output_a =
+                    p.insert_instruction(ins, hip_int8_gemm_pack_b{}, {inputs[0], packed_a});
                 instruction::replace_argument(ins, inputs[0], output_a);
             }
         }
-        else if (ins->name() == "gpu::quant_convolution")
+        else if(ins->name() == "gpu::quant_convolution")
         {
-
         }
     }
 }

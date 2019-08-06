e60aff637c258bb7533d409f77c6e52e44402cc9
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-06 10:15:27 -0500

    add a pass for packing int8 op inputs

---
+++ src/targets/gpu/include/migraphx/gpu/pack_int8_args.hpp
@@ -0,0 +1,23 @@
+#ifndef MIGRAPHX_GUARD_RTGLIB_PACK_INT8_ARGS_HPP
+#define MIGRAPHX_GUARD_RTGLIB_PACK_INT8_ARGS_HPP
+
+#include <migraphx/program.hpp>
+#include <migraphx/config.hpp>
+#include <migraphx/gpu/context.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+
+namespace gpu {
+
+struct pack_int8_args
+{
+    std::string name() const { return "gpu::pack_int8_args"; }
+    void apply(program& p) const;
+};
+
+} // namespace gpu
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx
+
+#endif

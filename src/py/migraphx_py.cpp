dfe588f7867e41d0556a84fe65537ba642924c09
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-14 11:17:25 -0500

    small fix to achieve same result between cpu and gpu

--- src/py/migraphx_py.cpp
+++ src/py/migraphx_py.cpp
@@ -196,6 +196,9 @@ PYBIND11_MODULE(migraphx, m)
              std::vector<std::pair<float, float>>& quant_params) {
               migraphx::quantize_int8(p, ins_names, quant_params);
           });
+    m.def("quantize_int8", [](migraphx::program& p, std::vector<std::string>& ins_names) {
+        migraphx::quantize_int8(p, ins_names);
+    });
     m.def("quantize_int8", [](migraphx::program& p) { migraphx::quantize_int8(p); });
 
     m.def("capture_arguments", [](migraphx::program& p, const std::vector<std::string>& ins_names) {

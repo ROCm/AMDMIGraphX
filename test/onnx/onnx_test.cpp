492ea6cc614ad09dcfd61d7b116f38ab04cd8d64
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-20 14:46:50 -0500

    fix a compiler related issue

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -4,6 +4,7 @@
 #include <migraphx/operators.hpp>
 #include <migraphx/program.hpp>
 #include <migraphx/instruction.hpp>
+#include <migraphx/instruction_ref.hpp>
 #include <migraphx/onnx.hpp>
 #include "test.hpp"
 
@@ -512,15 +513,24 @@ TEST_CASE(shape_gather_test)
 TEST_CASE(transpose_gather_test)
 {
     migraphx::program p;
+    auto make_contiguous = [&p](migraphx::instruction_ref ins)
+    {
+        if (ins->get_shape().standard())
+        {
+            return ins;
+        }
+
+        return p.add_instruction(migraphx::op::contiguous{}, ins);
+    };
+
     auto data = p.add_parameter("data", migraphx::shape{migraphx::shape::float_type, {3, 5, 4, 6}});
     auto ind =
         p.add_parameter("indices", migraphx::shape{migraphx::shape::int32_type, {2, 4, 3, 5}});
     auto tr_data  = p.add_instruction(migraphx::op::transpose{{0, 2, 1, 3}}, data);
     auto tr_ind   = p.add_instruction(migraphx::op::transpose{{0, 2, 1, 3}}, ind);
-    auto std_data = p.add_instruction(migraphx::op::contiguous{}, tr_data);
-    auto std_ind  = p.add_instruction(migraphx::op::contiguous{}, tr_ind);
     int axis      = 1;
-    p.add_instruction(migraphx::op::gather{axis}, std_data, std_ind);
+    p.add_instruction(migraphx::op::gather{axis}, make_contiguous(tr_data), make_contiguous(tr_ind));
+    
     auto prog = migraphx::parse_onnx("transpose_gather.onnx");
 
     EXPECT(p == prog);

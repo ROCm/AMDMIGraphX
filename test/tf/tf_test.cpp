2152df829a9776ee486877545197b4eeb677e442
Khalique <15948690+kahmed10@users.noreply.github.com>
2019-08-20 16:34:41 -0500

    add assert test

--- test/tf/tf_test.cpp
+++ test/tf/tf_test.cpp
@@ -48,6 +48,22 @@ TEST_CASE(add_bcast_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(assert_less_equal_test)
+{
+    migraphx::program p;
+    migraphx::shape s0{migraphx::shape::float_type, {2, 3}};
+    auto l0 = p.add_parameter("0", s0);
+    auto l1 = p.add_parameter("1", s0);
+    migraphx::literal l{migraphx::shape{migraphx::shape::int32_type, {2}}, {0, 1}};
+    auto l2 = p.add_literal(l);
+    p.add_instruction(migraphx::op::add{}, l0, l1);
+    auto l3 = p.add_instruction(migraphx::op::identity{}, l0, l1);
+    p.add_instruction(migraphx::op::identity{}, l3, l2);
+    auto prog = optimize_tf("assert_less_equal_test.pb", false);
+
+    EXPECT(p == prog);
+}
+
 TEST_CASE(batchmatmul_test)
 {
     migraphx::program p;

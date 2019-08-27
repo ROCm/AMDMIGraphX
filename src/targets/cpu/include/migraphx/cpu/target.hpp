a4ba8021870376aa597d3367f700c23d169c2653
Shucai Xiao <Shucai.Xiao@amd.com>
2019-08-27 18:31:45 -0500

    backup code changes

--- src/targets/cpu/include/migraphx/cpu/target.hpp
+++ src/targets/cpu/include/migraphx/cpu/target.hpp
@@ -16,8 +16,8 @@ struct target
     std::vector<pass> get_passes(migraphx::context& ctx) const;
     migraphx::context get_context() const { return context{}; }
 
-    argument copy_to(const argument& arg) const { return arg; }
-    argument copy_from(const argument& arg) const { return arg; }
+    argument copy_to(const argument& arg) const { return std::move(arg); }
+    argument copy_from(const argument& arg) const { return std::move(arg); }
     argument allocate(const shape& s) const;
 };
 

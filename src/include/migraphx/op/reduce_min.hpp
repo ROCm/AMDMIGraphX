62d8dd308649a24031e9295db7f689298d633b6d
Shucai Xiao <Shucai.Xiao@amd.com>
2019-09-07 16:44:30 -0500

    add implemenation of reduce_min and reduce_max

---
+++ src/include/migraphx/op/reduce_min.hpp
@@ -0,0 +1,30 @@
+#ifndef MIGRAPHX_GUARD_OPERATORS_REDUCE_MIN_HPP
+#define MIGRAPHX_GUARD_OPERATORS_REDUCE_MIN_HPP
+
+#include <migraphx/op/reduce_op.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace op {
+
+struct reduce_min : reduce_op<reduce_min>
+{
+    reduce_min() {}
+    reduce_min(std::vector<int64_t> ax) : reduce_op(std::move(ax)) {}
+
+    auto op() const
+    {
+        return [=](auto x, auto y) { return x < y ? x : y; };
+    }
+
+    auto init() const
+    {
+        return highest();
+    }
+};
+
+} // namespace op
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx
+
+#endif
--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -59,6 +59,8 @@
 #include <migraphx/gpu/rsqrt.hpp>
 #include <migraphx/gpu/sqrt.hpp>
 #include <migraphx/gpu/reduce_mean.hpp>
+#include <migraphx/gpu/reduce_min.hpp>
+#include <migraphx/gpu/reduce_max.hpp>
 #include <migraphx/gpu/pow.hpp>
 #include <migraphx/gpu/sqdiff.hpp>
 #include <migraphx/gpu/int8_conv_pack.hpp>
@@ -134,6 +136,8 @@ struct miopen_apply
         add_extend_op<hip_clip, op::clip>("clip");
         add_extend_op<hip_reduce_sum, op::reduce_sum>("reduce_sum");
         add_extend_op<hip_reduce_mean, op::reduce_mean>("reduce_mean");
+        add_extend_op<hip_reduce_min, op::reduce_min>("reduce_min");
+        add_extend_op<hip_reduce_max, op::reduce_max>("reduce_max");
 
         add_lrn_op();
         add_convolution_op();

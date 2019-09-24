26aea36c5e6bbe323c192cd74dd19436eceff0be
Paul <pfultz2@yahoo.com>
2019-09-24 10:18:17 -0500

    Formatting

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -169,27 +169,28 @@ struct find_add_convs
 {
     auto matcher() const
     {
-        return match::name("add")(match::args(conv_const_weights().bind("a"), conv_const_weights().bind("b")));
+        return match::name("add")(
+            match::args(conv_const_weights().bind("a"), conv_const_weights().bind("b")));
     }
 
     void apply(program& p, match::matcher_result r) const
     {
-        auto ins   = r.result;
-        auto a_conv = r.instructions["a"];
-        auto a_input = a_conv->inputs().at(0);
+        auto ins       = r.result;
+        auto a_conv    = r.instructions["a"];
+        auto a_input   = a_conv->inputs().at(0);
         auto a_weights = a_conv->inputs().at(1);
-        auto b_conv = r.instructions["b"];
-        auto b_input = b_conv->inputs().at(0);
+        auto b_conv    = r.instructions["b"];
+        auto b_input   = b_conv->inputs().at(0);
         auto b_weights = b_conv->inputs().at(1);
 
         // If convolution is not the same then skip
-        if (a_conv->get_operator() != b_conv->get_operator())
+        if(a_conv->get_operator() != b_conv->get_operator())
             return;
 
-        if (a_weights->get_shape() != b_weights->get_shape())
+        if(a_weights->get_shape() != b_weights->get_shape())
             return;
 
-        auto concat_input = p.insert_instruction(ins, op::concat{1}, a_input, b_input);
+        auto concat_input   = p.insert_instruction(ins, op::concat{1}, a_input, b_input);
         auto concat_weights = p.insert_instruction(ins, op::concat{1}, a_weights, b_weights);
         p.replace_instruction(ins, a_conv->get_operator(), concat_input, concat_weights);
     }

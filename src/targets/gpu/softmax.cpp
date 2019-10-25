a428fc78167eadd2d9e4a584ab17c8fa3cbfea06
Shucai Xiao <shucai.xiao@amd.com>
2019-10-25 13:59:14 -0500

    Cpu conv softmax simplify impl (#397)
    * simplify cpu implementation of the convolution, softmax, and logsoftmax
    
    * clang format
    
    * fix cppcheck error
    
    * improve code coverage

--- src/targets/gpu/softmax.cpp
+++ src/targets/gpu/softmax.cpp
@@ -6,31 +6,6 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
-shape miopen_softmax::compute_shape(const std::vector<shape>& inputs) const
-{
-    check_shapes{inputs, *this}.has(2).standard();
-    return op.compute_shape({inputs.at(0)});
-}
-
-argument miopen_softmax::compute(context& ctx,
-                                 const shape& output_shape,
-                                 const std::vector<argument>& args) const
-{
-    float alpha = 1;
-    float beta  = 0;
-    auto x_desc = make_tensor(args[0].get_shape());
-    auto y_desc = make_tensor(output_shape);
-    miopenSoftmaxForward(ctx.get_stream().get_miopen(),
-                         &alpha,
-                         x_desc.get(),
-                         args[0].implicit(),
-                         &beta,
-                         y_desc.get(),
-                         args[1].implicit());
-
-    return args[1];
-}
-
 shape hip_softmax::compute_shape(const std::vector<shape>& inputs) const
 {
     check_shapes{inputs, *this}.has(2).standard();

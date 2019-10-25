1f4f6d44f4bdd23126183d67b79d44494ed31f18
Shucai Xiao <shucai.xiao@amd.com>
2019-10-25 09:24:50 -0500

    Support initializer not in inputs (#396)
    * support onnx file with initializer data not an input
    
    * fix format
    
    * fix review comments and format issue
    
    * format
    
    * reorder alphabetically

--- test/onnx/gen_onnx.py
+++ test/onnx/gen_onnx.py
@@ -839,6 +839,26 @@ def implicit_sub_bcast_test():
 
 
 @onnx_test
+def initializer_not_an_input():
+    values = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+    w = helper.make_tensor(name='w',
+                           data_type=TensorProto.FLOAT,
+                           dims=values.shape,
+                           vals=values.flatten().astype(np.float))
+
+    x = helper.make_tensor_value_info('x', TensorProto.FLOAT, [5, 2])
+    y = helper.make_tensor_value_info('y', TensorProto.FLOAT, [5, 4])
+
+    node = onnx.helper.make_node(
+        'Gemm',
+        inputs=['x', 'w'],
+        outputs=['y'],
+    )
+
+    return ([node], [x], [y], [w])
+
+
+@onnx_test
 def leaky_relu_test():
     x = helper.make_tensor_value_info('0', TensorProto.FLOAT, [3])
     y = helper.make_tensor_value_info('1', TensorProto.FLOAT, [3])

aeb213dcb5d0048908fe5a24498da42bc385523a
Paul <pfultz2@yahoo.com>
2019-11-01 19:04:55 -0500

    Fix unit test

--- src/eliminate_concat.cpp
+++ src/eliminate_concat.cpp
@@ -5,6 +5,7 @@
 #include <migraphx/op/load.hpp>
 #include <migraphx/op/identity.hpp>
 #include <migraphx/iterator_for.hpp>
+#include <migraphx/ranges.hpp>
 #include <migraphx/dfor.hpp>
 
 namespace migraphx {
@@ -20,7 +21,7 @@ void eliminate_concat::apply(program& p) const
         // If any inputs are used more than once, then abort since there could
         // be errors due to aliasing
         if(std::any_of(ins->inputs().begin(), ins->inputs().end(), [](auto arg) {
-               return arg->name().front() == '@' or arg->get_operator().is_context_free() or
+               return arg->name().front() == '@' or (arg->get_operator().is_context_free() and not contains({"concat", "identity"}, arg->name())) or
                       arg->outputs().size() > 1;
            }))
             continue;

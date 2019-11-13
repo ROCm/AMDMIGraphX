ac74e6437a92382d61e61eca4167a24750f28d9b
Shucai Xiao <Shucai.Xiao@amd.com>
2019-11-13 17:06:10 -0600

    clang format

--- src/include/migraphx/op/gather.hpp
+++ src/include/migraphx/op/gather.hpp
@@ -62,9 +62,8 @@ struct gather
     {
         argument result{output_shape};
         // negative axis means counting dimensions from back
-        auto lens = args[0].get_shape().lens();
-        int axis_index =
-            (axis < 0) ? static_cast<int>(lens.size() + axis) : axis;
+        auto lens      = args[0].get_shape().lens();
+        int axis_index = (axis < 0) ? static_cast<int>(lens.size() + axis) : axis;
 
         std::size_t axis_dim_size = lens[axis_index];
         // max dimension in axis
@@ -73,8 +72,8 @@ struct gather
                 if(output_shape.scalar())
                 {
                     auto in_index = indices.front();
-                    in_index = (in_index < 0) ? in_index + axis_dim_size : in_index;
-                    output[0] = data[indices.front()];
+                    in_index      = (in_index < 0) ? in_index + axis_dim_size : in_index;
+                    output[0]     = data[indices.front()];
                 }
                 else
                 {
@@ -83,8 +82,8 @@ struct gather
                     migraphx::shape out_comp_shape{data.get_shape().type(), out_lens};
                     shape_for_each(out_comp_shape, [&](const auto& out_idx) {
                         auto data_idx        = out_idx;
-                        auto in_index = indices[data_idx[axis_index]];
-                        in_index = (in_index < 0) ? in_index + axis_dim_size : in_index;
+                        auto in_index        = indices[data_idx[axis_index]];
+                        in_index             = (in_index < 0) ? in_index + axis_dim_size : in_index;
                         data_idx[axis_index] = in_index;
                         output[out_comp_shape.index(out_idx.begin(), out_idx.end())] =
                             data(data_idx.begin(), data_idx.end());

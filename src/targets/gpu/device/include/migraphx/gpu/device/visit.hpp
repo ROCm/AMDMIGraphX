91f804a8b49c38cb6f15c72b47def791517f3a0a
Shucai Xiao <Shucai.Xiao@amd.com>
2019-12-06 14:55:35 -0600

    add one more dimens for tensor size

--- src/targets/gpu/device/include/migraphx/gpu/device/visit.hpp
+++ src/targets/gpu/device/include/migraphx/gpu/device/visit.hpp
@@ -44,7 +44,7 @@ constexpr void visit_tensor_size(index_int n, F f)
         f(std::integral_constant<index_int, 6>{});
         break;
     }
-    default: throw std::runtime_error("Unknown tensor size");
+    default: throw std::runtime_error("Tensor size dim out of range");
     }
 }
 

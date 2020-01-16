df7384a657a09ee5e0808cdfa361d28c5bfefd56
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-01-16 11:55:13 -0600

    add verify onnx test

---
+++ test/onnx/verify_onnx.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <migraphx/literal.hpp>
+#include <migraphx/operators.hpp>
+#include <migraphx/program.hpp>
+#include <migraphx/cpu/target.hpp>
+#include <migraphx/pass_manager.hpp>
+#include <migraphx/verify.hpp>
+#include <migraphx/onnx.hpp>
+#include "test.hpp"
+
+TEST_CASE(instance_norm_test)
+{
+    migraphx::program p = migraphx::parse_onnx("instance_norm_val_test.onnx");
+
+    p.compile(migraphx::cpu::target{});
+    auto result = p.eval({});
+    std::vector<float> result_vector(9);
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {-1.54919,
+                               -1.16189,
+                               -0.774596,
+                               -0.387298,
+                               0,
+                               0.387298,
+                               0.774596,
+                               1.16189,
+                               1.54919};
+    EXPECT(migraphx::verify_range(result_vector, gold));
+}
+
+int main(int argc, const char* argv[]) { test::run(argc, argv); }
+

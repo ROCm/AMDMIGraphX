41ab853624b0122ffea045ea8e08c1b1c968b3d4
Shucai Xiao <Shucai.Xiao@amd.com>
2020-02-14 12:26:37 -0600

    clang format

--- src/eliminate_contiguous.cpp
+++ src/eliminate_contiguous.cpp
@@ -70,9 +70,9 @@ void eliminate_contiguous::apply(program& p) const
     for(auto ins : iterator_for(p))
     {
         // return instruction should have inputs with standard shape
-        if (ins->name() == "return")
+        if(ins->name() == "return")
             continue;
-            
+
         // Make a copy so we can modify it while we iterate
         auto args = ins->inputs();
         for(auto arg : ins->inputs())

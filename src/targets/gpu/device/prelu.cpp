e88507a0a0677c127ccbf46b95a1cfb050608bd2
Shucai Xiao <Shucai.Xiao@amd.com>
2020-03-02 18:16:32 -0600

    clang format

--- src/targets/gpu/device/prelu.cpp
+++ src/targets/gpu/device/prelu.cpp
@@ -8,7 +8,8 @@ namespace device {
 
 void prelu(hipStream_t stream, const argument& result, const argument& arg1, const argument& arg2)
 {
-    nary(stream, result, arg1, arg2)([](auto x, auto slope) __device__ { return ((x < 0) ? (x * slope) : x); });
+    nary(stream, result, arg1, arg2)([](auto x, auto slope)
+                                         __device__ { return ((x < 0) ? (x * slope) : x); });
 }
 
 } // namespace device

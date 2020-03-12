1de7845737f18b374951765e1d33cbfccd92a70a
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-03-12 15:16:22 -0500

    add gelu_fn and tests

--- src/targets/gpu/device/gelu.cpp
+++ src/targets/gpu/device/gelu.cpp
@@ -1,6 +1,7 @@
 #include <migraphx/gpu/device/gelu.hpp>
 #include <migraphx/gpu/device/nary.hpp>
 #include <migraphx/gpu/device/types.hpp>
+#include <cmath>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -8,10 +9,16 @@ namespace gpu {
 namespace device {
 
 // x * 0.5 * (1.0 + erf(x / sqrt(2.0)))
+template<class T>
+auto gelu_fn(T x) __device__
+{
+    return x * 0.5 * (1 + ::erf(x * M_SQRT1_2));
+}
+
 void gelu(hipStream_t stream, const argument& result, const argument& arg)
 {
     nary(stream, result, arg)([](auto x) __device__ {
-        return to_hip_type(x) * 0.5 * (1 + ::erf(to_hip_type(x) * ::rsqrt(2)));
+        return gelu_fn(to_hip_type(x));
     });
 }
 
@@ -22,7 +29,7 @@ void add_gelu(hipStream_t stream,
 {
     nary(stream, result, arg1, arg2)([](auto x, auto y) __device__ {
         auto sum = to_hip_type(x + y);
-        return sum * 0.5 * (1 + ::erf(sum * ::rsqrt(2)));
+        return gelu_fn(sum);
     });
 }
 
--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -17,6 +17,7 @@
 
 #include <future>
 #include <thread>
+#include <cmath>
 
 #include <test.hpp>
 
@@ -665,6 +666,52 @@ struct test_triadd_broadcast : verify_program<test_triadd_broadcast>
         auto by  = p.add_instruction(migraphx::op::broadcast{0, x->get_shape().lens()}, y);
         auto sum = p.add_instruction(migraphx::op::add{}, x, by);
         p.add_instruction(migraphx::op::add{}, sum, z);
+        return p;
+    }
+};
+
+struct test_gelu : verify_program<test_gelu>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        std::vector<size_t> input_lens{1, 1, 5};
+        auto x = p.add_parameter("x", {migraphx::shape::float_type, input_lens});
+        auto half = p.add_literal(0.5f);
+        auto one = p.add_literal(1.0f);
+        auto sqrt2 = p.add_literal(static_cast<float>(M_SQRT2));
+        auto half_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, half);
+        auto mul_half = p.add_instruction(migraphx::op::mul{}, x, half_mbcast);
+        auto sqrt2_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, sqrt2);
+        auto div = p.add_instruction(migraphx::op::div{}, x, sqrt2_mbcast);
+        auto erf = p.add_instruction(migraphx::op::erf{}, div);
+        auto one_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, one);
+        auto add_one = p.add_instruction(migraphx::op::add{}, erf, one_mbcast);
+        p.add_instruction(migraphx::op::mul{}, mul_half, add_one);
+        return p;
+    }
+};
+
+struct test_add_gelu : verify_program<test_add_gelu>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        std::vector<size_t> input_lens{1, 1, 5};
+        auto x = p.add_parameter("x", {migraphx::shape::float_type, input_lens});
+        auto y = p.add_parameter("y", {migraphx::shape::float_type, input_lens});
+        auto half = p.add_literal(0.5f);
+        auto one = p.add_literal(1.0f);
+        auto sqrt2 = p.add_literal(static_cast<float>(M_SQRT2));
+        auto add = p.add_instruction(migraphx::op::add{}, x, y);
+        auto half_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, half);
+        auto mul_half = p.add_instruction(migraphx::op::mul{}, add, half_mbcast);
+        auto sqrt2_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, sqrt2);
+        auto div = p.add_instruction(migraphx::op::div{}, add, sqrt2_mbcast);
+        auto erf = p.add_instruction(migraphx::op::erf{}, div);
+        auto one_mbcast = p.add_instruction(migraphx::op::multibroadcast{input_lens}, one);
+        auto add_one = p.add_instruction(migraphx::op::add{}, erf, one_mbcast);
+        p.add_instruction(migraphx::op::mul{}, mul_half, add_one);
         return p;
     }
 };

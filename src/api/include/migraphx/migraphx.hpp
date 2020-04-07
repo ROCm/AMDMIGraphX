fc3cd2101ecd9fb3005cfd1c8e300deb9d5c3745
Shucai Xiao <Shucai.Xiao@amd.com>
2020-04-07 12:17:30 -0500

    fixed a bug in the cpp api for the case of zero input parameter

--- src/api/include/migraphx/migraphx.hpp
+++ src/api/include/migraphx/migraphx.hpp
@@ -344,7 +344,10 @@ struct program_parameter_shapes : MIGRAPHX_HANDLE_BASE(program_parameter_shapes)
     std::vector<const char*> names() const
     {
         std::vector<const char*> result(this->size());
-        call(&migraphx_program_parameter_shapes_names, result.data(), this->get_handle_ptr());
+        if (result.size() > 0)
+        {
+            call(&migraphx_program_parameter_shapes_names, result.data(), this->get_handle_ptr());
+        }
         return result;
     }
 };

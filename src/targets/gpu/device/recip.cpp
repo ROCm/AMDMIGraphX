9d1366655d79f5517d2e2d51983b25e05b4fc326
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-04-06 22:54:24 -0500

    formatting

--- src/targets/gpu/device/recip.cpp
+++ src/targets/gpu/device/recip.cpp
@@ -9,7 +9,7 @@ namespace device {
 
 void recip(hipStream_t stream, const argument& result, const argument& arg)
 {
-    nary(stream, result, arg)([](auto x) __device__ { return 1/(to_hip_type(x)); });
+    nary(stream, result, arg)([](auto x) __device__ { return 1 / (to_hip_type(x)); });
 }
 
 } // namespace device

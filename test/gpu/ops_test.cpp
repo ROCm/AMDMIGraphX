c0bab610e02b9ed34280fba44b55cc6a10d1d627
Paul <pfultz2@yahoo.com>
2020-04-09 14:43:12 -0500

    Formatting

--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -2225,7 +2225,7 @@ struct test_pad_transposed : verify_program<test_pad_transposed>
     {
         migraphx::program p;
         migraphx::shape s{migraphx::shape::int32_type, {1, 224, 224, 3}};
-        auto x                    = p.add_parameter("x", s);
+        auto x = p.add_parameter("x", s);
         auto t = p.add_instruction(migraphx::op::transpose{{0, 3, 1, 2}}, x);
         p.add_instruction(migraphx::op::pad{{0, 0, 2, 2, 0, 0, 3, 3}}, t);
         return p;

2c81e84a56c3e479d65aa03dc79ee089bd681610
Paul <pfultz2@yahoo.com>
2020-04-09 14:43:05 -0500

    Fix bug with transpose and pad

--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -2219,6 +2219,19 @@ struct test_pad : verify_program<test_pad>
     }
 };
 
+struct test_pad_transposed : verify_program<test_pad_transposed>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        migraphx::shape s{migraphx::shape::int32_type, {1, 224, 224, 3}};
+        auto x                    = p.add_parameter("x", s);
+        auto t = p.add_instruction(migraphx::op::transpose{{0, 3, 1, 2}}, x);
+        p.add_instruction(migraphx::op::pad{{0, 0, 2, 2, 0, 0, 3, 3}}, t);
+        return p;
+    }
+};
+
 struct test_pad_int8 : verify_program<test_pad_int8>
 {
     migraphx::program create_program() const

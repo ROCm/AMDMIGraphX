56c4fea63e61ba184f67f054ef68dbf2447120bd
Shucai Xiao <Shucai.Xiao@amd.com>
2020-04-09 20:49:51 -0500

    clang format

--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -1723,8 +1723,8 @@ struct gemm_multi_transpose : verify_program<gemm_multi_transpose>
         migraphx::program p;
         migraphx::shape m1_shape{migraphx::shape::float_type, {2, 2, 3}};
         migraphx::shape m2_shape{migraphx::shape::float_type, {3, 2, 4}};
-        auto l1 = p.add_parameter("1", m1_shape);
-        auto l2 = p.add_parameter("2", m2_shape);
+        auto l1  = p.add_parameter("1", m1_shape);
+        auto l2  = p.add_parameter("2", m2_shape);
         auto tl2 = p.add_instruction(migraphx::op::transpose{{1, 0, 2}}, l2);
 
         float alpha = 1.0f;

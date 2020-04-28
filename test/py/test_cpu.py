578d2829df3400b6e6743917cda11b2b9a31d2a6
Shucai Xiao <Shucai.Xiao@amd.com>
2020-04-28 16:15:44 -0500

    refine unit tests to cover code change

--- test/py/test_cpu.py
+++ test/py/test_cpu.py
@@ -1,6 +1,4 @@
-import migraphx
-import numpy as np
-
+import migraphx, array, sys
 
 def test_conv_relu():
     p = migraphx.parse_onnx("conv_relu_maxpool_test.onnx")
@@ -21,6 +19,15 @@ def test_conv_relu():
     print(r)
 
 
+def create_buffer(t, data, shape):
+    a = array.array(t, data)
+    if sys.version_info >= (3, 0):
+        m = memoryview(a.tobytes())
+        return m.cast(t, shape)
+    else:
+        m = memoryview(a.tostring())
+        return m
+
 def test_add_scalar():
     p = migraphx.parse_onnx("add_scalar_test.onnx")
     print(p)
@@ -30,13 +37,16 @@ def test_add_scalar():
     print(p)
     s2 = p.get_output_shapes()[-1]
     assert s1 == s2
-    params = {}
 
-    args = []
-    args.append(np.random.randn(2, 3, 4, 5).astype(np.single))
-    args.append(np.array(1).astype(np.single))
-    params["0"] = migraphx.argument(args[0])
-    params["1"] = migraphx.argument(args[1])
+
+    d0 = list(range(120))
+    arg0 = create_buffer("f", d0, [2, 3, 4, 5])
+    d1 = [1]
+    arg1 = create_buffer("f", d1, ())
+
+    params = {}
+    params["0"] = migraphx.argument(arg0)
+    params["1"] = migraphx.argument(arg1)
 
     r = p.run(params)[-1]
     print(r)

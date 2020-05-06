a5df9c993de0ef110e0c955d09febeaa1a7e400a
Paul <pfultz2@yahoo.com>
2020-05-06 16:12:17 -0500

    Add support for when beta is not 1

--- src/decompose.cpp
+++ src/decompose.cpp
@@ -7,6 +7,8 @@
 #include <migraphx/float_equal.hpp>
 #include <migraphx/matcher.hpp>
 #include <migraphx/op/dot.hpp>
+#include <migraphx/op/multibroadcast.hpp>
+#include <migraphx/op/mul.hpp>
 #include <migraphx/op/add.hpp>
 
 namespace migraphx {
@@ -20,11 +22,18 @@ struct find_dot_add
     {
         auto ins = r.result;
         auto dot = any_cast<op::dot>(ins->get_operator());
-        if(not float_equal(dot.beta, 1))
+        if(not float_equal(dot.beta, 1) and not contains({shape::float_type, shape::half_type, shape::double_type}, ins->get_shape().type()))
             return;
         auto dot_ins =
             p.insert_instruction(ins, op::dot{dot.alpha, 0}, ins->inputs()[0], ins->inputs()[1]);
-        p.replace_instruction(ins, op::add{}, dot_ins, ins->inputs()[2]);
+        auto c_ins = ins->inputs()[2];
+        if (not float_equal(dot.beta, 1))
+        {
+            auto beta = p.add_literal(literal{shape{ins->get_shape().type()}, {dot.beta}});
+            auto beta_broadcast = p.insert_instruction(ins, op::multibroadcast{ins->get_shape().lens()}, beta);
+            c_ins = p.insert_instruction(ins, op::mul{}, c_ins, beta_broadcast);
+        }
+        p.replace_instruction(ins, op::add{}, dot_ins, c_ins);
     }
 };
 

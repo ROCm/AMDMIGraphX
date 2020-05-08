6d2519608e52773d3f3f2103fbea357df1b680c1
Paul <pfultz2@yahoo.com>
2020-05-07 21:05:01 -0500

    Add test for max

--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -2285,6 +2285,23 @@ struct test_pad_lowest : verify_program<test_pad_lowest>
     }
 };
 
+struct test_pad_highest : verify_program<test_pad_highest>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        std::vector<migraphx::half> data0(4);
+        std::iota(data0.begin(), data0.end(), 0);
+        migraphx::shape s0{migraphx::shape::half_type, {2, 2}};
+        auto l0 = p.add_literal(migraphx::literal{s0, data0});
+        migraphx::op::pad op{};
+        op.value = std::numeric_limits<float>::max();
+        op.pads  = {0, 0, 1, 1};
+        p.add_instruction(op, l0);
+        return p;
+    }
+};
+
 struct test_pooling_autopad : verify_program<test_pooling_autopad>
 {
     migraphx::program create_program() const

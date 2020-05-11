fb73d6a95e5bad3b1bf2e9d9282608cb0d301c89
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-05-11 13:02:35 -0500

    formatting

--- src/eliminate_common_subexpression.cpp
+++ src/eliminate_common_subexpression.cpp
@@ -20,7 +20,7 @@ void cse_range(program& p, Range&& r)
         // Skip dead instructions
         if(ins->outputs().empty())
             continue;
-        
+
         // Find instruction with the same name
         auto found_instructions = range(instructions.equal_range(ins->name()));
         for(const auto& pp : found_instructions)

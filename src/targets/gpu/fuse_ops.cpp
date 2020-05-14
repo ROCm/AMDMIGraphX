656463a6842344a7f57213f78a0bbb68308861f2
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-05-14 17:23:08 -0500

    formatting

--- src/targets/gpu/fuse_ops.cpp
+++ src/targets/gpu/fuse_ops.cpp
@@ -406,10 +406,12 @@ struct find_gelu_new
 
     auto matcher() const
     {
-        return match::name("gpu::mul")(match::used_once(), match::either_arg(0, 1)(
-            match::any().bind("x"),
-            match::name("gpu::add")(match::any_arg(0, 1)(match::name("gpu::mul")(
-                match::either_arg(0, 1)(match::args(match::has_value(0.5f)), tanh_fn()))))));
+        return match::name("gpu::mul")(
+            match::used_once(),
+            match::either_arg(0, 1)(
+                match::any().bind("x"),
+                match::name("gpu::add")(match::any_arg(0, 1)(match::name("gpu::mul")(
+                    match::either_arg(0, 1)(match::args(match::has_value(0.5f)), tanh_fn()))))));
     }
 
     void apply(program& p, match::matcher_result r) const

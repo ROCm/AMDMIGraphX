e592a519fc8887aa545b25f14e394cdabcd7fa53
Shucai Xiao <Shucai.Xiao@amd.com>
2020-05-15 13:23:17 -0500

    code cleanup

--- src/targets/gpu/fuse_ops.cpp
+++ src/targets/gpu/fuse_ops.cpp
@@ -8,7 +8,6 @@
 #include <migraphx/gpu/oper.hpp>
 #include <migraphx/gpu/device/gelu.hpp>
 #include <migraphx/gpu/gemm.hpp>
-#include <migraphx/gpu/device/layernorm.hpp>
 #include <migraphx/gpu/device/mul_add.hpp>
 #include <migraphx/gpu/device/add_clip.hpp>
 #include <migraphx/gpu/device/add_relu.hpp>
@@ -263,10 +262,6 @@ struct hip_add_tanh : binary_device<hip_add_tanh, &device::add_tanh>
 {
 };
 
-struct hip_layernorm : unary_device<hip_layernorm, &device::layernorm>
-{
-};
-
 struct hip_gelu : unary_device<hip_gelu, &device::gelu>
 {
 };
@@ -274,9 +269,6 @@ struct hip_gelu : unary_device<hip_gelu, &device::gelu>
 struct hip_add_gelu : binary_device<hip_add_gelu, &device::add_gelu>
 {
 };
-// struct hip_layernorm : ternary_device<hip_layernorm, &device::layernorm>
-// {
-// };
 
 struct hip_gelu_new : unary_device<hip_gelu_new, &device::gelu_new>
 {
@@ -435,53 +427,6 @@ void move_standard_front(std::vector<instruction_ref>& args)
     if(it != last)
         std::swap(*it, args.front());
 }
-
-struct find_layernorm
-{
-    template <class... Ts>
-    static auto multibroadcast_op(Ts... xs)
-    {
-        return match::name("multibroadcast")(match::arg(0)(xs...));
-    }
-
-    static auto layernorm_onnx()
-    {
-        return match::arg(0)(match::name("gpu::sub")(
-            match::arg(0)(match::any().bind("x")),
-            match::arg(1)(multibroadcast_op(match::name("gpu::reduce_mean")))));
-    }
-
-    static auto layernorm_tf()
-    {
-        return match::either_arg(0, 1)(
-            match::name("gpu::mul")(match::arg(0)(match::name("gpu::add").bind("x"))),
-            match::name("gpu::sub")(match::either_arg(0, 1)(
-                multibroadcast_op().bind("bias"),
-                match::name("gpu::mul")(match::arg(1)(
-                    match::name("gpu::mul")(match::arg(1)(multibroadcast_op().bind("scale")))
-
-                        )))));
-    }
-
-    auto matcher() const
-    {
-        return match::name("gpu::div")(layernorm_onnx());
-        // return match::name("gpu::add")(match::any_of(layernorm_onnx(), layernorm_tf()));
-    }
-
-    void apply(program& p, match::matcher_result r) const
-    {
-        auto ins = r.result;
-        // auto scale_ins = r.instructions["scale"];
-        auto x_ins = r.instructions["x"];
-        // auto bias_ins  = r.instructions["bias"];
-        auto args = ins->inputs();
-
-        p.replace_instruction(ins, hip_layernorm{}, x_ins, args.back());
-        // p.replace_instruction(ins, hip_layernorm{}, x_ins, scale_ins, bias_ins, args.back());
-        // p.replace_instruction(ins, miopen_batch_norm{}, x_ins, scale_ins, bias_ins, args.back());
-    }
-};
 
 struct find_gelu
 {
@@ -955,12 +900,10 @@ struct find_conv_bias_relu
 
 void fuse_ops::apply(program& p) const
 {
-    // clang-format off
-    match::find_matches(p, find_layernorm{}, find_gelu{});
-    match::find_matches(p, find_slice_reshape_trans_cont{});
-    match::find_matches(p, find_slice_reshape_trans_cont_1{});    
     match::find_matches(p, find_gelu{}, find_gelu_new{});
     run_passes(p, {dead_code_elimination{}});
+    match::find_matches(p, find_slice_reshape_trans_cont{});
+    match::find_matches(p, find_slice_reshape_trans_cont_1{});    
     match::find_matches(p, find_triadd{});
     match::find_matches(p, 
         find_conv_bias_relu{ctx},

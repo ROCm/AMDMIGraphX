8ad892e9d88939584b2c6783059fd5ac772d3e50
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2020-05-15 20:17:39 -0500

    Add range op (#529)
    * add range op and test
    
    * add float test
    
    * use generate for vector
    
    * formatting
    
    * fix delta arg check
    
    * fix assert
    
    * formatting
    
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -1228,6 +1228,32 @@ TEST_CASE(prelu_brcst_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(range_test)
+{
+    migraphx::program p;
+    p.add_literal(int64_t{10});
+    p.add_literal(int64_t{6});
+    p.add_literal(int64_t{-3});
+    p.add_literal(migraphx::literal{{migraphx::shape::int64_type, {2}}, {10, 7}});
+
+    auto prog = optimize_onnx("range_test.onnx");
+
+    EXPECT(p == prog);
+}
+
+TEST_CASE(range_float_test)
+{
+    migraphx::program p;
+    p.add_literal(float{2});
+    p.add_literal(float{11});
+    p.add_literal(float{2});
+    p.add_literal(migraphx::literal{{migraphx::shape::float_type, {5}}, {2, 4, 6, 8, 10}});
+
+    auto prog = optimize_onnx("range_float_test.onnx");
+
+    EXPECT(p == prog);
+}
+
 TEST_CASE(recip_test)
 {
     migraphx::program p;

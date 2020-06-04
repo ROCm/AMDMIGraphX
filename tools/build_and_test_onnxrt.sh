3d7bef05344345f772c0b02748c172c500ec4261
Paul <pfultz2@yahoo.com>
2020-06-04 16:02:58 -0500

    Install requirements

--- tools/build_and_test_onnxrt.sh
+++ tools/build_and_test_onnxrt.sh
@@ -1,4 +1,5 @@
 cd /onnxruntime
+pip install -r requirements.txt
 # Add newer cmake to the path
 export PATH="/opt/cmake/bin:$PATH"
 ./build.sh --config Release --build_wheel --update --build --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=$(cat ./VERSION_NUMBER) --test --use_migraphx

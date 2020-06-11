de2c5c5bcd5d0faf0aba5c2c711ba3c251e620c4
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-06-10 19:09:06 -0500

    formatting

--- src/targets/gpu/include/migraphx/gpu/miopen.hpp
+++ src/targets/gpu/include/migraphx/gpu/miopen.hpp
@@ -75,8 +75,9 @@ inline tensor_descriptor make_tensor(const migraphx::shape& os, bool pack = fals
     return t;
 }
 
-inline void
-insert_attribute_dim(std::vector<int>& padding, std::vector<int>& stride, std::vector<int>& dilation)
+inline void insert_attribute_dim(std::vector<int>& padding,
+                                 std::vector<int>& stride,
+                                 std::vector<int>& dilation)
 {
     padding.insert(padding.begin(), 0);
     stride.insert(stride.begin(), 1);

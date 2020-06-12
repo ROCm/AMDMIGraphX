3a8642c12d6bbfb7e27355df41f8fa7294da4e16
Paul <pfultz2@yahoo.com>
2020-06-11 21:37:07 -0500

    Install pip3

--- tools/build_and_test_onnxrt.sh
+++ tools/build_and_test_onnxrt.sh
@@ -1,9 +1,5 @@
 cd /onnxruntime
-pip install -r requirements.txt
-echo "pip list"
-pip list
-echo "pip3 list"
-pip3 list
+pip3 install -r requirements.txt
 # Add newer cmake to the path
 export PATH="/opt/cmake/bin:$PATH"
 ./build.sh --config Release --build_wheel --update --build --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=$(cat ./VERSION_NUMBER) --test --use_migraphx

d7bc89f2c416ce67be0495c1dcec13565899014d
Paul <pfultz2@yahoo.com>
2020-06-12 10:49:26 -0500

    Add cxxflags for hip

--- tools/build_and_test_onnxrt.sh
+++ tools/build_and_test_onnxrt.sh
@@ -2,5 +2,6 @@ cd /onnxruntime
 pip3 install -r requirements.txt
 # Add newer cmake to the path
 export PATH="/opt/cmake/bin:$PATH"
+export CXXFLAGS="-D__HIP_PLATFORM_HCC__=1"
 ./build.sh --config Release --build_wheel --update --build --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=$(cat ./VERSION_NUMBER) --test --use_migraphx
 pip install /code/onnxruntime/build/Linux/Release/dist/*.whl

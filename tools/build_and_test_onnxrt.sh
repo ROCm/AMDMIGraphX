4f8d3303b691c039d9a15bc8272dfbdb0b0405ad
Paul <pfultz2@yahoo.com>
2020-06-15 10:11:01 -0500

    Install wheel with pip3

--- tools/build_and_test_onnxrt.sh
+++ tools/build_and_test_onnxrt.sh
@@ -4,4 +4,4 @@ pip3 install -r requirements.txt
 export PATH="/opt/cmake/bin:$PATH"
 export CXXFLAGS="-D__HIP_PLATFORM_HCC__=1"
 ./build.sh --config Release --build_wheel --update --build --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=$(cat ./VERSION_NUMBER) --test --use_migraphx
-pip install /code/onnxruntime/build/Linux/Release/dist/*.whl
+pip3 install /code/onnxruntime/build/Linux/Release/dist/*.whl

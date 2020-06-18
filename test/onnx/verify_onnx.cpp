893f846812a31eb6062b6e502d70f60c450a7238
Shucai Xiao <Shucai.Xiao@amd.com>
2020-06-18 12:17:42 -0500

    clang format

--- test/onnx/verify_onnx.cpp
+++ test/onnx/verify_onnx.cpp
@@ -44,13 +44,14 @@ TEST_CASE(gather_elements)
     migraphx::program p = migraphx::parse_onnx("gather_elements_axis0_test.onnx");
     p.compile(migraphx::cpu::target{});
     migraphx::shape s_data{migraphx::shape::float_type, {3, 4}};
-    std::vector<float> data = {0.25, 0.75, 0.9375, 0.4375, 0.6875, 0.5625, -0.875, 0.1875, -0.125, 0.5, -0.9375, -0.0625};
+    std::vector<float> data = {
+        0.25, 0.75, 0.9375, 0.4375, 0.6875, 0.5625, -0.875, 0.1875, -0.125, 0.5, -0.9375, -0.0625};
 
     migraphx::shape s_ind{migraphx::shape::int32_type, {2, 3}};
     std::vector<int> ind = {2, 1, 2, 0, 1, 0};
 
     migraphx::program::parameter_map pp;
-    pp["data"] = migraphx::argument(s_data, data.data());
+    pp["data"]    = migraphx::argument(s_data, data.data());
     pp["indices"] = migraphx::argument(s_ind, ind.data());
 
     auto result = p.eval(pp).back();

fbafa976b7042f06266883ef26a22b638ac5a2a4
Shucai Xiao <Shucai.Xiao@amd.com>
2020-06-30 10:09:11 -0500

    remove a unit test

--- test/gpu/ops_test.cpp
+++ test/gpu/ops_test.cpp
@@ -1837,36 +1837,6 @@ struct test_batchnorm_3d : verify_program<test_batchnorm_3d>
     }
 };
 
-struct test_batchnorm_3d_per_actv : verify_program<test_batchnorm_3d_per_actv>
-{
-    const size_t d1       = 2;
-    const size_t d2       = 3;
-    const size_t d3       = 2;
-    const size_t channels = 2;
-    const size_t batches  = 3;
-
-    migraphx::program create_program() const
-    {
-        migraphx::program p;
-
-        migraphx::shape s{migraphx::shape::float_type, {batches, channels, d1, d2, d3}};
-        migraphx::shape vars{migraphx::shape::float_type, {channels, d1, d2, d3}};
-        auto x        = p.add_parameter("x", s);
-        auto scale    = p.add_literal(migraphx::abs(migraphx::generate_literal(vars, 1)));
-        auto bias     = p.add_literal(migraphx::abs(migraphx::generate_literal(vars, 2)));
-        auto mean     = p.add_literal(migraphx::abs(migraphx::generate_literal(vars, 3)));
-        auto variance = p.add_literal(migraphx::abs(migraphx::generate_literal(vars, 4)));
-        p.add_instruction(
-            migraphx::op::batch_norm_inference{migraphx::op::batch_norm_inference::per_activation},
-            x,
-            scale,
-            bias,
-            mean,
-            variance);
-        return p;
-    }
-};
-
 struct test_clip : verify_program<test_clip>
 {
     migraphx::program create_program() const

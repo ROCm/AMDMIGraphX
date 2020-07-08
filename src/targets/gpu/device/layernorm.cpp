2f7000c03466d124082c52c8d285caa335377d5c
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-07-08 03:57:55 -0500

    more progress

--- src/targets/gpu/device/layernorm.cpp
+++ src/targets/gpu/device/layernorm.cpp
@@ -2,6 +2,7 @@
 #include <migraphx/gpu/device/reduce.hpp>
 #include <migraphx/gpu/device/pow.hpp>
 #include <iostream>
+#include <stdio.h>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -10,24 +11,21 @@ namespace device {
 
 // m = x - mean(x)
 // m / sqrt(mean(m ^ 2) + 1e-12)
-void layernorm(hipStream_t stream, const argument& result, const argument& arg1
-               //    ,
-               //    const argument& arg2,
-               //    const argument& arg3
-)
+void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
 {
     auto relements    = arg1.get_shape().lens().back();
+    auto nelements   = result.get_shape().elements() / relements;
     auto input_shape  = arg1.get_shape();
     auto output_shape = result.get_shape();
-    auto nelements    = output_shape.elements() / relements;
     auto reduce_output_lens(output_shape.lens());
     reduce_output_lens.back() = 1;
 
     std::vector<index_int> reduce_lens = get_reduce_lens(input_shape.lens(), reduce_output_lens);
     shape reduce_slice{output_shape.type(), reduce_lens};
     shape reduce_output_shape{output_shape.type(), reduce_output_lens};
-    hip_visit_all(result, arg1, reduce_slice, reduce_output_shape)(
-        [&](auto output, auto input, auto reduce_shape, auto reduce_output) {
+
+    hip_visit_all(result, arg1)(
+        [&](auto output, auto input) {
             using value_type = typename decltype(input)::value_type;
 
             const std::size_t max_block_size = 256;
@@ -37,110 +35,30 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1
                 const auto out_idx  = i / block_size;
                 const auto base_idx = out_idx * relements;
                 value_type x[4];
-                int k = 0;
 
-                idx.local_stride(relements, [&](auto j) { x[j] = input.data()[base_idx + j]; });
+                idx.local_stride(relements,
+                                [&](auto j) __device__ { x[j] = input.data()[base_idx + j]; });
+                auto m = block_reduce<max_block_size>(
+                            idx, sum{}, 0, relements, [&](auto j) __device__ { return x[j]; }) /
+                        relements;
 
-                idx.local_stride(relements, [&](auto j) {
-                    // k++;
-                    output[j] = x[base_idx + j];
 
-                    // output[i + j] = x[k - 1] * ::rsqrt(r + 1e-12);
+                idx.local_stride(relements, [&](auto j) __device__ { x[j] = x[j] - m; });
+
+                auto r = block_reduce<max_block_size>(
+                            idx, sum{}, 0, relements, [&](auto j) __device__ { return ::pow(to_hip_type(x[j]), 2); }) /
+                        relements;
+
+                idx.local_stride(relements, [&](auto j) __device__ {
+                    output.data()[base_idx + j] = (x[j]) / ::sqrt(r + 1e-12);
                 });
 
-                // k      = 0;
-                // auto m = block_reduce<max_block_size>(idx,
-                //                                         sum{},
-                //                                         0,
-                //                                         relements,
-                //                                         [&](auto j) __device__ {
-                //                                             return x[j];
-                //                                         }) /
-                //             relements;
-
-                // k = 0;
-
-                // idx.local_stride(relements, [&](auto j) {
-                //     x[j] -= m;
-                // });
-
-                // k = 0;
-                // auto r =
-                //     block_reduce<max_block_size>(idx,
-                //                                     sum{},
-                //                                     0,
-                //                                     relements,
-                //                                     [&](auto j) __device__ {
-                //                                         return ::pow(to_hip_type(x[j]), 2);
-                //                                     }) /
-                //     relements;
-
-                // k = 0;
 
             });
 
-            //     mi_launch(stream, reduce_output, reduce_shape, block_size)(
-            //         [=](auto idx, auto global, auto local) __device__ {
-            //             global([&](auto i) __device__ {
-
-            //                 value_type x[4];
-            //                 int k = 0;
-            //                 local([&](auto j) {
-            //                     k++;
-            //                     x[k - 1] = input[i + j];
-            //                 });
-
-            //                 k      = 0;
-            //                 auto m = block_reduce<max_block_size>(idx,
-            //                                                       sum{},
-            //                                                       0,
-            //                                                       relements,
-            //                                                       [&](auto) __device__ {
-            //                                                           k++;
-            //                                                           return x[k - 1];
-            //                                                       }) /
-            //                          relements;
-
-            //                 k = 0;
-            //                 local([&](auto) {
-            //                     k++;
-            //                     x[k - 1] -= m;
-            //                 });
-
-            //                 k = 0;
-            //                 auto r =
-            //                     block_reduce<max_block_size>(idx,
-            //                                                  sum{},
-            //                                                  0,
-            //                                                  relements,
-            //                                                  [&](auto) __device__ {
-            //                                                      k++;
-            //                                                      return ::pow(to_hip_type(x[k -
-            //                                                      1]), 2);
-            //                                                  }) /
-            //                     relements;
-
-            //                 k = 0;
-            //                 local([&](auto j) {
-            //                     k++;
-            //                     output[i + j] = x[k - 1] * ::rsqrt(r + 1e-12);
-            //                     // auto a        = scale[i + j] * ::rsqrt(r + 1e-12);
-            //                     // output[i + j] = a * x[k - 1] + bias[i + j];
-            //                 });
-
-            //             });
-            //         });
+            
         });
 
-    // hip_visit_all(result, arg1, arg2, arg3, reduce_slice, reduce_output_shape)(
-    // [&](auto output, auto input, auto scale, auto bias, auto reduce_shape, auto reduce_output) {
-    //     using value_type = typename decltype(input)::value_type;
-    //      auto nelements   = result.get_shape().elements() / relements;
-
-    //     const std::size_t max_block_size = 256;
-    //     const std::size_t block_size     = compute_block_size(relements, max_block_size);
-
-    // });
 }
 
 } // namespace device

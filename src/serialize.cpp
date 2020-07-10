f45545b9769b721a0529e522cd4bdd0c527211db
Paul <pfultz2@yahoo.com>
2020-07-10 16:42:03 -0500

    Formatting

--- src/serialize.cpp
+++ src/serialize.cpp
@@ -5,40 +5,31 @@
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
-template<class RawData>
+template <class RawData>
 void raw_data_to_value(value& v, const RawData& rd)
 {
     value result;
     result["shape"] = migraphx::to_value(rd.get_shape());
-    rd.visit([&](auto x) {
-        result["data"] = std::vector<value>(x.begin(), x.end());
-    });
+    rd.visit([&](auto x) { result["data"] = std::vector<value>(x.begin(), x.end()); });
     v = result;
 }
 
-void migraphx_to_value(value& v, const literal& l)
-{
-    raw_data_to_value(v, l);
-}
+void migraphx_to_value(value& v, const literal& l) { raw_data_to_value(v, l); }
 void migraphx_from_value(const value& v, literal& l)
 {
     auto s = migraphx::from_value<shape>(v.at("shape"));
     s.visit_type([&](auto as) {
         using type = typename decltype(as)::type;
-        l = literal{s, v.at("data").to_vector<type>()};
+        l          = literal{s, v.at("data").to_vector<type>()};
     });
 }
 
-void migraphx_to_value(value& v, const argument& a)
-{
-    raw_data_to_value(v, a);
-}
+void migraphx_to_value(value& v, const argument& a) { raw_data_to_value(v, a); }
 void migraphx_from_value(const value& v, argument& a)
 {
     literal l = migraphx::from_value<literal>(v);
-    a = l.get_argument();
+    a         = l.get_argument();
 }
-
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

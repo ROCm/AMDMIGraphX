5226d21c5ab788a2bb4e8e46132b3d198c855d92
Shucai Xiao <Shucai.Xiao@amd.com>
2020-07-17 18:13:00 -0500

    clang format

--- test/rewrite_pooling_test.cpp
+++ test/rewrite_pooling_test.cpp
@@ -26,7 +26,7 @@ TEST_CASE(rewrite_maxpooling_test)
         migraphx::program p;
         migraphx::shape s{migraphx::shape::float_type, {1, 2, 3}};
         auto input = p.add_parameter("x", s);
-        auto ret = p.add_instruction(migraphx::op::pooling{"max", {0}, {1}, {3}}, input);
+        auto ret   = p.add_instruction(migraphx::op::pooling{"max", {0}, {1}, {3}}, input);
         p.add_return({ret});
         return p;
     };
@@ -43,7 +43,8 @@ TEST_CASE(rewrite_avepooling_test)
     auto pooling_program = [&]() {
         migraphx::program p;
         auto input = p.add_parameter("x", s);
-        auto ret = p.add_instruction(migraphx::op::pooling{"average", {0, 0, 0}, {1, 1, 1}, {3, 4, 5}}, input);
+        auto ret   = p.add_instruction(
+            migraphx::op::pooling{"average", {0, 0, 0}, {1, 1, 1}, {3, 4, 5}}, input);
         p.add_return({ret});
         return p;
     };
@@ -51,9 +52,9 @@ TEST_CASE(rewrite_avepooling_test)
     auto opt_program = [&]() {
         migraphx::program p;
         auto input = p.add_parameter("x", s);
-        auto rsp = p.add_instruction(migraphx::op::reshape{{4, -1}}, input);
-        auto rdm = p.add_instruction(migraphx::op::reduce_mean{{1}}, rsp);
-        auto ret = p.add_instruction(migraphx::op::reshape{{2, 2, 1, 1, 1}}, rdm);
+        auto rsp   = p.add_instruction(migraphx::op::reshape{{4, -1}}, input);
+        auto rdm   = p.add_instruction(migraphx::op::reduce_mean{{1}}, rsp);
+        auto ret   = p.add_instruction(migraphx::op::reshape{{2, 2, 1, 1, 1}}, rdm);
         p.add_return({ret});
         return p;
     };
@@ -73,7 +74,8 @@ TEST_CASE(literal_rewrite_avepooling_test)
     auto pooling_program = [&]() {
         migraphx::program p;
         auto input = p.add_literal(migraphx::literal(s, data));
-        auto ret = p.add_instruction(migraphx::op::pooling{"average", {0, 0, 0}, {1, 1, 1}, {3, 4, 5}}, input);
+        auto ret   = p.add_instruction(
+            migraphx::op::pooling{"average", {0, 0, 0}, {1, 1, 1}, {3, 4, 5}}, input);
         p.add_return({ret});
         return p;
     };
@@ -81,9 +83,9 @@ TEST_CASE(literal_rewrite_avepooling_test)
     auto opt_program = [&]() {
         migraphx::program p;
         auto input = p.add_literal(migraphx::literal(s, data));
-        auto rsp = p.add_instruction(migraphx::op::reshape{{4, -1}}, input);
-        auto rdm = p.add_instruction(migraphx::op::reduce_mean{{1}}, rsp);
-        auto ret = p.add_instruction(migraphx::op::reshape{{2, 2, 1, 1, 1}}, rdm);
+        auto rsp   = p.add_instruction(migraphx::op::reshape{{4, -1}}, input);
+        auto rdm   = p.add_instruction(migraphx::op::reduce_mean{{1}}, rsp);
+        auto ret   = p.add_instruction(migraphx::op::reshape{{2, 2, 1, 1, 1}}, rdm);
         p.add_return({ret});
         return p;
     };

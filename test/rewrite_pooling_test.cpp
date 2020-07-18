17617cb8060cd0865c28d99783c2303406826c65
Shucai Xiao <Shucai.Xiao@amd.com>
2020-07-17 21:15:05 -0500

    remove a redundant unit test

--- test/rewrite_pooling_test.cpp
+++ test/rewrite_pooling_test.cpp
@@ -149,39 +149,4 @@ TEST_CASE(literal_rewrite_pooling_test)
     test_rewrite_pooling("average", migraphx::op::reduce_mean{{1}});
 }
 
-TEST_CASE(literal_rewrite_avepooling_test)
-{
-    migraphx::shape s{migraphx::shape::float_type, {2, 2, 3, 4, 5}};
-    std::vector<float> data(s.elements());
-    std::iota(data.begin(), data.end(), 1.0f);
-
-    auto pooling_program = [&]() {
-        migraphx::program p;
-        auto input = p.add_literal(migraphx::literal(s, data));
-        auto ret   = p.add_instruction(
-            migraphx::op::pooling{"average", {0, 0, 0}, {1, 1, 1}, {3, 4, 5}}, input);
-        p.add_return({ret});
-        return p;
-    };
-
-    auto opt_program = [&]() {
-        migraphx::program p;
-        auto input = p.add_literal(migraphx::literal(s, data));
-        auto rsp   = p.add_instruction(migraphx::op::reshape{{4, -1}}, input);
-        auto rdm   = p.add_instruction(migraphx::op::reduce_mean{{1}}, rsp);
-        auto ret   = p.add_instruction(migraphx::op::reshape{{2, 2, 1, 1, 1}}, rdm);
-        p.add_return({ret});
-        return p;
-    };
-
-    migraphx::program p1 = pooling_program();
-    migraphx::program p2 = opt_program();
-    p1.compile(migraphx::cpu::target{});
-    p2.compile(migraphx::cpu::target{});
-
-    auto result1 = p1.eval({}).back();
-    auto result2 = p2.eval({}).back();
-    visit_all(result1, result2)([&](auto r1, auto r2) { EXPECT(migraphx::verify_range(r1, r2)); });
-}
-
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

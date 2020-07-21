544d2a976c489860ce4aad4a2f7c47e875d264de
Paul <pfultz2@yahoo.com>
2020-07-20 21:35:06 -0500

    Formatting

--- src/reduce_dims.cpp
+++ src/reduce_dims.cpp
@@ -6,33 +6,33 @@ inline namespace MIGRAPHX_INLINE_NS {
 bool reduce_dim(std::vector<shape>& shapes, std::size_t n)
 {
     std::vector<std::size_t> new_lens;
-    for(const auto& s:shapes)
+    for(const auto& s : shapes)
     {
-        if (n >= s.lens().size())
+        if(n >= s.lens().size())
             return false;
-        if ((n+1) >= s.lens().size())
+        if((n + 1) >= s.lens().size())
             return false;
         auto astride = s.strides()[n];
-        auto alen = s.lens()[n];
-        auto bstride = s.strides()[n+1];
-        auto blen = s.lens()[n+1];
+        auto alen    = s.lens()[n];
+        auto bstride = s.strides()[n + 1];
+        auto blen    = s.lens()[n + 1];
 
-        if (astride == bstride*blen) 
+        if(astride == bstride * blen)
         {
-            new_lens.push_back(alen*blen);
+            new_lens.push_back(alen * blen);
         }
     }
-    if (new_lens.size() != shapes.size())
+    if(new_lens.size() != shapes.size())
         return false;
     std::size_t i = 0;
-    for(auto& s:shapes)
+    for(auto& s : shapes)
     {
-        auto lens = s.lens();
+        auto lens    = s.lens();
         auto strides = s.strides();
-        lens.erase(lens.begin()+n);
-        strides.erase(strides.begin()+n);
+        lens.erase(lens.begin() + n);
+        strides.erase(strides.begin() + n);
         lens[n] = new_lens[i];
-        s = shape{s.type(), lens, strides};
+        s       = shape{s.type(), lens, strides};
         i++;
     }
     return true;
@@ -45,28 +45,30 @@ std::size_t reduce_dim_all(std::vector<shape>& shapes, std::size_t n)
         assert(n < shapes.size());
     }
 
-    return n+1;
+    return n + 1;
 }
 void reduce_dim_all(std::vector<shape>& shapes)
 {
     std::size_t n = 0;
-    while(n < shapes.front().lens().size()-1) 
+    while(n < shapes.front().lens().size() - 1)
         n = reduce_dim_all(shapes, n);
 }
 
 std::vector<std::size_t> base_lens(const std::vector<shape>& shapes)
 {
-    return std::accumulate(shapes.begin()+1, shapes.end(), shapes.front().lens(), [](auto&& lens, auto&& s) {
-        std::vector<std::size_t> result;
-        const auto* x = &s.lens();
-        const auto* y = &lens;
-        if (x->size() > y->size())
-            std::swap(x, y);
-        std::transform(x->begin(), x->end(), y->begin(), std::back_inserter(result), [&](auto a, auto b) {
-            return std::max(a, b);
+    return std::accumulate(
+        shapes.begin() + 1, shapes.end(), shapes.front().lens(), [](auto&& lens, auto&& s) {
+            std::vector<std::size_t> result;
+            const auto* x = &s.lens();
+            const auto* y = &lens;
+            if(x->size() > y->size())
+                std::swap(x, y);
+            std::transform(
+                x->begin(), x->end(), y->begin(), std::back_inserter(result), [&](auto a, auto b) {
+                    return std::max(a, b);
+                });
+            return result;
         });
-        return result;
-    });
 }
 
 shape mask_shape(const shape& s, const std::vector<std::size_t>& lens)
@@ -74,14 +76,14 @@ shape mask_shape(const shape& s, const std::vector<std::size_t>& lens)
     assert(s.lens().size() == lens.size());
     std::vector<std::size_t> rstrides(lens.size());
     std::size_t stride = 1;
-    for(std::size_t i = lens.size() - 1;i < lens.size();i--)
+    for(std::size_t i = lens.size() - 1; i < lens.size(); i--)
     {
-        if (lens[i] == s.lens()[i])
+        if(lens[i] == s.lens()[i])
         {
             rstrides[i] = stride;
             stride *= lens[i];
         }
-        else if (lens[i] != 1 and s.lens()[i] != 1)
+        else if(lens[i] != 1 and s.lens()[i] != 1)
         {
             return shape{};
         }
@@ -91,26 +93,25 @@ shape mask_shape(const shape& s, const std::vector<std::size_t>& lens)
 
 std::vector<shape> reduce_dims(const std::vector<shape>& shapes)
 {
-    if (shapes.empty())
+    if(shapes.empty())
         return {};
     auto result = shapes;
-    auto base = base_lens(shapes);
-    for(auto&& s:shapes)
+    auto base   = base_lens(shapes);
+    for(auto&& s : shapes)
     {
-        if (s.lens().size() != base.size())
+        if(s.lens().size() != base.size())
             return shapes;
-        if (s.lens() == base)
+        if(s.lens() == base)
             continue;
         auto mshape = mask_shape(s, base);
-        if (mshape.lens().size() != base.size())
+        if(mshape.lens().size() != base.size())
             return shapes;
         result.push_back(mshape);
     }
     reduce_dim_all(result);
-    result.erase(result.begin()+shapes.size(), result.end());
+    result.erase(result.begin() + shapes.size(), result.end());
     return result;
 }
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx
-
--- test/reduce_dims.cpp
+++ test/reduce_dims.cpp
@@ -14,84 +14,85 @@ migraphx::shape make_shape(std::vector<std::size_t> lens, std::vector<std::size_
 
 TEST_CASE(same_standard)
 {
-    auto is = make_shape({64, 3, 7, 7});
-    auto os = make_shape({64*3*7*7});
+    auto is                              = make_shape({64, 3, 7, 7});
+    auto os                              = make_shape({64 * 3 * 7 * 7});
     std::vector<migraphx::shape> ishapes = {is, is, is};
     std::vector<migraphx::shape> eshapes = {os, os, os};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(same_broadcast1)
 {
-    auto is = make_shape({64, 3, 7, 7});
-    auto os = make_shape({64, 3, 7*7});
+    auto is                              = make_shape({64, 3, 7, 7});
+    auto os                              = make_shape({64, 3, 7 * 7});
     std::vector<migraphx::shape> ishapes = {is, make_shape({64, 3, 7, 7}, {0, 1, 0, 0}), is};
-    std::vector<migraphx::shape> eshapes = {os, make_shape({64, 3, 7*7}, {0, 1, 0}), os};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    std::vector<migraphx::shape> eshapes = {os, make_shape({64, 3, 7 * 7}, {0, 1, 0}), os};
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(same_broadcast2)
 {
-    auto is = make_shape({64, 3, 8, 7, 7});
-    auto os = make_shape({64, 8*3, 7*7});
+    auto is                              = make_shape({64, 3, 8, 7, 7});
+    auto os                              = make_shape({64, 8 * 3, 7 * 7});
     std::vector<migraphx::shape> ishapes = {is, make_shape({64, 3, 8, 7, 7}, {0, 8, 1, 0, 0}), is};
-    std::vector<migraphx::shape> eshapes = {os, make_shape({64, 8*3, 7*7}, {0, 1, 0}), os};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    std::vector<migraphx::shape> eshapes = {os, make_shape({64, 8 * 3, 7 * 7}, {0, 1, 0}), os};
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(same_transposed)
 {
-    auto is = make_shape({64, 3, 7, 7});
-    auto os = make_shape({64*3, 7, 7});
-    std::vector<migraphx::shape> ishapes = {is, migraphx::reorder_shape(is, {0,1,3,2}), is};
-    std::vector<migraphx::shape> eshapes = {os, migraphx::reorder_shape(os, {0,2,1}), os};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto is                              = make_shape({64, 3, 7, 7});
+    auto os                              = make_shape({64 * 3, 7, 7});
+    std::vector<migraphx::shape> ishapes = {is, migraphx::reorder_shape(is, {0, 1, 3, 2}), is};
+    std::vector<migraphx::shape> eshapes = {os, migraphx::reorder_shape(os, {0, 2, 1}), os};
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(different_masked1)
 {
-    auto is = make_shape({64, 3, 7, 7});
-    auto os = make_shape({64, 3, 7*7});
+    auto is                              = make_shape({64, 3, 7, 7});
+    auto os                              = make_shape({64, 3, 7 * 7});
     std::vector<migraphx::shape> ishapes = {is, make_shape({1, 3, 1, 1}), is};
     std::vector<migraphx::shape> eshapes = {os, make_shape({1, 3, 1}), os};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(different_masked2)
 {
-    auto is = make_shape({64, 3, 7, 7});
-    auto os = make_shape({64, 3, 7*7});
-    std::vector<migraphx::shape> ishapes = {is, make_shape({1, 3, 1, 1}), make_shape({64, 1, 7, 7})};
-    std::vector<migraphx::shape> eshapes = {os, make_shape({1, 3, 1}), make_shape({64, 1, 7* 7})};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto is                              = make_shape({64, 3, 7, 7});
+    auto os                              = make_shape({64, 3, 7 * 7});
+    std::vector<migraphx::shape> ishapes = {
+        is, make_shape({1, 3, 1, 1}), make_shape({64, 1, 7, 7})};
+    std::vector<migraphx::shape> eshapes = {os, make_shape({1, 3, 1}), make_shape({64, 1, 7 * 7})};
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(eshapes == rshapes);
 }
 
 TEST_CASE(different_incompatible)
 {
-    auto is = make_shape({64, 3, 7, 7});
+    auto is                              = make_shape({64, 3, 7, 7});
     std::vector<migraphx::shape> ishapes = {is, make_shape({1, 3, 2, 1}), is};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(ishapes == rshapes);
 }
 
 TEST_CASE(different_ranks)
 {
-    auto is = make_shape({64, 3, 7, 7});
+    auto is                              = make_shape({64, 3, 7, 7});
     std::vector<migraphx::shape> ishapes = {is, make_shape({1, 3}), is};
-    auto rshapes = migraphx::reduce_dims(ishapes);
+    auto rshapes                         = migraphx::reduce_dims(ishapes);
 
     EXPECT(ishapes == rshapes);
 }

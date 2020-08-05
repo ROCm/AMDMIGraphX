ac6df5432480095e534634286c538919fb584bd7
Paul <pfultz2@yahoo.com>
2020-08-05 17:51:44 -0500

    Formatting

--- src/register_op.cpp
+++ src/register_op.cpp
@@ -4,16 +4,13 @@
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
-std::unordered_map<std::string, operation>& op_map() {
+std::unordered_map<std::string, operation>& op_map()
+{
     static std::unordered_map<std::string, operation> m;
     return m;
 }
-void register_op(const operation& op) {
-    op_map()[op.name()] = op;
-}
-operation load_op(const std::string& name) {
-    return op_map().at(name);
-}
+void register_op(const operation& op) { op_map()[op.name()] = op; }
+operation load_op(const std::string& name) { return op_map().at(name); }
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

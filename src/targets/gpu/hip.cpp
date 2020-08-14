e4f56115ea908a2f59df7fe64a841db72736285e
Paul <pfultz2@yahoo.com>
2020-08-14 15:16:04 -0500

    Use correct op name

--- src/targets/gpu/hip.cpp
+++ src/targets/gpu/hip.cpp
@@ -14,7 +14,7 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
 MIGRAPHX_REGISTER_OP(hip_allocate)
-MIGRAPHX_REGISTER_OP(hip_sync)
+MIGRAPHX_REGISTER_OP(hip_sync_device)
 MIGRAPHX_REGISTER_OP(hip_copy_to_gpu)
 MIGRAPHX_REGISTER_OP(hip_copy_from_gpu)
 MIGRAPHX_REGISTER_OP(hip_copy)

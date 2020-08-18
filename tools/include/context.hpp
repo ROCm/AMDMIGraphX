0697c7c89509814322a43e3ed7751e0d25c95c18
Shucai Xiao <Shucai.Xiao@amd.com>
2020-08-18 15:28:37 -0500

    remove the from/to_value method for the generate context struct

--- tools/include/context.hpp
+++ tools/include/context.hpp
@@ -38,17 +38,20 @@ void from_value_context(T& x, const value& v)
     return migraphx::from_value(v, x);
 }
 
+
 <%
  interface('context',
-           virtual('finish', returns = 'void', const = True),
-           virtual('to_value', returns = 'value', const = True, default = 'to_value_context'),
-           virtual('from_value', v = 'const value&', default = 'from_value_context')) %>
+           virtual('finish', returns = 'void', const = True))
+%>
 
-    inline void migraphx_to_value(value& v, const context& ctx)
-{
-    v = ctx.to_value();
-}
-inline void migraphx_from_value(const value& v, context& ctx) { ctx.from_value(v); }
+// virtual('to_value', returns = 'value', const = True, default = 'to_value_context'),
+// virtual('from_value', v = 'const value&', default = 'from_value_context'))
+
+// inline void migraphx_to_value(value& v, const context& ctx)
+// {
+//     v = ctx.to_value();
+// }
+// inline void migraphx_from_value(const value& v, context& ctx) { ctx.from_value(v); }
 
 #endif
 

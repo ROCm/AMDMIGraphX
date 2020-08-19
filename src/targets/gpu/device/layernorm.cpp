eabb1a7add895650d9c4694668b9cd0bfd904b4c
Paul <pfultz2@yahoo.com>
2020-08-19 13:11:48 -0500

    Formatting

--- src/targets/gpu/device/layernorm.cpp
+++ src/targets/gpu/device/layernorm.cpp
@@ -35,14 +35,11 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
             value_type x_data[4];
             auto with_x = [&](auto f) {
                 int offset = 0;
-                return [=, &x_data](auto j) mutable {
-                    return f(x_data[offset++], j);
-                };
+                return [=, &x_data](auto j) mutable { return f(x_data[offset++], j); };
             };
 
-            idx.local_stride(relements, with_x([&](auto& x, auto j) {
-                x = input.data()[base_idx + j];
-            }));
+            idx.local_stride(relements,
+                             with_x([&](auto& x, auto j) { x = input.data()[base_idx + j]; }));
 
             auto m = block_reduce<max_block_size>(
                          idx, sum{}, 0, relements, with_x([](auto& x, auto) { return x; })) /
@@ -51,13 +48,12 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
             idx.local_stride(relements, with_x([&](auto& x, auto) { x = x - m; }));
 
             auto r = block_reduce<max_block_size>(
-                         idx, sum{}, 0, relements, with_x([&](auto& x, auto) { return x*x; })) /
+                         idx, sum{}, 0, relements, with_x([&](auto& x, auto) { return x * x; })) /
                      relements;
 
             const auto eps = ::rsqrt(r + 1e-12);
-            idx.local_stride(relements, with_x([&](auto& x, auto j) {
-                output.data()[base_idx + j] = x * eps;
-            }));
+            idx.local_stride(
+                relements, with_x([&](auto& x, auto j) { output.data()[base_idx + j] = x * eps; }));
 
         });
 

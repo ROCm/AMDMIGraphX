7aa6983d8d13492e1c39ba540dca2caef32915d5
Paul <pfultz2@yahoo.com>
2020-08-20 16:34:20 -0500

    Formatting

--- src/targets/gpu/device/layernorm.cpp
+++ src/targets/gpu/device/layernorm.cpp
@@ -41,13 +41,15 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
         const std::size_t block_size_div = encode_divisor(block_size);
 
         gs_launch(stream, nelements * block_size, block_size)([=](auto i, auto idx) __device__ {
-            const auto out_idx  = fast_div(i, block_size_div);
-            const auto base_idx = out_idx * relements;
-            const auto input_idx = base_idx+idx.local;
-            const bool in_range = idx.local < relements;
+            const auto out_idx   = fast_div(i, block_size_div);
+            const auto base_idx  = out_idx * relements;
+            const auto input_idx = base_idx + idx.local;
+            const bool in_range  = idx.local < relements;
 
             auto mean = [&](auto z) {
-                return block_reduce<max_block_size>(idx, sum{}, value_type(0), relements, [=](auto) { return z; }) / relements;
+                return block_reduce<max_block_size>(
+                           idx, sum{}, value_type(0), relements, [=](auto) { return z; }) /
+                       relements;
             };
 
             // m = x - mean(x)
@@ -55,10 +57,10 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
             value_type m = x - mean(x);
 
             // mean(m ^ 2) + 1e-12
-            value_type r = mean(m*m) + 1e-12;
+            value_type r = mean(m * m) + 1e-12;
 
             // m * rsqrt(mean(m ^ 2) + 1e-12)
-            if (in_range)
+            if(in_range)
                 output.data()[input_idx] = m * ::rsqrt(r);
         });
 
@@ -108,7 +110,7 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
         const std::size_t max_block_size = 256;
         const std::size_t block_size     = compute_block_size(relements_v, max_block_size);
         const std::size_t block_size_div = encode_divisor(block_size);
-        const auto ielements_v = result.get_shape().elements() / vec_size;
+        const auto ielements_v           = result.get_shape().elements() / vec_size;
         assert(relements_v < block_size);
 
         std::cout << "block_size: " << block_size << std::endl;
@@ -117,15 +119,17 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
         std::cout << "nelements: " << nelements << std::endl;
         std::cout << "nelements_v: " << nelements_v << std::endl;
 
-        for(int global=0;global<nelements_v;global++) {
+        for(int global = 0; global < nelements_v; global++)
+        {
             std::cout << "global: " << global << std::endl;
-            for(int local=0;local<block_size;local++) {
-                int i = global*block_size+local;
-                const auto out_idx  = fast_div(i, block_size_div);
-                const auto base_idx = out_idx * relements_v;
-                const auto input_idx = base_idx+local;
-                const bool in_range = input_idx < ielements_v;
-                if (local >= relements_v)
+            for(int local = 0; local < block_size; local++)
+            {
+                int i                = global * block_size + local;
+                const auto out_idx   = fast_div(i, block_size_div);
+                const auto base_idx  = out_idx * relements_v;
+                const auto input_idx = base_idx + local;
+                const bool in_range  = input_idx < ielements_v;
+                if(local >= relements_v)
                     continue;
                 std::cout << "input_idx: " << input_idx << " {";
                 std::cout << "i=" << i << ", ";
@@ -136,13 +140,15 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
         }
 
         gs_launch(stream, nelements_v * block_size, block_size)([=](auto i, auto idx) __device__ {
-            const auto out_idx  = fast_div(i, block_size_div);
-            const auto base_idx = out_idx * relements_v;
-            const auto input_idx = base_idx+idx.local;
-            const bool in_range = idx.local < relements_v;
+            const auto out_idx   = fast_div(i, block_size_div);
+            const auto base_idx  = out_idx * relements_v;
+            const auto input_idx = base_idx + idx.local;
+            const bool in_range  = idx.local < relements_v;
 
             auto mean = [&](auto z) -> value_type {
-                return block_reduce<max_block_size>(idx, sum{}, value_type(0), relements_v, [=](auto) { return z; }) / value_type(relements);
+                return block_reduce<max_block_size>(
+                           idx, sum{}, value_type(0), relements_v, [=](auto) { return z; }) /
+                       value_type(relements);
             };
 
             // m = x - mean(x)
@@ -150,14 +156,14 @@ void layernorm(hipStream_t stream, const argument& result, const argument& arg1)
             value_type m = x - mean(x);
 
             // mean(m ^ 2) + 1e-12
-            value_type r = mean(m*m) + value_type(1e-12);
+            value_type r = mean(m * m) + value_type(1e-12);
 
             // rsqrt(mean(m ^ 2) + 1e-12)
             value_type d;
             for(index_int k = 0; k < vec_size; k++)
                 d[k] = ::rsqrt(r[k]);
             // m * rsqrt(mean(m ^ 2) + 1e-12)
-            if (in_range)
+            if(in_range)
                 output.data()[input_idx] = m * d;
         });
 

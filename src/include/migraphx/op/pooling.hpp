7575da3790f93a76f6b9a4bd89019a9a228caf97
Shucai Xiao <Shucai.Xiao@amd.com>
2020-08-21 16:18:57 -0500

    fix cppcheck error

--- src/include/migraphx/op/pooling.hpp
+++ src/include/migraphx/op/pooling.hpp
@@ -23,7 +23,7 @@ struct pooling
     std::vector<std::size_t> padding = {0, 0};
     std::vector<std::size_t> stride  = {1, 1};
     std::vector<std::size_t> lengths = {1, 1};
-    bool ceil_mode                   = 0;
+    bool ceil_mode                   = false;
     padding_mode_t padding_mode      = default_;
 
     template <class Self, class F>

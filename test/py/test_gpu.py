ae53c69587463f44d938cef4fb963ec889c97653
Shucai Xiao <shucai.xiao@amd.com>
2020-08-28 11:09:37 -0500

    Onnx datatype parsing (#618)
    * add bool type
    
    * code backup
    
    * code backup
    
    * clang format
    
    * fix build warnings
    
    * clang format
    
    * add the equal operator
    
    * add the equal operator
    
    * clang format
    
    * remove unnecessary code
    
    * refine unit tests
    
    * clang format
    
    * fix review comments and a bug
    
    * clang format
    
    * additional changes
    
    * clang format
    
    * fix cppcheck error
    
    * add bool type in c api
    
    * fix cppcheck error
    
    * fix review comments
    
    * fix cppcheck error
    
    * fix a build error related to gcc
    
    * fix cppcheck error
    
    * fix cppcheck error
    
    * added the equal operator to register list
    
    * add parsing boolean type
    
    * clang format
    
    * fix bool type issue for python output
    
    * clang format
    
    * add support for automatic multibroadcast of the equal operator
    
    * additional unit tests for more code coverage
    
    * clang format
    
    * missing an onnx file
    
    * code cleanup
    
    * clang format
    
    * fix issue of int64 data type in python api
    
    * clang format
    
    * include more onnx backend unit tests
    
    * add more unit tests to cover code changes
    
    * clang format
    
    * disable python tests for python2.7
    
    * code backup for python half type support
    
    * clang format
    
    * code change to support half data type in python
    
    * add a unit test for python half type
    
    * clang format
    
    * add more unit tests for more code coverage
    
    * clang format
    
    * add more unit tests for more code coverage
    
    * clang format
    
    * refine unit tests to have more code coverage
    
    * clang format
    
    * update python unit test
    
    * revert to a previous version of test_gpu.py

--- test/py/test_gpu.py
+++ test/py/test_gpu.py
@@ -1,15 +1,81 @@
+import sys
+if sys.version_info < (3, 0):
+    sys.exit()
+
 import migraphx
+import numpy as np
 
-p = migraphx.parse_onnx("conv_relu_maxpool_test.onnx")
-print(p)
-print("Compiling ...")
-p.compile(migraphx.get_target("gpu"))
-print(p)
-params = {}
 
-for key, value in p.get_parameter_shapes().items():
-    print("Parameter {} -> {}".format(key, value))
-    params[key] = migraphx.generate_argument(value)
+def test_conv_relu():
+    p = migraphx.parse_onnx("conv_relu_maxpool_test.onnx")
+    print(p)
+    print("Compiling ...")
+    p.compile(migraphx.get_target("gpu"))
+    print(p)
+    params = {}
 
-r = p.run(params)
-print(r)
+    for key, value in p.get_parameter_shapes().items():
+        print("Parameter {} -> {}".format(key, value))
+        params[key] = migraphx.generate_argument(value)
+
+    r = p.run(params)
+    print(r)
+
+
+def test_sub_uint64():
+    p = migraphx.parse_onnx("implicit_sub_bcast_test.onnx")
+    print(p)
+    print("Compiling ...")
+    p.compile(migraphx.get_target("gpu"))
+    print(p)
+    params = {}
+
+    shapes = p.get_parameter_shapes()
+    arg0 = np.arange(120).reshape(shapes["0"].lens()).astype(np.uint64)
+    arg1 = np.arange(20).reshape(shapes["1"].lens()).astype(np.uint64)
+
+    params["0"] = migraphx.argument(arg0)
+    params["1"] = migraphx.argument(arg1)
+    r = p.run(params)
+    print(r)
+
+
+def test_neg_int64():
+    p = migraphx.parse_onnx("neg_test.onnx")
+    print(p)
+    print("Compiling ...")
+    p.compile(migraphx.get_target("gpu"))
+    print(p)
+    params = {}
+
+    shapes = p.get_parameter_shapes()
+    arg0 = np.arange(6).reshape(shapes["0"].lens()).astype(np.int64)
+
+    params["0"] = migraphx.argument(arg0)
+    r = p.run(params)
+    print(r)
+
+
+def test_fp16_imagescaler():
+    p = migraphx.parse_onnx("imagescaler_half_test.onnx")
+    print(p)
+    s1 = p.get_output_shapes()[-1]
+    print("Compiling ...")
+    p.compile(migraphx.get_target("gpu"))
+    print(p)
+    s2 = p.get_output_shapes()[-1]
+    assert s1 == s2
+
+    params = {}
+    shapes = p.get_parameter_shapes()
+    arg0 = np.random.randn(768).reshape(shapes["0"].lens()).astype(np.float16)
+    params["0"] = migraphx.argument(arg0)
+
+    r = p.run(params)[-1]
+    print(r)
+
+
+test_conv_relu()
+test_sub_uint64()
+test_neg_int64()
+test_fp16_imagescaler()

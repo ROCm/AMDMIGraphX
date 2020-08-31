e524cc7f2db7fb750acff1e66617f1a88e7ee6bf
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2020-08-31 15:39:30 -0500

    Instance norm kdims (#620)
    * fix parsing to kdims
    
    * add 5d size
    
    * fix assert
    
    * add 3d test
    
    * formatting
    
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- test/onnx/verify_onnx.cpp
+++ test/onnx/verify_onnx.cpp
@@ -39,6 +39,35 @@ TEST_CASE(instance_norm_test)
     EXPECT(migraphx::verify_range(result_vector, gold));
 }
 
+TEST_CASE(instance_norm_3d_test)
+{
+    migraphx::program p = migraphx::parse_onnx("instance_norm_val_3d_test.onnx");
+
+    p.compile(migraphx::cpu::target{});
+    auto result = p.eval({}).back();
+    std::vector<float> result_vector(16);
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {-1.52752,
+                               -1.09109,
+                               -0.654653,
+                               -0.218218,
+                               0.218218,
+                               0.654653,
+                               1.09109,
+                               1.52752,
+                               -2.05505,
+                               -1.18218,
+                               -0.309306,
+                               0.563565,
+                               1.43644,
+                               2.30931,
+                               3.18218,
+                               4.05505};
+
+    EXPECT(migraphx::verify_range(result_vector, gold));
+}
+
 TEST_CASE(averagepool_notset_test)
 {
     auto p = migraphx::parse_onnx("averagepool_notset_test.onnx");

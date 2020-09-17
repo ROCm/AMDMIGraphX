99ece7ba27d4956385cc2ea0c6d4ca9705ca42dd
Shucai Xiao <Shucai.Xiao@amd.com>
2020-09-17 09:22:09 -0500

    refine script

--- tools/download_models.sh
+++ tools/download_models.sh
@@ -9,8 +9,14 @@ model_dir=$ONNX_HOME/.onnx/models
 tmp_dir=$ONNX_HOME/tmp/
 mkdir -p $model_dir
 mkdir -p $tmp_dir
-for name in bvlc_alexnet densenet121 inception_v1 inception_v2 \
-        resnet50 shufflenet squeezenet vgg19 zfnet512 
+models="bvlc_alexnet \
+        densenet121 \
+        inception_v2 \
+        shufflenet \
+        vgg19 \
+        zfnet512"
+
+for name in $models
 do
 curl https://s3.amazonaws.com/download.onnx/models/opset_9/$name.tar.gz --output $tmp_dir/$name.tar.gz
 tar -xzvf $tmp_dir/$name.tar.gz --directory $model_dir && rm $tmp_dir/$name.tar.gz

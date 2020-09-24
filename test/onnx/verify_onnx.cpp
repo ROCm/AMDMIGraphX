73f05cc0743864e44f6285bc11c80c6c281f0547
Shucai Xiao <Shucai.Xiao@amd.com>
2020-09-23 22:00:53 -0500

    add unit tests for upsample

--- test/onnx/verify_onnx.cpp
+++ test/onnx/verify_onnx.cpp
@@ -127,6 +127,24 @@ TEST_CASE(gather_elements)
     EXPECT(migraphx::verify_range(result_vector, gold));
 }
 
+TEST_CASE(upsample_test)
+{
+    migraphx::program p = migraphx::parse_onnx("upsample_test.onnx");
+
+    std::vector<float> x_data = {1, 2, 3, 4};
+    migraphx::shape sx{migraphx::shape::float_type, {1, 1, 2, 2}};
+
+    migraphx::program::parameter_map pp;
+    pp["X"] = migraphx::argument(sx, x_data.data());
+    
+    auto result = p.eval(pp).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 3, 3, 3, 4, 4, 4};
+    EXPECT(migraphx::verify_range(result_vector, gold));
+}
+
 TEST_CASE(where_test)
 {
     migraphx::program p = migraphx::parse_onnx("where_test.onnx");

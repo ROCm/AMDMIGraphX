4f128cd29565baec61275e209907219c913cd481
Shucai Xiao <Shucai.Xiao@amd.com>
2020-09-24 16:10:48 -0500

    clang format

--- src/api/json_tokenize.cpp
+++ src/api/json_tokenize.cpp
@@ -13,32 +13,28 @@ inline namespace MIGRAPHX_INLINE_NS {
 using token = std::pair<const char*, const char*>;
 using lexer = std::function<const char*(const char* start, const char* end)>;
 
-template<class P>
+template <class P>
 auto lex_while(P p)
 {
     return [=](const char* start, const char* end) {
-        return std::find_if(start, end, [&](char c) {
-            return not p(c);
-        });
+        return std::find_if(start, end, [&](char c) { return not p(c); });
     };
 }
 
-template<class P>
+template <class P>
 auto lex_while1(P p)
 {
     return [=](const char* start, const char* end) {
-        return std::find_if(start, end, [&](char c) {
-            return p(c);
-        });
+        return std::find_if(start, end, [&](char c) { return p(c); });
     };
 }
 
-template<class P>
+template <class P>
 auto lex_if(P p)
 {
     return [=](const char* start, const char*) {
-        if (p(*start))
-            return start+1;
+        if(p(*start))
+            return start + 1;
         return start;
     };
 }
@@ -46,22 +42,22 @@ auto lex_if(P p)
 std::vector<token> tokenize(const char* start, const char* end, std::vector<lexer> lexers)
 {
     std::vector<token> result;
-    while(start != end) 
+    while(start != end)
     {
         bool error = true;
-        for(auto l:lexers) 
+        for(auto l : lexers)
         {
             auto next = l(start, end);
-            if (next != start)
+            if(next != start)
             {
-                if (not std::all_of(start, next, &isspace))
+                if(not std::all_of(start, next, &isspace))
                     result.emplace_back(start, next);
                 start = next;
                 error = false;
                 break;
             }
         }
-        if (error)
+        if(error)
         {
             std::abort();
         }
@@ -75,12 +71,12 @@ std::vector<token> json_tokenize(const std::string& s)
 
     // Quote
     lexers.push_back([](const char* start, const char* end) {
-        if (*start != '\"')
+        if(*start != '\"')
             return start;
         ++start;
         while(start != end and *start != '\"')
         {
-            if (*start == '\\')
+            if(*start == '\\')
                 start++;
             start++;
         }
@@ -92,11 +88,13 @@ std::vector<token> json_tokenize(const std::string& s)
 
     // Punctation
     lexers.push_back(lex_if(&ispunct));
-    
-    // Identifier/number
-    lexers.push_back(lex_while([](char c) { return (isalnum(c) or contains({'_', '.', '+'}, c)); }));
 
-    return tokenize(s.data(), s.data()+s.length(), lexers);
+    // Identifier/number
+    lexers.push_back(lex_while([](char c) {
+        return (isalnum(c) or contains({'_', '.', '+'}, c));
+    }));
+
+    return tokenize(s.data(), s.data() + s.length(), lexers);
 }
 
 std::string convert_to_json(const std::string& str)
@@ -104,10 +102,10 @@ std::string convert_to_json(const std::string& str)
     auto tokens = json_tokenize(str);
     std::stringstream ss;
 
-    for (auto& token : tokens)
+    for(auto& token : tokens)
     {
         std::string s(token.first, token.second);
-        if (std::isalpha(s.front()) and not contains({"null", "nan"}, s))
+        if(std::isalpha(s.front()) and not contains({"null", "nan"}, s))
         {
             ss << "\"" << s << "\"";
         }

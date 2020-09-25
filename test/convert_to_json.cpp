a0a9cb2ef42be7967d3005133736ad1d08297cb0
Shucai Xiao <Shucai.Xiao@amd.com>
2020-09-25 14:28:38 -0500

    clang format

--- test/convert_to_json.cpp
+++ test/convert_to_json.cpp
@@ -4,57 +4,56 @@
 TEST_CASE(key_num)
 {
     std::string str = "{abc:{key:1}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\":{\"key\":1}}");
 }
 
 TEST_CASE(key_null)
 {
     std::string str = "{abc:{key:null}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\":{\"key\":null}}");
 }
 
 TEST_CASE(key_nan)
 {
     std::string str = "{abc:{key:nan}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\":{\"key\":nan}}");
 }
 
 TEST_CASE(quote_key_num)
 {
     std::string str = "{\"abc\":{\"key\":1}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\":{\"key\":1}}");
 }
 
 TEST_CASE(quote_with_space_key_num)
 {
     std::string str = "{\"abc key\":{\"key\":1}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc key\":{\"key\":1}}");
 }
-
 
 TEST_CASE(key_value_num_space)
 {
     std::string str = "{abc    :    {    key    :    1}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\"    :    {    \"key\"    :    1}}");
 }
 
 TEST_CASE(key_value_str)
 {
     std::string str = "{abc : {key : value}}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\" : {\"key\" : \"value\"}}");
 }
 
 TEST_CASE(key_space_value)
 {
     std::string str = "{abc    : [key, value]}";
-    auto jstr = migraphx::convert_to_json(str);
+    auto jstr       = migraphx::convert_to_json(str);
     EXPECT(jstr == "{\"abc\"    : [\"key\", \"value\"]}");
 }
 

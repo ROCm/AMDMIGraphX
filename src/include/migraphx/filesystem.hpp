bb73146a6dda641b800eda9f10ecf858c94cc045
Paul <pfultz2@yahoo.com>
2020-10-07 23:36:06 -0500

    Formatting

--- src/include/migraphx/filesystem.hpp
+++ src/include/migraphx/filesystem.hpp
@@ -4,27 +4,27 @@
 #include <migraphx/config.hpp>
 
 #if defined(__has_include)
-#   if __has_include(<filesystem>) && __cplusplus >= 201703L
-#       define MIGRAPHX_HAS_FILESYSTEM 1
-#   else
-#       define MIGRAPHX_HAS_FILESYSTEM 0
-#   endif
-#   if __has_include(<experimental/filesystem>) && __cplusplus >= 201103L
-#       define MIGRAPHX_HAS_FILESYSTEM_TS 1
-#   else
-#       define MIGRAPHX_HAS_FILESYSTEM_TS 0
-#   endif
+#if __has_include(<filesystem>) && __cplusplus >= 201703L
+#define MIGRAPHX_HAS_FILESYSTEM 1
 #else
-#   define MIGRAPHX_HAS_FILESYSTEM 0
-#   define MIGRAPHX_HAS_FILESYSTEM_TS 0
+#define MIGRAPHX_HAS_FILESYSTEM 0
+#endif
+#if __has_include(<experimental/filesystem>) && __cplusplus >= 201103L
+#define MIGRAPHX_HAS_FILESYSTEM_TS 1
+#else
+#define MIGRAPHX_HAS_FILESYSTEM_TS 0
+#endif
+#else
+#define MIGRAPHX_HAS_FILESYSTEM 0
+#define MIGRAPHX_HAS_FILESYSTEM_TS 0
 #endif
 
 #if MIGRAPHX_HAS_FILESYSTEM
-#   include <filesystem>
+#include <filesystem>
 #elif MIGRAPHX_HAS_FILESYSTEM_TS
-#   include <experimental/filesystem>
+#include <experimental/filesystem>
 #else
-#   error "No filesystem include available"
+#error "No filesystem include available"
 #endif
 
 namespace migraphx {
@@ -33,9 +33,8 @@ inline namespace MIGRAPHX_INLINE_NS {
 #if MIGRAPHX_HAS_FILESYSTEM
 namespace fs = ::std::filesystem;
 #elif MIGRAPHX_HAS_FILESYSTEM_TS
-namespace fs = ::std::experimental::filesystem;    
+namespace fs = ::std::experimental::filesystem;
 #endif
-
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx

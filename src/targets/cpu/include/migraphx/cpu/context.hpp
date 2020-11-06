842a1077524c5c88164703137b8d8f36617b79f8
Paul <pfultz2@yahoo.com>
2020-11-06 12:24:37 -0600

    Add onednn

--- src/targets/cpu/include/migraphx/cpu/context.hpp
+++ src/targets/cpu/include/migraphx/cpu/context.hpp
@@ -2,15 +2,29 @@
 #define MIGRAPHX_GUARD_RTGLIB_CONTEXT_HPP
 
 #include <migraphx/config.hpp>
+#include <migraphx/cpu/dnnl.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace cpu {
 
+#ifdef USE_DNNL
+struct context
+{
+    dnnl::engine engine;
+    dnnl::stream stream;
+
+    context() 
+    : engine(dnnl::engine::kind::cpu, 0), stream(engine)
+    {}
+    void finish() const {}
+};
+#else
 struct context
 {
     void finish() const {}
 };
+#endif
 
 } // namespace cpu
 } // namespace MIGRAPHX_INLINE_NS

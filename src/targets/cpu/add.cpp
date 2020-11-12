4895b3476e144b824526c2d59a0d535780e73c12
Paul <pfultz2@yahoo.com>
2020-11-12 16:26:02 -0600

    Formatting

--- src/targets/cpu/add.cpp
+++ src/targets/cpu/add.cpp
@@ -11,10 +11,12 @@ template struct cpu_binary<op::add>;
 #if USE_DNNL
 struct dnnl_add : dnnl_op<dnnl_add, dnnl::binary, op::add>
 {
-    dnnl::binary::desc
-    get_desc(const std::unordered_map<int, dnnl::memory::desc>& m) const
+    dnnl::binary::desc get_desc(const std::unordered_map<int, dnnl::memory::desc>& m) const
     {
-        return dnnl::binary::desc(dnnl::algorithm::binary_add, m.at(DNNL_ARG_SRC_0), m.at(DNNL_ARG_SRC_1), m.at(DNNL_ARG_DST));
+        return dnnl::binary::desc(dnnl::algorithm::binary_add,
+                                  m.at(DNNL_ARG_SRC_0),
+                                  m.at(DNNL_ARG_SRC_1),
+                                  m.at(DNNL_ARG_DST));
     }
 };
 #endif

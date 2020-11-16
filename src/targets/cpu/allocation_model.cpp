3a4324d4d3e2f8ee0fdb4605fcf7b8a64a2859f5
Paul <pfultz2@yahoo.com>
2020-11-16 12:37:38 -0600

    Formatting

--- src/targets/cpu/allocation_model.cpp
+++ src/targets/cpu/allocation_model.cpp
@@ -5,13 +5,13 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace cpu {
 
-    std::string cpu_allocation_model::name() const { return "cpu::allocate"; }
-    operation cpu_allocation_model::allocate(const shape& s) const
-    {
-        return make_op(name(), {{"shape", to_value(s)}});
-    }
+std::string cpu_allocation_model::name() const { return "cpu::allocate"; }
+operation cpu_allocation_model::allocate(const shape& s) const
+{
+    return make_op(name(), {{"shape", to_value(s)}});
+}
 
-    std::string cpu_allocation_model::copy() const { return "cpu::copy"; }
+std::string cpu_allocation_model::copy() const { return "cpu::copy"; }
 
 } // namespace cpu
 } // namespace MIGRAPHX_INLINE_NS

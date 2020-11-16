ced8936aa2b4e7f10e2e7615995c1e10d73e9adb
Paul <pfultz2@yahoo.com>
2020-11-16 12:26:23 -0600

    Formatting

--- src/targets/gpu/allocation_model.cpp
+++ src/targets/gpu/allocation_model.cpp
@@ -5,11 +5,11 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
-    std::string gpu_allocation_model::name() const { return "hip::allocate"; }
-    operation gpu_allocation_model::allocate(const shape& s) const
-    {
-        return make_op(name(), {{"shape", to_value(s)}});
-    }
+std::string gpu_allocation_model::name() const { return "hip::allocate"; }
+operation gpu_allocation_model::allocate(const shape& s) const
+{
+    return make_op(name(), {{"shape", to_value(s)}});
+}
 
 } // namespace gpu
 } // namespace MIGRAPHX_INLINE_NS

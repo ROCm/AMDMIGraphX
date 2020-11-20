1bfb147d508e057c4893d5865cfa8ff210ab3c58
Paul Fultz II <pfultz2@yahoo.com>
2020-11-20 16:31:42 -0600

    Fuse skip layernorm (#683)
    * Unify the vectorized and non-vectorized path
    
    * Formatting
    
    * Make fusion easily extendable
    
    * Add skip layernorm fusion
    
    * Formatting
    
    * Call correct layernorm function
    
    * Fix compile errors
    
    * Add DCE
    
    * Add test for skip layernorm
    
    * Formatting
    
    * Remove unused typedef
    
    * Formatting
    
    * Fix tidy issues
    
    * Formatting
    
    Co-authored-by: Shucai Xiao <shucai.xiao@amd.com>

--- src/targets/gpu/include/migraphx/gpu/device/layernorm.hpp
+++ src/targets/gpu/include/migraphx/gpu/device/layernorm.hpp
@@ -12,6 +12,12 @@ namespace device {
 
 void layernorm(hipStream_t stream, const argument& result, const argument& arg1);
 
+void triadd_layernorm(hipStream_t stream,
+                      const argument& result,
+                      const argument& arg1,
+                      const argument& arg2,
+                      const argument& arg3);
+
 } // namespace device
 } // namespace gpu
 } // namespace MIGRAPHX_INLINE_NS

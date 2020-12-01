2719a03f69db91d9ccde2d3a8486e0450d3a8602
Paul <pfultz2@yahoo.com>
2020-12-01 14:39:40 -0600

    Fix bug in setting padding in eliminate_pad

--- tools/include/operation.hpp
+++ tools/include/operation.hpp
@@ -259,6 +259,8 @@ value to_value_op(const T& x)
 template <class T>
 void from_value_op(T& x, const value& v)
 {
+    if(not(v.is_object() or (v.empty() and v.is_array())))
+        MIGRAPHX_THROW("Value is not an object");
     return migraphx::from_value(v, x);
 }
 

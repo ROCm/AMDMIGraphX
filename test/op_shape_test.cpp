f1c567cb87a1c4d95cecdbe218d62901d8285e6e
Khalique <15948690+kahmed10@users.noreply.github.com>
2020-12-02 12:19:01 -0600

    formatting

--- test/op_shape_test.cpp
+++ test/op_shape_test.cpp
@@ -351,7 +351,8 @@ TEST_CASE(broadcast)
         std::vector<std::size_t> lens{1, 1};
         migraphx::shape input{migraphx::shape::float_type, {1}, {0}};
         expect_shape(migraphx::shape{migraphx::shape::float_type, {1, 1}, {0, 0}},
-        migraphx::op::broadcast{1, lens}, input);
+                     migraphx::op::broadcast{1, lens},
+                     input);
     }
 
     {

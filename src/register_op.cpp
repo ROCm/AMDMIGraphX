277d9efec00cfab7db145a8f96581715798b328b
Paul <pfultz2@yahoo.com>
2020-12-16 12:26:03 -0600

    Add missing has_op function

--- src/register_op.cpp
+++ src/register_op.cpp
@@ -16,6 +16,11 @@ operation load_op(const std::string& name)
     return at(op_map(), name, "Operator not found: " + name);
 }
 
+bool has_op(const std::string& name)
+{
+    return op_map().count(name) == 1;
+}
+
 std::vector<std::string> get_operators()
 {
     std::vector<std::string> result;

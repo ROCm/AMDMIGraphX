9c714f2ad653bcf857e4760a1e3896981961f852
Paul <pfultz2@yahoo.com>
2020-12-16 12:27:02 -0600

    Formatting

--- src/register_op.cpp
+++ src/register_op.cpp
@@ -16,10 +16,7 @@ operation load_op(const std::string& name)
     return at(op_map(), name, "Operator not found: " + name);
 }
 
-bool has_op(const std::string& name)
-{
-    return op_map().count(name) == 1;
-}
+bool has_op(const std::string& name) { return op_map().count(name) == 1; }
 
 std::vector<std::string> get_operators()
 {

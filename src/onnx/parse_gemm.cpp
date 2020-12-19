b4bbda0afda4b5fb0708784cbf5f3527ccf8538e
Paul Fultz II <pfultz2@yahoo.com>
2020-12-19 15:45:52 -0600

    Refactor onnx parser (#699)
    * Load op when serializing
    
    * Formatting
    
    * Add missing clip field
    
    * Use make_op almost everywhere
    
    * Formatting
    
    * More make ops for rnns
    
    * Get rid of spaces
    
    * Formatting
    
    * Remove operators headers
    
    * Formatting
    
    * Remove unused op headers
    
    * Increase line threshold
    
    * Refactor onnx_parser class
    
    * Formatting
    
    * Add op_parser
    
    * Formatting
    
    * Remove old onnx drivers
    
    * Use file GLOB
    
    * Parse arg ops
    
    * Formatting
    
    * Add pooling
    
    * Formatting
    
    * Add parse_natchnorm
    
    * Add more operators
    
    * Formatting
    
    * Add more operators
    
    * Formatting
    
    * Add more operators
    
    * Formatting
    
    * Add more operators
    
    * Add rnn operators
    
    * Formatting
    
    * Fix tidy issues
    
    * Formatting
    
    * Add back missing param
    
    * Formatting
    
    * Fix shadow variable
    
    * Fix shadow in declaration
    
    * Make global constant
    
    * Formatting
    
    * Add generic op
    
    * Formatting
    
    * Add binary op
    
    * Formatting
    
    * Add variadiac op
    
    * Formatting
    
    * Remove unused fields and functions
    
    * Set default values
    
    * Formatting
    
    * Remove unused member variable
    
    * Add add literal overload
    
    * Use info.add_literal
    
    * Formatting
    
    * Call add_instruction through info class
    
    * Fix tidy issues
    
    * Formatting
    
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

---
+++ src/onnx/parse_gemm.cpp
@@ -0,0 +1,73 @@
+#include <migraphx/onnx/op_parser.hpp>
+#include <migraphx/ranges.hpp>
+#include <migraphx/instruction.hpp>
+#include <migraphx/make_op.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace onnx {
+
+struct parse_gemm : op_parser<parse_gemm>
+{
+    std::vector<op_desc> operators() const { return {{"Gemm"}}; }
+
+    instruction_ref parse(const op_desc& /*opd*/,
+                          const onnx_parser& parser,
+                          onnx_parser::node_info info,
+                          std::vector<instruction_ref> args) const
+    {
+        float alpha = 1.0f;
+        float beta  = 1.0f;
+        bool transa = false;
+        bool transb = false;
+        if(contains(info.attributes, "alpha"))
+        {
+            alpha = parser.parse_value(info.attributes.at("alpha")).at<float>();
+        }
+        if(contains(info.attributes, "beta"))
+        {
+            beta = parser.parse_value(info.attributes.at("beta")).at<float>();
+        }
+        if(contains(info.attributes, "transA"))
+        {
+            transa = parser.parse_value(info.attributes.at("transA")).at<bool>();
+        }
+        if(contains(info.attributes, "transB"))
+        {
+            transb = parser.parse_value(info.attributes.at("transB")).at<bool>();
+        }
+
+        std::vector<int64_t> perm(args[0]->get_shape().lens().size());
+        std::iota(perm.begin(), perm.end(), int64_t{0});
+        // swap the last two elements
+        std::swap(*perm.rbegin(), *(perm.rbegin() + 1));
+
+        auto l1 = (transa) ? info.add_instruction(make_op("transpose", {{"dims", perm}}), args[0])
+                           : args[0];
+        auto l2 = (transb) ? info.add_instruction(make_op("transpose", {{"dims", perm}}), args[1])
+                           : args[1];
+        if(args.size() == 3)
+        {
+            if(beta != 0.f && args[2]->get_shape().elements() > 0)
+            {
+                auto out_lens   = l1->get_shape().lens();
+                out_lens.back() = l2->get_shape().lens().back();
+                auto l3         = args[2];
+                auto l3_lens    = l3->get_shape().lens();
+                if(!std::equal(out_lens.begin(), out_lens.end(), l3_lens.begin(), l3_lens.end()))
+                {
+                    l3 = info.add_instruction(
+                        make_op("multibroadcast", {{"output_lens", out_lens}}), args[2]);
+                }
+                return info.add_instruction(
+                    make_op("dot", {{"alpha", alpha}, {"beta", beta}}), l1, l2, l3);
+            }
+        }
+
+        return info.add_instruction(make_op("dot", {{"alpha", alpha}, {"beta", beta}}), l1, l2);
+    }
+};
+
+} // namespace onnx
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx

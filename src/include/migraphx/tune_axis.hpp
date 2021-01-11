3b9ff607fce2f107c709d7050f208961f0dfd664
Khalique <15948690+kahmed10@users.noreply.github.com>
2021-01-11 17:38:24 -0600

    add default name and change string to upper

--- src/include/migraphx/tune_axis.hpp
+++ src/include/migraphx/tune_axis.hpp
@@ -5,15 +5,16 @@
 #include <cstdint>
 #include <string>
 #include <migraphx/errors.hpp>
+#include <boost/algorithm/string.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
-inline int tune_axis(const int n_dim, const int axis, const std::string& op_name)
+inline int tune_axis(const int n_dim, const int axis, const std::string& op_name="OPERATOR")
 {
     if(axis >= n_dim || abs(axis) > n_dim)
     {
-        MIGRAPHX_THROW(op_name + ": axis is out of range.");
+        MIGRAPHX_THROW(boost::to_upper_copy<std::string>(op_name) + ": axis is out of range.");
     }
     return (axis < 0) ? axis + n_dim : axis;
 }

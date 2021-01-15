a7c28938091ec179f757dcc9a1ada52bb8c8c5f0
Shucai Xiao <Shucai.Xiao@amd.com>
2021-01-14 23:27:09 -0600

    add unit tests for the and operator

--- test/onnx/gen_onnx.py
+++ test/onnx/gen_onnx.py
@@ -102,6 +102,19 @@ def add_scalar_test():
 
 
 @onnx_test
+def and_bcast_test():
+    x = helper.make_tensor_value_info('0', TensorProto.BOOL, [2, 3, 4, 5])
+    y = helper.make_tensor_value_info('1', TensorProto.BOOL, [4, 5])
+    z = helper.make_tensor_value_info('2', TensorProto.BOOL, [2, 3, 4, 5])
+
+    node = onnx.helper.make_node('And',
+                                 inputs=['0', '1'],
+                                 outputs=['2'])
+
+    return ([node], [x, y], [z])
+
+
+@onnx_test
 def argmax_test():
     x = helper.make_tensor_value_info('x', TensorProto.FLOAT, [3, 4, 5, 6])
     y = helper.make_tensor_value_info('y', TensorProto.FLOAT, [3, 4, 6])
--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -102,6 +102,22 @@ TEST_CASE(add_scalar_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(and_bcast_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto l0  = mm->add_parameter("0", migraphx::shape{migraphx::shape::bool_type, {2, 3, 4, 5}});
+    auto l1  = mm->add_parameter("1", migraphx::shape{migraphx::shape::bool_type, {4, 5}});
+    auto l2  = mm->add_instruction(
+        migraphx::make_op("multibroadcast", {{"output_lens", l0->get_shape().lens()}}), l1);
+    auto ret = mm->add_instruction(migraphx::make_op("andd"), l0, l2);
+    mm->add_return({ret});
+
+    auto prog = migraphx::parse_onnx("and_bcast_test.onnx");
+
+    EXPECT(p == prog);
+}
+
 TEST_CASE(argmax_test)
 {
     migraphx::program p;

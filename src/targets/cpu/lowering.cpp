d30357c1b5b2aefa3bd7473853148506a26a4ddf
Paul <pfultz2@yahoo.com>
2021-01-29 18:25:59 -0600

    Remove unused code

--- src/targets/cpu/lowering.cpp
+++ src/targets/cpu/lowering.cpp
@@ -563,28 +563,6 @@ struct cpu_apply
         });
     }
 
-    void extend_dnnl_extend_op(const std::string& op_name,
-                               const std::string& cpu_name,
-                               const std::string& dnnl_name)
-    {
-        apply_map.emplace(op_name, [=](instruction_ref ins) {
-            auto&& op = ins->get_operator();
-            if(has_op(dnnl_name) and ins->get_shape().type() == shape::type_t::float_type)
-                return replace(ins, make_op(dnnl_name, op.to_value()));
-            return replace(ins, make_op(cpu_name, op.to_value()));
-        });
-    }
-
-    void extend_dnnl_extend_op(const std::string& op_name, const std::string& dnnl_name)
-    {
-        apply_map.emplace(op_name, [=](instruction_ref ins) {
-            auto&& op = ins->get_operator();
-            if(has_op(dnnl_name) and ins->get_shape().type() == shape::type_t::float_type)
-                return replace(ins, make_op(dnnl_name, op.to_value()));
-            return ins;
-        });
-    }
-
     void init()
     {
         create_output_names();
@@ -594,12 +572,6 @@ struct cpu_apply
         extend_op("dot", "dnnl::dot", true);
         extend_op("relu", "dnnl::relu", true);
 
-        // extend_dnnl_extend_op("add", "cpu::add", "dnnl::add");
-        // extend_dnnl_extend_op("mul", "cpu::mul", "dnnl::mul");
-        // extend_dnnl_extend_op("convolution", "cpu::convolution", "dnnl::convolution");
-        // extend_dnnl_extend_op("dot", "cpu::dot", "dnnl::dot");
-        // extend_dnnl_extend_op("relu", "cpu::relu", "dnnl::relu");
-        // extend_dnnl_extend_op("concat", "dnnl::concat");
         extend_op("contiguous", "cpu::contiguous", true);
         extend_op("deconvolution", "cpu::deconvolution");
         extend_op("elu", "cpu::elu");

62574e06bcf752e84bcfbe680e35bf678cabc2af
Paul <pfultz2@yahoo.com>
2021-02-03 11:52:56 -0600

    Fix tidy warning

--- test/module_test.cpp
+++ test/module_test.cpp
@@ -90,7 +90,7 @@ TEST_CASE(module_name)
     migraphx::module m1("name");
     EXPECT(m1.name() == "name");
 
-    auto m2 = m1;
+    auto m2 = m1; // NOLINT
     EXPECT(m2.name() == "name");
     migraphx::module m3;
     m3 = m1;

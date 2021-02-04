5657683877cd770a18c6d22a1a7461c4080b411a
Shucai Xiao <Shucai.Xiao@amd.com>
2021-02-04 09:01:15 -0600

    update onnxruntime to a later version and run disabled tests sequentially

--- tools/build_and_test_onnxrt.sh
+++ tools/build_and_test_onnxrt.sh
@@ -4,4 +4,6 @@ pip3 install -r requirements.txt
 export PATH="/opt/cmake/bin:$PATH"
 export CXXFLAGS="-D__HIP_PLATFORM_HCC__=1 -w"
 ./build.sh --config Release --update --build --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=$(cat ./VERSION_NUMBER) --use_migraphx
+/onnxruntime/build/Linux/Release/onnx_test_runner "/onnxruntime/cmake/external/onnx/onnx/backend/test/data/pytorch-converted" -c 1 -j1 -ep migraphx
+/onnxruntime/build/Linux/Release/onnx_test_runner "/onnxruntime/cmake/external/onnx/onnx/backend/test/data/pytorch-operator"a -c 1 -j1 -ep migraphx
 # pip3 install /code/onnxruntime/build/Linux/Release/dist/*.whl

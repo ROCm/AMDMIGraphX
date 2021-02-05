549cfe728e438db5657b8a27f3510429d6914523
Paul Fultz II <pfultz2@yahoo.com>
2021-02-05 09:17:27 -0600

    Normalize compute methods (#723)
    * Normalize compute functions
    
    * Formatting
    
    * Save normalization flag to the file
    
    * Formatting
    
    * Remove tuned functions
    
    * Formatting
    
    * Use in_index
    
    Co-authored-by: Shucai Xiao <shucai@gmail.com>
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- src/include/migraphx/op/argmax.hpp
+++ src/include/migraphx/op/argmax.hpp
@@ -36,28 +36,22 @@ struct argmax
     shape normalize_compute_shape(std::vector<shape> inputs) const
     {
         check_shapes{inputs, *this}.has(1).standard();
-        auto lens     = inputs[0].lens();
-        int64_t n_dim = static_cast<int64_t>(lens.size());
+        auto lens = inputs[0].lens();
 
-        int64_t tuned_axis = tune_axis(n_dim, axis, name());
-
-        lens[tuned_axis] = 1;
+        lens[axis] = 1;
 
         return {shape::int64_type, lens};
     }
 
     template <class T>
-    int64_t calc_argmax(T& input,
-                        int64_t tuned_axis,
-                        std::vector<std::size_t>& indices,
-                        size_t item_num) const
+    int64_t calc_argmax(T& input, std::vector<std::size_t>& indices, size_t item_num) const
     {
         auto max_val      = input(indices.begin(), indices.end());
         int64_t max_index = 0;
         for(std::size_t i = 1; i < item_num; ++i)
         {
-            indices[tuned_axis] = i;
-            auto cur_val        = input(indices.begin(), indices.end());
+            indices[axis] = i;
+            auto cur_val  = input(indices.begin(), indices.end());
             if(max_val < cur_val)
             {
                 max_val   = cur_val;
@@ -71,15 +65,13 @@ struct argmax
     argument compute(const shape& output_shape, std::vector<argument> args) const
     {
         argument result{output_shape};
-        auto n_dim          = args.front().get_shape().lens().size();
-        auto tuned_axis     = tune_axis(n_dim, axis, name());
-        auto batch_item_num = args.front().get_shape().lens()[tuned_axis];
+        auto batch_item_num = args.front().get_shape().lens()[axis];
 
         result.visit([&](auto output) {
             args[0].visit([&](auto input) {
                 par_for(output_shape.elements(), [&](auto i) {
                     auto data_idx = output_shape.multi(i);
-                    output[i]     = this->calc_argmax(input, tuned_axis, data_idx, batch_item_num);
+                    output[i]     = this->calc_argmax(input, data_idx, batch_item_num);
                 });
             });
         });

874bac0ce6c2b0e2dcdb6f457f19c0819df2aa77
Paul <pfultz2@yahoo.com>
2021-02-05 12:51:21 -0600

    Formatting

--- src/targets/cpu/logsoftmax.cpp
+++ src/targets/cpu/logsoftmax.cpp
@@ -6,8 +6,7 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace cpu {
 
-struct dnnl_logsoftmax
-    : dnnl_extend_op<dnnl_logsoftmax, dnnl::logsoftmax_forward, op::logsoftmax>
+struct dnnl_logsoftmax : dnnl_extend_op<dnnl_logsoftmax, dnnl::logsoftmax_forward, op::logsoftmax>
 {
     dnnl::logsoftmax_forward::desc
     get_desc(const std::unordered_map<int, dnnl::memory::desc>& m) const
--- src/targets/cpu/softmax.cpp
+++ src/targets/cpu/softmax.cpp
@@ -6,11 +6,9 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace cpu {
 
-struct dnnl_softmax
-    : dnnl_extend_op<dnnl_softmax, dnnl::softmax_forward, op::softmax>
+struct dnnl_softmax : dnnl_extend_op<dnnl_softmax, dnnl::softmax_forward, op::softmax>
 {
-    dnnl::softmax_forward::desc
-    get_desc(const std::unordered_map<int, dnnl::memory::desc>& m) const
+    dnnl::softmax_forward::desc get_desc(const std::unordered_map<int, dnnl::memory::desc>& m) const
     {
         int axis = this->op.axis;
         return {dnnl::prop_kind::forward_inference, m.at(DNNL_ARG_SRC_0), axis};

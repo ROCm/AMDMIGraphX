2f96ec26f5080b73ae28e5ba469c76fdaf19aa24
Cagri Eryilmaz <ceryilma@amd.com>
2021-02-08 23:17:17 +0000

    reordering not_test in onnx_test.cpp

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -1658,20 +1658,6 @@ TEST_CASE(logsoftmax_test)
     EXPECT(p == prog);
 }
 
-TEST_CASE(not_test)
-{
-    migraphx::program p;
-    auto* mm = p.get_main_module();
-    auto l0  = mm->add_parameter("0", migraphx::shape{migraphx::shape::int32_type, {4}});
-    auto ret = mm->add_instruction(migraphx::make_op("not"), l0);
-    mm->add_return({ret});
-
-    auto prog = migraphx::parse_onnx("not_test.onnx");
-
-    EXPECT(p == prog);
-}
-
-
 TEST_CASE(lrn_test)
 {
     migraphx::program p;
@@ -1926,6 +1912,19 @@ TEST_CASE(nonzero_int_test)
     mm->add_return({r});
 
     auto prog = migraphx::parse_onnx("nonzero_int_test.onnx");
+    EXPECT(p == prog);
+}
+
+TEST_CASE(not_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto l0  = mm->add_parameter("0", migraphx::shape{migraphx::shape::int32_type, {4}});
+    auto ret = mm->add_instruction(migraphx::make_op("not"), l0);
+    mm->add_return({ret});
+
+    auto prog = migraphx::parse_onnx("not_test.onnx");
+
     EXPECT(p == prog);
 }
 

9b5a462008acd59021dd149a389becd8b5bf237a
Shucai Xiao <Shucai.Xiao@amd.com>
2021-03-01 10:00:02 -0600

    fix a cppcheck error

--- src/include/migraphx/module.hpp
+++ src/include/migraphx/module.hpp
@@ -79,10 +79,10 @@ struct module
                                         const operation& op,
                                         std::vector<instruction_ref> args) MIGRAPHX_TIDY_CONST;
 
-    instruction_ref replace_instruction(instruction_ref ins,
+    instruction_ref replace_ins(instruction_ref ins,
                                         const operation& op,
                                         std::vector<instruction_ref> args,
-                                        std::vector<module_ref> module_args);
+                                        std::vector<module_ref> module_args) MIGRAPHX_TIDY_CONST;
 
     instruction_ref replace_instruction(instruction_ref ins, instruction_ref rep);
 

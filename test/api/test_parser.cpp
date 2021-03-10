5993c8aea82a37e72f0fe4d38a67e3d997608e65
Khalique <15948690+kahmed10@users.noreply.github.com>
2021-03-10 12:13:12 -0600

    simplify tests

--- test/api/test_parser.cpp
+++ test/api/test_parser.cpp
@@ -5,23 +5,8 @@
 TEST_CASE(load_and_run)
 {
     auto p             = migraphx::parse_tf("add_test.pb");
-    auto shapes_before = p.get_output_shapes();
-    migraphx_compile_options options;
-    options.offload_copy = true;
-    p.compile(migraphx::target("gpu"), options);
-    auto shapes_after = p.get_output_shapes();
-    CHECK(shapes_before.size() == 1);
-    CHECK(shapes_before.size() == shapes_after.size());
-    CHECK(bool{shapes_before.front() == shapes_after.front()});
-    migraphx::program_parameters pp;
-    auto param_shapes = p.get_parameter_shapes();
-    for(auto&& name : param_shapes.names())
-    {
-        pp.add(name, migraphx::argument::generate(param_shapes[name]));
-    }
-    auto outputs = p.eval(pp);
-    CHECK(shapes_before.size() == outputs.size());
-    CHECK(bool{shapes_before.front() == outputs.front().get_shape()});
+    auto shapes = p.get_output_shapes();
+    CHECK(shapes.size() == 1);
 }
 
 TEST_CASE(load_and_run_multi)
@@ -29,23 +14,8 @@ TEST_CASE(load_and_run_multi)
     migraphx::tf_options tf_options;
     tf_options.set_output_names({"relu", "tanh"});
     auto p             = migraphx::parse_tf("multi_output_test.pb", tf_options);
-    auto shapes_before = p.get_output_shapes();
-    migraphx_compile_options options;
-    options.offload_copy = true;
-    p.compile(migraphx::target("gpu"), options);
-    auto shapes_after = p.get_output_shapes();
-    CHECK(shapes_before.size() == 2);
-    CHECK(shapes_before.size() == shapes_after.size());
-    CHECK(bool{shapes_before.front() == shapes_after.front()});
-    migraphx::program_parameters pp;
-    auto param_shapes = p.get_parameter_shapes();
-    for(auto&& name : param_shapes.names())
-    {
-        pp.add(name, migraphx::argument::generate(param_shapes[name]));
-    }
-    auto outputs = p.eval(pp);
-    CHECK(shapes_before.size() == outputs.size());
-    CHECK(bool{shapes_before.front() == outputs.front().get_shape()});
+    auto shapes = p.get_output_shapes();
+    CHECK(shapes.size() == 2);
 }
 
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

dd8d4b216c9a156d1583db78764d6ccb99de82ef
Shucai Xiao <Shucai.Xiao@amd.com>
2021-03-10 18:10:33 -0600

    fix review comments

--- src/include/migraphx/op/if_op.hpp
+++ src/include/migraphx/op/if_op.hpp
@@ -18,10 +18,22 @@ struct if_op
 {
     std::string name() const { return "if"; }
 
-    shape compute_shape(const std::vector<shape>&, std::vector<module_ref> mods) const
+    shape compute_shape(const std::vector<shape>& inputs, std::vector<module_ref> mods) const
     {
-        auto out_shapes = mods[0]->get_output_shapes();
-        return out_shapes[0];
+        check_shapes{inputs, *this}.has(1).standard();
+        if (mods.size() != 2)
+        {
+            MIGRAPHX_THROW("IF: operator should have two submodules.");
+        }
+
+        auto out_shapes0 = mods[0]->get_output_shapes();
+        auto out_shapes1 = mods[1]->get_output_shapes();
+        if (not std::equal(out_shapes1.begin(), out_shapes1.end(), out_shapes0.begin(), out_shapes0.end()))
+        {
+            MIGRAPHX_THROW("IF: output shapes of submodules must be the same.");
+        }
+
+        return out_shapes0.front();
     }
 };
 

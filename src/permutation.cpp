115bc8d6b91d6c40dbf4c093add209d8581963bc
Paul <pfultz2@yahoo.com>
2021-03-17 19:04:56 -0500

    Sort with lens

--- src/permutation.cpp
+++ src/permutation.cpp
@@ -20,27 +20,27 @@ std::vector<int64_t> invert_permutation(const std::vector<int64_t>& permutation)
 
 std::vector<int64_t> find_permutation(const shape& s)
 {
-    std::vector<std::int64_t> result;
-    std::multimap<std::size_t, int64_t, std::greater<>> m;
-    int i = 0;
-    for(auto&& x : s.strides())
-        m.emplace(x, i++);
-    auto pred     = by(std::equal_to<>{}, [](auto&& p) { return p.first; });
-    int64_t base  = 0;
-    int64_t delta = 0;
-    auto each     = [&](auto start, auto last) {
-        auto n = std::distance(start, last);
-        std::transform(start, last, std::back_inserter(result), [](auto&& p) { return p.second; });
-        auto mag = delta > 0 ? -1 : 1;
-        std::stable_sort(result.end() - n, result.end(), by(std::less<>{}, [&](auto x) {
-                             auto diff = (x - base);
-                             return std::make_tuple(std::abs(diff), (x - base) * mag);
-                         }));
-        delta = result.back() - base;
-        base  = result.back();
-    };
-    group_unique(m.begin(), m.end(), each, pred);
+    std::vector<std::int64_t> result(s.lens().size());
+    std::iota(result.begin(), result.end(), 0);
+    std::stable_sort(
+        result.begin(), result.end(), by(std::greater<>{}, [&](auto x) { return std::make_tuple(s.strides()[x], s.lens()[x]); }));
     return result;
+
+    // std::vector<std::int64_t> result;
+    // std::multimap<std::size_t, int64_t, std::greater<>> m;
+    // int i = 0;
+    // for(auto&& x : s.strides())
+    //     m.emplace(x, i++);
+    // auto pred = by(std::equal_to<>{}, [](auto&& p) { return p.first; });
+    // auto each = [&](auto start, auto last) {
+    //     auto n = std::distance(start, last);
+    //     std::transform(start, last, std::back_inserter(result), [](auto&& p) { return p.second; });
+    //     std::stable_sort(result.end() - n, result.end(), by(std::greater<>{}, [&](auto x) {
+    //                          return s.lens()[x];
+    //                      }));
+    // };
+    // group_unique(m.begin(), m.end(), each, pred);
+    // return result;
 }
 
 } // namespace MIGRAPHX_INLINE_NS

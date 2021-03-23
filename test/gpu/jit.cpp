5d157640dc8ff0d4bdba77faf8278b4315412363
Paul <pfultz2@yahoo.com>
2021-03-22 20:26:25 -0500

    Move compilation to core

--- test/gpu/jit.cpp
+++ test/gpu/jit.cpp
@@ -42,7 +42,7 @@ int main() {}
 
 )migraphx";
 
-migraphx::gpu::src_file make_src_file(const std::string& name, const std::string& content)
+migraphx::src_file make_src_file(const std::string& name, const std::string& content)
 {
     return {name, std::make_pair(content.data(), content.data() + content.size())};
 }

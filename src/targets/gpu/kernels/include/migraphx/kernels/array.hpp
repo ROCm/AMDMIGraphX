581d31b02fd1b4f6e2fe670c9249f50ce5ce052a
Paul Fultz II <pfultz2@yahoo.com>
2021-03-26 09:09:22 -0500

    Add initial code generation (#762)
    * Add code object op
    
    * Formattting
    
    * Add more value tests
    
    * Formatting
    
    * Fix from_value conversion from binary
    
    * Formatting
    
    * Dont use offload copy
    
    * Remove iostream header
    
    * Fix compilation errors
    
    * Formatting
    
    * Rename var
    
    * Add missing files
    
    * Formatting
    
    * Remove duplicate variable
    
    * Remove comment
    
    * Template the function so sfinae will work
    
    * Formatting
    
    * Use template specialization since ADL is broken on hcc
    
    * Formatting
    
    * Annotate the constructor with HD for hcc
    
    * Make variable const
    
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

---
+++ src/targets/gpu/kernels/include/migraphx/kernels/array.hpp
@@ -0,0 +1,159 @@
+#ifndef MIGRAPHX_GUARD_AMDMIGRAPHX_KERNELS_ARRAY_HPP
+#define MIGRAPHX_GUARD_AMDMIGRAPHX_KERNELS_ARRAY_HPP
+
+#include <migraphx/kernels/types.hpp>
+#include <type_traits>
+
+namespace migraphx {
+
+// NOLINTNEXTLINE
+#define MIGRAPHX_DEVICE_ARRAY_OP(op, binary_op)                               \
+    constexpr array& operator op(const array& x)                              \
+    {                                                                         \
+        for(index_int i = 0; i < N; i++)                                      \
+            d[i] op x[i];                                                     \
+        return *this;                                                         \
+    }                                                                         \
+    constexpr array& operator op(const T& x)                                  \
+    {                                                                         \
+        for(index_int i = 0; i < N; i++)                                      \
+            d[i] op x;                                                        \
+        return *this;                                                         \
+    }                                                                         \
+    friend constexpr array operator binary_op(const array& x, const array& y) \
+    {                                                                         \
+        auto z = x;                                                           \
+        return z op y;                                                        \
+    }                                                                         \
+    friend constexpr array operator binary_op(const array& x, const T& y)     \
+    {                                                                         \
+        auto z = x;                                                           \
+        return z op y;                                                        \
+    }                                                                         \
+    friend constexpr array operator binary_op(const T& x, const array& y)     \
+    {                                                                         \
+        for(index_int i = 0; i < N; i++)                                      \
+            y[i] = x op y[i];                                                 \
+        return y;                                                             \
+    }
+
+template <class T, index_int N>
+struct array
+{
+    T d[N];
+    constexpr T& operator[](index_int i) { return d[i]; }
+    constexpr const T& operator[](index_int i) const { return d[i]; }
+
+    constexpr T& front() { return d[0]; }
+    constexpr const T& front() const { return d[0]; }
+
+    constexpr T& back() { return d[N - 1]; }
+    constexpr const T& back() const { return d[N - 1]; }
+
+    constexpr T* data() { return d; }
+    constexpr const T* data() const { return d; }
+
+    constexpr std::integral_constant<index_int, N> size() const { return {}; }
+
+    constexpr T* begin() { return d; }
+    constexpr const T* begin() const { return d; }
+
+    constexpr T* end() { return d + size(); }
+    constexpr const T* end() const { return d + size(); }
+
+    constexpr T dot(const array& x) const
+    {
+        T result = 0;
+        for(index_int i = 0; i < N; i++)
+            result += x[i] * d[i];
+        return result;
+    }
+
+    constexpr T product() const
+    {
+        T result = 1;
+        for(index_int i = 0; i < N; i++)
+            result *= d[i];
+        return result;
+    }
+
+    constexpr T single(index_int width = 100) const
+    {
+        T result = 0;
+        T a      = 1;
+        for(index_int i = 0; i < N; i++)
+        {
+            result += d[N - i - 1] * a;
+            a *= width;
+        }
+        return result;
+    }
+
+    MIGRAPHX_DEVICE_ARRAY_OP(+=, +)
+    MIGRAPHX_DEVICE_ARRAY_OP(-=, -)
+    MIGRAPHX_DEVICE_ARRAY_OP(*=, *)
+    MIGRAPHX_DEVICE_ARRAY_OP(/=, /)
+    MIGRAPHX_DEVICE_ARRAY_OP(%=, %)
+    MIGRAPHX_DEVICE_ARRAY_OP(&=, &)
+    MIGRAPHX_DEVICE_ARRAY_OP(|=, |)
+    MIGRAPHX_DEVICE_ARRAY_OP(^=, ^)
+
+    friend constexpr bool operator==(const array& x, const array& y)
+    {
+        for(index_int i = 0; i < N; i++)
+        {
+            if(x[i] != y[i])
+                return false;
+        }
+        return true;
+    }
+
+    friend constexpr bool operator!=(const array& x, const array& y) { return !(x == y); }
+    // This uses the product order rather than lexical order
+    friend constexpr bool operator<(const array& x, const array& y)
+    {
+        for(index_int i = 0; i < N; i++)
+        {
+            if(not(x[i] < y[i]))
+                return false;
+        }
+        return true;
+    }
+    friend constexpr bool operator>(const array& x, const array& y) { return y < x; }
+    friend constexpr bool operator<=(const array& x, const array& y) { return (x < y) or (x == y); }
+    friend constexpr bool operator>=(const array& x, const array& y) { return (y < x) or (x == y); }
+
+    constexpr array carry(array result) const
+    {
+        uint32_t overflow = 0;
+        for(std::ptrdiff_t i = result.size() - 1; i > 0; i--)
+        {
+            auto z = result[i] + overflow;
+            // Reset overflow
+            overflow = 0;
+            // Compute overflow using while loop instead of mod
+            while(z >= d[i])
+            {
+                z -= d[i];
+                overflow += 1;
+            }
+            result[i] = z;
+        }
+        result[0] += overflow;
+        return result;
+    }
+};
+
+template <class T, T... xs>
+struct integral_const_array : array<T, sizeof...(xs)>
+{
+    using base_array = array<T, sizeof...(xs)>;
+    MIGRAPHX_DEVICE_CONSTEXPR integral_const_array() : base_array({xs...}) {}
+};
+
+template <index_int... Ns>
+using index_ints = integral_const_array<index_int, Ns...>;
+
+} // namespace migraphx
+
+#endif

86069589376ee3ef3bee399203c4079916f74b85
Shucai Xiao <Shucai.Xiao@amd.com>
2021-03-27 23:47:33 -0500

    code backup

--- test/memory_coloring_test.cpp
+++ test/memory_coloring_test.cpp
@@ -3,6 +3,7 @@
 #include <migraphx/check_shapes.hpp>
 #include <migraphx/generate.hpp>
 #include <migraphx/instruction.hpp>
+#include <migraphx/make_op.hpp>
 #include <basic_ops.hpp>
 #include <test.hpp>
 
@@ -635,6 +636,45 @@ TEST_CASE(test38)
     run_pass(m);
     CHECK(m.get_parameter_shape("scratch").bytes() == 7225344); // Optimal solution is 6422528
     CHECK(no_allocate(m));
+}
+
+TEST_CASE(test39)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    migraphx::shape cond_s{migraphx::shape::bool_type};
+    auto cond = mm->add_parameter("cond", cond_s);
+    auto output = mm->add_parameter("output", {migraphx::shape::float_type, {20}});
+
+    migraphx::shape ds{migraphx::shape::float_type, {2, 3}};
+    auto i1 = add_alloc(*mm, ds);
+    auto i2 = add_alloc(*mm, ds);
+    std::vector<float> data2 = {-0.258047, 0.360394, 0.536804, -0.577762, 1.0217, 1.02442};
+    auto l2                  = mm->add_literal(migraphx::literal(ds, data2));
+
+    auto* then_mod           = p.create_module("If_0_if");
+    std::vector<float> data1 = {0.384804, -1.77948, -0.453775, 0.477438, -1.06333, -1.12893};
+    auto l1                  = then_mod->add_literal(migraphx::literal(ds, data1));
+    auto tx                  = then_mod->add_parameter("x", ds);
+    auto a1                  = then_mod->add_instruction(migraphx::make_op("add"), i1, l1);
+    then_mod->add_return({a1});
+
+    auto* else_mod = p.create_module("If_0_else");
+    auto ey        = else_mod->add_parameter("y", ds);
+    auto a2        = else_mod->add_instruction(migraphx::make_op("mul"), i2, l2);
+    else_mod->add_return({a2});
+
+    auto ret = mm->add_instruction(migraphx::make_op("if"), {cond}, {then_mod, else_mod});
+    mm->add_return({ret});
+
+    auto sub_modules = p.get_modules();
+    std::reverse(sub_modules.begin(), sub_modules.end());
+    for (auto& smod : sub_modules)
+    {
+        run_pass(*smod);
+    }
+    // CHECK(m.get_parameter_shape("scratch").bytes() == 320);
+    // CHECK(no_allocate(m));
 }
 
 TEST_CASE(literal_test)

d8a89184e9d1a3d9433589d0ef08782c9f13a735
Shucai Xiao <Shucai.Xiao@amd.com>
2021-03-29 12:25:48 -0500

    code backup

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -21,6 +21,7 @@
 #include <migraphx/serialize.hpp>
 
 #include <migraphx/algorithm.hpp>
+#include <unordered_set>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -58,6 +59,7 @@ struct find_mul_conv
 
     void apply(module& p, match::matcher_result r) const
     {
+        std::cout << "mul_conv, p = " << p << std::endl;
         auto ins      = r.result;
         auto conv_ins = r.instructions["conv"];
         auto a_ins    = r.instructions["a"];
@@ -96,6 +98,8 @@ struct find_mul_slice_conv
 
     void apply(module& p, match::matcher_result r) const
     {
+        std::cout << "mul_slice, p = " << p << std::endl;
+
         auto ins       = r.result;
         auto slice_ins = r.instructions["slice"];
         auto conv_ins  = r.instructions["conv"];
@@ -411,6 +415,29 @@ struct find_splits
             match::name("slice")(match::any_of[match::outputs()](pointwise(), reduction()))));
     }
 
+    static bool is_dependent(instruction_ref ins1, instruction_ref ins2)
+    {
+
+        std::unordered_set<instruction_ref> traversed;
+        return fix<bool>([&](auto self, auto ins) -> bool {
+            if(ins == ins2)
+                return true;
+
+            if(contains(traversed, ins))
+                return false;
+
+            traversed.insert(ins);
+            const auto& inputs = ins->inputs();
+            for(auto in : inputs)
+            {
+                if(self(in))
+                    return true;
+            }
+
+            return false;
+        })(ins1);
+    }
+
     static std::vector<std::vector<instruction_ref>>
     get_split_groups(const std::vector<instruction_ref>& splits)
     {
@@ -429,9 +456,18 @@ struct find_splits
                 if(it == split->outputs().end())
                     break;
                 assert((*it)->name() != "slice");
-                // If there is a duplicate bail
+
                 if(contains(group, *it))
                     return {};
+
+                // there are should be no dependent between instructions in the group
+                if (std::any_of(group.begin(), group.end(), [&](auto i) {
+                    return is_dependent(*it, i) or is_dependent(i, *it);
+                }))
+                {
+                    return {};
+                }
+
                 group.push_back(*it);
             }
             if(group.size() != splits.size())
@@ -468,6 +504,7 @@ struct find_splits
 
     void apply(module& p, const match::matcher_result& r) const
     {
+        std::cout << "find_splits = " << p << std::endl;
         auto ins = r.result;
 
         auto splits = get_splits(ins);
@@ -476,6 +513,14 @@ struct find_splits
 
         for(const auto& group : get_split_groups(splits))
         {
+            std::cout << "loc1, group_size = " << group.size() << std::endl;
+            for(const auto& elem : group)
+            {
+                std::cout << elem->name() << "\t";
+            }
+            std::cout << std::endl;
+            std::cout << "loc1.1" << std::endl;
+
             auto start       = group.front();
             auto split_front = splits.front();
             auto op          = start->get_operator();
@@ -485,6 +530,7 @@ struct find_splits
             }
 
             // Make sure there is no duplicates
+            std::cout << "loc2" << std::endl;
             assert(std::none_of(
                 std::next(group.begin()), group.end(), [&](auto i) { return i == start; }));
 
@@ -500,12 +546,14 @@ struct find_splits
                     return i->name() == "slice";
                 }) && "one argument must be a split");
                 auto data_idx = 1;
+                std::cout << "loc3" << std::endl;
                 if(start->inputs().back()->name() == "slice")
                 {
                     split_idx = 1;
                     data_idx  = 0;
                 }
 
+                std::cout << "loc4" << std::endl;
                 std::vector<instruction_ref> data_args;
                 std::transform(group.begin(),
                                group.end(),
@@ -525,6 +573,7 @@ struct find_splits
                 assert(not slice_op.axes.empty());
                 if(slice_op.axes.size() > 1)
                     return;
+                std::cout << "loc5" << std::endl;
                 auto concat_axis = slice_op.axes.front();
                 // TODO: Check if axises match
                 auto concat = p.insert_instruction(
@@ -535,13 +584,17 @@ struct find_splits
                 args[split_idx] = ins;
                 args[data_idx]  = concat;
                 c               = p.insert_instruction(std::next(ins), op, args);
+                std::cout << "loc6" << std::endl;
             }
             if(c != p.end())
             {
                 for(auto i : group)
                 {
+                    std::cout << "loc7.0, i_name = " << i->name() << std::endl;
                     auto split = i->inputs()[split_idx];
+                    std::cout << "loc7, split_name = " << split->name() << std::endl;
                     assert(split->name() == "slice");
+                    std::cout << "loc7.1, split_name = " << split->name() << std::endl;
                     // Insert contiguous for reshapes
                     for(auto output : i->outputs())
                     {
@@ -552,7 +605,14 @@ struct find_splits
                         p.replace_instruction(output, output->get_operator(), x);
                     }
 
+                    std::cout << "loc8, p = " << p << std::endl;
+                    std::cout << "i_name = " << i->name() << ", shape = " << i->get_shape()
+                              << std::endl;
+                    std::cout << "c_name = " << c->name() << ", shape = " << c->get_shape()
+                              << std::endl;
+                    std::cout << "op_name = " << split->get_operator().name() << std::endl;
                     p.replace_instruction(i, split->get_operator(), c);
+                    std::cout << "loc9" << std::endl;
                 }
             }
         }
@@ -1025,6 +1085,7 @@ void simplify_algebra::apply(module& p) const
     // Run simplifications multiple times
     for(int i = 0; i < 8; i++)
     {
+        std::cout << "i = " << i << std::endl;
         match::find_matches(p,
                             find_inner_broadcast{},
                             find_double_add_lit_broadcast{},

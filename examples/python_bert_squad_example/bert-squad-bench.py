10f7b7cd9ee34bf1dae63c2e9853644dd9b87640
Cagri Eryilmaz <ceryilma@amd.com>
2021-03-30 22:17:45 +0000

    benchmark file

---
+++ examples/python_bert_squad_example/bert-squad-bench.py
@@ -0,0 +1,108 @@
+import numpy as np
+import json
+import time
+import os.path
+from os import path
+import sys
+
+import tokenization
+from run_onnx_squad import *
+
+import time
+
+print(sys.path)
+
+migx_lib_path = "/opt/rocm/lib"
+if migx_lib_path not in sys.path:
+    sys.path.append(migx_lib_path)
+
+import migraphx
+
+#######################################
+input_file = 'inputs.json'
+with open(input_file) as json_file:
+    test_data = json.load(json_file)
+    print(json.dumps(test_data, indent=2))
+
+# preprocess input
+predict_file = 'inputs.json'
+
+# Use read_squad_examples method from run_onnx_squad to read the input file
+eval_examples = read_squad_examples(input_file=predict_file)
+
+max_seq_length = 256
+doc_stride = 128
+max_query_length = 64
+batch_size = 1
+n_best_size = 20
+max_answer_length = 30
+
+vocab_file = os.path.join('uncased_L-12_H-768_A-12', 'vocab.txt')
+
+start = time.time()
+tokenizer = tokenization.FullTokenizer(vocab_file=vocab_file,
+                                       do_lower_case=True)
+print("Tokenization time:\t %s ms"%(1000*(time.time() - start)))
+
+# Use convert_examples_to_features method from run_onnx_squad to get parameters from the input
+start = time.time()
+input_ids, input_mask, segment_ids, extra_data = convert_examples_to_features(
+    eval_examples, tokenizer, max_seq_length, doc_stride, max_query_length)
+print("Example to feature time:\t %s ms"%(1000*(time.time() - start)))
+
+#######################################
+# Compile
+print("INFO: Parsing and compiling the model...")
+start = time.time()
+model = migraphx.parse_onnx("bertsquad-10.onnx")
+model.compile(migraphx.get_target("gpu"))
+#model.print()
+print("MIGraphX Parse and Compile:\t %s ms"%(1000*(time.time() - start)))
+
+n = len(input_ids)
+bs = batch_size
+all_results = []
+
+for idx in range(0, n):
+    item = eval_examples[idx]
+    for _ in range(0, 30):
+        start = time.time()
+        result = model.run({
+            "unique_ids_raw_output___9:0":
+            np.array([item.qas_id], dtype=np.int64),
+            "input_ids:0":
+            input_ids[idx:idx + bs],
+            "input_mask:0":
+            input_mask[idx:idx + bs],
+            "segment_ids:0":
+            segment_ids[idx:idx + bs]
+        })
+        print("MIGraphX Model Run: %s ms"%(1000*(time.time() - start)))
+        
+    start = time.time()
+    in_batch = result[1].get_shape().lens()[0]
+    start_logits = [float(x) for x in result[1].tolist()]
+    end_logits = [float(x) for x in result[0].tolist()]
+    for i in range(0, in_batch):
+        unique_id = len(all_results)
+        all_results.append(
+            RawResult(unique_id=unique_id,
+                    start_logits=start_logits,
+                    end_logits=end_logits))
+    print("Logit processing: %s ms"%(1000*(time.time() - start)))
+
+output_dir = 'predictions'
+os.makedirs(output_dir, exist_ok=True)
+output_prediction_file = os.path.join(output_dir, "predictions.json")
+output_nbest_file = os.path.join(output_dir, "nbest_predictions.json")
+
+start = time.time()
+write_predictions(eval_examples, extra_data, all_results, n_best_size,
+                  max_answer_length, True, output_prediction_file,
+                  output_nbest_file)
+print("Raw result to predictions for %s samples: %s ms"%(n, 1000*(time.time() - start)))
+
+import json
+with open(output_prediction_file) as json_file:
+    test_data = json.load(json_file)
+    print(json.dumps(test_data, indent=2))

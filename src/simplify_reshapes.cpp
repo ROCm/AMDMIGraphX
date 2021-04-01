ccd4bbbb49f1800596835d1d726b64873810c525
Shucai Xiao <Shucai.Xiao@amd.com>
2021-03-31 23:05:21 -0500

    add one more optimzation to remove contiguous

--- src/simplify_reshapes.cpp
+++ src/simplify_reshapes.cpp
@@ -463,6 +463,51 @@ struct find_where_op
     }
 };
 
+struct find_reshape_cont
+{
+    auto matcher() const
+    {
+        return match::name("reshape")(match::args(match::name("continuous").bind("cont")), match::used_once());
+    }
+
+    void apply(module& p, match::matcher_result r) const
+    {
+        auto ins     = r.result;
+        auto ins_cont = r.instructions["cont"];
+        auto cont_input = ins_cont->inputs().front();
+        auto lens = cont_input->get_shape().lens();
+        std::vector<int64_t> dims(lens.begin(), lens.end());
+
+        auto out_ins = ins->outputs().front();
+        if (out_ins->get_shape() != ins->get_shape())
+        {
+            return;
+        }
+        auto out_lens = out_ins->get_shape().lens();
+        std::vector<int64_t> out_dims(out_lens.begin(), out_lens.end());
+
+        std::vector<instruction_ref> inputs;
+        for (const auto& in : out_ins->inputs())
+        {
+            if (in == ins)
+            {
+                inputs.push_back(cont_input);
+            }
+            else
+            {
+                auto std_in = in;
+                if (not in->get_shape().standard())
+                {
+                    std_in = p.insert_instruction(out_ins, make_op("contiguous"), in);
+                }
+                inputs.push_back(p.insert_instruction(out_ins, make_op("reshape", {{"dims", dims}}), std_in));
+            }
+        }
+        auto out = p.insert_instruction(out_ins, out_ins->get_operator(), inputs);
+        p.replace_instruction(out_ins, make_op("reshape", {{"dims", out_dims}}), out);
+    }
+};
+
 void simplify_reshapes::apply(module& p) const
 {
     for(int i = 0; i < 2; i++)

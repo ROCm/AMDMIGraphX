daa517f45b5e258a8e6141d14a3b5540856552d1
Shucai Xiao <Shucai.Xiao@amd.com>
2021-04-02 08:10:49 -0500

    clang format

--- src/simplify_reshapes.cpp
+++ src/simplify_reshapes.cpp
@@ -381,9 +381,11 @@ struct find_resize
         std::transform(index.begin(), index.end(), equal.begin(), [&](auto i) {
             auto out_idx = out_shape.multi(i);
             auto in_idx  = out_idx;
-            std::transform(out_idx.begin(), out_idx.end(), scales.begin(), in_idx.begin(), [&](auto io, auto scale) {
-                return io - (io % scale);
-            });
+            std::transform(out_idx.begin(),
+                           out_idx.end(),
+                           scales.begin(),
+                           in_idx.begin(),
+                           [&](auto io, auto scale) { return io - (io % scale); });
             auto iidx = out_shape.index(in_idx);
             return vec_ind[i] == vec_ind[iidx];
         });

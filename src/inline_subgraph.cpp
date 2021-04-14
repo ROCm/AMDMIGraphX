e14838155f9649a389d8f8b6e288a33a8f90d606
Shucai Xiao <Shucai.Xiao@amd.com>
2021-04-14 13:11:03 -0500

    code backup

--- src/inline_subgraph.cpp
+++ src/inline_subgraph.cpp
@@ -15,12 +15,45 @@ void inline_subgraph::apply(module& p) const
         if(ins->name() != "if")
             continue;
 
-        shape s = ins->get_shape();
-        if(not s.standard() and s.elements() != 0)
+        const auto& mod_inputs = ins->module_inputs();
+        std::vector<argument> arg_outs;
+        for (const auto& mod : mod_inputs)
         {
-            auto c = p.insert_instruction(std::next(ins), make_op("contiguous"), ins);
-            p.replace_instruction(ins, c);
+            auto last = std::prev(mod-end());
+            std::vector<instruction_ref> mod_outputs;
+            if (last->name() == "@return")
+            {
+                mod_outputs = last->inputs();
+            }
+            else
+            {
+                // no return instruction, last instruction is output
+                mod_outputs.push_back(last);
+            }
+
+            // only one output is considered for now
+            auto out = mod_outputs.front();
+            auto mod_out = out->eval();
+            if(mod_out.empty())
+            {
+                return;
+            }
+            arg_outs.push_back(mod_out);
         }
+        assert(arg_outs.size() == 2);
+
+        auto l0 = p.add_literal(literal(arg_outs.at(0).get_shape(), arg_outs.at(0).data()));
+        auto l1 = p.add_literal(literal(arg_outs.at(1).get_shape(), arg_outs.at(1).data()));
+        auto lens = l0->get_shape().lens();
+        auto type = l0->get_shape().type();
+        auto cond = ins->inputs().front();
+        auto icond = p.insert_instruction(make_op("convert", {{"target_type", type}}), cond);
+        auto mcond = p.insert_instruction(make_op("multibroadcast", {"output_lens", lens}), icond);        
+
+        auto l01 = p.insert_intruction(make_op("sub"), l0, l1);
+        auto lcond = p.insert_instruction(make_op("mul"), l01, mcond);
+        auto r = p.insert_instruction(make_op("add"), lcond, l1);
+        p.replace_instruction(ins, result);
     }
 }
 

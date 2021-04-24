4a12f4752c80dc3275c85e9f5ba5ec73af7c729f
Shucai Xiao <Shucai.Xiao@amd.com>
2021-04-24 13:40:55 -0500

    clang format

--- src/targets/gpu/gemm_impl.cpp
+++ src/targets/gpu/gemm_impl.cpp
@@ -66,8 +66,8 @@ void gemm_impl(context& ctx,
     auto compute_type = output_type;
 
 #if ROCBLAS_VERSION_MAJOR >= 2 && ROCBLAS_VERSION_MINOR >= 38
-    rocblas_gemm_flags flag = int8_x4_format ? 
-        rocblas_gemm_flags_pack_int8x4 : rocblas_gemm_flags_none;
+    rocblas_gemm_flags flag =
+        int8_x4_format ? rocblas_gemm_flags_pack_int8x4 : rocblas_gemm_flags_none;
 #else
     (void)int8_x4_format;
     rocblas_gemm_flags flag = rocblas_gemm_flags_none;

5822868f46b539da3d0202fffd111a97a5db2768
Paul <pfultz2@yahoo.com>
2021-04-28 22:06:22 -0500

    Reduce types generated for hip kernels

--- src/targets/gpu/device/contiguous.cpp
+++ src/targets/gpu/device/contiguous.cpp
@@ -9,7 +9,13 @@ namespace device {
 
 void contiguous(hipStream_t stream, const argument& result, const argument& arg)
 {
-    nary(stream, result, arg)([](auto x) __device__ { return x; });
+    shape s{result.get_shape().type(), result.get_shape().lens()};
+    visit_all(result, arg)([&](auto output_v, auto input_v) {
+        hip_visit_views(output_v, input_v, s)([&](auto output, auto input, auto standard_shape) {
+            mi_gs_launch(stream,
+                     standard_shape)([=](auto idx) __device__ { output[idx] = input[idx]; });
+                 });
+    });
 }
 
 } // namespace device

e786a6f559afa806ede5c8ee41fa714c15184188
Paul <pfultz2@yahoo.com>
2021-04-30 15:50:27 -0500

    Fix onnx tests

--- src/targets/gpu/device/contiguous.cpp
+++ src/targets/gpu/device/contiguous.cpp
@@ -7,7 +7,7 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
-void contiguous(hipStream_t stream, const argument& result, const argument& arg)
+void contiguous_nonstandard(hipStream_t stream, const argument& result, const argument& arg)
 {
     shape s{result.get_shape().type(), result.get_shape().lens()};
     visit_all(result, arg)([&](auto output_v, auto input_v) {
@@ -16,6 +16,24 @@ void contiguous(hipStream_t stream, const argument& result, const argument& arg)
                          standard_shape)([=](auto idx) __device__ { output[idx] = input[idx]; });
         });
     });
+}
+
+void contiguous_packed(hipStream_t stream, const argument& result, const argument& arg)
+{
+    index_int nelements = result.get_shape().elements();
+    visit_all(result, arg)([&](auto output_v, auto input_v) {
+        const auto* input = device_cast(input_v.data());
+        auto* output      = device_cast(output_v.data());
+        gs_launch(stream, nelements)([=](auto i) __device__ { output[i] = input[i]; });
+    });
+}
+
+void contiguous(hipStream_t stream, const argument& result, const argument& arg)
+{
+    if (result.get_shape() == arg.get_shape() and result.get_shape().packed())
+        contiguous_packed(stream, result, arg);
+    else
+        contiguous_nonstandard(stream, result, arg);
 }
 
 } // namespace device

ba0f1157e2b1448dff9e7c84ab53ee86b04300b8
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-08 08:49:31 -0500

    refine unit tests

--- test/verify/batch_quant_dot_3.cpp
+++ test/verify/batch_quant_dot_3.cpp
@@ -12,13 +12,11 @@ struct batch_quant_dot_3 : verify_program<batch_quant_dot_3>
         auto* mm = p.get_main_module();
         migraphx::shape m1_shape{migraphx::shape::int8_type, {3, 2, 2, 6}};
         migraphx::shape m2_shape{migraphx::shape::int8_type, {3, 2, 6, 7}};
-        migraphx::shape m3_shape{migraphx::shape::int32_type, {3, 2, 2, 7}};
 
         auto l1 = mm->add_parameter("a", m1_shape);
         auto l2 = mm->add_parameter("b", m2_shape);
-        auto l3 = mm->add_parameter("c", m3_shape);
         mm->add_instruction(
-            migraphx::make_op("quant_dot", {{"alpha", 1}, {"beta", 3}}), l1, l2, l3);
+            migraphx::make_op("quant_dot", {{"alpha", 1}, {"beta", 3}}), l1, l2);
         return p;
     }
 };

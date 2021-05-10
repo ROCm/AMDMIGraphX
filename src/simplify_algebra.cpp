098bd14adebfec76168b6e61efd22b624e326969
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-09 23:28:43 -0500

    clang format

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -685,12 +685,11 @@ struct find_add_convs
                     auto n = compute_stride_factor(b_op, a_op);
                     if(n == 0)
                         return;
-                    new_op  = a_op;
-		    if(not b_input->get_shape().standard())
-		    {
-                        b_input =
-                            p.insert_instruction(ins, make_op("contiguous"), b_input);
-		    }
+                    new_op = a_op;
+                    if(not b_input->get_shape().standard())
+                    {
+                        b_input = p.insert_instruction(ins, make_op("contiguous"), b_input);
+                    }
                     b_input = p.insert_instruction(
                         ins,
                         make_op(
@@ -703,12 +702,11 @@ struct find_add_convs
                     auto n = compute_stride_factor(a_op, b_op);
                     if(n == 0)
                         return;
-                    new_op  = b_op;
-		    if(not a_input->get_shape().standard())
-		    {
-                        a_input =
-                            p.insert_instruction(ins, make_op("contiguous"), a_input);
-		    }
+                    new_op = b_op;
+                    if(not a_input->get_shape().standard())
+                    {
+                        a_input = p.insert_instruction(ins, make_op("contiguous"), a_input);
+                    }
                     a_input = p.insert_instruction(
                         ins,
                         make_op(

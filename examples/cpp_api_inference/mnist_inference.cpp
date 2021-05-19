e64dd3a1ff83255b7bd16e56f49fea825b6ff34a
Cagri Eryilmaz <ceryilma@amd.com>
2021-05-19 19:01:03 +0000

    changes for CPP mnist example for cpu bug

--- examples/cpp_api_inference/mnist_inference.cpp
+++ examples/cpp_api_inference/mnist_inference.cpp
@@ -124,7 +124,12 @@ int main(int argc, char** argv)
     auto param_shapes = prog.get_parameter_shapes();
     for(auto&& name : param_shapes.names())
     {
-        prog_params.add(name, migraphx::argument(param_shapes[name], digit.data()));
+        if (std::string(name).find("Input3") != std::string::npos){
+            prog_params.add(name, migraphx::argument(param_shapes[name], digit.data()));
+        }
+        else if (std::string(name).find("Input3") == std::string::npos){
+            prog_params.add(name, migraphx::argument::generate(param_shapes[name]));
+        }
     }
 
     std::cout << "Model evaluating input..." << std::endl;

6957f0221102f86d1d69acd3454e6b08ed7baf1a
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-23 00:36:34 -0500

    clang format

--- test/verify/test_step.cpp
+++ test/verify/test_step.cpp
@@ -13,10 +13,10 @@ struct test_step : verify_program<test_step>
         migraphx::shape s1{migraphx::shape::float_type, {2, 1, 4, 6}};
         auto l0 = mm->add_parameter("x", s1);
         auto tl = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {0, 2, 3, 1}}}), l0);
-        auto r = mm->add_instruction(migraphx::make_op("step", {{"axes", {0, 1, 2}}, {"steps", {2, 2, 3}}}), tl);
+        auto r  = mm->add_instruction(
+            migraphx::make_op("step", {{"axes", {0, 1, 2}}, {"steps", {2, 2, 3}}}), tl);
         mm->add_return({r});
 
         return p;
     }
 };
-

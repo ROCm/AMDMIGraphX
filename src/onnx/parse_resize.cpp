8d3553c68d1ff5f8a69ed89a419c1479443345fb
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-24 21:17:20 -0500

    clang format

--- src/onnx/parse_resize.cpp
+++ src/onnx/parse_resize.cpp
@@ -187,23 +187,21 @@ struct parse_resize : op_parser<parse_resize>
             if(type == shape::int64_type)
             {
                 auto arg_out_s = arg->eval();
-                check_arg_empty(arg_out_s,
-                                "PARSE_RESIZE: dynamic output size is not supported!");
+                check_arg_empty(arg_out_s, "PARSE_RESIZE: dynamic output size is not supported!");
                 arg_out_s.visit([&](auto ol) { out_lens.assign(ol.begin(), ol.end()); });
 
                 if(out_lens.size() != in_lens.size())
                 {
-                    MIGRAPHX_THROW(
-                        "PARSE_RESIZE: specified output size does not match input size");
+                    MIGRAPHX_THROW("PARSE_RESIZE: specified output size does not match input size");
                 }
 
                 // compute the scale
                 vec_scale.resize(in_lens.size());
                 std::transform(in_lens.begin(),
-                                in_lens.end(),
-                                out_lens.begin(),
-                                vec_scale.begin(),
-                                [](auto iss, auto oss) { return 1.0 * oss / iss; });
+                               in_lens.end(),
+                               out_lens.begin(),
+                               vec_scale.begin(),
+                               [](auto iss, auto oss) { return 1.0 * oss / iss; });
             }
             else
             {
@@ -221,12 +219,12 @@ struct parse_resize : op_parser<parse_resize>
                     }
 
                     std::transform(in_lens.begin(),
-                                    in_lens.end(),
-                                    vec_scale.begin(),
-                                    out_lens.begin(),
-                                    [&](auto idx, auto scale) {
-                                        return static_cast<std::size_t>(idx * scale);
-                                    });
+                                   in_lens.end(),
+                                   vec_scale.begin(),
+                                   out_lens.begin(),
+                                   [&](auto idx, auto scale) {
+                                       return static_cast<std::size_t>(idx * scale);
+                                   });
                 }
             }
         }

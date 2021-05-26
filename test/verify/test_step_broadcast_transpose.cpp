6f93b80ac231ce3d80da6ebca4ebca33efa3ddb5
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-26 13:51:52 -0500

    rename two unit tests

--- test/verify/test_step_broadcast_transpose.cpp
+++ test/verify/test_step_broadcast_transpose.cpp
@@ -4,7 +4,7 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct test_step_brcst_trans : verify_program<test_step_brcst_trans>
+struct test_step_broadcast_transpose : verify_program<test_step_broadcast_transpose>
 {
     migraphx::program create_program() const
     {
--- test/verify/test_step_transpose.cpp
+++ test/verify/test_step_transpose.cpp
@@ -4,7 +4,7 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct test_step_trans : verify_program<test_step_trans>
+struct test_step_transpose : verify_program<test_step_transpose>
 {
     migraphx::program create_program() const
     {

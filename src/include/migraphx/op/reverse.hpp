feeb664df15792ca6f5ac97e26c674f5c8fc157f
Cagri Eryilmaz <ceryilma@amd.com>
2021-05-27 20:41:34 +0000

    cleanup

--- src/include/migraphx/op/reverse.hpp
+++ src/include/migraphx/op/reverse.hpp
@@ -50,43 +50,19 @@ struct reverse
 
     argument compute(const shape&, std::vector<argument> args) const
     {
-        auto input  = args[0].get_shape(); //float_type, {1, 16}, {16, 1}
+        auto input  = args[0].get_shape(); //float_type, {2, 16}, {16, 1}
 
-        std::cout << input << std::endl;
-        std::cout << input.type() << std::endl;
-
-        //auto data = args[0].eval().get<int32_t>.to_vector();
-        //auto data2 = args[0].eval().to_vector();
-
-        //auto step_arg = args[0].data();
-        std::vector<int> data;
-        args[0].visit([&](auto s) { data.assign(s.begin(), s.end()); });
-
-        for(auto k: data){
-            std::cout << k << "-";
-        }
+        std::vector<std::size_t> data; 
+        args[0].visit([&](auto s) { data.assign(s.begin(), s.end()); }); 
 
         const std::vector<std::size_t>& lens = input.lens();
-        const std::vector<std::size_t>& strides = input.strides();
-
-
-        std::cout << "CG_lens:" << std::endl;
-        for(auto i: lens)
-            std::cout << i << " ";
-        std::cout << std::endl;
-
-        std::cout << "CG_stride:" << std::endl;
-        for(auto i: strides){
-             std::cout << i << " ";
-        }
-
+        //const std::vector<std::size_t>& strides = input.strides();
 
         if (axis[0] == 0)
         {
             for(std::size_t i = 0; i < (lens[0] / 2); i++)
             {
                 //TODO
-                std::cout << "CG_in1" << std::endl;
                 continue;
             }
         }
@@ -94,19 +70,12 @@ struct reverse
         {
             for (std::size_t t = 0; t < lens[0]; t++) //4096
             {
-                std::cout << "CG_in2" << std::endl;
-                //std::reverse( args[0].data() + (t * lens[1]), args[0].data() + ((t+1) * lens[1]) ); //
                 std::reverse( data.begin() + (t * lens[1]), data.begin() + ((t+1) * lens[1]) ); //
             }
         }
-        std::cout << "done" << std::endl;
-        //std::vector<int> temp;
-        //args[0].visit([&](auto s) { temp.assign(s.begin(), s.end()); });
-
-        for(auto k: data){
-            std::cout << k << ",";
+        else {
+            MIGRAPHX_THROW("reverse op can only have axis=1 or axis=0");
         }
-        std::cout << "done2" << std::endl;
 
         argument result{input};
         

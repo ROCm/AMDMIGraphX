85ab8b382704a192851c420d578880e16324a457
Paul <pfultz2@yahoo.com>
2021-05-28 21:18:32 -0500

    Seperate time_op

--- src/targets/gpu/driver/compile_pointwise.cpp
+++ src/targets/gpu/driver/compile_pointwise.cpp
@@ -1,43 +1,12 @@
 #include <migraphx/gpu/driver/action.hpp>
+#include <migraphx/gpu/driver/perf.hpp>
 #include <migraphx/gpu/compile_pointwise.hpp>
 #include <migraphx/gpu/context.hpp>
-
-#include <migraphx/context.hpp>
-#include <migraphx/generate.hpp>
-#include <migraphx/time.hpp>
-#include <migraphx/gpu/hip.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace driver {
-
-std::vector<argument> generate_arguments(const std::vector<shape>& shapes, unsigned long seed = 0)
-{
-    std::vector<argument> args;
-    std::transform(shapes.begin(), shapes.end(), std::back_inserter(args), [&](auto& s) {
-        return to_gpu(generate_argument(s, seed++));
-    });
-    return args;
-}
-
-using milliseconds = std::chrono::duration<double, std::milli>;
-double time_op(context& ctx, operation op, const std::vector<shape>& inputs, int n = 100)
-{
-    migraphx::context gctx = ctx;
-    auto output            = op.compute_shape(inputs);
-    op.finalize(gctx, output, inputs);
-    auto args = generate_arguments(inputs);
-    auto run  = [&] {
-        op.compute(gctx, output, args);
-        gctx.finish();
-    };
-    run();
-    auto r   = range(n);
-    double t = std::accumulate(
-        r.begin(), r.end(), double{0.0}, [&](auto x, auto) { return x + time<milliseconds>(run); });
-    return t / n;
-}
 
 struct compile_pointwise : action<compile_pointwise>
 {

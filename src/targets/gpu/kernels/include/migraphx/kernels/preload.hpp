234623b9f771b32ba8c47707ff66122e23c11dc4
Paul <pfultz2@yahoo.com>
2021-06-02 15:51:47 -0500

    Formatting

--- src/targets/gpu/kernels/include/migraphx/kernels/preload.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/preload.hpp
@@ -18,8 +18,8 @@ template <class T, class... Shapes>
 constexpr auto traverse_preload(Shapes... ss)
 {
     return [=](auto f, auto... g) {
-        index_int offset         = 0;
-        auto each                = [&](auto x) {
+        index_int offset = 0;
+        auto each        = [&](auto x) {
             constexpr auto s    = decltype(x.get_shape()){};
             constexpr auto size = decltype(index_constant<s.element_space()>{}){};
             if constexpr(not s.broadcasted())
@@ -87,8 +87,8 @@ struct remove_vec<vec<T, N>>
 template <class T, class... Ts>
 __device__ auto preload(index idx, Ts... xs)
 {
-    using type          = typename remove_vec<T>::type;
-    constexpr auto size = compute_preload_size<type, decltype(xs.get_shape())...>();
+    using type               = typename remove_vec<T>::type;
+    constexpr auto size      = compute_preload_size<type, decltype(xs.get_shape())...>();
     const index_int max_size = 512 * sizeof(type);
     return [=](auto f) {
         if constexpr(size > 0 and size < max_size)

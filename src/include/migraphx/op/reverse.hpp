4533e758a4e377b2aece90469ec92845b6c4dead
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-02 16:01:56 -0500

    fixed some issues

--- src/include/migraphx/op/reverse.hpp
+++ src/include/migraphx/op/reverse.hpp
@@ -9,6 +9,7 @@
 #include <migraphx/argument.hpp>
 #include <migraphx/par_for.hpp>
 #include <migraphx/op/normalize_attribute.hpp>
+#include <migraphx/shape_for_each.hpp>
 #include <migraphx/argument.hpp>
 #include <migraphx/value.hpp>
 
@@ -45,45 +46,57 @@ struct reverse
         return shape{type,lens};
     }
 
-    argument compute(const shape&, std::vector<argument> args) const
+    argument compute(const shape& s, std::vector<argument> args) const
     {
-        auto input  = args[0].get_shape(); //float_type, {2, 16}, {16, 1}
-        
-        std::vector<std::size_t> data; 
-        args[0].visit([&](auto s) { data.assign(s.begin(), s.end()); });
-
-        const std::vector<std::size_t>& lens = input.lens();
-
-        if (axis == 0)
-        {
-            for(std::size_t k = 0; k < lens[0]/2; k++) //4
-            {
-                for(std::size_t i = 0; i < lens[1]; i++) //16
-                {
-                    std::iter_swap( data.begin() + i + (k * lens[1]), data.begin() + i + ((lens[0]-k-1)*lens[1]) );
-                }
-            }
-        }
-        else if (axis == 1) 
-        {
-            for (std::size_t t = 0; t < lens[0]; t++)
-            {
-                std::reverse( data.begin() + (t * lens[1]), data.begin() + ((t+1) * lens[1]) ); //
-            }
-        }
-        else {
-            MIGRAPHX_THROW("reverse op can only have axis=1 or axis=0");
-        }
-
-        argument result{input};
-        
-        result.visit([&](auto output) {
-            par_for(input.elements(), [&](auto i) {
-                output[i]     = data[i];
+        argument result{s};
+        auto dim_size = s.lens()[axis];
+        visit_all(result, args.front())([&](auto output, auto input){
+            shape_for_each(s, [&](const auto& out_idx){
+                auto in_idx = out_idx;
+                in_idx[axis] = dim_size - 1 - out_idx[axis];
+                output[s.index(out_idx)] = input[s.index(in_idx)];
             });
         });
 
         return result;
+
+        // auto input  = args[0].get_shape(); //float_type, {2, 16}, {16, 1}
+        
+        // std::vector<std::size_t> data; 
+        // args[0].visit([&](auto s) { data.assign(s.begin(), s.end()); });
+
+        // const std::vector<std::size_t>& lens = input.lens();
+
+        // if (axis == 0)
+        // {
+        //     for(std::size_t k = 0; k < lens[0]/2; k++) //4
+        //     {
+        //         for(std::size_t i = 0; i < lens[1]; i++) //16
+        //         {
+        //             std::iter_swap( data.begin() + i + (k * lens[1]), data.begin() + i + ((lens[0]-k-1)*lens[1]) );
+        //         }
+        //     }
+        // }
+        // else if (axis == 1) 
+        // {
+        //     for (std::size_t t = 0; t < lens[0]; t++)
+        //     {
+        //         std::reverse( data.begin() + (t * lens[1]), data.begin() + ((t+1) * lens[1]) ); //
+        //     }
+        // }
+        // else {
+        //     MIGRAPHX_THROW("reverse op can only have axis=1 or axis=0");
+        // }
+
+        // argument result{input};
+        
+        // result.visit([&](auto output) {
+        //     par_for(input.elements(), [&](auto i) {
+        //         output[i]     = data[i];
+        //     });
+        // });
+
+        // return result;
     }
 };
 
--- src/targets/gpu/device/reverse.cpp
+++ src/targets/gpu/device/reverse.cpp
@@ -12,25 +12,41 @@ namespace device {
 
 argument reverse(hipStream_t stream, argument result, argument arg1, int64_t axis)
 {
-    const auto& input_shape = arg1.get_shape(); 
-    auto lens               = input_shape.lens();
-
-    auto axis_dim_size      = lens[axis];
-    shape out_comp_shape{result.get_shape().type(), lens};
-    std::size_t nelements = result.get_shape().elements();
-
-    visit_all(result, arg1)([&](auto output, auto input_val) {
-        hip_visit_views(input_val, out_comp_shape)([&](auto input, auto out_comp) {
-            auto* output_ptr        = device_cast(output.data());
-            gs_launch(stream, nelements, 256)([=](auto i) __device__ {
-                auto idx      = out_comp.multi(i);
-                auto in_index = output_ptr[idx[axis]];
-                in_index      = (in_index < 0) ? axis_dim_size - in_index : in_index;
-                idx[axis]     = in_index;
-                output_ptr[i] = input[idx];
-            });
+    auto s = arg1.get_shape();
+    auto axis_dim_size      = s.lens()[axis];
+    std::size_t nelements = s.elements();
+    visit_all(result, arg1)([&](auto output1, auto input1) {
+    hip_visit_views(output1, input1, s)([&](auto output, auto input, auto hs) {
+        // const auto* in_ptr = device_cast(input.data());
+        // auto* out_ptr = device_cast(output.data());
+        gs_launch(stream, nelements, 256)([=](auto i) __device__ {
+            auto idx      = hs.multi(i);
+            auto in_idx = idx;
+            in_idx[axis] = axis_dim_size - 1 - idx[axis];
+            output[idx] = input[in_idx];
         });
     });
+    });
+
+    // const auto& input_shape = arg1.get_shape(); 
+    // auto lens               = input_shape.lens();
+
+    // auto axis_dim_size      = lens[axis];
+    // shape out_comp_shape{result.get_shape().type(), lens};
+    // std::size_t nelements = result.get_shape().elements();
+
+    // visit_all(result, arg1)([&](auto output, auto input_val) {
+    //     hip_visit_views(input_val, out_comp_shape)([&](auto input, auto out_comp) {
+    //         auto* output_ptr        = device_cast(output.data());
+    //         gs_launch(stream, nelements, 256)([=](auto i) __device__ {
+    //             auto idx      = out_comp.multi(i);
+    //             auto in_index = output_ptr[idx[axis]];
+    //             in_index      = (in_index < 0) ? axis_dim_size - in_index : in_index;
+    //             idx[axis]     = in_index;
+    //             output_ptr[i] = input[idx];
+    //         });
+    //     });
+    // });
     
 
     return result;

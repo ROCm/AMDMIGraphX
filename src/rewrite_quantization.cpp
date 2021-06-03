0e44516775cc85453964797e9fcd8fcb6f384f98
turneram <alturner@amd.com>
2021-06-03 18:52:03 -0500

    Formatting

--- src/rewrite_quantization.cpp
+++ src/rewrite_quantization.cpp
@@ -55,7 +55,9 @@ void rewrite_quantization::apply(module& m) const
             {
                 auto tuned_axis = tune_axis(n_dim, axis, ins->name());
                 divisor         = m.insert_instruction(
-                    ins, make_op("broadcast", {{"axis", tuned_axis}, {"dims", input_lens}}), y_scale);
+                    ins,
+                    make_op("broadcast", {{"axis", tuned_axis}, {"dims", input_lens}}),
+                    y_scale);
             }
             else
             {
@@ -121,24 +123,28 @@ void rewrite_quantization::apply(module& m) const
             instruction_ref zero_point_int32;
             if(x_zero_point->get_shape().elements() != 1)
             {
-                auto tuned_axis = tune_axis(n_dim, axis, ins->name());
+                auto tuned_axis  = tune_axis(n_dim, axis, ins->name());
                 zero_point_int32 = m.insert_instruction(
                     ins,
                     make_op("broadcast", {{"axis", tuned_axis}, {"dims", input_lens}}),
                     x_zero_point);
-                zero_point_int32 = m.insert_instruction(
-                    ins, make_op("convert", {{"target_type", shape::int32_type}}), zero_point_int32);
+                zero_point_int32 =
+                    m.insert_instruction(ins,
+                                         make_op("convert", {{"target_type", shape::int32_type}}),
+                                         zero_point_int32);
             }
             else
             {
                 zero_point_int32 = m.insert_instruction(
                     ins, make_op("convert", {{"target_type", shape::int32_type}}), x_zero_point);
-                zero_point_int32 = m.insert_instruction(
-                    ins, make_op("multibroadcast", {{"output_lens", input_lens}}), zero_point_int32);
+                zero_point_int32 =
+                    m.insert_instruction(ins,
+                                         make_op("multibroadcast", {{"output_lens", input_lens}}),
+                                         zero_point_int32);
             }
 
-            auto sub_zp_int32 =
-                m.insert_instruction(ins, make_op("convert", {{"target_type", shape::int32_type}}), x);
+            auto sub_zp_int32 = m.insert_instruction(
+                ins, make_op("convert", {{"target_type", shape::int32_type}}), x);
             auto sub = m.insert_instruction(ins, make_op("sub"), sub_zp_int32, zero_point_int32);
             auto dequant_input = m.insert_instruction(
                 ins, make_op("convert", {{"target_type", shape::float_type}}), sub);
@@ -148,7 +154,9 @@ void rewrite_quantization::apply(module& m) const
             {
                 auto tuned_axis = tune_axis(n_dim, axis, ins->name());
                 multiplier      = m.insert_instruction(
-                    ins, make_op("broadcast", {{"axis", tuned_axis}, {"dims", input_lens}}), x_scale);
+                    ins,
+                    make_op("broadcast", {{"axis", tuned_axis}, {"dims", input_lens}}),
+                    x_scale);
             }
             else
             {

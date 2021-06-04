09a01d34ec6bb32c5eb0188b679b5b3c91af8b62
Cagri Eryilmaz <ceryilma@amd.com>
2021-06-04 16:55:29 +0000

    remove local size, back to default

--- src/targets/gpu/device/reverse.cpp
+++ src/targets/gpu/device/reverse.cpp
@@ -17,7 +17,7 @@ argument reverse(hipStream_t stream, argument result, argument arg1, int64_t axi
     std::size_t nelements = s.elements();
     visit_all(result, arg1)([&](auto output1, auto input1) {
         hip_visit_views(output1, input1, s)([&](auto output, auto input, auto hs) {
-            gs_launch(stream, nelements, 256)([=](auto i) __device__ {
+            gs_launch(stream, nelements)([=](auto i) __device__ {
                 auto idx     = hs.multi(i);
                 auto in_idx  = idx;
                 in_idx[axis] = axis_dim_size - 1 - idx[axis];

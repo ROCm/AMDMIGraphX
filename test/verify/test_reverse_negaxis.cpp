c9784de75c263cc16f2a1a4b7260836005983543
Cagri Eryilmaz <ceryilma@amd.com>
2021-06-07 18:23:41 +0000

    more tests

--- test/verify/test_reverse_negaxis.cpp
+++ test/verify/test_reverse_negaxis.cpp
@@ -3,7 +3,7 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct test_reverse : verify_program<test_reverse>
+struct test_reverse_negaxis : verify_program<test_reverse_negaxis>
 {
     migraphx::program create_program() const
     {
@@ -11,7 +11,7 @@ struct test_reverse : verify_program<test_reverse>
         auto* mm = p.get_main_module();
         migraphx::shape s{migraphx::shape::float_type, {4, 16}};
         auto a0  = mm->add_parameter("data", s);
-        std::vector<int64_t> axis = {0};
+        std::vector<int64_t> axis = {-1};
         mm->add_instruction(migraphx::make_op("reverse", {{"axes", axis}}), a0);
         return p;
     }

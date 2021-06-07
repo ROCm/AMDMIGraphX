7825159cefd6a940d9b1c611720f3997cb502d3c
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-07 17:18:17 -0500

    clang format

--- src/targets/gpu/device/reverse.cpp
+++ src/targets/gpu/device/reverse.cpp
@@ -22,15 +22,15 @@ reverse(hipStream_t stream, argument result, argument arg1, const std::vector<in
     visit_all(result, arg1)([&](auto output1, auto input1) {
         hip_visit_views(output1, input1, s)([&](auto output, auto input, auto hs) {
             hip_visit_views(sa)([&](auto daxes) {
-            auto lens = hs.lens;
-            gs_launch(stream, nelements)([=](auto i) __device__ {
-                auto idx    = hs.multi(i);
-                auto in_idx = idx;
-                for(auto axis : daxes.lens)
-                    in_idx[axis] = lens[axis] - 1 - idx[axis];
-                output[idx] = input[in_idx];
+                auto lens = hs.lens;
+                gs_launch(stream, nelements)([=](auto i) __device__ {
+                    auto idx    = hs.multi(i);
+                    auto in_idx = idx;
+                    for(auto axis : daxes.lens)
+                        in_idx[axis] = lens[axis] - 1 - idx[axis];
+                    output[idx] = input[in_idx];
+                });
             });
-        });
         });
     });
 

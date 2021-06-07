20eac22b399cf3904d5f9a934fee643896754992
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-07 16:05:25 -0500

    fix a bug for the reverse device function

--- src/targets/gpu/device/reverse.cpp
+++ src/targets/gpu/device/reverse.cpp
@@ -14,20 +14,22 @@ argument
 reverse(hipStream_t stream, argument result, argument arg1, const std::vector<int64_t>& axes)
 {
     auto s                = arg1.get_shape();
-    auto lens             = s.lens();
+    // auto lens             = s.lens();
+    std::vector<std::size_t> axis_len(axes.begin(), axes.end());
+    shape sa{shape::float_type, axis_len};
     std::size_t nelements = s.elements();
     visit_all(result, arg1)([&](auto output1, auto input1) {
-        hip_visit_views(output1, input1, s)([&](auto output, auto input, auto hs) {
-            for(auto axis : axes)
-            {
-                auto dim_size = lens[axis];
-                gs_launch(stream, nelements)([=](auto i) __device__ {
-                    auto idx     = hs.multi(i);
-                    auto in_idx  = idx;
-                    in_idx[axis] = dim_size - 1 - idx[axis];
-                    output[idx]  = input[in_idx];
-                });
-            }
+        hip_visit_views(output1, input1, s, sa)([&](auto output, auto input, auto hs, auto daxes) {
+            auto lens = hs.lens;
+            gs_launch(stream, nelements)([=](auto i) __device__ {
+                auto idx     = hs.multi(i);
+                auto in_idx  = idx;
+                for (auto axis : daxes.lens)
+                {
+                    in_idx[axis] = lens[axis] - 1 - idx[axis];
+                }
+                output[idx]  = input[in_idx];
+            });
         });
     });
 

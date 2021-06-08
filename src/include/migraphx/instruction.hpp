e69e6a625202b5003e273f56a9abaf4e0bbfa59b
Paul <pfultz2@yahoo.com>
2021-06-08 14:27:10 -0500

    Set correct bool type

--- src/include/migraphx/instruction.hpp
+++ src/include/migraphx/instruction.hpp
@@ -168,7 +168,7 @@ template <>
 struct equal_to<migraphx::instruction_ref>
 {
     using argument_type = migraphx::instruction_ref;
-    using result_type   = std::size_t;
+    using result_type   = bool;
     result_type operator()(const migraphx::instruction_ref& x,
                            const migraphx::instruction_ref& y) const noexcept
     {

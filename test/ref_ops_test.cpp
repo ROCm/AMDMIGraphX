9c54fc4f1915df3b91ae54de314720ec1e32cda2
Cagri Eryilmaz <63118943+cagery@users.noreply.github.com>
2021-06-08 12:38:47 -0500

    Reverse Op (#846)
    * init reverseOp branch: ref op + ref test. WIP
    
    * first passing basic test
    
    * cleanup
    
    * additional axis implementation
    
    * additional test
    
    * ref op implementation vec to int for axis
    
    * ref op test change for axis
    
    * initial gpu files and test
    
    * updates to implementation and test
    
    * fixed some issues
    
    * clang format
    
    * cleanup
    
    * formatting
    
    * removing comments
    
    * remove local size, back to default
    
    * update tests: replace with std functions
    
    * multiple axis for reverse op
    
    * fix a build error
    
    * clang format
    
    * more tests
    
    * fix a bug for the reverse device function
    
    * clang format
    
    * fix a bug
    
    * clang format
    
    * ref test updates, multiaxis
    
    * formatting
    
    Co-authored-by: Shucai Xiao <Shucai.Xiao@amd.com>
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- test/ref_ops_test.cpp
+++ test/ref_ops_test.cpp
@@ -3560,6 +3560,66 @@ TEST_CASE(reshape_test)
     }
 }
 
+TEST_CASE(reverse_test_axis0)
+{
+    migraphx::shape in_shape{migraphx::shape::float_type, {2, 16}};
+    std::vector<float> data(32);
+    std::iota(data.begin(), data.end(), 1);
+    migraphx::program p;
+    auto* mm              = p.get_main_module();
+    auto l                = mm->add_literal(migraphx::literal{in_shape, data});
+    std::vector<int> axes = {0};
+    mm->add_instruction(migraphx::make_op("reverse", {{"axes", axes}}), l);
+    p.compile(migraphx::ref::target{});
+    auto result = p.eval({}).back();
+    std::vector<float> results_vector;
+    result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
+    std::vector<float> target_data = data;
+    std::swap_ranges(target_data.begin(), target_data.begin() + 16, target_data.begin() + 16);
+    EXPECT(migraphx::verify_range(results_vector, target_data));
+}
+
+TEST_CASE(reverse_test_axis1)
+{
+    migraphx::shape in_shape{migraphx::shape::float_type, {2, 16}};
+    std::vector<float> data(32);
+    std::iota(data.begin(), data.end(), 1);
+    migraphx::program p;
+    auto* mm              = p.get_main_module();
+    auto l                = mm->add_literal(migraphx::literal{in_shape, data});
+    std::vector<int> axes = {1};
+    mm->add_instruction(migraphx::make_op("reverse", {{"axes", axes}}), l);
+    p.compile(migraphx::ref::target{});
+    auto result = p.eval({}).back();
+    std::vector<float> results_vector;
+    result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
+    std::vector<float> target_data = data;
+    std::reverse(target_data.begin(), target_data.begin() + 16);
+    std::reverse(target_data.end() - 16, target_data.end());
+    EXPECT(migraphx::verify_range(results_vector, target_data));
+}
+
+TEST_CASE(reverse_test_axis10)
+{
+    migraphx::shape in_shape{migraphx::shape::float_type, {2, 16}};
+    std::vector<float> data(32);
+    std::iota(data.begin(), data.end(), 1);
+    migraphx::program p;
+    auto* mm              = p.get_main_module();
+    auto l                = mm->add_literal(migraphx::literal{in_shape, data});
+    std::vector<int> axes = {1, 0};
+    mm->add_instruction(migraphx::make_op("reverse", {{"axes", axes}}), l);
+    p.compile(migraphx::ref::target{});
+    auto result = p.eval({}).back();
+    std::vector<float> results_vector;
+    result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
+    std::vector<float> target_data = data;
+    std::reverse(target_data.begin(), target_data.begin() + 16);
+    std::reverse(target_data.end() - 16, target_data.end());
+    std::swap_ranges(target_data.begin(), target_data.begin() + 16, target_data.begin() + 16);
+    EXPECT(migraphx::verify_range(results_vector, target_data));
+}
+
 TEST_CASE(round_test)
 {
     migraphx::program p;

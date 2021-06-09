98486807195d44910a96bb84e486216f6b0bfe25
Paul Fultz II <pfultz2@yahoo.com>
2021-06-09 14:56:32 -0500

    Fixes when using C++ debug assertions (#837)
    * Enable libstdc++ debug mode
    
    * Add is_end function
    
    * Compare addresses in a map or set
    
    * Formatting
    
    * Check end
    
    * Fix comparision of instruction_ref
    
    * Formatting
    
    * Some more iterator fixes
    
    * Formatting
    
    * Fix assert
    
    * Fix invalid iterators
    
    * Fix debug print in program
    
    * Remove debug flag for now
    
    * Set correct bool type
    
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- src/program.cpp
+++ src/program.cpp
@@ -9,6 +9,7 @@
 #include <migraphx/pass_manager.hpp>
 #include <migraphx/register_target.hpp>
 #include <migraphx/iterator_for.hpp>
+#include <migraphx/iterator.hpp>
 #include <migraphx/algorithm.hpp>
 #include <migraphx/output_iterator.hpp>
 #include <migraphx/make_op.hpp>
@@ -598,7 +599,7 @@ void program::debug_print(instruction_ref ins) const
 {
     std::unordered_map<instruction_ref, std::string> names;
     if(std::any_of(this->impl->modules.begin(), this->impl->modules.end(), [&](const auto& pp) {
-           return (pp.second.end() == ins);
+           return is_end(pp.second.end(), ins);
        }))
     {
         std::cout << "End instruction" << std::endl;

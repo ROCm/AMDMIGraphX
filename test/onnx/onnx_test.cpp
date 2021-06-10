a5386c69dd5c79fcc60eec5292004baa42845992
Cagri Eryilmaz <63118943+cagery@users.noreply.github.com>
2021-06-10 15:05:14 -0500

    Update parse slice (#847)
    * init reverseOp branch: ref op + ref test. WIP
    
    * first passing basic test
    
    * cleanup
    
    * additional axis implementation
    
    * additional test
    
    * ref op implementation vec to int for axis
    
    * ref op test change for axis
    
    * initial gpu files and test
    
    * updates to implementation and test
    
    * fixed some issues
    
    * clang format
    
    * cleanup
    
    * formatting
    
    * removing comments
    
    * changes to parse_slice.cpp debug copy
    
    * cleanup + additional axis  for reverse instruction
    
    * formatting
    
    * remove local size, back to default
    
    * update tests: replace with std functions
    
    * multiple axis for reverse op
    
    * fix a build error
    
    * clang format
    
    * changes to parse_slice.cpp debug copy
    
    * cleanup + additional axis  for reverse instruction
    
    * formatting
    
    * axes update to parse slice
    
    * typo
    
    * more tests
    
    * fix a bug for the reverse device function
    
    * clang format
    
    * fix a bug
    
    * clang format
    
    * ref test updates, multiaxis
    
    * formatting
    
    * formatting, cleanup bool op
    
    * casting for tidy warning
    
    * tidy fix
    
    * remove bool, add steps, check only negative axis
    
    * clang-format
    
    * step op for parse slice
    
    * cleanup & format
    
    * missing axis for logsoftmax_nonstd_input_test
    
    * updated onnx file for logsoftmax_nonstd_input_test
    
    * updates to parse slice. tests for slice+reverse, slice+step+reverse
    
    * removing tests for slice+step+reverse as step requires normalization, will move it to other branch. removed related lines and tests
    
    * duplicate test removal
    
    * some refinement of the code
    
    * clang format
    
    * undefined behavior fix
    
    * undef behavior v2
    
    * formatting
    
    * formatting & updates
    
    * change to parse slice
    
    * update to parse_slice for undef/asan + test update
    
    * formatting
    
    * remove header, no if
    
    * assertions + change the loop from axis to steps for logsoftmax test segfault
    
    Co-authored-by: Shucai Xiao <Shucai.Xiao@amd.com>
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -2989,6 +2989,26 @@ TEST_CASE(slice_5arg_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(slice_5arg_reverse_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto l0  = mm->add_parameter("0", migraphx::shape{migraphx::shape::float_type, {5, 5}});
+    mm->add_literal({{migraphx::shape::int32_type, {2}}, {-1, 1}});
+    mm->add_literal({{migraphx::shape::int32_type, {2}}, {-1, -2}});
+    mm->add_literal({{migraphx::shape::int32_type, {2}}, {-1, -1}});
+    mm->add_literal({{migraphx::shape::int32_type, {2}}, {-5, -3}});
+    auto slice_out = mm->add_instruction(
+        migraphx::make_op("slice", {{"axes", {-1, -2}}, {"starts", {0, -3}}, {"ends", {-4, -1}}}),
+        l0);
+    auto ret = mm->add_instruction(migraphx::make_op("reverse", {{"axes", {-1}}}), slice_out);
+    mm->add_return({ret});
+
+    auto prog = migraphx::parse_onnx("slice_5arg_reverse_test.onnx");
+
+    EXPECT(p == prog);
+}
+
 TEST_CASE(slice_max_end_test)
 {
     migraphx::program p;

26730550e69b7e3132dcda92ea307e521468675d
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-21 11:28:34 -0500

    add topk operator

--- src/include/migraphx/op/topk.hpp
+++ src/include/migraphx/op/topk.hpp
@@ -19,13 +19,13 @@ struct topk
 {
     int64_t k;
     int64_t axis = 0;
-    bool larget  = true;
+    bool largest  = true;
     bool sorted  = true;
 
     template <class Self, class F>
     static auto reflect(Self& self, F f)
     {
-        return pack(f(self.axis, "axis"), f(self.largest, "largest"), f(self.sorted, "sort"));
+        return pack(f(self.k, "topk"), f(self.axis, "axis"), f(self.largest, "largest"), f(self.sorted, "sort"));
     }
 
     value attributes() const
@@ -46,42 +46,16 @@ struct topk
         shape s_val{type, lens};
         shape s_ind{shape::int64_type, lens};
 
-        return {s_val, s_ind};
+        return shape({s_val, s_ind});
     }
 
     argument compute(const shape& output_shape, std::vector<argument> args) const
     {
-        auto vec_ss = output_shape.get argument res_val{output_shape};
-        // negative axis means counting dimensions from back
-        auto lens                 = args[0].get_shape().lens();
-        std::size_t axis_dim_size = lens[axis];
-        // max dimension in axis
-        visit_all(result, args[0])([&](auto output, auto data) {
-            args[1].visit([&](auto indices) {
-                if(output_shape.scalar())
-                {
-                    auto in_index = indices.front();
-                    in_index      = (in_index < 0) ? in_index + axis_dim_size : in_index;
-                    output[0]     = data[in_index];
-                }
-                else
-                {
-                    auto out_lens  = data.get_shape().lens();
-                    out_lens[axis] = indices.get_shape().elements();
-                    migraphx::shape out_comp_shape{data.get_shape().type(), out_lens};
-                    shape_for_each(out_comp_shape, [&](const auto& out_idx) {
-                        auto data_idx  = out_idx;
-                        auto in_index  = indices[data_idx[axis]];
-                        in_index       = (in_index < 0) ? in_index + axis_dim_size : in_index;
-                        data_idx[axis] = in_index;
-                        output[out_comp_shape.index(out_idx.begin(), out_idx.end())] =
-                            data(data_idx.begin(), data_idx.end());
-                    });
-                }
-            });
-        });
+        auto vec_ss = output_shape.sub_shapes();
+        argument res_val{vec_ss.front()};
+        argument res_ind{vec_ss.back()};
 
-        return result;
+        return argument({res_val, res_ind});
     }
 };
 

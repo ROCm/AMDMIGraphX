4b511ba49917c4d451ed33cf096d6150b669c81c
Paul <pfultz2@yahoo.com>
2021-06-24 23:04:45 -0500

    Always allocate

--- src/targets/cpu/lowering.cpp
+++ src/targets/cpu/lowering.cpp
@@ -493,20 +493,6 @@ struct cpu_apply
 
     instruction_ref insert_allocation(instruction_ref ins, const shape& s)
     {
-        auto ins_alias = instruction::get_output_alias(ins);
-        if(last->name() == "@return" and prog_output_names.count(ins_alias) > 0)
-        {
-            return modl->insert_instruction(
-                ins,
-                make_op("cpu::preallocate",
-                        {{"shape", to_value(s)}, {"id", prog_output_names[ins_alias]}}));
-        }
-        else if(ins == last)
-        {
-            return modl->insert_instruction(
-                ins, make_op("cpu::preallocate", {{"shape", to_value(s)}, {"id", "output"}}));
-        }
-
         return modl->insert_instruction(ins, make_op("cpu::allocate", {{"shape", to_value(s)}}));
     }
 };

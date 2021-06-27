cf641c832acb1bb726831d9084498f62c4fa4a59
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-27 17:49:27 -0500

    clang format

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -2140,7 +2140,7 @@ TEST_CASE(nonzero_dynamic_test)
     auto* mm = p.get_main_module();
     migraphx::shape s{migraphx::shape::bool_type, {2, 2}};
     auto data = mm->add_parameter("data", s);
-    auto r = mm->add_instruction(migraphx::make_op("nonzero"), data);
+    auto r    = mm->add_instruction(migraphx::make_op("nonzero"), data);
     mm->add_return({r});
 
     auto prog = migraphx::parse_onnx("nonzero_dynamic_test.onnx");

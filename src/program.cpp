95384d6a4211d5fb6e2ad9e73618815861431cc3
Paul <pfultz2@yahoo.com>
2021-07-12 10:49:24 -0500

    Formatting

--- src/program.cpp
+++ src/program.cpp
@@ -250,8 +250,9 @@ std::vector<argument> generic_eval(const module* mod,
                             }));
         }
         assert(results.find(ins) != results.end());
-        if (results.at(ins).get_shape() != ins->get_shape())
-            std::cout << "Mismatch: " << results.at(ins).get_shape() << " != " << ins->get_shape() << std::endl;
+        if(results.at(ins).get_shape() != ins->get_shape())
+            std::cout << "Mismatch: " << results.at(ins).get_shape() << " != " << ins->get_shape()
+                      << std::endl;
         assert(results.at(ins).get_shape() == ins->get_shape());
     }
     return {results.at(std::prev(mod->end()))};
@@ -649,7 +650,9 @@ void program::print_cpp(std::ostream& os) const
 void program::dry_run(std::unordered_map<std::string, argument> params) const
 {
     auto& ctx = this->impl->ctx;
-    generic_eval(*this, ctx, std::move(params), [](auto ins, auto&&...) { return argument{ins->get_shape(), nullptr}; });
+    generic_eval(*this, ctx, std::move(params), [](auto ins, auto&&...) {
+        return argument{ins->get_shape(), nullptr};
+    });
 }
 
 void program::annotate(std::ostream& os, const std::function<void(instruction_ref)>& a) const

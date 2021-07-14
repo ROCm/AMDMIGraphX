3e6ddab76d5b3dd83e77bdc16100f7aa87011634
Shucai Xiao <Shucai.Xiao@amd.com>
2021-07-13 22:28:43 -0500

    clang format

--- test/quantization.cpp
+++ test/quantization.cpp
@@ -174,7 +174,7 @@ TEST_CASE(param_add_sub)
         migraphx::program p;
         auto* mm = p.get_main_module();
         migraphx::shape s{migraphx::shape::float_type, {2, 3}};
-        auto p1  = mm->add_parameter("x", s);
+        auto p1 = mm->add_parameter("x", s);
         mm->insert_instruction(
             std::next(p1),
             migraphx::make_op("convert",

53a53d2ca42c0caf0a37354ec5a2b9ec0cd041ca
Khalique <15948690+kahmed10@users.noreply.github.com>
2021-07-21 12:13:38 -0500

    formatting

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -1188,8 +1188,8 @@ TEST_CASE(flatten_nonstd_test)
     migraphx::program p;
     auto* mm = p.get_main_module();
     auto l0  = mm->add_parameter("0", migraphx::shape{migraphx::shape::float_type, {2, 3, 5, 4}});
-    auto l1 = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {0, 1, 3, 2}}}), l0);
-    auto l2 = mm->add_instruction(migraphx::make_op("contiguous"), l1);
+    auto l1  = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {0, 1, 3, 2}}}), l0);
+    auto l2  = mm->add_instruction(migraphx::make_op("contiguous"), l1);
     mm->add_instruction(migraphx::make_op("flatten", {{"axis", 2}}), l2);
     auto l3 = mm->add_instruction(migraphx::make_op("contiguous"), l1);
     mm->add_instruction(migraphx::make_op("flatten", {{"axis", 1}}), l3);

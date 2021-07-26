642aa36d062d7d611a01d261a5d3d955370b1e8e
Shucai Xiao <Shucai.Xiao@amd.com>
2021-07-26 16:53:19 -0500

    fix an issue for type mismatch in parsing gemm

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -1277,8 +1277,6 @@ TEST_CASE(gemm_test)
     auto beta  = 2.0f;
     auto a_l   = mm->add_literal(alpha);
     auto t_a   = add_common_op(*mm, migraphx::make_op("mul"), {a_l, l0});
-    t_a        = mm->add_instruction(
-        migraphx::make_op("convert", {{"target_type", l1->get_shape().type()}}), t_a);
     t_a     = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {1, 0}}}), t_a);
     auto t1 = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {1, 0}}}), l1);
 
@@ -1305,9 +1303,6 @@ TEST_CASE(gemm_ex_test)
     auto beta  = 0.8f;
     auto a_l   = mm->add_literal(alpha);
     auto t_a   = add_common_op(*mm, migraphx::make_op("mul"), {a_l, l0});
-    t_a        = mm->add_instruction(
-        migraphx::make_op("convert", {{"target_type", l1->get_shape().type()}}), t_a);
-
     t_a = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {0, 1, 3, 2}}}), t_a);
 
     auto b_l = mm->add_literal(beta);
@@ -1333,9 +1328,6 @@ TEST_CASE(gemm_ex_brcst_test)
     auto beta  = 0.8f;
     auto a_l   = mm->add_literal(alpha);
     auto t_a   = add_common_op(*mm, migraphx::make_op("mul"), {a_l, l0});
-    t_a        = mm->add_instruction(
-        migraphx::make_op("convert", {{"target_type", l1->get_shape().type()}}), t_a);
-
     t_a = mm->add_instruction(migraphx::make_op("transpose", {{"dims", {0, 1, 3, 2}}}), t_a);
 
     auto b_l = mm->add_literal(beta);

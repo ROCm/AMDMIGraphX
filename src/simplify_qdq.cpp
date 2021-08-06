9e233125f9cb8261d6344cb60525c2bcb5b8f1e7
turneram <alturner@amd.com>
2021-08-05 19:29:50 -0500

    Formatting

--- src/simplify_qdq.cpp
+++ src/simplify_qdq.cpp
@@ -138,7 +138,9 @@ void remove_qdq_pairs(module& m)
             if(arg->name() == "dequantizelinear")
             {
                 auto q = arg->inputs().front();
-                if((q->name() == "quantizelinear") and compare_literals(arg->inputs().at(1), q->inputs().at(1)) and compare_literals(arg->inputs().at(2), q->inputs().at(2)))
+                if((q->name() == "quantizelinear") and
+                   compare_literals(arg->inputs().at(1), q->inputs().at(1)) and
+                   compare_literals(arg->inputs().at(2), q->inputs().at(2)))
                 {
                     arg        = q->inputs().front();
                     replace_op = true;

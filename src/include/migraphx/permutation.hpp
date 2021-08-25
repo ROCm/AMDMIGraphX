da33b28928755b3e6aee0569716b267b7168e112
Shucai Xiao <Shucai.Xiao@amd.com>
2021-08-25 13:11:34 -0500

    fix two asserts for debug build

--- src/include/migraphx/permutation.hpp
+++ src/include/migraphx/permutation.hpp
@@ -11,11 +11,18 @@ inline namespace MIGRAPHX_INLINE_NS {
 template <class Vector>
 inline Vector reorder_dims(const Vector& dims, const std::vector<int64_t>& permutation)
 {
-    Vector result(dims.size());
-    assert(dims.size() == permutation.size());
-    for(std::size_t i = 0; i < dims.size(); i++)
+    auto tuned_dims = dims;
+    if(tuned_dims.size() == 0)
     {
-        result[i] = dims[permutation[i]];
+        tuned_dims.resize(permutation.size());
+        std::iota(tuned_dims.begin(), tuned_dims.end(), 0);
+        std::reverse(tuned_dims.begin(), tuned_dims.end());
+    }
+    Vector result(tuned_dims.size());
+    assert(tuned_dims.size() == permutation.size());
+    for(std::size_t i = 0; i < tuned_dims.size(); i++)
+    {
+        result[i] = tuned_dims[permutation[i]];
     }
     return result;
 }

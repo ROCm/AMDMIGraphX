7eae5e5c6486a54e9bc38114553678e4b0486795
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2021-08-30 13:45:09 -0500

    formatting

--- test/verify/test_conv_bias_relu.cpp
+++ test/verify/test_conv_bias_relu.cpp
@@ -13,19 +13,18 @@ struct test_conv_bias_relu : verify_program<test_conv_bias_relu>
         migraphx::program p;
         auto* mm = p.get_main_module();
         std::vector<float> bias_vals(64, 2.0f);
-        auto input =  
+        auto input =
             mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {1, 64, 56, 56}});
         auto weights =
             mm->add_parameter("w", migraphx::shape{migraphx::shape::float_type, {64, 64, 1, 1}});
-        auto l0        = migraphx::literal{migraphx::shape{migraphx::shape::float_type, {64}},
-                                    bias_vals};
+        auto l0 = migraphx::literal{migraphx::shape{migraphx::shape::float_type, {64}}, bias_vals};
         auto bias      = mm->add_literal(l0);
         auto conv      = mm->add_instruction(migraphx::make_op("convolution"), input, weights);
         auto bcast_add = mm->add_instruction(
             migraphx::make_op("broadcast", {{"axis", 1}, {"out_lens", conv->get_shape().lens()}}),
             bias);
         auto bias_add = mm->add_instruction(migraphx::make_op("add"), conv, bcast_add);
-        
+
         mm->add_instruction(migraphx::make_op("relu"), bias_add);
         return p;
     }

888f9be964833fd523905cc105aecf61345072ef
turneram <alturner@amd.com>
2021-08-31 15:29:59 -0500

    Use multi index in device function

--- src/targets/gpu/device/where.cpp
+++ src/targets/gpu/device/where.cpp
@@ -8,6 +8,12 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
+template<class Shape>
+constexpr auto get_rank(const Shape&)
+{
+    return decltype(typename Shape::hip_index{}.size()){};
+}
+
 void where(hipStream_t stream,
            const argument& result,
            const argument& arg0,
@@ -16,8 +22,14 @@ void where(hipStream_t stream,
 {
     hip_visit_all(result, arg1, arg2)([&](auto output, auto x, auto y) {
         hip_visit_all(arg0)([&](auto cond) {
-            gs_launch(stream, arg1.get_shape().elements())(
-                [=](auto i) __device__ { output[i] = cond[i] ? x[i] : y[i]; });
+            if constexpr(get_rank(cond.get_shape()) == get_rank(output.get_shape()))
+            {
+                gs_launch(stream, arg1.get_shape().elements())(
+                    [=](auto idx) __device__ { 
+                        auto i = output.get_shape().multi(idx);
+                        output[i] = cond[i] ? x[i] : y[i];
+                    });
+            }
         });
     });
 }

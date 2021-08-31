21f8bbe8e624ab1a8cf4f95a2670cd751cb13e9b
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2021-08-31 11:42:44 -0500

    Changes to support both OneDNN and ZenDNN builds (#929)
    * Add preallocate method
    
    * Add preallocate_param pass
    
    * Preallocate buffers on the cpu
    
    * Formatting
    
    * Preallocate on the gpu
    
    * Add missing cpp file
    
    * Formatting
    
    * Add lifetime function
    
    * Formatting
    
    * Improve handling of exceptions in test driver
    
    * Formatting
    
    * Auto print exception
    
    * Formatting
    
    * Fork each test case
    
    * Formatting
    
    * Exclude gcc 5 debug build
    
    * Fix tidy issues
    
    * Add color
    
    * Formatting
    
    * Create driver class
    
    * Formatting
    
    * Customize test_case names
    
    * Formatting
    
    * Report status from forked processes
    
    * Formatting
    
    * Update the verify driver
    
    * Formatting
    
    * Print out failed tests
    
    * Formatting
    
    * Fix tidy issues
    
    * Formatting
    
    * Expect passing
    
    * Improve failure reporting on non-linux systems
    
    * Fix ifdef
    
    * Always allocate
    
    * Fix tidy warning
    
    * Flush code code cov
    
    * Formatting
    
    * Fix tidy
    
    * Add const
    
    * Check if weak symbols is linked
    
    * Formatting
    
    * initial progress
    
    * formatting
    
    * Add continue flag
    
    * Formatting
    
    * Set exe name
    
    * Use stringstream and use quotes
    
    * rename vars
    
    * formatting
    
    * more testing
    
    * formatting
    
    * Fix bug when using --continue in the tests
    
    * Formatting
    
    * revert gemm
    
    * revert dot file
    
    * rename var
    
    * update cmakelists and deconv compute
    
    Co-authored-by: Paul <pfultz2@yahoo.com>
    Co-authored-by: mvermeulen <5479696+mvermeulen@users.noreply.github.com>

--- src/include/migraphx/op/deconvolution.hpp
+++ src/include/migraphx/op/deconvolution.hpp
@@ -9,6 +9,8 @@
 #include <migraphx/literal.hpp>
 #include <migraphx/shape_for_each.hpp>
 #include <migraphx/config.hpp>
+#include <migraphx/dfor.hpp>
+#include <migraphx/par_dfor.hpp>
 #include <cmath>
 #include <utility>
 
@@ -68,6 +70,81 @@ struct deconvolution
                     ((weights.lens()[i + 2] - 1) * dilation[i] + 1) - 2 * padding[i])));
         }
         return inputs[0].with_lens(output_lens);
+    }
+
+    argument compute(shape output_shape, std::vector<argument> args) const
+    {
+        argument result{output_shape};
+        auto kdims = this->kdims();
+        visit_all(result, args[0], args[1])([&](auto output, auto input, auto weights) {
+            using type = typename decltype(output)::value_type;
+
+            std::fill(output.begin(), output.end(), type{0});
+
+            auto in_lens = input.get_shape().lens();
+            auto in_n    = in_lens[0];
+            auto in_c    = in_lens[1];
+
+            auto wei   = weights.get_shape().lens();
+            auto wei_n = wei[0];
+            auto wei_c = wei[1];
+
+            auto out_lens = output_shape.lens();
+
+            std::vector<std::size_t> win_size{in_c};
+            std::copy(in_lens.begin() + 2, in_lens.end(), std::back_inserter(win_size));
+            std::copy(wei.begin() + 2, wei.end(), std::back_inserter(win_size));
+            shape win_shape{output_shape.type(), win_size};
+
+            par_dfor(in_n, wei_c)([&](int o, int k) {
+
+                shape_for_each(win_shape, [&](auto idx_win) {
+                    const int w = idx_win[0];
+
+                    auto input_dims_start = idx_win.begin() + 1;
+                    auto wei_dims_start   = idx_win.begin() + kdims + 1;
+
+                    std::vector<std::ptrdiff_t> win_start;
+                    for(std::size_t n = 0; n < kdims; ++n)
+                    {
+                        win_start.push_back(std::ptrdiff_t(*(input_dims_start + n) * stride[n]) -
+                                            std::ptrdiff_t(padding[n]));
+                    }
+
+                    const int group_id = w / (wei_n / group);
+                    const int in_ch    = group_id * wei_c + k;
+
+                    std::vector<std::ptrdiff_t> idx_out{o, in_ch};
+
+                    for(size_t n = 0; n < kdims; n++)
+                    {
+                        idx_out.push_back(win_start[n] + *(wei_dims_start + n) * dilation[n]);
+                    }
+
+                    std::vector<std::ptrdiff_t> idx_wei{w, k};
+                    std::copy(wei_dims_start, idx_win.end(), std::back_inserter(idx_wei));
+
+                    std::vector<std::ptrdiff_t> idx_in{o, w};
+                    std::copy(input_dims_start, wei_dims_start, std::back_inserter(idx_in));
+
+                    if(std::all_of(
+                           idx_out.begin() + 2, idx_out.end(), [&](auto ii) { return ii >= 0; }) and
+                       std::equal(idx_out.begin() + 2,
+                                  idx_out.end(),
+                                  out_lens.begin() + 2,
+                                  out_lens.end(),
+                                  std::less<std::ptrdiff_t>{}))
+                    {
+                        output(idx_out.begin(), idx_out.end()) +=
+                            input(idx_in.begin(), idx_in.end()) *
+                            weights(idx_wei.begin(), idx_wei.end());
+                    }
+                });
+
+            });
+
+        });
+        return result;
     }
 
     size_t kdims() const

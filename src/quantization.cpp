eaf73e9a12bfadc349d515805990f71054aa7875
Shucai Xiao <Shucai.Xiao@amd.com>
2021-08-31 21:47:30 -0500

    remove comment lines

--- src/quantization.cpp
+++ src/quantization.cpp
@@ -40,110 +40,11 @@ void quantize_fp16(program& prog, const std::vector<std::string>& ins_names)
                 dead_code_elimination{}});
 }
 
-// static std::size_t capture_argument_num(module& m, const std::vector<std::string>& ins_names)
-// {
-//     std::size_t num_params = 0;
-//     for(auto ins : iterator_for(m))
-//     {
-//         auto mod_inputs = ins->module_inputs();
-//         for(auto*& smod : mod_inputs)
-//         {
-//             num_params += capture_argument_num(*smod, ins_names);
-//         }
-
-//         if(not contains(ins_names, ins->name()))
-//         {
-//             continue;
-//         }
-
-//         num_params += ins->inputs().size();
-//     }
-
-//     return num_params;
-// }
-
-// static std::size_t capture_argument_num(program& prog, const std::vector<std::string>& ins_names)
-// {
-//     auto* mm = prog.get_main_module();
-//     return capture_argument_num(*mm, ins_names);
-// }
-
-// static const std::vector<std::pair<float, float>>
-// calc_quantize_params(program& prog,
-//                      const target& t,
-//                      const std::vector<parameter_map>& calibration,
-//                      const std::vector<std::string>& ins_names)
-// {
-//     auto num_quant_params = capture_argument_num(prog, ins_names);
-
-//     std::shared_ptr<std::vector<std::pair<float, float>>> int8_quant_params =
-//         std::make_shared<std::vector<std::pair<float, float>>>(
-//             num_quant_params, std::pair<float, float>(64.0f, 0.0f));
-//     std::shared_ptr<std::vector<float>> max_abs_vals =
-//         std::make_shared<std::vector<float>>(num_quant_params, 0.0f);
-
-//     auto calc_quant_params = [int8_quant_params, max_abs_vals, &t](std::size_t ins_index,
-//                                                                    std::vector<argument> args) {
-//         std::pair<float, float> param_pair{64.0f, 0.0f};
-//         // scale and shift is need for only int8 type, and we do not
-//         // consider shift, so set shift to 0
-//         std::vector<float> vec_val;
-//         argument arg = t.copy_from(args.front());
-//         arg.visit([&](auto output) { vec_val.assign(output.begin(), output.end()); });
-//         auto max_val                = *std::max_element(vec_val.begin(), vec_val.end());
-//         auto min_val                = *std::min_element(vec_val.begin(), vec_val.end());
-//         auto max_abs                = std::max(std::fabs(max_val), std::fabs(min_val));
-//         max_abs_vals->at(ins_index) = std::max(max_abs_vals->at(ins_index), max_abs);
-
-//         // if all values are 0, no need to do scaling
-//         if(max_abs_vals->at(ins_index) == 0.0f)
-//         {
-//             param_pair.first = 1.0f;
-//         }
-//         else
-//         {
-//             param_pair.first = 127.0f / max_abs_vals->at(ins_index);
-//         }
-//         int8_quant_params->at(ins_index) = param_pair;
-//     };
-
-//     // add capture argument op
-//     std::size_t param_index = 0;
-//     run_passes(prog, {capture_arguments_pass{ins_names, calc_quant_params, &param_index}});
-
-//     // use the calibration data to compute the quantization scale
-//     prog.compile(t);
-
-//     // use all calibration data to run the program to calculate the
-//     // quantization scale and shift
-//     for(auto&& arg : calibration)
-//     {
-//         parameter_map m;
-//         for(auto&& x : prog.get_parameter_shapes())
-//         {
-//             if(arg.count(x.first) > 0)
-//             {
-//                 assert(x.second == arg.at(x.first).get_shape());
-//                 m[x.first] = t.copy_to(arg.at(x.first));
-//             }
-//             else
-//             {
-//                 m[x.first] = t.allocate(x.second);
-//             }
-//         }
-//         prog.eval(m);
-//     }
-
-//     return std::move(*int8_quant_params);
-// }
-
 void quantize_int8(program& prog,
                    const target& t,
                    const std::vector<parameter_map>& calibration,
                    const std::vector<std::string>& ins_names)
 {
-    // // insert capture operator
-    // const auto& int8_quant_params = calc_quantize_params(prog, t, calibration, ins_names);
     std::set<std::string> op_names = {"convolution", "dot"};
     std::set<std::string> input_ins_names(ins_names.begin(), ins_names.end());
     if(!std::includes(
@@ -181,7 +82,7 @@ void quantize_int8(program& prog,
         int8_quant_params->at(ins_index) = param_pair;
     };
 
-    // add capture argument op
+    // pass to add capture argument op
     std::size_t param_num = 0;
     run_passes(prog, {capture_arguments_pass{ins_names, calc_quant_params, &param_num}});
     int8_quant_params->resize(param_num, std::pair<float, float>(64.0f, 0.0f));
--- src/quantize_int8.cpp
+++ src/quantize_int8.cpp
@@ -27,81 +27,6 @@ static std::vector<shape::type_t>& get_quantizable_type()
     return quantable_types;
 }
 
-// // int8 quantization is different from fp16 since int8 can only handle value
-// // -128 ~ 127. To convert the float or double to int8, we need a scale and
-// // a shift, then the convert can be done as v_int8 = fp * scale + shift.
-// // To simplify the changes, we consider shift as 0.0f for now.
-// void quantize_int8_impl(module& m,
-//                         const std::vector<std::pair<float, float>>& quant_params,
-//                         const std::vector<std::string>& ins_names,
-//                         // std::unordered_map<instruction_ref, instruction_ref>& map_quant_ins,
-//                         std::unordered_map<instruction_ref, std::size_t>& map_ins_index,
-//                         std::size_t& quant_param_index)
-// {
-//     const auto& quantizable_types = get_quantizable_type();
-//     for(auto ins : iterator_for(m))
-//     {
-//         if(ins->name() == "@return")
-//             break;
-
-//         // if contains subgraph, needs to handle subgraph
-//         auto mod_inputs = ins->module_inputs();
-//         for(auto*& smod : mod_inputs)
-//         {
-//             quantize_int8_impl(*smod, quant_params, ins_names, map_ins_index, quant_param_index);
-//         }
-
-//         if(not contains(ins_names, ins->name()))
-//         {
-//             continue;
-//         }
-
-//         // process all inputs, if input is a fp32 or fp64, convert it
-//         // to a int8 type by adding a convert operator and replace
-//         // the operator with the corresponding int8 version
-//         auto inputs = ins->inputs();
-//         std::vector<std::pair<float, float>> ins_quant_params;
-//         std::vector<instruction_ref> dq_inputs;
-//         for(auto input : inputs)
-//         {
-//             // calculate the index of each instruction to be quantized
-//             std::size_t ins_index =
-//                 (map_ins_index.count(input) > 0) ? map_ins_index[input] : quant_param_index++;
-//             map_ins_index[input] = ins_index;
-
-//             auto param = quant_params[map_ins_index[input]];
-//             ins_quant_params.push_back(param);
-
-//             // In general, the target_type is int8, but for the dot
-//             // operation, if it has 3 inputs, then the last one should
-//             // be converted to int32_type
-//             shape::type_t quant_type = shape::int8_type;
-//             auto s                   = input->get_shape();
-//             if(contains(quantizable_types, s.type()) and s.type() != quant_type)
-//             {
-//                 auto zero_point = m.add_literal(static_cast<int8_t>(param.second));
-//                 auto scale      = m.add_literal(literal({s.type()}, {1.0f / param.first}));
-//                 auto lens       = input->get_shape().lens();
-//                 scale           = m.insert_instruction(
-//                     ins, make_op("multibroadcast", {{"out_lens", lens}}), scale);
-//                 zero_point = m.insert_instruction(
-//                     ins, make_op("multibroadcast", {{"out_lens", lens}}), zero_point);
-//                 auto q_in =
-//                     m.insert_instruction(ins, make_op("quantizelinear"), input, scale,
-//                     zero_point);
-//                 auto dq_in =
-//                     m.insert_instruction(ins, make_op("dequantizelinear"), q_in, scale,
-//                     zero_point);
-//                 dq_inputs.push_back(dq_in);
-//             }
-//         }
-//         if(inputs != dq_inputs)
-//         {
-//             m.replace_instruction(ins, ins->get_operator(), dq_inputs);
-//         }
-//     }
-// }
-
 void quantize_int8_pass::apply(module& m) const // NOLINT
 {
     const auto& quantizable_types = get_quantizable_type();
@@ -133,42 +58,6 @@ void quantize_int8_pass::apply(module& m) const // NOLINT
                 m.insert_instruction(ins, make_op("dequantizelinear"), q_in, scale, zero_point);
             m.replace_instruction(ins, dq_in);
         }
-
-        // if(not contains(ins_names, ins->name()))
-        // {
-        //     continue;
-        // }
-
-        // auto inputs = ins->inputs();
-        // std::vector<instruction_ref> converted_inputs;
-        // for(auto input : inputs)
-        // {
-        //     assert(input->name() == "capture");
-        //     // replace the capture with the qdq pair
-        //     auto op = any_cast<op::capture>(input->get_operator());
-        //     auto param_index = op.ins_index;
-        //     auto param = quant_params[param_index];
-
-        //     auto in = input->inputs().front();
-        //     auto s = in->get_shape();
-        //     if(contains(quantizable_types, s.type()) and s.type() != shape::int8_type)
-        //     {
-        //         auto zero_point = m.add_literal(static_cast<int8_t>(param.second));
-        //         auto scale      = m.add_literal(literal({s.type()}, {1.0f / param.first}));
-        //         auto lens       = s.lens();
-        //         scale           = m.insert_instruction(
-        //             ins, make_op("multibroadcast", {{"out_lens", lens}}), scale);
-        //         zero_point = m.insert_instruction(
-        //             ins, make_op("multibroadcast", {{"out_lens", lens}}), zero_point);
-        //         auto q_in =
-        //             m.insert_instruction(ins, make_op("quantizelinear"), in, scale, zero_point);
-        //         auto dq_in =
-        //             m.insert_instruction(ins, make_op("dequantizelinear"), q_in, scale,
-        //             zero_point);
-        //         converted_inputs.push_back(dq_in);
-        //     }
-        // }
-        // m.replace_instruction(ins, ins->get_operator(), converted_inputs);
     }
 }
 

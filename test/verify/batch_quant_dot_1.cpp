d9aac6e53e43532bc52e2560fe2c6e601427cb4b
umangyadav <umayadav@amd.com>
2021-09-22 09:31:36 -0500

    add 3args test

--- test/verify/batch_quant_dot_1.cpp
+++ test/verify/batch_quant_dot_1.cpp
@@ -11,9 +11,9 @@ struct batch_quant_dot_1 : verify_program<batch_quant_dot_1>
     {
         migraphx::program p;
         auto* mm = p.get_main_module();
-        migraphx::shape m1_shape{migraphx::shape::int8_type, {3, 2, 8, 6}};
+        migraphx::shape m1_shape{migraphx::shape::int8_type, {3, 2, 8, 2}};
         migraphx::shape m2_shape{migraphx::shape::int8_type, {3, 2, 7, 8}};
-        migraphx::shape m3_shape{migraphx::shape::int32_type, {3, 2, 6, 7}};
+        migraphx::shape m3_shape{migraphx::shape::int32_type, {3, 2, 2, 7}};
 
         auto l1  = mm->add_parameter("a", m1_shape);
         auto tl1 = mm->add_instruction(

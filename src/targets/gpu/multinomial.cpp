ff979b06a3c67f5a10b7392acddd538770b953aa
turneram <alturner@amd.com>
2021-09-29 17:00:15 -0500

    Add standard shape check to gpu impl

--- src/targets/gpu/multinomial.cpp
+++ src/targets/gpu/multinomial.cpp
@@ -2,6 +2,7 @@
 #include <migraphx/gpu/device/multinomial.hpp>
 #include <migraphx/gpu/context.hpp>
 #include <migraphx/tune_axis.hpp>
+#include <migraphx/check_shapes.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -9,6 +10,7 @@ namespace gpu {
 
 shape hip_multinomial::compute_shape(std::vector<shape> inputs) const
 {
+    check_shapes{inputs, *this}.has(3).only_dims(2).standard();
     inputs.pop_back();
     return op.compute_shape(inputs);
 }

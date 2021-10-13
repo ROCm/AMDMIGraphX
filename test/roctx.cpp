de8f889ab364c8c51f0eac116308d69080f23b8c
Cagri Eryilmaz <ceryilma@amd.com>
2021-10-13 18:29:38 +0000

    unit test for roctx

--- test/roctx.cpp
+++ test/roctx.cpp
@@ -2,23 +2,58 @@
 #include <migraphx/ref/target.hpp>
 #include <migraphx/ranges.hpp>
 #include <migraphx/make_op.hpp>
+#include <migraphx/marker.hpp>
+#include <migraphx/instruction.hpp>
 
 #include "test.hpp"
 
-TEST_CASE(perf_report)
+struct mock_marker
+{
+    std::shared_ptr<std::stringstream> ss = std::make_shared<std::stringstream>();
+
+    void mark_start(migraphx::instruction_ref ins_ref)
+    {
+        std::string text = "Mock marker instruction start:" + ins_ref->name();
+        (*ss) << text;
+        
+    }
+    void mark_stop(migraphx::instruction_ref)
+    { 
+        std::string text = "Mock marker instruction stop.";
+        (*ss) << text;
+    }
+    void mark_start(const migraphx::program&)
+    {
+        std::string text = "Mock marker program start.";
+        (*ss) << text; 
+    }
+    void mark_stop(const migraphx::program&)
+    { 
+        std::string text = "Mock marker program stop.";
+        (*ss) << text;    
+    }
+};
+
+
+TEST_CASE(roctx)
 {
     migraphx::program p;
     auto* mm = p.get_main_module();
 
-    std::stringstream ss;
     auto one = mm->add_literal(1);
     auto two = mm->add_literal(2);
     mm->add_instruction(migraphx::make_op("add"), one, two);
     p.compile(migraphx::ref::target{});
-    p.roctx();
+    
+    mock_marker temp_marker;
+    p.roctx({}, temp_marker);
 
-    std::string output = ss.str();
-    EXPECT(migraphx::contains(output, "rocTX:  Loading rocTX library..."));
+    std::string output = temp_marker.ss->str();
+    EXPECT(migraphx::contains(output, "Mock marker instruction start:"));
+    EXPECT(migraphx::contains(output, "Mock marker instruction stop."));
+    EXPECT(migraphx::contains(output, "Mock marker program start."));
+    EXPECT(migraphx::contains(output, "Mock marker program stop."));
+
 }
 
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

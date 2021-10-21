26f4c2d0f4ff59717af6161f4a5f16cc28287606
Shucai Xiao <Shucai.Xiao@amd.com>
2021-10-20 21:40:02 -0500

    change to the similar implementation as the ref impl for the roialign operator

---
+++ src/targets/gpu/kernels/include/migraphx/kernels/dfor.hpp
@@ -0,0 +1,26 @@
+#ifndef MIGRAPHX_GUARD_AMDMIGRAPHX_KERNELS_DFOR_HPP
+#define MIGRAPHX_GUARD_AMDMIGRAPHX_KERNELS_DFOR_HPP
+
+
+namespace migraphx {
+
+// Multidimensional for loop
+inline __host__ __device__ auto dfor()
+{
+    return [](auto f) { f(); };
+}
+
+template <class T, class... Ts>
+ __host__ __device__  auto dfor(T x, Ts... xs)
+{
+    return [=](auto f) {
+        for(T i = 0; i < x; i++)
+        {
+            dfor(xs...)([&](Ts... is) { f(i, is...); });
+        }
+    };
+}
+
+} // namespace migraphx
+
+#endif
--- src/targets/gpu/kernels/include/migraphx/kernels/roialign.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/roialign.hpp
@@ -2,8 +2,10 @@
 #define MIGRAPHX_GUARD_KERNELS_ROIALIGN_HPP
 
 #include <migraphx/kernels/index.hpp>
+#include <migraphx/kernels/dfor.hpp>
 #include <migraphx/kernels/basic_ops.hpp>
 #include <args.hpp>
+#include <numeric>
 
 namespace migraphx {
 
@@ -12,7 +14,7 @@ struct max_pool
     MIGRAPHX_DEVICE_CONSTEXPR auto init() { return lowest(); }
 
     template <class T>
-    MIGRAPHX_DEVICE_CONSTEXPR T op(T x, T y)
+    MIGRAPHX_DEVICE_CONSTEXPR T operator()(T x, T y)
     {
         return x > y ? x : y;
     }
@@ -29,7 +31,7 @@ struct avg_pool
     MIGRAPHX_DEVICE_CONSTEXPR auto init() { return 0.0; }
 
     template <class T>
-    MIGRAPHX_DEVICE_CONSTEXPR T op(T x, T y)
+    MIGRAPHX_DEVICE_CONSTEXPR T operator()(T x, T y)
     {
         return x + y;
     }
@@ -41,85 +43,86 @@ struct avg_pool
     }
 };
 
-template <class T, class Op>
-__device__ T
-bilinear_interpolate(const T* data, const int height, const int width, float y, float x, Op pooling)
+template <class T, class Op> 
+MIGRAPHX_DEVICE_CONSTEXPR T
+bilinear_interpolate(const T* data, const std::array<std::size_t, 2>& dims, std::array<float, 2> xy, Op pooling)
 {
-    // deal with cases that inverse elements are out of feature map boundary
-    if(y < -1.0f || y > height || x < -1.0f || x > width)
+    std::array<int, 2> low{};
+    std::array<int, 2> high{};
+    max max_op{};
+    for(std::size_t ii = 0; ii < xy.size(); ++ii)
     {
-        return 0;
+        if(xy[ii] < -1.0f or xy[ii] > dims[ii])
+        {
+            return 0;
+        }
+
+        xy[ii] = max_op(xy[ii], 0);
+        low[ii] = xy[ii];
+        high[ii] = low[ii] + 1;
+        if(low[ii] >= dims[ii] - 1)
+        {
+            xy[ii] = high[ii] = low[ii] = dims[ii] - 1;
+        }
     }
 
-    y          = (y <= 0) ? 0 : y;
-    x          = (x <= 0) ? 0 : x;
-    auto y_low = static_cast<int>(y);
-    auto x_low = static_cast<int>(x);
-    int y_high;
-    int x_high;
-
-    y_high = y_low + 1;
-    if(y_low >= height - 1)
-    {
-        y = y_high = y_low = height - 1;
-    }
-
-    x_high = x_low + 1;
-    if(x_low >= width - 1)
-    {
-        x = x_high = x_low = width - 1;
-    }
-
-    float ly = y - y_low;
-    float lx = x - x_low;
+    float ly = xy[0] - low[0];
+    float lx = xy[1] - low[1];
     float hy = 1.0f - ly;
     float hx = 1.0f - lx;
 
-    // do bilinear interpolation
-    T v1 = data[y_low * width + x_low];
-    T v2 = data[y_low * width + x_high];
-    T v3 = data[y_high * width + x_low];
-    T v4 = data[y_high * width + x_high];
-    T w1 = static_cast<T>(hy * hx);
-    T w2 = static_cast<T>(hy * lx);
-    T w3 = static_cast<T>(ly * hx);
-    T w4 = static_cast<T>(ly * lx);
+    std::array<std::size_t, 4> locs = {low[0] * dims[1] + low[1], low[0] * dims[1] + high[1], high[0] * dims[1] + low[1], high[0] * dims[1] + high[1]};
+    std::array<T, 4> ws = {hy * hx, hy * lx, ly * hx, ly * lx};
+    auto v01 = pooling(data[locs[0]] * ws[0], data[locs[1]] * ws[1]);
+    auto v23 = pooling(data[locs[2]] * ws[2], data[locs[3]] * ws[3]);
+    return pooling(v01, v23);
 
-    T val12 = pooling.op(w1 * v1, w2 * v2);
-    T val34 = pooling.op(w3 * v3, w4 * v4);
 
-    return pooling.op(val12, val34);
+    // std::array<T, 4> vals;
+    // std::transform(locs.begin(), locs.end(), ws.begin(), vals.begin(), [&](auto pos, auto w){
+    //     return data[pos] * w;
+    // });
+
+    // T ini_val = pooling.init();
+    // return std::accumulate(vals.begin(), vals.end(), ini_val, pooling);
 }
 
 template <class T, class Op>
-__device__ T calc_pooling(const T* data,
-                          float roi_start_h,
-                          float roi_start_w,
-                          float bin_size_h,
-                          float bin_size_w,
-                          int ph,
-                          int pw,
-                          int64_t roi_bin_grid_h,
-                          int64_t roi_bin_grid_w,
-                          int height,
-                          int width,
+MIGRAPHX_DEVICE_CONSTEXPR T calc_pooling(const T*& data,
+                          const std::array<float, 2>& roi_starts,
+                          const std::array<float, 2>& bin_size,
+                          const std::array<int, 2>& idx,
+                          const std::array<std::size_t, 2>& bin_grid_size,
+                          const std::array<std::size_t, 2>& dims,
                           float roi_offset,
                           Op op)
 {
     T output_val        = op.init();
-    const int64_t count = roi_bin_grid_h * roi_bin_grid_w;
-    for(int iy = 0; iy < roi_bin_grid_h; ++iy)
-    {
-        float y = roi_start_h + ph * bin_size_h + (iy + 0.5f) * bin_size_h / roi_bin_grid_h;
-        y += roi_offset;
-        for(int ix = 0; ix < roi_bin_grid_w; ++ix)
-        {
-            float x = roi_start_w + pw * bin_size_w + (ix + .5f) * bin_size_w / roi_bin_grid_w;
-            x += roi_offset;
-            auto val   = bilinear_interpolate(data, height, width, y, x, op);
-            output_val = op.op(output_val, val);
-        }
-    }
+    const int64_t count = bin_grid_size[0] * bin_grid_size[1];
+    dfor(bin_grid_size[0], bin_grid_size[1])([&](auto iy, auto ix) {
+        std::array<float, 2> locs;
+
+        locs[0] = roi_starts[0] + idx[0] * bin_size[0] + (iy + 0.5f) * bin_size[0] / bin_grid_size[0];
+        locs[1] = roi_starts[1] + idx[1] * bin_size[1] + (ix + .5f) * bin_size[1] / bin_grid_size[1];
+
+        locs[0] += roi_offset;
+        locs[1] += roi_offset;
+        auto val   = bilinear_interpolate(data, dims, locs, op);
+        output_val = op(output_val, val);
+    });
+
+    // for(int iy = 0; iy < roi_bin_grid_h; ++iy)
+    // {
+    //     float y = roi_start_h + ph * bin_size_h + (iy + 0.5f) * bin_size_h / roi_bin_grid_h;
+    //     y += roi_offset;
+    //     for(int ix = 0; ix < roi_bin_grid_w; ++ix)
+    //     {
+    //         float x = roi_start_w + pw * bin_size_w + (ix + .5f) * bin_size_w / roi_bin_grid_w;
+    //         x += roi_offset;
+    //         auto val   = bilinear_interpolate(data, height, width, y, x, op);
+    //         output_val = op.op(output_val, val);
+    //     }
+    // }
 
     return op.final(output_val, count);
 }
@@ -136,20 +139,29 @@ __device__ void roialign(void* in_x, void* in_rois, void* in_ind, void* y)
             const auto* x    = x_t.data();
             const auto* rois = rois_t.data();
             const auto* ind  = ind_t.data();
-            // auto& ind = ind_t;
+
             auto* out_ptr = y_t.data();
 
             // input shape
             auto x_lens      = x_t.get_shape().lens;
             auto channel_num = x_lens[1];
-            auto height      = x_lens[2];
-            auto width       = x_lens[3];
+            // auto height      = x_lens[2];
+            // auto width       = x_lens[3];
+            // input dims of height and width, in all 2-dim arrays, the first dim
+            // is for height and second dim is for width
+            std::array<std::size_t, 2> in_dims = {x_lens[2], x_lens[3]};
 
             const auto stride   = index.nglobal();
             auto out_s          = y_t.get_shape();
             auto roi_column_num = rois_t.get_shape().lens[1];
-            auto pooling_height = out_s.lens[2];
-            auto pooling_width  = out_s.lens[3];
+            // auto pooling_height = out_s.lens[2];
+            // auto pooling_width  = out_s.lens[3];
+
+            // output dims of height and width, in all 2-dim arrays, the first dim
+            // is for height and second dim is for width
+            const auto& out_lens = out_s.lens;
+            std::array<std::size_t, 2> out_dims = {out_lens[2], out_lens[3]};
+
             for(index_int i = index.global; i < out_s.elements(); i += stride)
             {
                 auto idx = out_s.multi(i);
@@ -159,59 +171,63 @@ __device__ void roialign(void* in_x, void* in_rois, void* in_ind, void* y)
                 int pw   = idx[3];
 
                 const auto* offset_rois = rois + (n * roi_column_num);
-                const auto batch_ind    = static_cast<int>(ind[n]);
+                const int batch_ind    = ind[n];
 
-                float roi_start_w = static_cast<float>(offset_rois[0] * spatial_scale);
-                float roi_start_h = static_cast<float>(offset_rois[1] * spatial_scale);
-                float roi_end_w   = static_cast<float>(offset_rois[2] * spatial_scale);
-                float roi_end_h   = static_cast<float>(offset_rois[3] * spatial_scale);
+                std::array<float, 2> roi_starts = {offset_rois[1] * spatial_scale, offset_rois[0] * spatial_scale};
+                std::array<float, 2> roi_ends = {offset_rois[3] * spatial_scale, offset_rois[2] * spatial_scale};
 
-                float roi_width  = roi_end_w - roi_start_w;
-                float roi_height = roi_end_h - roi_start_h;
+                std::array<float, 2> roi_size{};
+                std::array<float, 2> bin_size{};
+                std::array<std::size_t, 2> bin_grid_size{};
 
-                roi_width  = roi_width > 1.0f ? roi_width : 1.0f;
-                roi_height = roi_height > 1.0f ? roi_height : 1.0f;
+                for(std::size_t ii = 0; ii < roi_size.size(); ++ii)
+                {
+                    roi_size[ii] = roi_ends[ii] - roi_starts[ii];
+                    roi_size[ii] = std::max(roi_size[ii], 1.0f);
 
-                float bin_size_w = roi_width / pooling_width;
-                float bin_size_h = roi_height / pooling_height;
+                    bin_size[ii]      = roi_size[ii] / out_dims[ii];
+                    bin_grid_size[ii] = (sampling_ratio > 0)
+                                            ? sampling_ratio
+                                            : std::ceil(roi_size[ii] / out_dims[ii]);
+                }
 
-                const auto* offset_x = x + ((batch_ind * channel_num + c) * height * width);
+                // float roi_width  = roi_end_w - roi_start_w;
+                // float roi_height = roi_end_h - roi_start_h;
+
+                // roi_width  = roi_width > 1.0f ? roi_width : 1.0f;
+                // roi_height = roi_height > 1.0f ? roi_height : 1.0f;
+
+                // float bin_size_w = roi_width / pooling_width;
+                // float bin_size_h = roi_height / pooling_height;
+
 
                 // We use roi_bin_grid to sample the grid and mimic integral
-                int roi_bin_grid_h =
-                    (sampling_ratio > 0) ? sampling_ratio : ceilf(roi_height / pooling_height);
-                int roi_bin_grid_w =
-                    (sampling_ratio > 0) ? sampling_ratio : ceilf(roi_width / pooling_width);
+                // int roi_bin_grid_h =
+                //     (sampling_ratio > 0) ? sampling_ratio : ceilf(roi_height / pooling_height);
+                // int roi_bin_grid_w =
+                //     (sampling_ratio > 0) ? sampling_ratio : ceilf(roi_width / pooling_width);
+
+                const auto* offset_x = x + ((batch_ind * channel_num + c) * in_dims[0] * in_dims[1]);
 
                 if(is_avg_pooling)
                 {
                     out_ptr[i] = calc_pooling(offset_x,
-                                              roi_start_h,
-                                              roi_start_w,
-                                              bin_size_h,
-                                              bin_size_w,
-                                              ph,
-                                              pw,
-                                              roi_bin_grid_h,
-                                              roi_bin_grid_w,
-                                              height,
-                                              width,
+                                              roi_starts,
+                                              bin_size,
+                                              {ph, pw},
+                                              bin_grid_size,
+                                              in_dims,
                                               roi_offset,
                                               avg_pool{});
                 }
                 else
                 {
                     out_ptr[i] = calc_pooling(offset_x,
-                                              roi_start_h,
-                                              roi_start_w,
-                                              bin_size_h,
-                                              bin_size_w,
-                                              ph,
-                                              pw,
-                                              roi_bin_grid_h,
-                                              roi_bin_grid_w,
-                                              height,
-                                              width,
+                                              roi_starts,
+                                              bin_size,
+                                              {ph, pw},
+                                              bin_grid_size,
+                                              in_dims,
                                               roi_offset,
                                               max_pool{});
                 }

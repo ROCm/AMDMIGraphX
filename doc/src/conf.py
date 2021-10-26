af2ba36b3545347a0488b8ff6d880a8741033beb
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2021-10-26 09:54:04 -0500

    formatting

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -29,8 +29,10 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['breathe', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode'
-                'sphinx_rtd_theme']
+extensions = [
+    'breathe', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode'
+    'sphinx_rtd_theme'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

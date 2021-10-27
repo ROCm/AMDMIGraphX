1afd4145c46dfad512fc42a6988ac3cc0bed0382
Paul <pfultz2@yahoo.com>
2021-10-26 21:41:51 -0500

    Add precompile op

--- src/targets/gpu/compile_ops.cpp
+++ src/targets/gpu/compile_ops.cpp
@@ -3,17 +3,42 @@
 #include <migraphx/module.hpp>
 #include <migraphx/iterator_for.hpp>
 #include <migraphx/instruction.hpp>
+#include <migraphx/register_op.hpp>
 #include <migraphx/gpu/compile_pointwise.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
+struct precompile_op {
+    operation op;
+
+    template <class Self, class F>
+    static auto reflect(Self& self, F f)
+    {
+        return pack(f(self.op, "op"));
+    }
+
+    std::string name() const { return "gpu::precompile_op"; }
+
+    shape compute_shape(std::vector<shape> inputs, std::vector<module_ref> mods) const {
+        inputs.pop_back();
+        return op.compute_shape(inputs, mods);
+    }
+
+    std::ptrdiff_t output_alias(const std::vector<shape>& shapes) const
+    {
+        return shapes.size() - 1;
+    }
+};
+
+MIGRAPHX_REGISTER_OP(precompile_op);
+
 struct pointwise_compiler
 {
     std::string name() const { return "pointwise"; }
 
-    operation apply(context& ctx, instruction_ref ins) const
+    operation apply(context& ctx, instruction_ref ins, operation) const
     {
         assert(not ins->module_inputs().empty());
         auto* pm    = ins->module_inputs().front();
@@ -23,12 +48,12 @@ struct pointwise_compiler
     }
 };
 
-using compiler_function = std::function<operation(context&, instruction_ref)>;
+using compiler_function = std::function<operation(context&, instruction_ref, operation)>;
 
 template <class T>
 compiler_function make_compiler_function(T x)
 {
-    return {[=](context& ctx, instruction_ref ins) { return x.apply(ctx, ins); }};
+    return {[=](auto&&... xs) { return x.apply(xs...); }};
 }
 
 template <class... Ts>
@@ -43,12 +68,12 @@ void compile_ops::apply(module& m) const
     auto insert_allocation = alloc.allocation_inserter(m);
     for(auto ins : iterator_for(m))
     {
-        if(not contains(compilers, ins->name()))
+        if (ins->name() != "gpu::precompile_op")
             continue;
-        auto op     = compilers[ins->name()](*ctx, ins);
-        auto inputs = ins->inputs();
-        inputs.push_back(insert_allocation(ins, ins->get_shape()));
-        m.replace_instruction(ins, op, inputs);
+        operation preop = any_cast<precompile_op>(ins->get_operator()).op;
+        assert(contains(compilers, preop.name()));
+        auto op     = compilers[preop.name()](*ctx, ins, preop);
+        m.replace_instruction(ins, op, ins->inputs());
     }
 }
 

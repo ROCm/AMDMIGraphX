d6029fb1b1ff2a62858fdca9bb28df1355ab64ab
Khalique <15948690+kahmed10@users.noreply.github.com>
2021-10-27 13:16:05 -0500

    fix to import re

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -19,7 +19,7 @@
 # import os
 # import sys
 from datetime import date
-import regex as re
+import re
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------

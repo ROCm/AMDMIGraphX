0553ad827d18ad20c0cab21041c601c4b8fd9c90
Shucai Xiao <shucai@gmail.com>
2021-10-29 06:37:58 -0500

    fixed build error

--- test/verify/test_conv_bn_add.cpp
+++ test/verify/test_conv_bn_add.cpp
@@ -2,7 +2,7 @@
 #include "verify_program.hpp"
 #include <migraphx/program.hpp>
 #include <migraphx/generate.hpp>
-#include <migraphx/op/common.hpp>
+#include <migraphx/make_op.hpp>
 
 struct test_conv_bn_add : verify_program<test_conv_bn_add>
 {
@@ -12,17 +12,19 @@ struct test_conv_bn_add : verify_program<test_conv_bn_add>
                                             std::size_t seed = 1)
     {
         migraphx::shape vars{migraphx::shape::float_type, {channels}};
+        auto* mm = p.get_main_module();
         auto scale    = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 1 + seed)));
         auto bias     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 2 + seed)));
         auto mean     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 3 + seed)));
         auto variance = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 4 + seed)));
         return mm->add_instruction(
-            migraphx::op::batch_norm_inference{}, x, scale, bias, mean, variance);
+            migraphx::make_op("batch_norm_inference"), x, scale, bias, mean, variance);
     }
 
     migraphx::program create_program() const
     {
         migraphx::program p;
+        auto* mm = p.get_main_module();
         std::size_t ichannels = 64;
         std::size_t ochannels = 256;
         auto x     = mm->add_parameter("x", {migraphx::shape::float_type, {1, ichannels, 56, 56}});
@@ -31,14 +33,14 @@ struct test_conv_bn_add : verify_program<test_conv_bn_add>
         auto y     = mm->add_parameter("y", {migraphx::shape::float_type, {1, ichannels, 56, 56}});
         auto v     = mm->add_literal(migraphx::generate_literal(
             {migraphx::shape::float_type, {ochannels, ichannels, 1, 1}}, 2));
-        auto relu1 = mm->add_instruction(migraphx::op::relu{}, x);
-        auto conv1 = mm->add_instruction(migraphx::op::convolution{}, relu1, w);
+        auto relu1 = mm->add_instruction(migraphx::make_op("relu"), x);
+        auto conv1 = mm->add_instruction(migraphx::make_op("convolution"), relu1, w);
         auto bn1   = add_bn(p, conv1, ochannels, 1);
-        auto relu2 = mm->add_instruction(migraphx::op::relu{}, y);
-        auto conv2 = mm->add_instruction(migraphx::op::convolution{}, relu2, v);
+        auto relu2 = mm->add_instruction(migraphx::make_op("relu"), y);
+        auto conv2 = mm->add_instruction(migraphx::make_op("convolution"), relu2, v);
         auto bn2   = add_bn(p, conv2, ochannels, 1);
-        auto sum   = mm->add_instruction(migraphx::op::add{}, bn1, bn2);
-        mm->add_instruction(migraphx::op::relu{}, sum);
+        auto sum   = mm->add_instruction(migraphx::make_op("add"), bn1, bn2);
+        mm->add_instruction(migraphx::make_op("relu"), sum);
         return p;
     }
 };

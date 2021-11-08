0516677e64d28cd024aaf16af9559649f25ea588
Paul <pfultz2@yahoo.com>
2021-11-08 11:54:36 -0600

    Format

--- test/stringutils.cpp
+++ test/stringutils.cpp
@@ -4,14 +4,14 @@
 TEST_CASE(interpolate_string_simple1)
 {
     std::string input = "Hello ${w}!";
-    auto s = migraphx::interpolate_string(input, {{"w", "world"}});
+    auto s            = migraphx::interpolate_string(input, {{"w", "world"}});
     EXPECT(s == "Hello world!");
 }
 
 TEST_CASE(interpolate_string_simple2)
 {
     std::string input = "${hello}";
-    auto s = migraphx::interpolate_string(input, {{"hello", "bye"}});
+    auto s            = migraphx::interpolate_string(input, {{"hello", "bye"}});
     EXPECT(s == "bye");
 }
 
@@ -24,28 +24,28 @@ TEST_CASE(interpolate_string_unbalanced)
 TEST_CASE(interpolate_string_extra_space)
 {
     std::string input = "${  hello  }";
-    auto s = migraphx::interpolate_string(input, {{"hello", "bye"}});
+    auto s            = migraphx::interpolate_string(input, {{"hello", "bye"}});
     EXPECT(s == "bye");
 }
 
 TEST_CASE(interpolate_string_multiple)
 {
     std::string input = "${h} ${w}!";
-    auto s = migraphx::interpolate_string(input, {{"w", "world"}, {"h", "Hello"}});
+    auto s            = migraphx::interpolate_string(input, {{"w", "world"}, {"h", "Hello"}});
     EXPECT(s == "Hello world!");
 }
 
 TEST_CASE(interpolate_string_next)
 {
     std::string input = "${hh}${ww}!";
-    auto s = migraphx::interpolate_string(input, {{"ww", "world"}, {"hh", "Hello"}});
+    auto s            = migraphx::interpolate_string(input, {{"ww", "world"}, {"hh", "Hello"}});
     EXPECT(s == "Helloworld!");
 }
 
 TEST_CASE(interpolate_string_dollar_sign)
 {
     std::string input = "$hello";
-    auto s = migraphx::interpolate_string(input, {{"hello", "bye"}});
+    auto s            = migraphx::interpolate_string(input, {{"hello", "bye"}});
     EXPECT(s == "$hello");
 }
 

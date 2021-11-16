b7595e4417cf3669d63319353db6d92daa7cebe8
turneram <alturner@amd.com>
2021-11-16 17:28:30 -0600

    Formatting

--- test/gpu/fusable_conv.cpp
+++ test/gpu/fusable_conv.cpp
@@ -10,13 +10,14 @@ TEST_CASE(conv_bias_relu)
 
     auto lit = mm->add_literal(
         migraphx::generate_literal(migraphx::shape{migraphx::shape::float_type, {64}}));
-    auto weights = mm->add_literal(migraphx::generate_literal(
-        migraphx::shape{migraphx::shape::float_type, {64, 64, 1, 1}}));
+    auto weights = mm->add_literal(
+        migraphx::generate_literal(migraphx::shape{migraphx::shape::float_type, {64, 64, 1, 1}}));
     auto input = mm->add_literal(
         migraphx::generate_literal(migraphx::shape{migraphx::shape::float_type, {1, 64, 56, 56}}));
 
     auto conv = mm->add_instruction(migraphx::make_op("convolution"), input, weights);
-    auto bnorm = mm->add_instruction(migraphx::make_op("batch_norm_inference"), conv, lit, lit, lit ,lit);
+    auto bnorm =
+        mm->add_instruction(migraphx::make_op("batch_norm_inference"), conv, lit, lit, lit, lit);
     mm->add_instruction(migraphx::make_op("relu"), bnorm);
 
     EXPECT(not test::throws([&] { p.compile(migraphx::gpu::target{}); }));

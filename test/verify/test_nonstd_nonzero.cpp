4ea24a33f2a7bbb1c14586655c69273d3f6040f6
umangyadav <umayadav@amd.com>
2021-11-24 07:50:35 -0600

    formatting

--- test/verify/test_nonstd_nonzero.cpp
+++ test/verify/test_nonstd_nonzero.cpp
@@ -11,7 +11,8 @@ struct test_nonstd_nonzero : verify_program<test_nonstd_nonzero>
         auto* mm = p.get_main_module();
         migraphx::shape s{migraphx::shape::float_type, {2, 2, 3, 3}};
         auto x = mm->add_parameter("data", s);
-        auto xt = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {3, 1, 0, 2}}}), x);
+        auto xt =
+            mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {3, 1, 0, 2}}}), x);
         auto r = mm->add_instruction(migraphx::make_op("nonzero"), xt);
         mm->add_return({r});
 

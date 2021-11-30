a67fbc117e7b8d4612158146da0984417e963ce6
umangyadav <umayadav@amd.com>
2021-11-30 09:20:22 -0600

    use constexpr for ranks in scatter

--- src/targets/gpu/device/scatter.cpp
+++ src/targets/gpu/device/scatter.cpp
@@ -10,28 +10,34 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
-argument scatter(
-    hipStream_t stream, argument result, argument arg0, argument arg1, argument arg2, int64_t axis)
+template <class Shape>
+constexpr auto get_rank(const Shape&)
+{
+    return decltype(typename Shape::hip_index{}.size()){};
+}
+
+argument scatter(hipStream_t stream, argument result, argument arg0, argument arg1, argument arg2, int64_t axis)
 {
     auto ds            = arg0.get_shape();
     auto inds          = arg1.get_shape();
     auto upds          = arg2.get_shape();
     auto axis_dim_size = ds.lens()[axis];
-    hip_visit_all(result, arg0, inds, upds)([&](auto output, auto data, auto inds1, auto upds1) {
+    hip_visit_all(result, arg0)([&](auto output, auto data) {
         auto* output_ptr     = device_cast(output.data());
         const auto* data_ptr = device_cast(data.data());
         gs_launch(stream, ds.elements())([=](auto i) __device__ { output_ptr[i] = data_ptr[i]; });
         hip_visit_all(arg1, arg2)([&](auto indices, auto update) {
-            const auto* upd_ptr     = device_cast(update.data());
             const auto* indices_ptr = device_cast(indices.data());
-            gs_launch(stream, inds.elements())([=](auto i) __device__ {
-                auto out_idx    = inds1.multi(i);
-                auto upd_idx    = upds1.index(out_idx);
-                auto idx        = indices_ptr[i];
-                idx             = idx < 0 ? idx + axis_dim_size : idx;
-                out_idx[axis]   = idx;
-                output[out_idx] = upd_ptr[upd_idx];
-            });
+            if constexpr (get_rank(update.get_shape()) == get_rank(output.get_shape())) {
+                gs_launch(stream, inds.elements())([=](auto i) __device__ {
+                                auto out_idx    = indices.get_shape().multi(i);
+                                auto upd_idx    = out_idx;
+                                auto idx        = indices_ptr[i];
+                                idx             = idx < 0 ? idx + axis_dim_size : idx;
+                                out_idx[axis]   = idx;
+                                output[out_idx] = update[upd_idx];
+                            });
+            }
         });
     });
 

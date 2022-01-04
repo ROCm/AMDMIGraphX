a6dd5c2c2acdd16acc89219af2d5d9655f41c61e
turneram <alturner@amd.com>
2022-01-04 13:25:08 -0600

    Formatting

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -2903,7 +2903,7 @@ TEST_CASE(randomnormal_generated_seed_test)
 {
     auto p1 = optimize_onnx("randomnormal_generated_seed_test.onnx");
     auto p2 = optimize_onnx("randomnormal_generated_seed_test.onnx");
-    
+
     EXPECT(p1 != p2);
 }
 
@@ -2973,7 +2973,7 @@ TEST_CASE(randomuniform_generated_seed_test)
 {
     auto p1 = optimize_onnx("randomuniform_generated_seed_test.onnx");
     auto p2 = optimize_onnx("randomuniform_generated_seed_test.onnx");
-    
+
     EXPECT(p1 != p2);
 }
 

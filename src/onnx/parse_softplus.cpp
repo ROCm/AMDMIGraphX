d5dfd3aaa29d77d9708761aefea7ee85b81cd500
turneram <alturner@amd.com>
2022-01-18 15:22:01 -0600

    Add comment to parser with softplus formula

--- src/onnx/parse_softplus.cpp
+++ src/onnx/parse_softplus.cpp
@@ -16,6 +16,7 @@ struct parse_softplus : op_parser<parse_softplus>
                           const onnx_parser::node_info& info,
                           std::vector<instruction_ref> args) const
     {
+        // Apply pointwise formula: y = ln(exp(x) + 1)
         auto mb_ones = info.add_instruction(
             migraphx::make_op("multibroadcast", {{"out_lens", args[0]->get_shape().lens()}}),
             info.add_literal(migraphx::literal{migraphx::shape{args[0]->get_shape().type()}, {1}}));

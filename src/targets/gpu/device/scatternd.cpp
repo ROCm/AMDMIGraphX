79e4518483dc6d62d63d9e6e0555793f4d63d75c
turneram <alturner@amd.com>
2022-02-01 13:15:32 -0600

    Fix gpu error

--- src/targets/gpu/device/scatternd.cpp
+++ src/targets/gpu/device/scatternd.cpp
@@ -21,24 +21,22 @@ argument scatternd(hipStream_t stream, argument result, argument arg0, argument 
         auto* output_ptr     = device_cast(output.data());
         const auto* data_ptr = device_cast(data.data());
         gs_launch(stream, ds.elements())([=](auto i) __device__ { output_ptr[i] = data_ptr[i]; });
-        // hip_visit_all(arg1)([&](auto indices) {
         arg1.visit([&](auto indices_view) {
             hip_visit_views(indices_view)([&](auto indices) {
                 const auto* updates_ptr = device_cast(updates.data());
                 const auto* indices_ptr = device_cast(indices.data());
                 gs_launch(stream, updates_shape.elements())([=](auto i) __device__ {
-                    printf("i: %i", i);
-                    auto offset       = updates_shape.multi(i).front();
+                    auto offset       = updates_shape.multi(i).at(0);
                     auto* index_start = indices_ptr + (offset * k);
                     auto* index_end   = index_start + k;
                     auto out_idx      = output_shape.multi(i);
                     std::copy(index_start, index_end, out_idx.begin());
                     if(op.reduction == "add")
-                        output[output_shape.index(out_idx)] += updates[i];
+                        output[out_idx] += updates[i];
                     else if(op.reduction == "mul")
-                        output[output_shape.index(out_idx)] *= updates[i];
+                        output[out_idx] *= updates[i];
                     else
-                        output[output_shape.index(out_idx)] = updates[i];
+                        output[out_idx] = updates[i];
                 });
             });
         });

ffb2d5c79e4233049e1f67e9cf623692e07035a4
turneram <alturner@amd.com>
2022-02-08 15:08:41 -0600

    Formatting

--- src/include/migraphx/op/scatternd.hpp
+++ src/include/migraphx/op/scatternd.hpp
@@ -44,20 +44,22 @@ struct scatternd
             args[1].visit([&](auto indices) {
                 auto updates_shape = updates.get_shape();
                 auto indices_shape = indices.get_shape();
-                auto k = indices_shape.lens().back();
-                auto q = indices_shape.lens().size();
-                auto r = output_shape.lens().size();
+                auto k             = indices_shape.lens().back();
+                auto q             = indices_shape.lens().size();
+                auto r             = output_shape.lens().size();
                 par_for(updates_shape.elements(), [&](const auto i) {
                     auto updates_idx = updates_shape.multi(i);
                     std::vector<std::size_t> indices_idx(q, 0);
-                    std::copy(updates_idx.begin(), updates_idx.begin() + q - 1, indices_idx.begin());
-                    auto* index_start = indices.data() + indices_shape.index(indices_idx.begin(), indices_idx.end());
+                    std::copy(
+                        updates_idx.begin(), updates_idx.begin() + q - 1, indices_idx.begin());
+                    auto* index_start = indices.data() +
+                                        indices_shape.index(indices_idx.begin(), indices_idx.end());
                     auto* index_end = index_start + k;
 
                     std::vector<std::size_t> out_idx(r, 0);
                     std::copy(index_start, index_end, out_idx.begin());
                     std::copy(updates_idx.begin() + q - 1, updates_idx.end(), out_idx.begin() + k);
-                    
+
                     if(reduction == "add")
                         output[output_shape.index(out_idx)] += updates[i];
                     else if(reduction == "mul")

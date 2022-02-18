06680b39ac8967868ce3dba85acbb4e2101919b1
Brian Pickrell <bpickrel@amd.com>
2022-02-17 16:32:23 -0800

    add 3 submodes of scatter op, passes new tests but doesn't parse reduction attribute

---
+++ src/include/migraphx/op/scatter_add.hpp
@@ -0,0 +1,73 @@
+#ifndef MIGRAPHX_GUARD_OPERATORS_SCATTER_ADD_HPP
+#define MIGRAPHX_GUARD_OPERATORS_SCATTER_ADD_HPP
+
+#include <array>
+#include <migraphx/check_shapes.hpp>
+#include <migraphx/stringutils.hpp>
+#include <migraphx/streamutils.hpp>
+#include <migraphx/shape_for_each.hpp>
+#include <migraphx/config.hpp>
+#include <migraphx/value.hpp>
+#include <migraphx/op/normalize_attribute.hpp>
+#include <cmath>
+#include <utility>
+
+// ScatterElement op. with "add" as the reduction attribute.  
+
+struct scatter_add
+{
+    int64_t axis = 0;
+
+    template <class Self, class F>
+    static auto reflect(Self& self, F f)
+    {
+        return pack(f(self.axis, "axis"));
+    }
+
+    value attributes() const
+    {
+        value normalize;
+        normalize["axis"] = value::array{normalize_attribute::include_min};
+        return {{"normalize_axes", normalize}};
+    }
+
+    std::string name() const { return "scatter"; }
+
+    shape normalize_compute_shape(std::vector<shape> inputs) const
+    {
+        check_shapes{inputs, *this}.has(3).standard();
+        return inputs.front();
+    }
+
+    argument compute(const shape& output_shape, std::vector<argument> args) const
+    {
+        argument result{output_shape};
+        // max dimension in axis
+        auto axis_dim_size = output_shape.lens()[axis];
+        visit_all(result, args[0], args[2])([&](auto output, auto data, auto update) {
+            std::copy(data.begin(), data.end(), output.begin());
+            args[1].visit([&](auto indices) {
+                auto ind_s = indices.get_shape();
+                shape_for_each(ind_s, [&](const auto& idx) {
+                    auto out_idx  = idx;
+                    auto index    = indices[ind_s.index(idx)];
+                    index         = (index < 0) ? index + axis_dim_size : index;
+                    out_idx[axis] = index;
+                    // reduction
+                    output[output_shape.index(out_idx)] += update[ind_s.index(idx)];
+
+                });
+            });
+        });
+
+        return result;
+    }
+
+}
+
+
+} // namespace op
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx
+
+#endif
---
+++ src/onnx/parse_scatter.cpp
@@ -0,0 +1,48 @@
+#include <migraphx/onnx/op_parser.hpp>
+#include <migraphx/instruction.hpp>
+#include <migraphx/ranges.hpp>
+#include <migraphx/make_op.hpp>
+
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
+namespace onnx {
+
+struct parse_scatter : op_parser<parse_scatter>
+{
+    std::vector<op_desc> operators() const { return {{"ScatterElements"}}; }
+
+    instruction_ref parse(const op_desc& /*opd*/,
+                          const onnx_parser& /*parser*/,
+                          const onnx_parser::node_info& info,
+                          const std::vector<instruction_ref>& args) const
+    {
+        // The ScatterElements op replaces Scatter (deprecated in Onnx) and has three
+        // possible values for the attribute "reduction", which we implement as 
+        // different structs.
+        operation op;
+ 
+        int axis = 0;
+        if(contains(info.attributes, "axis"))
+            axis = info.attributes.at("axis").i();
+        if(contains(info.attributes, "reduction"))
+        {
+            if(info.attributes.at("reduction").s() == "add")
+                op = migraphx::make_op("scatter_add", {{"axis", axis}});
+                // return info.add_instruction(migraphx::make_op("scatter_add"), args);
+            else if(info.attributes.at("reduction").s() == "mul")
+                op = migraphx::make_op("scatter_mul", {{"axis", axis}});
+                // return info.add_instruction(migraphx::make_op("scatter_mul"), args);
+            else 
+                op = migraphx::make_op("scatter_none", {{"axis", axis}});
+        }
+        else
+            op = migraphx::make_op("scatter_none", {{"axis", axis}});
+        // return info.add_instruction(migraphx::make_op("scatter_none"), args);
+
+        return info.add_instruction(op, args);
+    }
+};
+
+} // namespace onnx
+} // namespace MIGRAPHX_INLINE_NS
+} // namespace migraphx

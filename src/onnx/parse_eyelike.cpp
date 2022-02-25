29371cdaa552ba8e1d01efb3edc51d51b434c855
charlie <charlie.lin@amd.com>
2022-02-25 17:24:03 -0600

    Change to ptrdiff_t

--- src/onnx/parse_eyelike.cpp
+++ src/onnx/parse_eyelike.cpp
@@ -23,8 +23,8 @@ struct parse_eyelike : op_parser<parse_eyelike>
         {
             MIGRAPHX_THROW("EYELIKE: tensor input not of rank 2");
         }
-        auto num_rows = input_lens.front();
-        auto num_cols = input_lens.back();
+        std::ptrdiff_t num_rows = input_lens.front();
+        std::ptrdiff_t num_cols = input_lens.back();
 
         shape::type_t output_type = args[0]->get_shape().type();
         if(contains(info.attributes, "dtype"))
@@ -32,14 +32,14 @@ struct parse_eyelike : op_parser<parse_eyelike>
             output_type = get_type(info.attributes.at("dtype").i());
         }
 
-        int k = 0;
+        std::ptrdiff_t k = 0;
         if(contains(info.attributes, "k"))
         {
             k = info.attributes.at("k").i();
         }
         if(k >= 0)
         {
-            if(static_cast<size_t>(k) >= num_cols)
+            if(k >= num_cols)
             {
                 std::ostringstream oss;
                 oss << "EYELIKE: positive k out of bounds, k = " << k << " num_cols = " << num_cols;
@@ -48,7 +48,7 @@ struct parse_eyelike : op_parser<parse_eyelike>
         }
         else
         {
-            if(static_cast<size_t>(std::abs(k)) >= num_rows)
+            if(std::abs(k) >= num_rows)
             {
                 std::ostringstream oss;
                 oss << "EYELIKE: negative k out of bounds, k = " << k << " num_rows = " << num_cols;
@@ -57,22 +57,11 @@ struct parse_eyelike : op_parser<parse_eyelike>
         }
 
         std::vector<char> eyelike_mat(num_rows * num_cols, 0);
-        size_t k_abs = std::abs(k);
-        if(k >= 0)
+        for(std::ptrdiff_t i = 0; i < num_rows; ++i)
         {
-            for(size_t i = 0; i < num_rows; ++i)
-            {
-                if(i + k_abs < num_cols)
-                    eyelike_mat[(num_cols + 1) * i + k_abs] = 1.;
-            }
-        }
-        else
-        {
-            for(size_t i = 0; i < num_rows; ++i)
-            {
-                if(i >= k_abs) // i - k_abs >= 0
-                    eyelike_mat[(num_cols + 1) * i - k_abs] = 1.;
-            }
+            auto idx = i + k;
+            if(idx < num_cols and idx >= 0)
+                eyelike_mat[(num_cols + 1) * i + k] = 1.;
         }
         return info.add_literal(
             migraphx::literal{migraphx::shape{output_type, input_lens}, eyelike_mat});

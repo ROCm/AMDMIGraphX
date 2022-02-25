9956bb8a4b82f78424a03d4e8b16225f68cbc86b
turneram <alturner@amd.com>
2022-02-25 16:06:14 -0600

    Formatting

--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -517,7 +517,7 @@ struct miopen_apply
             auto args   = ins->inputs();
             args.push_back(output);
 
-            auto io_shapes              = to_shapes(args);
+            auto io_shapes = to_shapes(args);
             io_shapes.erase(io_shapes.begin());
             const std::string reduction = "none";
             auto co                     = compile_scatternd(get_context(), io_shapes, reduction);

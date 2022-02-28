881b95ae4bab35aa04f182d7ade2564f74594e10
turneram <alturner@amd.com>
2022-02-28 13:30:17 -0600

    Use bool visitor and constexpr for reverse and exclusive modes

--- src/targets/gpu/device/prefix_scan_sum.cpp
+++ src/targets/gpu/device/prefix_scan_sum.cpp
@@ -8,6 +8,15 @@ inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 namespace device {
 
+template<class F>
+MIGRAPHX_DEVICE_CONSTEXPR void visit_bool(bool b, F f)
+{
+    if (b)
+        f(std::true_type{});
+    else
+        f(std::false_type{});
+}
+
 void prefix_scan_sum(hipStream_t stream,
                      const argument& result,
                      const argument& arg,
@@ -20,29 +29,8 @@ void prefix_scan_sum(hipStream_t stream,
     auto rlens                 = result.get_shape().lens();
     rlens[axis]                = 1;
 
-    auto s      = result.get_shape();
-    auto bshape = shape{s.type(), rlens, s.strides()};
-    auto stride = s.strides()[axis];
-    auto len    = s.lens()[axis];
-
-    hip_visit_all(result, arg, result.get_shape().with_lens(rlens), bshape)(
-        [=](auto output, auto input, auto rshape, auto batch) {
-
-            if(exclusive)
-            {
-                gs_launch(stream, rshape.elements())([=](auto i) __device__ {
-                    auto input_slice  = input.begin() + batch.index(i);
-                    auto output_slice = output.begin() + batch.index(i);
-                    for(std::size_t j = 0; j < (len - 1) * stride; j += stride)
-                    {
-                        auto out_ind = reverse ? j : (len * stride) - j - stride;
-                        auto in_ind  = reverse ? j + stride : (len * stride) - j - (2 * stride);
-                        output_slice[out_ind] = input_slice[in_ind];
-                    }
-                    auto out_ind          = reverse ? (len - 1) * stride : 0;
-                    output_slice[out_ind] = 0;
-                });
-            }
+    hip_visit_all(result, arg, result.get_shape().with_lens(rlens))(
+        [=](auto output, auto input, auto rshape) {
             gs_launch(stream, rshape.elements() * block_size, block_size)(
                 [=](auto i, auto idx) __device__ {
                     const auto ridx  = rshape.multi(i / block_size);
@@ -51,32 +39,63 @@ void prefix_scan_sum(hipStream_t stream,
                         k[axis] = j;
                         return k;
                     };
-                    if(reverse)
-                    {
-                        block_scan<block_size>(
-                            idx,
-                            sum{},
-                            0,
-                            n,
-                            reverse_scan(n,
-                                         [&](auto j) {
-                                             return exclusive ? output[compute_idx(j)]
-                                                              : input[compute_idx(j)];
-                                         }),
-                            reverse_scan(n, [&](auto j, auto x) { output[compute_idx(j)] = x; }));
-                    }
-                    else
-                    {
-                        block_scan<block_size>(idx,
-                                               sum{},
-                                               0,
-                                               n,
-                                               [&](auto j) {
-                                                   return exclusive ? output[compute_idx(j)]
-                                                                    : input[compute_idx(j)];
-                                               },
-                                               [&](auto j, auto x) { output[compute_idx(j)] = x; });
-                    }
+                    visit_bool(reverse, [&](auto reverse_c) {
+                        if constexpr(reverse_c)
+                        {
+                            block_scan<block_size>(
+                                idx,
+                                sum{},
+                                0,
+                                n,
+                                reverse_scan(n,
+                                            [&](auto j) {
+                                                return input[compute_idx(j)];
+                                            }),
+                                reverse_scan(n, [&](auto j, auto x) { 
+                                    visit_bool(exclusive, [&](auto exclusive_c) {
+                                        if constexpr(exclusive_c)
+                                        {
+                                            if (j == n - 1)
+                                                output[compute_idx(j)] = 0;
+                                            if (j > 0)
+                                                output[compute_idx(j - 1)] = x;
+                                        }
+                                        else 
+                                        {
+                                            output[compute_idx(j)] = x;
+                                        }
+                                    });
+                                }));
+                        }
+                        else 
+                        {
+                            block_scan<block_size>(
+                                idx,
+                                sum{},
+                                0,
+                                n,
+                                [&](auto j) {
+                                    return input[compute_idx(j)];
+                                },
+                                [&](auto j, auto x) { 
+                                    visit_bool(exclusive, [&](auto exclusive_c) {
+                                        if constexpr(exclusive_c)
+                                        {
+                                            auto k = j + 1;
+                                            if (j == 0)
+                                                output[compute_idx(0)] = 0;
+                                            if (k < n)
+                                                output[compute_idx(k)] = x;
+                                        }
+                                        else 
+                                        {
+                                            output[compute_idx(j)] = x;
+                                        }
+                                    });
+                                });
+                        }
+                    });
+                    
                 });
         });
 }

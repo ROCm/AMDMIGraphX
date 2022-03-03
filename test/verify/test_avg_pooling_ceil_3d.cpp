00a6e6d01841fadee18709d312da949ca4e82a96
Brian Pickrell <bpickrel@amd.com>
2022-03-03 11:24:09 -0800

    fix test fail

--- test/verify/test_avg_pooling_ceil_3d.cpp
+++ test/verify/test_avg_pooling_ceil_3d.cpp
@@ -10,10 +10,11 @@ struct test_avg_pooling_ceil_3d : verify_program<test_avg_pooling_ceil_3d>
     {
         migraphx::program p;
         auto* mm = p.get_main_module();
+        
         auto input =
             mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {1, 3, 5, 5, 5}});
         auto op = migraphx::op::pooling{
-            migraphx::op::pooling_mode::average, {1, 1, 1}, {3, 3, 3}, {3, 3, 3}};
+            migraphx::op::pooling_mode::average, {1, 1, 1}, {3, 3, 3}, {3, 3, 3}, true};
         mm->add_instruction(op, input);
         return p;
     }

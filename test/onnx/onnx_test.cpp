641b28c6146a1547c27777c2050b39abc0501345
charlie <charlie.lin@amd.com>
2022-03-04 15:25:58 -0600

    Implement size ONNX operator and tests

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -4143,6 +4143,46 @@ TEST_CASE(sinh_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(size_float_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto s = migraphx::shape{migraphx::shape::float_type, {2, 3, 4}};
+    auto input_lens = s.lens();
+    mm->add_parameter("x", s);
+    int64_t size = std::accumulate(input_lens.cbegin(), input_lens.cend(), int64_t{1}, std::multiplies<>());
+    mm->add_literal(migraphx::literal{migraphx::shape::int64_type, {size}});
+
+    auto prog = optimize_onnx("size_float_test.onnx");
+    EXPECT(p == prog);
+}
+
+TEST_CASE(size_half_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto s = migraphx::shape{migraphx::shape::half_type, {3, 1}};
+    auto input_lens = s.lens();
+    mm->add_parameter("x", s);
+    int64_t size = std::accumulate(input_lens.cbegin(), input_lens.cend(), int64_t{1}, std::multiplies<>());
+    mm->add_literal(migraphx::literal{migraphx::shape::int64_type, {size}});
+    auto prog = optimize_onnx("size_half_test.onnx");
+    EXPECT(p == prog);
+}
+
+TEST_CASE(size_int_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto s = migraphx::shape{migraphx::shape::int32_type, {8, 2, 3}};
+    auto input_lens = s.lens();
+    mm->add_parameter("x", s);
+    int64_t size = std::accumulate(input_lens.cbegin(), input_lens.cend(), int64_t{1}, std::multiplies<>());
+    mm->add_literal(migraphx::literal{migraphx::shape::int64_type, {size}});
+    auto prog = optimize_onnx("size_int_test.onnx");
+    EXPECT(p == prog);
+}
+
 TEST_CASE(slice_test)
 {
     migraphx::program p;

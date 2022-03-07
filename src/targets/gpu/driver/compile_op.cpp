7e82581a79b87b5dd9428d15554c7b51757cb59d
Paul <pfultz2@yahoo.com>
2022-03-07 16:10:41 -0600

    Format

--- src/targets/gpu/driver/compile_op.cpp
+++ src/targets/gpu/driver/compile_op.cpp
@@ -14,7 +14,7 @@ struct compile_op : action<compile_op>
     {
         context ctx;
         auto inputs = p.parse_shapes(v.at("inputs"));
-        auto op = gpu::compile_op(v.at("name").to<std::string>(), ctx, inputs, v);
+        auto op     = gpu::compile_op(v.at("name").to<std::string>(), ctx, inputs, v);
         double t    = time_op(ctx, op, inputs, p.get(v, "iterations", 100));
         std::cout << op << ": " << t << "ms" << std::endl;
     }

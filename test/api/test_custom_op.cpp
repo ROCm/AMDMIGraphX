409fd2ad18841bfe48c0d803f4ca149ae9c2cfb6
Paul <pfultz2@yahoo.com>
2022-03-11 09:35:04 -0600

    Rename op

--- test/api/test_custom_op.cpp
+++ test/api/test_custom_op.cpp
@@ -2,9 +2,9 @@
 #include <migraphx/migraphx.hpp>
 #include "test.hpp"
 
-struct my_custom_op final : migraphx::experimental_custom_op_base
+struct simple_custom_op final : migraphx::experimental_custom_op_base
 {
-    virtual std::string name() const override { return "my_custom_op"; }
+    virtual std::string name() const override { return "simple_custom_op"; }
     virtual migraphx::shape compute_shape(migraphx::shapes inputs) const override
     {
         return inputs.front();
@@ -13,11 +13,11 @@ struct my_custom_op final : migraphx::experimental_custom_op_base
 
 TEST_CASE(register_custom_op)
 {
-    my_custom_op cop;
-    migraphx::register_experimental_custom_op(cop);
+    simple_custom_op simple_op;
+    migraphx::register_experimental_custom_op(simple_op);
 
-    auto op = migraphx::operation("my_custom_op");
-    EXPECT(op.name() == "my_custom_op");
+    auto op = migraphx::operation("simple_custom_op");
+    EXPECT(op.name() == "simple_custom_op");
 }
 
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

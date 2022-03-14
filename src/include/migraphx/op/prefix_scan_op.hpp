c0093e4172f92e4958a76f1b009619233a4d179f
turneram <alturner@amd.com>
2022-03-14 11:50:11 -0500

    Fix incorrect results on cpu target

--- src/include/migraphx/op/prefix_scan_op.hpp
+++ src/include/migraphx/op/prefix_scan_op.hpp
@@ -49,10 +49,23 @@ struct prefix_scan_op : op_name<Derived>
         }
     }
 
-    argument compute(const shape&, std::vector<argument> args) const
+    argument compute(const shape& output_shape, std::vector<argument> args) const
     {
-        argument result = args[0].copy();
-        auto s          = result.get_shape();
+        argument result{output_shape};
+        auto s          = args[0].get_shape();
+        if (s == output_shape)
+        {
+            result = args[0].copy();
+        }
+        else
+        {
+            visit_all(result, args[0])([&](auto output, auto input) {
+                par_for(output_shape.elements(), [&](auto i) {
+                    output[output_shape.index(i)] = input[s.index(i)];
+                });
+            });
+            s = output_shape;
+        }
         auto slice      = shape{s.type(), {s.lens()[axis]}, {s.strides()[axis]}};
         auto lens       = s.lens();
         lens[axis]      = 1;

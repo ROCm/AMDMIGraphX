e51f7e91edff4c9375dd9e798f39ffb4cab6b571
charlie <charlie.lin@amd.com>
2022-03-28 20:08:39 -0500

    Progress on refactoring pooling

--- src/include/migraphx/op/pooling.hpp
+++ src/include/migraphx/op/pooling.hpp
@@ -8,6 +8,7 @@
 #include <migraphx/streamutils.hpp>
 #include <migraphx/functional.hpp>
 #include <migraphx/literal.hpp>
+#include <migraphx/par_for.hpp>
 #include <migraphx/value.hpp>
 #include <migraphx/shape_for_each.hpp>
 #include <migraphx/int_divide.hpp>
@@ -27,6 +28,7 @@ struct pooling
     std::vector<std::size_t> stride  = {1, 1};
     std::vector<std::size_t> lengths = {1, 1};
     bool ceil_mode                   = false;
+    int lp_order                     = 2;
 
     template <class Self, class F>
     static auto reflect(Self& self, F f)
@@ -88,6 +90,135 @@ struct pooling
     {
         check_attribute_size();
         return stride.size();
+    }
+
+    struct lpnorm_pool
+    {
+        int p = 0;
+
+        lpnorm_pool() = delete;
+
+        explicit lpnorm_pool(int x) : p{x} {};
+
+        template <class T>
+        double init()
+        {
+            return 0.0;
+        }
+
+        double operator()(double x, double y) { return x + std::pow(std::abs(y), p); }
+
+        double final(double x, std::size_t) { return std::pow(x, 1. / p); }
+    };
+
+    struct avg_pool
+    {
+
+        template <class T>
+        double init()
+        {
+            return 0.0;
+        }
+
+        double operator()(double x, double y) { return x + y; }
+
+        double final(double x, std::size_t y) { return (y == 0) ? 0.0 : (x / y); }
+    };
+
+    struct max_pool
+    {
+        template <class T>
+        T init()
+        {
+            return std::numeric_limits<T>::lowest();
+        }
+
+        double operator()(double x, double y) { return std::max(x, y); }
+
+        double final(double x, std::size_t) { return (x); }
+    };
+
+    template <typename TOut, typename TIn, typename Op>
+    double calc_pooling(const TIn& data,
+                        const std::vector<std::size_t>& in_lens,
+                        const shape& in_s,
+                        const shape& win_shape,
+                        const std::vector<std::size_t>& win_start,
+                        const std::size_t pool_size,
+                        const std::vector<std::size_t>& idx_o,
+                        Op op) const
+    {
+        double output_val = op.template init<TOut>();
+        shape_for_each(win_shape, [&](auto idx_w) {
+            auto idx = idx_o;
+            std::transform(idx_w.begin(),
+                           idx_w.end(),
+                           win_start.begin(),
+                           idx.begin() + 2,
+                           [](auto ii, auto jj) { return ii + jj; });
+            if(std::all_of(idx.begin() + 2, idx.end(), [&](auto ii) { return ii >= 0; }) and
+               idx < in_lens)
+            {
+                output_val = op(output_val, data[in_s.index(idx)]);
+            }
+        });
+        return op.final(output_val, pool_size);
+    }
+
+    argument compute(const shape& output_shape, std::vector<argument> args) const
+    {
+        argument result{output_shape};
+        visit_all(result, args[0])([&](auto output, auto input) {
+            using type   = typename decltype(output)::value_type;
+            auto in_s    = input.get_shape();
+            auto in_lens = in_s.lens();
+            std::vector<std::size_t> vec_len(in_lens.begin() + 2, in_lens.end());
+
+            par_for(output_shape.elements(), [&](auto i) {
+                auto idx_o = output_shape.multi(i);
+                auto n_dim = idx_o.size();
+                std::vector<std::size_t> win_start;
+                std::vector<std::size_t> win_size;
+                for(std::size_t dim = 2; dim < n_dim; ++dim)
+                {
+                    auto d_2 = dim - 2;
+                    int start =
+                        static_cast<int>(idx_o[dim] * stride[d_2]) - static_cast<int>(padding[d_2]);
+                    int end = std::min(start + lengths[d_2], in_lens[dim]);
+                    start   = std::max(start, 0);
+                    win_start.push_back(start);
+                    win_size.push_back(end - start);
+                }
+
+                shape win_shape{output_shape.type(), win_size};
+                auto pool_size = win_shape.elements();
+                double output_val;
+                switch(mode)
+                {
+                case migraphx::op::pooling_mode::average:
+                    output_val = calc_pooling<type>(
+                        input, in_lens, in_s, win_shape, win_start, pool_size, idx_o, avg_pool{});
+                    break;
+                case migraphx::op::pooling_mode::max:
+                    output_val = calc_pooling<type>(
+                        input, in_lens, in_s, win_shape, win_start, pool_size, idx_o, max_pool{});
+                    break;
+                case migraphx::op::pooling_mode::lpnorm:
+                    output_val = calc_pooling<type>(input,
+                                                    in_lens,
+                                                    in_s,
+                                                    win_shape,
+                                                    win_start,
+                                                    pool_size,
+                                                    idx_o,
+                                                    lpnorm_pool{lp_order});
+                    break;
+                }
+                output[i] = type(output_val);
+            });
+        });
+
+        return args.back();
     }
 };
 

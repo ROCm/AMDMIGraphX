3565c14a84b2910529b2150e929ffa7a9465edd2
umangyadav <umayadav@amd.com>
2022-03-31 10:09:03 -0500

    add a error message if target is not available

--- src/register_target.cpp
+++ src/register_target.cpp
@@ -11,7 +11,13 @@ std::unordered_map<std::string, target>& target_map()
 }
 
 void register_target(const target& t) { target_map()[t.name()] = t; }
-target make_target(const std::string& name) { return target_map().at(name); }
+target make_target(const std::string& name) { 
+    if(target_map().find(name) == target_map().end()) {
+        MIGRAPHX_THROW("Requested target " + name+" is not supported");
+    }
+    return target_map().at(name); 
+    
+}
 std::vector<std::string> get_targets()
 {
     std::vector<std::string> result;

df854ea28426f03693946c2df5834cc31d9abc8b
charlie <charlie.lin@amd.com>
2022-04-06 12:38:28 -0500

    Changes wrt. comments

--- src/include/migraphx/op/pooling.hpp
+++ src/include/migraphx/op/pooling.hpp
@@ -2,6 +2,7 @@
 #define MIGRAPHX_GUARD_OPERATORS_POOLING_HPP
 
 #include <array>
+#include <type_traits>
 #include <migraphx/op/common.hpp>
 #include <migraphx/check_shapes.hpp>
 #include <migraphx/stringutils.hpp>
@@ -93,6 +94,51 @@ struct pooling
         return stride.size();
     }
 
+    // type erasure for pooling methods
+    struct abstract_pool
+    {
+        virtual double init() const = 0;
+
+        virtual double operator()(double x, double y) const = 0;
+
+        virtual double final(double, std::size_t) const = 0;
+
+        virtual ~abstract_pool() = default;
+    };
+
+    // InitType to avoid virtual function template issue
+    template <class PoolType, class InitType>
+    struct wrapper_pool : abstract_pool
+    {
+        PoolType p_method;
+
+        explicit wrapper_pool(PoolType&& x) : p_method(std::move(x)) {}
+
+        double init() const override { return p_method.template init<InitType>(); };
+
+        double operator()(double x, double y) const override { return p_method.operator()(x, y); };
+
+        double final(double x, std::size_t y) const override { return p_method.final(x, y); };
+    };
+
+    // for taking ownership of the pooling structs
+    struct alloc_pool
+    {
+        std::unique_ptr<abstract_pool> ap_ptr;
+
+        template <class PoolType, class InitType>
+        explicit alloc_pool(PoolType t, InitType)
+            : ap_ptr(std::make_unique<wrapper_pool<PoolType, InitType>>(std::move(t)))
+        {
+        }
+
+        double init() const { return ap_ptr->init(); };
+
+        double operator()(double x, double y) const { return ap_ptr->operator()(x, y); };
+
+        double final(double x, std::size_t y) const { return ap_ptr->final(x, y); };
+    };
+
     struct lpnorm_pool
     {
         int p = 0;
@@ -102,7 +148,7 @@ struct pooling
         explicit lpnorm_pool(int x) : p{x} {};
 
         template <class T>
-        double init()
+        double init() const
         {
             return 0.0;
         }
@@ -114,109 +160,91 @@ struct pooling
 
     struct avg_pool
     {
-
         template <class T>
-        double init()
+        double init() const
         {
             return 0.0;
         }
 
-        double operator()(double x, double y) { return x + y; }
+        double operator()(double x, double y) const { return x + y; }
 
-        double final(double x, std::size_t y) { return (y == 0) ? 0.0 : (x / y); }
+        double final(double x, std::size_t y) const { return (y == 0) ? 0.0 : (x / y); }
     };
 
     struct max_pool
     {
         template <class T>
-        T init()
+        T init() const
         {
             return std::numeric_limits<T>::lowest();
         }
 
-        double operator()(double x, double y) { return std::max(x, y); }
+        double operator()(double x, double y) const { return std::max(x, y); }
 
-        double final(double x, std::size_t) { return (x); }
+        double final(double x, std::size_t) const { return (x); }
     };
 
-    template <typename TOut, typename TIn, typename Op>
-    double calc_pooling(const TIn& input,
-                        const std::vector<std::size_t>& in_lens,
-                        const shape& in_s,
-                        const shape& win_shape,
-                        const std::vector<std::size_t>& win_start,
-                        const std::size_t pool_size,
-                        const std::vector<std::size_t>& idx_o,
-                        Op op) const
+    template <class Type, class Out, class In, class Op>
+    void calc_pooling(const shape& output_shape, Out& output, const In& input, const Op& op) const
     {
-        double output_val = op.template init<TOut>();
-        shape_for_each(win_shape, [&](auto idx_w) {
-            auto idx = idx_o;
-            std::transform(idx_w.begin(),
-                           idx_w.end(),
-                           win_start.begin(),
-                           idx.begin() + 2,
-                           [](auto ii, auto jj) { return ii + jj; });
-            if(std::all_of(idx.begin() + 2, idx.end(), [&](auto ii) { return ii >= 0; }) and
-               idx < in_lens)
+        auto in_s    = input.get_shape();
+        auto in_lens = in_s.lens();
+        par_for(output_shape.elements(), [&](auto i) {
+            auto idx_o = output_shape.multi(i);
+            auto n_dim = idx_o.size();
+            std::vector<std::size_t> win_start;
+            std::vector<std::size_t> win_size;
+            for(std::size_t dim = 2; dim < n_dim; ++dim)
             {
-                output_val = op(output_val, input[in_s.index(idx)]);
+                auto d_2 = dim - 2;
+                int start =
+                    static_cast<int>(idx_o[dim] * stride[d_2]) - static_cast<int>(padding[d_2]);
+                int end = std::min(start + lengths[d_2], in_lens[dim]);
+                start   = std::max(start, 0);
+                win_start.push_back(start);
+                win_size.push_back(end - start);
             }
+
+            shape win_shape{output_shape.type(), win_size};
+            auto pool_size    = win_shape.elements();
+            double output_val = op.init();
+            shape_for_each(win_shape, [&](auto idx_w) {
+                auto idx = idx_o;
+                std::transform(idx_w.begin(),
+                               idx_w.end(),
+                               win_start.begin(),
+                               idx.begin() + 2,
+                               [](auto ii, auto jj) { return ii + jj; });
+                if(std::all_of(idx.begin() + 2, idx.end(), [&](auto ii) { return ii >= 0; }) and
+                   idx < in_lens)
+                {
+                    output_val = op(output_val, input[in_s.index(idx)]);
+                }
+            });
+            output[i] = Type(op.final(output_val, pool_size));
         });
-        return op.final(output_val, pool_size);
     }
 
     argument compute(const shape& output_shape, std::vector<argument> args) const
     {
         argument result{output_shape};
         visit_all(result, args[0])([&](auto output, auto input) {
-            using type   = typename decltype(output)::value_type;
-            auto in_s    = input.get_shape();
-            auto in_lens = in_s.lens();
-            std::vector<std::size_t> vec_len(in_lens.begin() + 2, in_lens.end());
-
-            par_for(output_shape.elements(), [&](auto i) {
-                auto idx_o = output_shape.multi(i);
-                auto n_dim = idx_o.size();
-                std::vector<std::size_t> win_start;
-                std::vector<std::size_t> win_size;
-                for(std::size_t dim = 2; dim < n_dim; ++dim)
-                {
-                    auto d_2 = dim - 2;
-                    int start =
-                        static_cast<int>(idx_o[dim] * stride[d_2]) - static_cast<int>(padding[d_2]);
-                    int end = std::min(start + lengths[d_2], in_lens[dim]);
-                    start   = std::max(start, 0);
-                    win_start.push_back(start);
-                    win_size.push_back(end - start);
-                }
-
-                shape win_shape{output_shape.type(), win_size};
-                auto pool_size = win_shape.elements();
-                double output_val;
-                switch(mode)
-                {
-                case migraphx::op::pooling_mode::average:
-                    output_val = calc_pooling<type>(
-                        input, in_lens, in_s, win_shape, win_start, pool_size, idx_o, avg_pool{});
-                    break;
-                case migraphx::op::pooling_mode::max:
-                    output_val = calc_pooling<type>(
-                        input, in_lens, in_s, win_shape, win_start, pool_size, idx_o, max_pool{});
-                    break;
-                case migraphx::op::pooling_mode::lpnorm:
-                    output_val = calc_pooling<type>(input,
-                                                    in_lens,
-                                                    in_s,
-                                                    win_shape,
-                                                    win_start,
-                                                    pool_size,
-                                                    idx_o,
-                                                    lpnorm_pool{lp_order});
-                    break;
-                }
-                output[i] = type(output_val);
-            });
+            using type = typename decltype(output)::value_type;
+            std::unique_ptr<alloc_pool> ap;
+            type tmp; // for deducing type only
+            switch(mode)
+            {
+            case migraphx::op::pooling_mode::average:
+                ap = std::make_unique<alloc_pool>(avg_pool{}, tmp);
+                break;
+            case migraphx::op::pooling_mode::max:
+                ap = std::make_unique<alloc_pool>(max_pool{}, tmp);
+                break;
+            case migraphx::op::pooling_mode::lpnorm:
+                ap = std::make_unique<alloc_pool>(lpnorm_pool{lp_order}, tmp);
+                break;
+            }
+            calc_pooling<type>(output_shape, output, input, *ap);
         });
 
         return result;

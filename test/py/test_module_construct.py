5c8cd54a858122b7dddf22a810640b96307cb21f
umangyadav <umayadav@amd.com>
2022-04-11 16:37:20 -0500

    add_literal test with python array

--- test/py/test_module_construct.py
+++ test/py/test_module_construct.py
@@ -1,16 +1,25 @@
-import sys
-import migraphx
+import migraphx, array, sys
 try:
     import numpy as np
 except:
     sys.exit()
 
+def create_buffer(t, data, shape):
+    a = array.array(t, data)
+    if sys.version_info >= (3, 0):
+        m = memoryview(a.tobytes())
+        return m.cast(t, shape)
+    else:
+        m = memoryview(a.tostring())
+        return m
 
 def test_add_op():
     p = migraphx.program()
     mm = p.get_main_module()
     param_shape = migraphx.shape(lens=[3, 3], type="float")
-    x = mm.add_literal(param_shape, np.ones((3, 3), dtype='float32'))
+    # test add_literal with python array
+    x = mm.add_literal(param_shape, create_buffer('f', [1.0]*9, (3, 3)))
+    # test add_literal with numpy array
     y = mm.add_literal(param_shape, np.ones((3, 3), dtype='float32'))
     add_op = mm.add_instruction(migraphx.op("add"), [x, y])
     mm.add_return([add_op])

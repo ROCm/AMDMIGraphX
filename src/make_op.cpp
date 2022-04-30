4fc9bff71266356442bd6e08d7321ebb388c26e8
Paul <pfultz2@yahoo.com>
2022-04-29 20:20:49 -0500

    Simplify make_op

--- src/make_op.cpp
+++ src/make_op.cpp
@@ -5,40 +5,42 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
 operation make_op(const std::string& name) { return load_op(name); }
-operation make_op(const std::string& name,
-                  const std::initializer_list<std::pair<std::string, value>>& v)
+
+template<class F>
+operation make_op_generic(const std::string& name,
+                  F for_each)
 {
-    // if(not(v.is_object() or (v.empty() and v.is_array())))
-    //     MIGRAPHX_THROW("Value is not an object for make_op: " + name);
     auto op = load_op(name);
     // Merge values
     value w = op.to_value();
-    for(auto&& [key, x] : v)
-    {
+    for_each([&](const auto& key, const auto& x) {
         if(not w.contains(key))
             // NOLINTNEXTLINE(performance-inefficient-string-concatenation)
             MIGRAPHX_THROW("No key '" + key + "' in " + name);
         w.at(key) = x;
-    }
+    });
     op.from_value(w);
     return op;
+}
+
+
+operation make_op(const std::string& name,
+                  const std::initializer_list<std::pair<std::string, value>>& v)
+{
+    return make_op_generic(name, [&](auto f) {
+        for(auto&& [key, x] : v) 
+            f(key, x); 
+    });
 }
 
 operation make_op_from_value(const std::string& name, const value& v)
 {
     if(not(v.is_object() or (v.empty() and v.is_array())))
         MIGRAPHX_THROW("Value is not an object for make_op: " + name);
-    auto op = load_op(name);
-    // Merge values
-    value w = op.to_value();
-    for(auto&& x : v)
-    {
-        if(not w.contains(x.get_key()))
-            MIGRAPHX_THROW("No key '" + x.get_key() + "' in " + name);
-        w.at(x.get_key()) = x.without_key();
-    }
-    op.from_value(w);
-    return op;
+    return make_op_generic(name, [&](auto f) {
+        for(auto&& x : v)
+            f(x.get_key(), x.without_key());
+    });
 }
 
 } // namespace MIGRAPHX_INLINE_NS

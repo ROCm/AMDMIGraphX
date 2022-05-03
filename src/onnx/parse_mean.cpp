55962d59829f88e5eb3d38c9ea4140ec4020855d
Paul <pfultz2@yahoo.com>
2022-05-02 19:04:39 -0500

    Fix compiler error

--- src/onnx/parse_mean.cpp
+++ src/onnx/parse_mean.cpp
@@ -25,7 +25,7 @@ struct parse_mean : op_parser<parse_mean>
             migraphx::literal{migraphx::shape{args[0]->get_shape().type()}, {num_data}});
 
         return std::accumulate(
-            args.begin(), args.end(), args[0], [&](const auto& mean, const auto& data_i) {
+            args.begin(), args.end(), args[0], [&](const auto& mean, auto data_i) {
                 // Pre-divide each tensor element-wise by n to reduce risk of overflow during
                 // summation
                 data_i = info.add_broadcastable_binary_op("div", data_i, divisor);

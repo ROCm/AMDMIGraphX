814e7c8a986c1200d6857cac69de76fc9343e50c
umangyadav <umayadav@amd.com>
2022-05-06 18:13:45 -0500

    fix cppcheck and tidy

--- test/api/test_custom_op_gpu.cpp
+++ test/api/test_custom_op_gpu.cpp
@@ -12,7 +12,7 @@ struct simple_custom_op final : migraphx::experimental_custom_op_base
     {
         // sets first half size_bytes of the input 0, and rest of the half bytes are copied.
         float* d_output;
-        auto h_output    = reinterpret_cast<float*>(inputs[1].data());
+        auto* h_output    = reinterpret_cast<float*>(inputs[1].data());
         auto input_bytes = inputs[0].get_shape().bytes();
         auto copy_bytes  = input_bytes / 2;
         MIGRAPHX_HIP_ASSERT(hipSetDevice(0));
@@ -47,7 +47,7 @@ TEST_CASE(run_simple_custom_op)
     auto alloc         = m.add_instruction(
         migraphx::operation(
             "allocate",
-            "{\"shape\":{\"type\":\"float_type\",\"lens\":[4, 3], \"strides\":[3, 1]}}"),
+             R"({"shape":{"type":"float_type","lens":[4, 3], "strides":[3, 1]}})"),
         {});
     auto custom_kernel = m.add_instruction(migraphx::operation("simple_custom_op"), {x, alloc});
     m.add_return({custom_kernel});

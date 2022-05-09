10a0a7ea9b17a55b0189e09de0ebcb66956f7848
Paul <pfultz2@yahoo.com>
2022-05-09 12:42:07 -0500

    Use sqaure brackets

--- src/targets/cpu/lowering.cpp
+++ src/targets/cpu/lowering.cpp
@@ -352,7 +352,7 @@ struct cpu_apply
             std::transform(bind_inputs.begin(),
                            bind_inputs.end(),
                            std::back_inserter(inputs),
-                           [&](const auto& s) { return r.instructions.at(s); });
+                           [&](const auto& s) { return r.instructions[s]; });
             inputs.push_back(this->insert_allocation(ins, ins->get_shape()));
             modl->replace_instruction(ins, op, inputs);
         });

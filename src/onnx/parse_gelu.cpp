f99a3036da0d21537ae709a6d7f1c6fe54460ce9
turneram <alturner@amd.com>
2022-05-10 10:30:29 -0500

    Formatting

--- src/onnx/parse_gelu.cpp
+++ src/onnx/parse_gelu.cpp
@@ -17,18 +17,19 @@ struct parse_gelu : op_parser<parse_gelu>
                           onnx_parser::node_info info,
                           const std::vector<instruction_ref>& args) const
     {
-        if (args.size() != 1)
-            MIGRAPHX_THROW("Gelu: too many arguments. Expected 1; got " + std::to_string(args.size()));
-        
-        auto x = args.front();
-        auto x_type = x->get_shape().type();
+        if(args.size() != 1)
+            MIGRAPHX_THROW("Gelu: too many arguments. Expected 1; got " +
+                           std::to_string(args.size()));
+
+        auto x        = args.front();
+        auto x_type   = x->get_shape().type();
         auto root_inv = info.add_literal(literal{shape{x_type, {1}}, {1.0f / std::sqrt(2.0f)}});
-        auto product = info.add_broadcastable_binary_op("mul", x, root_inv);
-        auto erf = info.add_instruction(make_op("erf"), product);
-        auto one = info.add_literal(literal{shape{x_type, {1}}, {1.0f}});
-        erf = info.add_broadcastable_binary_op("add", one, erf);
-        auto half = info.add_literal(literal{shape{x_type, {1}}, {0.5f}});
-        erf = info.add_broadcastable_binary_op("mul", half, erf);
+        auto product  = info.add_broadcastable_binary_op("mul", x, root_inv);
+        auto erf      = info.add_instruction(make_op("erf"), product);
+        auto one      = info.add_literal(literal{shape{x_type, {1}}, {1.0f}});
+        erf           = info.add_broadcastable_binary_op("add", one, erf);
+        auto half     = info.add_literal(literal{shape{x_type, {1}}, {0.5f}});
+        erf           = info.add_broadcastable_binary_op("mul", half, erf);
 
         return info.add_instruction(make_op("mul"), x, erf);
     }

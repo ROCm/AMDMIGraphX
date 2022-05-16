c17b0092883cea1371fce4aeca1c362874d96275
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2022-05-16 02:38:19 -0500

    update compile_pointwise test to include op_names

--- test/gpu/jit.cpp
+++ test/gpu/jit.cpp
@@ -250,7 +250,7 @@ TEST_CASE(compile_pointwise)
 
     migraphx::gpu::context ctx;
     auto co = migraphx::gpu::compile_op(
-        "pointwise", ctx, {input, input}, {{"lambda", "[](auto x) { return x + 1; }"}});
+        "pointwise", ctx, {input, input}, {{"lambda", "[](auto x) { return x + 1; }"}, {"op_names", {"add"}}});
 
     migraphx::program p;
     auto* mm            = p.get_main_module();

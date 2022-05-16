c47f2205e279da4dbd15b1b102696f72cf0f1bd8
umangyadav <umayadav@amd.com>
2022-05-16 10:27:12 -0500

    formatting

--- examples/migraphx/custom_op_miopen_kernel/custom_op_miopen_kernel.cpp
+++ examples/migraphx/custom_op_miopen_kernel/custom_op_miopen_kernel.cpp
@@ -10,7 +10,7 @@
 
 inline miopenTensorDescriptor_t make_miopen_tensor(const migraphx::shape& s, bool pack = false)
 {
-    // TODO: normalize_standard shape for scalar 
+    // TODO: normalize_standard shape for scalar
     miopenTensorDescriptor_t t;
     MIGRAPHX_MIOPEN_ASSERT(miopenCreateTensorDescriptor(&t));
     // Convert to ints
@@ -46,15 +46,20 @@ inline miopenTensorDescriptor_t make_miopen_tensor(const migraphx::shape& s, boo
 }
 
 // create MIOpen stream handle
-inline auto make_miopen_handle(migraphx::context& ctx) {
+inline auto make_miopen_handle(migraphx::context& ctx)
+{
     MIGRAPHX_HIP_ASSERT(hipSetDevice(0));
-    auto* stream  = ctx.get_queue<hipStream_t>();
+    auto* stream = ctx.get_queue<hipStream_t>();
     miopenHandle_t out;
     MIGRAPHX_MIOPEN_ASSERT(miopenCreateWithStream(&out, stream));
     return out;
 }
 
-inline auto make_activation_descriptor(miopenActivationMode_t mode, double alpha=0, double beta=0, double gamma=0) {
+inline auto make_activation_descriptor(miopenActivationMode_t mode,
+                                       double alpha = 0,
+                                       double beta  = 0,
+                                       double gamma = 0)
+{
     miopenActivationDescriptor_t ad;
     MIGRAPHX_MIOPEN_ASSERT(miopenCreateActivationDescriptor(&ad));
     miopenSetActivationDescriptor(ad, mode, alpha, beta, gamma);
@@ -64,8 +69,9 @@ inline auto make_activation_descriptor(miopenActivationMode_t mode, double alpha
 struct abs_custom_op final : migraphx::experimental_custom_op_base
 {
     virtual std::string name() const override { return "abs_custom_op"; }
-    virtual migraphx::argument
-    compute(migraphx::context ctx, migraphx::shape output_shape, migraphx::arguments args) const override
+    virtual migraphx::argument compute(migraphx::context ctx,
+                                       migraphx::shape output_shape,
+                                       migraphx::arguments args) const override
     {
         float alpha = 1;
         float beta  = 0;
@@ -73,16 +79,10 @@ struct abs_custom_op final : migraphx::experimental_custom_op_base
         auto y_desc = make_miopen_tensor(output_shape);
         // create MIOpen stream handle
         auto miopen_handle = make_miopen_handle(ctx);
-        // make miopen activation descriptor 
+        // make miopen activation descriptor
         auto ad = make_activation_descriptor(miopenActivationABS, 0, 0, 0);
-        miopenActivationForward(miopen_handle,
-                                ad,
-                                &alpha,
-                                x_desc,
-                                args[0].data(),
-                                &beta,
-                                y_desc,
-                                args[1].data());
+        miopenActivationForward(
+            miopen_handle, ad, &alpha, x_desc, args[0].data(), &beta, y_desc, args[1].data());
         return args[1];
     }
 
@@ -103,9 +103,8 @@ int main(int argc, const char* argv[])
     migraphx::module m = p.get_main_module();
     auto x             = m.add_parameter("x", s);
     // do allocation for the output buffer for the custom_kernel
-    auto alloc = m.add_allocation(s);
-    auto custom_kernel =
-        m.add_instruction(migraphx::operation("abs_custom_op"), {x, alloc});
+    auto alloc         = m.add_allocation(s);
+    auto custom_kernel = m.add_instruction(migraphx::operation("abs_custom_op"), {x, alloc});
     m.add_return({custom_kernel});
     migraphx::compile_options options;
     // set offload copy to true for GPUs

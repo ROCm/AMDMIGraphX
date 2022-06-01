5818ebdeb64213abc8609ee1d0e62df3383a30e3
turneram <alturner@amd.com>
2022-06-01 14:14:39 -0500

    Fix pyflakes failure

--- test/onnx/gen_onnx.py
+++ test/onnx/gen_onnx.py
@@ -1,7 +1,6 @@
 # This script generates onnx files for MIGraphX onnx operator tests.
 # To generate an individual onnx file, you can use the following
 # command: python -c "import gen_onnx; gen_onnx.{test_name}_test()"
-from audioop import bias
 import numpy as np
 import onnx
 from onnx import helper

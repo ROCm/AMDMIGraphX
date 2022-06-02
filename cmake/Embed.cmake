bcac9858f117025c5758dc0f2d6242dc1dc11211
yves renier <superzerg@free.fr>
2022-06-02 15:33:53 +0200

    Fix compilation on Debian bookworm/sid (#1229)
    clang++ complained about not knowing of std::string for a file
    
    Authored-by: Yves Renier <102358016+yves-renier@users.noreply.github.com>

--- cmake/Embed.cmake
+++ cmake/Embed.cmake
@@ -39,6 +39,8 @@ function(generate_embed_source EMBED_NAME)
 
     file(WRITE "${PARSE_HEADER}" "
 #include <unordered_map>
+#include <string>
+#include <utility>
 const std::unordered_map<std::string, std::pair<const char*,const char*>>& ${EMBED_NAME}();
 ")
 

2e19d280c355915c12af74a7b6ba7939f4efaf03
Paul <pfultz2@yahoo.com>
2022-06-13 11:57:59 -0500

    Update failed tests

--- test/gpu/mlir.cpp
+++ test/gpu/mlir.cpp
@@ -18,10 +18,7 @@ using migraphx::trim;
 
 // m test_gpu_mlir && ./bin/test_gpu_mlir
 
-// [  FAILED  ] test_conv_add2
-// [  FAILED  ] test_conv_relu
 // [  FAILED  ] test_conv_relu_half
-// [  FAILED  ] test_literals
 
 struct mlir_gpu_target : migraphx::gpu::target
 {

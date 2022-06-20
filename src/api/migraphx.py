3434f0bb571bd26343a34f1d022789d2c45efe6f
umangyadav <umayadav@amd.com>
2022-06-20 09:37:43 -0500

    remove const from shape

--- src/api/migraphx.py
+++ src/api/migraphx.py
@@ -420,7 +420,7 @@ def experimental_custom_op(h):
     h.constructor('create', api.params(name='const char*'))
     h.virtual('compute',
               api.params(ctx='migraphx::context',
-                         output='const migraphx::shape&',
+                         output='migraphx::shape',
                          inputs='std::vector<migraphx::argument>'),
               returns='migraphx::argument')
     h.virtual('compute_shape',

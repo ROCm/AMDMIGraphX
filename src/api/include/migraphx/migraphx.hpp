09719fa72e15cb866b880b5cbd614360ea50335a
umangyadav <umayadav@amd.com>
2022-06-20 12:33:47 -0500

    formatting

--- src/api/include/migraphx/migraphx.hpp
+++ src/api/include/migraphx/migraphx.hpp
@@ -442,10 +442,7 @@ struct shape : MIGRAPHX_CONST_HANDLE_BASE(shape)
     shape() {}
 
     MIGRAPHX_DEPRECATED("Contructor without lifetime annotation is deprecated.")
-    shape(const migraphx_shape* p)
-    {
-        this->set_handle(p, borrow{});
-    }
+    shape(const migraphx_shape* p) { this->set_handle(p, borrow{}); }
 
     MIGRAPHX_HANDLE_CONSTRUCTOR(shape);
 

24ebc2aefc3cf5403a6497242397b921c7ac6408
umangyadav <umayadav@amd.com>
2022-06-22 09:49:37 -0500

    add device synchronize

--- test/api/test_custom_op_gpu.cpp
+++ test/api/test_custom_op_gpu.cpp
@@ -3,7 +3,7 @@
 #include <migraphx/migraphx.hpp>
 #include "test.hpp"
 
-#define MIGRAPHX_HIP_ASSERT(x) (EXPECT((x) == hipSuccess))
+#define MIGRAPHX_HIP_ASSERT(x) (EXPECT(x == hipSuccess))
 struct simple_custom_op final : migraphx::experimental_custom_op_base
 {
     virtual std::string name() const override { return "simple_custom_op"; }
@@ -11,21 +11,25 @@ struct simple_custom_op final : migraphx::experimental_custom_op_base
     compute(migraphx::context ctx, migraphx::shape, migraphx::arguments inputs) const override
     {
         // sets first half size_bytes of the input 0, and rest of the half bytes are copied.
-        float* d_output;
-        auto* h_output   = reinterpret_cast<float*>(inputs[0].data());
+        int* h_output = nullptr;
+        auto* d_output   = reinterpret_cast<int*>(inputs[0].data());
         auto input_bytes = inputs[0].get_shape().bytes();
+        auto output_ptr = inputs[1].data();
         auto copy_bytes  = input_bytes / 2;
         MIGRAPHX_HIP_ASSERT(hipSetDevice(0));
-        MIGRAPHX_HIP_ASSERT(hipMalloc(&d_output, input_bytes));
-        MIGRAPHX_HIP_ASSERT(hipMemcpyAsync(d_output,
-                                           inputs[0].data(),
+        MIGRAPHX_HIP_ASSERT(hipHostMalloc(&h_output, input_bytes));
+        MIGRAPHX_HIP_ASSERT(hipMemcpyAsync(h_output,
+                                           d_output,
                                            input_bytes,
-                                           hipMemcpyHostToDevice,
+                                           hipMemcpyDeviceToHost,
                                            ctx.get_queue<hipStream_t>()));
-        MIGRAPHX_HIP_ASSERT(hipMemset(d_output, 0, copy_bytes));
-        MIGRAPHX_HIP_ASSERT(hipMemcpy(h_output, d_output, input_bytes, hipMemcpyDeviceToHost));
-        MIGRAPHX_HIP_ASSERT(hipFree(d_output));
-        return inputs[0];
+        MIGRAPHX_HIP_ASSERT(hipDeviceSynchronize());
+        MIGRAPHX_HIP_ASSERT(hipMemset(h_output, 0, copy_bytes));
+        MIGRAPHX_HIP_ASSERT(hipDeviceSynchronize());
+        MIGRAPHX_HIP_ASSERT(hipMemcpy(output_ptr, h_output, input_bytes, hipMemcpyHostToDevice));
+        MIGRAPHX_HIP_ASSERT(hipDeviceSynchronize());
+        MIGRAPHX_HIP_ASSERT(hipHostFree(h_output));
+        return inputs[1];
     }
 
     virtual migraphx::shape compute_shape(migraphx::shapes inputs) const override
@@ -39,19 +43,25 @@ TEST_CASE(run_simple_custom_op)
     simple_custom_op simple_op;
     migraphx::register_experimental_custom_op(simple_op);
     migraphx::program p;
-    migraphx::shape s{migraphx_shape_float_type, {4, 3}};
+    migraphx::shape s{migraphx_shape_int32_type, {4, 3}};
     migraphx::module m = p.get_main_module();
     auto x             = m.add_parameter("x", s);
-    auto custom_kernel = m.add_instruction(migraphx::operation("simple_custom_op"), {x});
-    m.add_return({custom_kernel});
-    p.compile(migraphx::target("gpu"));
+    auto neg = m.add_instruction(migraphx::operation("neg"), x);
+    auto alloc = m.add_allocation(s);
+    auto custom_kernel = m.add_instruction(migraphx::operation("simple_custom_op"), {neg, alloc});
+    auto relu = m.add_instruction(migraphx::operation("relu"), custom_kernel);
+    m.add_return({relu});
+    migraphx::compile_options options;
+    options.set_offload_copy();
+    p.compile(migraphx::target("gpu"), options);
     migraphx::program_parameters pp;
-    std::vector<float> x_data(12, 1);
+    std::vector<int> x_data(12, -3);
     pp.add("x", migraphx::argument(s, x_data.data()));
     auto results = p.eval(pp);
     auto result  = results[0];
-    std::vector<float> expected_result(12, 0);
-    std::fill(expected_result.begin() + 6, expected_result.end(), 1);
+    auto result_vec = result.as_vector<int>();
+    std::vector<int> expected_result(12, 0);
+    std::fill(expected_result.begin() + 6, expected_result.end(), 3);
     EXPECT(bool{result == migraphx::argument(s, expected_result.data())});
 }
 

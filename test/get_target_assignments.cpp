5c7a26cba07509e63e4cdfca26eca07f0952e24f
Varun Sharma <varun.sharma@amd.com>
2022-06-24 18:07:33 +0000

    Rename assignment to target_assignments

--- test/get_target_assignments.cpp
+++ test/get_target_assignments.cpp
@@ -26,7 +26,7 @@
 #include <migraphx/make_op.hpp>
 #include <migraphx/program.hpp>
 #include <migraphx/register_target.hpp>
-#include <migraphx/assignment.hpp>
+#include <migraphx/target_assignments.hpp>
 
 migraphx::program create_program()
 {

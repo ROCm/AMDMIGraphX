1c7c50258042cb46fe6ef9337ad940f9562d0c28
umangyadav <umayadav@amd.com>
2022-06-29 10:29:51 -0500

    formatting

--- src/api/include/migraphx/migraphx.hpp
+++ src/api/include/migraphx/migraphx.hpp
@@ -347,7 +347,8 @@ struct interface_base : Base
             });
         };
         int status = 0;
-        std::string obj_typename = abi::__cxa_demangle(typeid(obj).name(), nullptr, nullptr, &status);
+        std::string obj_typename =
+            abi::__cxa_demangle(typeid(obj).name(), nullptr, nullptr, &status);
         this->make_handle(f, &obj, obj_typename.c_str(), copy, del, std::forward<Ts>(xs)...);
     }
 

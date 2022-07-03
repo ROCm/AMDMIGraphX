993a18f3642fcb3d6089bca51faf4a2267030bf6
Paul <pfultz2@yahoo.com>
2022-07-03 14:15:31 -0500

    Format

--- test/verify/test_conv_add_relu.cpp
+++ test/verify/test_conv_add_relu.cpp
@@ -38,11 +38,11 @@ struct test_conv_add_relu : verify_program<test_conv_add_relu>
             mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}});
         auto weights =
             mm->add_parameter("w", migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}});
-        auto bias_literal        = migraphx::literal{migraphx::shape{migraphx::shape::float_type, {4}},
-                                    {2.0f, 2.0f, 2.0f, 2.0f}};
-        auto bias      = mm->add_literal(bias_literal);
-        auto conv = mm->add_instruction(migraphx::make_op("convolution"), input, weights);
-        auto bcast_bias = mm->add_instruction(
+        auto bias_literal = migraphx::literal{migraphx::shape{migraphx::shape::float_type, {4}},
+                                              {2.0f, 2.0f, 2.0f, 2.0f}};
+        auto bias         = mm->add_literal(bias_literal);
+        auto conv         = mm->add_instruction(migraphx::make_op("convolution"), input, weights);
+        auto bcast_bias   = mm->add_instruction(
             migraphx::make_op("broadcast", {{"axis", 1}, {"out_lens", conv->get_shape().lens()}}),
             bias);
         auto bias_add = mm->add_instruction(migraphx::make_op("add"), conv, bcast_bias);

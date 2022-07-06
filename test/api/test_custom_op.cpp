b849260779760778fc300a10ee91305008a2cde2
umangyadav <umayadav@amd.com>
2022-07-06 07:23:17 -0500

    formatting, revert later

--- test/api/test_custom_op.cpp
+++ test/api/test_custom_op.cpp
@@ -44,7 +44,8 @@ struct sigmoid_custom_op final : migraphx::experimental_custom_op_base
 
     virtual migraphx::shape compute_shape(migraphx::shapes inputs) const override
     {
-        if(inputs.size() != 2) {
+        if(inputs.size() != 2)
+        {
             throw std::runtime_error("need two inputs");
         }
         CHECK(inputs[0].lengths().size() == 1);

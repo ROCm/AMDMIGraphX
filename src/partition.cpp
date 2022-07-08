687cd084e1831d917496db2f5c65da4d730d785f
Paul <pfultz2@yahoo.com>
2022-07-08 12:59:44 -0500

    Update

--- src/partition.cpp
+++ src/partition.cpp
@@ -1,5 +1,6 @@
 #include <migraphx/partition.hpp>
 #include <migraphx/program.hpp>
+#include <migraphx/register_op.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -10,11 +11,22 @@ struct partition_op
     std::string name() const { return "partition"; }
     // TODO: Implement compute_shape and compute
 };
+MIGRAPHX_REGISTER_OP(partition_op);
 
 void partition(program& p,
                module& root,
                const std::unordered_map<instruction_ref, std::string>& assignments)
 {
+    // Group instructions based on label
+    
+    // TODO: Recurse traverse submodule from root
+    for(auto ins:iterator_for(root))
+    {
+        // Rough idea of inserting submodules
+        std::string label;
+        auto sm = p.create_module(label);
+        root.add_instruction(partition_op{label}, {}, {sm});
+    }
 }
 
 } // namespace MIGRAPHX_INLINE_NS

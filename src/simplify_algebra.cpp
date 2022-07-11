f0e84612d8535f9c68c377e27fd5fbab3cf64fec
Ted Themistokleous <Ted.Themistokleous@amd.com>
2022-07-11 19:11:45 +0000

    remove skip_broadcast, and add tolerance to add_0

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -855,14 +855,14 @@ struct find_unit_ops
 {
     auto matcher() const
     {
-        auto mul_1 = match::name("mul")(match::either_arg(0, 1)(
-            match::skip_broadcasts_converts(match::has_value(1.0f)), match::any().bind("x")));
-        auto div_1 = match::name("div")(match::args(
-            match::any().bind("x"), match::skip_broadcasts_converts(match::has_value(1.0f))));
-        auto add_0 = match::name("add")(match::either_arg(0, 1)(
-            match::skip_broadcasts_converts(match::has_value(0.0f)), match::any().bind("x")));
-        auto sub_0 = match::name("sub")(match::args(
-            match::any().bind("x"), match::skip_broadcasts_converts(match::has_value(0.0f))));
+        auto mul_1 = match::name("mul")(
+            match::either_arg(0, 1)(match::has_value(1.0f), match::any().bind("x")));
+        auto div_1 =
+            match::name("div")(match::args(match::any().bind("x"), match::has_value(1.0f)));
+        auto add_0 = match::name("add")(
+            match::either_arg(0, 1)(match::has_value(0.0f, 1e-12), match::any().bind("x")));
+        auto sub_0 =
+            match::name("sub")(match::args(match::any().bind("x"), match::has_value(0.0f)));
         return match::any_of(mul_1, div_1, add_0, sub_0);
     }
 
@@ -879,12 +879,12 @@ struct find_neg_unit_ops
 {
     auto matcher() const
     {
-        auto mul_neg_1 = match::name("mul")(match::either_arg(0, 1)(
-            match::skip_broadcasts_converts(match::has_value(-1.0f)), match::any().bind("x")));
-        auto div_neg_1 = match::name("div")(match::args(
-            match::any().bind("x"), match::skip_broadcasts_converts(match::has_value(-1.0f))));
-        auto sub_0     = match::name("sub")(match::args(
-            match::skip_broadcasts_converts(match::has_value(0.0f)), match::any().bind("x")));
+        auto mul_neg_1 = match::name("mul")(
+            match::either_arg(0, 1)(match::has_value(-1.0f), match::any().bind("x")));
+        auto div_neg_1 =
+            match::name("div")(match::args(match::any().bind("x"), match::has_value(-1.0f)));
+        auto sub_0 =
+            match::name("sub")(match::args(match::has_value(0.0f), match::any().bind("x")));
         return match::any_of(mul_neg_1, div_neg_1, sub_0);
     }
 

ca630d4d3f71a582e773fd6a3825c46d1f244135
Ted Themistokleous <Ted.Themistokleous@amd.com>
2022-07-15 01:13:44 +0000

    Add additional test in verify_onnx for different datatypes using parsed in mod protobuf
    Verify the result fomr the protobuf is correct when using mixed datatypes. The goal here is to verify we cast up correctly from int32 and our results are sane.

--- test/onnx/verify_onnx.cpp
+++ test/onnx/verify_onnx.cpp
@@ -659,6 +659,36 @@ TEST_CASE(mod_test)
     EXPECT(migraphx::verify_range(result_vector, gold));
 }
 
+TEST_CASE(mod_test_different_types)
+{
+    migraphx::program p = migraphx::parse_onnx("mod_test_different_dtypes.onnx");
+    p.compile(migraphx::ref::target{});
+
+    migraphx::shape s_float{migraphx::shape::float_type, {3, 3, 3}};
+    migraphx::shape s_int{migraphx::shape::int32_type, {3, 3, 3}};
+
+    std::vector<float> a = {1.0,  -2.0, 3.0,  4.0,   -5.0,  6.0,   7.0,  -8.0, 9.0,
+                            10.0, 11.0, 12.0, 13.0,  -14.0, 15.0,  16.0, 17.0, 18.0,
+                            19.0, 20.0, 21.0, -22.0, 23.0,  -24.0, 25.0, 26.0, 27.0};
+
+    std::vector<int32_t> b = {30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+                              16, 15, 14, 13, 12, 11, 10, 9,  8,  7,  6,  5,  4};
+
+    migraphx::parameter_map p_map;
+    p_map["0"] = migraphx::argument(s_float, a.data());
+    p_map["1"] = migraphx::argument(s_int, b.data());
+
+    auto result = p.eval(p_map).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold{1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0, 8.0, 9.0,
+                            10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 1.0, 3.0, 5.0,
+                            7.0,  9.0,  1.0,  4.0,  7.0,  3.0,  1.0, 1.0, 3.0};
+
+    EXPECT(migraphx::verify_range(result_vector, gold));
+}
+
 TEST_CASE(mod_test_fmod)
 {
     migraphx::program p = migraphx::parse_onnx("mod_test_fmod.onnx");
@@ -676,6 +706,36 @@ TEST_CASE(mod_test_fmod)
     migraphx::parameter_map p_map;
     p_map["0"] = migraphx::argument(s, a.data());
     p_map["1"] = migraphx::argument(s, b.data());
+
+    auto result = p.eval(p_map).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold{1.0,  -2.0, 3.0,  4.0,  -5.0,  6.0,  7.0, -8.0, 9.0,
+                            10.0, 11.0, 12.0, 13.0, -14.0, 15.0, 1.0, 3.0,  5.0,
+                            7.0,  9.0,  1.0,  -4.0, 7.0,   -3.0, 1.0, 1.0,  3.0};
+
+    EXPECT(migraphx::verify_range(result_vector, gold));
+}
+
+TEST_CASE(mod_test_fmod_different_types)
+{
+    migraphx::program p = migraphx::parse_onnx("mod_test_fmod_different_dtypes.onnx");
+    p.compile(migraphx::ref::target{});
+
+    migraphx::shape s_float{migraphx::shape::float_type, {3, 3, 3}};
+    migraphx::shape s_int{migraphx::shape::int32_type, {3, 3, 3}};
+
+    std::vector<float> a = {1.0,  -2.0, 3.0,  4.0,   -5.0,  6.0,   7.0,  -8.0, 9.0,
+                            10.0, 11.0, 12.0, 13.0,  -14.0, 15.0,  16.0, 17.0, 18.0,
+                            19.0, 20.0, 21.0, -22.0, 23.0,  -24.0, 25.0, 26.0, 27.0};
+
+    std::vector<int32_t> b = {30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+                              16, 15, 14, 13, 12, 11, 10, 9,  8,  7,  6,  5,  4};
+
+    migraphx::parameter_map p_map;
+    p_map["0"] = migraphx::argument(s_float, a.data());
+    p_map["1"] = migraphx::argument(s_int, b.data());
 
     auto result = p.eval(p_map).back();
     std::vector<float> result_vector;

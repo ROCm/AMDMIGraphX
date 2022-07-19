de5b94b905ee57fa5c26a847bfcd6ed9f3740d5b
umangyadav <umayadav@amd.com>
2022-07-19 16:24:28 -0500

    revert test_nms

--- test/verify/test_nms.cpp
+++ test/verify/test_nms.cpp
@@ -38,21 +38,22 @@ struct test_nms : verify_program<test_nms>
 
         migraphx::shape scores_s{migraphx::shape::float_type, {1, 1, 6}};
         std::vector<float> scores_vec = {0.9, 0.75, 0.6, 0.95, 0.5, 0.3};
-        auto boxes_l                  = mm->add_parameter("boxes", boxes_s);
-        auto scores_l                 = mm->add_literal(migraphx::literal(scores_s, scores_vec));
-        auto max_out_l                = mm->add_literal(int64_t{4});
-        auto iou_threshold            = mm->add_literal(0.5f);
-        auto score_threshold          = mm->add_literal(0.0f);
-        auto relu                     = mm->add_instruction(migraphx::make_op("relu"), boxes_l);
+
+        auto boxes_l         = mm->add_parameter("boxes", boxes_s);
+        auto scores_l        = mm->add_literal(migraphx::literal(scores_s, scores_vec));
+        auto max_out_l       = mm->add_literal(int64_t{4});
+        auto iou_threshold   = mm->add_literal(0.5f);
+        auto score_threshold = mm->add_literal(0.0f);
+
         auto r =
             mm->add_instruction(migraphx::make_op("nonmaxsuppression", {{"center_point_box", 1}}),
-                                relu,
+                                boxes_l,
                                 scores_l,
                                 max_out_l,
                                 iou_threshold,
                                 score_threshold);
-        auto relu2 = mm->add_instruction(migraphx::make_op("relu"), r);
-        mm->add_return({relu2});
+        mm->add_return({r});
+
         return p;
     }
 };

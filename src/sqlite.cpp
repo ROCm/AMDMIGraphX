430c5196d835755b47f7613df4cf472a0f4b6c22
jungpark-mlir <jungwook.park@amd.com>
2022-07-20 10:33:14 +0100

    tidy
    WA to avoid signed bitwise

--- src/sqlite.cpp
+++ src/sqlite.cpp
@@ -84,8 +84,9 @@ sqlite sqlite::read(const fs::path& p)
 sqlite sqlite::write(const fs::path& p)
 {
     sqlite r;
+    uint32_t flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
     r.impl = std::make_shared<sqlite_impl>();
-    r.impl->open(p, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+    r.impl->open(p, flags);
     return r;
 }
 

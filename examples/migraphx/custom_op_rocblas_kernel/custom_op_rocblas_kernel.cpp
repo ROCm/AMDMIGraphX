4a84007c75bfdfebb0c36c05ec41ac636a7325af
umangyadav <umayadav@amd.com>
2022-08-04 12:58:24 -0500

    update examples to use runs_on_offload_target method

--- examples/migraphx/custom_op_rocblas_kernel/custom_op_rocblas_kernel.cpp
+++ examples/migraphx/custom_op_rocblas_kernel/custom_op_rocblas_kernel.cpp
@@ -51,6 +51,14 @@ rocblas_handle create_rocblas_handle_ptr(migraphx::context& ctx)
 struct sscal_custom_op final : migraphx::experimental_custom_op_base
 {
     virtual std::string name() const override { return "sscal_custom_op"; }
+
+    // flag to identify whether custom op runs on the GPU or on the host.
+    // Based on this flag MIGraphX would inject necessary copies to and from GPU for the input and output 
+    // buffers as necessary. Therefore, compute() method shouldn't do any copies itself. 
+    // Also, therefore if custom_op runs on GPU then it can assume its input buffers are in GPU memory, and 
+    // similarly for the host 
+    virtual bool runs_on_offload_target() const override { return true;}
+
     virtual migraphx::argument compute(migraphx::context ctx,
                                        migraphx::shape output_shape,
                                        migraphx::arguments args) const override

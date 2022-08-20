07f37b4c45d940ffa306f3d4621230b131cde77e
Paul <pfultz2@yahoo.com>
2022-08-19 22:35:04 -0500

    Use recipriocal

--- src/targets/gpu/kernels/include/migraphx/kernels/layernorm.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/layernorm.hpp
@@ -51,7 +51,7 @@ __device__ void generic_binary_layernorm(
         constexpr auto relements = r.template elements<Input1>();
         auto means = r.reduce(op::sum{}, make_array<vec_type<value_type>>(0, 0), [&](auto x1, auto x2) {
             auto x = op(x1, x2);
-            return make_array(x, x * x) / vec_type<value_type>{relements};
+            return make_array(x, x * x) * vec_type<value_type>{1.0 / relements};
         })(input1, input2);
 
         auto mean_x  = means[0];

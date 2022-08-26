43887ec30c75a16a302e79ea17caba415a929a47
Paul <pfultz2@yahoo.com>
2022-08-25 21:17:34 -0500

    Format

--- src/targets/gpu/jit/concat.cpp
+++ src/targets/gpu/jit/concat.cpp
@@ -63,9 +63,8 @@ struct concat_compiler : compiler<concat_compiler>
 
     static std::size_t get_concat_elements(const std::vector<shape>& inputs)
     {
-        auto total = std::accumulate(inputs.begin(), inputs.end(), 0, [](auto x, auto s) {
-            return x + s.elements();
-        });
+        auto total = std::accumulate(
+            inputs.begin(), inputs.end(), 0, [](auto x, auto s) { return x + s.elements(); });
         return total / inputs.size();
     }
 

20b9c7b080f7ec92dccd17cc00060fbf270c25d6
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2022-08-30 13:56:01 -0500

    update eps value type

--- src/targets/gpu/kernels/include/migraphx/kernels/layernorm.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/layernorm.hpp
@@ -58,7 +58,7 @@ __device__ void generic_binary_layernorm(
         auto mean_x   = means[0];
         auto mean_x2  = means[1];
         auto variance = mean_x2 - (mean_x * mean_x);
-        auto eps_val  = static_cast<value_type>(eps);
+        value_type eps_val  = eps; // implicit conversion for eps
 
         r.inner([&](auto& y, auto x1, auto x2, auto... xs) {
             auto x = op(x1, x2);

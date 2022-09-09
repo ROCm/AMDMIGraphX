26c0dac67a5390c63c19f9fe1e0d80aade38183d
umangyadav <umayadav@amd.com>
2022-09-09 08:53:15 -0500

    formatting

--- src/include/migraphx/streamutils.hpp
+++ src/include/migraphx/streamutils.hpp
@@ -60,7 +60,8 @@ inline stream_range_container<Range> stream_range(const Range& r)
 
 namespace detail {
 
-inline void stream_write_value_impl(rank<3>, std::ostream& os, const migraphx::value::binary& binary_object)
+inline void
+stream_write_value_impl(rank<3>, std::ostream& os, const migraphx::value::binary& binary_object)
 {
     os << "{binary_object: " << binary_object.size() << "}";
 }

ee519dfb394fcf27cf4f2ac153717654cda67b7b
umangyadav <umayadav@amd.com>
2022-09-16 01:45:18 +0000

    formatting

--- test/gpu/fuse_ops.cpp
+++ test/gpu/fuse_ops.cpp
@@ -11,26 +11,30 @@ void run_pass(migraphx::program& p)
     migraphx::run_passes(p, {migraphx::gpu::fuse_ops{}, migraphx::dead_code_elimination{}});
 }
 
-TEST_CASE(test_add_sigmoid) {
-    migraphx::program p1; 
+TEST_CASE(test_add_sigmoid)
+{
+    migraphx::program p1;
     {
         auto* mm = p1.get_main_module();
-        auto s = migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}};
-        auto x = mm->add_parameter("x", s);
-        auto y = mm->add_parameter("y", s);
-        auto add_buffer = mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
+        auto s   = migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}};
+        auto x   = mm->add_parameter("x", s);
+        auto y   = mm->add_parameter("y", s);
+        auto add_buffer =
+            mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
         auto add = mm->add_instruction(migraphx::make_op("gpu::add"), x, y, add_buffer);
-        auto sigmoid_buffer = mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
+        auto sigmoid_buffer =
+            mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
         mm->add_instruction(migraphx::make_op("gpu::sigmoid"), add, sigmoid_buffer);
     }
     run_pass(p1);
     migraphx::program p2;
     {
         auto* mm = p2.get_main_module();
-        auto s = migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}};
-        auto x = mm->add_parameter("x", s);
-        auto y = mm->add_parameter("y", s);
-        auto buffer = mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
+        auto s   = migraphx::shape{migraphx::shape::float_type, {4, 3, 3, 3}};
+        auto x   = mm->add_parameter("x", s);
+        auto y   = mm->add_parameter("y", s);
+        auto buffer =
+            mm->add_instruction(migraphx::make_op("allocate", {{"shape", migraphx::to_value(s)}}));
         mm->add_instruction(migraphx::make_op("gpu::add_sigmoid"), {x, y, buffer});
     }
     EXPECT(p1 == p2);

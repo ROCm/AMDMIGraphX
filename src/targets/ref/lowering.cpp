a9c0252a2b14c5cf99f1e087ca1560d559aaff36
charlie <charlie.lin@amd.com>
2022-09-30 10:46:57 -0500

    Revert ref_conv changes Not needed, special case with dynamic padding

--- src/targets/ref/lowering.cpp
+++ src/targets/ref/lowering.cpp
@@ -232,9 +232,8 @@ struct ref_convolution : auto_register_op<ref_convolution<Op>>
         return op.normalize_compute_shape(inputs);
     }
 
-    argument compute(context&, migraphx::dyn_output dyn_output, std::vector<argument> args) const
+    argument compute(context&, shape output_shape, std::vector<argument> args) const
     {
-        shape output_shape;
         std::vector<std::size_t> padding;
         if(op.padding_mode != op::padding_mode_t::default_)
         {
@@ -249,8 +248,12 @@ struct ref_convolution : auto_register_op<ref_convolution<Op>>
         }
         else
         {
-            padding      = op.padding;
-            output_shape = dyn_output.computed_shape;
+            padding = op.padding;
+            if(output_shape.dynamic())
+            {
+                output_shape =
+                    op.normalize_compute_shape({args.at(0).get_shape(), args.at(1).get_shape()});
+            }
         }
 
         argument result{output_shape};

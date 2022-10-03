a43013ce8b72ef9a5dd4cb60265212809bd03e8a
umangyadav <umayadav@amd.com>
2022-10-03 19:23:11 +0000

    formatting

--- src/targets/gpu/quant_convolution.cpp
+++ src/targets/gpu/quant_convolution.cpp
@@ -157,19 +157,19 @@ shape miopen_quant_convolution::find(context& ctx,
     int algo_count = 1;
     miopenConvAlgoPerf_t perf;
     status = miopenFindConvolutionForwardAlgorithm(ctx.get_stream().get_miopen(),
-                                                        x_desc.get(),
-                                                        x.implicit(),
-                                                        w_desc.get(),
-                                                        w.implicit(),
-                                                        cd.get(),
-                                                        y_desc.get(),
-                                                        y.implicit(),
-                                                        1,
-                                                        &algo_count,
-                                                        &perf,
-                                                        workspace.implicit(),
-                                                        workspace_size,
-                                                        false);
+                                                   x_desc.get(),
+                                                   x.implicit(),
+                                                   w_desc.get(),
+                                                   w.implicit(),
+                                                   cd.get(),
+                                                   y_desc.get(),
+                                                   y.implicit(),
+                                                   1,
+                                                   &algo_count,
+                                                   &perf,
+                                                   workspace.implicit(),
+                                                   workspace_size,
+                                                   false);
     if(status != miopenStatusSuccess)
         MIGRAPHX_THROW("MIOpen Quant Convolution: find convolution failed");
     algo = perf.fwd_algo;

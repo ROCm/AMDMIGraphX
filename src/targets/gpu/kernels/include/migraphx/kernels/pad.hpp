056b100fbdc336da808e505e1a664da56607372d
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2022-10-10 13:22:54 -0500

    add pad kernel

---
+++ src/targets/gpu/kernels/include/migraphx/kernels/pad.hpp
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef MIGRAPHX_GUARD_KERNELS_PAD_HPP
+#define MIGRAPHX_GUARD_KERNELS_PAD_HPP
+
+#include <migraphx/kernels/clamp.hpp>
+#include <migraphx/kernels/shape.hpp>
+#include <migraphx/kernels/index.hpp>
+#include <migraphx/kernels/print.hpp>
+
+namespace migraphx {
+
+template <class T, class U, class V, class W, class X, class Y>
+__device__ void pad(const T& idx, const U& offsets, const V& bounds, const W& input, X& output, const Y& pad_val)
+{
+    // using type = typename decltype(output)::value_type;
+    // W device_val = pad_clamp<host_type<W>>(pad_val);
+    auto output_shape = output.get_shape();
+    idx.global_stride(output_shape.elements(), [&](auto i) {
+        auto multi = output_shape.multi(i);
+
+        for(std::size_t j = 0; j < offsets.size(); j++)
+        {
+            if(multi[j] < offsets[j] or multi[j] >= bounds[j])
+            {
+                output[multi] = pad_val;
+                return;
+            }
+        }            
+        output[multi] = input[multi - offsets];
+    });
+}
+
+} // namespace migraphx
+#endif

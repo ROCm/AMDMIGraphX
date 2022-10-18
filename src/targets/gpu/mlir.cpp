af0da7a74287a4062f929e79f5a73a1c6755f024
jungpark-mlir <jungwook.park@amd.com>
2022-10-18 14:38:35 +0100

    format

--- src/targets/gpu/mlir.cpp
+++ src/targets/gpu/mlir.cpp
@@ -715,10 +715,7 @@ void use(T&)
 {
 }
 
-code_object_op compile_mlir(const module&)
-{
-    return {};
-}
+code_object_op compile_mlir(const module&) { return {}; }
 
 code_object_op compile_mlir(const context&, module m, const std::vector<instruction_ref>&)
 {

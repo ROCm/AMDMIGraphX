ccd6df556a3f9c77bc3351efd49283af8c5cd6bb
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2022-10-27 08:26:19 -0500

    Add JIT pad (#1411)
    updated GPU pad to now use JIT version.
    added range functions for JIT kernels.

--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -104,7 +104,6 @@ struct miopen_apply
         add_extend_op("lrn");
         add_extend_op("multinomial");
         add_extend_op("nonzero");
-        add_extend_op("pad");
         add_extend_op("pooling");
         add_extend_op("prefix_scan_sum");
         add_extend_op("reverse");

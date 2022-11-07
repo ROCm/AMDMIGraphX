e40f2e7e53135aabf0d7bef5e17e0809d09a4c79
charlie <charlie.lin@amd.com>
2022-11-07 15:06:52 -0600

    dyn global

--- src/include/migraphx/op/pooling.hpp
+++ src/include/migraphx/op/pooling.hpp
@@ -50,6 +50,9 @@ struct pooling
     bool ceil_mode                   = false;
     int lp_order                     = 2;
 
+    // Global pooling with dynamic shape input
+    bool dyn_global                  = false;
+
     template <class Self, class F>
     static auto reflect(Self& self, F f)
     {
@@ -58,7 +61,8 @@ struct pooling
                     f(self.stride, "stride"),
                     f(self.lengths, "lengths"),
                     f(self.ceil_mode, "ceil_mode"),
-                    f(self.lp_order, "lp_order"));
+                    f(self.lp_order, "lp_order"),
+                    f(self.dyn_global, "dyn_global"));
     }
 
     std::string name() const { return "pooling"; }
@@ -77,6 +81,7 @@ struct pooling
     shape normalize_compute_shape(std::vector<shape> inputs) const
     {
         check_shapes{inputs, *this, true}.has(1);
+        check_attribute_size();
 
         const shape& input = inputs.at(0);
         auto padding_size  = padding.size();
@@ -117,33 +122,48 @@ struct pooling
             auto input_dyn_dims = input.dyn_dims();
             std::vector<shape::dynamic_dimension> output_dyn_dims(input_dyn_dims.begin(),
                                                                   input_dyn_dims.begin() + 2);
-            auto min_spatial_dims = calc_spatial_dim_out(input.min_lens());
-            auto max_spatial_dims = calc_spatial_dim_out(input.max_lens());
-            auto opt_spatial_dims = calc_spatial_dim_out(input.opt_lens());
-            for(size_t i = 0; i < kdims; ++i)
+            if(dyn_global)
             {
-                output_dyn_dims.push_back(shape::dynamic_dimension{
-                    min_spatial_dims[i], max_spatial_dims[i], opt_spatial_dims[i]});
+                for(size_t i = 0; i < kdims; ++i)
+                {
+                    output_dyn_dims.push_back(shape::dynamic_dimension{1, 1, 1});
+                }
+                return {input.type(), output_dyn_dims};
             }
-            return {input.type(), output_dyn_dims};
+            else
+            {
+                auto min_spatial_dims = calc_spatial_dim_out(input.min_lens());
+                auto max_spatial_dims = calc_spatial_dim_out(input.max_lens());
+                auto opt_spatial_dims = calc_spatial_dim_out(input.opt_lens());
+                for(size_t i = 0; i < kdims; ++i)
+                {
+                    output_dyn_dims.push_back(shape::dynamic_dimension{
+                        min_spatial_dims[i], max_spatial_dims[i], opt_spatial_dims[i]});
+                }
+                return {input.type(), output_dyn_dims};
+            }
         }
         else
         {
             auto input_lens = input.lens();
 
             std::vector<std::size_t> output_lens(input_lens.begin(), input_lens.begin() + 2);
-
-            auto output_spatial_lens = calc_spatial_dim_out(input_lens);
-            output_lens.insert(
-                output_lens.end(), output_spatial_lens.begin(), output_spatial_lens.end());
-            return inputs[0].with_lens(output_lens);
+            if(dyn_global)
+            {
+                for(size_t i = 0; i < kdims; ++i)
+                {
+                    output_lens.push_back(1);
+                }
+                return {input.type(), output_lens};
+            }
+            else
+            {
+                auto output_spatial_lens = calc_spatial_dim_out(input_lens);
+                output_lens.insert(
+                    output_lens.end(), output_spatial_lens.begin(), output_spatial_lens.end());
+                return inputs[0].with_lens(output_lens);
+            }
         }
-    }
-
-    size_t kdims() const
-    {
-        check_attribute_size();
-        return stride.size();
     }
 
     struct lpnorm_pool
@@ -192,7 +212,7 @@ struct pooling
     };
 
     template <class Type, class Out, class In, class Op>
-    void calc_pooling(const shape& output_shape, Out& output, const In& input, Op op) const
+    void calc_pooling(const shape& output_shape, Out& output, const In& input, const std::vector<std::size_t>& kernel_dims, Op op) const
     {
         auto in_s    = input.get_shape();
         auto in_lens = in_s.lens();
@@ -206,7 +226,7 @@ struct pooling
                 auto d_2 = dim - 2;
                 int start =
                     static_cast<int>(idx_o[dim] * stride[d_2]) - static_cast<int>(padding[d_2]);
-                int end = std::min(start + lengths[d_2], in_lens[dim]);
+                int end = std::min(start + kernel_dims[d_2], in_lens[dim]);
                 start   = std::max(start, 0);
                 win_start.push_back(start);
                 win_size.push_back(end - start);
@@ -235,18 +255,28 @@ struct pooling
     argument compute(const dyn_output& dyn_out, std::vector<argument> args) const
     {
         argument result{dyn_out.computed_shape};
+        auto input_lens = args[0].get_shape().lens();
+        std::vector<std::size_t> kernel_dims;
+        if(dyn_global)
+        {
+            kernel_dims.insert(kernel_dims.end(), input_lens.begin()+2, input_lens.end());
+        }
+        else
+        {
+            kernel_dims = this->lengths;
+        }
         visit_all(result, args[0])([&](auto output, auto input) {
             using type = typename decltype(output)::value_type;
             switch(mode)
             {
             case migraphx::op::pooling_mode::average:
-                calc_pooling<type>(dyn_out.computed_shape, output, input, avg_pool{});
+                calc_pooling<type>(dyn_out.computed_shape, output, input, kernel_dims, avg_pool{});
                 break;
             case migraphx::op::pooling_mode::max:
-                calc_pooling<type>(dyn_out.computed_shape, output, input, max_pool{});
+                calc_pooling<type>(dyn_out.computed_shape, output, input, kernel_dims, max_pool{});
                 break;
             case migraphx::op::pooling_mode::lpnorm:
-                calc_pooling<type>(dyn_out.computed_shape, output, input, lpnorm_pool{lp_order});
+                calc_pooling<type>(dyn_out.computed_shape, output, input, kernel_dims, lpnorm_pool{lp_order});
                 break;
             }
         });

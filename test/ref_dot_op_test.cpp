ab148dbd08ec471889c8178411c6c1cee37374b3
charlie <charlie.lin@amd.com>
2022-12-06 12:29:40 -0600

    Fix dynamic test

--- test/ref_dot_op_test.cpp
+++ test/ref_dot_op_test.cpp
@@ -1250,7 +1250,8 @@ TEST_CASE(dot_dyn_4D_test)
     migraphx::program p;
 
     auto* mm = p.get_main_module();
-    migraphx::shape a_shape{migraphx::shape::float_type, {1, 1, 4, 5}};
+    migraphx::shape a_shape{migraphx::shape::float_type,
+                            {{1, 1, 0}, {1, 1, 0}, {4, 6, 4}, {5, 5, 0}}};
     auto al = mm->add_parameter("a", a_shape);
     migraphx::shape b_shape{migraphx::shape::float_type, {1, 1, 5, 3}};
     auto bl = mm->add_parameter("b", b_shape);

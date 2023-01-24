63f224a2e446787165cbc670ce61abee90377aa1
Ted Themistokleous <ted.themistokleous@amd.com>
2023-01-24 16:14:06 -0500

    Cleanup spacing for if tests

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -2663,14 +2663,19 @@ TEST_CASE(if_else_test_inlined)
 {
     migraphx::program p;
     auto* mm = p.get_main_module();
+
     migraphx::shape sc{migraphx::shape::bool_type, {1}};
     mm->add_literal(migraphx::literal(sc, {0}));
+
     migraphx::shape s{migraphx::shape::float_type, {2, 3}};
     std::vector<float> ones(s.elements(), 1.0f);
     mm->add_literal(s, ones);
+
     std::vector<float> rand = {0.811412, -0.949771, -0.169276, 0.36552, -0.14801, 2.07061};
     auto l2                 = mm->add_literal(s, rand);
+
     mm->add_parameter("x", s);
+
     auto y  = mm->add_parameter("y", s);
     auto re = mm->add_instruction(migraphx::make_op("mul"), y, l2);
     mm->add_return({re});
@@ -2819,15 +2824,21 @@ TEST_CASE(if_then_test_inlined)
 {
     migraphx::program p;
     auto* mm = p.get_main_module();
+
     migraphx::shape sc{migraphx::shape::bool_type, {1}};
     mm->add_literal(migraphx::literal(sc, {1}));
+
     migraphx::shape s{migraphx::shape::float_type, {2, 3}};
     std::vector<float> ones(s.elements(), 1.0f);
+
     auto l1                 = mm->add_literal(s, ones);
     std::vector<float> rand = {-1.26487, -2.42279, 0.990835, 1.63072, 0.812238, -0.174946};
+
     mm->add_literal(s, rand);
+
     auto x = mm->add_parameter("x", s);
     mm->add_parameter("y", s);
+
     auto rt = mm->add_instruction(migraphx::make_op("add"), x, l1);
     mm->add_return({rt});
 

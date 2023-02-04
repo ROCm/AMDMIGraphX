85bb27304bc651cdfbb7dbbc3e1e5cc5245fe260
Paul <pfultz2@yahoo.com>
2023-02-04 10:22:27 -0600

    Format

--- test/simplify_algebra_test.cpp
+++ test/simplify_algebra_test.cpp
@@ -577,7 +577,7 @@ TEST_CASE(simplify_inner_broadcast_scalar)
     {
         auto x    = m2.add_parameter("x", {migraphx::shape::int32_type, {1, 384}});
         auto y    = m2.add_parameter("y", {migraphx::shape::int32_type, {1, 1}});
-        auto yb  = m2.add_instruction(migraphx::op::multibroadcast{{1, 384}}, y);
+        auto yb   = m2.add_instruction(migraphx::op::multibroadcast{{1, 384}}, y);
         auto sum  = m2.add_instruction(migraphx::make_op("add"), x, yb);
         auto sumb = m2.add_instruction(b, sum);
         m2.add_instruction(pass_op{}, sumb);

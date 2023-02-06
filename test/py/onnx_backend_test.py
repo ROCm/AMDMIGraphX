3e9daa6c3969f71b85f3d08b77c0ce3cb6989204
Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
2023-02-06 18:02:21 -0500

    Add disable tests for one case that breaks in onnx v1.12.0 (#1461)
    * Add python 3.10 in search list
    
    * Add disable tests for one case that breaks in onnx v1.12.0
    
    currently breaking for CI but we're not relying on CPU right now. Seems to
    be on the onnx side of this error.
    
    * Revert "Add Ubuntu 22.04 to CI"
    
    This reverts commit f950b9e9ee600d8bc2588da1063e25a23baca3a6.
    
    ---------
    
    Co-authored-by: Chris Austen <christopher.austen@amd.com>
    Co-authored-by: Chris Austen <causten@users.noreply.github.com>

--- test/py/onnx_backend_test.py
+++ test/py/onnx_backend_test.py
@@ -94,6 +94,9 @@ def disabled_tests_onnx_1_8_1(backend_test):
     backend_test.exclude(r'test_unsqueeze_unsorted_axes_cpu')
 
 
+def disabled_tests_onnx_1_12_0(backend_test):
+    backend_test.exclude(r'test_scatter_elements_with_duplicate_indices_cpu')
+
 def disabled_tests_onnx_1_10_0(backend_test):
     # unsupported shape attributes
     backend_test.exclude(r'test_shape_end_1_cpu')
@@ -326,6 +329,9 @@ def create_backend_test(testname=None, target_device=None):
 
         if version.parse(onnx.__version__) >= version.parse("1.10.0"):
             disabled_tests_onnx_1_10_0(backend_test)
+            
+        if version.parse(onnx.__version__) => version.parse("1.12.0"):
+            disabled_tests_onnx_1_12_0(backend_test)
 
 
 # import all test cases at global scope to make

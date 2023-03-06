b1c60070cb853340a6d3afd3ffce7f76719b533a
Shiv <Shivad.Bhavsar@amd.com>
2023-03-06 11:45:23 -0800

    revert scalar update

--- src/include/migraphx/op/contiguous.hpp
+++ src/include/migraphx/op/contiguous.hpp
@@ -48,7 +48,7 @@ struct contiguous
     {
         check_shapes{inputs, *this, true}.has(1);
         auto s0 = inputs.front();
-        if(s0.dynamic() or s0.scalar())
+        if(s0.dynamic())
         {
             return s0;
         }

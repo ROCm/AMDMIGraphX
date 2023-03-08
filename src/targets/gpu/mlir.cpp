6571397ba6b84da2aef72020337f5691e05ca729
Manupa Karunaratne <manupa.karunaratne@amd.com>
2023-03-08 13:19:50 +0000

    [MLIR] Adds a runtime switch to trigger MLIR
    Currently, in order to use MLIR, we need to rebuild
    the MIGraphX with a CMake flag.
    
    This commit changes this have an switched polarity
    in the cmake flag -- i.e. MIGRAPHX_DISABLE_MLIR_BUILD.
    Then adds ENV var MIGRAPHX_MLIR to enable MLIR offloads
    in the fuse_mlir pass.

--- src/targets/gpu/mlir.cpp
+++ src/targets/gpu/mlir.cpp
@@ -24,7 +24,7 @@
 #include "migraphx/make_op.hpp"
 #include <migraphx/gpu/mlir.hpp>
 
-#ifdef MIGRAPHX_MLIR
+#ifndef MIGRAPHX_DISABLE_MLIR_BUILD
 #include <mlir-c/IR.h>
 #include <mlir-c/BuiltinAttributes.h>
 #include <mlir-c/BuiltinTypes.h>
@@ -35,7 +35,7 @@
 #include <mutex>
 #if !defined(MLIR_MIGRAPHX_DIALECT_API_VERSION) || MLIR_MIGRAPHX_DIALECT_API_VERSION != 3
 #warning "Incompatible version of rocMLIR library used, disabling"
-#undef MIGRAPHX_MLIR
+#define MIGRAPHX_DISABLE_MLIR_BUILD
 #else
 #include <mlir-c/RegisterRocMLIR.h>
 #endif
@@ -62,7 +62,7 @@ namespace gpu {
 
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_TRACE_MLIR);
 
-#ifdef MIGRAPHX_MLIR
+#ifndef MIGRAPHX_DISABLE_MLIR_BUILD
 template <class T, class F, F f> // NOLINT
 struct mlir_handle
 {

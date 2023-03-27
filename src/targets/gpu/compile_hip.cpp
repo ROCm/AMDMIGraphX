41bbb714f7a7bbc4c97017380194148f2321207d
Paul <pfultz2@yahoo.com>
2023-03-27 09:54:51 -0500

    Fix tidy

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -242,7 +242,7 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
 #else // MIGRAPHX_USE_HIPRTC
 
 std::vector<std::vector<char>>
-compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file>, std::string, const std::string&)
+compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file> srcs, std::string, const std::string&) // NOLINT
 {
     MIGRAPHX_THROW("Not using hiprtc");
 }

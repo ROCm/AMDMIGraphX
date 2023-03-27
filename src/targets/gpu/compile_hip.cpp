1ed5095b2317ab617bae1db366e7ee971d5afeed
Paul <pfultz2@yahoo.com>
2023-03-27 13:41:55 -0500

    Format

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -241,8 +241,9 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
 
 #else // MIGRAPHX_USE_HIPRTC
 
-std::vector<std::vector<char>>
-compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file> srcs, std::string, const std::string&) // NOLINT
+std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file> srcs,
+                                                           std::string,
+                                                           const std::string&) // NOLINT
 {
     MIGRAPHX_THROW("Not using hiprtc");
 }

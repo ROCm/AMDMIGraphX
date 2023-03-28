725b61d9fa2e7722b9b57e29f773bb336554394c
Paul <pfultz2@yahoo.com>
2023-03-27 19:09:39 -0500

    Add nolint

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -240,9 +240,10 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
 
 #else // MIGRAPHX_USE_HIPRTC
 
-std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file> srcs,
-                                                           std::string,
-                                                           const std::string&) // NOLINT
+
+std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file>, // NOLINT
+                                                           std::string, // NOLINT
+                                                           const std::string&)
 {
     MIGRAPHX_THROW("Not using hiprtc");
 }

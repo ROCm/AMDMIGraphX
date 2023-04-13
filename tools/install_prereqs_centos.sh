3918ced36796c45a4b95b2a4020360519d7017c5
Ted Themistokleous <ted.themistokleous@amd.com>
2023-04-13 00:39:01 -0400

    Add Dockerfile and prereqs to create a container for CentOS 8
    Currently ROCm doesn't have a later release for 5.2.5 for CENTOS but this
    is something still useful to test on
    
    needed to add rocblas-devel and miopen-devel to get properly picked up by
    cmake
    
    Will reuse this work for RHEL and other OS builds once I can confirm make analyze
    and make checks work with all debug and non debug builds

---
+++ tools/install_prereqs_centos.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#####################################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2015-2023 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#####################################################################################
+#
+# Build MIGraphX prerequisites for docker container
+
+set -e
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+
+# Need pip3 and Python headers to build dependencies
+dnf update && dnf install -y python3-pip python3-dev cmake rocm-cmake rocblas miopen-hip openmp-extras tbb2 tbb-devel
+
+# Needed for cmake to build various pip packages
+pip3.8 install setuptools wheel
+
+# install rbuild to build dependencies
+pip3.8 install https://github.com/RadeonOpenCompute/rbuild/archive/master.tar.gz
+
+
+PREFIX=/usr/local
+REQ_FILE_DIR=""
+if [ "$#" -ge 2 ]; then
+  PREFIX=$1
+  cd $2
+elif [ "$#" -eq 1 ]; then
+  PREFIX=$1
+fi
+
+echo "Dependencies are installed at $PREFIX"
+
+# Install deps with rbuild
+rbuild prepare -d $PREFIX -s develop
+
+# install onnx package for unit tests
+pip3.8 install onnx==1.10.2 numpy==1.21.6 typing==3.7.4 pytest==6.0.1 packaging==23.0
+
+# pin version of protobuf in Python for onnx runtime unit tests
+pip3.8 install protobuf==3.20.0

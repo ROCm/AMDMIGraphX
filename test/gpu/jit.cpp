53d0b0c9bb37e87e0042ec53ced2774dadac15a4
umangyadav <umayadav@amd.com>
2023-05-04 23:13:31 +0000

    Use workarounds env flag for the test

--- test/gpu/jit.cpp
+++ test/gpu/jit.cpp
@@ -206,10 +206,11 @@ TEST_CASE(compile_warnings)
     EXPECT(not compile("").empty());
     EXPECT(not compile("-Wunused-parameter -Wno-error").empty());
     EXPECT(not compile("-Wno-unused-parameter -Werror").empty());
-#ifndef MIGRAPHX_USE_HIPRTC
-    EXPECT(test::throws([&] { compile("-Werror=unused-parameter"); }));
-    EXPECT(test::throws([&] { compile("-Wunused-parameter -Werror"); }));
-#endif
+    if(migraphx::disabled(migraphx::gpu::MIGRAPHX_ENABLE_HIPRTC_WORKAROUNDS{}))
+    {
+        EXPECT(test::throws([&] { compile("-Werror=unused-parameter"); }));
+        EXPECT(test::throws([&] { compile("-Wunused-parameter -Werror"); }));
+    }
 }
 
 TEST_CASE(code_object_hip)

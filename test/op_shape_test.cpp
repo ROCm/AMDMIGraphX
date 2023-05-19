58108dbf22bac98f605ff32b58e4397e9bac3b39
Paul <pfultz2@yahoo.com>
2023-05-19 11:23:49 -0500

    Add a few more test cases

--- test/op_shape_test.cpp
+++ test/op_shape_test.cpp
@@ -2251,8 +2251,9 @@ TEST_CASE(reshape_nonstandard_error)
                                                    {4, 24, 1, 1, 1},
                                                    migraphx::invert_permutation({1, 0, 2, 3, 4}));
     for(auto&& new_shape : std::vector<std::vector<int64_t>>{{4, 8, 3, 2, 2},
+                                                             {1},
                                                              {4, 8, 4},
-                                                             {8, 4, 3, 2, 2},
+                                                             {4, 24, 1, 1, 1, 1, 2},
                                                              {8, 4, 4},
                                                              {4, 1, 3, -1, -1},
                                                              {4, 3, 0},

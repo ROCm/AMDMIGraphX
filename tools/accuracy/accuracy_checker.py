f9edae03a6ab714fd901dc524aa56620a7cd7515
umangyadav <umayadav@amd.com>
2023-05-25 12:03:10 -0500

    rever changes for the accuracy_checker

--- tools/accuracy/accuracy_checker.py
+++ tools/accuracy/accuracy_checker.py
@@ -52,14 +52,6 @@ def parse_args():
     parser.add_argument('--fill0',
                         action='store_true',
                         help='fill all arguments with a value of 0')
-    parser.add_argument('--numpy',
-                        action='append',
-                        help='fill argument with numpy saved array',
-                        type=str)
-    parser.add_argument('--np_path',
-                        action='append',
-                        help='Path for the saved numpy array',
-                        type=str)
     parser.add_argument('--verbose',
                         action='store_true',
                         help='show verbose information (for debugging)')
@@ -89,15 +81,9 @@ def parse_args():
                         action='store_true',
                         default=False,
                         help='Turn on ort VERBOSE logging via session options')
-    parser.add_argument(
-        '--save-ort-res',
-        dest="save_ort",
-        type=str,
-        help=
-        'Save output of ORT as numpy array at path provided by this argument')
+
     args = parser.parse_args()
-    if args.verbose:
-        print(f"Tolerance is set at {args.tolerance}")
+
     return args
 
 
@@ -116,15 +102,6 @@ def check_correctness(gold_outputs,
     ret = True
     for i in range(out_num):
         if not np.allclose(gold_outputs[i], outputs[i], rtol, atol):
-            failed_idx = ~np.isclose(gold_outputs[i], outputs[i], rtol, atol)
-            if verbose:
-                print("Shape of Failed elements{} :\n".format(
-                    gold_outputs[i][failed_idx].shape))
-                print("Expected :\n{}".format(
-                    gold_outputs[i][failed_idx].tolist()[0:400:5]))
-                print('......')
-                print("Actual \n{}".format(
-                    outputs[i][failed_idx].tolist()[0:400:5]))
             ret = False
             if verbose:
                 print('\nOutput {} is incorrect ...'.format(i))
@@ -209,14 +186,7 @@ def main():
             print(f'Parameter {name} -> {shape}')
         in_shape = shape.lens()
         in_type = shape.type_string()
-        if name in args.numpy:
-            in_path = args.np_path[args.numpy.index(name)]
-            if args.verbose:
-                print(
-                    f"Loading numpy array for input name : {name}, from path : {in_path}"
-                )
-            test_input = np.load(in_path).astype(get_np_datatype(in_type))
-        elif not args.fill1 and not args.fill0:
+        if not args.fill1 and not args.fill0:
             test_input = np.random.rand(*(in_shape)).astype(
                 get_np_datatype(in_type))
         elif not args.fill0:
@@ -246,12 +216,6 @@ def main():
 
         try:
             pred_fw = sess.run(None, ort_params)[-1]
-            if (args.save_ort):
-                if args.verbose:
-                    print(
-                        f"saving ORT result as numpy array at location : {args.save_ort}"
-                    )
-                np.save(args.save_ort, pred_fw)
         except Exception as e:
             if any(input_dims):
                 print(

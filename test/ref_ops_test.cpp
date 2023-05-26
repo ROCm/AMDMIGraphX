c930d79a88fbb68feb7bf5aad4cf619f3a629d90
Ted Themistokleous <ted.themistokleous@amd.com>
2023-05-25 20:31:38 -0400

    Fix Nonzero to track data value with sentinel value based on elements
    We can't change the behaviour of the nonzero op and we currently pad the output
    with zeros. This unfortunately obfuscates the following cases:
    
    1. When the only nonzero element is the first index - the whole tensor is padded
    with zeros its not obvious if the first value is valid index or padded
    
    2. When the nonzero elements vector is used for indicies. The resulting vector
       with the padded value of 0 is still a valid index thus gather/gatherND and other ops
       will assume the 0 index is valid and operate accordingly.
    
    In this case, by adding a sentinel value of the number of static elements used
    by the desired shape, the resulting nonzero output can now track how many elements
    are valid by determining the value in the correct range.
    
    Originally I intended to use -1 but not all datatypes use this if say, we're dealing with
    unsigned values in our vectors or booleans.
    
    Signed-off-by: Ted Themistokleous <ted.themistokleous@amd.com>

--- test/ref_ops_test.cpp
+++ test/ref_ops_test.cpp
@@ -5176,8 +5176,8 @@ TEST_CASE(nonzero_test)
     auto result = p.eval({}).back();
     std::vector<int64_t> result_vector;
     result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
-    std::vector<int64_t> gold = {0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
-                                 1, 1, 0, 0, 0, 0, 0, 1, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0};
+    std::vector<int64_t> gold = {0, 0, 0, 0, 1, 1, 1, 1, 36, 36, 36, 36, 0, 0, 1, 1, 0, 0,
+                                 1, 1, 36, 36, 36, 36, 0, 1, 0, 2, 0, 2, 0, 2, 36, 36, 36, 36};
     EXPECT(migraphx::verify_range(result_vector, gold));
 }
 

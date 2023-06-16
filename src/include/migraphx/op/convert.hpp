900ec620ce5c777bb7490cb7ff67e42c0499c0df
Umang Yadav <umangyadav95@gmail.com>
2023-06-16 01:38:22 +0000

    formatting

--- src/include/migraphx/op/convert.hpp
+++ src/include/migraphx/op/convert.hpp
@@ -67,11 +67,14 @@ struct convert : unary<convert>
         return [type](auto x) {
             auto y = x;
             shape::visit(type, [&](auto as) {
-                // clamping value between target_type's max and min doesn't work for NaNs, 
-                if(std::isnan(x))  {
+                // clamping value between target_type's max and min doesn't work for NaNs,
+                if(std::isnan(x))
+                {
                     y = as.nan();
-                } else {
-                    y = std::min(std::max(as(x), as.min()), as.max()); 
+                }
+                else
+                {
+                    y = std::min(std::max(as(x), as.min()), as.max());
                 }
             });
             return y;

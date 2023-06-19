af7dd944b60d0640fde0278f3b9bb3e2e3a3dea8
Artur Wojcik <artur.wojcik@amd.com>
2023-06-19 21:21:18 +0200

    math

--- src/include/migraphx/verify.hpp
+++ src/include/migraphx/verify.hpp
@@ -87,8 +87,7 @@ struct not_finite_fn
     template <class T>
     bool operator()(T x) const
     {
-        using std::isfinite;
-        return not isfinite(x);
+        return not std::isfinite(static_cast<double>(x));
     }
 };
 static constexpr not_finite_fn not_finite{};
@@ -98,8 +97,7 @@ struct compare_mag_fn
     template <class T, class U>
     bool operator()(T x, U y) const
     {
-        using std::fabs;
-        return fabs(x) < fabs(y);
+        return std::fabs(x) < std::fabs(y);
     }
 };
 static constexpr compare_mag_fn compare_mag{};

56153e074491d3aacaf4b7c836d642d78a96816e
Paul <pfultz2@yahoo.com>
2023-06-21 15:21:16 -0700

    Add ck_gemm

--- src/targets/gpu/jit/ck_gemm.cpp
+++ src/targets/gpu/jit/ck_gemm.cpp
@@ -266,7 +266,7 @@ struct ck_gemm_compiler : compiler<ck_gemm_compiler>
         s       = shape{s.type(), {m1, m2}};
     }
 
-    std::vector<std::string> names() const { return {"gpu::ck_gemm"}; }
+    std::vector<std::string> names() const { return {"ck_gemm", "gpu::ck_gemm"}; }
 
     static bool standard_batch(const shape& s)
     {

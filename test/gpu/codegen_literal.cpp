27188f55ed6ae2060dfb61f7f9c5e4608b5172fd
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2023-06-30 16:43:21 -0700

    cleanup test and add literal to_string() test

--- test/gpu/codegen_literal.cpp
+++ test/gpu/codegen_literal.cpp
@@ -21,17 +21,35 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include "migraphx/op/mul.hpp"
-#include <iostream>
-#include <vector>
-#include <migraphx/operators.hpp>
-#include <migraphx/instruction.hpp>
 #include <migraphx/program.hpp>
 #include <migraphx/register_target.hpp>
 #include <migraphx/verify.hpp>
-#include <migraphx/pass_manager.hpp>
 #include <test.hpp>
 #include <migraphx/make_op.hpp>
+
+
+void run_prog(migraphx::program p,
+              const migraphx::target& t,
+              migraphx::parameter_map& m_in,
+              std::vector<float>& res)
+{
+    p.compile(t);
+    migraphx::parameter_map m;
+    for(auto&& x : p.get_parameter_shapes())
+    {
+        if(m_in.count(x.first) > 0)
+        {
+            m[x.first] = t.copy_to(m_in[x.first]);
+        }
+        else
+        {
+            m[x.first] = t.allocate(x.second);
+        }
+    }
+
+    auto result = t.copy_from(p.eval(m).back());
+    result.visit([&](auto v) { res.assign(v.begin(), v.end()); });
+}
 
 // This test ensures that the codegen path doesn't round up literals,
 // otherwise there are accuracy differences compared to ref.
@@ -39,61 +57,31 @@
 // and after rounding must equal 63, not 64.
 TEST_CASE(mul_literal_round_test)
 {
-    auto run_prog = [](migraphx::program p,
-                       const migraphx::target& t,
-                       migraphx::parameter_map& m_in,
-                       std::vector<float>& res) {
-        p.compile(t);
-        migraphx::parameter_map m;
-        for(auto&& x : p.get_parameter_shapes())
-        {
-            if(m_in.count(x.first) > 0)
-            {
-                m[x.first] = t.copy_to(m_in[x.first]);
-            }
-            else
-            {
-                m[x.first] = t.allocate(x.second);
-            }
-        }
 
-        auto result = t.copy_from(p.eval(m).back());
-        result.visit([&](auto v) { res.assign(v.begin(), v.end()); });
-    };
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    migraphx::shape s0{migraphx::shape::float_type, {1}};
+    auto l0 = mm->add_parameter("a", s0);
+    auto l1 = mm->add_literal(1 / 0.00787402f);
 
-    auto create_program = [&] {
-        migraphx::program p;
-        auto* mm = p.get_main_module();
-        migraphx::shape s0{migraphx::shape::float_type, {1}};
-        auto l0 = mm->add_parameter("a", s0);
-        auto l1 = mm->add_literal(1 / 0.00787402f);
+    auto mul   = mm->add_instruction(migraphx::make_op("mul"), l0, l1);
+    auto round = mm->add_instruction(migraphx::make_op("round"), mul);
 
-        auto mul   = mm->add_instruction(migraphx::make_op("mul"), l0, l1);
-        auto round = mm->add_instruction(migraphx::make_op("round"), mul);
+    mm->add_return({round});
 
-        mm->add_return({round});
+    migraphx::parameter_map m;
+    std::vector<float> a = {0.5f};
 
-        return p;
-    };
+    m["a"] = migraphx::argument{s0, a.data()};
+    std::vector<float> ref_result;
+    migraphx::target ref_t = migraphx::make_target("ref");
+    run_prog(p, ref_t, m, ref_result);
 
-    {
-        auto p = create_program();
-        migraphx::parameter_map m;
-        migraphx::shape s0{migraphx::shape::float_type, {1}};
+    std::vector<float> gpu_result;
+    migraphx::target gpu_t = migraphx::make_target("gpu");
+    run_prog(p, gpu_t, m, gpu_result);
 
-        std::vector<float> a = {0.5f};
-
-        m["a"] = migraphx::argument{s0, a.data()};
-        std::vector<float> ref_result;
-        migraphx::target ref_t = migraphx::make_target("ref");
-        run_prog(p, ref_t, m, ref_result);
-
-        std::vector<float> gpu_result;
-        migraphx::target gpu_t = migraphx::make_target("gpu");
-        run_prog(p, gpu_t, m, gpu_result);
-
-        EXPECT(migraphx::verify_range(ref_result, gpu_result));
-    }
+    EXPECT(migraphx::verify_range(ref_result, gpu_result));
 }
 
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

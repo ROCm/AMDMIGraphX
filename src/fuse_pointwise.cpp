ce4f3cc122bcb52e400cf12c1c18560b30a6ef90
Paul <pfultz2@yahoo.com>
2023-07-11 16:55:09 -0500

    Format

--- src/fuse_pointwise.cpp
+++ src/fuse_pointwise.cpp
@@ -197,7 +197,8 @@ struct find_pointwise_reshape_pointwise
 {
     auto matcher() const
     {
-        auto reshape = match::name("reshape", "squeeze", "unsqueeze", "flatten")(match::used_once());
+        auto reshape =
+            match::name("reshape", "squeeze", "unsqueeze", "flatten")(match::used_once());
         auto skip_contiguous =
             match::arg(0)(match::skip(match::name("contiguous")(match::used_once())));
         auto pointwise         = match::name("pointwise")(match::used_once());

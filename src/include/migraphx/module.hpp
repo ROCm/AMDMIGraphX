2188f3fe4bcfc531cf482fa8bdeaa154ec72b67c
umangyadav <umayadav@amd.com>
2023-07-25 12:19:28 +0000

    update comments

--- src/include/migraphx/module.hpp
+++ src/include/migraphx/module.hpp
@@ -222,7 +222,7 @@ struct MIGRAPHX_EXPORT module
     void annotate(std::ostream& os, std::function<void(instruction_ref)> a) const;
 
     std::vector<module_ref> get_sub_modules(bool shallow = false) const;
-    // sorts the module in reverse-post order (RPO) DFS order
+    // sorts the module in topological order aka reverse-post order (RPO) DFS order
     module& sort();
     // if the instruction has the module arguments then all the parameters/instructions used by that
     // module from the main/parent module must be calculated before the instruction can be executed.

5205641bdeddb7a38455690cfb706b710d84c4ae
Paul <pfultz2@yahoo.com>
2023-08-07 10:19:12 -0500

    Format

--- src/targets/gpu/no_device.cpp
+++ src/targets/gpu/no_device.cpp
@@ -1,4 +1,5 @@
 
 #ifdef __HIP_DEVICE_COMPILE__
-#error "Device compilation not allowed for migraphx_gpu. Do not link with hip::device. Device code should go into migraphx_device or migraphx_kernels"
+#error \
+    "Device compilation not allowed for migraphx_gpu. Do not link with hip::device. Device code should go into migraphx_device or migraphx_kernels"
 #endif

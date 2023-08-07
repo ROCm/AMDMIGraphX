16993fcae94d7e25ef13eaf3f19697b24fbad3f7
Paul <pfultz2@yahoo.com>
2023-08-07 10:19:05 -0500

    Check that migraphx_gpu is not using device compilation in migraphx_gpu

---
+++ src/targets/gpu/no_device.cpp
@@ -0,0 +1,4 @@
+
+#ifdef __HIP_DEVICE_COMPILE__
+#error "Device compilation not allowed for migraphx_gpu. Do not link with hip::device. Device code should go into migraphx_device or migraphx_kernels"
+#endif

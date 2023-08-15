da81615cbae736447f9e9ff6933521057d555915
Ted Themistokleous <ted.themistokleous@amd.com>
2023-08-15 18:07:29 -0400

    Remove contiguous being added with onnx/tf parsing of reshape()
    Not needed so we don;'t need the extra contiguous as we're trying to
    eliminate the need to use contiguous

--- src/tf/parse_reshape.cpp
+++ src/tf/parse_reshape.cpp
@@ -45,9 +45,8 @@ struct parse_reshape : op_parser<parse_reshape>
         auto s = args[1]->eval();
         std::vector<int64_t> dims;
         s.visit([&](auto v) { copy(v, std::back_inserter(dims)); });
-        auto resh = info.add_instruction(make_op("reshape", {{"dims", dims}}),
+        return info.add_instruction(make_op("reshape", {{"dims", dims}}),
                                          info.make_contiguous(args[0]));
-        return info.make_contiguous(resh);
     }
 };
 

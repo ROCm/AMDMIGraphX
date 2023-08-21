7a38b35a8cd140a6481995d7096f5cc615432db6
Paul <pfultz2@yahoo.com>
2023-08-21 11:48:11 -0500

    Fix compilation error

--- src/targets/gpu/driver/compile_op.cpp
+++ src/targets/gpu/driver/compile_op.cpp
@@ -38,10 +38,8 @@ struct compile_op : action<compile_op>
         context ctx;
         auto inputs = p.parse_shapes(v.at("inputs"));
         auto op     = gpu::compile_op(v.at("name").to<std::string>(), ctx, inputs, v);
-        auto [host_time, device_time] = time_op(ctx, op, inputs, p.get(v, "iterations", 100));
-        std::cout << op << ": " << host_time << "ms";
-        if(device_time > 0)
-            std::cout << ", " << device_time << "ms";
+        auto t = time_op(ctx, op, inputs, p.get(v, "iterations", 100));
+        std::cout << op << ": " << t << "ms";
         std::cout << std::endl;
     }
 };

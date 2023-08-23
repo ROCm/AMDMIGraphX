43455f84a6eefceefe0a63a1cd88eea58b9e4e06
ravil-mobile <Ravil.Dorozhinskii@amd.com>
2023-08-23 18:41:33 +0000

    Renamed fuse_mlir.hpp/cpp to mlir_offload.hpp/cpp
    * renamed the corresponding struct
    * addressed suggestions of PR #2110

--- src/targets/gpu/target.cpp
+++ src/targets/gpu/target.cpp
@@ -58,8 +58,7 @@
 #include <migraphx/gpu/context.hpp>
 #include <migraphx/gpu/device_name.hpp>
 #include <migraphx/gpu/fuse_ck.hpp>
-#include <migraphx/gpu/fuse_mlir.hpp>
-#include <migraphx/gpu/standalone_mlir.hpp>
+#include <migraphx/gpu/mlir_offload.hpp>
 #include <migraphx/gpu/fuse_ops.hpp>
 #include <migraphx/gpu/prefuse_ops.hpp>
 #include <migraphx/gpu/lowering.hpp>
@@ -143,8 +142,7 @@ std::vector<pass> target::get_passes(migraphx::context& gctx, const compile_opti
         enable_pass(enabled(MIGRAPHX_ENABLE_CK{}), fuse_ck{}),
 #endif
         dead_code_elimination{},
-        enable_pass(mlir_enabled(), fuse_mlir{&ctx}),
-        enable_pass(mlir_enabled(), standalone_mlir{&ctx}),
+        enable_pass(mlir_enabled(), mlir_offload{&ctx}),
         dead_code_elimination{},
         lowering{&ctx, options.offload_copy},
         eliminate_contiguous{"gpu::contiguous"},

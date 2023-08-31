89e712739edfdf483b7122ea9c290f0c4adb931a
umang yadav <umangyadav95@gmail.com>
2023-08-31 17:47:15 +0000

    Partitioner working for nested if then else modules

--- test/multi_target/multitarget_test.cpp
+++ test/multi_target/multitarget_test.cpp
@@ -223,7 +223,7 @@ TEST_CASE(single_target_multi_compile)
     // eval
     migraphx::parameter_map params;
     std::vector<float> boxes_vec  = {0.5, 0.5,  1.0, 1.0, 0.5, 0.6,  1.0, 1.0, 0.5, 0.4,   1.0, 1.0,
-                                    0.5, 10.5, 1.0, 1.0, 0.5, 10.6, 1.0, 1.0, 0.5, 100.5, 1.0, 1.0};
+                                     0.5, 10.5, 1.0, 1.0, 0.5, 10.6, 1.0, 1.0, 0.5, 100.5, 1.0, 1.0};
     params["boxes"]               = migraphx::argument(boxes_s, boxes_vec.data());
     auto output                   = p.eval(params).back();
     std::vector<int64_t> gold_vec = {0, 0, 3, 0, 0, 0, 0, 0, 5};
@@ -470,6 +470,148 @@ TEST_CASE(multitarget_compile_nested_if_then_else)
                migraphx::make_target("ref"),
                migraphx::make_target("ref")},
               {gpu_opts});
+    EXPECT(check_compiled_program(p,
+                                  {migraphx::make_target("gpu"),
+                                   migraphx::make_target("cpu"),
+                                   migraphx::make_target("ref"),
+                                   migraphx::make_target("ref")}));
+    // do evaluation using different conditions
+    migraphx::parameter_map params;
+    float x_i   = 2.0;
+    float y_i   = 3.0;
+    float z_i   = 4.0;
+    params["x"] = migraphx::fill_argument(ds, x_i);
+    params["y"] = migraphx::fill_argument(ds, y_i);
+    params["z"] = migraphx::fill_argument(ds, z_i);
+    // cover all paths with different combination of conditions
+    std::vector<std::pair<bool, bool>> test_conds = {
+        {true, true}, {true, false}, {false, true}, {false, false}};
+    for(auto [cond_val_0, cond_val_1] : test_conds)
+    {
+        params["cond_0"] = migraphx::argument(cond_s, &cond_val_0);
+        params["cond_1"] = migraphx::argument(cond_s, &cond_val_1);
+        auto result      = p.eval(params).back();
+        // main has one instruction that is : if_then_else
+        // then mod is doing : {tmp = x+y; (cond) ? (((x-1)*y)-z)  : (((tmp-1)*y)-z);}
+        // else mod is doing : {tmp = x+z; (cond) ? (((tmp-1)*x)-y) : (((z-1)*y)-x);}
+        float gold_i = -1.0;
+        if(cond_val_0)
+        {
+            float tmp_i = x_i + y_i;
+            gold_i      = (cond_val_1) ? (((x_i - 1) * y_i) - z_i) : (((tmp_i - 1) * y_i) - z_i);
+        }
+        else
+        {
+            float tmp_i = x_i + z_i;
+            gold_i      = (cond_val_1) ? (((tmp_i - 1) * x_i) - y_i) : (((z_i - 1) * y_i) - x_i);
+        }
+        auto gold = migraphx::fill_argument(ds, gold_i);
+        EXPECT(gold == result);
+    }
+}
+
+// TODO : FPGA compilation is broken right now, below test mentions fpga but doesn't compile for it
+TEST_CASE(multitarget_compile_nested_if_then_else_partition)
+{
+    std::unordered_map<std::size_t, std::size_t> counter_map = {{0, 0}, {1, 0}};
+    migraphx::shape ds{migraphx::shape::float_type, {2, 3}};
+    migraphx::target_assignments tass;
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    migraphx::shape cond_s{migraphx::shape::bool_type};
+    auto cond_0             = mm->add_parameter("cond_0", cond_s);
+    auto cond_1             = mm->add_parameter("cond_1", cond_s);
+    auto x                  = mm->add_parameter("x", ds);
+    auto y                  = mm->add_parameter("y", ds);
+    auto z                  = mm->add_parameter("z", ds);
+    auto create_test_module = [&](migraphx::program& prog,
+                                  const std::vector<migraphx::instruction_ref>& inputs,
+                                  std::size_t tid) {
+        std::string mod_name =
+            "target_" + std::to_string(tid) + "_" + std::to_string(counter_map[tid]++);
+        auto* test_mod = prog.create_module(mod_name);
+        std::vector<float> data(ds.elements(), -1);
+        auto l1   = test_mod->add_literal(migraphx::literal(ds, data));
+        auto ins1 = test_mod->add_instruction(migraphx::make_op("add"), inputs[0], l1);
+        auto ins2 = test_mod->add_instruction(migraphx::make_op("mul"), ins1, inputs[1]);
+        auto ins3 = test_mod->add_instruction(migraphx::make_op("sub"), ins2, inputs[2]);
+        test_mod->add_return({ins3});
+        tass.insert(tass.begin(), std::make_pair(ins1, tid));
+        tass.insert(tass.begin(), std::make_pair(ins2, tid));
+        tass.insert(tass.begin(), std::make_pair(ins3, tid));
+        return test_mod;
+    };
+
+    auto* then_mod        = p.create_module("then_mod");
+    auto then_mod_cond    = then_mod->add_parameter("then_mod_cond", cond_s);
+    auto then_mod_param_0 = then_mod->add_parameter("then_mod_param_0", ds);
+    auto then_mod_param_1 = then_mod->add_parameter("then_mod_param_1", ds);
+    auto then_mod_param_2 = then_mod->add_parameter("then_mod_param_2", ds);
+    auto then_mod_ref_ins =
+        then_mod->add_instruction(migraphx::make_op("add"), then_mod_param_0, then_mod_param_1);
+    tass.insert(tass.begin(), std::make_pair(then_mod_ref_ins, 3));
+    auto then_mod_if = then_mod->add_instruction(
+        migraphx::make_op("if"),
+        {then_mod_cond,
+         then_mod_param_0,
+         then_mod_param_1,
+         then_mod_param_2,
+         then_mod_ref_ins,
+         then_mod_param_1,
+         then_mod_param_2},
+        {create_test_module(p, {then_mod_param_0, then_mod_param_1, then_mod_param_2}, 1),
+         create_test_module(p, {then_mod_ref_ins, then_mod_param_1, then_mod_param_2}, 0)});
+    auto then_mod_if_0 =
+        then_mod->add_instruction(migraphx::make_op("get_tuple_elem", {{"index", 0}}), then_mod_if);
+    then_mod->add_return({then_mod_if_0});
+
+    // create nested else_mod with multiple targets.
+    // else_mod has one instruction that runs a module on "fpga" and another instruction that
+    // creates nested modules using "If" that runs on "cpu" and "gpu"
+    auto* else_mod        = p.create_module("else_mod");
+    auto else_mod_cond    = else_mod->add_parameter("else_mod_cond", cond_s);
+    auto else_mod_param_0 = else_mod->add_parameter("else_mod_param_0", ds);
+    auto else_mod_param_1 = else_mod->add_parameter("else_mod_param_1", ds);
+    auto else_mod_param_2 = else_mod->add_parameter("else_mod_param_2", ds);
+    auto else_mod_fpga_ins =
+        else_mod->add_instruction(migraphx::make_op("add"), else_mod_param_0, else_mod_param_2);
+    tass.insert(tass.begin(), std::make_pair(else_mod_fpga_ins, 2));
+    auto else_mod_if = else_mod->add_instruction(
+        migraphx::make_op("if"),
+        {else_mod_cond,
+         else_mod_fpga_ins,
+         else_mod_param_0,
+         else_mod_param_1,
+         else_mod_param_2,
+         else_mod_param_1,
+         else_mod_param_0},
+        {create_test_module(p, {else_mod_fpga_ins, else_mod_param_0, else_mod_param_1}, 0),
+         create_test_module(p, {else_mod_param_2, else_mod_param_1, else_mod_param_0}, 1)});
+    auto else_mod_if_0 =
+        else_mod->add_instruction(migraphx::make_op("get_tuple_elem", {{"index", 0}}), else_mod_if);
+    else_mod->add_return({else_mod_if_0});
+
+    // Create nested and multi-target main module using "If"
+    auto main_if_ins = mm->add_instruction(
+        migraphx::make_op("if"), {cond_0, cond_1, x, y, z, cond_1, x, y, z}, {then_mod, else_mod});
+    auto r = mm->add_instruction(migraphx::make_op("get_tuple_elem", {{"index", 0}}), main_if_ins);
+    mm->add_return({r});
+
+    // compile
+    migraphx::compile_options gpu_opts;
+    gpu_opts.offload_copy = true;
+    std::cout << "before parition\n";
+    p.debug_print();
+    migraphx::partition(p, tass);
+    std::cout << "after partition\n";
+    p.debug_print();
+    p.compile({migraphx::make_target("gpu"),
+               migraphx::make_target("cpu"),
+               migraphx::make_target("ref"),
+               migraphx::make_target("ref")},
+              {gpu_opts});
+    std::cout << "after compilation\n";
+    p.debug_print();
     EXPECT(check_compiled_program(p,
                                   {migraphx::make_target("gpu"),
                                    migraphx::make_target("cpu"),

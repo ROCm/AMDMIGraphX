8171c0b37aefa95594484ebe552bafe19ba9ffec
Paul <pfultz2@yahoo.com>
2023-09-01 14:13:48 -0500

    Format

--- test/fuse_pointwise.cpp
+++ test/fuse_pointwise.cpp
@@ -450,13 +450,13 @@ TEST_CASE(add_unsqueeze_add_nonstandard)
     migraphx::shape s2{migraphx::shape::float_type, {3, 10, 1, 16}};
     migraphx::program p1;
     {
-        auto* mm     = p1.get_main_module();
-        auto x       = mm->add_parameter("x", s1);
-        auto y       = mm->add_parameter("y", s1);
-        auto z       = mm->add_parameter("z", s2);
-        auto add1    = mm->add_instruction(migraphx::make_op("add"), x, y);
+        auto* mm       = p1.get_main_module();
+        auto x         = mm->add_parameter("x", s1);
+        auto y         = mm->add_parameter("y", s1);
+        auto z         = mm->add_parameter("z", s2);
+        auto add1      = mm->add_instruction(migraphx::make_op("add"), x, y);
         auto unsqueeze = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {2}}}), add1);
-        auto add2    = mm->add_instruction(migraphx::make_op("add"), unsqueeze, z);
+        auto add2      = mm->add_instruction(migraphx::make_op("add"), unsqueeze, z);
         mm->add_return({add2});
     }
     run_pass(p1);

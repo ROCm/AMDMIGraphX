c01eda18d9f087961aea887f8be35f732db7ba9a
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2023-09-19 13:58:04 -0500

    formatting

--- src/simplify_reshapes.cpp
+++ src/simplify_reshapes.cpp
@@ -637,15 +637,16 @@ struct find_broadcast_transpose
 
     void apply(module& m, const match::matcher_result& r) const
     {
-        auto ins = r.result;
-        auto ins_lens = ins->get_shape().lens();
+        auto ins       = r.result;
+        auto ins_lens  = ins->get_shape().lens();
         auto bcast_ins = r.instructions["bcast_ins"];
-        auto input = bcast_ins->inputs().front();
+        auto input     = bcast_ins->inputs().front();
         // for now, focusing on scalar transformation
         if(not input->get_shape().scalar())
             return;
-        
-        auto new_mbcast = m.insert_instruction(bcast_ins, make_op("multibroadcast", {{"out_lens", ins_lens}}), input);
+
+        auto new_mbcast = m.insert_instruction(
+            bcast_ins, make_op("multibroadcast", {{"out_lens", ins_lens}}), input);
         m.replace_instruction(ins, new_mbcast);
     }
 };

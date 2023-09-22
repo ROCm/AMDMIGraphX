b907aaf33f4fca5f10856d5f93f22e7c55c921dd
Umang Yadav <umayadav@amd.com>
2023-09-22 23:15:31 +0000

    add flags for ck, disable hiprtc-driver

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -209,6 +209,10 @@ std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_sr
         options.push_back("-Wno-gnu-line-marker");
         options.push_back("-Wno-old-style-cast");
     }
+    options.push_back("-D__HIPCC_RTC__=1");
+    options.push_back("-DCK_DONT_USE_HIP_RUNTIME_HEADERS=1");
+    options.push_back("-D__HIP_PLATFORM_HCC__=1");
+    options.push_back("-D__HIP_PLATFORM_AMD__=1");
 
     if(enabled(MIGRAPHX_GPU_DEBUG{}))
         options.push_back("-DMIGRAPHX_DEBUG");
@@ -254,22 +258,22 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
     auto p      = dynamic_loader::path(&compile_hip_src_with_hiprtc);
     auto driver = p.parent_path().parent_path() / "bin" / "migraphx-hiprtc-driver";
 
-    if(fs::exists(driver))
-    {
-        value v;
-        v["srcs"]   = to_value(hsrcs);
-        v["params"] = to_value(params);
-        v["arch"]   = to_value(arch);
+    // if(fs::exists(driver))
+    // {
+    //     value v;
+    //     v["srcs"]   = to_value(hsrcs);
+    //     v["params"] = to_value(params);
+    //     v["arch"]   = to_value(arch);
 
-        tmp_dir td{};
-        auto out = td.path / "output";
+    //     tmp_dir td{};
+    //     auto out = td.path / "output";
 
-        process(driver.string() + " " + out.string()).write([&](auto writer) {
-            to_msgpack(v, writer);
-        });
-        if(fs::exists(out))
-            return {read_buffer(out.string())};
-    }
+    //     process(driver.string() + " " + out.string()).write([&](auto writer) {
+    //         to_msgpack(v, writer);
+    //     });
+    //     if(fs::exists(out))
+    //         return {read_buffer(out.string())};
+    // }
     return compile_hip_src_with_hiprtc(std::move(hsrcs), std::move(params), arch);
 }
 

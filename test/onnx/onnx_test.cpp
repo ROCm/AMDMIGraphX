7b458ac10cc96ce7d09e1bc58183bcbe5e0fc9a6
Ted Themistokleous <ted.themistokleous@amd.com>
2023-09-28 11:03:37 -0400

    Update rehape_test to remove contiguous ops

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -5366,12 +5366,9 @@ TEST_CASE(reshape_test)
         migraphx::literal{migraphx::shape{migraphx::shape::int64_type, {2}}, reshape_dims});
     auto l0 = mm->add_parameter("0", migraphx::shape{migraphx::shape::float_type, {4, 2, 3}});
     op.dims = reshape_dims;
-    auto c0 = mm->add_instruction(migraphx::make_op("contiguous"), l0);
-    mm->add_instruction(op, c0);
-    auto c1 = mm->add_instruction(migraphx::make_op("contiguous"), l0);
-    mm->add_instruction(op, c1);
+    mm->add_instruction(op, l0);
+    mm->add_instruction(op, l0);
     auto prog = optimize_onnx("reshape_test.onnx");
-
     EXPECT(p == prog);
 }
 

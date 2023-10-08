ef6a52f680190958ac6f1db2a92156285a6657ea
Artur Wojcik <artur.wojcik@amd.com>
2023-10-08 19:32:18 +0200

    process: incorporate review feedback

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -256,10 +256,11 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
 #else
     auto p      = dynamic_loader::path(&compile_hip_src_with_hiprtc);
     auto driver = p.parent_path().parent_path() / "bin" / "migraphx-hiprtc-driver";
-#endif
 
     if(fs::exists(driver))
+#endif
     {
+
         value v;
         v["srcs"]   = to_value(hsrcs);
         v["params"] = to_value(params);

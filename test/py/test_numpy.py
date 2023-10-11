a1cdbf5a2d8598de73feeadaece90cb2937e3686
Ahsan Saghir <ahsaghir@amd.com>
2023-10-11 16:45:32 +0000

    Remove try except from test_numpy.py as well

--- test/py/test_numpy.py
+++ test/py/test_numpy.py
@@ -1,7 +1,7 @@
 #####################################################################################
 # The MIT License (MIT)
 #
-# Copyright (c) 2015-2022 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2015-2023 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,10 +22,7 @@
 # THE SOFTWARE.
 #####################################################################################
 import migraphx, sys
-try:
-    import numpy as np
-except:
-    sys.exit()
+import numpy as np
 
 
 def test_add_op():

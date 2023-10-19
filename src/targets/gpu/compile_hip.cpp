f16d29dc417813515ecd90068590723065ebf931
Umang Yadav <umayadav@amd.com>
2023-10-19 23:21:06 +0000

    revert changes for compile_hip

--- src/targets/gpu/compile_hip.cpp
+++ src/targets/gpu/compile_hip.cpp
@@ -194,7 +194,7 @@ struct hiprtc_program
 };
 
 std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file> srcs,
-                                                           const std::string& params,
+                                                           std::string params,
                                                            const std::string& arch)
 {
     hiprtc_program prog(std::move(srcs));
@@ -276,7 +276,7 @@ compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std
 #else // MIGRAPHX_USE_HIPRTC
 
 std::vector<std::vector<char>> compile_hip_src_with_hiprtc(std::vector<hiprtc_src_file>, // NOLINT
-                                                           const std::string&,           // NOLINT
+                                                           std::string,                  // NOLINT
                                                            const std::string&)
 {
     MIGRAPHX_THROW("Not using hiprtc");
--- src/targets/gpu/include/migraphx/gpu/compile_hip.hpp
+++ src/targets/gpu/include/migraphx/gpu/compile_hip.hpp
@@ -58,7 +58,7 @@ struct hiprtc_src_file
 MIGRAPHX_GPU_EXPORT bool hip_has_flags(const std::vector<std::string>& flags);
 
 MIGRAPHX_GPU_EXPORT std::vector<std::vector<char>> compile_hip_src_with_hiprtc(
-    std::vector<hiprtc_src_file> srcs, const std::string& params, const std::string& arch);
+    std::vector<hiprtc_src_file> srcs, std::string params, const std::string& arch);
 
 MIGRAPHX_GPU_EXPORT std::vector<std::vector<char>>
 compile_hip_src(const std::vector<src_file>& srcs, std::string params, const std::string& arch);

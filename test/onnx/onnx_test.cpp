6ae4227a1a32f9bf4f73606d269686a9e9f1fff9
Zakor Gyula <126694206+gyulaz-htec@users.noreply.github.com>
2023-10-20 15:20:38 +0200

    Add support for select_last_index attribute for ArgMax & ArgMin (#2235)

--- test/onnx/onnx_test.cpp
+++ test/onnx/onnx_test.cpp
@@ -184,6 +184,19 @@ TEST_CASE(argmax_test)
     EXPECT(p == prog);
 }
 
+TEST_CASE(argmax_select_last_index_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto l0  = mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {3, 4, 5, 6}});
+    auto ins = mm->add_instruction(
+        migraphx::make_op("argmax", {{"axis", 2}, {"select_last_index", true}}), l0);
+    mm->add_instruction(migraphx::make_op("squeeze", {{"axes", {2}}}), ins);
+    auto prog = optimize_onnx("argmax_select_last_index_test.onnx");
+
+    EXPECT(p == prog);
+}
+
 TEST_CASE(argmax_dyn_test)
 {
     migraphx::program p;
@@ -209,6 +222,19 @@ TEST_CASE(argmin_test)
     auto ins = mm->add_instruction(migraphx::make_op("argmin", {{"axis", 3}}), l0);
     mm->add_instruction(migraphx::make_op("squeeze", {{"axes", {3}}}), ins);
     auto prog = optimize_onnx("argmin_test.onnx");
+
+    EXPECT(p == prog);
+}
+
+TEST_CASE(argmin_select_last_index_test)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto l0  = mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {3, 4, 5, 6}});
+    auto ins = mm->add_instruction(
+        migraphx::make_op("argmin", {{"axis", 3}, {"select_last_index", true}}), l0);
+    mm->add_instruction(migraphx::make_op("squeeze", {{"axes", {3}}}), ins);
+    auto prog = optimize_onnx("argmin_select_last_index_test.onnx");
 
     EXPECT(p == prog);
 }

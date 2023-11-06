8a3e4f5ec00b7511698995f0054820a910bcb75b
Gyula Zakor <gyula.zakor@htecgroup.com>
2023-11-06 14:20:35 +0000

    Expose limit_max_iteration to migraphx API

--- src/onnx/include/migraphx/onnx/onnx_parser.hpp
+++ src/onnx/include/migraphx/onnx/onnx_parser.hpp
@@ -97,9 +97,9 @@ struct onnx_parser
     shape::dynamic_dimension default_dyn_dim_value = {1, 1};
     std::unordered_map<std::string, std::vector<std::size_t>> map_input_dims;
     std::unordered_map<std::string, std::vector<shape::dynamic_dimension>> map_dyn_input_dims;
-    bool use_dyn_output         = false;
-    bool skip_unknown_operators = false;
-    int64_t max_loop_iterations = 10;
+    bool use_dyn_output          = false;
+    bool skip_unknown_operators  = false;
+    int64_t max_loop_iterations  = 10;
     int64_t limit_max_iterations = std::numeric_limits<uint16_t>::max();
     int64_t opset_version        = 13;
 

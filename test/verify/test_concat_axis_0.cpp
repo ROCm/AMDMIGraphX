bd0ae5fa0ee05b2b72ebf61a13fd7c5492393791
Umang Yadav <umayadav@amd.com>
2023-11-17 23:27:41 +0000

    add some more tests

--- test/verify/test_concat_axis_0.cpp
+++ test/verify/test_concat_axis_0.cpp
@@ -27,16 +27,18 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct test_concat_axis_0 : verify_program<test_concat_axis_0>
+template <migraphx::shape::type_t DType>
+
+struct test_concat_axis_0 : verify_program<test_concat_axis_0<DType>>
 {
     migraphx::program create_program() const
     {
         migraphx::program p;
         auto* mm = p.get_main_module();
         int axis = 0;
-        migraphx::shape s0{migraphx::shape::int32_type, {2, 2}};
-        migraphx::shape s1{migraphx::shape::int32_type, {3, 2}};
-        migraphx::shape s2{migraphx::shape::int32_type, {1, 2}};
+        migraphx::shape s0{DType, {2, 2}};
+        migraphx::shape s1{DType, {3, 2}};
+        migraphx::shape s2{DType, {1, 2}};
         auto l0 = mm->add_parameter("x", s0);
         auto l1 = mm->add_parameter("y", s1);
         auto l2 = mm->add_parameter("z", s2);
@@ -44,3 +46,8 @@ struct test_concat_axis_0 : verify_program<test_concat_axis_0>
         return p;
     }
 };
+
+template struct test_concat_axis_0<migraphx::shape::fp8e4m3fnuz_type>;
+template struct test_concat_axis_0<migraphx::shape::half_type>;
+template struct test_concat_axis_0<migraphx::shape::float_type>;
+template struct test_concat_axis_0<migraphx::shape::int32_type>;

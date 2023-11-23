004b30620a3dc96e3059437bc70fa772fc2deedc
Sam Wu <sam.wu2@amd.com>
2023-11-23 16:59:29 -0700

    Standardize documentation for ReadtheDocs
    Relates to https://github.com/RadeonOpenCompute/rocm-docs-core/issues/330

--- docs/conf.py
+++ docs/conf.py
@@ -24,14 +24,37 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import re
+
 from rocm_docs import ROCmDocs
+
+html_theme_options = {"flavor": "list"}
+templates_path = ["."] # Use the current folder for templates
 
 setting_all_article_info = True
 all_article_info_os = ["linux"]
 
-docs_core = ROCmDocs("AMD MIGraphX Documentation")
-docs_core.run_doxygen()
+with open('../CMakeLists.txt', encoding='utf-8') as f:
+    match = re.search(r'.*\brocm_setup_version\(VERSION\s+([0-9.]+)[^0-9.]+', f.read())
+    if not match:
+        raise ValueError("VERSION not found!")
+    version_number = match[1]
+left_nav_title = f"AMD MIGraphX {version_number} Documentation"
+
+# for PDF output on Read the Docs
+project = "AMD MIGraphX Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(left_nav_title)
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.setup()
+
+external_projects_current_project = "amdmigraphx"
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)

d473b80283b8030108fcb997e409697d47fb103f
Umang Yadav <umayadav@amd.com>
2023-11-26 16:42:13 +0000

    add tests and fix cmake

--- test/verify/gemm_2args_mm_3.cpp
+++ test/verify/gemm_2args_mm_3.cpp
@@ -22,19 +22,21 @@
  * THE SOFTWARE.
  */
 
+#include "migraphx/shape.hpp"
 #include "verify_program.hpp"
 #include <migraphx/program.hpp>
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct gemm_2args_mm_3 : verify_program<gemm_2args_mm_3>
+template <migraphx::shape::type_t DType>
+struct gemm_2args_mm_3 : verify_program<gemm_2args_mm_3<DType>>
 {
     migraphx::program create_program() const
     {
         migraphx::program p;
         auto* mm = p.get_main_module();
-        migraphx::shape m1_shape{migraphx::shape::float_type, {1, 2, 3}};
-        migraphx::shape m2_shape{migraphx::shape::float_type, {3, 3, 4}};
+        migraphx::shape m1_shape{DType, {1, 2, 3}};
+        migraphx::shape m2_shape{DType, {3, 3, 4}};
         auto l1 = mm->add_parameter("1", m1_shape);
         auto bl1 =
             mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", {3, 2, 3}}}), l1);
@@ -45,3 +47,6 @@ struct gemm_2args_mm_3 : verify_program<gemm_2args_mm_3>
         return p;
     }
 };
+
+template struct gemm_2args_mm_3<migraphx::shape::float_type>;
+template struct gemm_2args_mm_3<migraphx::shape::fp8e4m3fnuz_type>;
--- test/verify/gemm_2args_mm_6.cpp
+++ test/verify/gemm_2args_mm_6.cpp
@@ -27,14 +27,16 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct gemm_2args_mm_6 : verify_program<gemm_2args_mm_6>
+template <migraphx::shape::type_t DType>
+
+struct gemm_2args_mm_6 : verify_program<gemm_2args_mm_6<DType>>
 {
     migraphx::program create_program() const
     {
         migraphx::program p;
         auto* mm = p.get_main_module();
-        migraphx::shape m1_shape{migraphx::shape::float_type, {2, 1, 2, 3}};
-        migraphx::shape m2_shape{migraphx::shape::float_type, {1, 3, 3, 4}};
+        migraphx::shape m1_shape{DType, {2, 1, 2, 3}};
+        migraphx::shape m2_shape{DType, {1, 3, 3, 4}};
         auto l1  = mm->add_parameter("1", m1_shape);
         auto bl1 = mm->add_instruction(
             migraphx::make_op("multibroadcast", {{"out_lens", {2, 3, 2, 3}}}), l1);
@@ -47,3 +49,6 @@ struct gemm_2args_mm_6 : verify_program<gemm_2args_mm_6>
         return p;
     }
 };
+
+template struct gemm_2args_mm_6<migraphx::shape::float_type>;
+template struct gemm_2args_mm_6<migraphx::shape::fp8e4m3fnuz_type>;

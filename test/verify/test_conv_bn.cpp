04efdb4485477b7482a6e7ebe1980c352a6162d5
Umang Yadav <umayadav@amd.com>
2023-11-28 14:34:04 +0000

    add test for conv_bn with 1e-1f

--- test/verify/test_conv_bn.cpp
+++ test/verify/test_conv_bn.cpp
@@ -43,12 +43,11 @@ struct test_conv_bn : verify_program<test_conv_bn<DType>>
         auto x = mm->add_parameter("x", xs);
         auto w = mm->add_parameter("w", ws);
         // non-symmetrical tiling
-        // auto conv = mm->add_instruction(
-        //     migraphx::make_op("convolution",
-        //                       {{"padding", {3, 3}}, {"stride", {2, 2}}, {"dilation", {1, 1}}}),
-        //     x,
-        //     w);
-        auto conv = mm->add_parameter("conv", migraphx::shape{DType, {1, 64, 112, 112}});
+        auto conv = mm->add_instruction(
+            migraphx::make_op("convolution",
+                              {{"padding", {3, 3}}, {"stride", {2, 2}}, {"dilation", {1, 1}}}),
+            x,
+            w);
 
         auto scale    = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 1)));
         auto bias     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 2)));
@@ -56,7 +55,12 @@ struct test_conv_bn : verify_program<test_conv_bn<DType>>
         auto variance = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 4)));
 
         auto rt  = mm->add_literal(migraphx::literal{DType, {0.5}});
+
         auto eps = mm->add_literal(migraphx::literal{DType, {1e-5f}});
+        if constexpr((DType) == migraphx::shape::fp8e4m3fnuz_type)
+        {
+            eps = mm->add_literal(migraphx::literal{DType, {1e-1f}});
+        }
 
         auto usq_scale =
             mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1, 2}}}), scale);

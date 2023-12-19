2309333acb8a7ede365001209879ea0fd965573e
Paul <pfultz2@yahoo.com>
2023-12-19 09:51:03 -0600

    Format

--- test/onnx/parse/averagepool_nt_cip_test.cpp
+++ test/onnx/parse/averagepool_nt_cip_test.cpp
@@ -32,12 +32,12 @@ TEST_CASE(averagepool_nt_cip_test)
     auto input = mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {1, 1, 5, 5}});
     std::vector<int64_t> pads = {0, 0, 0, 0, 0, 0, 1, 1};
     auto ins_pad = mm->add_instruction(migraphx::make_op("pad", {{"pads", pads}}), input);
-    auto ret     = mm->add_instruction(migraphx::make_op("pooling",
-                                                         {{"mode", migraphx::op::pooling_mode::average},
-                                                          {"padding", {0, 0, 0, 0}},
-                                                          {"stride", {2, 2}},
-                                                          {"lengths", {6, 6}},
-                                                          {"dilations", {1, 1}}}),
+    auto ret                  = mm->add_instruction(migraphx::make_op("pooling",
+                                                                      {{"mode", migraphx::op::pooling_mode::average},
+                                                                       {"padding", {0, 0, 0, 0}},
+                                                                       {"stride", {2, 2}},
+                                                                       {"lengths", {6, 6}},
+                                                                       {"dilations", {1, 1}}}),
                                    ins_pad);
     mm->add_return({ret});
 

50edd17c5a263242a15e3183dc09693f26db24aa
Ted Themistokleous <tedthemistokleous@amd.com>
2023-12-20 18:26:53 +0000

    Fix format errors

--- src/include/migraphx/op/nonmaxsuppression.hpp
+++ src/include/migraphx/op/nonmaxsuppression.hpp
@@ -59,9 +59,9 @@ Other versions of this operator: 10
 
 Attributes
 center_point_box : int (default is 0)
-Integer indicate the format of the box data. The default is 0. 0 - the box data is supplied as [y1, x1, y2, x2]
-where (y1, x1) and (y2, x2) are the coordinates of any diagonal pair of box  corners and the coordinates can
-be provided as normalized (i.e., lying in the interval [0, 1]) or absolute.
+Integer indicate the format of the box data. The default is 0. 0 - the box data is supplied as [y1,
+x1, y2, x2] where (y1, x1) and (y2, x2) are the coordinates of any diagonal pair of box corners and
+the coordinates can be provided as normalized (i.e., lying in the interval [0, 1]) or absolute.
 Mostly used for TF models. 1 - the box data is supplied as [x_center, y_center, width, height].
 Mostly used for Pytorch models.
 

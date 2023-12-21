28c07b3053159bca6f2c28ef9b3b6506f08396dd
Paul <pfultz2@yahoo.com>
2023-12-20 18:47:05 -0600

    Add regex tests

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -402,7 +402,7 @@ auto within_abs(T px, U py, double ptol = 1e-6f)
     return make_function("near", [](auto x, auto y, auto tol) { return std::abs(x - y) < tol; })(
         px, py, ptol);
 }
-
+#if 1
 template <class Iterator1, class Iterator2>
 bool glob_match(Iterator1 start, Iterator1 last, Iterator2 pattern_start, Iterator2 pattern_last)
 {
@@ -426,7 +426,38 @@ bool glob_match(Iterator1 start, Iterator1 last, Iterator2 pattern_start, Iterat
         start++;
     return start != last;
 }
-
+#else
+template<class Iterator1, class Iterator2>
+bool glob_match(Iterator1 start, Iterator1 last, Iterator2 pattern_start, Iterator2 pattern_last)
+{
+    auto old = last;
+    auto pattern_old = pattern_last;
+    while(start != last)
+    {
+        std::tie(start, pattern_start) = std::mismatch(start, last, pattern_start, pattern_last, [](auto c, auto m) {
+            if (m == '?')
+                return true;
+            if (m == '*')
+                return false;
+            return c == m;
+        });
+        if(pattern_start == pattern_last and start == last)
+            return true;
+        if (pattern_start == pattern_last or *pattern_start != '*') {
+            if(pattern_old == pattern_last)
+                return false;
+            start = ++old;
+            pattern_start = pattern_old;
+        }
+        pattern_start = std::find_if(pattern_start, pattern_last, [](auto c) { return c != '*'; });
+        if(pattern_start == pattern_last)
+            return true;
+        old = start;
+        pattern_old = pattern_start;
+    }
+    return false;
+}
+#endif
 using string_map = std::unordered_map<std::string, std::vector<std::string>>;
 
 template <class Keyword>
--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,5 @@
 #include <test.hpp>
+#include <regex>
 
 bool glob_match(const std::string& input, const std::string& pattern)
 {
@@ -7,47 +8,124 @@ bool glob_match(const std::string& input, const std::string& pattern)
 
 TEST_CASE(globbing)
 {
-    EXPECT(not glob_match("ab", "a"));
-    EXPECT(not glob_match("ba", "a"));
-    EXPECT(not glob_match("bac", "a"));
-    EXPECT(glob_match("ab", "ab"));
+    for(int i=0;i<1000;i++)
+    {
+        EXPECT(not glob_match("ab", "a"));
+        EXPECT(not glob_match("ba", "a"));
+        EXPECT(not glob_match("bac", "a"));
+        EXPECT(glob_match("ab", "ab"));
 
-    // Star loop
-    EXPECT(glob_match("/foo/bar/baz/blig/fig/blig", "/foo/*/blig"));
-    EXPECT(glob_match("/foo/bar/baz/xlig/fig/blig", "/foo/*/blig"));
-    EXPECT(glob_match("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*b"));
-    EXPECT(glob_match("aabaabaab", "a*"));
-    EXPECT(glob_match("aabaabaab", "a*b*ab"));
-    EXPECT(glob_match("aabaabaab", "a*baab"));
-    EXPECT(glob_match("aabaabaab", "aa*"));
-    EXPECT(glob_match("aabaabaab", "aaba*"));
-    EXPECT(glob_match("aabaabqqbaab", "a*baab"));
-    EXPECT(glob_match("aabaabqqbaab", "a*baab"));
-    EXPECT(glob_match("abcdd", "*d"));
-    EXPECT(glob_match("abcdd", "*d*"));
-    EXPECT(glob_match("daaadabadmanda", "da*da*da*"));
-    EXPECT(glob_match("mississippi", "m*issip*"));
+        // Star loop
+        EXPECT(glob_match("/foo/bar/baz/blig/fig/blig", "/foo/*/blig"));
+        EXPECT(glob_match("/foo/bar/baz/xlig/fig/blig", "/foo/*/blig"));
+        EXPECT(glob_match("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*b"));
+        EXPECT(glob_match("aabaabaab", "a*"));
+        EXPECT(glob_match("aabaabaab", "a*b*ab"));
+        EXPECT(glob_match("aabaabaab", "a*baab"));
+        EXPECT(glob_match("aabaabaab", "aa*"));
+        EXPECT(glob_match("aabaabaab", "aaba*"));
+        EXPECT(glob_match("aabaabqqbaab", "a*baab"));
+        EXPECT(glob_match("aabaabqqbaab", "a*baab"));
+        EXPECT(glob_match("abcdd", "*d"));
+        EXPECT(glob_match("abcdd", "*d*"));
+        EXPECT(glob_match("daaadabadmanda", "da*da*da*"));
+        EXPECT(glob_match("mississippi", "m*issip*"));
 
-    // Repeated star
-    EXPECT(glob_match("aabaabqqbaab", "a****baab"));
-    EXPECT(glob_match("abcdd", "***d"));
-    EXPECT(glob_match("abcdd", "***d****"));
+        // Repeated star
+        EXPECT(glob_match("aabaabqqbaab", "a****baab"));
+        EXPECT(glob_match("abcdd", "***d"));
+        EXPECT(glob_match("abcdd", "***d****"));
 
-    // Single wildcard
-    EXPECT(glob_match("abc", "a?c"));
+        // Single wildcard
+        EXPECT(glob_match("abc", "a?c"));
 
-    // Special characters
-    EXPECT(glob_match("test.foo[gpu]", "test.foo[gpu]"));
-    EXPECT(glob_match("test.foo[gpu]", "test.foo[*]"));
-    EXPECT(glob_match("test.foo[gpu]", "*[*"));
+        // Special characters
+        EXPECT(glob_match("test.foo[gpu]", "test.foo[gpu]"));
+        EXPECT(glob_match("test.foo[gpu]", "test.foo[*]"));
+        EXPECT(glob_match("test.foo[gpu]", "*[*"));
 
-    EXPECT(glob_match("test.foo(gpu)", "test.foo(gpu)"));
-    EXPECT(glob_match("test.foo(gpu)", "test.foo(*)"));
-    EXPECT(glob_match("test.foo(gpu)", "*(*"));
+        EXPECT(glob_match("test.foo(gpu)", "test.foo(gpu)"));
+        EXPECT(glob_match("test.foo(gpu)", "test.foo(*)"));
+        EXPECT(glob_match("test.foo(gpu)", "*(*"));
 
-    EXPECT(not glob_match("test.foog", "test.foo[gpu]"));
-    EXPECT(not glob_match("test.foogpu", "test.foo[gpu]"));
-    EXPECT(not glob_match("test_foo", "test.foo"));
+        EXPECT(not glob_match("test.foog", "test.foo[gpu]"));
+        EXPECT(not glob_match("test.foogpu", "test.foo[gpu]"));
+        EXPECT(not glob_match("test_foo", "test.foo"));
+    }
+}
+
+inline void
+replace_string_inplace(std::string& subject, const std::string& search, const std::string& replace)
+{
+    size_t pos = 0;
+    while((pos = subject.find(search, pos)) != std::string::npos)
+    {
+        subject.replace(pos, search.length(), replace);
+        pos += replace.length();
+    }
+}
+
+bool regex_match(const std::string& input, const std::string& pattern)
+{
+#if 0
+    // replace_string_inplace(pattern, "\\", "\\\\");
+    std::string grep_command = "echo '" + input + "' | grep -q -E '" + pattern + "'";
+    // std::cout << grep_command << std::endl;
+    auto result = std::system(grep_command.c_str());
+    return result == 0;
+#else
+    std::regex r(pattern);
+    return std::regex_search(input, r);
+#endif
+}
+
+
+TEST_CASE(regex)
+{
+    for(int i=0;i<1000;i++)
+    {
+        EXPECT(not regex_match("ab", "^a$"));
+        EXPECT(not regex_match("ba", "^a$"));
+        EXPECT(not regex_match("bac", "^a$"));
+        EXPECT(regex_match("ab", "^ab$"));
+
+        // Star loop
+        EXPECT(regex_match("/foo/bar/baz/blig/fig/blig", "^/foo/.*/blig$"));
+        EXPECT(regex_match("/foo/bar/baz/xlig/fig/blig", "^/foo/.*/blig$"));
+        EXPECT(regex_match("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "^a.*a.*a.*a.*a.*a.*a.*a.*b$"));
+        EXPECT(regex_match("aabaabaab", "^a.*$"));
+        EXPECT(regex_match("aabaabaab", "^a.*b.*ab$"));
+        EXPECT(regex_match("aabaabaab", "^a.*baab$"));
+        EXPECT(regex_match("aabaabaab", "^aa.*$"));
+        EXPECT(regex_match("aabaabaab", "^aaba.*$"));
+        EXPECT(regex_match("aabaabqqbaab", "^a.*baab$"));
+        EXPECT(regex_match("aabaabqqbaab", "^a.*baab$"));
+        EXPECT(regex_match("abcdd", "^.*d$"));
+        EXPECT(regex_match("abcdd", "^.*d.*$"));
+        EXPECT(regex_match("daaadabadmanda", "^da.*da.*da.*$"));
+        EXPECT(regex_match("mississippi", "^m.*issip.*$"));
+
+        // Repeated star
+        EXPECT(regex_match("aabaabqqbaab", "^a.*.*.*.*baab$"));
+        EXPECT(regex_match("abcdd", "^.*.*.*d$"));
+        EXPECT(regex_match("abcdd", "^.*.*.*d.*.*.*.*$"));
+
+        // Single wildcard
+        EXPECT(regex_match("abc", "^a.c$"));
+
+        // Special characters
+        EXPECT(regex_match("test.foo[gpu]", "^test\\.foo\\[gpu\\]$"));
+        EXPECT(regex_match("test.foo[gpu]", "^test\\.foo\\[.*\\]$"));
+        EXPECT(regex_match("test.foo[gpu]", "^.*\\[.*$"));
+
+        EXPECT(regex_match("test.foo(gpu)", "^test\\.foo\\(gpu\\)$"));
+        EXPECT(regex_match("test.foo(gpu)", "^test\\.foo\\(.*\\)$"));
+        EXPECT(regex_match("test.foo(gpu)", "^.*\\(.*$"));
+
+        EXPECT(not regex_match("test.foog", "^test\\.foo\\[gpu\\]$"));
+        EXPECT(not regex_match("test.foogpu", "^test\\.foo\\[gpu\\]$"));
+        EXPECT(not regex_match("test_foo", "^test\\.foo$"));
+    }
 }
 
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

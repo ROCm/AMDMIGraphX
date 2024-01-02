5a7e20b52f43d174db4ca1cb1c7c1f722e073430
charlie <charlie.lin@amd.com>
2024-01-02 14:13:02 -0600

    license changes

--- tools/license_stamper.py
+++ tools/license_stamper.py
@@ -2,7 +2,7 @@
 #####################################################################################
 #  The MIT License (MIT)
 #
-#  Copyright (c) 2015-2023 Advanced Micro Devices, Inc. All rights reserved.
+#  Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -90,9 +90,9 @@ def getYearOfLatestCommit(rfile: str) -> datetime:
 
 def updateYear(filename: str, lastCommitYear: str) -> None:
     with open(filename, 'r') as f:
-        newfileContent = re.sub("2015-\d+ Advanced Micro Devices",
-                                f'2015-{lastCommitYear} Advanced Micro Devices',
-                                f.read())
+        newfileContent = re.sub(
+            "2015-\d+ Advanced Micro Devices",
+            f'2015-{lastCommitYear} Advanced Micro Devices', f.read())
 
     with open(filename, 'w') as f:
         f.write(newfileContent)

c0f82bc16009639f1c71cb4adcd4f7bbc7a9808c
charlie <charlie.lin@amd.com>
2024-01-10 10:53:18 -0600

    Unit ops matcher more picky and fix used fp8 eps

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -1219,7 +1219,7 @@ struct find_unit_ops
         auto add_0 = match::name("add")(
             match::either_arg(0, 1)(match::has_value(0.0f, 1, 0), match::any().bind("x")));
         auto sub_0 =
-            match::name("sub")(match::args(match::any().bind("x"), match::has_value(0.0f)));
+            match::name("sub")(match::args(match::any().bind("x"), match::has_value(0.0f, 1, 0)));
         return match::any_of(mul_1, div_1, add_0, sub_0);
     }
 

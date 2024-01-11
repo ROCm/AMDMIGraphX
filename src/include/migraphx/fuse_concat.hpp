4726c6022643a3bf194b5771a9df2f27edf58f98
Khalique Ahmed <15948690+kahmed10@users.noreply.github.com>
2024-01-11 19:00:09 +0000

    fix merge

--- src/include/migraphx/fuse_concat.hpp
+++ src/include/migraphx/fuse_concat.hpp
@@ -1,11 +1,7 @@
 /*
  * The MIT License (MIT)
  *
-<<<<<<<< HEAD:src/include/migraphx/op/scatter_min.hpp
- * Copyright (c) 2015-2023 Advanced Micro Devices, Inc. All rights reserved.
-========
  * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
->>>>>>>> 9941849ca4ffe76c66866b4af74bebfe40eb3c22:src/include/migraphx/fuse_concat.hpp
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,38 +21,16 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-< < < < < < < < HEAD
-    : src /
-      include / migraphx / op /
-      scatter_min.hpp
-#ifndef MIGRAPHX_GUARD_OPERATORS_SCATTER_ELEMENTS_MIN_HPP
-#define MIGRAPHX_GUARD_OPERATORS_SCATTER_ELEMENTS_MIN_HPP
-
-#include <migraphx/op/scatter_op.hpp>
-
-      namespace migraphx{inline namespace MIGRAPHX_INLINE_NS{namespace op{
-
-          struct scatter_min : public scatter_op<scatter_min>{
-              auto reduction() const {return [](auto& x, const auto& y) { x = std::min(x, y); };
-}
-}
-;
-
-} // namespace op
-} // namespace MIGRAPHX_INLINE_NS
-} // namespace migraphx
-
-#endif
-== == == ==
 #ifndef MIGRAPHX_GUARD_MIGRAPHX_FUSE_CONCAT_HPP
 #define MIGRAPHX_GUARD_MIGRAPHX_FUSE_CONCAT_HPP
 
 #include <migraphx/config.hpp>
 #include <string>
 
-    namespace migraphx{inline namespace MIGRAPHX_INLINE_NS{
+namespace migraphx {
+inline namespace MIGRAPHX_INLINE_NS {
 
-        struct module_pass_manager;
+struct module_pass_manager;
 
 struct MIGRAPHX_EXPORT fuse_concat
 {
@@ -67,4 +41,3 @@ struct MIGRAPHX_EXPORT fuse_concat
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx
 #endif // MIGRAPHX_GUARD_MIGRAPHX_FUSE_CONCAT_HPP
->>>>>>>> 9941849ca4ffe76c66866b4af74bebfe40eb3c22 : src / include / migraphx / fuse_concat.hpp

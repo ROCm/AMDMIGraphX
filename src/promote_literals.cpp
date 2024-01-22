0c3c9abc1918ffce5267ba6c5da4dd767df1f746
Janjic <aljanjic@amd.com>
2024-01-22 14:47:15 +0100

    copy ins->outputs() fix promote_literals_test clang-format

--- src/promote_literals.cpp
+++ src/promote_literals.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,14 +41,11 @@ void promote_literals::apply(module_pass_manager& mpm) const
     {
         if(ins->name() == "@literal")
         {
-            auto new_lit = root_module->add_literal(ins->get_literal());
-            for(auto out_ins : ins->outputs())
+            auto new_lit    = root_module->add_literal(ins->get_literal());
+            auto ins_output = ins->outputs();
+            for(auto out_ins : ins_output)
             {
                 out_ins->replace_argument(out_ins, ins, new_lit);
-                // safeguarding for loop iterator
-                auto out_vec = ins->outputs();
-                if(out_vec.size() == 0)
-                    break;
             }
         }
     }

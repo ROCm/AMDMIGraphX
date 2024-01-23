38f4cecefe9840621a4c0b916494630369f0ef93
Paul <pfultz2@yahoo.com>
2024-01-23 14:46:17 -0600

    Add spaces

--- src/targets/gpu/problem_cache.cpp
+++ src/targets/gpu/problem_cache.cpp
@@ -14,15 +14,18 @@ bool problem_cache::has(const std::string& name, const value& problem) const
 {
     return contains(cache, create_key(name, problem));
 }
+
 void problem_cache::insert(const std::string& name, const value& problem, const value& solution)
 {
     assert(not solution.is_null());
     cache[create_key(name, problem)] = solution;
 }
+
 void problem_cache::mark(const std::string& name, const value& problem)
 {
     cache.insert(std::make_pair(create_key(name, problem), value{}));
 }
+
 optional<value> problem_cache::get(const std::string& name, const value& problem) const
 {
     auto it = cache.find(create_key(name, problem));

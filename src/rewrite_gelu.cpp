c0a181d19483038a8c65c54e2df155e4d866852c
charlie <charlie.lin@amd.com>
2024-01-25 16:49:31 -0600

    Initial

--- src/rewrite_gelu.cpp
+++ src/rewrite_gelu.cpp
@@ -32,6 +32,8 @@
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
+MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_GELU_MODE)
+
 /**
  * Finds GELU blocks using the Gaussian distribution and replaces them with the sigmoid
  * approximation if the data type is fp16.
@@ -44,17 +46,62 @@ struct find_gelu_erf
     {
         auto ins = r.result;
         auto x   = r.instructions["x"];
-        if(x->get_shape().type() != migraphx::shape::half_type)
-            return;
+        auto gelu_mode = string_value_of(MIGRAPHX_GELU_MODE{}, "tanh_exp");
 
-        auto lit = m.add_literal(literal{shape{x->get_shape().type()}, {1.702f}});
-        auto mul = insert_common_op(m, ins, make_op("mul"), {x, lit});
-        auto sig = m.insert_instruction(ins, make_op("neg"), mul);
-        sig      = m.insert_instruction(ins, make_op("exp"), sig);
-        auto one = m.add_literal(literal{shape{x->get_shape().type()}, {1.0f}});
-        sig      = insert_common_op(m, ins, make_op("add"), {sig, one});
-        sig      = m.insert_instruction(ins, make_op("div"), x, sig);
-        m.replace_instruction(ins, sig);
+        if(gelu_mode == "erf")
+        {
+            return;
+        }
+        else if(gelu_mode == "tanh_pow")
+        {
+            double sqrt_2_rpi = sqrt(M_2_PI);
+            auto sqrt_2_rpi_lit =
+                m.add_literal(literal{shape{x->get_shape().type()}, {sqrt_2_rpi}});
+            auto fit_const = m.add_literal(literal{shape{x->get_shape().type()}, {0.044715f}});
+            auto one       = m.add_literal(literal{shape{x->get_shape().type()}, {1.0f}});
+            auto half      = m.add_literal(literal{shape{x->get_shape().type()}, {0.5f}});
+            auto three     = m.add_literal(literal{shape{x->get_shape().type()}, {3.0f}});
+            auto x3        = insert_common_op(m, ins, make_op("pow"), {x, three});
+            auto a         = insert_common_op(m, ins, make_op("mul"), {fit_const, x3});
+            auto b         = m.insert_instruction(ins, make_op("add"), x, a);
+            auto c         = insert_common_op(m, ins, make_op("mul"), {sqrt_2_rpi_lit, b});
+            auto tanh_ins  = m.insert_instruction(ins, make_op("tanh"), c);
+            auto d         = insert_common_op(m, ins, make_op("add"), {one, tanh_ins});
+            auto e         = insert_common_op(m, ins, make_op("mul"), {half, x});
+            auto y         = m.insert_instruction(ins, make_op("mul"), {e, d});
+            m.replace_instruction(ins, y);
+        }
+        else if(gelu_mode == "sig")
+        {
+            auto lit = m.add_literal(literal{shape{x->get_shape().type()}, {1.702f}});
+            auto mul = insert_common_op(m, ins, make_op("mul"), {x, lit});
+            auto sig = m.insert_instruction(ins, make_op("neg"), mul);
+            sig      = m.insert_instruction(ins, make_op("exp"), sig);
+            auto one = m.add_literal(literal{shape{x->get_shape().type()}, {1.0f}});
+            sig      = insert_common_op(m, ins, make_op("add"), {sig, one});
+            sig      = m.insert_instruction(ins, make_op("div"), x, sig);
+            m.replace_instruction(ins, sig);
+        }
+        else
+        { // alternative tanh approximation
+            double sqrt_2_rpi = sqrt(M_2_PI);
+            auto sqrt_2_rpi_lit =
+                m.add_literal(literal{shape{x->get_shape().type()}, {sqrt_2_rpi}});
+            auto fit_const = m.add_literal(literal{shape{x->get_shape().type()}, {0.044715f}});
+            auto one       = m.add_literal(literal{shape{x->get_shape().type()}, {1.0f}});
+            auto xb        = insert_common_op(m, ins, make_op("mul"), {x, sqrt_2_rpi_lit});
+            auto a         = insert_common_op(m, ins, make_op("mul"), {xb, fit_const});
+            auto b         = m.insert_instruction(ins, make_op("mul"), a, x);
+            auto c         = m.insert_instruction(ins, make_op("mul"), b, x);
+            auto u         = m.insert_instruction(ins, make_op("add"), c, xb);
+            auto neg_u     = m.insert_instruction(ins, make_op("neg"), u);
+            auto d         = m.insert_instruction(ins, make_op("sub"), neg_u, u);
+            auto emu       = m.insert_instruction(ins, make_op("exp"), d);
+            auto e         = insert_common_op(m, ins, make_op("add"), {one, emu});
+            auto cdf       = insert_common_op(m, ins, make_op("div"), {one, e});
+            auto y         = m.insert_instruction(ins, make_op("mul"), x, cdf);
+            m.replace_instruction(ins, y);
+        }
     }
 };
 

3760de966ebf59ee2d1163308c955100e3b6d029
Paul <pfultz2@yahoo.com>
2024-02-01 15:58:44 -0800

    Remove extra include

--- test/simplify_qdq_test.cpp
+++ test/simplify_qdq_test.cpp
@@ -34,7 +34,6 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/verify.hpp>
 #include <migraphx/apply_alpha_beta.hpp>
-#include <migraphx/pass_manager.hpp>
 
 bool is_convolution(const migraphx::instruction& ins) { return ins.name() == "convolution"; }
 bool is_dot(const migraphx::instruction& ins) { return ins.name() == "dot"; }

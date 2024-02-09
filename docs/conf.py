d3c47894e5ea5cef8b7e96fbe15298a7ba956e29
srawat <120587655+SwRaw@users.noreply.github.com>
2024-02-09 17:34:36 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ docs_core.setup()
 
 external_projects_current_project = "amdmigraphx"
 
-extensions = ['sphinx-collapse']
+extensions = ['sphinx_collapse']
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)

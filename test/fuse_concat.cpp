521b3b035584ad42d39cc6ba71aba6d38777dd2b
Umang Yadav <umayadav@amd.com>
2024-02-16 15:09:49 +0000

    add partial case

--- test/fuse_concat.cpp
+++ test/fuse_concat.cpp
@@ -142,6 +142,42 @@ TEST_CASE(simple_pointwise_concat_pointwise)
     EXPECT(p1 == p2);
 }
 
+TEST_CASE(partial_pointwise_concat)
+{
+    migraphx::shape s1{migraphx::shape::float_type, {1, 4, 8, 8}};
+    migraphx::shape s2{migraphx::shape::float_type, {1, 4, 16, 16}};
+    migraphx::program p1;
+    {
+        auto* mm     = p1.get_main_module();
+        auto x       = mm->add_parameter("x", s1);
+        auto y       = mm->add_parameter("y", s1);
+        auto z       = mm->add_parameter("z", s2);
+        auto pooling = mm->add_instruction(
+            migraphx::make_op("pooling", {{"lengths", {2, 2}}, {"stride", {2, 2}}}), z);
+        auto add    = add_pointwise(p1, "main:pointwise0", {x, y}, single_pointwise("add"));
+        auto concat = mm->add_instruction(migraphx::make_op("concat", {{"axis", 1}}), add, pooling);
+        mm->add_return({concat});
+    }
+    run_pass(p1);
+    migraphx::program p2;
+    {
+        auto* mm     = p2.get_main_module();
+        auto x       = mm->add_parameter("x", s1);
+        auto y       = mm->add_parameter("y", s1);
+        auto z       = mm->add_parameter("z", s2);
+        auto pooling = mm->add_instruction(
+            migraphx::make_op("pooling", {{"lengths", {2, 2}}, {"stride", {2, 2}}}), z);
+        auto fused_concat =
+            add_pointwise_concat(p2,
+                                 1,
+                                 arg("noop:concat2", {}, noop_pointwise()),
+                                 arg("concat:main:pointwise0", {x, y}, single_pointwise("add")),
+                                 arg("concat:noop1", {pooling}, noop_pointwise()));
+        mm->add_return({fused_concat});
+    }
+    EXPECT(p1 == p2);
+}
+
 TEST_CASE(partial_pointwise_concat_pointwise)
 {
     migraphx::shape s1{migraphx::shape::float_type, {1, 4, 8, 8}};

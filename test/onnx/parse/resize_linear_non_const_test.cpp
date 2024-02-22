e29a019907aa5e53020138e9e8b50560b26cd8ee
charlie <charlie.lin@amd.com>
2024-02-22 15:14:42 -0600

    Fix bad merge

--- test/onnx/parse/resize_linear_non_const_test.cpp
+++ test/onnx/parse/resize_linear_non_const_test.cpp
@@ -24,31 +24,9 @@
 
 #include <onnx_test.hpp>
 
-< < < < < < < <
-    HEAD : test / onnx / parse / matmul_dyn_broadcast_test.cpp TEST_CASE(matmul_dyn_broadcast_test)
+TEST_CASE(resize_linear_non_const_test)
 {
-    migraphx::program p;
-    auto* mm = p.get_main_module();
-    auto p0  = mm->add_parameter("1", migraphx::shape{migraphx::shape::float_type, {7}});
-    auto p1  = mm->add_parameter(
-        "2", migraphx::shape{migraphx::shape::float_type, {{5, 5}, {7, 7}, {4, 8, {6}}}});
-    auto usp0         = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0}}}), p0);
-    auto broadcast_p0 = mm->add_instruction(migraphx::make_op("broadcast_for_dot"), usp0, p1);
-    auto broadcast_p1 = mm->add_instruction(migraphx::make_op("broadcast_for_dot"), p1, usp0);
-    auto dot_ins      = mm->add_instruction(migraphx::make_op("dot"), broadcast_p0, broadcast_p1);
-    auto ret          = mm->add_instruction(migraphx::make_op("squeeze", {{"axes", {1}}}), dot_ins);
-    mm->add_return({ret});
-
+    // runtime (non-constant) input is only supported in "nearest" mode
     migraphx::onnx_options options;
-    options.map_dyn_input_dims["2"] = {{5, 5}, {7, 7}, {4, 8, {6}}};
-    auto prog                       = parse_onnx("matmul_dyn_broadcast_test.onnx", options);
-
-    EXPECT(p == prog);
-    == == == == TEST_CASE(resize_linear_non_const_test)
-    {
-        // runtime (non-constant) input is only supported in "nearest" mode
-        migraphx::onnx_options options;
-        EXPECT(test::throws([&] { parse_onnx("resize_linear_non_const_test.onnx", options); }));
-        >>>>>>>> f7cd1d94e93cec261f82ebdf9be74d15a4dc1ab8 : test / onnx / parse /
-                                                            resize_linear_non_const_test.cpp
-    }
+    EXPECT(test::throws([&] { parse_onnx("resize_linear_non_const_test.onnx", options); }));
+}

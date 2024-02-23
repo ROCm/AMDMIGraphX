b0b7cc8c8e4eab866c22f63595b28615f207fd83
Paul <pfultz2@yahoo.com>
2024-02-23 12:54:23 -0800

    Add initial test

---
+++ test/propagate_precision.cpp
@@ -0,0 +1,47 @@
+#include <migraphx/propagate_precision.hpp>
+#include <migraphx/dead_code_elimination.hpp>
+#include <migraphx/pass_manager.hpp>
+#include <migraphx/common.hpp>
+#include <migraphx/module.hpp>
+#include <migraphx/make_op.hpp>
+
+#include <test.hpp>
+
+void run_pass(migraphx::module& m)
+{
+    migraphx::run_passes(
+        m, {migraphx::propagate_precision{}, migraphx::dead_code_elimination{}});
+}
+
+TEST_CASE(simple_input)
+{
+    migraphx::shape s{migraphx::shape::half_type, {2, 3}};
+    migraphx::module m1;
+    {
+        auto x = m1.add_parameter("x", s);
+        auto y = m1.add_parameter("y", s);
+        auto two = m1.add_literal(migraphx::literal{{migraphx::shape::half_type}, {2}});
+        auto div = migraphx::add_common_op(m1, migraphx::make_op("div"), {x, two});
+        auto sqrt = m1.add_instruction(migraphx::make_op("sqrt"), div);
+        auto convert1 = m1.add_instruction(migraphx::make_op("convert", {{"target_type", migraphx::shape::float_type}}), sqrt);
+        auto mul = m1.add_instruction(migraphx::make_op("mul"), convert1, y);
+        auto convert2 = m1.add_instruction(migraphx::make_op("convert", {{"target_type", migraphx::shape::half_type}}), mul);
+        m1.add_return({convert2});
+    }
+    run_pass(m1);
+    migraphx::module m2;
+    {
+        auto x = m2.add_parameter("x", s);
+        auto y = m2.add_parameter("y", s);
+        auto convert1 = m2.add_instruction(migraphx::make_op("convert", {{"target_type", migraphx::shape::float_type}}), x);
+        auto two = m2.add_literal(migraphx::literal{{migraphx::shape::half_type}, {2}});
+        auto div = migraphx::add_common_op(m2, migraphx::make_op("div"), {convert1, two});
+        auto sqrt = m2.add_instruction(migraphx::make_op("sqrt"), div);
+        auto mul = m2.add_instruction(migraphx::make_op("mul"), sqrt, y);
+        auto convert2 = m2.add_instruction(migraphx::make_op("convert", {{"target_type", migraphx::shape::half_type}}), mul);
+        m2.add_return({convert2});
+    }
+    EXPECT(m1.sort() == m2.sort());
+}
+
+int main(int argc, const char* argv[]) { test::run(argc, argv); }

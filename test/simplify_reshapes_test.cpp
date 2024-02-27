1d34f3fe8eca448b3724223f6db688f319af82a5
Shiv <shbhavsa@amd.com>
2024-02-27 15:59:28 +0000

    rework pointwise_dot matcher to only apply when a dot_add or dot_mul simplifications are possible

--- test/simplify_reshapes_test.cpp
+++ test/simplify_reshapes_test.cpp
@@ -2087,24 +2087,20 @@ TEST_CASE(reshape_reshape_dot_gemm_axis)
     EXPECT(m1.sort() == m2.sort());
 }
 
-TEST_CASE(pointwise_transpose)
+TEST_CASE(mul_transpose)
 {
     migraphx::shape s{migraphx::shape::float_type, {2, 32, 64, 64}};
-    migraphx::shape s2{migraphx::shape::float_type, {2, 32, 32}};
+    migraphx::shape s2{migraphx::shape::float_type, {2, 64, 32, 32}};
     migraphx::module m1;
     {
         auto inp   = m1.add_parameter("input", s);
         auto c1    = m1.add_literal(migraphx::generate_literal(s));
-        auto c2    = m1.add_literal(migraphx::generate_literal(s));
         auto mul   = m1.add_instruction(migraphx::make_op("mul"), inp, c1);
-        auto add   = m1.add_instruction(migraphx::make_op("add"), mul, c2);
         auto trans = m1.add_instruction(
-            migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), add);
-        auto rsp =
-            m1.add_instruction(migraphx::make_op("reshape", {{"dims", {2, 4096, 32}}}), trans);
+            migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), mul);
 
         auto c3  = m1.add_literal(migraphx::generate_literal(s2));
-        auto dot = m1.add_instruction(migraphx::make_op("dot"), rsp, c3);
+        auto dot = m1.add_instruction(migraphx::make_op("dot"), trans, c3);
         m1.add_return({dot});
     };
     run_pass(m1);
@@ -2117,15 +2113,44 @@ TEST_CASE(pointwise_transpose)
         auto c1 = m2.add_literal(migraphx::generate_literal(s));
         auto c1_trans =
             m2.add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), c1);
-        auto c2 = m2.add_literal(migraphx::generate_literal(s));
-        auto c2_trans =
-            m2.add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), c2);
         auto mul = m2.add_instruction(migraphx::make_op("mul"), inp_trans, c1_trans);
-        auto add = m2.add_instruction(migraphx::make_op("add"), mul, c2_trans);
-        auto rsp = m2.add_instruction(migraphx::make_op("reshape", {{"dims", {2, 4096, 32}}}), add);
-
         auto c3  = m2.add_literal(migraphx::generate_literal(s2));
-        auto dot = m2.add_instruction(migraphx::make_op("dot"), rsp, c3);
+        auto dot = m2.add_instruction(migraphx::make_op("dot"), mul, c3);
+        m2.add_return({dot});
+    };
+
+    EXPECT(m1.sort() == m2.sort());
+}
+
+TEST_CASE(add_transpose)
+{
+    migraphx::shape s{migraphx::shape::float_type, {2, 32, 64, 64}};
+    migraphx::shape s2{migraphx::shape::float_type, {2, 64, 32, 32}};
+    migraphx::module m1;
+    {
+        auto inp   = m1.add_parameter("input", s);
+        auto c1    = m1.add_literal(migraphx::generate_literal(s));
+        auto mul   = m1.add_instruction(migraphx::make_op("add"), inp, c1);
+        auto trans = m1.add_instruction(
+            migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), mul);
+
+        auto c3  = m1.add_literal(migraphx::generate_literal(s2));
+        auto dot = m1.add_instruction(migraphx::make_op("dot"), trans, c3);
+        m1.add_return({dot});
+    };
+    run_pass(m1);
+
+    migraphx::module m2;
+    {
+        auto inp       = m2.add_parameter("input", s);
+        auto inp_trans = m2.add_instruction(
+            migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), inp);
+        auto c1 = m2.add_literal(migraphx::generate_literal(s));
+        auto c1_trans =
+            m2.add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), c1);
+        auto mul = m2.add_instruction(migraphx::make_op("add"), inp_trans, c1_trans);
+        auto c3  = m2.add_literal(migraphx::generate_literal(s2));
+        auto dot = m2.add_instruction(migraphx::make_op("dot"), mul, c3);
         m2.add_return({dot});
     };
 

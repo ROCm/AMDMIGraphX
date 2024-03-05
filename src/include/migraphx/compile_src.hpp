fcd54f96d948bc7e5a64b3839b3720404664b047
Artur Wojcik <artur.wojcik@amd.com>
2024-03-05 18:42:05 +0100

    merge with the latest changes

--- src/include/migraphx/compile_src.hpp
+++ src/include/migraphx/compile_src.hpp
@@ -56,7 +56,7 @@ struct MIGRAPHX_EXPORT src_compiler
     fs::path compiler                         = MIGRAPHX_CXX_COMPILER;
     std::vector<std::string> flags            = {};
     fs::path output                           = {};
-    fs::path launcher                         = "";
+    fs::path launcher                         = {};
     std::string out_ext                       = ".o";
     std::function<fs::path(fs::path)> process = nullptr;
     std::vector<char> compile(const std::vector<src_file>& srcs) const;

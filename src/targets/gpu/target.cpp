bd4bf0404e53eec359f8572e26320606722bf4da
Ted Themistokleous <tedthemistokleous@amd.com>
2024-03-22 20:57:47 +0000

    fixup! Remove need to exclud uint8 in target

--- src/targets/gpu/target.cpp
+++ src/targets/gpu/target.cpp
@@ -132,7 +132,7 @@ std::vector<pass> target::get_passes(migraphx::context& gctx, const compile_opti
         enable_pass(not mlir_enabled(), rewrite_quantization{}),
         dead_code_elimination{},
         // workaround for rocBLAS unsupported error when using uint8 in quant_dot
-        eliminate_data_type{unsupported_types, shape::type_t::float_type, {{"quant_dot"}}},
+        eliminate_data_type{{migraphx::shape::uint8_type}, shape::type_t::float_type, {{"quant_dot"}}},
         eliminate_data_type{unsupported_types, shape::type_t::float_type},
         simplify_reshapes{},
         eliminate_identity{},

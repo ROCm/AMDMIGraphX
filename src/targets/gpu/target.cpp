9970375febcd64b30945630b5ad953797d35c43c
Ted Themistokleous <tedthemistokleous@amd.com>
2024-04-01 20:41:19 +0000

    Handle uint8 only for dot, quant_dot, quant_conv_ and convolution

--- src/targets/gpu/target.cpp
+++ src/targets/gpu/target.cpp
@@ -92,7 +92,6 @@ std::vector<pass> target::get_passes(migraphx::context& gctx, const compile_opti
     unsupported_types.erase(shape::type_t::half_type);
     unsupported_types.erase(shape::type_t::bool_type);
     unsupported_types.erase(shape::type_t::int8_type);
-    unsupported_types.erase(shape::type_t::uint8_type);
     unsupported_types.erase(shape::type_t::int32_type);
     unsupported_types.erase(shape::type_t::tuple_type);
     // whiltelist supported Ops for the FP8
@@ -131,8 +130,11 @@ std::vector<pass> target::get_passes(migraphx::context& gctx, const compile_opti
         simplify_qdq{},
         enable_pass(not mlir_enabled(), rewrite_quantization{}),
         dead_code_elimination{},
-        // workaround for rocBLAS unsupported error when using uint8 in quant_dot
+        // workaround for rocBLAS unsupported error when using uint8 in quant_dot & quant_convolution
         eliminate_data_type{{migraphx::shape::uint8_type}, shape::type_t::float_type, {{"quant_dot"}}},
+        eliminate_data_type{{migraphx::shape::uint8_type}, shape::type_t::float_type, {{"dot"}}},
+        eliminate_data_type{{migraphx::shape::uint8_type}, shape::type_t::float_type, {{"quant_convolution"}}},
+        eliminate_data_type{{migraphx::shape::uint8_type}, shape::type_t::float_type, {{"convolution"}}},
         eliminate_data_type{unsupported_types, shape::type_t::float_type},
         simplify_reshapes{},
         eliminate_identity{},

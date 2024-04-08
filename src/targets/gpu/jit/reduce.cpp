0c518399a8d8ed1fec3281ec0dc70bc0daea6e88
Paul <pfultz2@yahoo.com>
2024-04-08 14:17:22 -0700

    Add docstring

--- src/targets/gpu/jit/reduce.cpp
+++ src/targets/gpu/jit/reduce.cpp
@@ -133,6 +133,12 @@ static std::size_t compute_subwave_size(context& ctx, std::size_t n)
     return wavefront_size;
 }
 
+/// This will adjust the input shapes so a partial reduction is done per workgroup.
+/// This is done by splitting the reduction axis so each split group becomes
+/// part of the batch. So if we want to do a split redution of a tensor
+/// {K}, then this will create a tensor of {K/N, N} where N is the number of
+/// split groups. To compute the number of split groups it finds the largets
+/// divisor that can divide K to make it less than min_size. 
 static std::vector<shape> split_reduce(const std::vector<shape>& inputs,
                                        std::size_t min_size = 1024)
 {

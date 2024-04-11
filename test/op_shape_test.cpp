66e3f0e9c5a6d7eee55327c18b612249617c0996
Brian Pickrell <bpickrel@amd.com>
2024-04-11 10:56:02 -0700

    correct data type in test

--- test/op_shape_test.cpp
+++ test/op_shape_test.cpp
@@ -973,7 +973,7 @@ TEST_CASE(broadcast_with_dims2)
     shape s1{migraphx::shape::int64_type, {4}};
     std::size_t max_int = std::numeric_limits<std::size_t>::max();
     std::vector<shape::dynamic_dimension> dyn_dims(4, shape::dynamic_dimension{0, max_int});
-    expect_shape(shape{migraphx::shape::int32_type, dyn_dims},
+    expect_shape(shape{migraphx::shape::float_type, dyn_dims},
                  migraphx::make_op("broadcast_with_dims"),
                  s0,
                  s1);

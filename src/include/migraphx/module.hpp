230247ad5d45a5b83796530639d73d3b59fdb91d
Paul <pfultz2@yahoo.com>
2024-04-16 15:00:32 -0700

    Add docstring

--- src/include/migraphx/module.hpp
+++ src/include/migraphx/module.hpp
@@ -209,6 +209,9 @@ struct MIGRAPHX_EXPORT module
         bool strict_lens                               = false;
         std::vector<std::size_t> scalar_const_out_lens = {};
     };
+    
+    /// Compute a new ouput shape by replacing each parameter with input
+    /// shapes passed in. 
     std::vector<shape> compute_shapes(const std::vector<shape>& inputs,
                                       compute_shapes_options options) const;
     std::vector<shape> compute_shapes(const std::vector<shape>& inputs) const;

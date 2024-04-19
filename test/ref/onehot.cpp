0244a5ef0917b2fb9a0c5aa2140f1f16b0972a19
charlie <charlie.lin@amd.com>
2024-04-19 16:30:21 -0500

    add more tests

--- test/ref/onehot.cpp
+++ test/ref/onehot.cpp
@@ -77,7 +77,7 @@ TEST_CASE(onehot1)
     auto depth_lit    = mm->add_literal(migraphx::literal{depth_s, {3}});
     auto values_param = mm->add_parameter("values", values_s);
     mm->add_instruction(
-        migraphx::make_op("onehot", {{"axis", -1}}), inds_param, depth_lit, values_param);
+        migraphx::make_op("onehot", {{"axis", 0}}), inds_param, depth_lit, values_param);
     p.compile(migraphx::make_target("ref"));
 
     migraphx::parameter_map params;
@@ -90,13 +90,77 @@ TEST_CASE(onehot1)
     // clang-format off
     std::vector<float> gold =
     {
-        1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0
+        1.0, 0.0,
+        0.0, 0.0,
+
+        0.0, 0.0,
+        1.0, 0.0,
+
+        0.0, 1.0,
+        0.0, 1.0
     };
     // clang-format on
     std::vector<float> results_vector(2 * 2 * 3);
     result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
     EXPECT(migraphx::verify::verify_rms_range(results_vector, gold));
+}
+
+TEST_CASE(onehot_dyn)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    migraphx::shape inds_s{migraphx::shape::int64_type, {{1, 4}, {2, 2}}};
+    migraphx::shape depth_s{migraphx::shape::int64_type, {1}};
+    migraphx::shape values_s{migraphx::shape::int32_type, {2}};
+    auto inds_param   = mm->add_parameter("indices", inds_s);
+    auto depth_lit    = mm->add_literal(migraphx::literal{depth_s, {3}});
+    auto values_param = mm->add_parameter("values", values_s);
+    mm->add_instruction(
+        migraphx::make_op("onehot", {{"axis", -1}}), inds_param, depth_lit, values_param);
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::parameter_map params;
+    std::vector<int64_t> indices_data = {0, 2, 1, -1};
+    std::vector<int64_t> depth_data   = {3};
+    std::vector<int32_t> values_data  = {-3, 5};
+    migraphx::shape static_inds_shape{migraphx::shape::int64_type, {2, 2}};
+    params["indices"] = migraphx::argument(static_inds_shape, indices_data.data());
+    params["values"]  = migraphx::argument(values_s, values_data.data());
+    auto result       = p.eval(params).back();
+    // clang-format off
+    std::vector<int32_t> gold =
+    {
+        5, -3, -3,
+        -3, -3, 5,
+        -3, 5, -3,
+        -3, -3, 5 
+    };
+    // clang-format on
+    std::vector<int32_t> results_vector(2 * 2 * 3);
+    result.visit([&](auto output) { results_vector.assign(output.begin(), output.end()); });
+    EXPECT(migraphx::verify::verify_rms_range(results_vector, gold));
+}
+
+TEST_CASE(onehot_neg_depth_error)
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    migraphx::shape inds_s{migraphx::shape::int64_type, {4}};
+    migraphx::shape depth_s{migraphx::shape::int64_type, {1}};
+    migraphx::shape values_s{migraphx::shape::float_type, {2}};
+    auto inds_param   = mm->add_parameter("indices", inds_s);
+    auto depth_param  = mm->add_parameter("depth", depth_s);
+    auto values_param = mm->add_parameter("values", values_s);
+    mm->add_instruction(
+        migraphx::make_op("onehot", {{"axis", -1}}), inds_param, depth_param, values_param);
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::parameter_map params;
+    std::vector<int64_t> indices_data = {0, 2, -1, 5};
+    std::vector<int64_t> depth_data   = {-2};
+    std::vector<float> values_data    = {0.0, 5.0};
+    params["indices"]                 = migraphx::argument(inds_s, indices_data.data());
+    params["depth"]                   = migraphx::argument(depth_s, depth_data.data());
+    params["values"]                  = migraphx::argument(values_s, values_data.data());
+    EXPECT(test::throws([&] { std::ignore = p.eval(params).back(); }));
 }

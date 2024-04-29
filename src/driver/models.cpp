27203029b45364f8dd4b4322989154a87a99ad7a
Umang Yadav <umayadav@amd.com>
2024-04-29 22:43:08 +0000

    Remove "--model" and add "--test" flag

--- src/driver/models.cpp
+++ src/driver/models.cpp
@@ -22,13 +22,23 @@
  * THE SOFTWARE.
  */
 
+#include "models.hpp"
 #include <migraphx/program.hpp>
+#include <migraphx/make_op.hpp>
 
 namespace migraphx {
 namespace driver {
 inline namespace MIGRAPHX_INLINE_NS {
 
-migraphx::program test_gemm();
+migraphx::program test_gemm()
+{
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+    auto a   = mm->add_parameter("a", migraphx::shape{migraphx::shape::float_type, {4, 5}});
+    auto b   = mm->add_parameter("b", migraphx::shape{migraphx::shape::float_type, {5, 3}});
+    mm->add_instruction(migraphx::make_op("dot"), a, b);
+    return p;
+}
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace driver

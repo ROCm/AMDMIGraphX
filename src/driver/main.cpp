ad9453a0716d0615224a2855cbbe1acd388a3776
Umang Yadav <umayadav@amd.com>
2024-04-29 22:56:01 +0000

    formatting

--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -682,7 +682,7 @@ struct run_cmd : command<run_cmd>
 struct perf : command<perf>
 {
     compiler c;
-    unsigned n = 100;
+    unsigned n    = 100;
     bool detailed = false;
     void parse(argument_parser& ap)
     {

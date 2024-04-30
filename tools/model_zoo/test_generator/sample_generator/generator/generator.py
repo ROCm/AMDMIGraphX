1cb3c8c42384409b7d1c62a52fe408eba9f2363f
Attila Dusnoki <attila.dusnoki@htecgroup.com>
2024-04-30 15:11:46 +0000

    Move inference into function

--- tools/model_zoo/test_generator/sample_generator/generator/generator.py
+++ tools/model_zoo/test_generator/sample_generator/generator/generator.py
@@ -28,6 +28,28 @@ import onnxruntime as ort
 from ..utils import get_model_io, numpy_to_pb
 
 
+def _inference(session, input_data_map, outputs, folder_name):
+    input_pb_name = "input_{}.pb"
+    output_pb_name = "output_{}.pb"
+    os.makedirs(folder_name, exist_ok=True)
+    for input_idx, (input_name,
+                    input_data) in enumerate(input_data_map.items()):
+        numpy_to_pb(input_name, input_data,
+                    f"{folder_name}/{input_pb_name.format(input_idx)}")
+
+    ort_result = session.run(outputs, input_data_map)
+    output_data_map = {
+        output_name: result_data
+        for (output_name, result_data) in zip(outputs, ort_result)
+    }
+    for output_idx, (output_name,
+                     result_data) in enumerate(output_data_map.items()):
+        numpy_to_pb(output_name, result_data,
+                    f"{folder_name}/{output_pb_name.format(output_idx)}")
+
+    return output_data_map
+
+
 def generate_test_dataset(model,
                           dataset,
                           output_folder_prefix=None,
@@ -35,8 +57,6 @@ def generate_test_dataset(model,
                           decode_limit=None):
     output_path = f"{output_folder_prefix or 'generated'}/{dataset.name()}/{model.name()}"
     folder_name_prefix = f"{output_path}/test_data_set"
-    input_pb_name = "input_{}.pb"
-    output_pb_name = "output_{}.pb"
 
     # Model
     try:
@@ -54,23 +74,9 @@ def generate_test_dataset(model,
         is_eos, decode_idx = False, 0
         while not is_eos and not (decode_limit and decode_limit <= decode_idx):
             folder_name = f"{folder_name_prefix}_{test_idx}"
-            os.makedirs(folder_name, exist_ok=True)
-            for input_idx, (input_name,
-                            input_data) in enumerate(input_data_map.items()):
-                numpy_to_pb(
-                    input_name, input_data,
-                    f"{folder_name}/{input_pb_name.format(input_idx)}")
 
-            ort_result = sess.run(outputs, input_data_map)
-            output_data_map = {
-                output_name: result_data
-                for (output_name, result_data) in zip(outputs, ort_result)
-            }
-            for output_idx, (output_name, result_data) in enumerate(
-                    output_data_map.items()):
-                numpy_to_pb(
-                    output_name, result_data,
-                    f"{folder_name}/{output_pb_name.format(output_idx)}")
+            output_data_map = _inference(sess, input_data_map, outputs,
+                                         folder_name)
 
             is_eos = not model.is_decoder() or model.decode_step(
                 input_data_map, output_data_map)

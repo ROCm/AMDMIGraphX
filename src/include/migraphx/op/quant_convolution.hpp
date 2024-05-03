da0ae2ac3b6839fac4ea2ad842474041f27ee68c
Ted Themistokleous <tedthemistokleous@amd.com>
2024-05-03 19:13:33 +0000

    Add uint8 as supported input type for quant_convolution

--- src/include/migraphx/op/quant_convolution.hpp
+++ src/include/migraphx/op/quant_convolution.hpp
@@ -81,6 +81,7 @@ struct quant_convolution
         const shape& input   = inputs.at(0);
         const shape& weights = inputs.at(1);
         auto t               = input.type();
+        auto w               = weights.type();
         size_t kdims         = input.lens().size() - 2;
         if(kdims != this->kdims())
         {
@@ -89,10 +90,16 @@ struct quant_convolution
 
         // all input type must be int8_type and output is float_type
         std::set<migraphx::shape::type_t> supported_types = {
-            shape::int8_type, shape::uint8_type, shape::fp8e4m3fnuz_type};
+            shape::uint8_type, shape::int8_type, shape::fp8e4m3fnuz_type};
         if(not contains(supported_types, t))
         {
-            MIGRAPHX_THROW("QUANT_CONVOLUTION: only accept input and weights of type int8_t or "
+            MIGRAPHX_THROW("QUANT_CONVOLUTION: only accept input of type int8_t or "
+                           "fp8e4m3fnuz_type");
+        }
+
+        if(not contains(supported_types, w))
+        {
+            MIGRAPHX_THROW("QUANT_CONVOLUTION: only accept weights of type int8_t or "
                            "fp8e4m3fnuz_type");
         }
 

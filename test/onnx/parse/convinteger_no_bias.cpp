47b5f10772183b7f16aa4f3a173ae23c525c23ae
Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
2024-05-07 09:17:45 -0400

    Handle weight and data input zero points for ConvInteger (#2888)

--- test/onnx/parse/convinteger_no_bias.cpp
+++ test/onnx/parse/convinteger_no_bias.cpp
@@ -24,19 +24,17 @@
 
 #include <onnx_test.hpp>
 
-TEST_CASE(conv_bias_test)
+TEST_CASE(convinteger_no_bias)
 {
     migraphx::program p;
-    auto* mm      = p.get_main_module();
-    auto l0       = mm->add_parameter("0", {migraphx::shape::float_type, {1, 3, 32, 32}});
-    auto l1       = mm->add_parameter("1", {migraphx::shape::float_type, {1, 3, 5, 5}});
-    auto l2       = mm->add_parameter("2", {migraphx::shape::float_type, {1}});
-    uint64_t axis = 1;
-    auto l3       = mm->add_instruction(migraphx::make_op("convolution"), l0, l1);
-    auto l4       = mm->add_instruction(
-        migraphx::make_op("broadcast", {{"axis", axis}, {"out_lens", l3->get_shape().lens()}}), l2);
-    mm->add_instruction(migraphx::make_op("add"), l3, l4);
+    auto* mm    = p.get_main_module();
+    auto data   = mm->add_parameter("0", {migraphx::shape::int8_type, {1, 3, 5, 5}});
+    auto weight = mm->add_parameter("1", {migraphx::shape::int8_type, {1, 3, 2, 2}});
+    mm->add_literal(migraphx::literal{migraphx::shape{data->get_shape().type(), {1}, {0}}, {0}});
+    mm->add_literal(migraphx::literal{migraphx::shape{data->get_shape().type(), {1}, {0}}, {0}});
 
-    auto prog = optimize_onnx("conv_bias_test.onnx");
+    mm->add_instruction(migraphx::make_op("quant_convolution"), data, weight);
+
+    auto prog = optimize_onnx("convinteger_no_bias_test.onnx");
     EXPECT(p == prog);
 }

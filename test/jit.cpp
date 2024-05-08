5163f518dd08ad77dc0048255fbb5b0c0badd0bf
Artur Wojcik <artur.wojcik@amd.com>
2024-05-08 22:55:29 +0200

    add missing EXPORT for Linux

--- test/jit.cpp
+++ test/jit.cpp
@@ -49,6 +49,7 @@ std::function<F> compile_function(std::string_view src, const std::string& symbo
     compiler.flags.emplace_back("-std=c++14");
 #ifndef _WIN32
     compiler.flags.emplace_back("-fPIC");
+    compiler.flags.emplace_back("-DEXPORT=\"\"");
 #else
     compiler.flags.emplace_back("-DEXPORT=\"__declspec(dllexport)\"");
 #endif

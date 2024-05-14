190c292e7f5f2465f05b6d8ed744712f56f0fade
Umang Yadav <umayadav@amd.com>
2024-05-14 20:43:12 +0000

    enable fusion on gfx941/2

--- src/targets/gpu/fuse_ck.cpp
+++ src/targets/gpu/fuse_ck.cpp
@@ -21,11 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include <migraphx/gpu/fuse_ck.hpp>
-#include <migraphx/gpu/gemm_softmax_gemm.hpp>
+
 #include <migraphx/matcher.hpp>
 #include <migraphx/pass_manager.hpp>
+#include <migraphx/stringutils.hpp>
 #include <migraphx/register_op.hpp>
+#include <migraphx/gpu/fuse_ck.hpp>
+#include <migraphx/gpu/gemm_softmax_gemm.hpp>
 #include <migraphx/gpu/device_name.hpp>
 
 namespace migraphx {
@@ -106,7 +108,7 @@ MIGRAPHX_PRED_MATCHER(is_ck_gemm, instruction_ref ins)
             return false;
     }
     auto device_name = trim(split_string(get_device_name(), ':').front());
-    if(device_name == "gfx940")
+    if(starts_with(device_name, "gfx94"))
     {
         if(ins->get_shape().type() == shape::half_type)
         {

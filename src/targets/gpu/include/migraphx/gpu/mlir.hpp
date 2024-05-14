e093b3b0e1671c6c41d2ed18e2880400f49edccc
Umang Yadav <umayadav@amd.com>
2024-05-14 19:31:16 +0000

    formating

--- src/targets/gpu/include/migraphx/gpu/mlir.hpp
+++ src/targets/gpu/include/migraphx/gpu/mlir.hpp
@@ -39,7 +39,8 @@ namespace gpu {
 
 MIGRAPHX_GPU_EXPORT std::string dump_mlir(const module& m);
 
-MIGRAPHX_GPU_EXPORT bool is_module_fusible(const module& m, const context& migraphx_ctx, const value& solution);
+MIGRAPHX_GPU_EXPORT bool
+is_module_fusible(const module& m, const context& migraphx_ctx, const value& solution);
 
 struct MIGRAPHX_GPU_EXPORT mlir_code_object
 {

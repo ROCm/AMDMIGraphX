ffdba3cae478400570783730234bb178a4990c40
Paul <pfultz2@yahoo.com>
2024-05-14 15:41:20 -0700

    Add another comment

--- src/targets/gpu/fuse_mlir.cpp
+++ src/targets/gpu/fuse_mlir.cpp
@@ -392,6 +392,8 @@ bool is_pointwise_op_supported_by_mlir(const instruction& i)
     return false;
 }
 
+// A seprate function so we can remove operators that are supported by mlir
+// but not supported for an input fusion.
 bool is_pointwise_op_supported_by_mlir_for_input(const instruction& i)
 {
     return is_pointwise_op_supported_by_mlir(i);

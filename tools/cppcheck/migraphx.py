dada9f849a73b5a25efcc6c461aa90109445be9c
Eddie Liao <eddie.liao@amd.com>
2024-06-12 17:19:28 +0000

    Add NestedParentheses check

--- tools/cppcheck/migraphx.py
+++ tools/cppcheck/migraphx.py
@@ -426,3 +426,10 @@ def useStlAlgorithms(cfg, data):
                                  "Use std::equal_range instead.")
         elif token.str == 'memchr':
             cppcheck.reportError(token, "style", "Use std::find instead.")
+
+@cppcheck.checker
+def NestedParentheses(cfg, data):
+    for token in cfg.tokenlist:
+        if not simpleMatch(token, ") ) ) )"):
+            continue
+        cppcheck.reportError(token, "style", "Too many nested parentheses can affect readability; consider using variables instead.")

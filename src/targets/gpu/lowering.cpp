df952cd96b50cb2c6eb8bffe4bee0ad96ce9f779
tvukovic-amd <tvukovic@amd.com>
2024-06-13 22:16:39 +0200

    pooling fallback to cpu in gpu tests

--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -116,6 +116,7 @@ struct miopen_apply
         add_loop_op();
         add_neg_op();
         add_nms_op();
+        add_pooling_op();
         add_select_module_op();
         add_reshape_lazy_op();
     }
@@ -362,6 +363,26 @@ struct miopen_apply
     void add_nms_op()
     {
         apply_map.emplace("nonmaxsuppression", [=](instruction_ref ins) {
+            auto s      = ins->get_shape();
+            auto output = insert_allocation(ins, s);
+            std::vector<instruction_ref> cpu_inputs;
+            auto inputs = ins->inputs();
+            std::transform(
+                inputs.begin(), inputs.end(), std::back_inserter(cpu_inputs), [&](auto in) {
+                    return mod->insert_instruction(ins, make_op("hip::copy_from_gpu"), in);
+                });
+            cpu_inputs.front() =
+                mod->insert_instruction(ins, make_op("hip::sync_stream"), cpu_inputs);
+            auto cpu_out = mod->insert_instruction(ins, ins->get_operator(), cpu_inputs);
+            auto gpu_out =
+                mod->insert_instruction(ins, make_op("hip::copy_to_gpu"), cpu_out, output);
+            return mod->replace_instruction(ins, gpu_out);
+        });
+    }
+
+    void add_pooling_op()
+    {
+        apply_map.emplace("pooling", [=](instruction_ref ins) {
             auto s      = ins->get_shape();
             auto output = insert_allocation(ins, s);
             std::vector<instruction_ref> cpu_inputs;

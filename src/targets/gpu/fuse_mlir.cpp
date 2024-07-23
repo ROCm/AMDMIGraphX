1e981a2d0a4f7e6c4fd039e59acca0ed2dc46162
Umang Yadav <umayadav@amd.com>
2024-07-23 15:06:38 +0000

    add verify test

--- src/targets/gpu/fuse_mlir.cpp
+++ src/targets/gpu/fuse_mlir.cpp
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include "migraphx/instruction_ref.hpp"
 #include <migraphx/gpu/fuse_mlir.hpp>
 #include <migraphx/gpu/mlir.hpp>
 #include <migraphx/matcher.hpp>

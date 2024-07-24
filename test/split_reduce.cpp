0f785f00f6c564dc820c085358009869e5221b9a
Paul Fultz II <pfultz2@yahoo.com>
2024-07-24 09:56:39 -0500

    Update test/split_reduce.cpp
    Co-authored-by: Umang Yadav <29876643+umangyadav@users.noreply.github.com>

--- test/split_reduce.cpp
+++ test/split_reduce.cpp
@@ -352,7 +352,7 @@ TEST_CASE(double_split_live)
                 auto rsum1 = rm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", axes}}),
                                                  inputs[0]);
                 auto mul   = add_pointwise(
-                    p2, rm, "main:pointwise1", {inputs[0], inputs[0]}, single_pointwise("mul"));
+                    p2, rm, "main:pointwise1", {inputs[0]}, squared());
                 auto rsum2 =
                     rm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", axes}}), mul);
                 return {rsum1, rsum2};

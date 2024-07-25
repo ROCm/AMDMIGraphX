ff94e0490991e1a807f4974bc5381e84ec563aa6
Umang Yadav <umayadav@amd.com>
2024-07-25 14:54:21 +0000

    add logic for checking is mlir_split_reduce

--- src/targets/gpu/fuse_mlir.cpp
+++ src/targets/gpu/fuse_mlir.cpp
@@ -355,7 +355,6 @@ bool is_pointwise_op_supported_by_mlir(const instruction& i)
         "erf",
         "exp",
         "floor",
-        "reduce_sum",
         "log",
         "recip",
         "sqrt",
@@ -387,6 +386,22 @@ bool is_pointwise_op_supported_by_mlir(const instruction& i)
     return false;
 }
 
+bool is_reduce_op_supported_by_mlir(const instruction& i)
+{
+    using type_t                                      = shape::type_t;
+    const auto& name                                  = i.name();
+    const auto result_type                            = i.get_shape().type();
+    const std::initializer_list<type_t> allowed_types = {
+        type_t::float_type, type_t::half_type, type_t::fp8e4m3fnuz_type};
+    // Preliminary type check.
+    if(not contains(allowed_types, result_type))
+    {
+        return false;
+    }
+    const std::initializer_list<std::string> reduce_ops = {"reduce_mean", "reduce_sum"};
+    return contains(reduce_ops, i.name());
+}
+
 // A separate function so we can remove operators that are supported by mlir
 // but not supported for an input fusion.
 bool is_pointwise_op_supported_by_mlir_for_input(const instruction& i)
@@ -398,15 +413,29 @@ MIGRAPHX_PRED_MATCHER(mlir_split_reduce, instruction_ref ins)
 {
     if(ins->name() != "split_fused_reduce")
         return false;
-    std::vector<module_ref> sub_mods = ins->module_inputs().front()->get_sub_modules(true);
-    sub_mods.insert(sub_mods.begin(), ins->module_inputs().front());
-    // for(const auto& mod : sub_mods)
-    // {
-    //     if(not std::all_of(mod->begin(), mod->end(), &is_pointwise_op_supported_by_mlir))
-    //     {
-    //         return false;
-    //     }
-    // }
+    auto mod_arg            = ins->module_inputs().front();
+    auto supported_reshapes = reshaper_names();
+    supported_reshapes.erase("slice");
+    std::unordered_set<std::string> builtins = {"@param", "@literal", "@return"};
+    for(const auto i : iterator_for(*mod_arg))
+    {
+        if(is_reduce(*i))
+        {
+            if(not is_reduce_op_supported_by_mlir(*i))
+                return false;
+        }
+        else if(i->name() == "pointwise")
+        {
+            if(not std::all_of(i->module_inputs().front()->begin(),
+                               i->module_inputs().front()->end(),
+                               &is_pointwise_op_supported_by_mlir))
+                return false;
+        }
+        else if(not contains(reshaper_names(), i->name()) and not contains(builtins, i->name()))
+        {
+            return false;
+        }
+    }
     return true;
 }
 

4fffb177e1e58fae3e18782374d587ec652fa752
Brian Pickrell <bpickrel@amd.com>
2024-08-08 17:26:59 +0000

    format

--- src/include/migraphx/check_shapes.hpp
+++ src/include/migraphx/check_shapes.hpp
@@ -241,9 +241,9 @@ struct check_shapes
     const check_shapes& compatible_layout() const
     {
         if(begin != end and this->any_of([&](const shape& s) {
-            return not migraphx::is_compatible_shape(s, *begin) and
-                   find_permutation(s) != find_permutation(*begin);
-        }))
+               return not migraphx::is_compatible_shape(s, *begin) and
+                      find_permutation(s) != find_permutation(*begin);
+           }))
             MIGRAPHX_THROW(prefix() + "Layouts do not match");
         return *this;
     }

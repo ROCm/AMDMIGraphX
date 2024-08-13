440eef3de1c8b5eaff789f6a27350df086abb34d
Paul Fultz II <pfultz2@yahoo.com>
2024-08-13 09:16:45 -0500

    Fix matching of concat op in eliminate_concat on the gpu (#3365)

--- tools/include/concat_opt.hpp
+++ tools/include/concat_opt.hpp
@@ -33,6 +33,7 @@
 
 #include <migraphx/operation.hpp>
 #include <migraphx/op/concat.hpp>
+#include <migraphx/optional.hpp>
 #include <migraphx/config.hpp>
 
 namespace migraphx {
@@ -43,22 +44,20 @@ inline namespace MIGRAPHX_INLINE_NS {
 /// An interface for target-dependent optimization for the concat instruction
 struct concat_optimization
 {
-    /// The name of the target-dependent concat operator
-    std::string name() const;
     /// A name of the target-dependent allocate operator
     std::string allocate() const;
     /// Return the target-independent concat operator
-    op::concat get_concat(const operation& op) const;
+    optional<op::concat> get_concat(const operation& op) const;
 };
 
 #else
 
 <%
-interface('concat_optimization',
-    virtual('name', returns='std::string', const=True),
-    virtual('allocate', returns='std::string', const=True),
-    virtual('get_concat', returns='op::concat', op='const operation&', const=True)
-)
+    interface(
+        'concat_optimization',
+        virtual('allocate', returns = 'std::string', const = True),
+        virtual(
+            'get_concat', returns = 'optional<op::concat>', op = 'const operation&', const = True))
 %>
 
 #endif

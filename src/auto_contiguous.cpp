9bf51a2861030359df0e4a78b141b6c1bfaa0c63
Brian Pickrell <bpickrel@amd.com>
2024-08-16 21:20:56 +0000

    typo in auto_contiguous logic

--- src/auto_contiguous.cpp
+++ src/auto_contiguous.cpp
@@ -66,7 +66,7 @@ void auto_contiguous::apply(module& m) const
         shape s = ins->get_shape();
         // If s is not standard layout or has out of sequence strides, insert "contiguous" op
         // to make a standard shape
-        if(not s.dynamic() and (not s.transposed() or s.normalize_standard() != s) and
+        if(not s.dynamic() and (not s.standard() or s.normalize_standard() != s) and
             s.elements() != 0)
         {
             auto c = m.insert_instruction(std::next(ins), make_op("contiguous"), ins);

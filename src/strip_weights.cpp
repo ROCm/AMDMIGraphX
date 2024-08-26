f9ea7abf6c3e3c6cfef3448f754f711edcbcad3c
simberg-amd <Stephen.Simberg@amd.com>
2024-08-26 13:53:12 -0500

    Got weights fetched at runtime working, tried to optimize file writing/reading

--- src/strip_weights.cpp
+++ src/strip_weights.cpp
@@ -28,10 +28,12 @@
 #include <migraphx/make_op.hpp>
 #include <migraphx/register_op.hpp>
 #include <msgpack.hpp>
+#include <sys/stat.h>
 #include <fstream>
 
 // Define if want to test reading literals back into instructions
-#define TEST_READ_LITERALS
+
+#define TEST_READ
 
 // Currently run using:
 // "MIGRAPHX_COPY_LITERALS=true ./build/bin/driver compile <model> --strip-weights -o <.mxr>
@@ -40,23 +42,42 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
 
-// Used as a dummy instruction to repalce literals in .mxr, 
-// currently saves shape of literal as cannot serialize shape with a msgpack
+// Operation used to fetch literal arguments from file during runtime
 struct fetch_literal
 {
     size_t id;
     shape l_shape;
-    argument l_argument;
+    std::string file_header;
     std::string name() const { return "fetch_literal"; }
 
     template <class Self, class F>
     static auto reflect(Self& self, F f)
     {
-        return pack(f(self.l_shape, "shape"), f(self.id, "id"));
+        return pack(f(self.l_shape, "shape"), f(self.id, "id"), f(self.file_header, "file_header"));
     }
 
     shape compute_shape(const std::vector<shape>&) const { return l_shape; }
-    argument compute(const std::vector<argument>&) const { return l_argument; }
+    argument compute(context&, const shape&, const std::vector<argument>&) const { 
+
+        // read literal argument from file
+        std::ifstream is;
+        is.open(file_header + "/literal_" + std::to_string(id), std::ios::binary);
+        if(not is.is_open())
+        {
+            MIGRAPHX_THROW("Could not open file: " + file_header + "/literal_" + std::to_string(id));
+        }
+
+        auto buffer = make_shared_array<char>(l_shape.bytes());
+
+        if(not is.read(buffer.get(), l_shape.bytes()))
+        {
+            MIGRAPHX_THROW("Failed to read file: " + file_header + "/literal_" + std::to_string(id));
+        }
+        is.close();
+            
+        return argument(l_shape, buffer); 
+    
+    }
     friend std::ostream& operator<<(std::ostream& os, const fetch_literal& x)
     {
         os << x.name() << "[id=" << x.id << "]";
@@ -65,163 +86,82 @@ struct fetch_literal
 };
 MIGRAPHX_REGISTER_OP(fetch_literal);
 
-// serializes the literals
-struct vector_stream
-{
-    std::vector<char> buffer{};
-    vector_stream& write(const char* b, std::size_t n)
-    {
-        buffer.insert(buffer.end(), b, b + n);
-        return *this;
-    }
-};
 
-class test_literal : public raw_data<test_literal>
-{
-    private:
-    // TODO Use this id to match with saved literals instead of assuming saved literals will always be in order they appear in.
-    size_t l_id; // id of literal
-
-    std::vector<char> l_data; // literals are char buffers so save data with std::vector<char>
-
-    public:
-    MSGPACK_DEFINE(l_id, l_data); // serializes class
-
-    test_literal(size_t id = 0, std::size_t bytes = 0, const char* data = nullptr)
-        : l_id(id)
-    {
-        l_data = std::vector<char>(data, data + bytes);
-    }
-
-    bool empty() const { return l_data.empty(); }
-
-    const char* get_data() const { return &l_data[0]; }
-
-    std::vector<test_literal> get_sub_objects() const { return {}; }
-
-    void print(std::ostream& os) const
-    {
-        os << "test_literal[id=" << l_id << ", data={";
-        if(l_data.size() < 10)
-        {
-            os << this;
-        }
-        else
-        {
-            os << " ... ";
-        }
-        os << ", size=" << l_data.size() << "}";
-    }
-};
-
-
-// test to actually strip weights, 
+// strips the weights (might eventually move to write_literals.cpp, but keep here now)
+// writes each literal to its own file in literals folder
 void strip_weights::apply(module& m) const
 {
-    std::cout << "Before packing: \n";
-
-    std::vector<test_literal> vec; // vector used to save literals
     
-    #ifdef TEST_READ_LITERALS
-        std::vector<literal> testing; // vector used to compare saved literals to original
+
+    #ifdef TEST_READ
+        std::vector<argument> testing; // vector used to compare saved literal arguments to original
     #endif
 
+
     size_t n = 0;
+    std::ofstream fs;
+    std::string output_file_header = output.substr(0, output.find(".")) + "_weights";
+
+    if(mkdir(output_file_header.c_str(), 0777) == -1){
+       std::cout << "Weights already created, delete weights folder and try again.\n\n";
+       return;
+    }
+
+
     for(auto ins : iterator_for(m))
     {
         if(ins->name() == "@literal")
         {
             ins->debug_print();
-            // save cur literal
-            vec.push_back(test_literal{n, ins->get_shape().bytes(), ins->get_literal().data()});
+            // save current literal to it's own file
+            std::vector<char> cur_literal = std::vector<char>(ins->get_literal().data(), ins->get_literal().data() + ins->get_shape().bytes());
+            auto buffer = make_shared_array<char>(cur_literal.begin(), cur_literal.end());
 
-            #ifdef TEST_READ_LITERALS
-                testing.push_back(ins->get_literal());
+            std::string output_file = output_file_header + "/literal_" + std::to_string(n);
+            std::cout << output_file << "\n";
+            fs.open(output_file, std::ofstream::out | std::ofstream::trunc | std::ios::binary);
+            fs.write(buffer.get(), ins->get_shape().bytes());
+            fs.close();
+            
+            #ifdef TEST_READ
+                testing.push_back(ins->get_literal().get_argument());
             #endif
-            // replace literal with small dummy instruction
-            m.replace_instruction(
-                ins, fetch_literal{n, ins->get_shape(), ins->get_literal().get_argument()});
+
+            m.replace_instruction(ins, fetch_literal{n, ins->get_shape(), output_file_header});
             n++;
         }
     }
 
-    // Pack and write to file
-    vector_stream vs;
-    msgpack::pack(vs, vec);
-
-    std::string output_file = output + "_weights";
-    // std::cout << "Output: " << output << std::endl;
-    // std::cout << "Output file: " << output_file << std::endl;
-
-    auto* os = &std::cout;
-    std::ofstream fs;
-    fs.open(output_file, std::ios::binary);
-    os = &fs;
-    (*os).write(vs.buffer.data(), vs.buffer.size());
-
-    // Read and unpack from file
-
-    #ifdef TEST_READ_LITERALS
-        vector_stream vs2;
-        std::ifstream is;
-        is.open(output_file, std::ios::binary | std::ios::ate);
-        if(not is.is_open())
-        {
-            std::cout << "Failed to open file" << std::endl;
-        }
-
-        size_t nbytes = is.tellg();
-        is.seekg(0, std::ios::beg);
-
-        std::vector<char> buffer(nbytes, 0);
-
-        if(not is.read(&buffer[0], nbytes))
-        {
-            std::cout << "Failed to read file" << std::endl;
-        }
-
-        msgpack::object_handle oh = msgpack::unpack(buffer.data(), buffer.size());
-        msgpack::object obj       = oh.get();
-
-        std::vector<test_literal> vec2;
-        obj.convert(vec2);
-
-        std::cout << "After unpacking: \n";
-
-        // insert literals back into engine and remove dummy instructions
+    #ifdef TEST_READ
         int i = 0;
-        std::vector<instruction_ref> to_remove;
         for(auto ins : iterator_for(m))
         {
             if(ins->name() == "fetch_literal")
             {
+                std::ifstream is;
+                is.open(output_file_header + "/literal_" + std::to_string(i), std::ios::binary);
+                if(not is.is_open())
+                {
+                    // MIGRAPHX_THROW("Could not open file: " + file_header + "/literal_" + std::to_string(id));
+                }
 
+                auto buffer = make_shared_array<char>(ins->get_shape().bytes());
 
-                literal l = literal(ins->get_shape(), vec2[i].get_data());
-                auto literal_ins = m.insert_literal(ins, l);
-                m.replace_instruction(ins, literal_ins);
-                to_remove.push_back(ins);
-                literal_ins->debug_print();
+                if(not is.read(buffer.get(), ins->get_shape().bytes()))
+                {
+                    // MIGRAPHX_THROW("Failed to read file: " + file_header + "/literal_" + std::to_string(id));
+                }
+                is.close();
+
+                argument test(ins->get_shape(), buffer);
+                if(test.to_string() == testing[i].to_string()){
+                    std::cout << "worked" + std::to_string(i) << "\n";
+                }
+                else {
+                    std::cout << "\n\n" << test.to_string() << "\n\n";
+                    std::cout << "\n\n" << testing[i].to_string() << "\n\n";
+                }
                 i++;
-            }
-        }
-
-        for(const auto& ins : to_remove){
-            m.remove_instruction(ins);
-        }
-
-        // test if new literals match (could probably combine with upper for loop)
-        int j = 0;
-        for(auto ins : iterator_for(m))
-        {
-            if(ins->name() == "@literal")
-            {
-                if(ins->get_literal().to_string() == testing[j].to_string() && ins->get_literal().get_shape() == testing[j].get_shape()){
-                    std::cout << "literal added matches original" << j << "\n";
-                }
-                else{
-                }
-                j++;
             }
         }
     #endif

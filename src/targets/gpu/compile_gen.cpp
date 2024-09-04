9e1e12478236b44334971c7c860ae45f5f95b20c
Paul Fultz II <pfultz2@yahoo.com>
2024-09-04 11:41:06 -0500

    Update src/targets/gpu/compile_gen.cpp
    Co-authored-by: Charlie Lin <charlie.lin@amd.com>

--- src/targets/gpu/compile_gen.cpp
+++ src/targets/gpu/compile_gen.cpp
@@ -246,6 +246,7 @@ tile tile::elements(const std::vector<shape>& inputs, std::size_t noutputs)
 
     auto tile_size  = dim1 * dim2;
     result.ntiles   = s.elements() / tile_size;
+    // equivalent to dim1 * (dim2 + 1) to avoid bank conflicts 
     auto tile_bytes = (tile_size + dim1) * s.type_size();
     if(tile_bytes > 65536)
         return {};

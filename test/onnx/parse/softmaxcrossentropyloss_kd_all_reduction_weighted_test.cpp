0e05a08a9a84d6113b28578f058c2a9645345536
Ted Themistokleous <ted.themistokleous@amd.com>
2024-09-04 13:24:18 +0000

    Fix k dimension parser tests

--- test/onnx/parse/softmaxcrossentropyloss_kd_all_reduction_weighted_test.cpp
+++ test/onnx/parse/softmaxcrossentropyloss_kd_all_reduction_weighted_test.cpp
@@ -27,42 +27,58 @@
 TEST_CASE(softmaxcrossentropyloss_kd_sum_reduction_weighted_double_test)
 {
     migraphx::program p;
-    auto* mm          = p.get_main_module();
+    auto* mm = p.get_main_module();
     size_t batch_size = 4;
     size_t class_size = 4;
 
-    auto scores = mm->add_parameter(
-        "0", migraphx::shape{migraphx::shape::double_type, {batch_size, class_size, 2, 2}});
-    auto labels =
-        mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {batch_size, 2, 2}});
-    auto weights =
-        mm->add_parameter("2", migraphx::shape{migraphx::shape::double_type, {batch_size}});
+    auto scores = mm->add_parameter("0", migraphx::shape{migraphx::shape::double_type, {batch_size, class_size, 2, 2}});
+    auto labels   = mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {class_size, 2, 2}});
+    auto weights  = mm->add_parameter("2", migraphx::shape{migraphx::shape::double_type, {class_size}});
 
-    auto weight_tensor =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 0}}), weights, labels);
+    auto weights_dflt = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::double_type, {1}, {0}), {1}));
+    auto labels_idx = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {class_size}, {1}), {0, 1, 2, 3}));
 
-    mm->add_literal(
-        migraphx::literal(migraphx::shape(migraphx::shape::int64_type, {1}, {0}), {-1}));
+    // Index variables used for gather on k dimensions that span their dimension
+    auto kd_1 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
+    auto kd_2 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
 
-    auto rs_scores = mm->add_instruction(
-        migraphx::make_op("reshape", {{"dims", {batch_size, class_size, 4}}}), scores);
 
-    auto softmax       = mm->add_instruction(migraphx::make_op("softmax"), rs_scores);
-    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), softmax);
+    mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", {class_size}}}), weights_dflt);
+    auto softmax         = mm->add_instruction(migraphx::make_op("softmax"), scores);
+    auto unsq_labels     = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {-1}}}), labels);
+
+    auto unsq_labels_idx = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1, 2, 3}}}), labels_idx);
+    auto bc_unsq_labels_idx = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx);
+
+    auto unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), kd_1);
+    auto bc_unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx2);
+
+    auto unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 1, 3}}}), kd_2);
+    auto bc_unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx3);
+
+    auto concat          = mm->add_instruction(migraphx::make_op("concat", {{"axis", -1}}), bc_unsq_labels_idx, bc_unsq_labels_idx2, bc_unsq_labels_idx3, unsq_labels);
+
+    auto transpose = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), softmax);
+
+    auto gathernd        = mm->add_instruction(migraphx::make_op("gathernd"), transpose, concat);
+    auto unsq_mb_weights = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), weights);
+    auto unsq_mb         = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", scores->get_shape().lens()}} ), unsq_mb_weights); 
+    auto transpose2      = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), unsq_mb);
+    auto gathernd2       = mm->add_instruction(migraphx::make_op("gathernd"), transpose2, concat);
+    
+    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), gathernd);
     auto neglogsoftmax = mm->add_instruction(migraphx::make_op("neg"), logsoftmax);
 
-    auto loss =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 1}}), neglogsoftmax, labels);
+    auto weighted_loss =
+        mm->add_instruction(migraphx::make_op("mul"), neglogsoftmax, gathernd2);
+    auto loss_out = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {0, 1, 2}}}), weighted_loss);
+    mm->add_return({loss_out});
 
-    mm->debug_print();
-    loss = mm->add_instruction(migraphx::make_op("reshape", {{"dims", labels->get_shape().lens()}}),
-                               loss);
-
-    loss = mm->add_instruction(migraphx::make_op("mul"), loss, weight_tensor);
-    loss = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {1}}}), loss);
-    mm->add_return({loss});
-
-    auto prog = migraphx::parse_onnx("softmaxcrossentropyloss_kd_sum_reduction_double_test.onnx");
+    auto prog = migraphx::parse_onnx("softmaxcrossentropyloss_kd_sum_reduction_double_weighted_test.onnx");
 
     EXPECT(p == prog);
 }
@@ -70,42 +86,58 @@ TEST_CASE(softmaxcrossentropyloss_kd_sum_reduction_weighted_double_test)
 TEST_CASE(softmaxcrossentropyloss_kd_no_reduction_weighted_test)
 {
     migraphx::program p;
-    auto* mm          = p.get_main_module();
+    auto* mm = p.get_main_module();
     size_t batch_size = 4;
     size_t class_size = 4;
 
-    auto scores = mm->add_parameter(
-        "0", migraphx::shape{migraphx::shape::float_type, {batch_size, class_size, 2, 2}});
-    auto labels =
-        mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {batch_size, 2, 2}});
-    auto weights =
-        mm->add_parameter("2", migraphx::shape{migraphx::shape::float_type, {batch_size}});
+    auto scores = mm->add_parameter("0", migraphx::shape{migraphx::shape::float_type, {batch_size, class_size, 2, 2}});
+    auto labels   = mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {class_size, 2, 2}});
+    auto weights  = mm->add_parameter("2", migraphx::shape{migraphx::shape::float_type, {class_size}});
 
-    auto weight_tensor =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 0}}), weights, labels);
+    auto weights_dflt = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::float_type, {1}, {0}), {1}));
+    auto labels_idx = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {class_size}, {1}), {0, 1, 2, 3}));
 
-    mm->add_literal(
-        migraphx::literal(migraphx::shape(migraphx::shape::int64_type, {1}, {0}), {-1}));
+    // Index variables used for gather on k dimensions that span their dimension
+    auto kd_1 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
+    auto kd_2 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
 
-    auto rs_scores = mm->add_instruction(
-        migraphx::make_op("reshape", {{"dims", {batch_size, class_size, 4}}}), scores);
 
-    auto softmax       = mm->add_instruction(migraphx::make_op("softmax"), rs_scores);
-    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), softmax);
+    mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", {class_size}}}), weights_dflt);
+    auto softmax         = mm->add_instruction(migraphx::make_op("softmax"), scores);
+    auto unsq_labels     = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {-1}}}), labels);
+
+    auto unsq_labels_idx = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1, 2, 3}}}), labels_idx);
+    auto bc_unsq_labels_idx = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx);
+
+    auto unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), kd_1);
+    auto bc_unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx2);
+
+    auto unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 1, 3}}}), kd_2);
+    auto bc_unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx3);
+
+    auto concat          = mm->add_instruction(migraphx::make_op("concat", {{"axis", -1}}), bc_unsq_labels_idx, bc_unsq_labels_idx2, bc_unsq_labels_idx3, unsq_labels);
+
+    auto transpose = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), softmax);
+
+    auto gathernd        = mm->add_instruction(migraphx::make_op("gathernd"), transpose, concat);
+    auto unsq_mb_weights = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), weights);
+    auto unsq_mb         = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", scores->get_shape().lens()}} ), unsq_mb_weights); 
+    auto transpose2      = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), unsq_mb);
+    auto gathernd2       = mm->add_instruction(migraphx::make_op("gathernd"), transpose2, concat);
+    
+    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), gathernd);
     auto neglogsoftmax = mm->add_instruction(migraphx::make_op("neg"), logsoftmax);
 
-    auto loss =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 1}}), neglogsoftmax, labels);
+    auto weighted_loss =
+        mm->add_instruction(migraphx::make_op("mul"), neglogsoftmax, gathernd2);
 
-    mm->debug_print();
-    loss = mm->add_instruction(migraphx::make_op("reshape", {{"dims", labels->get_shape().lens()}}),
-                               loss);
+    mm->add_return({weighted_loss}); 
 
-    loss = mm->add_instruction(migraphx::make_op("mul"), loss, weight_tensor);
-    loss = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {1}}}), loss);
-    mm->add_return({loss});
-
-    auto prog = migraphx::parse_onnx("softmaxcrossentropyloss_kd_no_reduction_weightd_test.onnx");
+    auto prog = migraphx::parse_onnx("softmaxcrossentropyloss_kd_no_reduction_weighted_test.onnx");
 
     EXPECT(p == prog);
 }
@@ -113,43 +145,61 @@ TEST_CASE(softmaxcrossentropyloss_kd_no_reduction_weighted_test)
 TEST_CASE(softmaxcrossentropyloss_kd_mean_reduction_half_weighted_test)
 {
     migraphx::program p;
-    auto* mm          = p.get_main_module();
+    auto* mm = p.get_main_module();
     size_t batch_size = 4;
     size_t class_size = 4;
 
-    auto scores = mm->add_parameter(
-        "0", migraphx::shape{migraphx::shape::half_type, {batch_size, class_size, 2, 2}});
-    auto labels =
-        mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {batch_size, 2, 2}});
-    auto weights =
-        mm->add_parameter("2", migraphx::shape{migraphx::shape::half_type, {batch_size}});
+    auto scores = mm->add_parameter("0", migraphx::shape{migraphx::shape::half_type, {batch_size, class_size, 2, 2}});
+    auto labels   = mm->add_parameter("1", migraphx::shape{migraphx::shape::int32_type, {class_size, 2, 2}});
+    auto weights  = mm->add_parameter("2", migraphx::shape{migraphx::shape::half_type, {class_size}});
 
-    auto weight_tensor =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 0}}), weights, labels);
+    auto weights_dflt = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::half_type, {1}, {0}), {1}));
+    auto labels_idx = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {class_size}, {1}), {0, 1, 2, 3}));
 
-    mm->add_literal(
-        migraphx::literal(migraphx::shape(migraphx::shape::int64_type, {1}, {0}), {-1}));
+    // Index variables used for gather on k dimensions that span their dimension
+    auto kd_1 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
+    auto kd_2 = mm->add_literal(
+        migraphx::literal(migraphx::shape(migraphx::shape::int32_type, {2}, {1}), {0, 1}));
 
-    auto rs_scores = mm->add_instruction(
-        migraphx::make_op("reshape", {{"dims", {batch_size, class_size, 4}}}), scores);
+    mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", {class_size}}}), weights_dflt);
+    auto softmax         = mm->add_instruction(migraphx::make_op("softmax"), scores);
+    auto unsq_labels     = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {-1}}}), labels);
 
-    auto softmax       = mm->add_instruction(migraphx::make_op("softmax"), rs_scores);
-    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), softmax);
+    auto unsq_labels_idx = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1, 2, 3}}}), labels_idx);
+    auto bc_unsq_labels_idx = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx);
+
+    auto unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), kd_1);
+    auto bc_unsq_labels_idx2 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx2);
+
+    auto unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 1, 3}}}), kd_2);
+    auto bc_unsq_labels_idx3 = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", unsq_labels->get_shape().lens()}}), unsq_labels_idx3);
+
+    auto concat          = mm->add_instruction(migraphx::make_op("concat", {{"axis", -1}}), bc_unsq_labels_idx, bc_unsq_labels_idx2, bc_unsq_labels_idx3, unsq_labels);
+
+    auto transpose = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), softmax);
+
+    auto gathernd        = mm->add_instruction(migraphx::make_op("gathernd"), transpose, concat);
+    auto unsq_mb_weights = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {0, 2, 3}}}), weights);
+    auto unsq_mb         = mm->add_instruction(migraphx::make_op("multibroadcast", {{"out_lens", scores->get_shape().lens()}} ), unsq_mb_weights); 
+    auto transpose2      = mm->add_instruction(migraphx::make_op("transpose", {{"permutation", {0, 2, 3, 1}}}), unsq_mb);
+    auto gathernd2       = mm->add_instruction(migraphx::make_op("gathernd"), transpose2, concat);
+    
+    auto logsoftmax    = mm->add_instruction(migraphx::make_op("log"), gathernd);
     auto neglogsoftmax = mm->add_instruction(migraphx::make_op("neg"), logsoftmax);
 
-    auto loss =
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 1}}), neglogsoftmax, labels);
+    auto weighted_loss =
+        mm->add_instruction(migraphx::make_op("mul"), neglogsoftmax, gathernd2);
 
-    mm->debug_print();
-    loss = mm->add_instruction(migraphx::make_op("reshape", {{"dims", labels->get_shape().lens()}}),
-                               loss);
+    auto loss_x = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {0,1 ,2}}}), weighted_loss);
+    auto loss_w = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {0,1 ,2}}}), gathernd2);
 
-    loss = mm->add_instruction(migraphx::make_op("mul"), loss, weight_tensor);
-    loss = mm->add_instruction(migraphx::make_op("reduce_sum", {{"axes", {1}}}), loss);
-    mm->add_return({loss});
-
-    auto prog =
-        migraphx::parse_onnx("softmaxcrossentropyloss_kd_mean_reduction_half_weightd_test.onnx");
-
+    auto loss_out = mm->add_instruction(migraphx::make_op("div"), loss_x, loss_w);
+    mm->add_return({loss_out}); 
+    
+    auto prog = migraphx::parse_onnx("softmaxcrossentropyloss_kd_mean_reduction_half_weighted_test.onnx");
     EXPECT(p == prog);
 }
+

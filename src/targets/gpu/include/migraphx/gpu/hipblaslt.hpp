d3615a70443972b4f94ff5b9d26f917c317c24bb
Ahsan Saghir <ahsaghir@amd.com>
2024-09-16 19:42:26 +0000

    Add MIGRaphX version for hipblasStatusToString function instead of using the one in hipblas

--- src/targets/gpu/include/migraphx/gpu/hipblaslt.hpp
+++ src/targets/gpu/include/migraphx/gpu/hipblaslt.hpp
@@ -29,6 +29,38 @@
 #include <migraphx/errors.hpp>
 #if MIGRAPHX_USE_HIPBLASLT
 #include <hipblaslt/hipblaslt.h>
+#include <hipblaslt/hipblaslt-ext.hpp>
+
+// TODO: Remove hipblasStatusToString() function when hipblaslt
+// provides an API for doing this in hipBLASLt.
+
+// Convert hipblas_status to string
+inline const char* hipblasStatusToString(hipblasStatus_t status)
+{
+#define CASE(x) \
+    case x:     \
+        return #x
+    switch(status)
+    {
+        CASE(HIPBLAS_STATUS_SUCCESS);
+        CASE(HIPBLAS_STATUS_NOT_INITIALIZED);
+        CASE(HIPBLAS_STATUS_ALLOC_FAILED);
+        CASE(HIPBLAS_STATUS_INVALID_VALUE);
+        CASE(HIPBLAS_STATUS_MAPPING_ERROR);
+        CASE(HIPBLAS_STATUS_EXECUTION_FAILED);
+        CASE(HIPBLAS_STATUS_INTERNAL_ERROR);
+        CASE(HIPBLAS_STATUS_NOT_SUPPORTED);
+        CASE(HIPBLAS_STATUS_ARCH_MISMATCH);
+        CASE(HIPBLAS_STATUS_HANDLE_IS_NULLPTR);
+        CASE(HIPBLAS_STATUS_INVALID_ENUM);
+        CASE(HIPBLAS_STATUS_UNKNOWN);
+    }
+#undef CASE
+    // We don't use default: so that the compiler warns us if any valid enums are missing
+    // from our switch. If the value is not a valid hipblas_status, we return this string.
+    return "<undefined hipblasStatus_t value>";
+}
+
 template <class F, class... Ts>
 inline auto hipblaslt_invoke(F f, Ts... xs)
 {

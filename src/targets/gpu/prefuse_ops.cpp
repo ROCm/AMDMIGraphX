421bdf802f8b7b5090b122815d49e3f6b4e4dd05
Paul <pfultz2@yahoo.com>
2024-09-20 13:05:11 -0500

    Remove layernorm fusion

--- src/targets/gpu/prefuse_ops.cpp
+++ src/targets/gpu/prefuse_ops.cpp
@@ -38,95 +38,7 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
-MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_DISABLE_LAYERNORM_FUSION);
-
 namespace {
-
-template <class Derived, std::size_t N>
-struct layernorm_base
-{
-    float epsilon = 1e-12f;
-    template <class Self, class F>
-    static auto reflect(Self& self, F f)
-    {
-        return pack(f(self.epsilon, "epsilon"));
-    }
-    shape compute_shape(std::vector<shape> inputs, std::vector<module_ref> mods) const
-    {
-        std::size_t nargs = N;
-        if(not mods.empty())
-        {
-            auto* pm = mods.front();
-            nargs += pm->get_parameter_names().size() - 1;
-        }
-        check_shapes{inputs, static_cast<const Derived&>(*this)}.has(nargs);
-        auto s = inputs.front();
-        auto t = s.type();
-        if(not mods.empty())
-            t = mods.front()->get_output_shapes().front().type();
-
-        // Scalar output if all inputs are scalar
-        if(inputs.front().elements() == 1 and
-           all_of(inputs, [](const auto& ss) { return ss.scalar(); }))
-            return inputs.front();
-        auto l_s = shape::from_permutation(
-            t, s.lens(), find_permutation(std::vector<shape>(inputs.begin(), inputs.begin() + N)));
-        // just prelayernorm or preadd_layernorm
-        if(nargs <= N)
-            return l_s;
-        // else, layernorm + pointwise fusion, preserve layout of fused op
-        std::vector<shape> lp_s(inputs.begin() + N, inputs.end());
-        lp_s.insert(lp_s.begin(), l_s);
-        return shape::from_permutation(t, s.lens(), find_permutation(lp_s));
-    }
-};
-
-struct layernorm : layernorm_base<layernorm, 1>
-{
-
-    std::string name() const { return "gpu::prelayernorm"; }
-};
-MIGRAPHX_REGISTER_OP(layernorm);
-
-struct add_layernorm : layernorm_base<add_layernorm, 2>
-{
-    std::string name() const { return "gpu::preadd_layernorm"; }
-};
-MIGRAPHX_REGISTER_OP(add_layernorm);
-
-struct find_layernorm
-{
-    auto matcher() const { return match::layernorm(); }
-
-    void apply(module& m, const match::matcher_result& r) const
-    {
-        auto ins   = r.result;
-        auto x_ins = r.instructions["x"];
-        float eps  = 0;
-        if(contains(r.instructions, "eps"))
-            eps = r.instructions["eps"]->eval().at<float>();
-
-        m.replace_instruction(ins, layernorm{eps}, x_ins);
-    }
-};
-
-struct find_add_layernorm
-{
-    auto matcher() const
-    {
-        return match::name("gpu::prelayernorm")(
-            match::args(match::name("add")(match::used_once()).bind("add")));
-    }
-
-    void apply(module& m, const match::matcher_result& r) const
-    {
-        auto ins     = r.result;
-        auto add_ins = r.instructions["add"];
-        auto op      = any_cast<layernorm>(ins->get_operator());
-
-        m.replace_instruction(ins, add_layernorm{op.epsilon}, add_ins->inputs());
-    }
-};
 
 struct pre_gemm_softmax_gemm : gemm_softmax_gemm
 {
@@ -239,12 +151,6 @@ struct find_gemm_softmax_gemm
 
 void prefuse_ops::apply(module_pass_manager& mpm) const
 {
-    if(not enabled(MIGRAPHX_DISABLE_LAYERNORM_FUSION{}))
-    {
-        match::find_matches(mpm.get_module(), find_layernorm{});
-        mpm.run_pass(dead_code_elimination{});
-        match::find_matches(mpm.get_module(), find_add_layernorm{});
-    }
     match::find_matches(mpm, find_gemm_softmax_gemm{enable_attention});
 }
 

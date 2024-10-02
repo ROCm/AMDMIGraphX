4e1fce733f1bbf54902b893e896c923971b7e56e
Paul <pfultz2@yahoo.com>
2024-10-02 09:36:19 -0500

    Rewrite all convolutions

--- src/rewrite_dot.cpp
+++ src/rewrite_dot.cpp
@@ -28,10 +28,60 @@
 #include <migraphx/make_op.hpp>
 #include <migraphx/permutation.hpp>
 
+#include <migraphx/check_shapes.hpp>
+#include <migraphx/register_op.hpp>
+
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_ENABLE_REWRITE_DOT);
+
+struct window
+{
+    std::vector<int64_t> axes    = {};
+    std::vector<int64_t> stride  = {};
+    std::vector<int64_t> lengths = {};
+
+    template <class Self, class F>
+    static auto reflect(Self& self, F f)
+    {
+        return pack(f(self.axes, "axes"), f(self.stride, "stride"), f(self.lengths, "lengths"));
+    }
+
+    std::string name() const { return "window"; }
+
+    shape compute_shape(const std::vector<shape>& inputs) const
+    {
+        check_shapes{inputs, *this}.has(1);
+        const auto& input = inputs[0];
+        auto lens         = input.lens();
+        auto strides      = input.strides();
+        for(auto i : range(axes.size()))
+        {
+            auto axis       = axes[i];
+            auto win_stride = stride[i];
+            auto win_len    = lengths[i];
+            auto& dim       = lens[axis];
+            auto& s         = strides[axis];
+            dim             = ((dim - win_len) / win_stride) + 1;
+            s *= win_stride;
+        }
+        std::copy(lengths.begin(), lengths.end(), std::back_inserter(lens));
+        std::transform(axes.begin(), axes.end(), std::back_inserter(strides), [&](auto axis) {
+            return input.strides()[axis];
+        });
+        shape result{input.type(), lens, strides};
+        if(result.element_space() > input.element_space())
+            MIGRAPHX_THROW("Out of bounds window access");
+        return result;
+    }
+
+    argument compute(const shape& output_shape, const std::vector<argument>& args) const
+    {
+        return args[0].reshape(output_shape);
+    }
+};
+MIGRAPHX_REGISTER_OP(window);
 
 namespace {
 
@@ -48,6 +98,14 @@ MIGRAPHX_PRED_MATCHER(conv_1x1, instruction_ref ins)
         return false;
     auto w = ins->inputs().at(1)->get_shape();
     return std::all_of(w.lens().begin() + 2, w.lens().end(), [](std::size_t i) { return i == 1; });
+}
+
+std::vector<int64_t> nhwc_perm(std::size_t ndim)
+{
+    std::vector<int64_t> result(ndim);
+    std::iota(result.begin(), result.end(), 0);
+    std::rotate(result.begin() + 1, result.begin() + 2, result.end());
+    return result;
 }
 
 struct find_1x1_convolution
@@ -67,9 +125,7 @@ struct find_1x1_convolution
         auto n_dim   = weights->get_shape().lens()[0];
         auto k_dim   = weights->get_shape().lens()[1];
 
-        std::vector<int64_t> aperm(ins->get_shape().ndim());
-        std::iota(aperm.begin(), aperm.end(), 0);
-        std::rotate(aperm.begin() + 1, aperm.begin() + 2, aperm.end());
+        std::vector<int64_t> aperm = nhwc_perm(ins->get_shape().ndim());
         auto transpose =
             m.insert_instruction(ins, make_op("transpose", {{"permutation", aperm}}), input);
         auto a_mat =
@@ -89,13 +145,81 @@ struct find_1x1_convolution
     }
 };
 
+struct find_all_convolution
+{
+    auto matcher() const { return match::name("convolution"); }
+
+    void apply(module& m, const match::matcher_result& r) const
+    {
+        auto ins = r.result;
+        auto kdims = ins->get_shape().ndim() - 2;
+        auto input   = ins->inputs().front();
+        auto weights = ins->inputs().back();
+
+        auto v = ins->get_operator().to_value();
+        auto vpadding = v.at("padding").to_vector<std::size_t>();
+        auto vstride = v.at("stride").to_vector<std::size_t>();
+
+        std::vector<std::size_t> padding(2);
+        padding.insert(padding.end(), vpadding.begin(), vpadding.begin() + kdims);
+        padding.push_back(0);
+        padding.push_back(0);
+        if(vpadding.size() > kdims)
+            padding.insert(padding.end(), vpadding.begin() + kdims, vpadding.end());
+        else
+            padding.insert(padding.end(), vpadding.begin(), vpadding.begin() + kdims);
+
+        auto pad = m.insert_instruction(ins, make_op("pad", {{"pads", padding}}), input);
+
+        window w;
+        w.axes.resize(kdims);
+        std::iota(w.axes.begin(), w.axes.end(), 2);
+        w.lengths.insert(w.lengths.end(), weights->get_shape().lens().begin() + 2, weights->get_shape().lens().end());
+        w.stride.insert(w.stride.end(), vstride.begin(), vstride.end());
+
+        auto iwindow = m.insert_instruction(ins, w, pad);
+
+        auto out_channels   = weights->get_shape().lens()[0];
+        auto in_channels   = weights->get_shape().lens()[1];
+
+        auto m_dim   = std::accumulate(iwindow->get_shape().lens().begin() + 2,
+                                     iwindow->get_shape().lens().begin() + 2 + kdims,
+                                     iwindow->get_shape().lens().front(),
+                                     std::multiplies<>{});
+        auto k_dim   = std::accumulate(weights->get_shape().lens().begin() + 2, weights->get_shape().lens().end(), in_channels);
+        auto n_dim   = out_channels;
+
+        std::vector<int64_t> aperm(iwindow->get_shape().ndim());
+        std::iota(aperm.begin(), aperm.end(), 0);
+        std::rotate(aperm.begin() + 1, aperm.begin() + 2, aperm.begin() + 2 + kdims);
+        auto transpose =
+            m.insert_instruction(ins, make_op("transpose", {{"permutation", aperm}}), iwindow);
+        auto a_mat =
+            m.insert_instruction(ins, make_op("reshape", {{"dims", {m_dim, k_dim}}}), transpose);
+
+        auto reshape =
+            m.insert_instruction(ins, make_op("reshape", {{"dims", {n_dim, k_dim}}}), weights);
+        auto b_mat =
+            m.insert_instruction(ins, make_op("transpose", {{"permutation", {1, 0}}}), reshape);
+
+        auto dot        = m.insert_instruction(ins, make_op("dot"), a_mat, b_mat);
+        auto bperm = nhwc_perm(ins->get_shape().ndim());
+        auto out_dims   = reorder_dims(ins->get_shape().lens(), bperm);
+
+        auto creshape   = m.insert_instruction(ins, make_op("reshape", {{"dims", out_dims}}), dot);
+        m.replace_instruction(
+            ins, make_op("transpose", {{"permutation", invert_permutation(bperm)}}), creshape);
+    }
+};
+
 } // namespace
 
 void rewrite_dot::apply(module& m) const 
 {
     if(not enabled(MIGRAPHX_ENABLE_REWRITE_DOT{}))
         return;
-    match::find_matches(m, find_1x1_convolution{}); 
+    match::find_matches(m, find_1x1_convolution{});
+    match::find_matches(m, find_all_convolution{});
 }
 
 } // namespace MIGRAPHX_INLINE_NS

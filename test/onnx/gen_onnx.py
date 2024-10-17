c0c8120f47dc739573d7d01d9339aaeb6943e661
Ted Themistokleous <ted.themistokleous@amd.com>
2024-10-17 23:13:09 +0000

    Add parser for bias with zero points

--- test/onnx/gen_onnx.py
+++ test/onnx/gen_onnx.py
@@ -7551,9 +7551,9 @@ def matmulintegertofloat_zp_bias_test():
     m2 = helper.make_tensor_value_info('2', TensorProto.INT8, [3, 2])
     s1 = helper.make_tensor_value_info('3', TensorProto.FLOAT, [4])
     s2 = helper.make_tensor_value_info('4', TensorProto.FLOAT, [2])
-    zp1 = helper.make_tensor('5', TensorProto.INT8, [], [0])
-    zp2 = helper.make_tensor('6', TensorProto.UINT8, [], [128])
-    b1 = helper.make_tensor('7', TensorProto.UINT8, [2], [128, -128])
+    zp1 = helper.make_tensor_value_info('5', TensorProto.INT8, [4])
+    zp2 = helper.make_tensor_value_info('6', TensorProto.INT8, [2])
+    b1 = helper.make_tensor_value_info('7', TensorProto.FLOAT, [2])
     y = helper.make_tensor_value_info('y', TensorProto.INT32, [4, 2])
 
     node = onnx.helper.make_node(

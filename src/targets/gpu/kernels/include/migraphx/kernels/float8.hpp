d458472d4bee2211ba45e848fd384ba62a0e5ff4
charlie <charlie.lin@amd.com>
2024-10-22 13:32:18 -0500

    fix

--- src/targets/gpu/kernels/include/migraphx/kernels/float8.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/float8.hpp
@@ -150,32 +150,35 @@ struct float8
                     cast_to_f8<3, 4, float, FNUZ /*negative_zero_nan*/, false /*clip*/>(
                         v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
 #endif // MIGRAPHX_F8_DOWNCAST_CLIPPING
-            }
+        }
+        else
+        {
             if(__builtin_is_constant_evaluated())
             {
                 if constexpr(T == migraphx::fp8::f8_type::fp8)
                 {
 #ifdef MIGRAPHX_F8_DOWNCAST_CLIPPING
-                data = migraphx::fp8::impl::
-                    cast_to_f8<3, 4, float, FNUZ /*negative_zero_nan*/, true /*clip*/>(
-                        v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
+                    data = migraphx::fp8::impl::
+                        cast_to_f8<3, 4, float, FNUZ /*negative_zero_nan*/, true /*clip*/>(
+                            v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
 #else  // MIGRAPHX_F8_DOWNCAST_CLIPPING
-                data = migraphx::fp8::impl::
-                    cast_to_f8<3, 4, float, FNUZ /*negative_zero_nan*/, false /*clip*/>(
-                        v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
+                    data = migraphx::fp8::impl::
+                        cast_to_f8<3, 4, float, FNUZ /*negative_zero_nan*/, false /*clip*/>(
+                            v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
 #endif // MIGRAPHX_F8_DOWNCAST_CLIPPING
                 }
-            else
-            {
+                else
+                {
 #ifdef MIGRAPHX_F8_DOWNCAST_CLIPPING
-                data = migraphx::fp8::impl::
-                    cast_to_f8<2, 5, float, FNUZ /*negative_zero_nan*/, true /*clip*/>(
-                        v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
+                    data = migraphx::fp8::impl::
+                        cast_to_f8<2, 5, float, FNUZ /*negative_zero_nan*/, true /*clip*/>(
+                            v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
 #else  // MIGRAPHX_F8_DOWNCAST_CLIPPING
-                data = migraphx::fp8::impl::
-                    cast_to_f8<2, 5, float, FNUZ /*negative_zero_nan*/, false /*clip*/>(
-                        v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
+                    data = migraphx::fp8::impl::
+                        cast_to_f8<2, 5, float, FNUZ /*negative_zero_nan*/, false /*clip*/>(
+                            v, (rm == migraphx::fp8::rounding_mode::stochastic), rng);
 #endif // MIGRAPHX_FP8_DOWNCAST_CLIPPING}
+                }
             }
         }
         else
@@ -186,7 +189,7 @@ struct float8
             else
                 data = cast_to_f8fnuz_from_f32<false>(v);
         }
-        }
+    }
 #else
     // DEVICE for non-gfx940 using s/w simulation
     explicit constexpr __device__

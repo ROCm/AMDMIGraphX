6735534e350d0f9d8460ff7cc9c11f34f31f9480
Oliver Toth <Oliver.Toth@amd.com>
2024-10-28 07:39:06 -0500

    Use dataset from numpy files if available

--- examples/transformers/mgx_llama2/mgxllama2.cc
+++ examples/transformers/mgx_llama2/mgxllama2.cc
@@ -11,8 +11,9 @@
 using namespace mlinfer;
 
 // TODO: fix paths
-const std::string MODEL_FOLDER = "/code/AMDMIGraphX/examples/transformers/python_llama2/models/llama-2-7b-chat-hf/";
+const std::string MODEL_FOLDER = "/model/";
 const std::string ONNX_FILE = "model.onnx";
+const std::string DATASET_FOLDER = "/dataset/";
 std::vector<int64_t> SAMPLE_IDS = {1,6804,5207,387,287,29973};
 // sequence length from model config
 const size_t SEQ_SIZE = 256;
@@ -120,6 +121,108 @@ static migraphx::program loadProgram(ModelLoadSettings& settings)
     return prog;
 };
 
+using NumpyVector = std::vector<std::vector<int64_t>>;
+
+struct Dataset
+{
+    Dataset() = default;
+
+    void initialize()
+    {
+        std::string input_file_path = DATASET_FOLDER + "input_ids_size_1_seq_256.npy";
+        std::string attention_mask_file_path = DATASET_FOLDER + "attention_mask_size_1_seq_256.npy";
+        std::string position_ids_file_path = DATASET_FOLDER + "position_ids_size_1_seq_256.npy";
+        std::ifstream input_file(input_file_path.c_str());
+        std::ifstream attention_mask_file(attention_mask_file_path.c_str());
+        std::ifstream position_ids_file(position_ids_file_path.c_str());
+        if (input_file.good() && attention_mask_file.good() && position_ids_file.good())
+        {
+            npy::NpyFile input_ids_npy{input_file_path};
+            npy::NpyFile attention_mask_npy{attention_mask_file_path};
+            npy::NpyFile position_ids_npy{position_ids_file_path};
+            input_ids = loadNumpy(input_ids_npy);
+            attention_mask = loadNumpy(attention_mask_npy);
+            position_ids = loadNumpy(position_ids_npy);
+
+            if (input_ids.size() == attention_mask.size() == position_ids.size())
+            {
+                std::cout << "Loaded numpy files\n";
+                npy_files_loaded = true;
+            }
+            else
+            {
+                std::cout << "Numpy files do not have the same size\n";
+                input_ids.clear();
+                attention_mask.clear();
+                position_ids.clear();
+            }
+        }
+
+        if (!npy_files_loaded)
+        {
+            std::cout << "Numpy files are not loaded, using dummy data\n";
+            auto input_ids_sample = SAMPLE_IDS;
+            input_ids_sample.resize(SEQ_SIZE, EOS);
+            input_ids.emplace_back(input_ids_sample);
+            std::vector<int64_t> attention_mask_sample = input_ids_sample;
+            std::transform(std::begin(attention_mask_sample), std::end(attention_mask_sample), std::begin(attention_mask_sample), [](auto i){
+                return (i != EOS) ? 1 : 0;
+            });
+            attention_mask.emplace_back(attention_mask_sample);
+
+            std::vector<int64_t> position_ids_sample;
+            for (int64_t i=0; i < SEQ_SIZE; ++i)
+            {
+                position_ids_sample.emplace_back(i);
+            }
+            position_ids.emplace_back(std::move(position_ids_sample));
+        }
+
+    }
+
+    NumpyVector loadNumpy(npy::NpyFile& file)
+    {
+        NumpyVector numpyData;
+        auto load_size = file.GetTensorSize()/sizeof(int64_t);
+        numpyData.push_back(std::vector<int64_t>(load_size));
+        file.LoadAll(numpyData.back().data());
+
+    #ifdef TRACE
+        for (auto& vec: numpyData)
+        {
+            for (auto val: vec)
+            {
+                std::cout << val << " ";
+            }
+            std::cout << "\n";
+        }
+    #endif
+        return numpyData;
+    }
+
+    size_t getLastIdx() const
+    {
+        auto res = std::find_if(std::rbegin(attention_mask[current_idx]), std::rend(attention_mask[current_idx]), [](uint64_t val) { return 1 == val;});
+        size_t last_idx = std::distance(res, std::rend(attention_mask[current_idx]));
+        //std::cout << "Last input idx: " << last_idx << std::endl;
+        return last_idx;
+    }
+
+    std::vector<int64_t> getInputIds() { return input_ids[current_idx]; }
+    std::vector<int64_t> getAttentionMask() { return attention_mask[current_idx]; }
+    std::vector<int64_t> getPositionIds() { return position_ids[current_idx]; }
+
+    Dataset(const Dataset &buf) = delete;
+    Dataset &operator=(const Dataset &buf) = delete;
+
+    NumpyVector input_ids;
+    NumpyVector attention_mask;
+    NumpyVector position_ids;
+
+    size_t current_idx = 0;
+    bool npy_files_loaded = false;
+};
+
 struct LLama2Inputs
 {
     LLama2Inputs(
@@ -128,28 +231,20 @@ struct LLama2Inputs
         bool offload_copy)
         : offload_copy(offload_copy)
     {
-        auto input_ids = SAMPLE_IDS;
-        input_ids.resize(SEQ_SIZE, 0);
-        std::vector<int64_t> attention_mask = input_ids;
-        std::transform(std::begin(attention_mask), std::end(attention_mask), std::begin(attention_mask), [](auto i){
-            return (i != 0) ? 1 : 0;
-        });
+        data.initialize();
 
-        std::vector<int64_t> position_ids;
-        for (int64_t i=0; i < SEQ_SIZE; ++i)
-        {
-            position_ids.emplace_back(i);
-        }
-
+        auto input_ids = data.getInputIds();
         auto param_shapes = prog.get_parameter_shapes();
         auto input_ids_str = "input_ids";
         input_ids_buffer = std::make_unique<LLama2InputBuffer>(std::move(input_ids), offload_copy);
         prog_args.add(input_ids_str, migraphx::argument(param_shapes[input_ids_str], input_ids_buffer->data()));
 
+        auto attention_mask = data.getAttentionMask();
         auto attention_mask_str = "attention_mask";
         attention_mask_buffer = std::make_unique<LLama2InputBuffer>(std::move(attention_mask), offload_copy);
         prog_args.add(attention_mask_str, migraphx::argument(param_shapes[attention_mask_str], attention_mask_buffer->data()));
 
+        auto position_ids = data.getPositionIds();
         auto position_ids_str = "position_ids";
         position_ids_buffer = std::make_unique<LLama2InputBuffer>(std::move(position_ids), offload_copy);
         prog_args.add(position_ids_str, migraphx::argument(param_shapes[position_ids_str], position_ids_buffer->data()));
@@ -163,6 +258,8 @@ struct LLama2Inputs
         position_ids_buffer->upload_to_device(stream);
     }
 
+    size_t getLastInputIndex() { return data.getLastIdx(); }
+
     LLama2Inputs() = delete;
     LLama2Inputs(const LLama2Inputs &buf) = delete;
     LLama2Inputs &operator=(const LLama2Inputs &buf) = delete;
@@ -170,18 +267,19 @@ struct LLama2Inputs
     std::unique_ptr<LLama2InputBuffer> input_ids_buffer;
     std::unique_ptr<LLama2InputBuffer> attention_mask_buffer;
     std::unique_ptr<LLama2InputBuffer> position_ids_buffer;
+    Dataset data;
     bool offload_copy;
 };
 
 int main() {
-    bool offload_copy = false;
+    bool offload_copy = true;
     std::cout << "Offload copy: " << std::boolalpha << offload_copy << std::endl;
-    ModelLoadSettings settings = {SEQ_SIZE, true /*quantize_fp16*/, false /*offload_copy*/, true /*fast_math*/};
+    ModelLoadSettings settings = {SEQ_SIZE, true /*quantize_fp16*/, offload_copy /*offload_copy*/, true /*fast_math*/};
     migraphx::program prog = loadProgram(settings);
     std::cout << "Model loaded" << std::endl;
 
     // Setup model inputs
-    auto output_tokens = SAMPLE_IDS;
+    std::vector<uint64_t> output_tokens;
     migraphx::program_parameters prog_args;
     hipStream_t stream;
     check_hip_status(hipStreamCreateWithFlags(&stream, hipStreamNonBlocking));
@@ -201,7 +299,7 @@ int main() {
     std::cout << "Starting evaluation" << std::endl;
     size_t token_count = 0;
     auto start = std::chrono::steady_clock::now();
-    for (int i = SAMPLE_IDS.size() - 1; i < SEQ_SIZE - 1; ++i)
+    for (int i = model_inputs.getLastInputIndex(); i < SEQ_SIZE - 1; ++i)
     {
         auto outputs = prog.run_async(prog_args, stream);
         if (not offload_copy)
--- examples/transformers/mgx_llama2/run_docker.sh
+++ examples/transformers/mgx_llama2/run_docker.sh
@@ -7,8 +7,16 @@ else
   MODEL_DIR="${MODEL_DIR_PATH}"
 fi
 
+if [[ -z "${DATA_DIR_PATH}" ]]; then
+  echo "DATA_DIR_PATH is not set, please provide the path to dataset before running docker."
+  exit 1
+else
+  DATA_DIR="${DATA_DIR_PATH}"
+fi
+
 docker run --device='/dev/kfd' --device='/dev/dri' --group-add video \
 -v $MODEL_DIR:/model \
+-v $DATA_DIR:/dataset \
 -w /mgx_llama2/build \
 -it mgx_llama2:v0.1
 

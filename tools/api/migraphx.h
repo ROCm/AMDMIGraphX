ce05928ba19f4b388e96211556154aef928edf8e
charlie <charlie.lin@amd.com>
2024-10-29 15:50:16 -0500

    Change order on API

--- tools/api/migraphx.h
+++ tools/api/migraphx.h
@@ -46,9 +46,9 @@
     m(uint32_type, uint32_t) \
     m(uint64_type, uint64_t) \
     m(fp8e4m3fnuz_type, migraphx::fp8::fp8e4m3fnuz) \
-    m(fp8e5m2fnuz_type, migraphx::fp8::fp8e5m2fnuz) \
     m(fp8e4m3fn_type, migraphx::fp8::fp8e4m3fn) \
-    m(fp8e5m2_type, migraphx::fp8::fp8e5m2)
+    m(fp8e5m2_type, migraphx::fp8::fp8e5m2) \
+    m(fp8e5m2fnuz_type, migraphx::fp8::fp8e5m2fnuz)
 // clang-format on
 
 #ifdef __cplusplus

2e3bd2537828bbca7f872f1c5d948315d018a009
richagadgil <richa.gadgil@amd.com>
2024-11-05 21:44:12 +0000

    tidy

--- test/half.cpp
+++ test/half.cpp
@@ -51,7 +51,7 @@ TEST_CASE(check_numeric_limits)
     CHECK(bit_equal(std::numeric_limits<migraphx::half>::signaling_NaN(), uint16_t{0x7d00}));
 }
 
-const std::map<uint16_t, float>& half_lut()
+const std::map<uint16_t, float>& half_lut() // NOLINT(readability-function-size)
 {
     static const std::map<uint16_t, float> result = {
         {0x0000, 0},

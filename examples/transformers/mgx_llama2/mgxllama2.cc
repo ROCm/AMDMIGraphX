9220cf13c24be103610ee18c860b1c684010c5e8
Oliver Toth <Oliver.Toth@amd.com>
2024-11-13 09:41:56 -0600

    Support llama 7b quantized model without offload copy

--- examples/transformers/mgx_llama2/mgxllama2.cc
+++ examples/transformers/mgx_llama2/mgxllama2.cc
@@ -457,7 +457,7 @@ void writeResults(const std::vector<std::vector<uint64_t>>& results)
 }
 
 int main() {
-    bool offload_copy = true;
+    bool offload_copy = false;
     std::cout << "Offload copy: " << std::boolalpha << offload_copy << std::endl;
     ModelLoadSettings settings = {SEQ_SIZE, false /*quantize_fp16*/, offload_copy /*offload_copy*/, false /*fast_math*/, false /*input_one_dim*/};
     migraphx::program prog = loadProgram(settings);
@@ -488,26 +488,32 @@ int main() {
     // prog_args.add(output_name, migraphx::argument(out_shape, output_buffer.data()));
 
     size_t output_size = SEQ_SIZE * VOCAB_SIZE;
-    auto output_name = "logits";
+    auto output_name = "main:#output_0";
     auto output_buffer = LLama2PastKeyValueBuffer(std::vector<half>(output_size), offload_copy);
-    migraphx::shape out_shape{migraphx_shape_float_type, {1, SEQ_SIZE, VOCAB_SIZE}};
+    auto output_buffer_oneDim = LLama2PastKeyValueBuffer(std::vector<half>(VOCAB_SIZE), offload_copy);
+    migraphx::shape out_shape{migraphx_shape_half_type, {1, SEQ_SIZE, VOCAB_SIZE}};
     prog_args.add(output_name, migraphx::argument(out_shape, output_buffer.data()));
 
-    // std::vector<std::unique_ptr<LLama2PastKeyValueBuffer>> present_key_buffers;
-    // std::vector<std::unique_ptr<LLama2PastKeyValueBuffer>> present_value_buffers;
-    // for (size_t i = 0; i < HIDDEN_LAYERS_NUM; ++i)
-    // {
-    //     migraphx::shape present_shape{migraphx_shape_half_type, {1, HIDDEN_LAYERS_NUM, SEQ_SIZE, HEAD_SIZE}};
-    //     auto present_keyStr = getPresentKeyString(i);
-    //     auto present_keyString = present_keyStr.c_str();
-    //     present_key_buffers.emplace_back(std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(PAST_KEY_VAL_SIZE, 0.0_h), offload_copy));
-    //     prog_args.add(present_keyString, migraphx::argument(present_shape, present_key_buffers[i]->data()));
+    std::vector<std::unique_ptr<LLama2PastKeyValueBuffer>> present_key_buffers;
+    std::vector<std::unique_ptr<LLama2PastKeyValueBuffer>> present_value_buffers;
+    for (size_t i = 0; i < HIDDEN_LAYERS_NUM; ++i)
+    {
+        migraphx::shape present_shape{migraphx_shape_half_type, {1, HIDDEN_LAYERS_NUM, SEQ_SIZE, HEAD_SIZE}};
+        auto present_keyStr = getPresentKeyString(i);
+        auto present_keyString = present_keyStr.c_str();
+        present_key_buffers.emplace_back(std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(PAST_KEY_VAL_SIZE, 0.0_h), offload_copy));
+        prog_args.add(present_keyString, migraphx::argument(present_shape, present_key_buffers[i]->data()));
 
-    //     auto present_valueStr = getPresentValueStr(i);
-    //     auto present_valueString = present_valueStr.c_str();
-    //     present_value_buffers.emplace_back(std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(PAST_KEY_VAL_SIZE, 0.0_h), offload_copy));
-    //     prog_args.add(present_valueString, migraphx::argument(present_shape, present_value_buffers[i]->data()));
-    // }
+        auto present_valueStr = getPresentValueStr(i);
+        auto present_valueString = present_valueStr.c_str();
+        present_value_buffers.emplace_back(std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(PAST_KEY_VAL_SIZE, 0.0_h), offload_copy));
+        prog_args.add(present_valueString, migraphx::argument(present_shape, present_value_buffers[i]->data()));
+        if (offload_copy)
+        {
+            model_inputs.past_key_buffers[i]->upload_to_device(stream);
+            model_inputs.past_value_buffers[i]->upload_to_device(stream);
+        }
+    }
 
     std::cout << "Starting evaluation" << std::endl;
     size_t token_count = 0;
@@ -521,17 +527,30 @@ int main() {
         auto lastInputIdx = model_inputs.getLastInputIndex();
         for (size_t i = lastInputIdx; i < SEQ_SIZE - 1; ++i)
         {
+            bool firstIter = (i == lastInputIdx);
             auto outputs = prog.run_async(prog_args, stream);
             if (not offload_copy)
             {
-                output_buffer.download_from_device(stream, i * VOCAB_SIZE, (i + 1) * VOCAB_SIZE);
+                if (firstIter)
+                {
+                    output_buffer.download_from_device(stream, i * VOCAB_SIZE, (i + 1) * VOCAB_SIZE);
+                }
+                else
+                {
+                    output_buffer_oneDim.download_from_device(stream);
+                }
+
+                for (size_t i = 0; i < HIDDEN_LAYERS_NUM; ++i)
+                {
+                    present_key_buffers[i]->download_from_device(stream);
+                    present_value_buffers[i]->download_from_device(stream);
+                }
             }
 
             check_hip_status(hipStreamSynchronize(stream));
-            half* results   = offload_copy ? reinterpret_cast<half*>(outputs[0].data()) : reinterpret_cast<half*>(output_buffer.hbuff.data());
+            half* results = offload_copy ? reinterpret_cast<half*>(outputs[0].data()) : reinterpret_cast<half*>( firstIter ? output_buffer.hbuff.data() : output_buffer_oneDim.hbuff.data());
             std::vector<half> logits(results, results + output_size);
 
-            bool firstIter = (i == lastInputIdx);
             auto logits_begin = firstIter ? std::begin(logits) + (i * VOCAB_SIZE) : std::begin(logits);
             auto logits_end = firstIter ? std::begin(logits) + ((i + 1) * VOCAB_SIZE) : std::end(logits);
             std::vector<half>::iterator max = std::max_element(logits_begin, logits_end);
@@ -546,19 +565,25 @@ int main() {
             for (size_t i = 0; i < HIDDEN_LAYERS_NUM; ++i)
             {
                 migraphx::shape past_shape{migraphx_shape_half_type, {1, HIDDEN_LAYERS_NUM, SEQ_SIZE, HEAD_SIZE}};
-                half* res   = reinterpret_cast<half*>(outputs[2*i+1].data());
+                half* res   = offload_copy ? reinterpret_cast<half*>(outputs[2*i+1].data()) : reinterpret_cast<half*>(present_key_buffers[i]->hbuff.data());
                 std::vector<half> present_key(res, res + PAST_KEY_VAL_SIZE);
 
                 auto past_keyStr = getPastKeyString(i);
                 model_inputs.past_key_buffers[i]->update(std::move(present_key));
                 prog_args.add(past_keyStr.c_str(), migraphx::argument(past_shape, model_inputs.past_key_buffers[i]->data()));
 
-                res = reinterpret_cast<half*>(outputs[2*i+2].data());
+                res = offload_copy ? reinterpret_cast<half*>(outputs[2*i+2].data()) : reinterpret_cast<half*>(present_value_buffers[i]->hbuff.data());
                 std::vector<half> present_value(res, res + PAST_KEY_VAL_SIZE);
 
                 auto past_valueStr = getPastValueStr(i);
                 model_inputs.past_value_buffers[i]->update(std::move(present_value));
                 prog_args.add(past_valueStr.c_str(), migraphx::argument(past_shape, model_inputs.past_value_buffers[i]->data()));
+
+                if (offload_copy)
+                {
+                    model_inputs.past_key_buffers[i]->upload_to_device(stream);
+                    model_inputs.past_value_buffers[i]->upload_to_device(stream);
+                }
             }
 
             if (new_token == EOS)
@@ -572,12 +597,15 @@ int main() {
             {
                 prog = progSimpleInput;
                 output_size = VOCAB_SIZE;
+                migraphx::shape out_shape{migraphx_shape_half_type, {1, 1, VOCAB_SIZE}};
+                prog_args.add(output_name, migraphx::argument(out_shape, output_buffer_oneDim.data()));
 
                 auto param_shapes = prog.get_parameter_shapes();
                 auto inputShape = param_shapes[model_inputs.INPUTS_ID_STR];
                 std::vector<int64_t> input_ids = {new_token};
                 model_inputs.input_ids_buffer = std::make_unique<LLama2InputBuffer>(std::move(input_ids), offload_copy);
                 prog_args.add(model_inputs.INPUTS_ID_STR, migraphx::argument(inputShape, model_inputs.input_ids_buffer->data()));
+                model_inputs.input_ids_buffer->upload_to_device(stream);
             }
             else
             {

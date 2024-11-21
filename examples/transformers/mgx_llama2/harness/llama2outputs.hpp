10596c1cd64b3720189170d63d1a516962e0fffe
Oliver Toth <Oliver.Toth@amd.com>
2024-11-21 08:18:27 -0600

    Refactor outputs to llama2outputs file and move main to MGXLlama2 struct

---
+++ examples/transformers/mgx_llama2/harness/llama2outputs.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "buffer.hpp"
+#include "config.hpp"
+
+#include <migraphx/migraphx.hpp>
+
+struct LLama2Outputs
+{
+    LLama2Outputs(bool offload_copy)
+        : offload_copy(offload_copy)
+    {
+    }
+
+    void prepareProgArgs(migraphx::program_parameters& prog_args, migraphx::program_parameters& prog_args_one_dim)
+    {
+        output_buffer = std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(OUTPUT_SIZE), offload_copy);
+        one_dim_output_buffer = std::make_unique<LLama2PastKeyValueBuffer>(std::vector<half>(VOCAB_SIZE), offload_copy);
+        migraphx::shape out_shape{migraphx_shape_half_type, {1, SEQ_SIZE, VOCAB_SIZE}};
+        prog_args.add(OUTPUT_NAME, migraphx::argument(out_shape, output_buffer->data()));
+
+        migraphx::shape x_shape_one_dim{migraphx_shape_half_type, {1, 1, VOCAB_SIZE}};
+        prog_args_one_dim.add(OUTPUT_NAME, migraphx::argument(x_shape_one_dim, one_dim_output_buffer->data()));
+    }
+
+    void prepareProgArgsArgMax(migraphx::program_parameters& prog_args_argmax, migraphx::program_parameters& prog_args_argmax_one_dim)
+    {
+        // setting up argmax arguments
+        migraphx::shape x_shape{migraphx_shape_half_type, {1, SEQ_SIZE, VOCAB_SIZE}};
+        prog_args_argmax.add("x", migraphx::argument(x_shape, output_buffer->data()));
+        argm_output_buffer = std::make_unique<ArgMaxOutputBuffer>(std::vector<int64_t>(VOCAB_SIZE), offload_copy);
+        migraphx::shape argm_out_shape{migraphx_shape_int64_type, {1, SEQ_SIZE, 1}};
+        prog_args_argmax.add(OUTPUT_NAME, migraphx::argument(argm_out_shape, argm_output_buffer->data()));
+
+        migraphx::shape x_shape_one_dim{migraphx_shape_half_type, {1, 1, VOCAB_SIZE}};
+        prog_args_argmax_one_dim.add("x", migraphx::argument(x_shape_one_dim, one_dim_output_buffer->data()));
+        argm_output_buffer_one_dim = std::make_unique<ArgMaxOutputBuffer>(std::vector<int64_t>(1), offload_copy);
+        migraphx::shape argm_out_shape_one_dim{migraphx_shape_int64_type, {1, 1, 1}};
+        prog_args_argmax_one_dim.add(OUTPUT_NAME, migraphx::argument(argm_out_shape_one_dim, argm_output_buffer_one_dim->data()));
+    }
+
+    LLama2Outputs() = delete;
+    LLama2Outputs(const LLama2Outputs &buf) = delete;
+    LLama2Outputs &operator=(const LLama2Outputs &buf) = delete;
+
+    std::unique_ptr<LLama2PastKeyValueBuffer> output_buffer;
+    std::unique_ptr<LLama2PastKeyValueBuffer> one_dim_output_buffer;
+    std::unique_ptr<ArgMaxOutputBuffer> argm_output_buffer;
+    std::unique_ptr<ArgMaxOutputBuffer> argm_output_buffer_one_dim;
+
+    bool offload_copy = false;
+
+    const char* OUTPUT_NAME = "main:#output_0";
+    const size_t OUTPUT_SIZE = SEQ_SIZE * VOCAB_SIZE;
+};
--- examples/transformers/mgx_llama2/mgxllama2.cc
+++ examples/transformers/mgx_llama2/mgxllama2.cc
@@ -2,151 +2,162 @@
 #include "common.hpp"
 #include "dataset.hpp"
 #include "llama2inputs.hpp"
+#include "llama2outputs.hpp"
 #include "utils.hpp"
 #include <migraphx/migraphx.hpp>
 
 
-int main() {
-    bool offload_copy = false;
-    check_hip_status(hipSetDevice(DEVICE_ID));
-    std::cout << "Offload copy: " << std::boolalpha << offload_copy << std::endl;
-    ModelLoadSettings settings = {SEQ_SIZE, false /*quantize_fp16*/, offload_copy /*offload_copy*/, false /*fast_math*/, false /*input_one_dim*/};
-    migraphx::program progMultipleInputDim = loadProgram(settings);
-    std::cout << "Model loaded" << std::endl;
-    migraphx::program progArgMaxMultipleInputDim = create_argmax_program(settings);
-    std::cout << "ArgMax model created" << std::endl;
-
-    // Load {1,1} input_ids model
-    settings.input_one_dim = true;
-    migraphx::program progSimpleInput = loadProgram(settings);
-    std::cout << "Model 1 dim input loaded" << std::endl;
-    migraphx::program progArgMaxSimpleInput = create_argmax_program(settings);
-    std::cout << "ArgMax model for 1 dim model created" << std::endl;
-
-    migraphx::program *prog = &progMultipleInputDim;
-    migraphx::program *progArgMax = &progArgMaxMultipleInputDim;
-
-    // Setup model inputs
-    std::vector<std::vector<uint64_t>> results;
-    std::vector<uint64_t> output_tokens;
-    migraphx::program_parameters prog_args;
-    hipStream_t stream;
-    check_hip_status(hipStreamCreateWithFlags(&stream, hipStreamNonBlocking));
-    auto model_inputs = LLama2Inputs(*prog, prog_args, offload_copy);
-    if (not offload_copy)
+struct MGXLlama2
+{
+    MGXLlama2()
     {
-        model_inputs.upload_to_device(stream);
-    }
+        check_hip_status(hipSetDevice(DEVICE_ID));
+        std::cout << "Offload copy: " << std::boolalpha << offload_copy << std::endl;
+        ModelLoadSettings settings = {SEQ_SIZE, false /*quantize_fp16*/, offload_copy /*offload_copy*/, false /*fast_math*/, false /*input_one_dim*/};
+        progMultipleInputDim = loadProgram(settings);
+        std::cout << "Model loaded" << std::endl;
+        progArgMaxMultipleInputDim = create_argmax_program(settings);
+        std::cout << "ArgMax model created" << std::endl;
 
-    auto output_name = "main:#output_0";
+        // Load {1,1} input_ids model
+        settings.input_one_dim = true;
+        progSimpleInput = loadProgram(settings);
+        std::cout << "Model 1 dim input loaded" << std::endl;
+        progArgMaxSimpleInput = create_argmax_program(settings);
+        std::cout << "ArgMax model for 1 dim model created" << std::endl;
 
-    size_t output_size = SEQ_SIZE * VOCAB_SIZE;
-    auto output_buffer = LLama2PastKeyValueBuffer(std::vector<half>(output_size), offload_copy);
-    auto output_buffer_oneDim = LLama2PastKeyValueBuffer(std::vector<half>(VOCAB_SIZE), offload_copy);
-    migraphx::shape out_shape{migraphx_shape_half_type, {1, SEQ_SIZE, VOCAB_SIZE}};
-    prog_args.add(output_name, migraphx::argument(out_shape, output_buffer.data()));
-
-    // setting up argmax arguments
-    migraphx::program_parameters prog_args_argmax;
-    migraphx::shape x_shape{migraphx_shape_half_type, {1, SEQ_SIZE, VOCAB_SIZE}};
-    prog_args_argmax.add("x", migraphx::argument(x_shape, output_buffer.data()));
-    auto argm_output_buffer = ArgMaxOutputBuffer(std::vector<int64_t>(VOCAB_SIZE), offload_copy);
-    migraphx::shape argm_out_shape{migraphx_shape_int64_type, {1, SEQ_SIZE, 1}};
-    prog_args_argmax.add(output_name, migraphx::argument(argm_out_shape, argm_output_buffer.data()));
-
-    migraphx::program_parameters prog_args_argmax_one_dim;
-    migraphx::shape x_shape_one_dim{migraphx_shape_half_type, {1, 1, VOCAB_SIZE}};
-    prog_args_argmax_one_dim.add("x", migraphx::argument(x_shape_one_dim, output_buffer_oneDim.data()));
-    auto argm_output_buffer_one_dim = ArgMaxOutputBuffer(std::vector<int64_t>(1), offload_copy);
-    migraphx::shape argm_out_shape_one_dim{migraphx_shape_int64_type, {1, 1, 1}};
-    prog_args_argmax_one_dim.add(output_name, migraphx::argument(argm_out_shape_one_dim, argm_output_buffer_one_dim.data()));
-
-
-    migraphx::program_parameters prog_args_one_dim;
-    model_inputs.prepareOneDimProgArgs(progSimpleInput, prog_args_one_dim);
-    prog_args_one_dim.add(output_name, migraphx::argument(x_shape_one_dim, output_buffer_oneDim.data()));
-
-    if (not offload_copy)
-    {
-        model_inputs.one_dim_input_buffer->upload_to_device(stream);
-    }
-
-    std::cout << "Dataset size: " << model_inputs.dataSize() << std::endl;
-    std::cout << "Starting evaluation" << std::endl;
-    size_t token_count = 0;
-    auto start = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < model_inputs.dataSize(); ++i)
-    {
-        #ifdef TRACE
-        std::cout << "Iter #" << i << std::endl;
-        #endif
-        auto lastInputIdx = model_inputs.getLastInputIndex();
-        for (size_t i = lastInputIdx; i < SEQ_SIZE - 1; ++i)
-        {
-            bool firstIter = (i == lastInputIdx);
-            prog->run_async(firstIter ? prog_args : prog_args_one_dim, stream);
-            auto outputs = progArgMax->run_async(firstIter ? prog_args_argmax : prog_args_argmax_one_dim, stream);
-            if (not offload_copy)
-            {
-                firstIter ? argm_output_buffer.download_from_device(stream, i, i + 1) : argm_output_buffer_one_dim.download_from_device(stream);
-            }
-
-            check_hip_status(hipStreamSynchronize(stream));
-            int64_t* results = offload_copy ? reinterpret_cast<int64_t*>(outputs[0].data()) : reinterpret_cast<int64_t*>( firstIter ? argm_output_buffer.hbuff.data() : argm_output_buffer_one_dim.hbuff.data());
-            auto new_token_idx = firstIter ? i : 0; 
-            int64_t new_token = results[new_token_idx];
-
-            token_count++;
-            #ifdef TRACE
-            std::cout << "New token: " << new_token << std::endl;
-            #endif
-            output_tokens.push_back(new_token);
-
-            if (new_token == EOS)
-            {
-                break;
-            }
-
-            model_inputs.attention_mask_buffer->update_data(1, i + 1, stream);
-
-            if (firstIter)
-            {
-                prog = &progSimpleInput;
-                progArgMax = &progArgMaxSimpleInput;
-            }
-
-            model_inputs.one_dim_input_buffer->update_data(new_token, 0, stream);
-        }
-
-#ifdef TRACE
-        std::cout << "######### Output token ids for #" << i << " #########" << std::endl;
-        // print output tokens
-        for (auto tok: output_tokens){
-            std::cout << tok << ", ";
-        }
-        std::cout << std::endl;
-#endif
         prog = &progMultipleInputDim;
         progArgMax = &progArgMaxMultipleInputDim;
 
-        auto updated = model_inputs.updateData(*prog, prog_args);
-
-        if (updated && not offload_copy)
+        check_hip_status(hipStreamCreateWithFlags(&stream, hipStreamNonBlocking));
+        model_inputs = std::make_unique<LLama2Inputs>(*prog, prog_args, offload_copy);
+        if (not offload_copy)
         {
-            model_inputs.upload_to_device(stream);
+            model_inputs->upload_to_device(stream);
         }
-        results.emplace_back(output_tokens);
-        output_tokens.clear();
+
+        model_inputs->prepareOneDimProgArgs(progSimpleInput, prog_args_one_dim);
+
+        model_outputs = std::make_unique<LLama2Outputs>(offload_copy);
+        model_outputs->prepareProgArgs(prog_args,prog_args_one_dim);
+        // setting up argmax arguments
+        model_outputs->prepareProgArgsArgMax(prog_args_argmax, prog_args_argmax_one_dim);
+
+        if (not offload_copy)
+        {
+            model_inputs->one_dim_input_buffer->upload_to_device(stream);
+        }
     }
 
-    float dur = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() / 1000.f;
-    std::cout << "Duration: " << dur << " seconds." << std::endl;
-    std::cout << "Completed " << token_count << " tokens." << std::endl;
-    std::cout << "Tokens/sec: " << token_count / dur << std::endl;
-
-    if (WRITE_RESULT_FILE)
+    void run()
     {
-        writeResults(results);
+        std::cout << "Dataset size: " << model_inputs->dataSize() << std::endl;
+        std::cout << "Starting evaluation" << std::endl;
+        size_t token_count = 0;
+        auto start = std::chrono::steady_clock::now();
+        for (size_t i = 0; i < model_inputs->dataSize(); ++i)
+        {
+            #ifdef TRACE
+            std::cout << "Iter #" << i << std::endl;
+            #endif
+            auto lastInputIdx = model_inputs->getLastInputIndex();
+            for (size_t i = lastInputIdx; i < SEQ_SIZE - 1; ++i)
+            {
+                bool firstIter = (i == lastInputIdx);
+                prog->run_async(firstIter ? prog_args : prog_args_one_dim, stream);
+                auto outputs = progArgMax->run_async(firstIter ? prog_args_argmax : prog_args_argmax_one_dim, stream);
+                if (not offload_copy)
+                {
+                    firstIter ? model_outputs->argm_output_buffer->download_from_device(stream, i, i + 1) : model_outputs->argm_output_buffer_one_dim->download_from_device(stream);
+                }
+
+                check_hip_status(hipStreamSynchronize(stream));
+                int64_t* results = offload_copy ? reinterpret_cast<int64_t*>(outputs[0].data()) : reinterpret_cast<int64_t*>( firstIter ? model_outputs->argm_output_buffer->hbuff.data() : model_outputs->argm_output_buffer_one_dim->hbuff.data());
+                auto new_token_idx = firstIter ? i : 0;
+                int64_t new_token = results[new_token_idx];
+
+                token_count++;
+                #ifdef TRACE
+                std::cout << "New token: " << new_token << std::endl;
+                #endif
+                output_tokens.push_back(new_token);
+
+                if (new_token == EOS)
+                {
+                    break;
+                }
+
+                model_inputs->attention_mask_buffer->update_data(1, i + 1, stream);
+
+                if (firstIter)
+                {
+                    prog = &progSimpleInput;
+                    progArgMax = &progArgMaxSimpleInput;
+                }
+
+                model_inputs->one_dim_input_buffer->update_data(new_token, 0, stream);
+            }
+
+    #ifdef TRACE
+            std::cout << "######### Output token ids for #" << i << " #########" << std::endl;
+            // print output tokens
+            for (auto tok: output_tokens){
+                std::cout << tok << ", ";
+            }
+            std::cout << std::endl;
+    #endif
+            prog = &progMultipleInputDim;
+            progArgMax = &progArgMaxMultipleInputDim;
+
+            auto updated = model_inputs->updateData(*prog, prog_args);
+
+            if (updated && not offload_copy)
+            {
+                model_inputs->upload_to_device(stream);
+            }
+            results.emplace_back(output_tokens);
+            output_tokens.clear();
+        }
+
+        float dur = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() / 1000.f;
+        std::cout << "Duration: " << dur << " seconds." << std::endl;
+        std::cout << "Completed " << token_count << " tokens." << std::endl;
+        std::cout << "Tokens/sec: " << token_count / dur << std::endl;
+
+        if (WRITE_RESULT_FILE)
+        {
+            writeResults(results);
+        }
     }
+
+    MGXLlama2(const MGXLlama2 &buf) = delete;
+    MGXLlama2 &operator=(const MGXLlama2 &buf) = delete;
+
+    migraphx::program progMultipleInputDim;
+    migraphx::program progArgMaxMultipleInputDim;
+    migraphx::program progSimpleInput;
+    migraphx::program progArgMaxSimpleInput;
+    migraphx::program *prog = nullptr;
+    migraphx::program *progArgMax = nullptr;
+
+    migraphx::program_parameters prog_args;
+    migraphx::program_parameters prog_args_one_dim;
+    migraphx::program_parameters prog_args_argmax;
+    migraphx::program_parameters prog_args_argmax_one_dim;
+
+    std::vector<std::vector<uint64_t>> results;
+    std::vector<uint64_t> output_tokens;
+    hipStream_t stream;
+    bool offload_copy = false;
+
+    std::unique_ptr<LLama2Inputs> model_inputs;
+    std::unique_ptr<LLama2Outputs> model_outputs;
+};
+
+
+int main()
+{
+    MGXLlama2 mgxllama2;
+    mgxllama2.run();
     return 0;
 }

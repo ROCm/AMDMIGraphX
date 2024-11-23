6457ec4d0b5447c313bd72472fd229239e04d3c1
Paul <pfultz2@yahoo.com>
2024-11-22 18:22:01 -0600

    Check for empty instead

--- src/targets/gpu/kernels/include/migraphx/kernels/pp.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/pp.hpp
@@ -31,10 +31,25 @@
 #define MIGRAPHX_PP_IIF_0(t, ...) __VA_ARGS__
 #define MIGRAPHX_PP_IIF_1(t, ...) t
 
-#define MIGRAPHX_PP_IS_PAREN(x) MIGRAPHX_PP_IS_PAREN_CHECK(MIGRAPHX_PP_IS_PAREN_PROBE x)
-#define MIGRAPHX_PP_IS_PAREN_CHECK(...) MIGRAPHX_PP_IS_PAREN_CHECK_N(__VA_ARGS__, 0)
-#define MIGRAPHX_PP_IS_PAREN_PROBE(...) ~, 1,
-#define MIGRAPHX_PP_IS_PAREN_CHECK_N(x, n, ...) n
+#define MIGRAPHX_PP_COMPL(b) MIGRAPHX_PP_PRIMITIVE_CAT(MIGRAPHX_PP_COMPL_, b)
+#define MIGRAPHX_PP_COMPL_0 1
+#define MIGRAPHX_PP_COMPL_1 0
+
+#define MIGRAPHX_PP_BITAND(x) MIGRAPHX_PP_PRIMITIVE_CAT(MIGRAPHX_PP_BITAND_, x)
+#define MIGRAPHX_PP_BITAND_0(y) 0
+#define MIGRAPHX_PP_BITAND_1(y) y
+
+#define MIGRAPHX_PP_CHECK(...) MIGRAPHX_PP_CHECK_N(__VA_ARGS__, 0,)
+#define MIGRAPHX_PP_CHECK_N(x, n, ...) n
+#define MIGRAPHX_PP_PROBE(x) x, 1,
+
+#define MIGRAPHX_PP_IS_PAREN(x) MIGRAPHX_PP_CHECK(MIGRAPHX_PP_IS_PAREN_PROBE x)
+#define MIGRAPHX_PP_IS_PAREN_PROBE(...) MIGRAPHX_PP_PROBE(~)
+
+#define MIGRAPHX_PP_PRIMITIVE_IS_EMPTY(x) MIGRAPHX_PP_CHECK(MIGRAPHX_PP_PRIMITIVE_IS_EMPTY_PROBE x ())
+#define MIGRAPHX_PP_PRIMITIVE_IS_EMPTY_PROBE(...) MIGRAPHX_PP_PROBE(~)
+
+#define MIGRAPHX_PP_IS_EMPTY_ARG(x) MIGRAPHX_PP_BITAND(MIGRAPHX_PP_COMPL(MIGRAPHX_PP_IS_PAREN(x)))(MIGRAPHX_PP_PRIMITIVE_IS_EMPTY(x))
 
 #define MIGRAPHX_PP_EAT(...)
 #define MIGRAPHX_PP_EXPAND(...) __VA_ARGS__
@@ -55,22 +70,7 @@
 #define MIGRAPHX_PP_REPEAT(n, m, ...) \
     MIGRAPHX_PP_PRIMITIVE_CAT(MIGRAPHX_PP_REPEAT, n)(m, __VA_ARGS__)
 
-#define MIGRAPHX_PP_RES_ARGS (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        (),              \
-                                                        ()
+#define MIGRAPHX_PP_RES_ARGS() ,,,,,,,,,,,,,,,
 
 #define MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARGS(...) MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARGS_IMPL(__VA_ARGS__)
 
@@ -108,19 +108,19 @@
     MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARG(delim, x15) MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARG(m, x15)
 
 #define MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARG(m, x) \
-    MIGRAPHX_PP_IIF(MIGRAPHX_PP_IS_PAREN(x))(MIGRAPHX_PP_EAT, m)(x)
+    MIGRAPHX_PP_IIF(MIGRAPHX_PP_IS_EMPTY_ARG(x))(MIGRAPHX_PP_EAT, m)(x)
 
 
 #define MIGRAPHX_PP_EACH_ARGS(m, ...)                                    \
     MIGRAPHX_PP_EXPAND(MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARGS(m,             \
                                                         MIGRAPHX_PP_EAT, \
                                                         __VA_ARGS__,   \
-                                                        MIGRAPHX_PP_RES_ARGS))
+                                                        MIGRAPHX_PP_RES_ARGS()))
 
 #define MIGRAPHX_PP_TRANSFORM_ARGS(m, ...)                                 \
     MIGRAPHX_PP_EXPAND(MIGRAPHX_PP_PRIMITIVE_TRANSFORM_ARGS(m,               \
                                                         MIGRAPHX_PP_COMMA, \
                                                         __VA_ARGS__,     \
-                                                        MIGRAPHX_PP_RES_ARGS))
+                                                        MIGRAPHX_PP_RES_ARGS()))
 
 #endif // MIGRAPHX_GUARD_KERNELS_PP_HPP

43364a103b0a3b7dd0cb39b394ffa026eaca593e
Oliver Toth <Oliver.Toth@amd.com>
2024-12-04 07:59:12 -0600

    Fix python files format issues

--- examples/transformers/mgx_llama2/preprocess_dataset.py
+++ examples/transformers/mgx_llama2/preprocess_dataset.py
@@ -16,22 +16,25 @@ else:
     raise RuntimeError(f"Missing dataset from {DATASET_PATH}")
 
 toks = d['tok_input'].to_list()
-#toks = [toks[0]]
 
 toks_np = np.ones((len(toks), G_MAX_TOK_LEN), dtype=np.int64) * G_LLAMA2_EOS
 mask_np = np.zeros((len(toks), G_MAX_TOK_LEN), dtype=np.int64)
-position_nps = [np.arange(0, G_MAX_TOK_LEN, dtype=np.int64) for _ in range(len(toks))]
+position_nps = [
+    np.arange(0, G_MAX_TOK_LEN, dtype=np.int64) for _ in range(len(toks))
+]
 
 
 for i, q in enumerate(toks):
     toks_np[i, :len(q)] = q
     mask_np[i, :len(q)] = np.ones_like(q)
 
-
 token_size = len(toks)
 
-np.save(f"{OUTPUT_PATH}input_ids_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy", toks_np)
-np.save(f"{OUTPUT_PATH}attention_mask_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy", mask_np)
-np.save(f"{OUTPUT_PATH}position_ids_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy", position_nps)
+np.save(f"{OUTPUT_PATH}input_ids_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy",
+        toks_np)
+np.save(f"{OUTPUT_PATH}attention_mask_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy",
+         mask_np)
+np.save(f"{OUTPUT_PATH}position_ids_size_{token_size}_seq_{G_MAX_TOK_LEN}.npy",
+        position_nps)
 
 print("Npy files are created")

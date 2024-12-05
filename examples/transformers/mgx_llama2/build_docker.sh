20fb8bc3db7cbeab642fa94b1fd341f8b949128b
Oliver Toth <Oliver.Toth@amd.com>
2024-12-05 10:25:44 -0600

    Pass GPU_TARGET from build_docker script to Dockerfile

--- examples/transformers/mgx_llama2/build_docker.sh
+++ examples/transformers/mgx_llama2/build_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --platform linux/amd64 --tag mgx_llama2:v0.2 --file Dockerfile .
+GPU_TARGET=$(/opt/rocm/bin/rocminfo | grep -o -m1 'gfx.*')
+
+docker build --platform linux/amd64 --tag mgx_llama2:v0.2 --build-arg GPU_TARGET=$GPU_TARGET --file Dockerfile .

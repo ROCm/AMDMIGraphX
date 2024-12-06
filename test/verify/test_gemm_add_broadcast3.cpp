a2e1a82fb0d077eceb3c0a279ad41e07815ad5a4
shivadbhavsar <Shivad.Bhavsar@amd.com>
2024-12-06 11:18:39 -0600

    remove fp8 types in test case for now

--- test/verify/test_gemm_add_broadcast3.cpp
+++ test/verify/test_gemm_add_broadcast3.cpp
@@ -53,7 +53,7 @@ struct test_gemm_add_broadcast3 : verify_program<test_gemm_add_broadcast3<DType>
 
 template struct test_gemm_add_broadcast3<migraphx::shape::float_type>;
 template struct test_gemm_add_broadcast3<migraphx::shape::half_type>;
-template struct test_gemm_add_broadcast3<migraphx::shape::fp8e4m3fnuz_type>;
+// template struct test_gemm_add_broadcast3<migraphx::shape::fp8e4m3fnuz_type>;
 // template struct test_gemm_add_broadcast3<migraphx::shape::fp8e5m2fnuz_type>;
-template struct test_gemm_add_broadcast3<migraphx::shape::fp8e4m3fn_type>;
-template struct test_gemm_add_broadcast3<migraphx::shape::fp8e5m2_type>;
+// template struct test_gemm_add_broadcast3<migraphx::shape::fp8e4m3fn_type>;
+// template struct test_gemm_add_broadcast3<migraphx::shape::fp8e5m2_type>;

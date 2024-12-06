e52b95ea6bcaf5a3b6ac61ee2edb3f2397070cbe
Richa Gadgil <richa.gadgil@amd.com>
2024-12-06 10:17:58 -0800

    Update hip.hpp

--- src/targets/gpu/kernels/include/migraphx/kernels/hip.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/hip.hpp
@@ -27,9 +27,7 @@
 #ifndef MIGRAPHX_USE_HIPRTC
 #include <hip/hip_runtime.h>
 #include <hip/hip_fp16.h>
-#include <hip/hip_bf16.h>
 #include <hip/math_functions.h>
-#include <hip/hip_bf16.h>
 #endif
 
 #endif // MIGRAPHX_GUARD_KERNELS_HIP_HPP

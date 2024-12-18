755ce18b4d99f852cc51d9d503b57a11393007ea
charlie <charlie.lin@amd.com>
2024-12-18 17:21:33 -0600

    Fix the bug add RFC tests

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,37 +1,52 @@
 #include <migraphx/base64.hpp>
 #include <vector>
+#include <array>
+#include <iostream>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
+namespace{
+
+std::array<char, 64> constexpr B64chars{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', '+', '/'};
+
 /// base64 encoder snippet altered from https://stackoverflow.com/a/37109258
-std::string b64_encode(const std::string& input)
+const std::string b64_encode(const void* data, const size_t &len)
 {
-    static const std::string B64chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    const std::size_t len = input.size();
     std::vector<char> res_vec((len + 2) / 3 * 4, '=');
-    std::size_t j     = 0;
-    std::size_t pad   = len % 3;
+    const unsigned char *p = static_cast<const unsigned char*>(data);
+    size_t j = 0, pad = len % 3;
     const size_t last = len - pad;
 
-    for(size_t i = 0; i < last; i += 3)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+    for (size_t i = 0; i < last; i += 3)
     {
-        int n = static_cast<int>(input.at(i)) << 16 | static_cast<int>(input.at(i + 1)) << 8 |
-                input.at(i + 2);
+        unsigned n = static_cast<unsigned>(p[i]) << 16 | static_cast<unsigned>(p[i + 1]) << 8 | p[i + 2];
         res_vec.at(j++) = B64chars.at(n >> 18);
-        res_vec[j++]    = B64chars.at(n >> 12 & 0x3F);
-        res_vec[j++]    = B64chars.at(n >> 6 & 0x3F);
-        res_vec[j++]    = B64chars.at(n & 0x3F);
+        res_vec.at(j++) = B64chars.at(n >> 12 & 0x3F);
+        res_vec.at(j++) = B64chars.at(n >> 6 & 0x3F);
+        res_vec.at(j++) = B64chars.at(n & 0x3F);
     }
-    if(pad) /// Set padding
+    if (pad)  /// Set padding
     {
-        int n = --pad ? static_cast<int>(input.at(last)) << 8 | input.at(last + 1) : input.at(last);
-        res_vec[j++] = B64chars.at(pad ? n >> 10 & 0x3F : n >> 2);
-        res_vec[j++] = B64chars.at(pad ? n >> 4 & 0x03F : n << 4 & 0x3F);
-        res_vec[j++] = pad ? B64chars.at(n << 2 & 0x3F) : '=';
+        unsigned n = --pad ? static_cast<unsigned>(p[last]) << 8 | p[last + 1] : p[last];
+        res_vec.at(j++) = B64chars.at(pad ? n >> 10 & 0x3F : n >> 2);
+        res_vec.at(j++) = B64chars.at(pad ? n >> 4 & 0x03F : n << 4 & 0x3F);
+        res_vec.at(j++) = pad ? B64chars.at(n << 2 & 0x3F) : '=';
     }
+    #pragma clang diagnostic pop
     return std::string(res_vec.begin(), res_vec.end());
+}
+
+}
+
+std::string b64_encode(const std::string& str)
+{
+    return b64_encode(str.c_str(), str.size());
 }
 
 } // namespace MIGRAPHX_INLINE_NS
--- test/base64_test.cpp
+++ test/base64_test.cpp
@@ -47,4 +47,122 @@ TEST_CASE(base64_encoding)
     EXPECT(migraphx::b64_encode(input_4) == expected_4);
 }
 
+TEST_CASE(base64_RFC_test_vectors)
+{
+    std::string input_0    = "";
+    std::string expected_0 = "";
+    EXPECT(migraphx::b64_encode(input_0) == expected_0);
+
+    std::string input_1    = "f";
+    std::string expected_1 = "Zg==";
+    EXPECT(migraphx::b64_encode(input_1) == expected_1);
+
+    std::string input_2    = "fo";
+    std::string expected_2 = "Zm8=";
+    EXPECT(migraphx::b64_encode(input_2) == expected_2);
+
+    std::string input_3    = "foo";
+    std::string expected_3 = "Zm9v";
+    EXPECT(migraphx::b64_encode(input_3) == expected_3);
+    
+    std::string input_4    = "foob";
+    std::string expected_4 = "Zm9vYg==";
+    EXPECT(migraphx::b64_encode(input_4) == expected_4);
+
+    std::string input_5    = "fooba";
+    std::string expected_5 = "Zm9vYmE=";
+    EXPECT(migraphx::b64_encode(input_5) == expected_5);
+
+    std::string input_6    = "foobar";
+    std::string expected_6 = "Zm9vYmFy";
+    EXPECT(migraphx::b64_encode(input_6) == expected_6);
+}
+
+// Following tests altered from https://github.com/tobiaslocker/base64/blob/master/test/base64_tests.cpp
+TEST_CASE(base64_encodes_empty)
+{
+  std::string const expected{};
+  std::string const actual{migraphx::b64_encode({})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_three_bytes_zeros) 
+{
+  std::array<std::uint8_t, 3> const input{0x00, 0x00, 0x00};
+  auto const expected{"AAAA"};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual);
+}
+
+TEST_CASE(base64_encodes_three_bytes_random) 
+{
+  std::array<std::uint8_t, 3> const input{0xFE, 0xE9, 0x72};
+  std::string const expected{"/uly"};
+  std::string const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_two_bytes)
+{
+  std::array<std::uint8_t, 2> const input{0x00, 0x00};
+  auto const expected{"AAA="};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_one_byte)
+{
+  std::array<std::uint8_t, 1> const input{0x00};
+  auto const expected{"AA=="};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_four_bytes) {
+  std::array<std::uint8_t, 4> const input{0x74, 0x68, 0x65, 0x20};
+  auto const expected{"dGhlIA=="};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_five_bytes) {
+  std::array<std::uint8_t, 5> const input{0x20, 0x62, 0x72, 0x6f, 0x77};
+  auto const expected{"IGJyb3c="};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_six_bytes) {
+  std::array<std::uint8_t, 6> const input{0x20, 0x6a, 0x75, 0x6d, 0x70, 0x73};
+  auto const expected{"IGp1bXBz"};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_BrownFox) {
+  std::array<std::uint8_t, 43> const input{
+      0x74, 0x68, 0x65, 0x20, 0x71, 0x75, 0x69, 0x63, 0x6b, 0x20, 0x62,
+      0x72, 0x6f, 0x77, 0x6e, 0x20, 0x66, 0x6f, 0x78, 0x20, 0x6a, 0x75,
+      0x6d, 0x70, 0x73, 0x20, 0x6f, 0x76, 0x65, 0x72, 0x20, 0x74, 0x68,
+      0x65, 0x20, 0x6c, 0x61, 0x7a, 0x79, 0x20, 0x64, 0x6f, 0x67};
+
+  auto const expected{
+      "dGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw=="};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
+TEST_CASE(base64_encodes_EncodesBrownFastFoxNullInMiddle) {
+  std::array<std::uint8_t, 45> const input{
+      0x74, 0x68, 0x65, 0x20, 0x71, 0x75, 0x69, 0x63, 0x6b, 0x21, 0x20, 0x62,
+      0x72, 0x6f, 0x77, 0x6e, 0x20, 0x66, 0x6f, 0x78, 0x20, 0x6a, 0x75, 0x6d,
+      0x70, 0x73, 0x20, 0x6f, 0x76, 0x65, 0x72, 0x20, 0x74, 0x68, 0x65, 0x00,
+      0x20, 0x6c, 0x61, 0x7a, 0x79, 0x20, 0x64, 0x6f, 0x67};
+
+  auto const expected{
+      "dGhlIHF1aWNrISBicm93biBmb3gganVtcHMgb3ZlciB0aGUAIGxhenkgZG9n"};
+  auto const actual{migraphx::b64_encode({input.begin(), input.end()})};
+  EXPECT(expected == actual); 
+}
+
 int main(int argc, const char* argv[]) { test::run(argc, argv); }

0da99d4c86b08c2cfb6bd351759f98749307d627
Ted Themistokleous <ted.themistokleous@amd.com>
2024-12-23 18:06:37 +0000

    Fix test case for activation argument

--- test/onnx/parse/group_norm_contrib_silu_3d_test.cpp
+++ test/onnx/parse/group_norm_contrib_silu_3d_test.cpp
@@ -30,6 +30,12 @@ TEST_CASE(group_norm_contrib_silu_3d_test)
     migraphx::program p = make_group_norm(
         {1, 4, 2}, {2}, {2}, {1, 2, 2, 2}, {2, 3}, 1e-5f, migraphx::shape::float_type, "gamma", "beta");
 
+    // Add sigmoid at the end of the program to represent the added SILU block
+    auto *mm = p.get_main_module();
+    auto output   = std::prev(mm->end());
+    auto sigmoid  = mm->add_instruction(migraphx::make_op("sigmoid"), output);
+    output = mm->add_instruction(migraphx::make_op("mul"), output, sigmoid);
+
     auto prog = optimize_onnx("group_norm_contrib_silu_3d_test.onnx");
     EXPECT(p == prog);
 }

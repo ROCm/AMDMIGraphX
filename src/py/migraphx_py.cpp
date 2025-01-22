7fa69419d5ef9bbfc7cd925c61fe88a9e663751d
Shiv <shbhavsa@amd.com>
2025-01-22 16:52:34 -0600

    revert python == behavior for instruction_ref objects to default python object equality check

--- src/py/migraphx_py.cpp
+++ src/py/migraphx_py.cpp
@@ -422,10 +422,7 @@ MIGRAPHX_PYBIND11_MODULE(migraphx, m)
         .def("__hash__",
              [](const migraphx::instruction_ref& i) {
                  return std::hash<migraphx::instruction_ref>()(i);
-             })
-        .def("__eq__", [](const migraphx::instruction_ref& i, const migraphx::instruction_ref& j) {
-            return std::equal_to<migraphx::instruction_ref>()(i, j);
-        });
+             });
 
     py::class_<migraphx::module, std::unique_ptr<migraphx::module, py::nodelete>>(m, "module")
         .def("print", [](const migraphx::module& mm) { std::cout << mm << std::endl; })

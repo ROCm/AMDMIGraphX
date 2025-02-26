d4f19a564132fe66e6b3e8e90caba15218b48601
Eddie Liao <54926923+eddieliao@users.noreply.github.com>
2025-02-26 11:34:36 -0800

    Exclude failing tests for ONNX 1.17.0 (#3846)

--- test/py/onnx_backend_test.py
+++ test/py/onnx_backend_test.py
@@ -1,7 +1,7 @@
 #####################################################################################
 # The MIT License (MIT)
 #
-# Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2015-2025 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -617,6 +617,19 @@ def disabled_tests_onnx_1_16_0(backend_test):
     backend_test.exclude(r'test_maxpool_3d_dilations_use_ref_impl_large_cpu')
 
 
+def disabled_tests_onnx_1_17_0(backend_test):
+    # TODO: convtranspose failing with mismatched channel numbers
+    backend_test.exclude(r'test_convtranspose_group_2_cpu')
+    backend_test.exclude(r'test_convtranspose_group_2_image_3_cpu')
+    # TODO: empty set ReduceOps tests are generating dynamic shapes
+    backend_test.exclude(r'test_reduce_max_empty_set_cpu')
+    backend_test.exclude(r'test_reduce_sum_empty_axes_input_noop_cpu')
+    # TODO: not supported
+    backend_test.exclude(
+        r'test_resize_tf_crop_and_resize_extrapolation_value_cpu')
+    backend_test.exclude(r'test_resize_upsample_sizes_nearest_not_smaller_cpu')
+
+
 def disabled_tests_int4(backend_test):
     # quantizelinear
     backend_test.exclude(r'test_quantizelinear_int4')
@@ -1193,6 +1206,9 @@ def create_backend_test(testname=None, target_device=None):
 
         if version.parse(onnx.__version__) >= version.parse("1.16.0"):
             disabled_tests_onnx_1_16_0(backend_test)
+
+        if version.parse(onnx.__version__) >= version.parse("1.17.0"):
+            disabled_tests_onnx_1_17_0(backend_test)
 
 
 # import all test cases at global scope to make

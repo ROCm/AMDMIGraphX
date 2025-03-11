4952bc487d878845a5617e5c590c23fd619b6f6f
Paul <pfultz2@yahoo.com>
2025-03-11 09:40:53 -0700

    Add unit tests

--- src/rewrite_topk.cpp
+++ src/rewrite_topk.cpp
@@ -1,32 +1,9 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015-2025 Advanced Micro Devices, Inc. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
 #include <migraphx/rewrite_topk.hpp>
 #include <migraphx/module.hpp>
 #include <migraphx/matcher.hpp>
 #include <migraphx/make_op.hpp>
 #include <migraphx/array.hpp>
+
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -52,7 +29,6 @@ struct find_large_topk
             r /= *it;
             n *= *it;
         }
-        assert(n != 1);
         return n;
     }
 
@@ -70,6 +46,8 @@ struct find_large_topk
 
         auto gdims = dims;
         auto group = split_dim(gdims[axis], std::max<std::size_t>(n_threshold / 2, k * 4));
+        if(group < 2)
+            return;
         gdims.insert(gdims.begin() + axis, group);
         op["axis"] = axis + 1;
 
@@ -98,7 +76,13 @@ struct find_large_topk
 
 } // namespace
 
-void rewrite_topk::apply(module& m) const { match::find_matches(m, find_large_topk{}); }
+
+void rewrite_topk::apply(module& m) const
+{
+    match::find_matches(m, find_large_topk{});
+}
 
 } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx
+
+

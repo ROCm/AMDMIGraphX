07e749569ff2a51ff245539552635752924185d7
lakhinderwalia <lakhinder.walia@amd.com>
2025-03-19 19:00:45 -0700

    For a multi-gpu scenario, allow the hipSetDevice() to discern between their default streams

--- src/targets/gpu/include/migraphx/gpu/context.hpp
+++ src/targets/gpu/include/migraphx/gpu/context.hpp
@@ -142,6 +142,13 @@ struct hip_device
 
         void wait() const
         {
+            // For a multi-GPU setup, and if using their Default stream,
+            // allow a device setup call, to account for an async usage.
+            // No device setup should be required for a (non-default) stream.
+            // as confirmed with the HIP team, for synchronization.
+            if(s == nullptr)
+                setup();
+
             auto status = hipStreamSynchronize(s.get());
             if(status != hipSuccess)
                 MIGRAPHX_THROW("Failed to wait.");

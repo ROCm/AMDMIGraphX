8b04a4e0e57b8f11812886d6fbafc31ccb13fd11
Lakhinder Walia <139581206+lakhinderwalia@users.noreply.github.com>
2025-03-29 15:43:22 -0700

    Bug fix: events would get created on the wrong device in a multi-gpu scenario. (#3910)
    Currently the GPU events are being created without setting the device, which would thus fail for a multi-gpu setup.

--- src/targets/gpu/hip.cpp
+++ src/targets/gpu/hip.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -237,7 +237,7 @@ void set_device(std::size_t id)
 {
     auto status = hipSetDevice(id);
     if(status != hipSuccess)
-        MIGRAPHX_THROW("Error setting device");
+        MIGRAPHX_THROW("Error setting device: " + hip_error(status));
 }
 
 void gpu_sync()

52b27bc0d25754560f004f88c47ad99bec923a99
Ahsan Saghir <ahsaghir@amd.com>
2025-04-16 13:09:03 -0500

    Address tidy: make f parameter in time_loop function a const reference

--- src/targets/gpu/include/migraphx/gpu/time_op.hpp
+++ src/targets/gpu/include/migraphx/gpu/time_op.hpp
@@ -47,7 +47,7 @@ MIGRAPHX_GPU_EXPORT double
 time_op(const context& ictx, operation op, int bundle = 1, int nruns = 100);
 
 MIGRAPHX_GPU_EXPORT double
-time_loop(migraphx::gpu::context& gctx, int bundle, int nruns, std::function<void()> f);
+time_loop(migraphx::gpu::context& gctx, int bundle, int nruns, const std::function<void()>& f);
 
 } // namespace gpu
 } // namespace MIGRAPHX_INLINE_NS

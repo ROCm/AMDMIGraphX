afe7c3cedc4e6a0dfa4498cfb29eddafebdb7ca1
Paul <pfultz2@yahoo.com>
2025-04-17 08:32:19 -0700

    More fixes

--- src/targets/cpu/include/migraphx/cpu/parallel.hpp
+++ src/targets/cpu/include/migraphx/cpu/parallel.hpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -86,7 +86,7 @@ void parallel_for_impl(std::size_t n, std::size_t threadsize, F f)
 inline std::size_t max_threads() { return omp_get_max_threads(); }
 
 template <class F>
-void parallel_for_impl(std::size_t n, std::size_t threadsize, const F& f)
+void parallel_for_impl(std::size_t n, std::size_t threadsize, F f) // NOLINT(performance-unnecessary-value-param)
 {
     if(threadsize <= 1)
     {
--- src/targets/cpu/lowering.cpp
+++ src/targets/cpu/lowering.cpp
@@ -65,20 +65,6 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace cpu {
 
-template <typename T>
-static T zero(const T&)
-{
-    return T(0);
-}
-
-template <class T>
-static typename std::
-    conditional_t<std::is_integral<T>{}, std::make_signed<T>, std::enable_if<true, T>>::type
-    make_signed(T x)
-{
-    return x;
-}
-
 struct cpu_im2col
 {
     op::im2col op;

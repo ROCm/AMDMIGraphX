22c2658b21fc46c871740aa900fad7c36ef455a9
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2025-04-24 13:06:23 -0400

    update error message for parse_layernorm

--- src/onnx/parse_layernorm.cpp
+++ src/onnx/parse_layernorm.cpp
@@ -74,7 +74,8 @@ struct parse_layernorm : op_parser<parse_layernorm>
 
         if(x_rank < 2)
         {
-            MIGRAPHX_THROW("PARSE_LAYERNORM: invalid input shape");
+            MIGRAPHX_THROW("PARSE_SIMPLIFIED_LAYER_NORMALIZATION: invalid ndims=" +
+                           std::to_string(x_rank) + ", must be at least 2");
         }
 
         // If rank(X) is r, axis' allowed range is [-r, r)

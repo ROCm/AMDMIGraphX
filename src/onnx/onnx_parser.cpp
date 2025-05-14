8dcd26569b2c6b30ac9e7c9a9ae1f335dd29ffaa
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2025-05-14 11:53:02 -0500

    remove check for empty string

--- src/onnx/onnx_parser.cpp
+++ src/onnx/onnx_parser.cpp
@@ -500,9 +500,8 @@ static bool check_sorted(const onnx::GraphProto& graph,
     {
         for(auto&& input : node.input())
         {
-            if(not input.empty())
-                if(visited_nodes.count(input) == 0)
-                    return false;
+            if(not contains(visited_nodes, input))
+                return false;
         }
         // node outputs struct is used to keep track of currently visited nodes
         visited_nodes.insert(node.output().begin(), node.output().end());

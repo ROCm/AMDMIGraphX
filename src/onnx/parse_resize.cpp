14e43a3c914ed60ab6c7e937b09e5b5e33cbcc98
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2025-05-16 11:38:27 -0500

    add check for sizes input for keep_aspect_ratio_policy

--- src/onnx/parse_resize.cpp
+++ src/onnx/parse_resize.cpp
@@ -288,11 +288,31 @@ struct parse_resize : op_parser<parse_resize>
             MIGRAPHX_THROW("PARSE_" + opd.onnx_name + ": exclude_outside 1 is not supported!");
         }
 
+        // TODO: support implementation of 'axes' attribute.
+        // For now, it's used to check the length of 'sizes' input (if present)
+        std::vector<int64_t> axes;
+        if(contains(info.attributes, "axes"))
+        {
+            auto&& axes_vals = info.attributes.at("axes").ints();
+            axes = std::vector<int64_t>(axes_vals.begin(), axes_vals.end());
+        }
+
         if(contains(info.attributes, "keep_aspect_ratio_policy"))
         {
-            MIGRAPHX_THROW("PARSE_" + opd.onnx_name +
-                           ": keep_aspect_ratio_policy is not supported!");
+            shape last_arg_shape = args.back()->get_shape();
+            size_t last_arg_elements = last_arg_shape.elements();
+            // Check if the last arg is 'sizes' input.
+            // This attribute is only relevant if 'sizes' input is used.
+            // The shape constraints for 'sizes' are below:
+            if(last_arg_shape.type() == shape::int64_type and (last_arg_elements == args.front()->get_shape().ndim() or (
+                contains(info.attributes, "axes") and last_arg_elements == axes.size()
+            )))
+            {
+                MIGRAPHX_THROW("PARSE_" + opd.onnx_name +
+                    ": keep_aspect_ratio_policy is not supported!");
+            }
         }
+        
 
         // input data shape info
         auto in_s    = args[0]->get_shape().to_static(1);

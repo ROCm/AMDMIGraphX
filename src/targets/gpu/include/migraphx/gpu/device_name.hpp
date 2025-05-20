826c1020e50a534788ce3163417dd486fe0a6a3a
Artur Wojcik <artur.wojcik@amd.com>
2025-05-20 21:09:16 +0200

    Fix compilation with hipBLASLt OFF (#3996)

--- src/targets/gpu/include/migraphx/gpu/device_name.hpp
+++ src/targets/gpu/include/migraphx/gpu/device_name.hpp
@@ -45,7 +45,9 @@ MIGRAPHX_GPU_EXPORT bool gfx_has_bf16_intrinsics();
 
 MIGRAPHX_GPU_EXPORT bool gfx_has_fp8fnuz_support();
 
+#if MIGRAPHX_USE_HIPBLASLT
 MIGRAPHX_GPU_EXPORT bool gfx_default_rocblas();
+#endif
 
 MIGRAPHX_GPU_EXPORT bool hipblaslt_supported();
 

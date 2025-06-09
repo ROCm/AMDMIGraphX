003f3295403a6b9e8b9933c45c4d0d7de96da63c
charlie <charlie.lin@amd.com>
2025-06-09 16:24:41 -0500

    progress 2

--- src/include/migraphx/op/pack_fp4.hpp
+++ src/include/migraphx/op/pack_fp4.hpp
@@ -27,6 +27,7 @@
 #include <migraphx/check_shapes.hpp>
 #include <migraphx/op/normalize_attribute.hpp>
 #include <migraphx/argument.hpp>
+#include <migraphx/par_for.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -35,8 +36,41 @@ namespace op {
 namespace {
 // roundTiesToEven
 constexpr uint8_t cast_to_fp4(float f_x)
-
+{
     uint32_t x = migraphx::bit_cast<uint32_t>(f_x);
+    uint32_t head     = 0;
+    uint32_t mantissa = 0;
+    int exponent      = 0;
+    uint32_t bias     = 0;
+    uint32_t sign     = 0;
+    head              = x & 0xFF800000;
+    mantissa          = x & 0x7FFFFF;
+    exponent          = (head >> 23) & 0xFF;
+    sign              = head >> 31;
+    bias              = 127;
+    // input is inf or NaN. No inf or NaN in fp4
+    if((x & 0x7F800000) == 0x7F800000)
+    {
+        // inf
+        if(mantissa == 0)
+        {
+            if(sign == 0)
+                return 0x7;
+            else
+                return 0xF;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    // postive zero
+    if(x == 0)
+        return 0;
+    // negative zero
+    else if(x == 0x80000000)
+        return 0x80;
+}
 
 } // namespace
 
@@ -78,15 +112,26 @@ struct pact_fp4
         auto in_shape = input.get_shape();
 
         auto uint8_shape = shape{migraphx::shape::uint8_type, output_shape.lens()};
-        argument result{uint8_shape};
-        visit(result)([&](auto res) {
-            visit(input)([&](auto inp) {
+        argument uint_arg{uint8_shape};
+        uint_arg.visit([&](auto out) {
+            input.visit([&](auto inp) {
                 par_for(output_shape.elements(), [&](auto i) {
-                    uint8_t lowest_fp4 = 0xf;
-                    uint8_t max_fp4    = 0x7;
+                    using inp_type         = typename decltype(inp)::value_type;
+                    auto data_idx          = output_shape.multi(i);
+                    auto in_data_multi_idx = data_idx;
+                    in_data_multi_idx[axis] *= 2;
+                    inp_type inp_val0 = inp[in_data_multi_idx];
+                    in_data_multi_idx[axis] += 1;
+                    inp_type inp_val1 = inp[in_data_multi_idx];
+                    uint8_t out_val0  = cast_to_fp4(inp_val0);
+                    uint8_t out_val1  = cast_to_fp4(inp_val1);
+                    out[i]            = (out_val0 << 4) | (out_val1 & 0xf);
                 });
             });
         });
+        migraphx::argument result =
+            uint_arg.reshape({migraphx::shape::mxfp4_type, output_shape.lens()});
+        return result;
     }
 };
 

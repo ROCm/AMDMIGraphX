01b5705848928b091f2e9f52cd352cdc3026bdd2
Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
2025-06-10 15:29:55 -0400

    Change formatting for test input string
    Co-authored-by: kahmed10 <15948690+kahmed10@users.noreply.github.com>

--- tools/accuracy/accuracy_checker.py
+++ tools/accuracy/accuracy_checker.py
@@ -254,7 +254,7 @@ def main():
         test_inputs[name] = test_input
 
         if args.show_data:
-            print(test_input + "\n")
+            print(f'{test_input}\n')
 
 
         migraphx_arg = migraphx.argument(test_inputs[name])

653b2435546d29599852b018d00f7e14b93ffffd
Paul <pfultz2@yahoo.com>
2025-06-13 16:20:54 -0700

    Format

--- src/include/migraphx/transform_view.hpp
+++ src/include/migraphx/transform_view.hpp
@@ -11,168 +11,160 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace views {
 
-template<class Range, class F>
-struct transform_view {
+template <class Range, class F>
+struct transform_view
+{
 
-    constexpr transform_view(Range& prng, F pf)
-      : rng(&prng), f(std::move(pf)) {}
+    constexpr transform_view(Range& prng, F pf) : rng(&prng), f(std::move(pf)) {}
 
-    struct iterator {
+    struct iterator
+    {
         using underlying_iterator = decltype(std::begin(std::declval<Range&>()));
-        using reference           = decltype(std::declval<const F>()(std::declval<typename std::iterator_traits<underlying_iterator>::reference>()));
+        using reference           = decltype(std::declval<const F>()(
+            std::declval<typename std::iterator_traits<underlying_iterator>::reference>()));
         using value_type          = std::decay_t<reference>;
 
-        using iterator_category = typename std::iterator_traits<underlying_iterator>::iterator_category;
-        using difference_type   = typename std::iterator_traits<underlying_iterator>::difference_type;
-        using pointer           = std::add_pointer_t<std::remove_reference_t<reference>>;
+        using iterator_category =
+            typename std::iterator_traits<underlying_iterator>::iterator_category;
+        using difference_type = typename std::iterator_traits<underlying_iterator>::difference_type;
+        using pointer         = std::add_pointer_t<std::remove_reference_t<reference>>;
 
-        struct arrow_proxy {
+        struct arrow_proxy
+        {
             reference value;
-            constexpr std::add_pointer_t<std::remove_reference_t<reference>> operator->() && { return std::addressof(value); }
+            constexpr std::add_pointer_t<std::remove_reference_t<reference>> operator->() &&
+            {
+                return std::addressof(value);
+            }
         };
 
         constexpr iterator() = default;
 
         constexpr iterator(const transform_view* pparent, underlying_iterator it)
-          : parent(pparent), current(it) {}
-
-        constexpr reference operator*() const {
-            return parent->f(*current);
+            : parent(pparent), current(it)
+        {
         }
 
-        constexpr arrow_proxy operator->() const {
-            return arrow_proxy{parent->f(*current)};
-        }
+        constexpr reference operator*() const { return parent->f(*current); }
 
-        constexpr iterator& operator++() {
+        constexpr arrow_proxy operator->() const { return arrow_proxy{parent->f(*current)}; }
+
+        constexpr iterator& operator++()
+        {
             ++current;
             return *this;
         }
-        constexpr iterator  operator++(int) {
+        constexpr iterator operator++(int)
+        {
             auto tmp = *this;
             ++*this;
             return tmp;
         }
 
-        constexpr iterator& operator--() {
+        constexpr iterator& operator--()
+        {
             --current;
             return *this;
         }
 
-        constexpr iterator operator--(int) {
+        constexpr iterator operator--(int)
+        {
             iterator temp = *this;
             --(*this);
             return temp;
         }
 
-        constexpr iterator operator+(difference_type n) const {
+        constexpr iterator operator+(difference_type n) const
+        {
             return iterator(parent, current + n);
         }
 
-        constexpr iterator operator-(difference_type n) const {
+        constexpr iterator operator-(difference_type n) const
+        {
             return iterator(parent, current - n);
         }
 
-        constexpr difference_type operator-(const iterator& other) const {
+        constexpr difference_type operator-(const iterator& other) const
+        {
             return current - other.current;
         }
 
-        constexpr iterator& operator+=(difference_type n) {
+        constexpr iterator& operator+=(difference_type n)
+        {
             current += n;
             return *this;
         }
 
-        constexpr iterator& operator-=(difference_type n) {
+        constexpr iterator& operator-=(difference_type n)
+        {
             current -= n;
             return *this;
         }
 
-        constexpr reference operator[](difference_type n) const {
+        constexpr reference operator[](difference_type n) const
+        {
             return parent->f(*(current + n));
         }
 
-        constexpr bool operator==(const iterator& other) const {
-            return current == other.current;
-        }
+        constexpr bool operator==(const iterator& other) const { return current == other.current; }
 
-        constexpr bool operator!=(const iterator& other) const {
-            return !(*this == other);
-        }
+        constexpr bool operator!=(const iterator& other) const { return !(*this == other); }
 
-        constexpr bool operator<(const iterator& other) const {
-            return current < other.current;
-        }
+        constexpr bool operator<(const iterator& other) const { return current < other.current; }
 
-        constexpr bool operator<=(const iterator& other) const {
-            return current <= other.current;
-        }
+        constexpr bool operator<=(const iterator& other) const { return current <= other.current; }
 
-        constexpr bool operator>(const iterator& other) const {
-            return current > other.current;
-        }
+        constexpr bool operator>(const iterator& other) const { return current > other.current; }
 
-        constexpr bool operator>=(const iterator& other) const {
-            return current >= other.current;
-        }
+        constexpr bool operator>=(const iterator& other) const { return current >= other.current; }
 
-    private:
-        const transform_view*    parent  = nullptr;
+        private:
+        const transform_view* parent = nullptr;
         underlying_iterator current{};
     };
 
-    constexpr iterator begin() const { return { this, std::begin(*rng) }; }
-    constexpr iterator end() const  { return { this, std::end  (*rng) }; }
+    constexpr iterator begin() const { return {this, std::begin(*rng)}; }
+    constexpr iterator end() const { return {this, std::end(*rng)}; }
 
-    friend constexpr bool operator==(const transform_view& a,
-                                     const transform_view& b)
+    friend constexpr bool operator==(const transform_view& a, const transform_view& b)
     {
-        return std::equal(
-            a.begin(), a.end(),
-            b.begin(), b.end()
-        );
+        return std::equal(a.begin(), a.end(), b.begin(), b.end());
     }
 
-    friend constexpr bool operator!=(const transform_view& a,
-                                     const transform_view& b)
+    friend constexpr bool operator!=(const transform_view& a, const transform_view& b)
     {
-        return not (a == b);
+        return not(a == b);
     }
 
-    friend constexpr bool operator<(const transform_view& a,
-                                    const transform_view& b)
+    friend constexpr bool operator<(const transform_view& a, const transform_view& b)
     {
-        return std::lexicographical_compare(
-            a.begin(), a.end(),
-            b.begin(), b.end()
-        );
+        return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
     }
 
-    friend constexpr bool operator>(const transform_view& a,
-                                    const transform_view& b)
+    friend constexpr bool operator>(const transform_view& a, const transform_view& b)
     {
         return b < a;
     }
 
-    friend constexpr bool operator<=(const transform_view& a,
-                                     const transform_view& b)
+    friend constexpr bool operator<=(const transform_view& a, const transform_view& b)
     {
-        return not (b < a);
+        return not(b < a);
     }
 
-    friend constexpr bool operator>=(const transform_view& a,
-                                     const transform_view& b)
+    friend constexpr bool operator>=(const transform_view& a, const transform_view& b)
     {
-        return not (a < b);
+        return not(a < b);
     }
 
-private:
+    private:
     Range* rng = nullptr;
-    F   f;
+    F f;
 };
 
 // helper for type deduction
-template<class Range, class F>
-auto transform(Range& rng, F f) {
+template <class Range, class F>
+auto transform(Range& rng, F f)
+{
     return transform_view<Range, F>(rng, std::move(f));
 }
 
--- test/transform_view.cpp
+++ test/transform_view.cpp
@@ -6,27 +6,37 @@
 
 #include <test.hpp>
 
-TEST_CASE(basic_transform) {
+TEST_CASE(basic_transform)
+{
     std::vector<int> vec = {1, 2, 3, 4, 5};
-    auto view = migraphx::views::transform(vec, [](int x) { return x * x; });
+    auto view            = migraphx::views::transform(vec, [](int x) { return x * x; });
 
     auto it = view.begin();
-    EXPECT(*it == 1); ++it;
-    EXPECT(*it == 4); ++it;
-    EXPECT(*it == 9); ++it;
-    EXPECT(*it == 16); ++it;
+    EXPECT(*it == 1);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
+    EXPECT(*it == 9);
+    ++it;
+    EXPECT(*it == 16);
+    ++it;
     EXPECT(*it == 25);
 }
 
-TEST_CASE(transform_with_reference) {
+TEST_CASE(transform_with_reference)
+{
     std::vector<int> vec = {1, 2, 3, 4, 5};
-    auto view = migraphx::views::transform(vec, [](int& x) -> int& { return x; });
+    auto view            = migraphx::views::transform(vec, [](int& x) -> int& { return x; });
 
     auto it = view.begin();
-    EXPECT(*it == 1); ++it;
-    EXPECT(*it == 2); ++it;
-    EXPECT(*it == 3); ++it;
-    EXPECT(*it == 4); ++it;
+    EXPECT(*it == 1);
+    ++it;
+    EXPECT(*it == 2);
+    ++it;
+    EXPECT(*it == 3);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
     EXPECT(*it == 5);
 
     // Modify the original vector through the view
@@ -34,34 +44,45 @@ TEST_CASE(transform_with_reference) {
     EXPECT(vec[0] == 10);
 }
 
-TEST_CASE(empty_range) {
+TEST_CASE(empty_range)
+{
     std::vector<int> vec;
     auto view = migraphx::views::transform(vec, [](int x) { return x * x; });
 
     EXPECT(bool{view.begin() == view.end()});
 }
 
-TEST_CASE(non_random_access_iterator) {
+TEST_CASE(non_random_access_iterator)
+{
     std::list<int> lst = {1, 2, 3, 4, 5};
-    auto view = migraphx::views::transform(lst, [](int x) { return x * 2; });
+    auto view          = migraphx::views::transform(lst, [](int x) { return x * 2; });
 
     auto it = view.begin();
-    EXPECT(*it == 2); ++it;
-    EXPECT(*it == 4); ++it;
-    EXPECT(*it == 6); ++it;
-    EXPECT(*it == 8); ++it;
+    EXPECT(*it == 2);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
+    EXPECT(*it == 6);
+    ++it;
+    EXPECT(*it == 8);
+    ++it;
     EXPECT(*it == 10);
 }
 
-TEST_CASE(non_random_access_iterator_with_reference) {
+TEST_CASE(non_random_access_iterator_with_reference)
+{
     std::list<int> lst = {1, 2, 3, 4, 5};
-    auto view = migraphx::views::transform(lst, [](int& x) -> int& { return x; });
+    auto view          = migraphx::views::transform(lst, [](int& x) -> int& { return x; });
 
     auto it = view.begin();
-    EXPECT(*it == 1); ++it;
-    EXPECT(*it == 2); ++it;
-    EXPECT(*it == 3); ++it;
-    EXPECT(*it == 4); ++it;
+    EXPECT(*it == 1);
+    ++it;
+    EXPECT(*it == 2);
+    ++it;
+    EXPECT(*it == 3);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
     EXPECT(*it == 5);
 
     // Modify the original list through the view
@@ -69,31 +90,41 @@ TEST_CASE(non_random_access_iterator_with_reference) {
     EXPECT(lst.front() == 10);
 }
 
-TEST_CASE(forward_iterator) {
+TEST_CASE(forward_iterator)
+{
     std::forward_list<int> flst = {1, 2, 3, 4, 5};
-    auto view = migraphx::views::transform(flst, [](int x) { return x + 1; });
+    auto view                   = migraphx::views::transform(flst, [](int x) { return x + 1; });
 
     auto it = view.begin();
-    EXPECT(*it == 2); ++it;
-    EXPECT(*it == 3); ++it;
-    EXPECT(*it == 4); ++it;
-    EXPECT(*it == 5); ++it;
+    EXPECT(*it == 2);
+    ++it;
+    EXPECT(*it == 3);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
+    EXPECT(*it == 5);
+    ++it;
     EXPECT(*it == 6);
-    
+
     auto it2 = view.begin();
     std::advance(it2, 3);
     EXPECT(*it2 == 5);
 }
 
-TEST_CASE(forward_iterator_with_reference) {
+TEST_CASE(forward_iterator_with_reference)
+{
     std::forward_list<int> flst = {1, 2, 3, 4, 5};
     auto view = migraphx::views::transform(flst, [](int& x) -> int& { return x; });
 
     auto it = view.begin();
-    EXPECT(*it == 1); ++it;
-    EXPECT(*it == 2); ++it;
-    EXPECT(*it == 3); ++it;
-    EXPECT(*it == 4); ++it;
+    EXPECT(*it == 1);
+    ++it;
+    EXPECT(*it == 2);
+    ++it;
+    EXPECT(*it == 3);
+    ++it;
+    EXPECT(*it == 4);
+    ++it;
     EXPECT(*it == 5);
 
     // Modify the original forward_list through the view
@@ -101,7 +132,8 @@ TEST_CASE(forward_iterator_with_reference) {
     EXPECT(flst.front() == 10);
 }
 
-TEST_CASE(transform_view_element_comparison) {
+TEST_CASE(transform_view_element_comparison)
+{
     std::vector<int> vec1 = {1, 2, 3, 4, 5};
     std::vector<int> vec2 = {1, 2, 3, 4, 5};
     std::vector<int> vec3 = {5, 4, 3, 2, 1};
@@ -120,48 +152,62 @@ TEST_CASE(transform_view_element_comparison) {
     EXPECT(bool{view3 >= view1});
 }
 
-struct non_comparable {
+struct non_comparable
+{
     int value;
 
-    friend bool operator==(const non_comparable& lhs, const non_comparable& rhs) {
+    friend bool operator==(const non_comparable& lhs, const non_comparable& rhs)
+    {
         return lhs.value == rhs.value;
     }
 
-    friend bool operator!=(const non_comparable& lhs, const non_comparable& rhs) {
+    friend bool operator!=(const non_comparable& lhs, const non_comparable& rhs)
+    {
         return !(lhs == rhs);
     }
 };
 
-TEST_CASE(transform_view_non_comparable_elements) {
+TEST_CASE(transform_view_non_comparable_elements)
+{
 
-    std::vector<int> vec1 = {1, 2, 3};
-    std::vector<int> vec2 = {2, 3, 4};
+    std::vector<int> vec1  = {1, 2, 3};
+    std::vector<int> vec2  = {2, 3, 4};
     auto as_non_comparable = [](int x) -> non_comparable { return {x}; };
-    auto view = migraphx::views::transform(vec1, as_non_comparable);
-    auto view2 = migraphx::views::transform(vec1, as_non_comparable);
-    auto view3 = migraphx::views::transform(vec2, as_non_comparable);
+    auto view              = migraphx::views::transform(vec1, as_non_comparable);
+    auto view2             = migraphx::views::transform(vec1, as_non_comparable);
+    auto view3             = migraphx::views::transform(vec2, as_non_comparable);
 
     EXPECT(bool{view == view2});
     EXPECT(bool{view != view3});
 }
 
-TEST_CASE(operator_arrow_in_loop_reference) {
-    struct T { int val; };
-    std::vector<T> data{{1},{2},{3}};
-    auto view = migraphx::views::transform(data, [](T& t)->T& { return t; });
-    int sum = 0;
-    for (auto it = view.begin(); it != view.end(); ++it) {
+TEST_CASE(operator_arrow_in_loop_reference)
+{
+    struct T
+    {
+        int val;
+    };
+    std::vector<T> data{{1}, {2}, {3}};
+    auto view = migraphx::views::transform(data, [](T& t) -> T& { return t; });
+    int sum   = 0;
+    for(auto it = view.begin(); it != view.end(); ++it)
+    {
         sum += it->val;
     }
     EXPECT(sum == 6);
 }
 
-TEST_CASE(operator_arrow_in_loop_value) {
-    struct T { int val; };
-    std::vector<T> data{{1},{2},{3}};
+TEST_CASE(operator_arrow_in_loop_value)
+{
+    struct T
+    {
+        int val;
+    };
+    std::vector<T> data{{1}, {2}, {3}};
     auto view = migraphx::views::transform(data, [](const T& t) { return T{t.val * 2}; });
     std::vector<int> out;
-    for (auto it = view.begin(); it != view.end(); ++it) {
+    for(auto it = view.begin(); it != view.end(); ++it)
+    {
         out.push_back(it->val);
     }
     EXPECT(out.size() == 3);

0d92773086d1048b9e0cd15d26c1d59c84fed283
charlie <charlielin7@gmail.com>
2025-06-20 15:57:41 -0400

    comments on what to do

--- src/onnx/parse_mxfixneuron.cpp
+++ src/onnx/parse_mxfixneuron.cpp
@@ -42,12 +42,22 @@ struct parse_mxfixneuron : op_parser<parse_mxfixneuron>
 		/**
 		 * TODO
 		 * Parse the ONNX op into a fake quantized Q/DQ pair with fp4 type as output of quantizelinear.
+		 * Also have to do the dynamic quantization steps.
 		 * simplify_qdq pass will add pack and unpack. Then it will then do the real quantization by
 		 * moving the DQ and recalculating scales and zero_points.
 		 *
-		 * Currently MXFixNeuron is doing dynamic quantization. If that's the case we will have to also parse into
-		 * MIGX operators to the do the dynamic quantization. Should be similar to what is in quantization.cpp.
+		 * dynamic quantization:
+		 * V_k = fp32 vector input of block size k
+		 * B_k = pow(2, floor(log2(reduce_max(V_k)))) # largest power of 2 less than V
+		 * probably can also do: B = most significant exponent of max(V)
+		 * but don't have the bit operators to do this (bit_or and bitshifts)
+		 * X_k = block scale k = B_k / (largest power of 2 in fp4e2m1) = B_k / 4
+		 * 
+		 * Reshape and broadcast the scales for quantization operators to be linear.
+		 * Quantize and dequantize with the reshaped and broadcasted scales.
 		 */
+		
+
 
 		
     }

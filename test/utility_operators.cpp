b5f1da3c284a413bc143b71dfa6b4d59398a941c
Paul <pfultz2@yahoo.com>
2025-06-26 12:40:11 -0700

    Fix operator used

--- test/utility_operators.cpp
+++ test/utility_operators.cpp
@@ -159,20 +159,20 @@ struct custom_compare_adl : migraphx::totally_ordered<custom_compare_adl>
         return lhs.x == rhs;
     }
 
-    friend constexpr bool operator<(const custom_compare_any& lhs, const custom_compare_any& rhs)
+    friend constexpr bool operator<(const custom_compare_adl& lhs, const custom_compare_adl& rhs)
     {
         return lhs.x < rhs.x;
     }
 
     template <class T>
-    friend constexpr auto operator<(const custom_compare_any& lhs,
+    friend constexpr auto operator<(const custom_compare_adl& lhs,
                                     const T& rhs) -> decltype(std::declval<int>() < rhs)
     {
         return lhs.x < rhs;
     }
 
     template <class T>
-    friend constexpr auto operator>(const custom_compare_any& lhs,
+    friend constexpr auto operator>(const custom_compare_adl& lhs,
                                     const T& rhs) -> decltype(std::declval<int>() > rhs)
     {
         return lhs.x > rhs;

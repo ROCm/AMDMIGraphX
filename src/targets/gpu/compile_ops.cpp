1bb9d22eedc42c0873f4b76480c94cd80a9c6f7a
Eddie Liao <eddie.liao@amd.com>
2025-07-01 17:52:29 +0000

    Remove leftover code

--- src/targets/gpu/compile_ops.cpp
+++ src/targets/gpu/compile_ops.cpp
@@ -145,26 +145,6 @@ struct compile_plan
             {
                 ctx->get_problem_cache().mark(preop.name(), problem);
                 const auto& solutions = config->solutions;
-                std::stringstream current_module;
-                for(const auto m : ins->module_inputs())
-                {
-                    m->print_py(current_module);
-                    current_module << "\n";
-                    for(const auto sm : m->get_sub_modules())
-                    {
-                        sm->print_py(current_module);
-                        current_module << "\n";
-                    }
-                }
-                std::stringstream submodules;
-                for(const auto m : ins->module_inputs())
-                {
-                    for(const auto sm : m->get_sub_modules())
-                    {
-                        sm->print_py(submodules);
-                        current_module << "\n";
-                    }
-                }
                 if(solutions.empty())
                     MIGRAPHX_THROW("No solutions provided for " + preop.name() + " with " +
                                    problem_string() + "\n\n" + print_modules());

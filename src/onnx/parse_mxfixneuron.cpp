ffa4c005b0ad64af51288aa43cc83a9e9adbebc2
charlie <charlie.lin@amd.com>
2025-07-01 14:49:55 -0500

    Fix parsing and formatting

--- src/onnx/parse_mxfixneuron.cpp
+++ src/onnx/parse_mxfixneuron.cpp
@@ -25,6 +25,7 @@
 #include <migraphx/ranges.hpp>
 #include <migraphx/instruction.hpp>
 #include <migraphx/make_op.hpp>
+#include <migraphx/tune_axis.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
@@ -39,11 +40,14 @@ struct parse_mxfixneuron : op_parser<parse_mxfixneuron>
                           onnx_parser::node_info info,
                           std::vector<instruction_ref> args) const
     {
+        instruction_ref input = args.front();
+        const auto input_lens = input->get_shape().lens();
         if(args.size() != 1)
         {
             MIGRAPHX_THROW("MXFixNeuron: must have only 1 input");
         }
         int block_axis = info.attributes.at("axis").i();
+        block_axis     = tune_axis(input->get_shape().ndim(), block_axis, "MXFixNeuron");
         int block_size = info.attributes.at("block_size").i();
         if(block_size != 32)
         {
@@ -61,26 +65,26 @@ struct parse_mxfixneuron : op_parser<parse_mxfixneuron>
         }
 
         // make reduction axes for calculating block scales
-        instruction_ref input = args.front();
-        const auto input_lens  = input->get_shape().lens();
         // tmp_lens != input_lens if runt block is padded
-        auto tmp_lens = input_lens;
-        std::size_t block_modulo = tmp_lens.at(block_axis) % block_size;
+        auto tmp_lens  = input_lens;
+        auto block_dim = tmp_lens.at(block_axis);
+        std::size_t block_padding =
+            ceil(double(block_dim) / double(block_size)) * block_size - block_dim;
         // handle runt block by padding
-        if(block_modulo != 0)
+        if(block_padding != 0)
         {
             std::vector<std::size_t> pads_vec(2 * tmp_lens.size(), 0);
-            pads_vec.at(block_axis + tmp_lens.size()) = block_modulo;
-            input = info.add_instruction(make_op("pad", {{"pads", pads_vec}}), input);
+            pads_vec.at(block_axis + tmp_lens.size()) = block_padding;
+            input    = info.add_instruction(make_op("pad", {{"pads", pads_vec}}), input);
             tmp_lens = input->get_shape().lens();
         }
         // reshape block dimension to {num_blocks, block_size}
-        auto num_blocks = tmp_lens.at(block_axis) / block_size;
+        std::size_t num_blocks               = tmp_lens.at(block_axis) / std::size_t(block_size);
         std::vector<std::size_t> reduct_dims = tmp_lens;
-        reduct_dims.at(block_axis) /= num_blocks;
+        reduct_dims.at(block_axis)           = block_size;
         reduct_dims.insert(reduct_dims.begin() + block_axis, num_blocks);
         instruction_ref reshape_ins =
-            info.add_instruction(make_op("reshape", {{"dims", reduct_dims}}), args.front());
+            info.add_instruction(make_op("reshape", {{"dims", reduct_dims}}), input);
 
         // dynamic quantization for MX types:
         // V_k = fp32 vector input of block size k
@@ -94,25 +98,29 @@ struct parse_mxfixneuron : op_parser<parse_mxfixneuron>
         auto lit_2_ins = info.add_literal(
             migraphx::literal{migraphx::shape{migraphx::shape::float_type}, {2.f}});
         auto broadcast_lit_2_ins = info.add_instruction(
-            make_op("multibroadcast", {{"dims", reduce_max_ins->get_shape().lens()}}), lit_2_ins);
+            make_op("multibroadcast", {{"out_lens", reduce_max_ins->get_shape().lens()}}),
+            lit_2_ins);
         auto pow_ins   = info.add_instruction(make_op("pow"), broadcast_lit_2_ins, floor_ins);
         auto lit_4_ins = info.add_literal(
             migraphx::literal{migraphx::shape{migraphx::shape::float_type}, {4.f}});
         auto broadcast_lit_4_ins = info.add_instruction(
-            make_op("multibroadcast", {{"dims", reduce_max_ins->get_shape().lens()}}), lit_4_ins);
+            make_op("multibroadcast", {{"out_lens", reduce_max_ins->get_shape().lens()}}),
+            lit_4_ins);
         auto block_scales_ins = info.add_instruction(make_op("div"), pow_ins, broadcast_lit_4_ins);
 
         // broadcast scales for use in quantizelinear
         block_scales_ins = info.add_instruction(
-            make_op("multibroadcast", {{"dims", reduct_dims}}), block_scales_ins);
+            make_op("multibroadcast", {{"out_lens", reduct_dims}}), block_scales_ins);
         block_scales_ins =
             info.add_instruction(make_op("reshape", {{"dims", tmp_lens}}), block_scales_ins);
-        
+
         // if padded runt block do slicing
         if(tmp_lens != input_lens)
         {
             std::size_t slice_size = input_lens.at(block_axis);
-            block_scales_ins = info.add_instruction(make_op("slice", {{"axes", {block_axis}}, {"starts", {0}}, {"ends", {slice_size}}}), block_scales_ins);
+            block_scales_ins       = info.add_instruction(
+                make_op("slice", {{"axes", {block_axis}}, {"starts", {0}}, {"ends", {slice_size}}}),
+                block_scales_ins);
         }
 
         auto q_ins =

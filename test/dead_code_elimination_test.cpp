7041a02ab8ec4f609a23ff4b0d9af7bc3f87e9a4
Eddie Liao <eddie.liao@amd.com>
2025-07-08 16:58:29 +0000

    Change scalar to empty literal

--- test/dead_code_elimination_test.cpp
+++ test/dead_code_elimination_test.cpp
@@ -286,11 +286,11 @@ TEST_CASE(tuple_test)
     EXPECT(std::distance(mm->begin(), mm->end()) == (count - 1));
 }
 
-TEST_CASE(convert_test)
+TEST_CASE(empty_literal)
 {
     migraphx::program p;
     auto* mm = p.get_main_module();
-    auto x   = mm->add_literal(0);
+    auto x   = mm->add_literal();
     mm->add_instruction(migraphx::make_op("convert"), x);
     mm->add_return({x});
     auto count = std::distance(mm->begin(), mm->end());

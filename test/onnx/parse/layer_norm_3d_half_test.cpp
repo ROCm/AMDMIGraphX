203b8ec03f12e80d4fc080ac45263a6b1184db6a
Ted Themistokleous <ted.themistokleous@amd.com>
2025-07-18 15:41:10 +0000

    Update parser tests

--- test/onnx/parse/layer_norm_3d_half_test.cpp
+++ test/onnx/parse/layer_norm_3d_half_test.cpp
@@ -28,7 +28,7 @@
 TEST_CASE(layer_norm_3d_half_test)
 {
     migraphx::program p =
-        make_layer_norm({1, 4, 2}, {2}, {2}, 2, false, 1e-5f, migraphx::shape::half_type);
+        make_layer_norm({1, 4, 2}, {2}, {2}, 2, false, true, 1e-5f, migraphx::shape::half_type);
 
     auto prog = optimize_onnx("layer_norm_3d_half_test.onnx");
     EXPECT(p == prog);
--- test/onnx/parse/layer_norm_small_eps_half_test.cpp
+++ test/onnx/parse/layer_norm_small_eps_half_test.cpp
@@ -28,7 +28,7 @@
 TEST_CASE(layer_norm_small_eps_half_test)
 {
     migraphx::program p =
-        make_layer_norm({1, 2}, {2}, {1}, 1, true, 1e-7, migraphx::shape::half_type);
+        make_layer_norm({1, 2}, {2}, {1}, 1, true, true, 1e-7, migraphx::shape::half_type);
 
     auto prog = optimize_onnx("layer_norm_small_eps_half_test.onnx");
     EXPECT(p == prog);

e3b06475474c04ce2ad647123c7852bfb5d4a7fd
Paul <pfultz2@yahoo.com>
2025-07-19 19:03:26 -0500

    More c++23 fixes

--- src/onnx/parse_gridsample.cpp
+++ src/onnx/parse_gridsample.cpp
@@ -168,7 +168,7 @@ struct grid_sampler
         return std::accumulate(std::next(instructions.begin()),
                                instructions.end(),
                                instructions.front(),
-                               [&info](auto& ret, auto& ins) {
+                               [&info](const auto& ret, const auto& ins) {
                                    return info.add_instruction(
                                        make_op("concat", {{"axis", 0}}), ret, ins);
                                });
@@ -181,7 +181,7 @@ struct grid_sampler
         return std::accumulate(std::next(instructions.begin()),
                                instructions.end(),
                                instructions.front(),
-                               [&info, &dim](auto& ret, auto& ins) {
+                               [&info, &dim](const auto& ret, const auto& ins) {
                                    return info.add_instruction(
                                        make_op("concat", {{"axis", dim}}), ret, ins);
                                });
@@ -530,7 +530,7 @@ struct bicubic_sampler : grid_sampler
         auto weights_t = std::accumulate(std::next(corner_weights.begin()),
                                          corner_weights.end(),
                                          corner_weights.front(),
-                                         [&info](auto& acc, auto& ins) {
+                                         [&info](const auto& acc, const auto& ins) {
                                              return info.add_instruction(
                                                  make_op("concat", {{"axis", 1}}), acc, ins);
                                          });

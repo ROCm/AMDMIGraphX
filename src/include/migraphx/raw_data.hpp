2521d1b4c57efdc97d4eae9f7f8099aca415a83c
charlie <charlie.lin@amd.com>
2025-07-31 15:32:49 -0500

    Fix unused var

--- src/include/migraphx/raw_data.hpp
+++ src/include/migraphx/raw_data.hpp
@@ -204,7 +204,6 @@ struct raw_data : raw_data_base
     template <class T>
     T* cast() const
     {
-        auto&& s      = static_cast<const Derived&>(*this).get_shape();
         auto&& buffer = static_cast<const Derived&>(*this).data();
         assert(s.computable() and static_cast<const Derived&>(*this).get_shape().type() ==
                                       migraphx::shape::get_type<T>{});

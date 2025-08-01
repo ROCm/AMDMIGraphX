d2858f375ade92ab5bd89b918edbf20a7bb0d174
Gyula Chinoradszki <gyula.chinoradszki@htecgroup.com>
2025-08-01 11:31:51 -0500

    first test for batchnorm

--- test/op/builder/batch_norm_test.cpp
+++ test/op/builder/batch_norm_test.cpp
@@ -22,10 +22,48 @@
  * THE SOFTWARE.
  */
 
+#include <migraphx/common.hpp>
 #include <test.hpp>
+#include <migraphx/program.hpp>
+#include <migraphx/op/builder/insert.hpp>
 
-TEST_CASE(test_1)
+TEST_CASE(batch_norm_1d_op_builder_test)
 {
-    std::cout <<"hello!\n";
+    migraphx::program prog_op_builded = migraphx::program();
+    migraphx::program prog_manually_built = migraphx::program();
+    auto* mm_op = prog_op_builded.get_main_module();
+    auto* mm    = prog_manually_built.get_main_module();
+
+    auto x     = mm->add_parameter("x", {migraphx::shape::half_type, {2, 3, 4}});
+    auto scale = mm->add_parameter("scale", {migraphx::shape::float_type, {3}});
+    auto bias  = mm->add_parameter("bias", {migraphx::shape::float_type, {3}});
+    auto mean  = mm->add_parameter("mean", {migraphx::shape::float_type, {3}});
+    auto var   = mm->add_parameter("variance", {migraphx::shape::float_type, {3}});
+  
+    // copy input parameters to the module that'll be built by op-builder
+    *mm_op = *mm;
+
+    auto eps = mm->add_literal(migraphx::literal{migraphx::shape::half_type, {1e-5f}});
+
+    auto usq_scale = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1}}}), scale);
+    auto usq_bias  = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1}}}), bias);
+    auto usq_mean  = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1}}}), mean);
+    auto usq_var   = mm->add_instruction(migraphx::make_op("unsqueeze", {{"axes", {1}}}), var);
+
+    auto x_sub_mean = add_common_op(*mm, migraphx::make_op("sub"), {x, usq_mean});
+    auto var_eps    = add_common_op(*mm, migraphx::make_op("add"), {usq_var, eps});
+    auto rsqrt      = mm->add_instruction(migraphx::make_op("rsqrt"), var_eps);
+    auto mul0       = add_common_op(*mm, migraphx::make_op("mul"), {usq_scale, rsqrt});
+    auto r0         = add_common_op(*mm, migraphx::make_op("mul"), {x_sub_mean, mul0});
+    add_common_op(*mm, migraphx::make_op("add"), {r0, usq_bias});
+
+    // obtain arguments
+    const auto& from = mm_op->get_parameters();
+    std::vector<migraphx::instruction_ref> args{from.rbegin(), from.rend()};
+    
+    // call the SUT
+    migraphx::op::builder::add("batchnorm", *mm_op, args, {}).at(0);
+
+    EXPECT(prog_op_builded == prog_manually_built);
 }
 

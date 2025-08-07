7f3861059106d51cabb8fba52c517577a50f08d5
Paul <pfultz2@yahoo.com>
2025-08-06 19:16:37 -0500

    Update comment style

--- test/instruction.cpp
+++ test/instruction.cpp
@@ -1,26 +1,26 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 
 #include <migraphx/instruction.hpp>
 #include <migraphx/program.hpp>
@@ -89,11 +89,11 @@ TEST_CASE(check_replace_dag)
 }
 
 // Tests for the reaches function
-/*
- * Linear graph:
- *
- * x --> relu --> tanh --> abs
- */
+//
+// Linear graph:
+//
+// x --> relu --> tanh --> abs
+//
 TEST_CASE(reaches_direct_connection)
 {
     migraphx::module m;
@@ -118,22 +118,22 @@ TEST_CASE(reaches_direct_connection)
     EXPECT(migraphx::reaches(abs, abs, &m));
 }
 
-/*
- * Branched graph:
- *
- *     x       y
- *      \     /
- *       \   /
- *        v v
- *        add
- *       /   \
- *      v     v
- *   relu1   relu2
- *      \     /
- *       \   /
- *        v v
- *      concat
- */
+//
+// Branched graph:
+//
+//     x       y
+//      \     /
+//       \   /
+//        v v
+//        add
+//       /   \
+//      v     v
+//   relu1   relu2
+//      \     /
+//       \   /
+//        v v
+//      concat
+//
 TEST_CASE(reaches_branched_connections)
 {
     migraphx::module m;
@@ -165,25 +165,25 @@ TEST_CASE(reaches_branched_connections)
     EXPECT(not migraphx::reaches(relu1, relu2, &m));
 }
 
-/*
- * Complex diamond graph:
- *
- *     x       y
- *      \     /
- *       \   /
- *        v v
- *        add
- *       /   \
- *      v     v
- *   relu    tanh
- *     |      |
- *     v      v
- *    abs     sin
- *      \    /
- *       \  /
- *        vv
- *      concat
- */
+//
+// Complex diamond graph:
+//
+//     x       y
+//      \     /
+//       \   /
+//        v v
+//        add
+//       /   \
+//      v     v
+//   relu    tanh
+//     |      |
+//     v      v
+//    abs     sin
+//      \    /
+//       \  /
+//        vv
+//      concat
+//
 TEST_CASE(reaches_complex_graph1)
 {
     migraphx::module m;
@@ -255,11 +255,11 @@ TEST_CASE(reaches_complex_graph2)
 }
 
 // Tests for the is_interdependent function
-/*
- * Linear chain:
- *
- * x --> relu --> tanh --> abs
- */
+//
+// Linear chain:
+//
+// x --> relu --> tanh --> abs
+//
 TEST_CASE(is_interdependent_simple)
 {
     migraphx::module m;
@@ -286,18 +286,18 @@ TEST_CASE(is_interdependent_simple)
     EXPECT(migraphx::is_interdependent(empty, &m, m.begin()));
 }
 
-/*
- * Branched Y graph:
- *
- *     x       y
- *      \     /
- *       \   /
- *        v v
- *        add
- *       /   \
- *      v     v
- *   relu    tanh
- */
+//
+// Branched Y graph:
+//
+//     x       y
+//      \     /
+//       \   /
+//        v v
+//        add
+//       /   \
+//      v     v
+//   relu    tanh
+//
 TEST_CASE(is_interdependent_branched)
 {
     migraphx::module m;
@@ -321,22 +321,22 @@ TEST_CASE(is_interdependent_branched)
     EXPECT(migraphx::is_interdependent(mixed, &m, m.begin()));
 }
 
-/*
- * Complex graph with multiple paths:
- *
- *     x       y       z
- *      \     / \     /
- *       \   /   \   /
- *        v v     v v
- *       add1    add2
- *        |       |
- *        v       v
- *      relu1   relu2
- *        \     /
- *         \   /
- *          v v
- *        concat
- */
+//
+// Complex graph with multiple paths:
+//
+//     x       y       z
+//      \     / \     /
+//       \   /   \   /
+//        v v     v v
+//       add1    add2
+//        |       |
+//        v       v
+//      relu1   relu2
+//        \     /
+//         \   /
+//          v v
+//        concat
+//
 TEST_CASE(is_interdependent_complex)
 {
     migraphx::module m;
@@ -363,15 +363,15 @@ TEST_CASE(is_interdependent_complex)
     EXPECT(not migraphx::is_interdependent(indep_outputs, &m, m.begin()));
 }
 
-/*
- * Long chain:
- *
- *   x     y
- *    \   /
- *     \ /
- *      v
- *     add --> relu1 --> relu2 --> ... --> relu9
- */
+//
+// Long chain:
+//
+//   x     y
+//    \   /
+//     \ /
+//      v
+//     add --> relu1 --> relu2 --> ... --> relu9
+//
 TEST_CASE(is_interdependent_large_set)
 {
     migraphx::module m;
@@ -399,11 +399,11 @@ TEST_CASE(is_interdependent_large_set)
 }
 
 // Tests for the find_instructions_between function
-/*
- * Linear chain:
- *
- * x --> relu --> tanh --> abs
- */
+//
+// Linear chain:
+//
+// x --> relu --> tanh --> abs
+//
 TEST_CASE(find_instructions_between_simple)
 {
     migraphx::module m;
@@ -434,22 +434,22 @@ TEST_CASE(find_instructions_between_simple)
     EXPECT(result2.size() == 3);
 }
 
-/*
- * Branched Y graph:
- *
- *     x       y
- *      \     /
- *       \   /
- *        v v
- *        add
- *       /   \
- *      v     v
- *   relu    tanh
- *      \     /
- *       \   /
- *        v v
- *      concat
- */
+//
+// Branched Y graph:
+//
+//     x       y
+//      \     /
+//       \   /
+//        v v
+//        add
+//       /   \
+//      v     v
+//   relu    tanh
+//      \     /
+//       \   /
+//        v v
+//      concat
+//
 TEST_CASE(find_instructions_between_branched)
 {
     migraphx::module m;
@@ -484,28 +484,28 @@ TEST_CASE(find_instructions_between_branched)
     EXPECT(result2.size() == 5);
 }
 
-/*
- * Complex diamond graph with multiple inputs:
- *
- *    w     x     y     z
- *     \   /      \   /
- *      \ /        \ /
- *       v          v
- *     add1       add2
- *       \         /
- *        \       /
- *         \     /
- *          \   /
- *           v v
- *           mul
- *          /   \
- *         v     v
- *       relu   tanh
- *         \     /
- *          \   /
- *           v v
- *         concat
- */
+//
+// Complex diamond graph with multiple inputs:
+//
+//    w     x     y     z
+//     \   /      \   /
+//      \ /        \ /
+//       v          v
+//     add1       add2
+//       \         /
+//        \       /
+//         \     /
+//          \   /
+//           v v
+//           mul
+//          /   \
+//         v     v
+//       relu   tanh
+//         \     /
+//          \   /
+//           v v
+//         concat
+//
 TEST_CASE(find_instructions_between_complex)
 {
     migraphx::module m;

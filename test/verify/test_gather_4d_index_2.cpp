84b1d04ec1a33327e8df31f72f224dd61750e144
lakhinderwalia <lakhinder.walia@amd.com>
2025-08-14 13:13:45 -0700

    break 3 axis tests into multiple test files

--- test/verify/test_gather_4d_index_2.cpp
+++ test/verify/test_gather_4d_index_2.cpp
@@ -27,22 +27,6 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/make_op.hpp>
 
-struct test_gather_4d_index_0 : verify_program<test_gather_4d_index_0>
-{
-    migraphx::program create_program() const
-    {
-        migraphx::program p;
-        auto* mm = p.get_main_module();
-        migraphx::shape s{migraphx::shape::int8_type, {4, 2, 2, 1}};
-        migraphx::shape s_indices{migraphx::shape::int32_type, {2, 4}};
-        std::vector<int> indices{3, 2, 1, 0, 0, 1, 2, 3};
-        auto a0 = mm->add_parameter("data", s);
-        auto a1 = mm->add_literal(migraphx::literal{s_indices, indices});
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", 0}}), a0, a1);
-        return p;
-    }
-};
-
 struct test_gather_4d_index_2 : verify_program<test_gather_4d_index_2>
 {
     migraphx::program create_program() const
@@ -55,22 +39,6 @@ struct test_gather_4d_index_2 : verify_program<test_gather_4d_index_2>
         auto a0 = mm->add_parameter("data", s);
         auto a1 = mm->add_literal(migraphx::literal{s_indices, indices});
         mm->add_instruction(migraphx::make_op("gather", {{"axis", 2}}), a0, a1);
-        return p;
-    }
-};
-
-struct test_gather_4d_index_3 : verify_program<test_gather_4d_index_3>
-{
-    migraphx::program create_program() const
-    {
-        migraphx::program p;
-        auto* mm = p.get_main_module();
-        migraphx::shape s{migraphx::shape::int8_type, {4, 2, 2, 1}};
-        migraphx::shape s_indices{migraphx::shape::int32_type, {2, 2}};
-        std::vector<int> indices{0, 0, 0, 0};
-        auto a0 = mm->add_parameter("data", s);
-        auto a1 = mm->add_literal(migraphx::literal{s_indices, indices});
-        mm->add_instruction(migraphx::make_op("gather", {{"axis", -1}}), a0, a1);
         return p;
     }
 };

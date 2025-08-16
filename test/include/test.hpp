d434a3233bf4e210f1a1b668b66d18769c5d5e38
Paul <pfultz2@yahoo.com>
2025-08-16 00:08:41 +0000

    Format

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -175,7 +175,8 @@ template <class Stream,
           class Range,
           class = typename std::enable_if<not std::is_convertible<Range, std::string>{}>::type>
 auto print_stream_impl(rank<4>, Stream& s, const Range& v) -> decltype(v.end(),
-                                                                       s << *v.begin(), void())
+                                                                       s << *v.begin(),
+                                                                       void())
 {
     auto start = v.begin();
     auto last  = v.end();

2220f37c0ebfb95e4d4e4f76e8aaed84bf65990a
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2025-09-10 08:15:21 -0500

    remove unused variables to fix cppcheck

--- src/targets/gpu/fixed_pad.cpp
+++ src/targets/gpu/fixed_pad.cpp
@@ -40,8 +40,6 @@ argument hip_fixed_pad::compute(context& ctx, const shape&, const std::vector<ar
 {
     if(args.front().get_shape() == args.back().get_shape())
         return args.front();
-    auto input_shape  = args.front().get_shape();
-    auto output_shape = args.back().get_shape();
 
     return device::fixed_pad(ctx.get_stream().get(), args.back(), args.front());
 }

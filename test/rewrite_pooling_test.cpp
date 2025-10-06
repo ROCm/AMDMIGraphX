c7cf3ee72fb6c4fbdbc69d2d7e8fabf909464779
aarushjain29 <Aarushi.Jain2@amd.com>
2025-10-06 14:50:04 +0000

    remove flag in test and formatting

--- test/rewrite_pooling_test.cpp
+++ test/rewrite_pooling_test.cpp
@@ -314,8 +314,6 @@ TEST_CASE(rewrite_pooling_dialtions_test5)
 
 TEST_CASE(test_lower_lrn_to_pooling)
 {
-    if(not migraphx::enabled(MIGRAPHX_REWRITE_LRN{}))
-        return;
     migraphx::module m1;
     {
         migraphx::shape input_shape{migraphx::shape::float_type, {1, 64, 55, 55}};

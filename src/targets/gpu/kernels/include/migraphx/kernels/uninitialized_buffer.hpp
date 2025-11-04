64b797d82e709f7c99e61de24081e53977d52245
Paul <pfultz2@yahoo.com>
2025-11-04 17:30:20 -0600

    Format

--- src/targets/gpu/kernels/include/migraphx/kernels/uninitialized_buffer.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/uninitialized_buffer.hpp
@@ -6,34 +6,31 @@
 
 namespace migraphx {
 
-template<typename T, index_int N>
-struct uninitialized_buffer {
+template <typename T, index_int N>
+struct uninitialized_buffer
+{
     // Use aligned char storage to avoid initialization
     alignas(T) char storage[sizeof(T) * N];
-    
-    __device__ T* data() {
-        return reinterpret_cast<T*>(storage);
-    }
-    
-    __device__ const T* data() const {
-        return reinterpret_cast<const T*>(storage);
-    }
+
+    __device__ T* data() { return reinterpret_cast<T*>(storage); }
+
+    __device__ const T* data() const { return reinterpret_cast<const T*>(storage); }
 
     // Array-like interface
-    __device__ T& operator[](index_int i) {
+    __device__ T& operator[](index_int i)
+    {
         MIGRAPHX_ASSERT(i < N);
         return data()[i];
     }
-    
-    __device__ const T& operator[](index_int i) const {
+
+    __device__ const T& operator[](index_int i) const
+    {
         MIGRAPHX_ASSERT(i < N);
         return data()[i];
     }
-    
-    __device__ constexpr index_int size() const {
-        return N;
-    }
-    
+
+    __device__ constexpr index_int size() const { return N; }
+
     // Iterator support (if needed)
     __device__ T* begin() { return data(); }
     __device__ T* end() { return data() + N; }

c6569fcd400d2d2e294d54e81e1a76f67f34167a
Paul <pfultz2@yahoo.com>
2026-01-07 20:05:54 +0000

    Year update

--- tools/cppcheck/test/empty_while_statement.cpp
+++ tools/cppcheck/test/empty_while_statement.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2025-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

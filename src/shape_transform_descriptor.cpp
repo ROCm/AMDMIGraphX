7e93d6e8ff184bb8d2c0d5a60b11264b31973883
Lagos <klagos@amd.com>
2026-01-23 15:06:55 -0500

    format

--- src/shape_transform_descriptor.cpp
+++ src/shape_transform_descriptor.cpp
@@ -790,8 +790,10 @@ shape_transform_descriptor shape_transform_descriptor::rebase(const std::vector<
             return {};
     }
     for(auto& dim : result.dimensions)
+	{
         remove_empty_sub_dims(dim.subdimensions);
-	result.simplify();
+	}
+    result.simplify();
     if(broadcast and not is_broadcast_only(dimensions, result.dimensions))
         return {};
 

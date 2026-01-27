4fa66778891f6f3c86c5de26b62858494d545b25
Shiv <Shivad.Bhavsar@amd.com>
2026-01-27 10:59:47 -0800

    fix compute_shapes lens check

--- src/module.cpp
+++ src/module.cpp
@@ -717,12 +717,13 @@ std::vector<shape> module::compute_shapes(const std::vector<shape>& inputs,
                                ins->get_shape().type_string() + " but passed " +
                                ins_shapes[ins].type_string());
             }
-            if(not ins->get_shape().dynamic() and options.strict_lens and
-               ins->get_shape().lens() != ins_shapes[ins].lens())
+            if(options.strict_lens and not shape::is_compatible(ins_shapes[ins], ins->get_shape()))
             {
-                MIGRAPHX_THROW(options.name + ": Mismatched lens: expected {" +
-                               to_string_range(ins->get_shape().lens()) + "} but passed {" +
-                               to_string_range(ins_shapes[ins].lens()) + "}");
+                MIGRAPHX_THROW(options.name + ": Mismatched lens, strides: expected {" +
+                               to_string_range(ins->get_shape().lens()) + "}, {" +
+                               to_string_range(ins->get_shape().strides()) + "} but passed {" +
+                               to_string_range(ins_shapes[ins].lens()) + "}, {" +
+                               to_string_range(ins_shapes[ins].strides()) + "}");
             }
         }
         else if(ins->name() == "@literal")

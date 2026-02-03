d0aa8b8a98eb9b503bf72e20dc09f6f5912db427
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2026-02-02 19:00:38 -0600

    refactor and simplify, update test

--- test/onnx/parse/clip_test_args_type_mismatch.cpp
+++ test/onnx/parse/clip_test_args_type_mismatch.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,5 +26,27 @@
 
 TEST_CASE(clip_test_args_type_mismatch)
 {
-    EXPECT(test::throws([&] { read_onnx("clip_test_args_type_mismatch.onnx"); }));
+    migraphx::program p;
+    auto* mm = p.get_main_module();
+
+    auto input = mm->add_parameter("0", migraphx::shape{migraphx::shape::float_type, {3, 3}});
+    auto min_val =
+        mm->add_literal(migraphx::literal{migraphx::shape{migraphx::shape::float_type, {1, 3}},
+                                          {1.5f, 2.5f, 3.5f}});
+    auto max_val =
+        mm->add_literal(migraphx::literal{migraphx::shape{migraphx::shape::int64_type, {3, 1}},
+                                          {2, 3, 4}});
+
+    auto min_bc = mm->add_instruction(
+        migraphx::make_op("multibroadcast", {{"out_lens", {3, 3}}}), min_val);
+    auto max_bc = mm->add_instruction(
+        migraphx::make_op("multibroadcast", {{"out_lens", {3, 3}}}), max_val);
+    auto max_converted = mm->add_instruction(
+        migraphx::make_op("convert", {{"target_type", migraphx::shape::float_type}}), max_bc);
+
+    auto clip = mm->add_instruction(migraphx::make_op("clip"), input, min_bc, max_converted);
+    mm->add_return({clip});
+
+    auto prog = read_onnx("clip_test_args_type_mismatch.onnx");
+    EXPECT(p.sort() == prog.sort());
 }

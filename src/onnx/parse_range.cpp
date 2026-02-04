03d1ca61dc78a36fd4aa9b17f18d6e8b9f06149a
kazhang2 <kazhang2@amd.com>
2026-02-03 21:44:09 -0800

    range op support dynamic shape

--- src/onnx/parse_range.cpp
+++ src/onnx/parse_range.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,11 +41,13 @@ struct parse_range : op_parser<parse_range>
                           std::vector<instruction_ref> args) const
     {
         auto start_arg = args[0]->eval();
-        check_arg_empty(start_arg, "PARSE_RANGE: start arg dynamic shape is not supported");
         auto limit_arg = args[1]->eval();
-        check_arg_empty(limit_arg, "PARSE_RANGE: limit arg dynamic shape is not supported");
         auto delta_arg = args[2]->eval();
-        check_arg_empty(delta_arg, "PARSE_RANGE: delta arg dynamic shape is not supported");
+
+        if(start_arg.empty() or limit_arg.empty() or delta_arg.empty())
+        {
+            return info.add_instruction(make_op("range"), args);
+        }
 
         assert(args[0]->get_shape().elements() == 1 and args[1]->get_shape().elements() == 1 and
                args[2]->get_shape().elements() == 1);

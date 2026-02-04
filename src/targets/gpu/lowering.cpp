b85386b169bdd296b5c3d1d03aeeb2ca18c219b3
kazhang2 <kazhang2@amd.com>
2026-02-03 21:44:09 -0800

    range op support dynamic shape

--- src/targets/gpu/lowering.cpp
+++ src/targets/gpu/lowering.cpp
@@ -125,6 +125,7 @@ struct miopen_apply
         add_reshape_lazy_op();
         add_concat_past_present_op();
         add_scan_slice_op();
+        add_range_op();
     }
 
     void copy_params() const
@@ -489,6 +490,26 @@ struct miopen_apply
             auto cpu_out = mod->insert_instruction(ins, ins->get_operator(), cpu_inputs);
             auto gpu_out =
                 mod->insert_instruction(ins, make_op("hip::copy_to_gpu"), cpu_out, output);
+            return mod->replace_instruction(ins, gpu_out);
+        });
+    }
+
+    void add_range_op()
+    {
+        apply_map.emplace("range", [=](instruction_ref ins) {
+            auto s      = ins->get_shape();
+            std::vector<instruction_ref> cpu_inputs;
+            auto inputs = ins->inputs();
+            std::transform(
+                inputs.begin(), inputs.end(), std::back_inserter(cpu_inputs), [&](auto in) {
+                    return mod->insert_instruction(ins, make_op("hip::copy_from_gpu"), in);
+                });
+            cpu_inputs.front() =
+                mod->insert_instruction(ins, make_op("hip::sync_stream"), cpu_inputs);
+            auto cpu_out = mod->insert_instruction(ins, ins->get_operator(), cpu_inputs);
+            /* We use copy_to_gpu_alloc instead of copy_to_gpu because it allocates memory based on the runtime-determined
+            range size rather than pre-allocating the maximum possible size numeric_limits<std::size_t>::max() */
+            auto gpu_out = mod->insert_instruction(ins, make_op("hip::copy_to_gpu_alloc"), cpu_out);
             return mod->replace_instruction(ins, gpu_out);
         });
     }

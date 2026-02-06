f2074d005e1019113fe42002bfeec9688871c541
Paul <pfultz2@yahoo.com>
2026-02-05 19:11:17 -0600

    Format

--- src/simplify_reshapes.cpp
+++ src/simplify_reshapes.cpp
@@ -1064,7 +1064,7 @@ struct find_gather
                 return std::make_pair(arithmetic_segment{}, begin);
             if(length == 1)
                 return std::make_pair(*begin, std::next(begin));
-            auto start  = *begin;
+            auto start   = *begin;
             auto lstride = std::next(begin)->base - start.base;
             if(lstride < 0)
                 return std::make_pair(*begin, std::next(begin));

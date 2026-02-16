1792edbb745ae0465e9e0024fb6a8eb996c16334
Paul <pfultz2@yahoo.com>
2026-02-16 16:23:08 +0000

    Update slice functions

--- src/targets/gpu/kernels/include/migraphx/kernels/slice.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/slice.hpp
@@ -21,129 +21,138 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#ifndef MIGRAPHX_GUARD_KERNELS_SLICE_HPP
-#define MIGRAPHX_GUARD_KERNELS_SLICE_HPP
-
-#include <migraphx/kernels/shape.hpp>
-#include <migraphx/kernels/tensor_view.hpp>
-#include <migraphx/kernels/index.hpp>
-
-namespace migraphx {
-
-template <class Shape, class Size>
-constexpr auto slice_make_multi_lens(Shape, Size)
-{
-    return return_array_c([] {
-        auto n     = Size{} - _c<1>;
-        auto i     = Shape{}.multi(n);
-        using type = typename decltype(i)::value_type;
-        return i + type{1};
-    });
-}
-
-template <class Shape, class T, T... Xs>
-constexpr auto slice_make_multi_lens(Shape, integral_const_array<T, Xs...> x)
-{
-    return x;
-}
-
-template <class Shape, class Select>
-constexpr auto make_slice(Shape, Select select)
-{
-    auto inner_lens = transform_i(Shape{}.lens, [=](index_int x, index_int ii) -> index_int {
-        if(select(x, ii, Shape{}.lens.size()))
-            return x;
-        return 1;
-    });
-    return make_shape(inner_lens, Shape{}.strides);
-}
-
-template <class Shape, class Select, class Size>
-constexpr auto make_slice(Shape input, Select select, Size size)
-{
-    auto as   = make_slice(input, select);
-    auto lens = slice_make_multi_lens(as, size);
-    return make_shape(lens, Shape{}.strides);
-}
-
-template <class F>
-struct slice_size_transform
-{
-    F f;
-
-    template <class... Ts>
-    constexpr auto operator()(Ts... xs) const
-    {
-        return f(xs...);
-    }
-};
-MIGRAPHX_AUTO_DEDUCE(slice_size_transform);
-
-template <class Shape, class Select, class F>
-constexpr auto make_slice(Shape input, Select select, slice_size_transform<F> t)
-{
-    auto as   = make_slice(input, select);
-    auto lens = slice_make_multi_lens(as, decltype(t(input, as)){});
-    return make_shape(lens, Shape{}.strides);
-}
-
-template <class Shape, class... Ss>
-constexpr auto nslices(Shape input, Ss... ss)
-{
-    auto as = make_slice(input, ss...);
-    return input.elements() / as.elements();
-}
-
-template <index_int N>
-constexpr auto slice_group()
-{
-    return slice_size_transform{[](auto input, auto s) {
-        auto r = return_array_c([] {
-            auto lens = decltype(s){}.lens.base();
-            lens.back() *= N;
-            lens -= 1;
-            return decltype(input){}.lens.carry(lens) + index_int{1};
-        });
-        return r;
-    }};
-}
-
-template <index_int N>
-constexpr auto slice_split()
-{
-    return slice_size_transform{[](auto, auto s) { return s.elements() / _c<N>; }};
-}
-
-template <diff_int... Axes>
-constexpr auto slice_axes()
-{
-    return [](auto, auto i, auto n) { return ((Axes < 0 ? i == (n + Axes) : i == Axes) or ...); };
-}
-
-template <class Input, class T, class... Ss>
-constexpr auto slice_tensor(Input input, T start, Ss... ss)
-{
-    constexpr auto inner_shape = make_slice(get_shape_c<Input>{}, ss...);
-    auto outer_lens            = transform(get_shape_c<Input>{}.lens,
-                                inner_shape.lens,
-                                [=](auto x, auto inner) { return 1 + x - inner; });
-    auto outer_shape           = make_shape(outer_lens, get_shape_c<Input>{}.strides);
-    auto offset                = outer_shape.index(start);
-    MIGRAPHX_ASSERT((offset + inner_shape.element_space()) <= get_shape_c<Input>{}.element_space());
-    return make_tensor_view(input.data() + offset, inner_shape);
-}
-
-template <class Schedule, class... Ss>
-constexpr auto slice_schedule(index idx, Ss... ss)
-{
-    return [=](auto... xs) {
-        return [=](auto f) {
-            // TODO: Assert nslices is the same for all xs
-            constexpr auto n = nslices(get_shape_c<decltype(arg_c<0>()(xs...))>{}, ss...);
-            Schedule{idx}.group_stride(n, [&](auto i) { f(slice_tensor(xs, i, ss...)...); });
-        };
-    };
-}
-
-} // namespace migraphx
-#endif // MIGRAPHX_GUARD_KERNELS_SLICE_HPP
+ #ifndef MIGRAPHX_GUARD_KERNELS_SLICE_HPP
+ #define MIGRAPHX_GUARD_KERNELS_SLICE_HPP
+ 
+ #include <migraphx/kernels/shape.hpp>
+ #include <migraphx/kernels/tensor_view.hpp>
+ #include <migraphx/kernels/index.hpp>
+ 
+ namespace migraphx {
+ 
+ template <class Shape, class Size>
+ constexpr auto slice_make_multi_lens(Shape, Size)
+ {
+     return return_array_c([] {
+         auto n     = Size{} - _c<1>;
+         auto i     = Shape{}.multi(n);
+         using type = typename decltype(i)::value_type;
+         return i + type{1};
+     });
+ }
+ 
+ template <class Shape, class T, T... Xs>
+ constexpr auto slice_make_multi_lens(Shape, integral_const_array<T, Xs...> x)
+ {
+     return x;
+ }
+ 
+ template <class Shape, class Select>
+ constexpr auto make_slice(Shape, Select select)
+ {
+     auto inner_lens = transform_i(Shape{}.lens, [=](index_int x, index_int ii) -> index_int {
+         if(select(x, ii, Shape{}.lens.size()))
+             return x;
+         return 1;
+     });
+     return make_shape(inner_lens, Shape{}.strides);
+ }
+ 
+ template <class Shape, class Select, class Size>
+ constexpr auto make_slice(Shape input, Select select, Size size)
+ {
+     auto as   = make_slice(input, select);
+     auto lens = slice_make_multi_lens(as, size);
+     return make_shape(lens, Shape{}.strides);
+ }
+ 
+ template <class F>
+ struct slice_size_transform
+ {
+     F f;
+ 
+     template <class... Ts>
+     constexpr auto operator()(Ts... xs) const
+     {
+         return f(xs...);
+     }
+ };
+ MIGRAPHX_AUTO_DEDUCE(slice_size_transform);
+ 
+ template <class Shape, class Select, class F>
+ constexpr auto make_slice(Shape input, Select select, slice_size_transform<F> t)
+ {
+     auto as   = make_slice(input, select);
+     auto lens = slice_make_multi_lens(as, decltype(t(input, as)){});
+     return make_shape(lens, Shape{}.strides);
+ }
+ 
+ template <class Shape, class... Ss>
+ constexpr auto nslices(Shape input, Ss... ss)
+ {
+     auto as = make_slice(input, ss...);
+     return input.elements() / as.elements();
+ }
+ 
+ template <index_int N>
+ constexpr auto slice_group()
+ {
+     return slice_size_transform{[](auto input, auto s) {
+         auto r = return_array_c([] {
+             auto lens = decltype(s){}.lens.base();
+             lens.back() *= N;
+             lens -= 1;
+             return decltype(input){}.lens.carry(lens) + index_int{1};
+         });
+         return r;
+     }};
+ }
+ 
+ template <index_int N>
+ constexpr auto slice_split()
+ {
+     return slice_size_transform{[](auto, auto s) { return s.elements() / _c<N>; }};
+ }
+ 
+ template <diff_int... Axes>
+ constexpr auto slice_axes()
+ {
+     return [](auto, auto i, auto n) { return ((Axes < 0 ? i == (n + Axes) : i == Axes) or ...); };
+ }
+ 
+ template <class Input, class T, class... Ss>
+ constexpr auto slice_tensor(Input input, T start, Ss... ss)
+ {
+     constexpr auto inner_shape = make_slice(get_shape_c<Input>{}, ss...);
+     auto outer_lens            = transform(
+         get_shape_c<Input>{}.lens, inner_shape.lens, [=](auto x, auto inner) { return x / inner; });
+     // TODO: Handle non-divisble dimensions
+     auto outer_shape = make_shape(outer_lens, get_shape_c<Input>{}.strides * inner_shape.lens);
+     auto offset                = outer_shape.index(start);
+     MIGRAPHX_ASSERT(outer_shape.elements() * inner_shape.elements() ==
+                     input.get_shape().elements());
+     MIGRAPHX_ASSERT((offset + inner_shape.element_space()) <= get_shape_c<Input>{}.element_space());
+     return make_tensor_view(input.data() + offset, inner_shape);
+ }
+ 
+ template <class Schedule, class... Ss>
+ constexpr auto slice_schedule(index idx, Ss... ss)
+ {
+     return [=](auto... xs) {
+         return [=](auto f) {
+             constexpr auto first = get_shape_c<decltype(arg_c<0>()(xs...))>{};
+             constexpr auto n     = nslices(first, ss...);
+             MIGRAPHX_ASSERT(((n == nslices(get_shape_c<decltype(xs)>{}, ss...)) and ...));
+             Schedule{idx}.group_stride(n, [&](auto i) {
+                 MIGRAPHX_ASSERT(((slice_tensor(xs, i, ss...).get_shape().elements() * n ==
+                                   xs.get_shape().elements()) and
+                                  ...));
+                 f(slice_tensor(xs, i, ss...)...);
+             });
+         };
+     };
+ }
+ 
+ } // namespace migraphx
+ #endif // MIGRAPHX_GUARD_KERNELS_SLICE_HPP
+ 

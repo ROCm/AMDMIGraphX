4651be70e2fb985da8c7f0f69155ef51d135c224
Paul Fultz II <paul.fultz@amd.com>
2026-03-05 07:47:26 -0600

    Dont use double or int64 when using fast_math (#4641)

--- src/targets/gpu/target.cpp
+++ src/targets/gpu/target.cpp
@@ -116,7 +116,7 @@ std::vector<pass> target::get_passes(migraphx::context& gctx, const compile_opti
         dead_code_elimination{},
         rewrite_rnn{},
         dead_code_elimination{},
-        eliminate_data_type_for_gpu{},
+        eliminate_data_type_for_gpu{.disable_64bit = options.fast_math},
         rewrite_resize{.affine_only = true},
         dead_code_elimination{},
         simplify_reshapes{.enable_gather_rewrite = true},

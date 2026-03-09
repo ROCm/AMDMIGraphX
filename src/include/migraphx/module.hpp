19f61659da4d4daf42ae0c2015339ca39ac86728
charlie <charlie.lin@amd.com>
2026-03-09 14:28:36 -0500

    Comment formatting

--- src/include/migraphx/module.hpp
+++ src/include/migraphx/module.hpp
@@ -138,7 +138,7 @@ struct MIGRAPHX_EXPORT module
     };
 
     /// Replaces an array of instructions within the same function to properly handle debug symbols
-    /// propagation Returns vector of instruction_ref to replaced instructions
+    /// propagation. Returns vector of instruction_ref to replaced instructions.
     std::vector<instruction_ref> batch_replace_instruction(
         const std::vector<instruction_replacer>& replacers) MIGRAPHX_TIDY_CONST;
 

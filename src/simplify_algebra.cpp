a46c3dd656a73a124f2232525fca076f70f07093
Uros Petkovic <urpetkov@amd.com>
2026-03-12 00:07:32 +0100

    Deleting unnecessary code

--- src/simplify_algebra.cpp
+++ src/simplify_algebra.cpp
@@ -1033,8 +1033,8 @@ struct find_concat_op
         };
         auto pred = [](auto i, auto j) {
             return i->get_operator() == j->get_operator() and
-                   i->inputs().size() == j->inputs().size() and
-                   i->outputs().size() == j->outputs().size();
+                   i->inputs().size() == i->inputs().size() and
+                   i->outputs().size() == i->outputs().size();
         };
         group_unique(ins->inputs().begin(), ins->inputs().end(), update_args, pred);
         if(args.size() == 1)
@@ -2224,14 +2224,11 @@ struct find_conv_broadcast_input
         else
             x_2d = m.insert_instruction(
                 ins, make_op("reshape", {{"dims", std::vector<std::size_t>{n, ic}}}), x_ins);
-
         auto dot_result = m.insert_instruction(ins, make_op("dot"), x_2d, w_t);
-
         if(n == 1)
         {
             auto dot_1d = m.insert_instruction(
                 ins, make_op("squeeze", {{"axes", std::vector<int64_t>{0}}}), dot_result);
-
             m.replace_instruction(
                 ins, make_op("broadcast", {{"axis", 1}, {"out_lens", out_lens}}), dot_1d);
         }
@@ -2241,7 +2238,6 @@ struct find_conv_broadcast_input
             std::iota(unsqueeze_axes.begin(), unsqueeze_axes.end(), 2);
             auto dot_unsqueezed = m.insert_instruction(
                 ins, make_op("unsqueeze", {{"axes", unsqueeze_axes}}), dot_result);
-
             m.replace_instruction(
                 ins, make_op("multibroadcast", {{"out_lens", out_lens}}), dot_unsqueezed);
         }

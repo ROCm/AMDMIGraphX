c426b7ba6bbf0a596984b04c84789410f81733ef
Breanna Devore-McDonald <breanna.devore-mcdonald@amd.com>
2026-03-13 11:28:40 -0500

    bugs and edge case tests

--- test/verify/test_fill.cpp
+++ test/verify/test_fill.cpp
@@ -45,8 +45,11 @@ struct test_fill : verify_program<test_fill<DType>>
 
 template struct test_fill<migraphx::shape::float_type>;
 template struct test_fill<migraphx::shape::half_type>;
+template struct test_fill<migraphx::shape::bf16_type>;
 template struct test_fill<migraphx::shape::int32_type>;
 template struct test_fill<migraphx::shape::int64_type>;
+template struct test_fill<migraphx::shape::fp8e4m3fnuz_type>;
+template struct test_fill<migraphx::shape::fp8e4m3fn_type>;
 
 template <migraphx::shape::type_t DType>
 struct test_fill_large : verify_program<test_fill_large<DType>>
@@ -66,6 +69,7 @@ struct test_fill_large : verify_program<test_fill_large<DType>>
 
 template struct test_fill_large<migraphx::shape::float_type>;
 template struct test_fill_large<migraphx::shape::half_type>;
+template struct test_fill_large<migraphx::shape::bf16_type>;
 
 template <migraphx::shape::type_t DType>
 struct test_fill_literal : verify_program<test_fill_literal<DType>>
@@ -85,3 +89,49 @@ struct test_fill_literal : verify_program<test_fill_literal<DType>>
 
 template struct test_fill_literal<migraphx::shape::float_type>;
 template struct test_fill_literal<migraphx::shape::int32_type>;
+
+
+struct test_fill_single_element : verify_program<test_fill_single_element>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        auto* mm = p.get_main_module();
+        migraphx::shape scalar_shape{migraphx::shape::float_type, {1}, {0}};
+        migraphx::shape data_shape{migraphx::shape::float_type, {1}};
+        auto value = mm->add_parameter("value", scalar_shape);
+        auto data  = mm->add_parameter("data", data_shape);
+        mm->add_instruction(migraphx::make_op("fill"), value, data);
+        return p;
+    }
+};
+
+struct test_fill_1d : verify_program<test_fill_1d>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        auto* mm = p.get_main_module();
+        migraphx::shape scalar_shape{migraphx::shape::float_type, {1}, {0}};
+        migraphx::shape data_shape{migraphx::shape::float_type, {256}};
+        auto value = mm->add_parameter("value", scalar_shape);
+        auto data  = mm->add_parameter("data", data_shape);
+        mm->add_instruction(migraphx::make_op("fill"), value, data);
+        return p;
+    }
+};
+
+struct test_fill_2d : verify_program<test_fill_2d>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        auto* mm = p.get_main_module();
+        migraphx::shape scalar_shape{migraphx::shape::float_type, {1}, {0}};
+        migraphx::shape data_shape{migraphx::shape::float_type, {16, 32}};
+        auto value = mm->add_parameter("value", scalar_shape);
+        auto data  = mm->add_parameter("data", data_shape);
+        mm->add_instruction(migraphx::make_op("fill"), value, data);
+        return p;
+    }
+};

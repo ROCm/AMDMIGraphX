6a469c3c870a61edae3c5360baec13bfb1649ae2
Gyula Chinoradszki <gyula.chinoradszki@htecgroup.com>
2026-03-17 08:53:23 -0500

    replace tabs to spaces

--- src/common_api/NvBuilderConfig_impl.cpp
+++ src/common_api/NvBuilderConfig_impl.cpp
@@ -86,293 +86,293 @@ namespace nvinfer1
 
     bool NvBuilderConfig_impl::isDeviceTypeSet(ILayer const* layer) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     void NvBuilderConfig_impl::resetDeviceType(ILayer const* layer) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     bool NvBuilderConfig_impl::canRunOnDLA(ILayer const* layer) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     void NvBuilderConfig_impl::setDLACore(int32_t dlaCore) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     int32_t NvBuilderConfig_impl::getDLACore() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     void NvBuilderConfig_impl::setDefaultDeviceType(DeviceType deviceType) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     DeviceType NvBuilderConfig_impl::getDefaultDeviceType() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::reset() noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setProfileStream(const hipStream_t stream) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     hipStream_t NvBuilderConfig_impl::getProfileStream() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     int32_t NvBuilderConfig_impl::addOptimizationProfile(IOptimizationProfile const* profile) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     int32_t NvBuilderConfig_impl::getNbOptimizationProfiles() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     void NvBuilderConfig_impl::setProfilingVerbosity(ProfilingVerbosity verbosity) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     ProfilingVerbosity NvBuilderConfig_impl::getProfilingVerbosity() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setAlgorithmSelector(IAlgorithmSelector* selector) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     IAlgorithmSelector* NvBuilderConfig_impl::getAlgorithmSelector() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     bool NvBuilderConfig_impl::setCalibrationProfile(IOptimizationProfile const* profile) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     IOptimizationProfile const* NvBuilderConfig_impl::getCalibrationProfile() noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     void NvBuilderConfig_impl::setQuantizationFlags(QuantizationFlags flags) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     QuantizationFlags NvBuilderConfig_impl::getQuantizationFlags() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::clearQuantizationFlag(QuantizationFlag flag) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setQuantizationFlag(QuantizationFlag flag) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     bool NvBuilderConfig_impl::getQuantizationFlag(QuantizationFlag flag) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     bool NvBuilderConfig_impl::setTacticSources(TacticSources tacticSources) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     TacticSources NvBuilderConfig_impl::getTacticSources() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     nvinfer1::ITimingCache* NvBuilderConfig_impl::createTimingCache(void const* blob, std::size_t size) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     bool NvBuilderConfig_impl::setTimingCache(ITimingCache const& cache, bool ignoreMismatch) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     nvinfer1::ITimingCache const* NvBuilderConfig_impl::getTimingCache() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     void NvBuilderConfig_impl::setMemoryPoolLimit(MemoryPoolType pool, std::size_t poolSize) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     std::size_t NvBuilderConfig_impl::getMemoryPoolLimit(MemoryPoolType pool) const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setPreviewFeature(PreviewFeature feature, bool enable) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     bool NvBuilderConfig_impl::getPreviewFeature(PreviewFeature feature) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     void NvBuilderConfig_impl::setBuilderOptimizationLevel(int32_t level) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     int32_t NvBuilderConfig_impl::getBuilderOptimizationLevel() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     void NvBuilderConfig_impl::setHardwareCompatibilityLevel(HardwareCompatibilityLevel hardwareCompatibilityLevel) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     HardwareCompatibilityLevel NvBuilderConfig_impl::getHardwareCompatibilityLevel() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setPluginsToSerialize(char const* const* paths, int32_t nbPaths) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     char const* NvBuilderConfig_impl::getPluginToSerialize(int32_t index) const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     int32_t NvBuilderConfig_impl::getNbPluginsToSerialize() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     void NvBuilderConfig_impl::setMaxAuxStreams(int32_t nbStreams) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     int32_t NvBuilderConfig_impl::getMaxAuxStreams() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     void NvBuilderConfig_impl::setProgressMonitor(IProgressMonitor* monitor) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     IProgressMonitor* NvBuilderConfig_impl::getProgressMonitor() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
     void NvBuilderConfig_impl::setRuntimePlatform(RuntimePlatform runtimePlatform) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     RuntimePlatform NvBuilderConfig_impl::getRuntimePlatform() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     void NvBuilderConfig_impl::setMaxNbTactics(int32_t maxTactics) noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     int32_t NvBuilderConfig_impl::getMaxNbTactics() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     bool NvBuilderConfig_impl::setTilingOptimizationLevel(TilingOptimizationLevel level) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     TilingOptimizationLevel NvBuilderConfig_impl::getTilingOptimizationLevel() const noexcept
     {
-		// TODO! implement
-	}
+        // TODO! implement
+    }
 
     bool NvBuilderConfig_impl::setL2LimitForTiling(int64_t size) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     int64_t NvBuilderConfig_impl::getL2LimitForTiling() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return 0;
-	}
+    }
 
     bool NvBuilderConfig_impl::setRemoteAutoTuningConfig(char const* config) noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return false;
-	}
+    }
 
     char const* NvBuilderConfig_impl::getRemoteAutoTuningConfig() const noexcept
     {
-		// TODO! implement
+        // TODO! implement
         return nullptr;
-	}
+    }
 
 } // ns:nvinfer1
 

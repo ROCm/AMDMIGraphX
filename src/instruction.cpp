6671be0180acf2a2e32b26256237efa6a8475f9c
charlie <charlie.lin@amd.com>
2026-03-17 19:09:52 -0500

    Edit debug symbols print

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -454,7 +454,7 @@ void instruction::print(std::ostream& os,
     // print debug symbols if they exist
     if(not ins->debug_symbols.empty())
     {
-        os << " # " << join_strings(ins->debug_symbols, ", ") << " #";
+        os << " # " << join_strings(ins->debug_symbols, ", ");
     }
 }
 
@@ -491,7 +491,7 @@ void instruction::debug_print() const
     // print debug symbols if they exist
     if(not debug_symbols.empty())
     {
-        std::cout << " # " << join_strings(debug_symbols, ", ") << " #";
+        std::cout << " # " << join_strings(debug_symbols, ", ");
     }
     std::cout << std::endl;
 }

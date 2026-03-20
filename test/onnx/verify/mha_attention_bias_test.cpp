94b461a4c34c6af0718b543db3035a74d2feb781
Uros Petkovic <urpetkov@amd.com>
2026-03-20 17:07:03 +0100

    Adding verify tests

--- test/onnx/verify/mha_attention_bias_test.cpp
+++ test/onnx/verify/mha_attention_bias_test.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,24 +26,37 @@
 #include <migraphx/verify.hpp>
 #include <onnx_test.hpp>
 
-TEST_CASE(multi_head_attention_kv_packed_test)
+TEST_CASE(mha_bias_key_padding_mask_attention_bias_test)
 {
-    migraphx::program p = read_onnx("mha_kv_packed_test.onnx");
+    migraphx::program p = read_onnx("mha_attention_bias_test.onnx");
     p.compile(migraphx::make_target("ref"));
 
     migraphx::shape q_s{migraphx::shape::float_type, {1, 2, 4}};
-    migraphx::shape kv_s{migraphx::shape::float_type, {1, 2, 2, 2, 2}};
-    std::vector<float> query     = {1, 3, 5, 7, 2, 4, 6, 8};
-    std::vector<float> key_value = {1, 3, 1, 3, 5, 7, 5, 7, 2, 4, 2, 4, 6, 8, 6, 8};
+    migraphx::shape k_s{migraphx::shape::float_type, {1, 2, 4}};
+    migraphx::shape v_s{migraphx::shape::float_type, {1, 2, 4}};
+    migraphx::shape bias_s{migraphx::shape::float_type, {12}};
+    migraphx::shape mask_s{migraphx::shape::int32_type, {1, 2}};
+    migraphx::shape attn_bias_s{migraphx::shape::float_type, {1, 2, 2, 2}};
+
+    std::vector<float> query = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<float> key = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<float> value = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<float> bias_data(12, 0);
+    std::vector<int32_t> mask_data = {1, 1};
+    std::vector<float> attn_bias_data(8, 0);
 
     migraphx::parameter_map pp;
-    pp["q"]  = migraphx::argument(q_s, query.data());
-    pp["kv"] = migraphx::argument(kv_s, key_value.data());
+    pp["q"]                = migraphx::argument(q_s, query.data());
+    pp["k"]                = migraphx::argument(k_s, key.data());
+    pp["v"]                = migraphx::argument(v_s, value.data());
+    pp["bias"]             = migraphx::argument(bias_s, bias_data.data());
+    pp["key_padding_mask"] = migraphx::argument(mask_s, mask_data.data());
+    pp["attention_bias"]   = migraphx::argument(attn_bias_s, attn_bias_data.data());
 
     auto result = p.eval(pp).back();
     std::vector<float> result_vector;
     result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
-    std::vector<float> gold = {
-        1.9441926, 3.9441926, 5.9997935, 7.999794, 1.9858339, 3.9858341, 5.99995, 7.9999495};
+
+    std::vector<float> gold = {4.99917, 5.99917, 7, 8, 5, 6, 7, 8};
     EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
 }

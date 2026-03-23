6d77f5f650c0acdd467a677fb0d1b64e0428fe06
Ted Themistokleous <ted.themistokleous@amd.com>
2026-03-23 21:32:52 +0000

    Fix comment

--- src/targets/gpu/compile_gen.cpp
+++ src/targets/gpu/compile_gen.cpp
@@ -234,7 +234,7 @@ tile tile::elements(const std::vector<shape>& inputs, std::size_t noutputs)
     auto dim1      = compute_tile_factor(s.lens()[result.axis]);
     auto dim2      = compute_tile_factor(s.lens().back(), 4096 / dim1);
     auto tile_size = dim1 * dim2;
-    // equivalent to dim1 * (dim2 + 1) to avoid bank conflicts
+    // equivalent to dim2 * (dim1 + 1) to avoid bank conflicts
     auto tile_bytes = (tile_size + dim2) * s.type_size();
 
     if(dim1 == 1 or dim2 == 1 or tile_bytes > 65536)

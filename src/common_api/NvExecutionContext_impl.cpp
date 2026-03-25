abc7f7628dd25a4a07ef17da7580492d592a58e2
Gyula Chinoradszki <gyula.chinoradszki@htecgroup.com>
2026-03-25 07:04:50 -0500

    introducing NvExecutionContext skeleton class

---
+++ src/common_api/NvExecutionContext_impl.cpp
@@ -0,0 +1,315 @@
+#include "NvExecutionContext_impl.hpp"
+
+namespace nvinfer1
+{
+
+NvExecutionContext_impl::NvExecutionContext_impl(void* logger, int32_t version) noexcept
+    : mEngine(nullptr)
+{
+    // TODO! implement
+    mImpl = this;
+}
+
+NvExecutionContext_impl::~NvExecutionContext_impl()
+{
+    // TODO! implement
+}
+
+IExecutionContext* NvExecutionContext_impl::getPImpl() noexcept
+{
+    // TODO! implement
+    return this;
+}
+
+void NvExecutionContext_impl::setDebugSync(bool sync) noexcept
+{
+    // TODO! implement
+}
+
+bool NvExecutionContext_impl::getDebugSync() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+void NvExecutionContext_impl::setProfiler(IProfiler*) noexcept
+{
+    // TODO! implement
+}
+
+IProfiler* NvExecutionContext_impl::getProfiler() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+ICudaEngine const& NvExecutionContext_impl::getEngine() const noexcept
+{
+    // TODO! implement
+    return *mEngine;
+}
+
+void NvExecutionContext_impl::setName(char const* name) noexcept
+{
+    // TODO! implement
+}
+
+char const* NvExecutionContext_impl::getName() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+void NvExecutionContext_impl::setDeviceMemory(void* memory) noexcept
+{
+    // TODO! implement
+}
+
+int32_t NvExecutionContext_impl::getOptimizationProfile() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvExecutionContext_impl::allInputDimensionsSpecified() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::allInputShapesSpecified() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+void NvExecutionContext_impl::setErrorRecorder(IErrorRecorder* recorder) noexcept
+{
+    // TODO! implement
+}
+
+IErrorRecorder* NvExecutionContext_impl::getErrorRecorder() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvExecutionContext_impl::executeV2(void* const* bindings) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::setOptimizationProfileAsync(int32_t profileIndex, hipStream_t stream) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+void NvExecutionContext_impl::setEnqueueEmitsProfile(bool enqueueEmitsProfile) noexcept
+{
+    // TODO! implement
+}
+
+bool NvExecutionContext_impl::getEnqueueEmitsProfile() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::reportToProfiler() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::setInputShape(char const* tensorName, Dims const& dims) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+Dims NvExecutionContext_impl::getTensorShape(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return Dims{};
+}
+
+Dims NvExecutionContext_impl::getTensorStrides(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return Dims{};
+}
+
+bool NvExecutionContext_impl::setTensorAddress(char const* tensorName, void* data) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+void const* NvExecutionContext_impl::getTensorAddress(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvExecutionContext_impl::setInputTensorAddress(char const* tensorName, void const* data) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::setOutputTensorAddress(char const* tensorName, void* data) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+int32_t NvExecutionContext_impl::inferShapes(int32_t nbMaxNames, char const** tensorNames) noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvExecutionContext_impl::setInputConsumedEvent(hipEvent_t event) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+hipEvent_t NvExecutionContext_impl::getInputConsumedEvent() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+void* NvExecutionContext_impl::getOutputTensorAddress(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvExecutionContext_impl::setOutputAllocator(char const* tensorName, IOutputAllocator* outputAllocator) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+IOutputAllocator* NvExecutionContext_impl::getOutputAllocator(char const* name) noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+int64_t NvExecutionContext_impl::getMaxOutputSize(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvExecutionContext_impl::setTemporaryStorageAllocator(IGpuAllocator* allocator) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+IGpuAllocator* NvExecutionContext_impl::getTemporaryStorageAllocator() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvExecutionContext_impl::enqueueV3(hipStream_t stream) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+void NvExecutionContext_impl::setPersistentCacheLimit(size_t size) noexcept
+{
+    // TODO! implement
+}
+
+size_t NvExecutionContext_impl::getPersistentCacheLimit() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvExecutionContext_impl::setNvtxVerbosity(ProfilingVerbosity verbosity) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+ProfilingVerbosity NvExecutionContext_impl::getNvtxVerbosity() const noexcept
+{
+    // TODO! implement
+    return ProfilingVerbosity::kLAYER_NAMES_ONLY;
+}
+
+void NvExecutionContext_impl::setAuxStreams(hipStream_t* auxStreams, int32_t nbStreams) noexcept
+{
+    // TODO! implement
+}
+
+bool NvExecutionContext_impl::setDebugListener(IDebugListener* listener) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+IDebugListener* NvExecutionContext_impl::getDebugListener() noexcept
+{
+    // TODO! implement
+    return nullptr;
+}   
+
+bool NvExecutionContext_impl::setTensorDebugState(char const* name, bool flag) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::getDebugState(char const* name) const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::setAllTensorsDebugState(bool flag) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+size_t NvExecutionContext_impl::updateDeviceMemorySizeForShapes() noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+void NvExecutionContext_impl::setDeviceMemoryV2(void* memory, int64_t size) noexcept
+{
+    // TODO! implement
+}
+
+TRT_NODISCARD IRuntimeConfig* NvExecutionContext_impl::getRuntimeConfig() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvExecutionContext_impl::setUnfusedTensorsDebugState(bool flag) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+bool NvExecutionContext_impl::getUnfusedTensorsDebugState() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+}   // ns:nvinfer1
---
+++ src/common_api/NvExecutionContext_impl.hpp
@@ -0,0 +1,75 @@
+#ifndef NV_EXECUTION_CONTEXT_IMPL_HPP
+#define NV_EXECUTION_CONTEXT_IMPL_HPP
+
+#include <memory>
+#include "migraphx/common_api/NvInferRuntime.h"
+
+namespace nvinfer1
+{
+    class NvExecutionContext_impl : public IExecutionContext, public apiv::VExecutionContext
+    {
+    public:
+        NvExecutionContext_impl(void* logger, int32_t version) noexcept;
+        ~NvExecutionContext_impl() override;
+
+        // public API
+    public:
+        IExecutionContext* getPImpl() noexcept override;
+        void setDebugSync(bool sync) noexcept override;
+        bool getDebugSync() const noexcept override;
+        void setProfiler(IProfiler*) noexcept override;
+        IProfiler* getProfiler() const noexcept override;
+        ICudaEngine const& getEngine() const noexcept override;
+        void setName(char const* name) noexcept override;
+        char const* getName() const noexcept override;
+        void setDeviceMemory(void* memory) noexcept override;
+        int32_t getOptimizationProfile() const noexcept override;
+        bool allInputDimensionsSpecified() const noexcept override;
+        bool allInputShapesSpecified() const noexcept override;
+        void setErrorRecorder(IErrorRecorder* recorder) noexcept override;
+        IErrorRecorder* getErrorRecorder() const noexcept override;
+        bool executeV2(void* const* bindings) noexcept override;
+        bool setOptimizationProfileAsync(int32_t profileIndex, hipStream_t stream) noexcept override;
+        void setEnqueueEmitsProfile(bool enqueueEmitsProfile) noexcept override;
+        bool getEnqueueEmitsProfile() const noexcept override;
+        bool reportToProfiler() const noexcept override;
+        bool setInputShape(char const* tensorName, Dims const& dims) noexcept override;
+        Dims getTensorShape(char const* tensorName) const noexcept override;
+        Dims getTensorStrides(char const* tensorName) const noexcept override;
+        bool setTensorAddress(char const* tensorName, void* data) noexcept override;
+        void const* getTensorAddress(char const* tensorName) const noexcept override;
+        bool setInputTensorAddress(char const* tensorName, void const* data) noexcept override;
+        bool setOutputTensorAddress(char const* tensorName, void* data) noexcept override;
+        int32_t inferShapes(int32_t nbMaxNames, char const** tensorNames) noexcept override;
+        bool setInputConsumedEvent(hipEvent_t event) noexcept override;
+        hipEvent_t getInputConsumedEvent() const noexcept override;
+        void* getOutputTensorAddress(char const* tensorName) const noexcept override;
+        bool setOutputAllocator(char const* tensorName, IOutputAllocator* outputAllocator) noexcept override;
+        IOutputAllocator* getOutputAllocator(char const* name) noexcept override;
+        int64_t getMaxOutputSize(char const* tensorName) const noexcept override;
+        bool setTemporaryStorageAllocator(IGpuAllocator* allocator) noexcept override;
+        IGpuAllocator* getTemporaryStorageAllocator() const noexcept override;
+        bool enqueueV3(hipStream_t stream) noexcept override;
+        void setPersistentCacheLimit(size_t size) noexcept override;
+        size_t getPersistentCacheLimit() const noexcept override;
+        bool setNvtxVerbosity(ProfilingVerbosity verbosity) noexcept override;
+        ProfilingVerbosity getNvtxVerbosity() const noexcept override;
+        void setAuxStreams(hipStream_t* auxStreams, int32_t nbStreams) noexcept override;
+        bool setDebugListener(IDebugListener* listener) noexcept override;
+        IDebugListener* getDebugListener() noexcept override;
+        bool setTensorDebugState(char const* name, bool flag) noexcept override;
+        bool getDebugState(char const* name) const noexcept override;
+        bool setAllTensorsDebugState(bool flag) noexcept override;
+        size_t updateDeviceMemorySizeForShapes() noexcept override;
+        void setDeviceMemoryV2(void* memory, int64_t size) noexcept override;
+        TRT_NODISCARD IRuntimeConfig* getRuntimeConfig() const noexcept override;
+        bool setUnfusedTensorsDebugState(bool flag) noexcept override;
+        bool getUnfusedTensorsDebugState() const noexcept override;
+
+    private:
+        std::unique_ptr<ICudaEngine> mEngine;
+    };
+
+} // namespace nvinfer1
+
+#endif // NV_EXECUTION_CONTEXT_IMPL_HPP

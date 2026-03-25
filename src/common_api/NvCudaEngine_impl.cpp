d50541f40b46e66d1940bddc5b0ca2f0764af298
Gyula Chinoradszki <gyula.chinoradszki@htecgroup.com>
2026-03-25 05:28:57 -0500

    introducing NvCudaEngine skeleton class

---
+++ src/common_api/NvCudaEngine_impl.cpp
@@ -0,0 +1,353 @@
+#include "migraphx/common_api/NvInfer.h"
+#include "NvCudaEngine_impl.hpp"
+
+namespace nvinfer1
+{
+
+NvCudaEngine_impl::NvCudaEngine_impl(void* logger, int32_t version) noexcept
+{
+    // TODO! implement
+    mImpl = this;
+}
+
+NvCudaEngine_impl::~NvCudaEngine_impl()
+{
+    // TODO! implement
+}
+
+ICudaEngine* NvCudaEngine_impl::getPImpl() noexcept
+{
+    // TODO! implement
+    return this;
+}
+
+int32_t NvCudaEngine_impl::getNbLayers() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+IHostMemory* NvCudaEngine_impl::serialize() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+IExecutionContext* NvCudaEngine_impl::createExecutionContext(ExecutionContextAllocationStrategy strategy) noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+IExecutionContext* NvCudaEngine_impl::createExecutionContextWithoutDeviceMemory() noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+size_t NvCudaEngine_impl::getDeviceMemorySize() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvCudaEngine_impl::isRefittable() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+char const* NvCudaEngine_impl::getName() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+int32_t NvCudaEngine_impl::getNbOptimizationProfiles() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int32_t const* NvCudaEngine_impl::getProfileTensorValues(
+    char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+EngineCapability NvCudaEngine_impl::getEngineCapability() const noexcept
+{
+    // TODO! implement
+    return EngineCapability::kSTANDARD;
+}
+
+void NvCudaEngine_impl::setErrorRecorder(IErrorRecorder* recorder) noexcept
+{
+    // TODO! implement
+}
+
+IErrorRecorder* NvCudaEngine_impl::getErrorRecorder() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvCudaEngine_impl::hasImplicitBatchDimension() const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+TacticSources NvCudaEngine_impl::getTacticSources() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+ProfilingVerbosity NvCudaEngine_impl::getProfilingVerbosity() const noexcept
+{
+    // TODO! implement
+    return ProfilingVerbosity::kLAYER_NAMES_ONLY;
+}
+
+IEngineInspector* NvCudaEngine_impl::createEngineInspector() const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+Dims NvCudaEngine_impl::getTensorShape(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return Dims{};
+}
+
+DataType NvCudaEngine_impl::getTensorDataType(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return DataType::kFLOAT;
+}
+
+TensorLocation NvCudaEngine_impl::getTensorLocation(char const* tensorName) const noexcept
+{   
+    // TODO! implement
+    return TensorLocation::kDEVICE;
+}
+
+bool NvCudaEngine_impl::isShapeInferenceIO(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+TensorIOMode NvCudaEngine_impl::getTensorIOMode(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return TensorIOMode::kNONE;
+}
+
+int32_t NvCudaEngine_impl::getTensorBytesPerComponent(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int32_t NvCudaEngine_impl::getTensorComponentsPerElement(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+TensorFormat NvCudaEngine_impl::getTensorFormat(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return TensorFormat::kLINEAR;
+}
+
+char const* NvCudaEngine_impl::getTensorFormatDesc(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+int32_t NvCudaEngine_impl::getTensorVectorizedDim(char const* tensorName) const noexcept
+{
+    // TODO! implement
+    return -1;
+}
+
+Dims NvCudaEngine_impl::getProfileShape(
+    char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept
+{
+    // TODO! implement
+    return Dims{};
+}
+
+int32_t NvCudaEngine_impl::getNbIOTensors() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+char const* NvCudaEngine_impl::getIOTensorName(int32_t index) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+HardwareCompatibilityLevel NvCudaEngine_impl::getHardwareCompatibilityLevel() const noexcept
+{
+    // TODO! implement
+    return HardwareCompatibilityLevel::kNONE;
+}
+
+int32_t NvCudaEngine_impl::getNbAuxStreams() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int32_t NvCudaEngine_impl::getTensorBytesPerComponentV2(char const* tensorName, int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int32_t NvCudaEngine_impl::getTensorComponentsPerElementV2(char const* tensorName, int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+TensorFormat NvCudaEngine_impl::getTensorFormatV2(char const* tensorName, int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return TensorFormat::kLINEAR;
+}
+
+char const* NvCudaEngine_impl::getTensorFormatDescV2(char const* tensorName, int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+int32_t NvCudaEngine_impl::getTensorVectorizedDimV2(char const* tensorName, int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return -1;
+}
+
+ISerializationConfig* NvCudaEngine_impl::createSerializationConfig() noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+IHostMemory* NvCudaEngine_impl::serializeWithConfig(ISerializationConfig& config) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+size_t NvCudaEngine_impl::getDeviceMemorySizeForProfile(int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+IRefitter* NvCudaEngine_impl::createRefitter(ILogger& logger) noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+bool NvCudaEngine_impl::setWeightStreamingBudget(int64_t gpuMemoryBudget) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+int64_t NvCudaEngine_impl::getWeightStreamingBudget() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getMinimumWeightStreamingBudget() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getStreamableWeightsSize() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+bool NvCudaEngine_impl::isDebugTensor(char const* name) const noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+// Added in TensorRT 10.1
+bool NvCudaEngine_impl::setWeightStreamingBudgetV2(int64_t gpuMemoryBudget) noexcept
+{
+    // TODO! implement
+    return false;
+}
+
+int64_t NvCudaEngine_impl::getWeightStreamingBudgetV2() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getWeightStreamingAutomaticBudget() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getWeightStreamingScratchMemorySize() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getDeviceMemorySizeV2() const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+int64_t NvCudaEngine_impl::getDeviceMemorySizeForProfileV2(int32_t profileIndex) const noexcept
+{
+    // TODO! implement
+    return 0;
+}
+
+// Added in TensorRT 10.11
+TRT_NODISCARD int64_t const* NvCudaEngine_impl::getProfileTensorValuesV2(
+    char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+TRT_NODISCARD IExecutionContext* NvCudaEngine_impl::createExecutionContextWithRuntimeConfig(
+    IRuntimeConfig* runtimeConfig) noexcept
+{
+    // TODO! implement
+    return nullptr;
+}
+
+TRT_NODISCARD IRuntimeConfig* NvCudaEngine_impl::createRuntimeConfig() noexcept
+{
+    // TODO! implement
+    return nullptr; 
+}
+
+} // ns:nvinfer1
---
+++ src/common_api/NvCudaEngine_impl.hpp
@@ -0,0 +1,85 @@
+#ifndef NV_CUDA_ENGINE_IMPL_H
+#define NV_CUDA_ENGINE_IMPL_H
+
+#include "migraphx/common_api/NvInferRuntime.h"
+
+namespace nvinfer1
+{
+    class NvCudaEngine_impl : public ICudaEngine, public apiv::VCudaEngine
+    {
+    public:
+        NvCudaEngine_impl(void* logger, int32_t version) noexcept;
+        ~NvCudaEngine_impl() override;
+
+        // public API
+        ICudaEngine* getPImpl() noexcept override;
+        int32_t getNbLayers() const noexcept override;
+        IHostMemory* serialize() const noexcept override;
+        IExecutionContext* createExecutionContext(ExecutionContextAllocationStrategy strategy) noexcept override;
+        IExecutionContext* createExecutionContextWithoutDeviceMemory() noexcept override;
+        size_t getDeviceMemorySize() const noexcept override;
+        bool isRefittable() const noexcept override;
+        char const* getName() const noexcept override;
+        int32_t getNbOptimizationProfiles() const noexcept override;
+        int32_t const* getProfileTensorValues(
+            char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept override;
+        EngineCapability getEngineCapability() const noexcept override;
+        void setErrorRecorder(IErrorRecorder* recorder) noexcept override;
+        IErrorRecorder* getErrorRecorder() const noexcept override;
+        bool hasImplicitBatchDimension() const noexcept override;
+        TacticSources getTacticSources() const noexcept override;
+        ProfilingVerbosity getProfilingVerbosity() const noexcept override;
+        IEngineInspector* createEngineInspector() const noexcept override;
+        Dims getTensorShape(char const* tensorName) const noexcept override;
+        DataType getTensorDataType(char const* tensorName) const noexcept override;
+        TensorLocation getTensorLocation(char const* tensorName) const noexcept override;
+        bool isShapeInferenceIO(char const* tensorName) const noexcept override;
+        TensorIOMode getTensorIOMode(char const* tensorName) const noexcept override;
+        int32_t getTensorBytesPerComponent(char const* tensorName) const noexcept override;
+        int32_t getTensorComponentsPerElement(char const* tensorName) const noexcept override;
+        TensorFormat getTensorFormat(char const* tensorName) const noexcept override;
+        char const* getTensorFormatDesc(char const* tensorName) const noexcept override;
+        int32_t getTensorVectorizedDim(char const* tensorName) const noexcept override;
+        Dims getProfileShape(
+            char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept override;
+        int32_t getNbIOTensors() const noexcept override;
+        char const* getIOTensorName(int32_t index) const noexcept override;
+        HardwareCompatibilityLevel getHardwareCompatibilityLevel() const noexcept override;
+        int32_t getNbAuxStreams() const noexcept override;
+
+        int32_t getTensorBytesPerComponentV2(char const* tensorName, int32_t profileIndex) const noexcept override;
+        int32_t getTensorComponentsPerElementV2(char const* tensorName, int32_t profileIndex) const noexcept override;
+        TensorFormat getTensorFormatV2(char const* tensorName, int32_t profileIndex) const noexcept override;
+        char const* getTensorFormatDescV2(char const* tensorName, int32_t profileIndex) const noexcept override;
+        int32_t getTensorVectorizedDimV2(char const* tensorName, int32_t profileIndex) const noexcept override;
+
+        ISerializationConfig* createSerializationConfig() noexcept override;
+        IHostMemory* serializeWithConfig(ISerializationConfig& config) const noexcept override;
+
+        size_t getDeviceMemorySizeForProfile(int32_t profileIndex) const noexcept override;
+        IRefitter* createRefitter(ILogger& logger) noexcept override;
+
+        bool setWeightStreamingBudget(int64_t gpuMemoryBudget) noexcept override;
+        int64_t getWeightStreamingBudget() const noexcept override;
+        int64_t getMinimumWeightStreamingBudget() const noexcept override;
+        int64_t getStreamableWeightsSize() const noexcept override;
+
+        bool isDebugTensor(char const* name) const noexcept override;
+
+        // Added in TensorRT 10.1
+        bool setWeightStreamingBudgetV2(int64_t gpuMemoryBudget) noexcept override;
+        int64_t getWeightStreamingBudgetV2() const noexcept override;
+        int64_t getWeightStreamingAutomaticBudget() const noexcept override;
+        int64_t getWeightStreamingScratchMemorySize() const noexcept override;
+        int64_t getDeviceMemorySizeV2() const noexcept override;
+        int64_t getDeviceMemorySizeForProfileV2(int32_t profileIndex) const noexcept override;
+        // Added in TensorRT 10.11
+        TRT_NODISCARD int64_t const* getProfileTensorValuesV2(
+            char const* tensorName, int32_t profileIndex, OptProfileSelector select) const noexcept override;
+        TRT_NODISCARD IExecutionContext* createExecutionContextWithRuntimeConfig(
+            IRuntimeConfig* runtimeConfig) noexcept override;
+        TRT_NODISCARD IRuntimeConfig* createRuntimeConfig() noexcept override;
+    };
+} // ns:nvinfer1
+
+#endif // NV_CUDA_ENGINE_IMPL_H

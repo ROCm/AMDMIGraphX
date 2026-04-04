f8a5d55d04f2ea4b30df971148ae4829ddc33c46
Paul <pfultz2@yahoo.com>
2026-04-04 16:30:29 -0500

    Handle more cases for global pooling

--- src/rewrite_pooling.cpp
+++ src/rewrite_pooling.cpp
@@ -236,11 +236,14 @@ void rewrite_pooling::apply(module& m) const
             s.lens().cbegin() + 2, s.lens().cend(), op.lengths.cbegin(), op.lengths.cend());
         bool default_strides =
             std::all_of(op.stride.cbegin(), op.stride.cend(), [](auto i) { return i == 1; });
+        bool strides_equal_lengths = std::equal(
+            op.stride.cbegin(), op.stride.cend(), op.lengths.cbegin(), op.lengths.cend());
         bool default_padding =
             std::all_of(op.padding.cbegin(), op.padding.cend(), [](auto i) { return i == 0; });
         bool default_dilations =
             std::all_of(op.dilations.cbegin(), op.dilations.cend(), [](auto i) { return i == 1; });
-        if(same_kernel_as_shape and default_strides and default_padding and default_dilations)
+        if(same_kernel_as_shape and (default_strides or strides_equal_lengths) and
+           default_padding and default_dilations)
         {
             replace_with_reduce(m, ins);
         }

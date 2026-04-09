bc7c1281437048fefc318906a1bbfb59db832451
kahmed10 <15948690+kahmed10@users.noreply.github.com>
2026-04-09 17:06:01 -0400

    refactor device name functions

--- src/targets/gpu/device_name.cpp
+++ src/targets/gpu/device_name.cpp
@@ -38,9 +38,10 @@ namespace gpu {
 
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_SET_GEMM_PROVIDER)
 
-std::string get_gfx_name(const std::string& device_name)
+static std::string get_gfx_name(const std::string& device_name)
 {
-    return trim(split_string(device_name, ':').front());
+    const auto& name = device_name.empty() ? get_device_name() : device_name;
+    return trim(split_string(name, ':').front());
 }
 
 int get_device_id()
@@ -61,105 +62,53 @@ std::string get_device_name()
     return props.gcnArchName;
 }
 
-static bool gfx_has_fp8fnuz_intrinsics_impl(const std::string& gfx_name)
+bool gfx_has_fp8fnuz_intrinsics(const std::string& device_name)
 {
+    const auto gfx_name = get_gfx_name(device_name);
     return (starts_with(gfx_name, "gfx94"));
 }
 
-bool gfx_has_fp8fnuz_intrinsics()
+bool gfx_has_fp8ocp_intrinsics(const std::string& device_name)
 {
-    return gfx_has_fp8fnuz_intrinsics_impl(get_gfx_name(get_device_name()));
-}
-
-bool gfx_has_fp8fnuz_intrinsics(const context& ctx)
-{
-    return gfx_has_fp8fnuz_intrinsics_impl(
-        get_gfx_name(ctx.get_current_device().get_device_name()));
-}
-
-static bool gfx_has_fp8ocp_intrinsics_impl(const std::string& gfx_name)
-{
+    const auto gfx_name      = get_gfx_name(device_name);
     bool is_navi_with_fp8ocp = starts_with(gfx_name, "gfx12") and gfx_name >= "gfx1200";
     bool is_mi_with_fp8ocp   = starts_with(gfx_name, "gfx9") and gfx_name >= "gfx950";
     return (is_navi_with_fp8ocp or is_mi_with_fp8ocp);
 }
 
-bool gfx_has_fp8ocp_intrinsics()
+bool gfx_has_bf16_intrinsics(const std::string& device_name)
 {
-    return gfx_has_fp8ocp_intrinsics_impl(get_gfx_name(get_device_name()));
-}
-
-bool gfx_has_fp8ocp_intrinsics(const context& ctx)
-{
-    return gfx_has_fp8ocp_intrinsics_impl(get_gfx_name(ctx.get_current_device().get_device_name()));
-}
-
-static bool gfx_has_bf16_intrinsics_impl(const std::string& gfx_name)
-{
+    const auto gfx_name = get_gfx_name(device_name);
     return not(starts_with(gfx_name, "gfx1030"));
 }
 
-bool gfx_has_bf16_intrinsics()
+bool gfx_has_mx_intrinsics(const std::string& device_name)
 {
-    return gfx_has_bf16_intrinsics_impl(get_gfx_name(get_device_name()));
-}
-
-bool gfx_has_bf16_intrinsics(const context& ctx)
-{
-    return gfx_has_bf16_intrinsics_impl(get_gfx_name(ctx.get_current_device().get_device_name()));
-}
-
-static bool gfx_has_mx_intrinsics_impl(const std::string& gfx_name)
-{
+    const auto gfx_name = get_gfx_name(device_name);
     return starts_with(gfx_name, "gfx9") and gfx_name >= "gfx950";
 }
 
-bool gfx_has_mx_intrinsics() { return gfx_has_mx_intrinsics_impl(get_gfx_name(get_device_name())); }
-
-bool gfx_has_mx_intrinsics(const context& ctx)
-{
-    return gfx_has_mx_intrinsics_impl(get_gfx_name(ctx.get_current_device().get_device_name()));
-}
-
 #if MIGRAPHX_USE_HIPBLASLT
-static bool gfx_default_rocblas_impl(const std::string& gfx_name)
+bool gfx_default_rocblas(const std::string& device_name)
 {
+    const auto gfx_name = get_gfx_name(device_name);
     return ((string_value_of(MIGRAPHX_SET_GEMM_PROVIDER{}) == "hipblaslt")
                 ? false
                 : (gfx_name == "gfx90a"));
 }
-
-bool gfx_default_rocblas() { return gfx_default_rocblas_impl(get_gfx_name(get_device_name())); }
-
-bool gfx_default_rocblas(const context& ctx)
-{
-    return gfx_default_rocblas_impl(get_gfx_name(ctx.get_current_device().get_device_name()));
-}
 #endif
 
-static bool hipblaslt_supported_impl(const std::string& gfx_name)
+bool hipblaslt_supported(const std::string& device_name)
 {
-    return (gfx_name == "gfx90a" or (starts_with(gfx_name, "gfx94") and gfx_name >= "gfx942") or
+#if !MIGRAPHX_USE_HIPBLASLT
+    (void)device_name;
+    return false;
+#else
+    const auto gfx_name = get_gfx_name(device_name);
+    return (gfx_name == "gfx90a" or
+            (starts_with(gfx_name, "gfx94") and gfx_name >= "gfx942") or
             (starts_with(gfx_name, "gfx95") and gfx_name >= "gfx950") or
             starts_with(gfx_name, "gfx110") or starts_with(gfx_name, "gfx120"));
-}
-
-bool hipblaslt_supported()
-{
-#if !MIGRAPHX_USE_HIPBLASLT
-    return false;
-#else
-    return hipblaslt_supported_impl(get_gfx_name(get_device_name()));
-#endif
-}
-
-bool hipblaslt_supported(const context& ctx)
-{
-#if !MIGRAPHX_USE_HIPBLASLT
-    (void)ctx;
-    return false;
-#else
-    return hipblaslt_supported_impl(get_gfx_name(ctx.get_current_device().get_device_name()));
 #endif
 }
 

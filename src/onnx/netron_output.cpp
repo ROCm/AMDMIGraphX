31fd8c150124d57302c6c928fee46e93019e42b4
charlie <charlie.lin@amd.com>
2026-04-14 13:51:50 -0500

    netron output only main module

--- src/onnx/netron_output.cpp
+++ src/onnx/netron_output.cpp
@@ -241,11 +241,10 @@ void write_netron_output(const program& prog, std::ostream& os)
     model.set_ir_version(prog.get_program_file_version());
     model.set_producer_name("AMDMIGraphX");
     model.set_producer_version(prog_value.at("migraphx_version").to<std::string>());
-
-    for(auto* mod : prog.get_modules())
-    {
-        build_graph(model.mutable_graph(), mod);
-    }
+    
+    // only exporting the main module
+    // TODO handle submodules as ONNX subgraphs
+    build_graph(model.mutable_graph(), prog.get_main_module());
 
     model.SerializeToOstream(&os);
 }

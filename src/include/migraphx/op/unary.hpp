03f91d49e8fc4943585d5b7585f70b1208d34d66
Paul <pfultz2@yahoo.com>
2026-05-07 13:19:53 -0500

    Undo binary and unary

--- src/include/migraphx/op/unary.hpp
+++ src/include/migraphx/op/unary.hpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2026 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2015-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -68,9 +68,13 @@ struct unary : op_name<Derived>
     {
         check_shapes{inputs, static_cast<const Derived&>(*this), true}.has(1);
         auto s = inputs.at(0);
-        if(s.dynamic() or s.broadcasted())
+        if(s.dynamic() or s.scalar())
         {
             return s;
+        }
+        else if(s.broadcasted())
+        {
+            return {s.type(), s.lens()};
         }
         else
         {

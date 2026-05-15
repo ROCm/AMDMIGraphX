7d9e8766415f9e3235b8ee830ddb2a265030466b
Paul <pfultz2@yahoo.com>
2026-05-15 14:41:50 -0500

    Fix tile miscompilation

--- src/targets/gpu/kernels/include/migraphx/kernels/tile.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/tile.hpp
@@ -28,6 +28,7 @@
 #include <migraphx/kernels/index.hpp>
 #include <migraphx/kernels/functional.hpp>
 #include <migraphx/kernels/tensor_view.hpp>
+#include <migraphx/kernels/uninitialized_buffer.hpp>
 #include <migraphx/kernels/copy.hpp>
 
 namespace migraphx {
@@ -61,8 +62,8 @@ struct tile
                 using type          = typename T::type;
                 constexpr auto s    = pad_shape(make_packed_shape(get_shape_c<T>{}));
                 constexpr auto size = s.element_space();
-                __shared__ type buffer[size];
-                auto b = make_tensor_view(buffer, s);
+                __shared__ uninitialized_buffer<type, size> buffer;
+                auto b = make_tensor_view(buffer.data(), s);
                 local_tensor_copy(idx, x, b);
                 f(b);
             };
@@ -77,8 +78,8 @@ struct tile
                 using type          = typename T::type;
                 constexpr auto s    = pad_shape(make_packed_shape(get_shape_c<T>{}));
                 constexpr auto size = s.element_space();
-                __shared__ type buffer[size];
-                auto b = make_tensor_view(buffer, s);
+                __shared__ uninitialized_buffer<type, size> buffer;
+                auto b = make_tensor_view(buffer.data(), s);
                 f(b);
                 local_tensor_copy(idx, b, x);
             };

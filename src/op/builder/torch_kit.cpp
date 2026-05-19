49ca2e237ecb66a19ad5567976cce8fbbe3ecf9c
Paul <pfultz2@yahoo.com>
2026-05-19 18:57:33 -0500

    Add lstm op builder

--- src/op/builder/torch_kit.cpp
+++ src/op/builder/torch_kit.cpp
@@ -1,10 +1,53 @@
 
 #include <migraphx/op/builder/kit.hpp>
+#include <migraphx/op/common.hpp>
 
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace op {
 namespace builder {
+
+struct torch_lstm : op_builder<torch_lstm>
+{
+    std::size_t hidden_size = 1;
+    std::vector<operation> actv_funcs{};
+    rnn_direction direction = rnn_direction::forward;
+    float clip              = 0.0f;
+    int input_forget        = 0;
+
+    template <class Self, class F>
+    static auto reflect(Self& self, F f)
+    {
+        return pack(f(self.hidden_size, "hidden_size"),
+                    f(self.actv_funcs, "actv_func"),
+                    f(self.direction, "direction"),
+                    f(self.clip, "clip"),
+                    f(self.input_forget, "input_forget"));
+    }
+
+    static std::vector<std::string> names() { return {"tm::lstm"}; }
+
+    std::vector<instruction_ref>
+    insert(module& m, instruction_ref ins, const std::vector<instruction_ref>& args) const
+    {
+        auto self = *this;
+        if(self.actv_funcs.empty())
+        {
+            self.actv_funcs = {make_op("sigmoid"), make_op("tanh"), make_op("tanh")};
+            if(self.direction == rnn_direction::bidirectional)
+            {
+                self.actv_funcs.insert(self.actv_funcs.end(),
+                                       {make_op("sigmoid"), make_op("tanh"), make_op("tanh")});
+            }
+        }
+        auto hidden_states =
+            m.insert_instruction(ins, make_op("lstm", migraphx::to_value(self)), args);
+        auto last_hs = m.insert_instruction(ins, make_op("rnn_last_hs_output"), hidden_states);
+        auto last_cell =
+            m.insert_instruction(ins, make_op("rnn_last_cell_output"), hidden_states);
+        return {hidden_states, last_hs, last_cell};
+    }
+};
 
 struct torch_kit : kit<torch_kit>
 {
@@ -57,13 +100,6 @@ struct torch_kit : kit<torch_kit>
             "transpose",
             "undefined",
             "unsqueeze",
-        });
-
-        // TODO: Make a wrapper for rnn ops
-        this->ops({
-            "lstm",
-            "rnn_last_cell_output",
-            "rnn_last_hs_output",
         });
     }
 };

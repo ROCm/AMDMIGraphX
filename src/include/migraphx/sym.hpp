361b85de73b9942d4d1de9e15535e2960013a6d5
Paul <pfultz2@yahoo.com>
2026-05-19 19:10:56 -0500

    Add strict_less

--- src/include/migraphx/sym.hpp
+++ src/include/migraphx/sym.hpp
@@ -29,6 +29,7 @@
 #include <limits>
 #include <functional>
 #include <memory>
+#include <optional>
 #include <ostream>
 #include <set>
 #include <string>
@@ -301,6 +302,11 @@ MIGRAPHX_EXPORT expr ceil(expr e);
 MIGRAPHX_EXPORT expr pow(expr x, expr y);
 MIGRAPHX_EXPORT expr min(expr x, expr y);
 MIGRAPHX_EXPORT expr max(expr x, expr y);
+
+MIGRAPHX_EXPORT std::optional<bool>
+strict_less(const expr& a,
+            const expr& b,
+            const std::unordered_map<expr, interval>& vars = {});
 
 // Pattern matching rewrite DSL
 expr pvar(int id);

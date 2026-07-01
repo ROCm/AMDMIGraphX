82d37cc86155f5df9dcd0cd0957cacac57c283be
Paul <pfultz2@yahoo.com>
2026-07-01 13:18:25 -0500

    Fix tidy warnings

--- src/driver/verify.cpp
+++ src/driver/verify.cpp
@@ -150,7 +150,7 @@ static std::vector<argument> run_target(program p,
 bool verify_program(const std::string& name,
                     const program& p,
                     const target& t,
-                    compile_options options,
+                    const compile_options& options,
                     const verify_options& vo,
                     const parameter_map& inputs,
                     verify::tolerance tols)
@@ -181,7 +181,7 @@ bool verify_program(const std::string& name,
 
 void verify_instructions(const program& prog,
                          const target& t,
-                         compile_options options,
+                         const compile_options& options,
                          const verify_options& vo,
                          verify::tolerance tols)
 {
@@ -239,7 +239,7 @@ static bool verify_reduced(program p,
     log::info() << p;
     try
     {
-        return verify_program(std::to_string(n), p, t, options, vo, inputs, tols);
+        return verify_program(std::to_string(n), p, t, std::move(options), vo, inputs, tols);
     }
     catch(const std::exception& e)
     {
@@ -251,7 +251,7 @@ static bool verify_reduced(program p,
 
 void verify_reduced_program(const program& p,
                             const target& t,
-                            compile_options options,
+                            const compile_options& options,
                             const verify_options& vo,
                             const parameter_map& inputs,
                             verify::tolerance tols)
@@ -324,7 +324,7 @@ static std::vector<std::size_t> find_trim_instructions(const module& m)
 
 void verify_bisected_program(const program& p,
                              const target& t,
-                             compile_options options,
+                             const compile_options& options,
                              const verify_options& vo,
                              const parameter_map& inputs,
                              verify::tolerance tols)

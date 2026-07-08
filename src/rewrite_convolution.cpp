ffb49a15cc4ae287035adde70da02cdb19efb623
charlie <charlie.lin@amd.com>
2026-07-08 13:27:12 -0500

    hs & ht -> htilda

--- src/rewrite_convolution.cpp
+++ src/rewrite_convolution.cpp
@@ -57,7 +57,8 @@ inline namespace MIGRAPHX_INLINE_NS {
 ///      out[oc, ho] = sum_{ic, j}( inp[ic, hi] * w[ic, oc, j] )
 ///      ho = hi * S - P + j*D
 ///
-/// Where hi = input spatial index.
+/// Where hi = input spatial index. In this form, the inp/out are the backward convolution's input/output, so
+/// the channel and pstial roles and swapped vs. the forward convolution.
 ///
 /// Idea is to split the filter index to do the upsampling as strided access:
 ///
@@ -66,17 +67,19 @@ inline namespace MIGRAPHX_INLINE_NS {
 /// 
 /// making:
 ///
-///     ho = S*hs + itilda*D - P    (eqn 1)
-///     hs = hi + idot * (D/g)
+///     ho = S*htilda + itilda*D - P    (eqn 1)
+///     htilda = hi + idot * (D/g)
 ///
-/// For a set itilda value, note how (eqn 1) becomes constant except for hs.
+/// For a set itilda value, note how (eqn 1) becomes constant except for htilda.
 /// Continuing with a set itilda value we can define:
 ///
-///     out_itilda[oc, S*hs + itilda*D - P] := outdot_itilda[oc, hs]
+///     out_itilda[oc, S*htilda + itilda*D - P] := outdot_itilda[oc, htilda]
 ///
 /// where outdot_itilda is a packed matrix. Doing some rearranging, substitutions, and flipping the spatial index we get:
 ///
-///     outdot_itilda[oc, hs] = sum_{ic, idot}( inp[ic, hs - (ydot - 1) * (D/g) + idot*(D/g)] * w[ic, oc, (ydot - 1 - idot) * ytilda + itilda] )
+///     outdot_itilda[oc, htilda] = sum_{ic, idot}(
+///         inp[ic, htilda - (ydot - 1) * (D/g) + idot*(D/g)] * w[ic, oc, (ydot - 1 - idot) * ytilda + itilda]
+///     )
 ///     ydot = ceil(y / ytilda)
 ///     y = kernel dimension size
 ///
@@ -314,7 +317,7 @@ residue_set build_partials(module& m,
 }
 
 // Pixel-shuffle reassembly (no dilation): stack the residues on a new trailing axis, then
-// reshape/transpose so residue itilda interleaves into spatial position ht*S + itilda.
+// reshape/transpose so residue itilda interleaves into spatial position htilda*S + itilda.
 instruction_ref reassemble_interleave(module& m,
                                       instruction_ref ins,
                                       const std::vector<instruction_ref>& partials,

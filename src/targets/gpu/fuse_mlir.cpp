5a7f2987c01107b8bc3f465cde7e635c62a476d9
Breanna Devore-McDonald <breanna.devore-mcdonald@amd.com>
2026-07-13 19:06:51 -0500

    change find to contains

--- src/targets/gpu/fuse_mlir.cpp
+++ src/targets/gpu/fuse_mlir.cpp
@@ -1512,11 +1512,11 @@ void fuse_mlir::apply(module_pass_manager& mpm) const
         if(is_navi)
             return mlir_mode::all;
 #if !MIGRAPHX_USE_MIOPEN
-        if(option.find("conv") != std::string_view::npos)
+        if(contains(option, "conv"))
             return mlir_mode::all;
 #endif
 #if !MIGRAPHX_USE_ROCBLAS and !MIGRAPHX_USE_HIPBLASLT
-        if(option == "dot" or option == "fused_dot")
+        if(contains(option, "dot") or contains(option, "fused_dot"))
             return mlir_mode::all;
 #endif
         return std::max(m1, m2);

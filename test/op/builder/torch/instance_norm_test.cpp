4a2ce4ca46fb9d8ba5e5f4fcef9740fe09a47571
shivadbhavsar <105248561+shivadbhavsar@users.noreply.github.com>
2026-07-15 13:26:27 -0700

    Update test/op/builder/torch/instance_norm_test.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/op/builder/torch/instance_norm_test.cpp
+++ test/op/builder/torch/instance_norm_test.cpp
@@ -62,7 +62,6 @@ TEST_CASE(torch_kit_instance_norm_low_rank_op_builder_test)
     const auto f = migraphx::shape::float_type;
     migraphx::module mm;
     mm.add_parameter("x", {f, {4}});
-    EXPECT(test::throws<migraphx::exception>([&] {
-        make_op_module("tm::instance_norm", {{"epsilon", 1e-5f}}, mm.get_parameters());
-    }));
+    EXPECT(test::throws<migraphx::exception>(
+        [&] { make_op_module("tm::instance_norm", {{"epsilon", 1e-5f}}, mm.get_parameters()); }));
 }

68780540bce914943c7f65c9afe2266997fab771
Paul <pfultz2@yahoo.com>
2026-07-16 14:46:07 -0500

    Skip rocblas

--- src/targets/gpu/hipgraphify.cpp
+++ src/targets/gpu/hipgraphify.cpp
@@ -46,7 +46,7 @@ namespace gpu {
 static bool is_unsupported(const std::string& name)
 {
     static const std::unordered_set<std::string> unsupported = {
-        "hip::copy_from_gpu", "hip::copy_to_gpu", "hip::sync_stream"};
+        "hip::copy_from_gpu", "hip::copy_to_gpu", "hip::sync_stream", "gpu::gemm",};
     return contains(unsupported, name);
 }
 

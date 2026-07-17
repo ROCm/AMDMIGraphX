da9b5077027d7126d46b3f682e85dab597dadc91
Chris Austen <christopher.austen@amd.com>
2026-07-16 20:41:19 -0500

    add back tests

--- test/gpu/gemm_tune.cpp
+++ test/gpu/gemm_tune.cpp
@@ -53,8 +53,7 @@ static void run_lowering(migraphx::program& p, bool offload_copy = false)
  * rocBLAS API functions are called to quickly benchmark all the GEMM solutions
  * available in the currently installed rocBLAS library and choose the index of the fastest.
  */
-// TODO: re-enable in ROCm 7.14; gfx90a Tensile kernels missing from TheRock 7.13 rocBLAS package.
-TEST_CASE_SKIP(gemm_tune, "rocBLAS Tensile kernels not available for gfx90a in ROCm 7.13")
+TEST_CASE(gemm_tune)
 {
     migraphx::program p;
     auto* mm = p.get_main_module();

b2594490eb3e969820b7aa78a0d77915ba8da94f
Paul <pfultz2@yahoo.com>
2026-08-28 10:43:14 -0500

    Use builtin for max/min

--- src/targets/gpu/kernels/include/migraphx/kernels/math.hpp
+++ src/targets/gpu/kernels/include/migraphx/kernels/math.hpp
@@ -195,10 +195,6 @@ MIGRAPHX_DEVICE_MATH_FOR(float, abs, ::abs)
 MIGRAPHX_DEVICE_MATH_FOR(double, abs, ::abs)
 MIGRAPHX_DEVICE_MATH_FOR(migraphx::half, abs, ::__habs)
 MIGRAPHX_DEVICE_MATH_FOR(migraphx::bf16, abs, ::fabsf)
-MIGRAPHX_DEVICE_MATH_BINARY_FOR(float, max, ::fmaxf)
-MIGRAPHX_DEVICE_MATH_BINARY_FOR(float, min, ::fminf)
-MIGRAPHX_DEVICE_MATH_BINARY_FOR(double, max, ::max)
-MIGRAPHX_DEVICE_MATH_BINARY_FOR(double, min, ::min)
 
 template <class T, MIGRAPHX_REQUIRES(not is_any_vec<T>() and is_integral<T>{})>
 constexpr auto abs(const T& a)
@@ -209,13 +205,20 @@ constexpr auto abs(const T& a)
 template <class T, MIGRAPHX_REQUIRES(not is_any_vec<T>())>
 constexpr auto max(const T& a, const T& b)
 {
-    return where(a < b, b, a);
+    if constexpr (is_floating_point<T>{})
+        return __builtin_elementwise_maximum(a, b);
+    else
+        return where(a < b, b, a);
 }
 
 template <class T, MIGRAPHX_REQUIRES(not is_any_vec<T>())>
 constexpr auto min(const T& a, const T& b)
 {
-    return where(a < b, a, b);
+    if constexpr (is_floating_point<T>{})
+        return __builtin_elementwise_minimum(a, b);
+    else
+        return where(a < b, a, b);
+
 }
 
 template <class T, class Compare, MIGRAPHX_REQUIRES(not is_any_vec<T>())>
@@ -281,8 +284,8 @@ MIGRAPHX_DEVICE_MATH_VEC(isinf)
 MIGRAPHX_DEVICE_MATH_VEC(isnan)
 MIGRAPHX_DEVICE_MATH_VEC(log)
 MIGRAPHX_DEVICE_MATH_VEC(log2)
-MIGRAPHX_DEVICE_MATH_VEC(max)
-MIGRAPHX_DEVICE_MATH_VEC(min)
+// MIGRAPHX_DEVICE_MATH_VEC(max)
+// MIGRAPHX_DEVICE_MATH_VEC(min)
 MIGRAPHX_DEVICE_MATH_VEC(mod)
 MIGRAPHX_DEVICE_MATH_VEC(nearbyint)
 MIGRAPHX_DEVICE_MATH_VEC(pow)
@@ -295,6 +298,24 @@ MIGRAPHX_DEVICE_MATH_VEC(sqrt)
 MIGRAPHX_DEVICE_MATH_VEC(tan)
 MIGRAPHX_DEVICE_MATH_VEC(tanh)
 MIGRAPHX_DEVICE_MATH_VEC(where)
+
+template <class T, MIGRAPHX_REQUIRES(is_any_vec<T>())>
+constexpr auto min(const T& a, const T& b)
+{
+    if constexpr (is_integral<vec_type<T>>{})
+        return __builtin_elementwise_min(a, b);
+    else    
+        return __builtin_elementwise_minimum(a, b);
+}
+
+template <class T, MIGRAPHX_REQUIRES(is_any_vec<T>())>
+constexpr auto max(const T& a, const T& b)
+{
+    if constexpr (is_integral<vec_type<T>>{})
+        return __builtin_elementwise_max(a, b);
+    else   
+        return __builtin_elementwise_maximum(a, b);
+}
 
 // Map math functions to hip half2 functions
 // The half2 type is defined in include/hip/amd_detail/hip_fp16_gcc.h and is 2 16-bit floats

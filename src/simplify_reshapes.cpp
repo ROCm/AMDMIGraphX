b419c9b402abe26fa173f3fbc110bad49f732b0a
Chris Austen <causten@users.noreply.github.com>
2026-09-02 09:06:14 -0400

    Update src/simplify_reshapes.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/simplify_reshapes.cpp
+++ src/simplify_reshapes.cpp
@@ -819,7 +819,7 @@ struct find_concat_multibroadcasts
 // Offsets of each concat input along the concat axis, from 0 up to the total
 // length of the axis
 std::vector<std::size_t> concat_offsets(const std::vector<instruction_ref>& inputs,
-                                               std::size_t axis)
+                                        std::size_t axis)
 {
     std::vector<std::size_t> result = {0};
     transform_partial_sum(inputs.begin(),

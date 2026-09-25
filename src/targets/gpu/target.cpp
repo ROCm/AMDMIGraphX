1acc859cec829df92f22972e125faee077259e1a
Artur Wojcik <artur.wojcik@amd.com>
2026-09-25 09:30:07 +0200

    Add fused M=1 INT4 GEMV kernel for LLM decode (opt-in) (#5272)

--- src/targets/gpu/target.cpp
+++ src/targets/gpu/target.cpp
@@ -77,6 +77,7 @@
 #include <migraphx/gpu/device_name.hpp>
 #include <migraphx/gpu/eliminate_data_type_for_gpu.hpp>
 #include <migraphx/gpu/fuse_ck.hpp>
+#include <migraphx/gpu/fuse_int4_gemv.hpp>
 #include <migraphx/gpu/fuse_mlir.hpp>
 #include <migraphx/gpu/fuse_ops.hpp>
 #include <migraphx/gpu/prefuse_ops.hpp>
@@ -100,6 +101,7 @@ MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_REWRITE_LRN)
 #ifndef _WIN32
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_ENABLE_CK)
 #endif
+MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_ENABLE_INT4_GEMV)
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_SET_GEMM_PROVIDER)
 MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_ENABLE_FULL_DYNAMIC)
 
@@ -253,6 +255,8 @@ struct pipeline_factory
 #ifndef _WIN32
             enable_pass(enabled(MIGRAPHX_ENABLE_CK{}), fuse_ck{}),
 #endif
+            dead_code_elimination{},
+            enable_pass(enabled(MIGRAPHX_ENABLE_INT4_GEMV{}), fuse_int4_gemv{get_context()}),
             dead_code_elimination{},
             enable_pass(mlir_enabled(), fuse_mlir{get_context()}),
             dead_code_elimination{},
